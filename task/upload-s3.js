dd74e75e0a4cd38286b1f6bb31764bcd78abeb26
竹内佑介 <kaidouji85@gmail.com>
2017-03-18 10:16:04 +0900

    ファイルアップロード処理を追加した

--- task/upload-s3.js
+++ task/upload-s3.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
+const AWS = require('aws-sdk');
+
 const TARGET_BUCKET = 'study-three-js';
 
-const AWS = require('aws-sdk');
 AWS.config.loadFromPath('aws-credentials.json');
 AWS.config.update({region: 'ap-northeast-1'});
 
@@ -12,17 +14,47 @@ const listObjectsV2 = params => new Promise((resolve, reject) => s3.listObjectsV
 const deleteObjects = params => new Promise((resolve, reject) => s3.deleteObjects(params, (err, data) =>
   err ? reject(err) : resolve(data)));
 
+const upload = params => new Promise((resolve, reject) => s3.upload(params, (err, data) =>
+  err ? reject(err) : resolve(data)))
 
-listObjectsV2({
-  Bucket: TARGET_BUCKET
-}).then(data => {
-  const objects = data.Contents.map(item => ({Key: item.Key}));
-  return deleteObjects({
-    Bucket: TARGET_BUCKET,
-    Delete: {
-      Objects: objects,
-      Quiet: true
+
+/**
+ * アップロード先バケット内をクリアする
+ */
+function clearBucket() {
+  return listObjectsV2({
+    Bucket: TARGET_BUCKET
+  }).then(data => {
+    const objects = data.Contents.map(item => ({Key: item.Key}));
+    if (objects.length <= 0) {
+      return Promise.resolve();
     }
+
+    return deleteObjects({
+      Bucket: TARGET_BUCKET,
+      Delete: {
+        Objects: objects,
+        Quiet: true
+      }
+    });
+  }).then(data => {
+    if (!data) {
+      console.log(`${TARGET_BUCKET} is empty`);
+    }
+    console.log('clear success');
   });
-}).then(data => console.log('delete success'))
-  .catch(err => console.log(err));
+}
+
+/**
+ * プロダクト環境にファイルをアップロードする
+ */
+function uploadProduct() {
+  return upload({
+    Bucket: TARGET_BUCKET,
+    Key: 'test/file',
+    Body: fs.createReadStream('build/production/index.html')
+  });
+}
+
+clearBucket();
+uploadProduct();
