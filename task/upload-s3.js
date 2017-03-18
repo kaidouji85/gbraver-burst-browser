cdf0e58ae6dcd4efe75fb888c71905886abdbba3
竹内佑介 <kaidouji85@gmail.com>
2017-03-18 09:36:18 +0900

    S３にアップしたファイルを削除する命令を追加した

--- task/upload-s3.js
+++ task/upload-s3.js
@@ -1,30 +1,28 @@
+const TARGET_BUCKET = 'study-three-js';
+
 const AWS = require('aws-sdk');
 AWS.config.loadFromPath('aws-credentials.json');
 AWS.config.update({region: 'ap-northeast-1'});
 
 const s3 = new AWS.S3();
 
-/**
- * S3上のオブジェクトを削除する
- * 実態はs3.deleteObjectsをPromise化しただけ
- *
- * @param params s3.deleteObjectsのパラメータ
- */
-const deleteObjects = params => new Promise((resolve, reject) => s3.deleteObjects(params, (err, data) =>
-  err ? reject(err) : resolve(data)
-));
+const listObjectsV2 = params => new Promise((resolve, reject) => s3.listObjectsV2(params, (err, data) =>
+  err ? reject(err) : resolve(data)));
 
-deleteObjects({
-  Bucket: 'study-three-js',
-  Delete: {
-    Objects: [
-      {Key: 'bundle.js'}
-    ],
-    Quiet: true
-  }
-}).then((data) => {
-  console.log('success!!');
-  console.log(data);
-}).catch(err => {
-  console.log(err);
-});
+const deleteObjects = params => new Promise((resolve, reject) => s3.deleteObjects(params, (err, data) =>
+  err ? reject(err) : resolve(data)));
+
+
+listObjectsV2({
+  Bucket: TARGET_BUCKET
+}).then(data => {
+  const objects = data.Contents.map(item => ({Key: item.Key}));
+  return deleteObjects({
+    Bucket: TARGET_BUCKET,
+    Delete: {
+      Objects: objects,
+      Quiet: true
+    }
+  });
+}).then(data => console.log('delete success'))
+  .catch(err => console.log(err));
