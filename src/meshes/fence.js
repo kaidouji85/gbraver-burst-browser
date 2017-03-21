8a661f2b30ab1a721f7211ca773d7cf1a48b2a2f
竹内佑介 <kaidouji85@gmail.com>
2017-03-22 00:21:21 +0900

    網モデルを作成した

--- src/meshes/fence.js
+++ src/meshes/fence.js
@@ -6,6 +6,45 @@ import {TEXURE_PATHS} from '../resource-manager';
 const THREE = ThreeLib();
 const HEIGHT = 100;
 const WIDTH = 100;
+const COLOR = 0x006400;
+
+/**
+ * 網
+ *
+ * @param resources リソース管理クラス
+ * @returns 網
+ */
+function Network(resources: Resources): THREE.Mesh {
+  let geometry = new THREE.PlaneGeometry(WIDTH, HEIGHT, 32, 32);
+
+  let texture = resources.textures.find(item => item.path === TEXURE_PATHS.FENCE);
+  let material = new THREE.MeshBasicMaterial({
+    side: THREE.DoubleSide,
+    transparent: true,
+    alphaMap: texture ? texture.texture : new THREE.Texture(),
+    color: new THREE.Color( COLOR )
+  });
+
+  let mesh = new THREE.Mesh(geometry, material);
+  mesh.position.y = HEIGHT / 2;
+  return mesh;
+}
+
+/**
+ * 円柱
+ *
+ * @returns 円柱
+ */
+function Cylinder() {
+  let cylinder = new THREE.Mesh(
+    new THREE.CylinderGeometry(2, 2, HEIGHT, 50),
+    new THREE.MeshPhongMaterial({
+      color: COLOR
+    }));
+  cylinder.position.x = WIDTH / 2;
+  cylinder.position.y = HEIGHT / 2;
+  return cylinder;
+}
 
 /**
  * フェンスメッシュを生成する
@@ -14,17 +53,10 @@ const WIDTH = 100;
  * @returns {THREE.Mesh}  フェンスメッシュ
  */
 export default function Fence(resources: Resources): THREE.Mesh {
-  let geometry = new THREE.PlaneGeometry(WIDTH, HEIGHT, 32, 32);
+  let fence = new THREE.Group();
 
-  let texture = resources.textures.find(item => item.path === TEXURE_PATHS.FENCE);
-  let material = new THREE.MeshBasicMaterial({
-    side: THREE.DoubleSide,
-    transparent: true,
-    alphaMap: texture ? texture.texture : new THREE.Texture(),
-    color: new THREE.Color( 0x006400 )
-  });
+  fence.add(Network(resources));
+  fence.add(Cylinder());
 
-  let mesh = new THREE.Mesh(geometry, material);
-  mesh.position.y = HEIGHT / 2;
-  return mesh;
+  return fence;
 }
