41bc9ae76c950ae8ea5bf767e2cd710a5d9ae3e0
竹内佑介 <kaidouji85@gmail.com>
2017-05-07 14:15:19 +0900

    マップチップ表示ができるようになった

--- src/stage/school/city-road.js
+++ src/stage/school/city-road.js
@@ -4,25 +4,22 @@ import ThreeLib from 'three-js';
 import {TEXTURE_PATHS} from '../../resource-manager';
 
 const THREE = ThreeLib();
-const HEIGHT = 1200;
-const WIDTH = 300;
 
-/**
- * テクスチャを生成して返す
- *
- * @param texture
- */
-function createTexture(resources: Resources): Three.Texture {
-  let origin = resources.textures.find(item => item.path === TEXTURE_PATHS.CITY_LOAD);
-  let texture = origin.texture.clone();
+const WIDTH = 100;
+const HEIGHT = 100;
 
-  texture.needsUpdate = true;
-  texture.wrapS = THREE.RepeatWrapping;
-  texture.wrapT = THREE.RepeatWrapping;
-  //texture.repeat.set(0.5, 0.5);
+const PICT_WIDTH = 256;
+const PICT_HEIGHT = 256;
 
-  return texture;
-}
+const TILE_WIDTH = 32;
+const TILE_HEIGHT = 32;
+
+const tileMapping = (x: number, y: number): THREE.Vector[] => [
+  new THREE.Vector2(TILE_WIDTH/PICT_WIDTH * x, TILE_HEIGHT/PICT_HEIGHT * y),
+  new THREE.Vector2(TILE_WIDTH/PICT_WIDTH * (x + 1), TILE_HEIGHT/PICT_HEIGHT * y),
+  new THREE.Vector2(TILE_WIDTH/PICT_WIDTH * (x + 1), TILE_HEIGHT/PICT_HEIGHT * (y + 1)),
+  new THREE.Vector2(TILE_WIDTH/PICT_WIDTH * x, TILE_HEIGHT/PICT_HEIGHT * (y + 1)),
+]
 
 /**
  * 都会の道路
@@ -31,13 +28,20 @@ function createTexture(resources: Resources): Three.Texture {
  * @return 都会の道路
  */
 export default function CityRoad(resources: Resources): THREE.Mesh {
-  let geometry = new THREE.PlaneGeometry(HEIGHT, WIDTH, 32, 32);
+  let geometry = new THREE.PlaneGeometry(WIDTH, HEIGHT, 1, 1);
+  let uvMapping = tileMapping(0, 4);
+  geometry.faceVertexUvs[0][0] = [uvMapping[0], uvMapping[1], uvMapping[3]];
+  geometry.faceVertexUvs[0][1] = [uvMapping[1], uvMapping[2], uvMapping[3]];
+
+  let texture = resources.textures.find(item => item.path === TEXTURE_PATHS.CITY_LOAD);
   let material = new THREE.MeshBasicMaterial({
     side: THREE.DoubleSide,
-    map: createTexture(resources),
+    map: texture ? texture.texture : new THREE.Texture(),
   });
 
   let mesh = new THREE.Mesh(geometry, material);
   mesh.rotation.x = -90 * Math.PI / 180;
+  mesh.rotation.z = 90 * Math.PI / 180;
+
   return mesh;
 }
