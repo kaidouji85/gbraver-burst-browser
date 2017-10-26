57f48e3e5ae7aa8c29280447612043f48579e9d8
竹内佑介 <kaidouji85@gmail.com>
2017-10-26 23:48:15 +0900

    aws-upload.conf.jsをgitの追跡から外した

--- aws-upload.conf.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  credentials:"aws-credentials.json",
-  // TODO 設定ファイルに外だしする
-  bucketName:"study-three-js",
-  patterns:[
-    "build/production/**",
-    "build/stub/**",
-  ]
-}
