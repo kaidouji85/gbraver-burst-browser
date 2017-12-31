6ed8d056109ec4556688269991a7c5294783c65d
竹内佑介 <kaidouji85@gmail.com>
2017-12-31 18:18:22 +0900

    学校ステージのフォルダ構成変更

--- src/game-object/stage/school/index.js
+++ src/game-object/stage/school/index.js
@@ -1,28 +1,23 @@
 // @flow
 import type {Resources} from '../../../resource/resource-manager';
-import {MODEL_PATHS} from '../../../resource/resource-manager';
 
 import * as THREE from 'three';
-import SchoolBuild from './school/index';
+import SchoolBuild from './school-build/index';
 import SkyBox from './blue-sky';
 import CityRoad from './city-road';
-import {createMeshFromJson} from '../../../util/mesh/three-json-mesh';
 import {Stage} from '../base';
+import {Mansions} from "./mansions";
 
 /** 学校ステージ */
 export default class SchoolStage implements Stage {
   /** スカイボックス（青空） */
   skyBox: THREE.Mesh;
-
   /** 校舎 */
   schoolBuild: SchoolBuild;
-
   /** 道路 */
   road: CityRoad;
-
   /** 光源 */
   lights: THREE.Light[];
-
   /** マンション群 */
   mansions: THREE.Mesh;
 
@@ -85,27 +80,3 @@ function Light(): THREE.Light[] {
   ];
 }
 
-/**
- * マンションを生成して返す
- *
- * @param resources リソース管理クラス
- * @return マンション
- */
-function Mansions(resources: Resources): THREE.Mesh[] {
-  const mansion = (x: number, y:number, z: number): THREE.Mesh => {
-    let mesh = createMeshFromJson(MODEL_PATHS.MANSION01, resources);
-    mesh.position.set(x, y, z);
-    mesh.scale.set(0.5, 0.5, 0.5);
-    return mesh;
-  };
-
-  return [
-    mansion(950, 0, 450),
-    mansion(950, 0, 0),
-    mansion(950, 0, -450),
-
-    mansion(-950, 0, 450),
-    mansion(-950, 0, 0),
-    mansion(-950, 0, -450),
-  ];
-}
