0a2722df3f64f8c67ee6aa8d1d91b3581ac0a0bc
竹内佑介 <kaidouji85@gmail.com>
2018-03-01 22:59:23 +0900

    タッチエンドにもレイキャスト生成を追加した

--- src/scene/battle/action-handler/touch-end.js
+++ src/scene/battle/action-handler/touch-end.js
@@ -3,9 +3,14 @@
 import {BattleSceneView} from "../view";
 import type {TouchEnd} from "../../action";
 import type {BattleSceneState} from "../index";
+import type {TouchEventRaycaster} from "../../../touch/touch-raycaster";
+import {createTouchEventRaycaster} from "../../../touch/touch-raycaster";
 
 /** ゲーム画面内をタッチエンドした際のイベント */
 export function touchEnd(view: BattleSceneView, state: BattleSceneState, action: TouchEnd) {
   console.log('touchEnd');
   console.log(action.event);
+
+  const hudRaycaster: TouchEventRaycaster = createTouchEventRaycaster(action.event, view.renderer, view.hudLayer.camera);
+  console.log(hudRaycaster);
 }
