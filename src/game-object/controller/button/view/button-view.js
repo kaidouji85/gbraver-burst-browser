02c131042b029e72843da2e71e16249fd3d4c7c7
竹内佑介 <kaidouji85@gmail.com>
2018-03-01 23:28:35 +0900

    タッチ対象オブジェクトのインターフェースを作成した

--- src/game-object/controller/button/view/button-view.js
+++ src/game-object/controller/button/view/button-view.js
@@ -2,13 +2,12 @@
 
 import type {ButtonModel} from "../model/button-model";
 import * as THREE from "three";
+import {TouchTarget} from "../../../../touch/touch-target";
 
 /** ボタンのビュー */
-export interface ButtonView {
+export interface ButtonView extends TouchTarget {
   /** モデルをビューに反映させる */
   gameLoop(model: ButtonModel): void;
   /** シーンに追加するthree.jsのオブジェクトを取得する */
   getThreeJsObjectList(): THREE.Mesh[];
-  /** マウス、指とボタンが重なっているかを判定する */
-  isOverlap(raycaster: THREE.Raycaster): boolean;
 }
