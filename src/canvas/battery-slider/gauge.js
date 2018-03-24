61c2213d280089d807e676b64efe24db2b63d78b
竹内佑介 <kaidouji85@gmail.com>
2018-03-24 13:18:44 +0900

    変数名、関数名をシンプルにした

--- src/canvas/battery-slider/gauge.js
+++ src/canvas/battery-slider/gauge.js
@@ -14,13 +14,13 @@ import type {CanvasImageResource} from "../../resource/canvas-image";
  * @param dx 描画位置X
  * @param dy 描画位置Y
  */
-export function drawBatterySliderGauge(context: CanvasRenderingContext2D, resources: Resources, battery: number, maxBattery: number, dx: number, dy: number) {
-  const batterySliderGaugeResource: ?CanvasImageResource = resources.canvasImages.find(v => v.id === CANVAS_IMAGE_IDS.BATTERY_SLIDER_GAUGE);
-  const batterySliderGauge: Image = batterySliderGaugeResource ? batterySliderGaugeResource.image : new Image();
-  const gaugeWidth = batterySliderGauge.width * battery / maxBattery;
-  const gaugeHeight = batterySliderGauge.height;
-  const drawX = dx - batterySliderGauge.width / 2;
-  const drawY = dy - batterySliderGauge.height / 2;
+export function BatterySliderGauge(context: CanvasRenderingContext2D, resources: Resources, battery: number, maxBattery: number, dx: number, dy: number): void {
+  const sliderGaugeResource: ?CanvasImageResource = resources.canvasImages.find(v => v.id === CANVAS_IMAGE_IDS.BATTERY_SLIDER_GAUGE);
+  const sliderGauge: Image = sliderGaugeResource ? sliderGaugeResource.image : new Image();
+  const gaugeWidth = sliderGauge.width * battery / maxBattery;
+  const gaugeHeight = sliderGauge.height;
+  const drawX = dx - sliderGauge.width / 2;
+  const drawY = dy - sliderGauge.height / 2;
 
-  context.drawImage(batterySliderGauge, 0, 0, gaugeWidth, gaugeHeight, drawX, drawY, gaugeWidth, gaugeHeight);
+  context.drawImage(sliderGauge, 0, 0, gaugeWidth, gaugeHeight, drawX, drawY, gaugeWidth, gaugeHeight);
 }
--- stub/canvas/battery-slider.js
+++ stub/canvas/battery-slider.js
@@ -1,9 +1,9 @@
 // @flow
 import type {Resources} from '../../src/resource/index';
 import {CanvasStubBase} from '../util/canvas-stub-base';
-import {drawBatterySlider} from "../../src/canvas/battery-slider";
+import {BatterySlider} from "../../src/canvas/battery-slider";
 
 CanvasStubBase('../resources/', (context: CanvasRenderingContext2D, reources: Resources) => {
 
-  drawBatterySlider(context, reources, 4, 5, window.innerWidth / 2, window.innerHeight / 2);
+  BatterySlider(context, reources, 4, 5, window.innerWidth / 2, window.innerHeight / 2);
 });
