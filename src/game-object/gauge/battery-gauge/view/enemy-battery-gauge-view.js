986c4eed1b3eb64d1b3e84210fef771bc3b92cb0
竹内佑介 <kaidouji85@gmail.com>
2018-03-27 21:46:24 +0900

    バッテリーゲージのビューを正方メッシュにした

--- src/game-object/gauge/battery-gauge/view/enemy-battery-gauge-view.js
+++ src/game-object/gauge/battery-gauge/view/enemy-battery-gauge-view.js
@@ -3,7 +3,7 @@
 import type {BatteryGaugeModel} from "../model/battery-gauge-model";
 import type {Resources} from "../../../../resource/index";
 import {drawEnemyBatteryGauge} from "../../../../canvas/battery-gauge";
-import {MESH_WIDTH, PADDING_TOP, PlayerBatteryGaugeView} from "./player-battery-gauge-view";
+import {MESH_SIZE, PADDING_TOP, PlayerBatteryGaugeView} from "./player-battery-gauge-view";
 
 /** 敵バッテリーゲージ */
 export class EnemyBatteryGaugeView extends PlayerBatteryGaugeView {
@@ -16,15 +16,13 @@ export class EnemyBatteryGaugeView extends PlayerBatteryGaugeView {
   _refreshGauge(model: BatteryGaugeModel): void {
     this._canvasMesh.draw((context: CanvasRenderingContext2D) => {
       context.clearRect(0, 0, this._canvasMesh.canvas.width, this._canvasMesh.canvas.height);
-
-      // UVマッピングの原点は左下なので、HPゲージがテクスチャの一番下に描画されるようにする
-      drawEnemyBatteryGauge(context, this._resources, context.canvas.width/2, context.canvas.height - 32, model.battery, model.maxBattery);
+      drawEnemyBatteryGauge(context, this._resources, context.canvas.width / 2, context.canvas.height / 2, model.battery, model.maxBattery);
     });
   }
 
   /** 表示位置を更新する */
   _refreshPos(): void {
-    this._canvasMesh.mesh.position.x = (-window.innerWidth + MESH_WIDTH * this._scale) / 2;
+    this._canvasMesh.mesh.position.x = (-window.innerWidth + MESH_SIZE * this._scale) / 2;
     this._canvasMesh.mesh.position.y = window.innerHeight / 2 - PADDING_TOP * this._scale;
   }
 }
--- src/game-object/gauge/battery-gauge/view/player-battery-gauge-view.js
+++ src/game-object/gauge/battery-gauge/view/player-battery-gauge-view.js
@@ -5,11 +5,10 @@ import {BatteryGaugeView} from './battery-gauge-view';
 import type {BatteryGaugeModel} from "../model/battery-gauge-model";
 import type {Resources} from "../../../../resource/index";
 import * as THREE from "three";
-import {rectangle} from "../../../../uv-mapping/rectangle";
 import {drawPlayerBatteryGauge} from "../../../../canvas/battery-gauge";
 
-export const MESH_WIDTH = 300;
-export const MESH_HEIGHT = 80;
+/** メッシュの大きさ */
+export const MESH_SIZE = 300;
 
 /** 上パディング */
 export const PADDING_TOP = 96;
@@ -26,21 +25,13 @@ export class PlayerBatteryGaugeView implements BatteryGaugeView {
   constructor(resources: Resources, scale: number) {
     this._canvasMesh = new CanvasMesh({
       resources,
-      meshWidth: MESH_WIDTH,
-      meshHeight: MESH_HEIGHT,
+      meshWidth: MESH_SIZE,
+      meshHeight: MESH_SIZE,
       canvasWidth: 256,
       canvasHeight: 256,
     });
     this._scale = scale;
     this._resources = resources;
-
-    // バッテリーゲージに必要な大きさだけテクスチャから抜き取る
-    rectangle({
-      geo: this._canvasMesh.mesh.geometry,
-      pos: new THREE.Vector2(0, 0),
-      width: 1,
-      height: MESH_HEIGHT / MESH_WIDTH
-    });
   }
 
   /** ビューにモデルを反映させる */
@@ -54,15 +45,13 @@ export class PlayerBatteryGaugeView implements BatteryGaugeView {
   _refreshGauge(model: BatteryGaugeModel): void {
     this._canvasMesh.draw((context: CanvasRenderingContext2D) => {
       context.clearRect(0, 0, this._canvasMesh.canvas.width, this._canvasMesh.canvas.height);
-
-      // UVマッピングの原点は左下なので、HPゲージがテクスチャの一番下に描画されるようにする
-      drawPlayerBatteryGauge(context, this._resources, context.canvas.width/2, context.canvas.height - 32, model.battery, model.maxBattery);
+      drawPlayerBatteryGauge(context, this._resources, context.canvas.width / 2, context.canvas.height / 2, model.battery, model.maxBattery);
     });
   }
 
   /** 表示位置を更新する */
   _refreshPos(): void {
-    this._canvasMesh.mesh.position.x = (window.innerWidth - MESH_WIDTH * this._scale) / 2;
+    this._canvasMesh.mesh.position.x = (window.innerWidth - MESH_SIZE * this._scale) / 2;
     this._canvasMesh.mesh.position.y = window.innerHeight / 2 - PADDING_TOP * this._scale;
   }
 
