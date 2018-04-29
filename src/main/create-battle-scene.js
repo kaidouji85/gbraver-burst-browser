a376a464ea7f8e3c7681374460a71e23126e7558
竹内佑介 <kaidouji85@gmail.com>
2018-04-29 12:44:20 +0900

    戦闘シーン生成処理を別ファイルに切り出した

---
+++ src/main/create-battle-scene.js
@@ -0,0 +1,27 @@
+// @flow
+import type {Resources} from "../resource";
+import {DOMEventObserver} from "../observer/dom-event/dom-event-observer";
+import * as THREE from "three";
+import {BattleScene} from "../scene/battle";
+import {start} from "gbraver-burst-core";
+import {ArmDozerIdList, ArmDozers} from "gbraver-burst-core/lib/master/armdozers";
+
+/** 戦闘シーン生成のヘルパー関数 */
+export function createBattleScene(resources: Resources, domEventObserver: DOMEventObserver, renderer: THREE.WebGLRenderer): BattleScene {
+  // TODO 開発用にダミーデータを作成している
+  return new BattleScene({
+    resources: resources,
+    renderer: renderer,
+    domEventListener: domEventObserver,
+    playerId: 'test01',
+    battleState: start(
+      {
+        playerId: 'test01',
+        armdozer: ArmDozers.find(v => v.id === ArmDozerIdList.SHIN_BRAVER) || ArmDozers[0]
+      }, {
+        playerId: 'test02',
+        armdozer: ArmDozers.find(v => v.id === ArmDozerIdList.NEO_LANDOZER) || ArmDozers[0]
+      }
+    )
+  });
+}
--- src/main/index.js
+++ src/main/index.js
@@ -1,14 +1,12 @@
 // @flow
 
-import {ArmDozerIdList, ArmDozers} from 'gbraver-burst-core/lib/master/armdozers';
-import {start} from "gbraver-burst-core";
 import {loadServiceWorker} from "../service-worker/load-service-worker";
 import {createRender} from "../render/renderer";
 import {DOMEventObserver} from "../observer/dom-event/dom-event-observer";
 import {bindDom} from "../render/bind-dom";
-import {BattleScene} from "../scene/battle";
 import Tween from "@tweenjs/tween.js/src/Tween";
 import {loadAllResource} from "../resource";
+import {createBattleScene} from "./create-battle-scene";
 
 export async function main() {
   loadServiceWorker();
@@ -17,23 +15,7 @@ export async function main() {
   const renderer = createRender();
   bindDom(renderer);
   const domEventObserver = new DOMEventObserver(renderer.domElement);
-
-  // TODO 開発用にダミーデータを作成している
-  const scene: BattleScene = new BattleScene({
-    resources: resources,
-    renderer: renderer,
-    domEventListener: domEventObserver,
-    playerId: 'test01',
-    battleState: start(
-      {
-        playerId: 'test01',
-        armdozer: ArmDozers.find(v => v.id === ArmDozerIdList.SHIN_BRAVER) || ArmDozers[0]
-      }, {
-        playerId: 'test02',
-        armdozer: ArmDozers.find(v => v.id === ArmDozerIdList.NEO_LANDOZER) || ArmDozers[0]
-      }
-    )
-  });
+  const scene = createBattleScene(resources, domEventObserver, renderer);
 
   const gameLoop = (time: DOMHighResTimeStamp) => {
     requestAnimationFrame(gameLoop);
