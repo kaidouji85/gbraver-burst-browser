678d0c836d01e891f12eab32699962b4a6fa6314
竹内佑介 <kaidouji85@gmail.com>
2018-05-04 15:22:48 +0900

    プレイヤーバーストゲージの位置調整をした

--- src/game-object/gauge/burst-gauge/view/player-burst-gauge-view.js
+++ src/game-object/gauge/burst-gauge/view/player-burst-gauge-view.js
@@ -8,7 +8,11 @@ import {drawBurstGauge} from "../../../../canvas/burst-gauge";
 import * as THREE from "three";
 
 /** メッシュの大きさ */
-const MESH_SIZE = 256;
+export const MESH_SIZE = 128;
+/** パディングトップ */
+export const PADDING_TOP = 198;
+/** パディングレフト */
+export const PADDING_LEFT = 128;
 
 /** プレイヤーのバーストゲージ */
 export class PlayerBurstGaugeView implements BurstGaugeView {
@@ -27,12 +31,26 @@ export class PlayerBurstGaugeView implements BurstGaugeView {
     });
   }
 
+  /** モデルをビューに反映させる */
   gameLoop(model: BurstGaugeModel): void {
-    // TODO 描画位置を調整する
+    const scale = 1;
+    this._canvasMesh.mesh.scale.set(scale, scale, scale);
+    this._refreshGauge(model);
+    this._refreshPos();
+  }
+
+  /** ゲージ内容を更新する */
+  _refreshGauge(model: BurstGaugeModel): void {
     this._canvasMesh.draw(context => {
       context.clearRect(0, 0, this._canvasMesh.canvas.width, this._canvasMesh.canvas.height);
       drawBurstGauge(context, this._resources, model.isActive, context.canvas.width / 2, context.canvas.height / 2);
     });
+  }
+
+  /** ゲージ位置を調整する */
+  _refreshPos(): void {
+    this._canvasMesh.mesh.position.x = window.innerWidth / 2 - PADDING_LEFT;
+    this._canvasMesh.mesh.position.y = window.innerHeight / 2 - PADDING_TOP;
   }
 
   getThreeJsObjectList(): THREE.Mesh[] {
