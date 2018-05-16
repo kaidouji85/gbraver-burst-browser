cb996a02868cc5f6188067ba6f2d092ffd6611d3
竹内佑介 <kaidouji85@gmail.com>
2018-05-16 22:21:43 +0900

    ボタンシャドウを大きくした

--- src/canvas/button/attack-button.js
+++ src/canvas/button/attack-button.js
@@ -5,7 +5,7 @@ import {drawImageInCenter} from "../draw/image-drawer";
 import type {CanvasImageResource} from "../../resource/canvas-image";
 import {CANVAS_IMAGE_IDS} from "../../resource/canvas-image";
 
-const SHADOW_PADDING_BOTTOM = 470;
+const SHADOW_PADDING_BOTTOM = 100;
 
 /**
  * コウゲキボタンを描画する
@@ -15,11 +15,12 @@ const SHADOW_PADDING_BOTTOM = 470;
  */
 export function drawAttackButton(context: CanvasRenderingContext2D, resources: Resources, depth: number, dx: number, dy: number): void {
   const scale = (1 + 0.1 * depth);
+  const shadowScale = (1 + 0.15 * depth);
   const attackButtonResource: ?CanvasImageResource = resources.canvasImages.find(v => v.id === CANVAS_IMAGE_IDS.ATTACK_BUTTON);
   const attackButtonImage: Image = attackButtonResource ? attackButtonResource.image : new Image();
   const buttonShadowResource: ?CanvasImageResource = resources.canvasImages.find(v => v.id === CANVAS_IMAGE_IDS.BUTTON_SHADOW);
   const buttonShadowImage: Image = buttonShadowResource ? buttonShadowResource.image : new Image();
 
-  drawImageInCenter(context, buttonShadowImage, dx, dy + SHADOW_PADDING_BOTTOM, scale);
+  drawImageInCenter(context, buttonShadowImage, dx, dy + SHADOW_PADDING_BOTTOM, shadowScale);
   drawImageInCenter(context, attackButtonImage, dx, dy, scale);
 }
