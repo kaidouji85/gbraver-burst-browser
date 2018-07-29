6699adfc07cc61368b47906c88c9d6e655e60d30
竹内佑介 <kaidouji85@gmail.com>
2018-07-29 14:10:36 +0900

    アクティブバーの描画プログラムを作成した

--- src/canvas/battery-slider/index.js
+++ src/canvas/battery-slider/index.js
@@ -29,6 +29,30 @@ export type Param = {
 export function drawBatterySlider(context: CanvasRenderingContext2D, resources: Resources, param: Param): void {
   const sliderBaseResource: ?CanvasImageResource = resources.canvasImages.find(v => v.id === CANVAS_IMAGE_IDS.BATTERY_SLIDER_BASE);
   const sliderBase: Image = sliderBaseResource ? sliderBaseResource.image : new Image();
+  const activeBarResource: ?CanvasImageResource = resources.canvasImages.find(v => v.id === CANVAS_IMAGE_IDS.BATTERY_SLIDER_ACTIVE_BAR);
+  const activeBar: Image = activeBarResource ? activeBarResource.image : new Image();
 
   drawImageInCenter(context, sliderBase, param.dx, param.dy);
+  drawActiveBar(context, sliderBase, activeBar, param);
+}
+
+/**
+ * アクティブバーを描画する
+ *
+ * @param context 描画対象のキャンバス
+ * @param sliderBase バッテリースライダーのベース画像
+ * @param activeBar バッテリースライダーのアクティブバー画像
+ */
+export function drawActiveBar(context: CanvasRenderingContext2D, sliderBase: Image, activeBar: Image, param: Param): void {
+  const PADDING_LEFT = 8;
+
+  const sx = 0;
+  const sy = 0;
+  const sw = activeBar.width * param.battery / param.maxBattery;
+  const sh = activeBar.height;
+  const dx = param.dx - sliderBase.width / 2 + PADDING_LEFT;
+  const dy = param.dy - sh / 2;
+  const dw = sw;
+  const dh = sh;
+  context.drawImage(activeBar, sx, sy, sw, sh, dx, dy, dw, dh);
 }
