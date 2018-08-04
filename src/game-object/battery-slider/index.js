20ef2187dcff6ba1c05ac2fca97e59836ee72da0
竹内佑介 <kaidouji85@gmail.com>
2018-08-04 18:43:30 +0900

    バッテリースライダーのイベントをシンプルにした

--- src/game-object/battery-slider/index.js
+++ src/game-object/battery-slider/index.js
@@ -1,16 +1,16 @@
 // @flow
 
-import {Subject} from 'rxjs';
+import {Observable, Subject} from 'rxjs';
 import type {BatterySliderModel} from "./model/battery-slider-model";
 import {BatterySliderView} from "./view/battery-slider-view";
 import type {Resources} from "../../resource/index";
 import * as THREE from "three";
 import {change} from './model/change';
 import {Group, Tween} from "@tweenjs/tween.js";
-import type {TouchRaycastContainer} from "../../screen-touch/touch/touch-raycaster";
+import type {TouchRaycastContainer, TouchRaycaster} from "../../screen-touch/touch/touch-raycaster";
 import type {MouseRaycaster} from "../../screen-touch/mouse/mouse-raycaster";
 import {getControllerScale} from "../../device-scale/controller-scale";
-import { map, filter, distinctUntilChanged, withLatestFrom } from 'rxjs/operators';
+import {map, filter, distinctUntilChanged} from 'rxjs/operators';
 import {visible} from './model/visible';
 import {isGroupPlaying} from "../../tween/is-group-playing";
 
@@ -31,8 +31,9 @@ export class BatterySlider {
   _batteryTween: Group;
   /** 透明度のTweenグループ */
   _opacityTween: Group;
-  /** バッテリー目盛りの値が変化した場合の処理 */
-  _onChangeBattery: Subject<number>;
+  /** マウス、指の当たり判定サブジェクト */
+  _onOverlap: Subject<number[]>;
+
 
   constructor(param: Param) {
     const initialBattery = 3;
@@ -49,14 +50,17 @@ export class BatterySlider {
     this._batteryTween = new Group();
     this._opacityTween = new Group();
 
-    this._onChangeBattery = new Subject();
-    this._onChangeBattery
-      .pipe(distinctUntilChanged())
-      .subscribe(battery => {
-        this.stopBatteryAnimation();
-        this.changeBattery(battery).start();
-        param.onBatteryChange(battery);
-      })
+    this._onOverlap = new Subject();
+    this._onOverlap.pipe(
+      filter(v => 0 < v.length),
+      filter(() => this.canOperate()),
+      map(v => v.reduce((a, b) => Math.max(a, b))),
+      distinctUntilChanged()
+    ).subscribe(v => {
+      this.stopBatteryAnimation();
+      this.changeBattery(v).start();
+      param.onBatteryChange(v);
+    });
   }
 
   /** ゲームループの処理 */
@@ -92,40 +96,44 @@ export class BatterySlider {
     return visible(this._model, this._opacityTween, isVisible);
   }
 
+  /**
+   * 操作可能か否かを返す
+   *
+   * @return 判定結果、trueで操作可能
+   */
+  canOperate(): boolean {
+    const ret = !isGroupPlaying(this._opacityTween)
+      && !isGroupPlaying(this._batteryTween)
+      && this._model.opacity === 1;
+    return ret;
+  }
+
   /** マウスダウンした際の処理 */
   onMouseDown(mouse: MouseRaycaster): void {
     const overlap = this._view.getMouseOverlap(mouse);
-    this.onOverlap(overlap);
+    this._onOverlap.next(overlap);
   }
 
   /** マウスムーブした際の処理 */
-  onMouseMove(mouseRaycaster: MouseRaycaster, isLeftButtonPushed: boolean): void {
-    if (isLeftButtonPushed) {
-      const overlap = this._view.getMouseOverlap(mouseRaycaster);
-      this.onOverlap(overlap);
+  onMouseMove(mouse: MouseRaycaster, isLeftButtonPushed: boolean): void {
+    if (!isLeftButtonPushed) {
+      return;
     }
+
+    const overlap = this._view.getMouseOverlap(mouse);
+    this._onOverlap.next(overlap);
   }
 
   /** タッチスタートした際の処理 */
   onTouchStart(touch: TouchRaycastContainer): void {
     const overlap = this._view.getTouchOverlap(touch);
-    this.onOverlap(overlap);
+    this._onOverlap.next(overlap);
   }
 
   /** タッチムーブした際の処理 */
   onTouchMove(touch: TouchRaycastContainer): void {
     const overlap = this._view.getTouchOverlap(touch);
-    this.onOverlap(overlap);
-  }
-
-  /** 指、マウスがバッテリースライダー目盛りに接触した際の処理 */
-  onOverlap(overlap: number[]): void {
-    if (overlap.length <= 0 || isGroupPlaying(this._opacityTween) || this._model.opacity !== 1) {
-      return;
-    }
-
-    const battery = overlap.reduce((a, b) => Math.min(a, b));
-    this._onChangeBattery.next(battery);
+    this._onOverlap.next(overlap);
   }
 
   /** シーンに追加するthree.jsオブジェクトを返す */
