c12694d2688ebc414c5139f0ff0e9ec7987c9a35
竹内佑介 <kaidouji85@gmail.com>
2018-09-21 21:43:18 +0900

    バーストボタンにアニメーションを追加した

---
+++ src/game-object/burst-button/animation/invisible.js
@@ -0,0 +1,18 @@
+// @flow
+
+import {Tween, Group} from '@tweenjs/tween.js';
+import type {BurstButtonModel} from "../model/burst-button-model";
+import type {MultiTween} from "../../../tween/multi-tween/multi-tween";
+
+export function invisible(model: BurstButtonModel, group: Group): MultiTween {
+  const start = new Tween(model, group)
+    .to({opacity: 1}, 0);
+  const end = new Tween(model, group)
+    .to({opacity: 0}, 300);
+
+  start.chain(end);
+  return {
+    start: start,
+    end: end
+  }
+}
---
+++ src/game-object/burst-button/model/initial-value.js
@@ -0,0 +1,9 @@
+// @flow
+
+import type {BurstButtonModel} from "./burst-button-model";
+
+export function createInitialValue(): BurstButtonModel {
+  return {
+    opacity: 1
+  }
+}
