b031dce8d72787b03a561bb1970f0b4583134556
竹内佑介 <kaidouji85@gmail.com>
2018-10-20 13:38:57 +0900

    レンダラーのゲームオブジェクトを追加

--- src/game-object/renderer/index.js
+++ src/game-object/renderer/index.js
@@ -4,11 +4,13 @@ import * as THREE from 'three';
 import type {DOMEvent} from "../../action/dom-event";
 import type {Resize} from "../../action/dom-event/resize";
 import type {Render} from "../../action/game-loop/render";
+import {Observable} from "rxjs";
 
 type Param = {
   renderer: THREE.WebGLRenderer,
   listener: {
-    domEvent: DOMEvent
+    domEvent: Observable<DOMEvent>,
+    render: Observable<Render>
   }
 };
 
@@ -18,15 +20,24 @@ export class Renderer {
 
   constructor(param: Param) {
     this._renderer = param.renderer;
+
+    param.listener.domEvent.subscribe(action => {
+      if (action.type === 'resize') {
+        this._resize(action);
+      }
+    });
+    param.listener.render.subscribe(action => {
+      this._render(action);
+    });
   }
 
   /** リサイズ */
   _resize(action: Resize): void {
-
+    this._renderer.setSize(action.width, action.height);
   }
 
   /** レンダリング */
   _render(action: Render): void {
-
+    this._renderer.render(action.scene, action.camera);
   }
 }
