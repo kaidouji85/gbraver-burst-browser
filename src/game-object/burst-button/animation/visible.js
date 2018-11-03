c224a34b96cf0c3a4e3831d2ca8e4e706469945a
竹内佑介 <kaidouji85@gmail.com>
2018-11-03 17:03:02 +0900

    バーストボタンのアニメーションをTweenAnimationに差し替えた

--- src/game-object/burst-button/animation/visible.js
+++ src/game-object/burst-button/animation/visible.js
@@ -2,18 +2,16 @@
 
 import {Group, Tween} from '@tweenjs/tween.js';
 import type {BurstButtonModel} from "../model/burst-button-model";
-import type {MultiTween} from "../../../depricated-tween/multi-tween/multi-tween";
+import {TweenAnimation} from "../../../animation/tween-animation";
+import {tween} from "../../../animation/tween";
 
 /** バーストボタンを表示する */
-export function visible(model: BurstButtonModel, group: Group): MultiTween {
-  const start = new Tween(model, group)
-    .to({opacity: 0}, 0);
-  const end = new Tween(model, group)
-    .to({opacity: 1}, 300);
-
-  start.chain(end);
-  return {
-    start: start,
-    end: end
-  }
+export function visible(model: BurstButtonModel, group: Group): TweenAnimation {
+  return tween(new Tween(model, group)
+    .to({opacity: 0}, 0)
+  ).chain(
+    tween(new Tween(model, group)
+      .to({opacity: 1}, 300)
+    )
+  );
 }
