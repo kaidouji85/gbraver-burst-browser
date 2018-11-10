6bd300179eeb640cc253195ab41e60dd65ba4e3d
竹内佑介 <kaidouji85@gmail.com>
2018-11-10 11:57:20 +0900

    マイターンアニメをメッシュに置き換えた

---
+++ src/game-object/armdozer/shin-breaver/mesh/my-turn.js
@@ -0,0 +1,41 @@
+// @flow
+
+import type {Resources} from "../../../../resource";
+import type {TextureResource} from "../../../../resource/texture";
+import {TEXTURE_IDS} from "../../../../resource/texture";
+import * as THREE from "three";
+import {shinBraverMaterial} from "./material";
+import {SPRITE_RENDER_ORDER} from "../../../../mesh/render-order";
+import {createAnimatedTexture} from "../../../../texture/animation/texture-animation";
+import {MAX_ANIMATION} from "../view/texture/my-turn";
+import {normalizeTextureOffset} from "../../../../texture/animation/texture-offset";
+
+export const MESH_WIDTH = 320;
+export const MESH_HEIGHT = 320;
+
+/** シンブレイバーのマイターンポーズ */
+export class ShinBraverMyTurn {
+  _texture: THREE.Texture;
+  _mesh: THREE.Mesh;
+
+  constructor(resources: Resources) {
+    const textureResource: ?TextureResource = resources.textures.find(v => v.id === TEXTURE_IDS.SHIN_BRAVER_MY_TURN);
+    const originTexture: THREE.Texture = textureResource ? textureResource.texture : new THREE.Texture();
+    this._texture = createAnimatedTexture(originTexture, MAX_ANIMATION, 1);
+
+    const geometry = new THREE.PlaneGeometry(MESH_HEIGHT, MESH_WIDTH, 1, 1);
+    const material = shinBraverMaterial(this._texture);
+    this._mesh = new THREE.Mesh(geometry, material);
+    this._mesh.position.y = 150;
+    this._mesh.renderOrder = SPRITE_RENDER_ORDER;
+  }
+
+  animate(frame: number): void {
+    this._texture.offset.x = normalizeTextureOffset(frame, MAX_ANIMATION);
+    this._texture.offset.y = 0;
+  }
+
+  getObject3D(): THREE.Object3D {
+    return this._mesh;
+  }
+}
--- src/game-object/armdozer/shin-breaver/view/player-shin-braver-view.js
+++ src/game-object/armdozer/shin-breaver/view/player-shin-braver-view.js
@@ -6,22 +6,38 @@ import type {Resources} from "../../../../resource/index";
 import type {ShinBraverModel} from "../model/shin-braver-model";
 import type {ArmdozerMesh} from "../../common/armdozer-mesh";
 import {ShinBraverStand} from "../mesh/stand";
+import {ShinBraverMyTurn} from "../mesh/my-turn";
+import type {AnimationType} from "../model/animation-type";
 
 /** プレイヤー側シンブレイバーのビュー */
 export class PlayerShinBraverView implements ShinBraverView {
   _group: THREE.Group;
   _stand: ArmdozerMesh;
+  _myTurn: ArmdozerMesh;
 
   constructor(resources: Resources) {
     this._group = new THREE.Group();
 
     this._stand = new ShinBraverStand(resources);
-    this._group.add(this._stand.getObject3D())
+    this._group.add(this._stand.getObject3D());
+
+    this._myTurn = new ShinBraverMyTurn(resources);
+    this._group.add(this._myTurn.getObject3D());
   }
 
   /** モデルをビューに反映させる */
   engage(model: ShinBraverModel): void {
     this._refreshPos(model);
+
+    const activeMesh = this._getActiveMesh(model.animation.type);
+    this._getAllMeshes()
+      .filter(v => v !== activeMesh)
+      .forEach(v => {
+        v.getObject3D().visible = false;
+      });
+
+    activeMesh.getObject3D().visible = true;
+    activeMesh.animate(model.animation.frame);
   }
 
   /** カメラの真正面を向く */
@@ -43,4 +59,19 @@ export class PlayerShinBraverView implements ShinBraverView {
     );
   }
 
+  _getAllMeshes(): ArmdozerMesh[] {
+    return [this._stand, this._myTurn];
+  }
+
+  /** アクティブなメッシュを取得 */
+  _getActiveMesh(animationType: AnimationType): ArmdozerMesh {
+    switch(animationType) {
+      case 'STAND':
+        return this._stand;
+      case 'MY_TURN':
+        return this._myTurn;
+      default:
+        return this._stand;
+    }
+  }
 }
