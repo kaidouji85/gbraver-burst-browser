0ee33d9d7c119bf25ee35a543d7c9f7e1244fe30
竹内佑介 <kaidouji85@gmail.com>
2018-11-23 17:09:22 +0900

    Tween再生時間取得のユニットテストが完成した

--- src/animation/tween-time.js
+++ src/animation/tween-time.js
@@ -4,5 +4,5 @@ import {Tween} from '@tweenjs/tween.js';
 
 /** Tween単体の再生時間を計算する */
 export function tweenTime(tween: Tween): number {
-  return tween._duration + tween._delayTime + tween._repeat * (tween._duration);
+  return tween._duration + tween._delayTime + tween._repeat * tween._duration;
 }
