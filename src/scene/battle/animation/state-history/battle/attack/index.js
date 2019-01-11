3e6a6c644fee915051a11b90665ce2741b9681d4
竹内佑介 <kaidouji85@gmail.com>
2019-01-11 20:26:45 +0900

    BattleObjectsを渡さないようした

--- src/scene/battle/animation/state-history/battle/attack/index.js
+++ src/scene/battle/animation/state-history/battle/attack/index.js
@@ -4,12 +4,21 @@ import {Animate} from "../../../../../../animation/animate";
 import {ShinBraver} from "../../../../../../game-object/armdozer/shin-breaver/shin-braver";
 import {shinBraverAttack} from "./shin-braver";
 import {emptyBattleAnimation} from "./empty-battle-animation";
+import {empty} from "../../../../../../animation/delay";
+import {BattleSceneView} from "../../../../view";
+import type {BattleSceneState} from "../../../../state/battle-scene-state";
+import type {GameState} from "gbraver-burst-core/lib/game-state/game-state";
+import type {Battle} from "gbraver-burst-core/lib/effect/battle/effect/index";
 
 /** 攻撃側スプライトに応じて、戦闘アニメーションを切り替える */
-export function attackAnimation(objects: BattleAnimationObjects<ArmDozerSprite>): Animate {
+export function attackAnimation(view: BattleSceneView, sceneState: BattleSceneState, gameState: GameState, effect: Battle): Animate {
+  return empty();
+
+  /*
   if (objects.attacker.sprite instanceof ShinBraver) {
     return shinBraverAttack(objects);
   } else {
     return emptyBattleAnimation(objects);
   }
+  */
 }
--- src/scene/battle/animation/state-history/battle/index.js
+++ src/scene/battle/animation/state-history/battle/index.js
@@ -5,10 +5,8 @@ import {BattleSceneView} from "../../../view";
 import type {BattleSceneState} from "../../../state/battle-scene-state";
 import type {GameState} from "gbraver-burst-core/lib/game-state/game-state";
 import type {Battle} from "gbraver-burst-core/lib/effect/battle/effect/index";
-import type {BattleAnimationObjects} from "./animation-objects";
 import {visibleBattery} from "./visible-battery";
 import {delay} from "../../../../../animation/delay";
-import type {ArmDozerSprite} from "../../../../../game-object/armdozer/armdozer-sprite";
 import {attackAnimation} from "./attack";
 
 /**
@@ -21,19 +19,9 @@ import {attackAnimation} from "./attack";
  * @return アニメーション
  */
 export function battleAnimation(view: BattleSceneView, sceneState: BattleSceneState, gameState: GameState, effect: Battle): Animate {
-  const isAttacker = effect.attacker === sceneState.playerId;
-  const objects: BattleAnimationObjects<ArmDozerSprite> = {
-    attacker: isAttacker ? view.td.player : view.td.enemy,
-    attackerState: gameState.players.find(v => v.playerId === effect.attacker) || gameState.players[0],
-    defender: isAttacker ? view.td.enemy : view.td.player,
-    defenderState: gameState.players.find(v => v.playerId !== effect.attacker) || gameState.players[0],
-    view: view,
-    effect: effect
-  };
-
   return visibleBattery(view, sceneState, gameState)
     .chain(delay(500))
-    .chain(attackAnimation(objects))
+    .chain(attackAnimation(view, sceneState, gameState))
     .chain(delay(500));
 }
 
