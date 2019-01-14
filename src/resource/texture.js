c9233b6ed65e43dc46980a635a99ce42c8ebe3d6
竹内佑介 <kaidouji85@gmail.com>
2019-01-14 20:44:21 +0900

    ランドーザパンチアニメーションをリソースに追加した

--- src/resource/texture.js
+++ src/resource/texture.js
@@ -25,6 +25,9 @@ export const TEXTURE_IDS = {
   NEO_LANDOZER_STAND: 'NEO_LANDOZER_STAND',
   NEO_LANDOZER_KNOCK_BACK: 'NEO_LANDOZER_KNOCK_BACK',
   NEO_LANDOZER_GUARD: 'NEO_LANDOZER_GUARD',
+  NEO_LANDOZER_SP_CHARGE: 'NEO_LANDOZER_SP_CHARGE',
+  NEO_LANDOZER_SP_ATTACK: 'NEO_LANDOZER_SP_ATTACK',
+  NEO_LANDOZER_SP_TO_STAND: 'NEO_LANDOZER_SP_TO_STAND',
   BLUE_SKY_FRONT: 'BLUE_SKY_FRONT',
   BLUE_SKY_RIGHT: 'BLUE_SKY_RIGHT',
   BLUE_SKY_BACK: 'BLUE_SKY_BACK',
@@ -65,6 +68,18 @@ export const TEXTURE_CONFIGS: TextureConfig[] = [
   {
     id: TEXTURE_IDS.NEO_LANDOZER_GUARD,
     path: 'armdozer/neo-landozer/guard.png'
+  },
+  {
+    id: TEXTURE_IDS.NEO_LANDOZER_SP_CHARGE,
+    path: 'armdozer/neo-landozer/sp-attack.png'
+  },
+  {
+    id: TEXTURE_IDS.NEO_LANDOZER_SP_ATTACK,
+    path: 'armdozer/neo-landozer/sp-attack.png'
+  },
+  {
+    id: TEXTURE_IDS.NEO_LANDOZER_SP_TO_STAND,
+    path: 'armdozer/neo-landozer/sp-to-stand.png'
   },
   // 青空スカイボックス関連
   {
