3cb1ac00c5b9407d56e33bf3e76e11671cd5cc87
竹内佑介 <kaidouji85@gmail.com>
2019-05-11 15:12:19 +0900

    シンブレイバー攻撃アニメーションを新形式に完全に置き換えた

--- src/scene/battle/animation/state-history/battle/attack/shin-braver.js
+++ src/scene/battle/animation/state-history/battle/attack/shin-braver.js
@@ -6,8 +6,8 @@ import type {AttackAnimationParam} from "./animation-param";
 import {ShinBraver} from "../../../../../../game-object/armdozer/shin-breaver/shin-braver";
 import type {
   BattleResult,
-  CriticalHit,
-  Guard,
+  CriticalHit, Feint,
+  Guard, Miss,
   NormalHit
 } from "gbraver-burst-core/lib/effect/battle/result/battle-result";
 import {overWriteAttackAnimResult} from "./animation-param";
@@ -21,15 +21,19 @@ import {all} from "../../../../../../animation/all";
  */
 export function shinBraverAttack(param: AttackAnimationParam<ShinBraver, BattleResult>): Animate {
   const result = param.result;
-  if (result.name === 'NormalHit') {
-    return attack(overWriteAttackAnimResult(param, result));
-  } else if (result.name === 'Guard') {
-    return guard(overWriteAttackAnimResult(param, result));
-  } else if (result.name === 'Critical') {
-    return attack(overWriteAttackAnimResult(param, result));
+  switch (result.name) {
+    case 'NormalHit':
+    case 'Critical':
+      return attack(overWriteAttackAnimResult(param, result));
+    case 'Guard':
+      return guard(overWriteAttackAnimResult(param, result));
+    case 'Miss':
+      return miss(overWriteAttackAnimResult(param, result));
+    case 'Feint':
+      return feint(overWriteAttackAnimResult(param, result));
+    default:
+      return empty();
   }
-
-  return empty();
 }
 
 /** 通常ヒット、クリティカル */
@@ -60,4 +64,23 @@ function guard(param: AttackAnimationParam<ShinBraver, Guard>): Animate {
   ).chain(
     param.defenderTD.sprite.guardToStand()
   );
+}
+
+/** ミス */
+function miss(param: AttackAnimationParam<ShinBraver, Miss>): Animate {
+  return all(
+    param.attackerTD.sprite.straightPunch(),
+    delay(700).chain(
+      param.defenderTD.sprite.avoid()
+    )
+  );
+}
+
+/** フェイント */
+function feint(param: AttackAnimationParam<ShinBraver, Feint>): Animate {
+  if (!param.result.isDefenderMoved) {
+    return empty();
+  }
+
+  return param.defenderTD.sprite.avoid();
 }
