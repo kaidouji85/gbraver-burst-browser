e1d94bb5775b238fcc4b923a1a6cc5b318999515
竹内佑介 <kaidouji85@gmail.com>
2019-05-12 21:32:15 +0900

    ネオランドーザの攻撃アニメーションを新形式に置き換えた

--- src/scene/battle/animation/state-history/battle/attack/index.js
+++ src/scene/battle/animation/state-history/battle/attack/index.js
@@ -7,10 +7,10 @@ import type {BattleSceneState} from "../../../../state/battle-scene-state";
 import type {GameState} from "gbraver-burst-core/lib/game-state/game-state";
 import {ShinBraver} from "../../../../../../game-object/armdozer/shin-breaver/shin-braver";
 import {NeoLandozer} from "../../../../../../game-object/armdozer/neo-landozer/neo-landozer";
-import {neoLandozerAttack} from "./neo-landozer";
 import type {Battle} from "gbraver-burst-core/lib/effect/battle/effect/index";
 import {shinBraverAttack} from "./shin-braver";
 import {overWriteTDSprite} from "../../../../view/td/player";
+import {neoLandozerAttack} from "./neo-landozer";
 
 /** 攻撃側スプライトに応じて、戦闘アニメーションを切り替える */
 export function attackAnimation(view: BattleSceneView, sceneState: BattleSceneState, gameState: GameState): Animate {
@@ -47,7 +47,10 @@ export function attackAnimation(view: BattleSceneView, sceneState: BattleSceneSt
       attackerTD: overWriteTDSprite(attackerTD, sprite),
     });
   } else if (sprite instanceof NeoLandozer) {
-    return neoLandozerAttack(view, sceneState, gameState);
+    return neoLandozerAttack({
+      ...commonAnimParam,
+      attackerTD: overWriteTDSprite(attackerTD, sprite)
+    })
   }
 
   return emptyBattleAnimation(view, sceneState, gameState);
