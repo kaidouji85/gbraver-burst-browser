668bfd60d62247ed12d1358132c2b8f854e5633e
竹内佑介 <kaidouji85@gmail.com>
2019-05-13 22:34:50 +0900

    各種オブジェクトから戦闘アニメーションパラメータを作る処理を関数として切り出した

--- src/scene/battle/animation/state-history/battle/animation-param.js
+++ src/scene/battle/animation/state-history/battle/animation-param.js
@@ -7,6 +7,11 @@ import type {ArmDozerSprite} from "../../../../../game-object/armdozer/armdozer-
 import type {TDGameObjects} from "../../../view/td/game-objects";
 import type {HUDGameObjects} from "../../../view/hud/game-objects";
 import {overWriteTDSprite} from "../../../view/td/player";
+import type {BattleResult} from "gbraver-burst-core/lib/effect/battle/result/battle-result";
+import {BattleSceneView} from "../../../view";
+import type {BattleSceneState} from "../../../state/battle-scene-state";
+import type {GameState} from "gbraver-burst-core/lib/game-state/game-state";
+import type {Battle} from "gbraver-burst-core/lib/effect/battle/effect/index";
 
 /**
  * 戦闘アニメーション共通で使うパラメータ
@@ -28,6 +33,45 @@ export type BattleAnimationParam<SPRITE, RESULT> = {
   hudObjects: HUDGameObjects,
   result: RESULT
 };
+
+/**
+ * 各種オブジェクトから戦闘アニメパラメータを生成する
+ *
+ * @param view ビュー
+ * @param sceneState 戦闘画面のステート
+ * @param gameState ゲームステート
+ * @return 戦闘アニメパラメータ
+ */
+export function toBattleAnimationParam(view: BattleSceneView, sceneState: BattleSceneState, gameState: GameState): ?BattleAnimationParam<ArmDozerSprite, BattleResult> {
+  if (gameState.effect.name !== 'Battle') {
+    return null;
+  }
+
+  const effect: Battle = gameState.effect;
+  const attackerState = gameState.players.find(v => v.playerId === effect.attacker);
+  const attackerTD = view.td.players.find(v => v.playerId === effect.attacker);
+  const attackerHUD = view.hud.players.find(v => v.playerId === effect.attacker);
+  const defenderState = gameState.players.find(v => v.playerId !== effect.attacker);
+  const defenderTD = view.td.players.find(v => v.playerId !== effect.attacker);
+  const defenderHUD = view.hud.players.find(v => v.playerId !== effect.attacker);
+  if (!attackerState || !attackerTD || !attackerHUD || !defenderState || !defenderTD || !defenderHUD) {
+    return null;
+  }
+
+  return {
+    attackerBattery: effect.attackerBattery,
+    attackerState: attackerState,
+    attackerTD: attackerTD,
+    attackerHUD: attackerHUD,
+    defenderBattery: effect.defenderBattery,
+    defenderState: defenderState,
+    defenderTD: defenderTD,
+    defenderHUD: defenderHUD,
+    tdObjects: view.td.gameObjects,
+    hudObjects: view.hud.gameObjects,
+    result: effect.result,
+  };
+}
 
 /**
  * 戦闘アニメーションパラメータの戦闘結果を引数の内容で上書きする
--- src/scene/battle/animation/state-history/battle/index.js
+++ src/scene/battle/animation/state-history/battle/index.js
@@ -7,10 +7,7 @@ import type {GameState} from "gbraver-burst-core/lib/game-state/game-state";
 import {visibleBattery} from "./visible-battery";
 import {delay, empty} from "../../../../../animation/delay";
 import {attackAnimation} from "./attack";
-import type {Battle} from "gbraver-burst-core/lib/effect/battle/effect/index";
-import type {BattleAnimationParam} from "./animation-param";
-import type {BattleResult} from "gbraver-burst-core/lib/effect/battle/result/battle-result";
-import type {ArmDozerSprite} from "../../../../../game-object/armdozer/armdozer-sprite";
+import {toBattleAnimationParam} from "./animation-param";
 
 /**
  * 戦闘アニメーション
@@ -21,34 +18,10 @@ import type {ArmDozerSprite} from "../../../../../game-object/armdozer/armdozer-
  * @return アニメーション
  */
 export function battleAnimation(view: BattleSceneView, sceneState: BattleSceneState, gameState: GameState): Animate {
-  if (gameState.effect.name !== 'Battle') {
+  const param = toBattleAnimationParam(view, sceneState, gameState);
+  if (!param) {
     return empty();
   }
-
-  const effect: Battle = gameState.effect;
-  const attackerState = gameState.players.find(v => v.playerId === effect.attacker);
-  const attackerTD = view.td.players.find(v => v.playerId === effect.attacker);
-  const attackerHUD = view.hud.players.find(v => v.playerId === effect.attacker);
-  const defenderState = gameState.players.find(v => v.playerId !== effect.attacker);
-  const defenderTD = view.td.players.find(v => v.playerId !== effect.attacker);
-  const defenderHUD = view.hud.players.find(v => v.playerId !== effect.attacker);
-  if (!attackerState || !attackerTD || !attackerHUD || !defenderState || !defenderTD || !defenderHUD) {
-    return empty();
-  }
-
-  const param: BattleAnimationParam<ArmDozerSprite, BattleResult> = {
-    attackerBattery: effect.attackerBattery,
-    attackerState: attackerState,
-    attackerTD: attackerTD,
-    attackerHUD: attackerHUD,
-    defenderBattery: effect.defenderBattery,
-    defenderState: defenderState,
-    defenderTD: defenderTD,
-    defenderHUD: defenderHUD,
-    tdObjects: view.td.gameObjects,
-    hudObjects: view.hud.gameObjects,
-    result: effect.result,
-  };
 
   return visibleBattery(param)
     .chain(delay(500))
