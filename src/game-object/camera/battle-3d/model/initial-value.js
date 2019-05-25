e3d09f263bf066f518f8d2deee0596c749685332
竹内佑介 <kaidouji85@gmail.com>
2019-05-25 19:15:14 +0900

    モバイルでゲージとキャラがかぶる不具合を修正

--- src/game-object/camera/battle-3d/model/initial-value.js
+++ src/game-object/camera/battle-3d/model/initial-value.js
@@ -7,12 +7,12 @@ export function createInitialValue(): Battle3DCameraModel {
   return {
     position: {
       x: 0,
-      y: 160,
+      y: 220,
       z: 300
     },
     target: {
       x: 0,
-      y: 140,
+      y: 200,
       z: 0
     }
   };
