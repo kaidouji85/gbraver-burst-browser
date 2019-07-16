478af6a51aa9c683f5d70310e6b1047dbb6a7c1d
竹内佑介 <kaidouji85@gmail.com>
2019-07-17 01:05:03 +0900

    ライトを追加した

--- src/game-object/stage/shopping-street/index.js
+++ src/game-object/stage/shopping-street/index.js
@@ -12,6 +12,8 @@ export default class ShoppingStreet implements Stage {
   _cityMesh: THREE.Mesh;
   _directionalLight1: THREE.DirectionalLight;
   _directionalLight2: THREE.DirectionalLight;
+  _directionalLight3: THREE.DirectionalLight;
+  _directionalLight4: THREE.DirectionalLight;
   _ambientLight: THREE.AmbientLight;
 
   constructor(resources: Resources) {
@@ -23,6 +25,12 @@ export default class ShoppingStreet implements Stage {
 
     this._directionalLight2 = new THREE.DirectionalLight(0xAAAAAA, 0.8);
     this._directionalLight2.position.set(-1, 1, 1);
+
+    this._directionalLight3 = new THREE.DirectionalLight(0xAAAAAA, 0.5);
+    this._directionalLight3.position.set(1, 0, 1);
+
+    this._directionalLight4 = new THREE.DirectionalLight(0xAAAAAA, 0.5);
+    this._directionalLight4.position.set(-1, 0, 1);
 
     this._ambientLight = new THREE.AmbientLight(0xAAAAAA);
   }
@@ -38,6 +46,8 @@ export default class ShoppingStreet implements Stage {
       this._skyBox,
       this._directionalLight1,
       this._directionalLight2,
+      this._directionalLight3,
+      this._directionalLight4,
       this._ambientLight,
     ];
   }
