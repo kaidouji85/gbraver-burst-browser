a341b2e443b216aa7225d5079090b120d05a06c0
竹内佑介 <kaidouji85@gmail.com>
2019-09-01 17:21:08 +0900

    警告メッセージの拡大率をウインドウ幅に応じて変更するようにした

--- src/game-object/warning/play-in-landscape/index.js
+++ src/game-object/warning/play-in-landscape/index.js
@@ -6,6 +6,7 @@ import type {Resources} from "../../../resource";
 import {Observable} from "rxjs";
 import type {GameObjectAction} from "../../../action/game-object-action";
 import {CANVAS_IMAGE_IDS} from "../../../resource/canvas-image";
+import type {PreRender} from "../../../action/game-loop/pre-render";
 
 /** キャンバスサイズ */
 export const CANVAS_SIZE = 512;
@@ -26,6 +27,12 @@ export class PlayInLandscape {
       canvasSize: CANVAS_SIZE,
       image: playInLandscape,
     });
+
+    listener.subscribe(action => {
+      if (action.type === 'PreRender') {
+        this._preRender(action);
+      }
+    });
   }
 
   /**
@@ -35,5 +42,27 @@ export class PlayInLandscape {
    */
   getObject3D(): THREE.Object3D {
     return this._mesh.getObject3D();
+  }
+
+  /**
+   * プリレンダーの処理
+   *
+   * @param action アクション
+   */
+  _preRender(action: PreRender): void {
+    const scale = this._getScale(action.rendererDOM.clientWidth);
+    this._mesh.getObject3D().scale.set(scale, scale, scale);
+  }
+
+  /**
+   * 画面幅からメッシュのスケールを取得する
+   *
+   * @param screenWidth 画面幅
+   * @return {number} メッシュのスケール
+   */
+  _getScale(screenWidth: number): number {
+    return (CANVAS_SIZE < screenWidth)
+      ? 1
+      : 0.9 * screenWidth / CANVAS_SIZE;
   }
 }
