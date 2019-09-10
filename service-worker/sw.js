146bdc6fce4fe50fcc94dedff46a3597f33e8243
竹内佑介 <kaidouji85@gmail.com>
2019-09-11 00:32:54 +0900

    ランタイムキャッシュに1週間の起源を追加した

--- service-worker/sw.js
+++ service-worker/sw.js
@@ -1,6 +1,7 @@
 import * as PreCaching from 'workbox-precaching';
 import * as Routing from 'workbox-routing';
 import * as Strategies from 'workbox-strategies';
+import * as Expiration from 'workbox-expiration';
 
 PreCaching.precacheAndRoute([
   {url: "index.html", revision: REVISION_INDEX_HTML},
@@ -10,6 +11,11 @@ PreCaching.precacheAndRoute([
 Routing.registerRoute(
   /\.(?:png|glb)$/,
   new Strategies.CacheFirst({
-    cacheName: 'resource-cache'
+    cacheName: 'resource-cache',
+    plugins: [
+      new Expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 7,
+      }),
+    ]
   })
 );
