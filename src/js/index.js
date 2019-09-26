b63c968e54ddd3dab99054d6acd15a46d521b65e
takeuchi-y <takeuchi.y@opst.co.jp>
2019-09-26 17:50:21 +0900

    サービスワーカー更新チェックを追加した

--- src/js/index.js
+++ src/js/index.js
@@ -11,13 +11,25 @@ import {Game} from "./game";
 async function main(): Promise<void> {
   try {
     addEventToLoadingManager();
-    loadServiceWorker();
     viewPerformanceStats(document.body);
+    const sw = await loadServiceWorker();
+    if (sw && willUpdate(sw)) {
+      console.log("service worker will update!!");
+      return;
+    }
     const resources = await loadAllResource(`${GBRAVER_BURST_RESOURCE_HASH}/`);
     new Game(resources);
   } catch (e) {
     console.error(e.stack);
   }
+}
+
+function willUpdate(sw: ServiceWorkerRegistration): boolean {
+  const hasActiveSW = sw.active !== null;
+  const hasInstallingSW = sw.installing !== null;
+  const hasWaitingSW = sw.waiting !== null;
+
+  return hasActiveSW && (!hasInstallingSW || !hasWaitingSW);
 }
 
 window.onload = main;
