6dfcf54f638199e4af4cb17843b4b382110fd3c4
takeuchi-y <takeuchi.y@opst.co.jp>
2019-10-02 11:14:07 +0900

    戦闘クラス生成系処理を書き直した

--- src/js/game/index.js
+++ src/js/game/index.js
@@ -17,25 +17,42 @@ import type {DOMEvent} from "../action/dom-event";
 import {createRender} from "../render/create-render";
 import {OfflineBattleRoom} from "../battle-room/offline-battle-room";
 
+/** シーン */
+export type Scene = {
+  destructor(): void;
+};
+
+/** 空のシーン */
+export function emptyScene(): Scene {
+  return {
+    destructor: () => {
+      // NOP
+    }
+  };
+}
+
 /** ゲーム全体の制御を行う */
 export class Game {
   _resources: Resources;
-  _scene: BattleScene;
+
   _threeJsRender: THREE.WebGLRenderer;
-  _renderer: Renderer;
-  _gameLoop: Observable<GameLoop>;
   _domEvent: Observable<DOMEvent>;
+  _renderer: Renderer;
+
+  _scene: Scene;
+  _sceneSubscription: Subscription[];
+
+  _gameLoop: Observable<GameLoop>;
   _endBattle: Subject<EndBattle>;
-  _subscription: Subscription;
+  _gameSubscription: Subscription[];
 
   constructor(resources: Resources) {
+    this._resources = resources;
+
     this._threeJsRender = createRender();
     if (this._threeJsRender.domElement && document.body) {
       document.body.appendChild(this._threeJsRender.domElement);
     }
-
-    this._resources = resources;
-    this._gameLoop = createGameLoopListener();
     this._domEvent = createDOMEventListener(this._threeJsRender.domElement);
     this._renderer = new Renderer({
       renderer: this._threeJsRender,
@@ -43,15 +60,32 @@ export class Game {
         domEvent: this._domEvent
       }
     });
+
+    this._scene = emptyScene();
+    this._sceneSubscription = [];
+
+    this._gameLoop = createGameLoopListener();
     this._endBattle = new Subject();
-
-    this._subscription = this._endBattle.subscribe(action => {
-      if (action.type === 'endBattle') {
+    this._gameSubscription = [
+      this._endBattle.subscribe(action => {
         this._onEndBattle(action);
-      }
-    });
+      })
+    ];
 
-    this._startBattle();
+    this._onStart();
+  }
+
+  /** デストラクタ相当の処理 */
+  destructor(): void {
+    this._scene.destructor();
+    this._disposeSceneSubscription();
+    this._disposeGameSubscription();
+  }
+
+  /** ゲーム開始時のイベント */
+  async _onStart(): Promise<void> {
+    const battleScene = await this._createBattleScene();
+    this._changeBattleScene(battleScene);
   }
 
   /**
@@ -59,13 +93,17 @@ export class Game {
    *
    * @param action アクション
    */
-  _onEndBattle(action: EndBattle): void {
-    this._scene.destructor();
-    this._startBattle();
+  async _onEndBattle(action: EndBattle): Promise<void> {
+    const battleScene = await this._createBattleScene();
+    this._changeBattleScene(battleScene);
   }
 
-  /** 戦闘を開始するヘルパーメソッド */
-  async _startBattle() {
+  /**
+   * 戦闘シーンを生成する
+   *
+   * @returns 戦闘シーン
+   */
+  async _createBattleScene(): Promise<BattleScene> {
     // TODO テスト用にダミーデータを用いている
     const player: Player = {
       playerId: 'test01',
@@ -75,7 +113,7 @@ export class Game {
     const battleRoom = new OfflineBattleRoom(player, npc);
     const initialState = await battleRoom.start();
 
-    this._scene = new BattleScene({
+    return new BattleScene({
       resources: this._resources,
       rendererDOM: this._threeJsRender.domElement,
       battleRoom: battleRoom,
@@ -89,7 +127,32 @@ export class Game {
         endBattle: this._endBattle
       }
     });
-    // デバッグ用にレンダラ情報をコンソールに出力
-    // console.log(this._renderer._renderer.info);
+  }
+
+  /**
+   * 現在のシーンを戦闘シーンに変更する
+   *
+   * @param battleScene 戦闘シーン
+   */
+  _changeBattleScene(battleScene: BattleScene): void {
+    this._disposeSceneSubscription();
+    this._scene.destructor();
+
+    this._scene = battleScene;
+    this._sceneSubscription = [];
+  }
+
+  /** シーン固有のサブスクリプションを破棄する */
+  _disposeSceneSubscription(): void {
+    this._sceneSubscription.forEach(v => {
+      v.unsubscribe();
+    });
+  }
+
+  /** ゲーム固有のサブスクリプションを破棄する */
+  _disposeGameSubscription(): void {
+    this._gameSubscription.forEach(v => {
+      v.unsubscribe();
+    });
   }
 }
