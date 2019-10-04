e281f48b63ae96bfd01e97fceb4861370be8bfa3
takeuchi-y <takeuchi.y@opst.co.jp>
2019-10-04 17:47:34 +0900

    コメント漏れを修正

--- src/js/action/loading/create-listener.js
+++ src/js/action/loading/create-listener.js
@@ -4,6 +4,12 @@ import {Observable} from "rxjs";
 import type {LoadingAction} from "./loading";
 import * as THREE from "three";
 
+/**
+ * ローディングアクションのストリームを生成する
+ *
+ * @param manager THREE.jsのローディングマネジャー
+ * @return 生成結果
+ */
 export function createLoadingActionListener(manager: THREE.LoadingManager): Observable<LoadingAction> {
   return new Observable(subscriber => {
     manager.onProgress = (url: string, itemsLoaded: number, itemsTotal: number) => {
--- src/js/action/service-worker/create-listener.js
+++ src/js/action/service-worker/create-listener.js
@@ -4,6 +4,12 @@ import {Observable} from "rxjs";
 import type {ServiceWorkerAction} from "./service-worker";
 import {hasWaitingServiceWorker} from "../../service-worker/has-waiting-service-worker";
 
+/**
+ * サービスワーカーアクションのストリームを生成する
+ *
+ * @param serviceWorker サービスワーカー
+ * @returns 生成結果
+ */
 export function createServiceWorkerActionListener(serviceWorker: ServiceWorkerRegistration): Observable<ServiceWorkerAction> {
   return new Observable(subscriber => {
     serviceWorker.onupdatefound = () => {
