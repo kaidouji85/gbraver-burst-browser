96c6bb8305dd0b6b5f53def27fefdf798cf3509b
takeuchi-y <takeuchi.y@opst.co.jp>
2019-10-04 11:36:57 +0900

    スタイル調整

--- src/js/outer-game/service-worker-update/view/service-worker-update-view.js
+++ src/js/outer-game/service-worker-update/view/service-worker-update-view.js
@@ -1,7 +1,7 @@
 // @flow
 
 import {render} from 'react-dom';
-import {ServiceWorkerUpdate} from './service-worker-update';
+import {ServiceWorkerUpdateComponent} from './service-worker-update-component';
 import type {ServiceWorkerUpdateState} from "../state/service-worker-update-state";
 
 /** サービスワーカー更新ビュー */
@@ -18,7 +18,7 @@ export class ServiceWorkerUpdateView {
    * @param state 状態
    */
   engage(state: ServiceWorkerUpdateState): void {
-    render(ServiceWorkerUpdate({
+    render(ServiceWorkerUpdateComponent({
       isVisible: state.isVisible
     }), this._dom);
   }
