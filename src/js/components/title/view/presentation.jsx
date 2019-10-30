92bc33b61a3a182f19259a1f875374c2272c0e6d
竹内佑介 <kaidouji85@gmail.com>
2019-10-30 22:07:18 +0900

    タイトルロゴ復活

--- src/js/components/title/view/presentation.jsx
+++ src/js/components/title/view/presentation.jsx
@@ -1,6 +1,7 @@
 // @flow
 
 import React from 'react';
+import {resourceBasePath} from "../../../resource/resource-base-path";
 
 /** プロパティ */
 type Props = {
@@ -16,7 +17,7 @@ export function TitlePresentation(props: Props) {
     <div className="title" onClick={props.onTouch} onTouchStart={props.onTouch} style={{
       display: props.isVisible ? 'flex' : 'none'
     }}>
-      タイトル
+      <img src={`${resourceBasePath()}/logo.png`} />
     </div>
   );
 }
