a5f943910314cf7447f8bfd8f82fbc8808172b29
竹内佑介 <kaidouji85@gmail.com>
2019-11-02 16:08:59 +0900

    SW更新メッセージを修正

--- src/js/game/components/play-in-landscape/presentation.jsx
+++ src/js/game/components/play-in-landscape/presentation.jsx
@@ -3,6 +3,11 @@
 import React from 'react';
 import {resourceBasePath} from "../../../resource/resource-base-path";
 
+/**
+ * ランドスケープ警告ビュー
+ *
+ * @return ランドスケープ警告ビュー
+ */
 export function PlayInLandscapePresentation() {
   return (
     <div className="play-in-landscape">
