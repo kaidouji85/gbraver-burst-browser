84027c81139656fd14b0afeb1edcb7381b11860e
竹内佑介 <kaidouji85@gmail.com>
2019-11-03 22:35:56 +0900

    ランタイムキャッシュ削除のロジックを追加

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,32 +1,49 @@
+import * as Core from 'workbox-core';
 import * as PreCaching from 'workbox-precaching';
 import * as Routing from 'workbox-routing';
 import * as Strategies from 'workbox-strategies';
-import * as Expiration from 'workbox-expiration';
 
 PreCaching.precacheAndRoute([
   {url: "index.html", revision: REVISION_INDEX_HTML}
 ]);
 
+/**
+ * ランタイムキャッシュのキー
+ * 末尾にビルドごとのハッシュ値を追加して、現行バージョンのキャッシュか否かを判別できるようにしている
+ */
+const RUNTIME_CACHE_KEY = `RUNTIME_CACHE_KEY_-${RUNTIME_CACHE_HASH}`;
+
 Routing.registerRoute(
   /\/.+-index\.js$/,
   new Strategies.CacheFirst({
-    cacheName: 'js-cache',
-    plugins: [
-      new Expiration.Plugin({
-        maxAgeSeconds: 60 * 60 * 24 * 7,
-      }),
-    ]
+    cacheName: RUNTIME_CACHE_KEY
   })
 );
 
 Routing.registerRoute(
   /\.(?:png|glb|css|json)$/,
   new Strategies.CacheFirst({
-    cacheName: 'resource-cache',
-    plugins: [
-      new Expiration.Plugin({
-        maxAgeSeconds: 60 * 60 * 24 * 7,
-      }),
-    ]
+    cacheName: RUNTIME_CACHE_KEY
   })
 );
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clearOldRuntimeCache());
+});
+
+/** 古いランタイムキャッシュを削除する */
+async function clearOldRuntimeCache() {
+  try {
+    const keys = await caches.keys();
+    const deleteKeys = keys
+      .filter(v => v !== RUNTIME_CACHE_KEY)
+      .filter(v => v!== Core.cacheNames.precache);
+    await Promise.all(deleteKeys.map(v => {
+      console.log(`delete cache ${v}`);
+      return caches.delete(v);
+    }
+    ));
+  } catch(e) {
+    throw e;
+  }
+}
