0b907aae7eb6a140455a73de7d082a5d93dce67a
竹内佑介 <kaidouji85@gmail.com>
2019-11-05 22:22:56 +0900

    ゲームスタート時に攻撃側プレイヤーをズームアップするようにした

--- src/js/game/components/three-js-canvas/scene/battle/animation/state-history/start-game.js
+++ src/js/game/components/three-js-canvas/scene/battle/animation/state-history/start-game.js
@@ -4,11 +4,17 @@ import {Animate} from "../../../../../../../animation/animate";
 import {BattleSceneView} from "../../view";
 import type {BattleSceneState} from "../../state/battle-scene-state";
 import type {GameState} from "gbraver-burst-core/lib/game-state/game-state";
-import {all} from "../../../../../../../animation/all";
 import {delay, empty} from "../../../../../../../animation/delay";
+import {all} from "../../../../../../../animation/all";
 
-/** ターン変更のアニメーション */
-export function turnChangeAnimation(view: BattleSceneView, sceneState: BattleSceneState, gameState: GameState): Animate {
+/**
+ * ゲーム開始時のアニメーション
+ *
+ * @param view ビュー
+ * @param sceneState シーンの状態
+ * @param gameState ゲームの状態
+ */
+export function startGameAnimation(view: BattleSceneView, sceneState: BattleSceneState, gameState: GameState): Animate {
   const activeTDPlayer = view.td.players.find(v => v.playerId === gameState.activePlayerId);
   const activeHUDPlayer = view.hud.players.find(v => v.playerId === gameState.activePlayerId);
   const activeStatus = gameState.players.find(v => v.playerId === gameState.activePlayerId);
@@ -19,20 +25,14 @@ export function turnChangeAnimation(view: BattleSceneView, sceneState: BattleSce
   const cameraX = activeTDPlayer.sprite.getObject3D().position.x;
   return all(
     all(
-      view.td.camera.moveViewPoint({x: cameraX}, 500),
-      view.td.camera.moveCamera({x: cameraX, z: '-50'}, 500)
+      view.td.camera.moveViewPoint({x: cameraX}, 300),
+      view.td.camera.moveCamera({x: cameraX, z: '-50'}, 300)
     ).chain(
       delay(1300)
     ).chain(
-      view.td.camera.moveViewPoint({x: 0}, 500),
-      view.td.camera.moveCamera({x: 0, z: '+50'}, 500)
-    ),
-
-    delay(700).chain(all(
-      // TODO バッテリー回復値をeffectに持たせる
-      activeTDPlayer.recoverBattery.popUp(3),
-      activeHUDPlayer.gauge.battery(activeStatus.armdozer.battery),
-    ))
+      view.td.camera.moveViewPoint({x: 0}, 300),
+      view.td.camera.moveCamera({x: 0, z: '+50'}, 300)
+    )
   ).chain(
     delay(800)
   );
