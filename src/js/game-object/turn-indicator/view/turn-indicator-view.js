3f0c06bdbdbc3d139353671b6651c2927f35dea5
竹内佑介 <kaidouji85@gmail.com>
2019-11-17 14:42:24 +0900

    ゴミ掃除

--- src/js/game-object/turn-indicator/view/turn-indicator-view.js
+++ src/js/game-object/turn-indicator/view/turn-indicator-view.js
@@ -10,14 +10,25 @@ import {CANVAS_IMAGE_IDS} from "../../../resource/canvas-image";
 
 export const MESH_SIZE = 200;
 export const CANVAS_SIZE = 512;
-export const CANVAS_NUM = 3;
 
 /** ターンインジケータービュー */
 export class TurnIndicatorView {
   _canvasMesh: CanvasMesh;
 
   constructor(resources: Resources) {
-    this._canvasMesh = createCanvasMesh(resources);
+    this._canvasMesh = new CanvasMesh({
+      canvasWidth: CANVAS_SIZE,
+      canvasHeight: CANVAS_SIZE,
+      meshWidth: MESH_SIZE,
+      meshHeight: MESH_SIZE,
+    });
+    this._canvasMesh.draw(context => {
+      const turnIndicatorResource: ?CanvasImageResource = resources.canvasImages.find(v => v.id === CANVAS_IMAGE_IDS.TURN_INDICATOR);
+      const turnIndicatorImage: Image = turnIndicatorResource ? turnIndicatorResource.image : new Image();
+      const dx = context.canvas.width / 2;
+      const dy = context.canvas.height / 2;
+      drawImageInCenter(context, turnIndicatorImage, dx, dy);
+    });
   }
 
   /** デストラクタ */
@@ -27,7 +38,20 @@ export class TurnIndicatorView {
 
   /** モデルをビューに反映させる */
   engage(model: TurnIndicatorModel): void {
-    engageMesh(this._canvasMesh, model);
+    const scale = (model.animation * 0.3 + 0.7);
+    this._canvasMesh.mesh.scale.x = model.isPlayerTurn
+      ? scale
+      : -scale;
+    this._canvasMesh.mesh.scale.y = scale;
+
+    const x = 70 - 120 * model.animation;
+    this._canvasMesh.mesh.position.x = model.isPlayerTurn
+      ? x
+      : -x;
+    this._canvasMesh.mesh.position.y = 150;
+    this._canvasMesh.mesh.position.z = 20;
+
+    this._canvasMesh.setOpacity(model.opacity);
   }
 
   /** カメラの方向を向く */
@@ -39,45 +63,4 @@ export class TurnIndicatorView {
   getObject3D(): THREE.Object3D {
     return this._canvasMesh.getObject3D();
   }
-}
-
-/** キャンバスメッシュを生成するヘルパー関数 */
-function createCanvasMesh(resources: Resources): CanvasMesh {
-  const mesh = new CanvasMesh({
-    canvasWidth: CANVAS_SIZE,
-    canvasHeight: CANVAS_SIZE,
-    meshWidth: MESH_SIZE,
-    meshHeight: MESH_SIZE,
-  });
-  mesh.draw(context => {
-    const turnIndicatorResource: ?CanvasImageResource = resources.canvasImages.find(v => v.id === CANVAS_IMAGE_IDS.TURN_INDICATOR);
-    const turnIndicatorImage: Image = turnIndicatorResource ? turnIndicatorResource.image : new Image();
-    const dx = context.canvas.width / 2;
-    const dy = context.canvas.height / 2;
-    drawImageInCenter(context, turnIndicatorImage, dx, dy);
-  });
-  return mesh;
-}
-
-/**
- * キャンバスメッシュにモデルを反省させる
- *
- * @param canvasMesh
- * @param model
- */
-function engageMesh(canvasMesh: CanvasMesh, model: TurnIndicatorModel): void {
-  const scale = (model.animation * 0.3 + 0.7);
-  canvasMesh.mesh.scale.x = model.isPlayerTurn
-    ? scale
-    : -scale;
-  canvasMesh.mesh.scale.y = scale;
-
-  const x = 70 - 120 * model.animation;
-  canvasMesh.mesh.position.x = model.isPlayerTurn
-    ? x
-    : -x;
-  canvasMesh.mesh.position.y = 150;
-  canvasMesh.mesh.position.z = 20;
-
-  canvasMesh.setOpacity(model.opacity);
 }
