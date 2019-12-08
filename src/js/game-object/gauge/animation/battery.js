0aadaa9f0e52ef95288dcc9b207002eeab8305df
竹内佑介 <kaidouji85@gmail.com>
2019-12-08 17:18:38 +0900

    コメントを追加

--- src/js/game-object/gauge/animation/battery.js
+++ src/js/game-object/gauge/animation/battery.js
@@ -5,7 +5,13 @@ import type {GaugeModel} from "../model/gauge-model";
 import {tween} from "../../../animation/tween";
 import {all} from "../../../animation/all";
 
-/** バッテリーを変更するアニメーション */
+/**
+ * バッテリーを変更するアニメーション
+ *
+ * @param model モデル
+ * @param value バッテリー値
+ * @return アニメーション
+ */
 export function battery(model: GaugeModel, value: number): Animate {
   const animations = model.batteryList.map(v => {
     const opacity = v.value <= value ? 1 : 0;
--- src/js/game-object/gauge/view/enemy-battery-gauge.js
+++ src/js/game-object/gauge/view/enemy-battery-gauge.js
@@ -3,6 +3,7 @@
 import {PlayerBatteryGauge} from "./player-battery-gauge";
 import type {Resources} from "../../../resource";
 
+/** 敵のバッテリーゲージ */
 export class EnemyBatteryGauge extends PlayerBatteryGauge {
   constructor(resources: Resources) {
     super(resources);
