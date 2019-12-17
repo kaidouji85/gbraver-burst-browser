9742c9979d04eb76c778b5e661f6996887f1cba1
竹内佑介 <kaidouji85@gmail.com>
2019-12-17 21:26:57 +0900

    ターンスタートポーズをさらに調整した

--- src/js/game-object/armdozer/neo-landozer/animation/turn-start.js
+++ src/js/game-object/armdozer/neo-landozer/animation/turn-start.js
@@ -14,12 +14,12 @@ import {delay} from "../../../../animation/delay";
  */
 export function turnStart(model: NeoLandozerModel): Animate {
   return process(() => {
-    model.animation.type = 'GUTS_DOWN';
+    model.animation.type = 'GUTS_UP';
     model.animation.frame = 0;
-  }).chain(tween(model.animation, t => t.to({frame: 1}, 200)))
+  }).chain(tween(model.animation, t => t.to({frame: 1}, 300)))
     .chain(delay(100))
     .chain(process(() => {
-      model.animation.type = 'GUTS_UP';
+      model.animation.type = 'GUTS_DOWN';
       model.animation.frame = 0;
     }))
     .chain(tween(model.animation, t => t.to({frame: 1}, 300)));
