8d6552bb40aa3101616f30a2e16f307d8c22e46b
takeuchi-y <takeuchi.y@opst.co.jp>
2019-12-24 10:36:32 +0900

    ローディングビューのファイル構成をシンプルにした

--- src/js/game/components/loading/index.js
+++ src/js/game/components/loading/index.js
@@ -1,22 +1,23 @@
 // @flow
 
+import {render} from 'react-dom';
 import type {LoadingState} from "./state/loading-state";
-import {LoadingView} from "./view";
 import {createInitialState} from "./state/initial-value";
 import {progress} from "./state/progress";
 import {complete} from "./state/complete";
 import type {LoadingAction, LoadingComplete, LoadingProgress, LoadingStart} from "../../../action/loading/loading";
 import {Observable, Subscription} from "rxjs";
+import {loadingView} from "./view/loading-view";
 
 /** ローディング */
 export class Loading {
+  _dom: HTMLElement;
   _state: LoadingState;
-  _view: LoadingView;
   _subscription: Subscription;
 
   constructor(dom: HTMLElement, loading: Observable<LoadingAction>) {
+    this._dom = dom;
     this._state = createInitialState();
-    this._view = new LoadingView(dom);
     this._subscription = loading.subscribe(action => {
       if (action.type === 'LoadingStart') {
         this._onLoadingStart(action);
@@ -27,12 +28,20 @@ export class Loading {
       }
     });
 
-    this._view.engage(this._state);
+    this._engage();
   }
 
   /** デストラクタ相当の処理 */
   destructor(): void {
     this._subscription.unsubscribe();
+  }
+
+  /** ステートをビューに反映させる */
+  _engage(): void {
+    const component = loadingView({
+      state: this._state
+    });
+    render(component, this._dom);
   }
 
   /**
@@ -42,7 +51,7 @@ export class Loading {
    */
   _onLoadingStart(action: LoadingStart): void {
     this._state = {...this._state, isVisible: true};
-    this._view.engage(this._state);
+    this._engage();
   }
 
   /**
@@ -52,7 +61,7 @@ export class Loading {
    */
   _onLoadingProgress(action: LoadingProgress): void {
     this._state = progress(this._state, action.completedRate);
-    this._view.engage(this._state);
+    this._engage();
   }
 
   /**
@@ -62,6 +71,6 @@ export class Loading {
    */
   _onLoadingComplete(action: LoadingComplete): void {
     this._state = complete(this._state);
-    this._view.engage(this._state);
+    this._engage();
   }
 }
