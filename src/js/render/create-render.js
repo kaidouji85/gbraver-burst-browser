3f7ee1fc0d8671ebd61ebc79d68385c7fcf5a396
竹内佑介 <kaidouji85@gmail.com>
2019-12-27 20:49:06 +0900

    ピクセルレート計算を共通関数に切り出した

--- src/js/render/create-render.js
+++ src/js/render/create-render.js
@@ -1,12 +1,13 @@
 // @flow
 import * as THREE from 'three';
+import {renderPixelRatio} from "./pixel-ratio";
 
 /** レンダラを生成する */
 export function createRender(): THREE.WebGLRenderer {
   const renderer = new THREE.WebGLRenderer();
   renderer.autoClear = false;
   renderer.setSize(window.innerWidth, window.innerHeight);
-  const pixelRatio = Math.min(window.devicePixelRatio, 2);
+  const pixelRatio = renderPixelRatio(window.devicePixcelRatio);
   renderer.setPixelRatio(pixelRatio);
   return renderer;
 }
