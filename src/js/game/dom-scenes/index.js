f21c614c2af3f715996da24917135b1ca8a81188
竹内佑介 <kaidouji85@gmail.com>
2020-02-02 13:55:27 +0900

    割り込みシーン、DOMシーンを別クラスにした

--- src/js/game/dom-scenes/index.js
+++ src/js/game/dom-scenes/index.js
@@ -1,52 +1,26 @@
 // @flow
 
-import {Loading} from "./loading";
 import {Observable} from "rxjs";
-import type {LoadingAction} from "../../action/loading/loading";
-import {ServiceWorkerUpdate} from "./service-worker-update";
-import type {ServiceWorkerAction} from "../../action/service-worker/service-worker";
 import {Title} from "./title";
 import type {EndTitle} from "../../action/game/end-title";
-import {PlayInLandscape} from "./play-in-landscape";
 
 /** イベント通知 */
 type Notifier = {
   endTitle: Observable<EndTitle>
 };
 
-/** コンストラクタのパラメータ */
-type Param = {
-  listener: {
-    loading: Observable<LoadingAction>,
-    serviceWorker: Observable<ServiceWorkerAction>,
-  }
-};
-
 /** HTMLオンリーで生成されたシーンを集めたもの */
 export class DOMScenes {
-  _loading: Loading;
-  _serviceWorkerUpdate: ServiceWorkerUpdate;
-  _playInLandscape: PlayInLandscape;
   _title: Title;
 
-  constructor(param: Param) {
-    const loadingDOM: HTMLElement = document.querySelector('#loading-scene') || document.createElement('div');
-    this._loading = new Loading(loadingDOM, param.listener.loading);
-
-    const serviceWorkerDOM: HTMLElement = document.querySelector("#service-worker-update-scene") || document.createElement('div');
-    this._serviceWorkerUpdate = new ServiceWorkerUpdate(serviceWorkerDOM, param.listener.serviceWorker);
-
+  constructor() {
     const titleDOM: HTMLElement = document.querySelector("#title-scene") || document.createElement('div');
     this._title = new Title(titleDOM);
-
-    const playInLandscapeDOM: HTMLElement = document.querySelector("#play-in-landscape") || document.createElement('div');
-    this._playInLandscape = new PlayInLandscape(playInLandscapeDOM);
   }
 
   /** デストラクタ相当の処理 */
   destructor() {
-    this._loading.destructor();
-    this._serviceWorkerUpdate.destructor();
+    // NOP
   }
 
   /**
