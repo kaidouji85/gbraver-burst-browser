dfcbc62ff5db77e75e05e680399f4d08b9e8a1e3
竹内佑介 <kaidouji85@gmail.com>
2020-02-02 19:06:29 +0900

    遊び方の文言を追加

--- src/js/game/dom-scenes/how-to-play/view/how-to-play-view.js
+++ src/js/game/dom-scenes/how-to-play/view/how-to-play-view.js
@@ -11,7 +11,14 @@ export class HowToPlayView {
     const rootId = domUuid();
     dom.innerHTML = `
       <div class="how-to-play" id="${rootId}">
-        遊び方(準備中)
+        <h1 class="how-to-play__title">遊び方</h1>
+        <ul class="how-to-play__rule">
+          <li class="how-to-play__rule__1">相手の体力を先にゼロにした方の勝ち</li>
+          <li class="how-to-play__rule__2">攻撃、防御側でバッテリーが出せる</li>
+          <li class="how-to-play__rule__3">バッテリーの大小でのみ当たり判定が行われる</li>
+          <li class="how-to-play__rule__4">1試合に1回だけ機体がパワーアップするバーストが使える</li>
+        </ul>
+        <button class="how-to-play__prev">戻る</button>
       </div>
     `;
 
