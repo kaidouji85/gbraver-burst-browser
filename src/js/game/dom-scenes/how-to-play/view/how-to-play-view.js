da1abce904703325c34626930dba7267e2eb3163
竹内佑介 <kaidouji85@gmail.com>
2020-02-03 12:57:08 +0900

    遊び方ページのスタイルを調整した

--- src/js/game/dom-scenes/how-to-play/view/how-to-play-view.js
+++ src/js/game/dom-scenes/how-to-play/view/how-to-play-view.js
@@ -22,14 +22,16 @@ export class HowToPlayView {
     const prevId = domUuid();
     dom.innerHTML = `
       <div class="how-to-play" id="${rootId}">
-        <h1 class="how-to-play__title">遊び方</h1>
-        <ul class="how-to-play__rule">
-          <li class="how-to-play__rule__1">相手の体力を先にゼロにした方の勝ち</li>
-          <li class="how-to-play__rule__2">攻撃、防御側でバッテリーが出せる</li>
-          <li class="how-to-play__rule__3">バッテリーの大小でのみ当たり判定が行われる</li>
-          <li class="how-to-play__rule__4">1試合に1回だけ機体がパワーアップするバーストが使える</li>
-        </ul>
-        <button class="how-to-play__prev" id="${prevId}">戻る</button>
+        <div class="how-to-play__container">
+          <h1 class="how-to-play__container__title">遊び方</h1>
+          <ul class="how-to-play__container__rule">
+            <li class="how-to-play__container__rule__1">相手の体力を先にゼロにした方の勝ち</li>
+            <li class="how-to-play__container__rule__2">攻撃、防御側でバッテリーが出せる</li>
+            <li class="how-to-play__container__rule__3">バッテリーの大小でのみ当たり判定が行われる</li>
+            <li class="how-to-play__container__rule__4">1試合に1回だけ機体がパワーアップするバーストが使える</li>
+          </ul>
+          <button class="how-to-play__container__prev" id="${prevId}">戻る</button>
+        </div>
       </div>
     `;
 
