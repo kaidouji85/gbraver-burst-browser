62a15bc3484c760774fb378e75a581872daf4834
竹内佑介 <kaidouji85@gmail.com>
2020-02-18 10:09:26 +0900

    レイアウト調整をした

--- src/js/game/dom-scenes/how-to-play/index.js
+++ src/js/game/dom-scenes/how-to-play/index.js
@@ -3,6 +3,9 @@
 import {HowToPlayView} from "./view/how-to-play-view";
 import {howToPlayMovieURL} from "../../../how-to-play/how-to-play-movie";
 
+/**
+ * 遊び方シーン
+ */
 export class HowToPlay {
   _view: HowToPlayView;
 
--- src/js/game/dom-scenes/how-to-play/view/how-to-play-view.js
+++ src/js/game/dom-scenes/how-to-play/view/how-to-play-view.js
@@ -1,16 +1,23 @@
 // @flow
 
+/** パラメータ */
 export type Param = {
   dom: HTMLElement,
   movieURL: string
 };
 
+/**
+ * 遊び方シーンのビュー
+ */
 export class HowToPlayView {
   constructor(param: Param) {
     param.dom.innerHTML = `
       <div class="how-to-play">
-        <iframe width="560" height="315" src="${param.movieURL}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen> 
-        </iframe>
+        <div class="how-to-play__title">遊び方</div>
+        <div class="how-to-play__movie-container">
+          <iframe class="how-to-play__movie-container__movie" width="560" height="315" src="${param.movieURL}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <button class="how-to-play__prev">戻る</button>
       </div>
     `;
   }
