ff89c5301a1597f6a4fe5da8ff4d77feeffac87f
竹内佑介 <kaidouji85@gmail.com>
2020-02-18 21:33:26 +0900

    遊び方画面のレイアウト調整

--- src/js/game/dom-scenes/how-to-play/view/how-to-play-view.js
+++ src/js/game/dom-scenes/how-to-play/view/how-to-play-view.js
@@ -29,11 +29,12 @@ export class HowToPlayView {
     const prevId = domUuid();
     param.dom.innerHTML = `
       <div class="how-to-play" id="${rootId}">
-        <div class="how-to-play__title">遊び方</div>
         <div class="how-to-play__movie-container">
           <iframe class="how-to-play__movie-container__movie" width="560" height="315" src="${param.movieURL}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
-        <button class="how-to-play__prev" id="${prevId}">戻る</button>
+        <div class="how-to-play__controllers">
+          <button class="how-to-play__controllers__prev" id="${prevId}">戻る</button>
+        </div>
       </div>
     `;
 
