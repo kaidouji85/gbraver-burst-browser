8a51d8dc6417a504f4b38c7a88a8c476e3b384e2
竹内佑介 <kaidouji85@gmail.com>
2020-03-26 16:52:44 +0900

    ライトニングドーザのタックルメッシュを追加した

--- src/js/game-object/armdozer/lightning-dozer/model/lightning-dozer-model.js
+++ src/js/game-object/armdozer/lightning-dozer/model/lightning-dozer-model.js
@@ -1,5 +1,10 @@
 // @flow
 
+/** アニメーション種別 */
+export type AnimationType =
+  'STAND' |
+  'TACKLE';
+
 /** ライトニングドーザのモデル */
 export type LightningDozerModel = {
   /** 座標 */
@@ -7,5 +12,13 @@ export type LightningDozerModel = {
     x: number,
     y: number,
     z: number
+  },
+
+  /** アニメーション */
+  animation: {
+    /** 種別 */
+    type: AnimationType,
+    /** 0〜1で指定するフレーム */
+    frame: number
   },
 }
--- src/js/game-object/armdozer/lightning-dozer/view/player-lighting-dozer-view.js
+++ src/js/game-object/armdozer/lightning-dozer/view/player-lighting-dozer-view.js
@@ -4,8 +4,9 @@ import type {LightningDozerView} from "./lightning-dozer-view";
 import type {Resources} from "../../../../resource";
 import type {ArmdozerAnimation} from "../../mesh/armdozer-animation";
 import {lightningDozerStand} from "../mesh/stand";
-import type {LightningDozerModel} from "../model/lightning-dozer-model";
+import type {AnimationType, LightningDozerModel} from "../model/lightning-dozer-model";
 import * as THREE from "three";
+import {lightningDozerTackle} from "../mesh/sp-to-stand";
 
 /**
  * プレイヤー側のライトニングドーザビュー
@@ -13,12 +14,16 @@ import * as THREE from "three";
 export class PlayerLightingDozerView implements LightningDozerView {
   _group: THREE.Group;
   _stand: ArmdozerAnimation;
+  _tackle: ArmdozerAnimation;
 
   constructor(resources: Resources) {
     this._group = new THREE.Group();
 
     this._stand = lightningDozerStand(resources);
-    this._group.add(this._stand.getObject3D());
+    this._tackle = lightningDozerTackle(resources);
+    this._getAllMeshes().forEach(v => {
+      this._group.add(v.getObject3D());
+    });
   }
 
   /** デストラクタ相当の処理 */
@@ -38,8 +43,15 @@ export class PlayerLightingDozerView implements LightningDozerView {
       model.position.z
     );
 
-    this._stand.visible(true);
-    this._stand.animate(0);
+    const activeMesh = this._getActiveMesh(model.animation.type);
+    activeMesh.visible(true);
+    activeMesh.animate(model.animation.frame);
+
+    const disActiveMeshes = this._getAllMeshes()
+      .filter(v => v !== activeMesh);
+    disActiveMeshes.forEach(v => {
+      v.visible(false);
+    });
   }
 
   /**
@@ -58,5 +70,22 @@ export class PlayerLightingDozerView implements LightningDozerView {
    */
   lookAt(camera: THREE.Camera): void {
     this._group.quaternion.copy(camera.quaternion);
+  }
+
+  _getAllMeshes(): ArmdozerAnimation[] {
+    return [
+      this._stand,
+      this._tackle,
+    ];
+  }
+
+  _getActiveMesh(animationType: AnimationType): ArmdozerAnimation {
+    switch(animationType) {
+      case 'TACKLE':
+        return this._tackle;
+      case 'STAND':
+      default:
+        return this._stand;
+    }
   }
 }
