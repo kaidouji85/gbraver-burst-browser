0e68bd90aac06f9dce9a8cd4984f3952c9f20fa8
竹内佑介 <kaidouji85@gmail.com>
2020-03-26 16:35:52 +0900

    ネオランドーザのタックル画像を追加した

--- src/js/battle-room/dummy-battle-room.js
+++ src/js/battle-room/dummy-battle-room.js
@@ -6,6 +6,7 @@ import {ArmDozerIdList, ArmDozers} from "gbraver-burst-core";
 import type {NPC} from "../npc/npc";
 import {OfflineBattleRoom} from "./offline-battle-room";
 import {NeoLandozerNpc} from "../npc/neo-landozer-npc";
+import {LightningDozerNPC} from "../npc/lightning-dozer";
 
 /**
  * ダミーの戦闘ルームを生成する
@@ -17,7 +18,7 @@ export function createDummyBattleRoom(): BattleRoom {
     playerId: 'test01',
     armdozer: ArmDozers.find(v => v.id === ArmDozerIdList.SHIN_BRAVER) || ArmDozers[0]
   };
-  const npc: NPC = NeoLandozerNpc;
-  //const npc: NPC = LightningDozerNPC;
+  //const npc: NPC = NeoLandozerNpc;
+  const npc: NPC = LightningDozerNPC;
   return new OfflineBattleRoom(player, npc);
 }
