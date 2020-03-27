9edfc1e90fbf2265555ec46d99384590df881207
竹内佑介 <kaidouji85@gmail.com>
2020-03-27 20:42:31 +0900

    ガードアニメーションを追加

--- src/js/game/td-scenes/battle/animation/state-history/battle/attack/lightning-dozer.js
+++ src/js/game/td-scenes/battle/animation/state-history/battle/attack/lightning-dozer.js
@@ -6,6 +6,7 @@ import {LightningDozer} from "../../../../../../../game-object/armdozer/lightnin
 import {delay, empty} from "../../../../../../../animation/delay";
 import type {BattleResult, CriticalHit, NormalHit} from "gbraver-burst-core";
 import {all} from "../../../../../../../animation/all";
+import type {Guard} from "gbraver-burst-core/lib/effect/battle/result/guard";
 
 /**
  * ライトニングドーザ 戦闘アニメーション パラメータ
@@ -25,6 +26,12 @@ export function lightningDozerAttack(param: LightningDozerBattleParam<BattleResu
     const castResult = (param.result: NormalHit);
     const castParam = ((param: any): BattleAnimationParamX<LightningDozer, AttackResult | typeof castResult>);
     return attack(castParam);
+  }
+
+  if (param.result.name === 'Guard') {
+    const castResult = (param.result: Guard);
+    const castParam = ((param: any): BattleAnimationParamX<LightningDozer, Guard | typeof castResult>);
+    return guard(castParam);
   }
 
   return empty();
@@ -57,5 +64,30 @@ function attack(param: LightningDozerBattleParam<AttackResult>): Animate {
         param.defenderTD.gauge.hp(param.defenderState.armdozer.hp)
       ).chain(delay(2500))
       .chain(param.defenderSprite.knockBackToStand()),
+  );
+}
+
+/**
+ * ガード
+ *
+ * @param param パラメータ
+ * @return アニメーション
+ */
+function guard(param: LightningDozerBattleParam<Guard>): Animate {
+  return all(
+    param.attackerSprite.charge()
+      .chain(delay(800))
+      .chain(param.attackerSprite.armHammer())
+      .chain(delay(2000))
+      .chain(param.attackerSprite.hmToStand()),
+
+    delay(1200)
+      .chain(
+        param.defenderTD.damageIndicator.popUp(param.result.damage),
+        param.defenderSprite.guard(),
+        param.defenderTD.hitMark.shockWave.popUp(),
+        param.defenderTD.gauge.hp(param.defenderState.armdozer.hp)
+      ).chain(delay(2500))
+      .chain(param.defenderSprite.guardToStand()),
   );
 }
