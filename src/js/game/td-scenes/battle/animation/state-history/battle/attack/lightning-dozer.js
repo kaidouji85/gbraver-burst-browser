1ae4bb0387e5ed063fbae0605e4a25f12ff0f3bf
竹内佑介 <kaidouji85@gmail.com>
2020-03-27 20:49:59 +0900

    攻撃ミスのアニメーションを追加した

--- src/js/game/td-scenes/battle/animation/state-history/battle/attack/lightning-dozer.js
+++ src/js/game/td-scenes/battle/animation/state-history/battle/attack/lightning-dozer.js
@@ -7,6 +7,7 @@ import {delay, empty} from "../../../../../../../animation/delay";
 import type {BattleResult, CriticalHit, NormalHit} from "gbraver-burst-core";
 import {all} from "../../../../../../../animation/all";
 import type {Guard} from "gbraver-burst-core/lib/effect/battle/result/guard";
+import type {Miss} from "gbraver-burst-core/lib/effect/battle/result/miss";
 
 /**
  * ライトニングドーザ 戦闘アニメーション パラメータ
@@ -32,6 +33,12 @@ export function lightningDozerAttack(param: LightningDozerBattleParam<BattleResu
     const castResult = (param.result: Guard);
     const castParam = ((param: any): BattleAnimationParamX<LightningDozer, Guard | typeof castResult>);
     return guard(castParam);
+  }
+
+  if (param.result.name === 'Miss') {
+    const castResult = (param.result: Miss);
+    const castParam = ((param: any): BattleAnimationParamX<LightningDozer, Miss | typeof castResult>);
+    return miss(castParam);
   }
 
   return empty();
@@ -89,5 +96,26 @@ function guard(param: LightningDozerBattleParam<Guard>): Animate {
         param.defenderTD.gauge.hp(param.defenderState.armdozer.hp)
       ).chain(delay(2500))
       .chain(param.defenderSprite.guardToStand()),
+  );
+}
+
+/**
+ * ミス
+ *
+ * @param param パラメータ
+ * @return アニメーション
+ */
+function miss(param: LightningDozerBattleParam<Miss>): Animate {
+  return all(
+    param.attackerSprite.charge()
+      .chain(delay(800))
+      .chain(param.attackerSprite.armHammer())
+      .chain(delay(1000))
+      .chain(param.attackerSprite.hmToStand()),
+
+    delay(1200)
+      .chain(param.defenderSprite.avoid())
+      .chain(delay(2000))
+      .chain(param.defenderSprite.avoidToStand()),
   );
 }
