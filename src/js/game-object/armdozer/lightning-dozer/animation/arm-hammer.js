0a4b150cdad503cbbae392ce5ebf7f95326d6c31
竹内佑介 <kaidouji85@gmail.com>
2020-03-28 16:05:55 +0900

    ライトニングドーザの攻撃モーションを調整した

--- src/js/game-object/armdozer/lightning-dozer/animation/arm-hammer.js
+++ src/js/game-object/armdozer/lightning-dozer/animation/arm-hammer.js
@@ -17,7 +17,7 @@ export function armHammer(model: LightningDozerModel): Animate {
     model.animation.type = 'HM_ATTACK';
     model.animation.frame = 0;
   }).chain(all(
-    tween(model.animation, t => t.to({frame: 1}, 200)),
-    tween(model.position, t => t.to({x: '-80'}, 200))
+    tween(model.animation, t => t.to({frame: 1}, 150)),
+    tween(model.position, t => t.to({x: '-80'}, 150))
   ));
 }
--- src/js/game-object/armdozer/lightning-dozer/animation/hm-to-stand.js
+++ src/js/game-object/armdozer/lightning-dozer/animation/hm-to-stand.js
@@ -14,17 +14,15 @@ import {all} from "../../../../animation/all";
  */
 export function hmToStand(model: LightningDozerModel): Animate {
   return all(
-    tween(model.position, t => t.to({x: '+80'}, 400)),
+    tween(model.position, t => t.to({x: '+80'}, 600)),
 
     process(() => {
       model.animation.type = 'HM_TO_STAND';
       model.animation.frame = 0;
-    }).chain(all(
-      tween(model.animation, t => t.to({frame: 1}, 400)),
-
-    )).chain(process(() => {
-      model.animation.type = 'STAND';
-      model.animation.frame = 0;
-    }))
+    }).chain(tween(model.animation, t => t.to({frame: 1}, 600)))
+      .chain(process(() => {
+        model.animation.type = 'STAND';
+        model.animation.frame = 0;
+      }))
   );
 }
