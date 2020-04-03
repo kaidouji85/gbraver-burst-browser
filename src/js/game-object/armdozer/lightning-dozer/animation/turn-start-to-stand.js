848d8d3381ad190084145e1f77c029fe6fa4b3a2
竹内佑介 <kaidouji85@gmail.com>
2020-04-03 14:31:15 +0900

    ライトニングドーザのアニメーション雛形を追加した

---
+++ src/js/game-object/armdozer/lightning-dozer/animation/turn-start-to-stand.js
@@ -0,0 +1,28 @@
+// @flow
+
+import type {LightningDozerModel} from "../model/lightning-dozer-model";
+import {Animate} from "../../../../animation/animate";
+import {process} from "../../../../animation/process";
+import {tween} from "../../../../animation/tween";
+
+/**
+ * ターンスタート -> 立ち
+ *
+ * @param model モデル
+ * @return アニメーション
+ */
+export function turnStartToStand(model: LightningDozerModel): Animate {
+  return process(() => {
+    model.animation.type = 'GUTS_DOWN';
+    model.animation.frame = 1;
+  }).chain(tween(model.animation, t => t.to({frame: 0}, 300)))
+    .chain(process(() => {
+      model.animation.type = 'GUTS_UP';
+      model.animation.frame = 1;
+    }))
+    .chain(tween(model.animation, t => t.to({frame: 0}, 300)))
+    .chain(process(() => {
+      model.animation.type = 'STAND';
+      model.animation.frame = 0;
+    }));
+}
