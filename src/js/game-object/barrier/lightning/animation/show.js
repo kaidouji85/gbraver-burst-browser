66d9dd66f6b95c4e6826bb44d9ffb175086ac447
竹内佑介 <kaidouji85@gmail.com>
2020-04-04 16:56:34 +0900

    ライトニングドーザにバースト発動ポーズを追加した

--- src/js/game-object/barrier/lightning/animation/show.js
+++ src/js/game-object/barrier/lightning/animation/show.js
@@ -1,10 +1,9 @@
 // @flow
 
+import TWEEN from '@tweenjs/tween.js';
 import {Animate} from "../../../../animation/animate";
 import type {LightningBarrierModel} from "../model/lightning-barrier-model";
 import {tween} from "../../../../animation/tween";
-import {process} from '../../../../animation/process';
-import {all} from "../../../../animation/all";
 
 /**
  * バリアを表示する
@@ -13,11 +12,8 @@ import {all} from "../../../../animation/all";
  * @return アニメーション
  */
 export function show(model: LightningBarrierModel): Animate {
-  return all(
-    process(() => {
-      model.scale = 0.9;
-    }).chain(tween(model, t => t.to({scale: 1}, 200))),
-
-    tween(model, t => t.to({opacity: 1}, 500))
+  return tween(model, t =>
+    t.to({opacity: 1}, 500)
+      .easing(TWEEN.Easing.Quadratic.Out)
   );
 }
