ccaf48e7184fdee8320e5d40da27f8343595edfd
竹内佑介 <kaidouji85@gmail.com>
2020-04-04 16:31:12 +0900

    電撃バリア出現時にスケールアップする演出を追加した

--- src/js/game-object/barrier/lightning/animation/show.js
+++ src/js/game-object/barrier/lightning/animation/show.js
@@ -3,6 +3,8 @@
 import {Animate} from "../../../../animation/animate";
 import type {LightningBarrierModel} from "../model/lightning-barrier-model";
 import {tween} from "../../../../animation/tween";
+import {process} from '../../../../animation/process';
+import {all} from "../../../../animation/all";
 
 /**
  * バリアを表示する
@@ -11,5 +13,11 @@ import {tween} from "../../../../animation/tween";
  * @return アニメーション
  */
 export function show(model: LightningBarrierModel): Animate {
-  return tween(model, t => t.to({opacity: 1}, 500));
+  return all(
+    process(() => {
+      model.scale = 0.9;
+    }).chain(tween(model, t => t.to({scale: 1}, 200))),
+
+    tween(model, t => t.to({opacity: 1}, 500))
+  );
 }
