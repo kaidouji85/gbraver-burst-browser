c33c798f7044352eb501bda8464d4c1cdb06b4f8
竹内佑介 <kaidouji85@gmail.com>
2020-04-11 15:13:37 +0900

    HUDレイヤー ネオランドーザ固有オブジェクトの雛形を追加した

---
+++ src/js/game/td-scenes/battle/view/hud/armdozer/neo-landozer.js
@@ -0,0 +1,76 @@
+// @flow
+
+import * as THREE from 'three';
+import type {HUDArmdozer} from "./index";
+import type {PlayerId} from "gbraver-burst-core/lib/player/player";
+import {NeoLandozerCutIn} from "../../../../../../game-object/cut-in/neo-landozer/neo-landozer-cutin";
+import type {Resources} from "../../../../../../resource";
+import {Observable} from "rxjs";
+import type {GameObjectAction} from "../../../../../../action/game-object-action";
+import type {Player} from "gbraver-burst-core";
+
+/**
+ * HUDレイヤー ネオランドーザ固有オブジェクト フィールド
+ */
+interface NeoLandozerHUDField {
+  cutIn: NeoLandozerCutIn;
+}
+
+/**
+ * HUDレイヤー ネオランドーザ固有のオブジェクトをあつめたもの
+ */
+export class NeoLandozerHUD implements HUDArmdozer, NeoLandozerHUDField {
+  playerId: PlayerId;
+  cutIn: NeoLandozerCutIn;
+
+  constructor(playerId: PlayerId, field: NeoLandozerHUDField) {
+    this.playerId = playerId;
+    this.cutIn = field.cutIn;
+  }
+
+  /**
+   * デストラクタ相当の処理
+   */
+  destructor(): void {
+    this.cutIn.destructor();
+  }
+
+  /**
+   * シーンに追加するオブジェクトを取得する
+   *
+   * @return シーンに追加するオブジェクト
+   */
+  getObject3Ds(): THREE.Object3D {
+    return [
+      this.cutIn.getObject3D()
+    ];
+  }
+}
+
+/**
+ * プレイヤー側 HUDレイヤー ネオランドーザ固有オブジェクト
+ *
+ * @param resources リソース管理オブジェクト
+ * @param listener イベントリスナ
+ * @param state プレイヤー情報
+ * @return 生成結果
+ */
+export function playerNeoLandozerHUD(resources: Resources, listener: Observable<GameObjectAction>, state: Player): NeoLandozerHUD {
+  return new NeoLandozerHUD(state.playerId, {
+    cutIn: new NeoLandozerCutIn(resources)
+  });
+}
+
+/**
+ * 敵側 HUDレイヤー ネオランドーザ固有オブジェクト
+ *
+ * @param resources リソース管理オブジェクト
+ * @param listener イベントリスナ
+ * @param state プレイヤー情報
+ * @return 生成結果
+ */
+export function enemyNeoLandozerHUD(resources: Resources, listener: Observable<GameObjectAction>, state: Player): NeoLandozerHUD {
+  return new NeoLandozerHUD(state.playerId, {
+    cutIn: new NeoLandozerCutIn(resources)
+  });
+}
