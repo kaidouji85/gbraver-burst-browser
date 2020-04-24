8c0a89449b76139ddd71373ebb51f5411a7a8730
takeuchi-y <takeuchi.y@opst.co.jp>
2020-04-24 17:24:34 +0900

    リソースコピーのビルド処理を追加した

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,24 @@
+const custom = require('../webpack.config');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
+  webpackFinal: (config) => {
+    return {
+      ...config,
+      module: {
+        ...config.module,
+        rules: custom.module.rules
+      },
+      plugins: config.plugins.concat(
+        new CopyWebpackPlugin([
+          {
+            from: path.resolve(__dirname, "../src/resources"),
+            to: path.resolve(__dirname, '../build/storybook-static/resources')
+          }
+        ])
+      )
+    };
+  },
 };
