fc7c1c153afcb2d2d6ca6c172c4e357fa04bb3e6
竹内佑介 <kaidouji85@gmail.com>
2020-04-24 23:35:44 +0900

    とりあえず動くようにした

--- stories/game-object-stub.js
+++ stories/game-object-stub.js
@@ -1,3 +1,5 @@
+// @flow
+
 import type {Render} from "../src/js/action/game-loop/render";
 import * as THREE from "three";
 import {Observable, Subject, Subscription} from "rxjs";
@@ -21,14 +23,15 @@ import type {TdDOMEvent} from "../src/js/action/td-dom";
 import type {Resources} from "../src/js/resource";
 import {loadAllResource} from "../src/js/resource";
 
+type Object3dCreator  = (resources: Resources, listener: Observable<GameObjectAction>) => THREE.Object3D[];
+
 /**
  * ゲームオブジェクト スタブ
  */
-class GameObjectStub {
+export class GameObjectStub {
   _scene: THREE.Scene;
   _camera: THREE.Camera;
   _threeJsRenderer: THREE.Renderer;
-  _renderer: Renderer;
   _safeAreaInset: SafeAreaInset;
   _domEvent: Observable<TdDOMEvent>;
   _gameLoop: Observable<GameLoop>;
@@ -38,24 +41,25 @@ class GameObjectStub {
   _update: Subject<Update>;
   _preRender: Subject<PreRender>;
   _gameObjectAction: Observable<GameObjectAction>;
-  _resources: ?Resources;
+  _renderer: Renderer;
   _subscription: Subscription[];
 
   constructor() {
+    this._scene = new THREE.Scene();
+    const aspect = getViewPortWidth() / getViewPortHeight();
+    this._camera = new THREE.PerspectiveCamera(75, aspect, 1, 10000);
     this._threeJsRenderer = createRender();
     this._safeAreaInset = createSafeAreaInset();
 
     this._domEvent = createDOMEventStream(this._threeJsRenderer.domElement);
     this._gameLoop = gameLoopStream();
+    this._update = new Subject<Update>();
+    this._preRender = new Subject<PreRender>();
     this._resize = createResizeStream();
-    this._render = new Subject<Render>;
-    this._overlap = toOverlapStream(this._domEvent, this._threeJsRenderer, this._camera);
+    this._render = new Subject();
+    this._overlap = toOverlapStream(this._domEvent, this._threeJsRenderer.domElement, this._camera);
     this._gameObjectAction = gameObjectStream(this._update, this._preRender, this._overlap);
 
-    this._scene = new THREE.Scene();
-
-    const aspect = getViewPortWidth() / getViewPortHeight();
-    this._camera = new THREE.PerspectiveCamera(75, aspect, 1, 10000);
 
     this._renderer = new Renderer({
       resize: this._resize,
@@ -63,40 +67,28 @@ class GameObjectStub {
       threeJsRender: this._threeJsRenderer
     });
 
-    this._resources = null;
-
     this._subscription = [
       this._gameLoop.subscribe(this._onGameLoop.bind(this))
     ];
   }
 
-  /**
-   * 全リソースを読み込む
-   *
-   * @return 結果
-   */
-  async loadResource(): Promise<void> {
+  async start(creator: Object3dCreator): Promise<void> {
     try {
-      this._resources = await loadAllResource('');
-    } catch (e) {
+      console.log('start');
+      const resources = await loadAllResource('/');
+      console.log(resources);
+      console.log('complete load resource');
+      const object3Ds = creator(resources, this._gameObjectAction);
+      object3Ds.forEach(object3D => {
+        this._scene.add(object3D);
+      });
+    } catch(e) {
       throw e;
     }
   }
 
-  /**
-   * オブジェクトを追加する
-   *
-   * @param fn
-   */
-  add(fn: (resources: Resources, listener: Observable<GameObjectAction>) => THREE.Object3D[]): void {
-    if (!this._resources) {
-      return;
-    }
-
-    const object3Ds = fn(this._resources, this._gameObjectAction);
-    object3Ds.forEach(object3D => {
-      this._scene.add(object3D);
-    });
+  domElement(): HTMLElement{
+    return this._threeJsRenderer.domElement;
   }
 
   /**
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,5 +1,11 @@
 // @flow
 
+import {GameObjectStub} from "./game-object-stub";
+import {Observable} from "rxjs";
+import type {GameObjectAction} from "../src/js/action/game-object-action";
+import {PlayerShinBraver} from "../src/js/game-object/armdozer/shin-braver";
+import type {Resources} from "../src/js/resource";
+
 export default {
   title: 'three-js-objects',
 };
@@ -14,6 +20,10 @@ export const Button = () => {
 };
 
 export const ShinBraver = () => {
-  const div = document.createElement('div');
-  return div;
+  const stub = new GameObjectStub();
+  stub.start((resources: Resources, listener: Observable<GameObjectAction>) => {
+    const sprite = PlayerShinBraver(resources, listener);
+    return [sprite.getObject3D()];
+  });
+  return stub.domElement();
 }
