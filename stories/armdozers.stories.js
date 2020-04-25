346e58e19f62fc1b97d2e57f70118b74617126ea
竹内佑介 <kaidouji85@gmail.com>
2020-04-25 15:49:34 +0900

    アームドーザ、コマンドを別ファイルに分けた

--- stories/armdozers.stories.js
+++ stories/armdozers.stories.js
@@ -4,14 +4,12 @@ import {TDGameObjectStub} from "./stub/td-game-object-stub";
 import {PlayerShinBraver} from "../src/js/game-object/armdozer/shin-braver";
 import {PlayerNeoLandozer} from "../src/js/game-object/armdozer/neo-landozer";
 import {PlayerLightningDozer} from "../src/js/game-object/armdozer/lightning-dozer";
-import {HUDGameObjectStub} from "./stub/hud-game-object-stub";
-import {BatterySelector} from "../src/js/game-object/battery-selector";
 
 export default {
-  title: 'three-js-objects',
+  title: 'armdozers',
 };
 
-export const ShinBraver = () => {
+export const shinBraver = () => {
   const stub = new TDGameObjectStub((resources, listener) => {
     const sprite = PlayerShinBraver(resources, listener);
     return [sprite.getObject3D()];
@@ -20,7 +18,7 @@ export const ShinBraver = () => {
   return stub.domElement();
 }
 
-export const NeoLandozer = () => {
+export const neoLandozer = () => {
   const stub = new TDGameObjectStub((resources, listener) => {
     const sprite = PlayerNeoLandozer(resources, listener);
     return [sprite.getObject3D()];
@@ -29,30 +27,10 @@ export const NeoLandozer = () => {
   return stub.domElement();
 }
 
-export const LightningDozer = () => {
+export const lightningDozer = () => {
   const stub = new TDGameObjectStub((resources, listener) => {
     const sprite = PlayerLightningDozer(resources, listener);
     return [sprite.getObject3D()];
-  });
-  stub.start();
-  return stub.domElement();
-}
-
-export const BatterySelectorStub = () => {
-  const stub = new HUDGameObjectStub((resources, listener) => {
-    const selector = new BatterySelector({
-      resources: resources,
-      listener: listener,
-      maxBattery: 5,
-      onBatteryChange: (battery: number) => {
-        // NOP
-      },
-      onOkButtonPush: () => {
-        // NOP
-      },
-    });
-    selector.open(1, 5,'Attack').play();
-    return [selector.getObject3D()];
   });
   stub.start();
   return stub.domElement();
