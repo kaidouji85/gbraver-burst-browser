549c12ad4dcd2813f467927916e945828b8ae469
竹内佑介 <kaidouji85@gmail.com>
2020-05-01 18:03:04 +0900

    プレイヤーセレクトに新モックを適用

--- stories/player-select.stories.js
+++ stories/player-select.stories.js
@@ -1,13 +1,15 @@
 // @flow
 
 import {PlayerSelect} from "../src/js/game/dom-scenes/player-select";
+import {DOMStub} from "./stub/dom-stub";
 
 export default {
   title: 'player-select',
 };
 
 export const Scene = () => {
-  const div = document.createElement('div');
-  const scene = new PlayerSelect(div);
-  return div;
+  const stub = new DOMStub((parent, resourcePath) => {
+    const scene = new PlayerSelect(parent);
+  });
+  return stub.domElement();
 }
