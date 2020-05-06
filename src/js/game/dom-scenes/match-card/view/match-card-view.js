4174441bb9ab067d0ee29745992b322c461974df
竹内佑介 <kaidouji85@gmail.com>
2020-05-06 18:35:52 +0900

    キャプションを追加

--- src/js/game/dom-scenes/match-card/view/match-card-view.js
+++ src/js/game/dom-scenes/match-card/view/match-card-view.js
@@ -13,6 +13,9 @@ export class MatchCardView {
     this._root.className = 'match-card';
     this._root.innerHTML = `
       <div class="match-card__contents">
+        <div class="match-card__contents__caption">
+          STAGE 1
+        </div>
         <div class="match-card__contents__cards">
           <img class="match-card__contents__cards__enemy" src="${resourcePath.get()}/armdozer/neo-landozer/player-select.png">
           <div class="match-card__contents__cards__vs">vs</div>
