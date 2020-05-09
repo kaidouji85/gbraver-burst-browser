f6864c22043f077adb2d6809cada6530c0e2ff8f
竹内佑介 <kaidouji85@gmail.com>
2020-05-09 13:44:49 +0900

    コミット漏れ

--- src/js/game/preload-links/image-ruls.js
+++ src/js/game/preload-links/image-ruls.js
@@ -5,7 +5,7 @@ import type {ResourcePath} from "../../resource/path/resource-path";
 /**
  * プリロードする画像ファイル一覧
  *
- * @param resourcePath
+ * @param resourcePath リソースパス
  * @return プリロードする画像ファイル一覧
  */
 export function imageURLs(resourcePath: ResourcePath): string[] {
