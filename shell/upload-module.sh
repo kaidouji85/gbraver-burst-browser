e8e8dfb1a47111598c6e03004b1e745c198451a2
竹内佑介 <kaidouji85@gmail.com>
2020-05-17 20:35:40 +0900

    ビルド生成物直下のファイルはブラウザキャッシュ無効になるようにした

--- shell/upload-module.sh
+++ shell/upload-module.sh
@@ -2,16 +2,8 @@
 
 S3_BUCKET=$1
 BUILD_PATH="./build/production"
-NO_CACHE_FILES=(\
-  "index.html" \
-  "index.js" \
-  "clear.html"\
-  "clear.js" \
-  "sw.js" \
-  "manifest.json"\
-)
 
-for file_name in ${NO_CACHE_FILES[@]}; do
+for file_name in `ls -F ${BUILD_PATH} | grep -v / `; do
   aws s3 cp --cache-control "no-cache,no-store" "${BUILD_PATH}/${file_name}" "s3://${S3_BUCKET}/${file_name}"
 done
 aws s3 sync --delete --exact-timestamps "${BUILD_PATH}" "s3://${S3_BUCKET}"
