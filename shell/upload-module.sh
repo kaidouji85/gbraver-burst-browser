d2d7b45dfa47154de16692fd580d7b4a3c1f1e9a
竹内佑介 <kaidouji85@gmail.com>
2020-05-17 19:50:30 +0900

    index.js、clear.js、clear.htmlにブラウザキャッシュ無効の指定を追加

--- shell/upload-module.sh
+++ shell/upload-module.sh
@@ -3,7 +3,7 @@
 S3_BUCKET=$1
 BUILD_PATH="./build/production"
 
-for file_name in "index.html" ; do
+for file_name in "index.html" "index.js" "clear.html" "clear.js"; do
   aws s3 cp --cache-control "no-cache,no-store" "${BUILD_PATH}/${file_name}" "s3://${S3_BUCKET}/${file_name}"
 done
 aws s3 sync --delete --exact-timestamps "${BUILD_PATH}" "s3://${S3_BUCKET}"
