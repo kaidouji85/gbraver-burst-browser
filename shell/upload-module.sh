8b6763225abb5d1310e06563d4733e97e30e30e7
竹内佑介 <kaidouji85@gmail.com>
2020-05-17 20:17:40 +0900

    ブラウザキャッシュ無効ファイルを配列形式にした

--- shell/upload-module.sh
+++ shell/upload-module.sh
@@ -2,8 +2,16 @@
 
 S3_BUCKET=$1
 BUILD_PATH="./build/production"
+NO_CACHE_FILES=(\
+  "index.html" \
+  "index.js" \
+  "clear.html"\
+  "clear.js" \
+  "sw.js" \
+  "manifest.json"\
+)
 
-for file_name in "index.html" "index.js" "clear.html" "clear.js" "sw.js" "manifest.json"; do
+for file_name in ${NO_CACHE_FILES[@]}; do
   aws s3 cp --cache-control "no-cache,no-store" "${BUILD_PATH}/${file_name}" "s3://${S3_BUCKET}/${file_name}"
 done
 aws s3 sync --delete --exact-timestamps "${BUILD_PATH}" "s3://${S3_BUCKET}"
