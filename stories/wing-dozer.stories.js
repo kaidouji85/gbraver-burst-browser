e3de22b6053f55fa2a2b48ed680cd4f19941ab78
竹内佑介 <kaidouji85@gmail.com>
2020-05-24 19:13:41 +0900

    ウィングドーザスプライトのstorybookを追加した

---
+++ stories/wing-dozer.stories.js
@@ -0,0 +1,17 @@
+// @flow
+
+import {TDGameObjectStub} from "./stub/td-game-object-stub";
+import {WingDozer} from "../src/js/game-object/armdozer/wing-dozer/wing-dozer";
+
+export default {
+  title: 'wing-dozer',
+};
+
+export const stand = () => {
+  const stub = new TDGameObjectStub((resources, listener) => {
+    const sprite = new WingDozer(resources);
+    return [sprite.getObject3D()];
+  });
+  stub.start();
+  return stub.domElement();
+}
