2469eda5bfb9a6e4df8a0b9355b98692d45d42ed
竹内佑介 <kaidouji85@gmail.com>
2020-05-29 11:36:06 +0900

    リソースロード専用のクラスを作った

--- src/js/resource/index.js
+++ src/js/resource/index.js
@@ -1,17 +1,16 @@
 // @flow
 import type {TextureResource} from "./texture";
-import {loadAllTexture} from "./texture";
+import {loadAllTexture, loadingAllTextures} from "./texture";
 import type {CanvasImageResource} from "./canvas-image";
-import {loadAllCanvasImage} from "./canvas-image";
+import {loadAllCanvasImage, loadingAllCanvasImages} from "./canvas-image";
 import type {GlTFResource} from "./gltf";
-import {loadAllGlTFModel} from "./gltf";
+import {loadAllGlTFModel, loadingAllGTLFModels} from "./gltf";
 import type {CubeTextureResource} from "./cube-texture";
-import {loadAllCubeTexture} from "./cube-texture";
+import {loadAllCubeTexture, loadingAllCubeTextures} from "./cube-texture";
 import type {SoundResource} from "./sound";
-import {loadAllSounds} from "./sound";
+import {loadAllSounds, loadingAllSounds} from "./sound";
 import type {ResourcePath} from "./path/resource-path";
-import {Observable} from "rxjs";
-import {Loading} from "../game/dom-scenes/loading";
+import {Observable, Subject} from "rxjs";
 import type {LoadingAction} from "../action/loading/loading";
 
 /**
@@ -59,16 +58,88 @@ export async function loadAllResource(resourcePath: ResourcePath): Promise<Resou
   };
 }
 
-// export class ResourceLoader {
-//   constructor(resourcePath: ResourcePath) {
-//
-//   }
-//
-//   load(): Promise<Resources> {
-//
-//   }
-//
-//   progress(): Observable<LoadingAction> {
-//
-//   }
-// }
+/**
+ * リソース読み込み
+ */
+export class ResourceLoader {
+  _resourcePath: ResourcePath;
+  _gltfLoading: Array<Promise<GlTFResource>>;
+  _textureLoading: Array<Promise<TextureResource>>;
+  _cubeTextureLoading: Array<Promise<CubeTextureResource>>;
+  _canvasImageLoading: Array<Promise<CanvasImageResource>>;
+  _soundLoading: Array<Promise<SoundResource>>;
+  _allLoadingCounts: number;
+  _completedLoadingCounts: number;
+  _loading: Subject<LoadingAction>;
+
+  /**
+   * コンストラクタ
+   *
+   * @param resourcePath リソースパス
+   */
+  constructor(resourcePath: ResourcePath) {
+    this._resourcePath = resourcePath;
+    this._gltfLoading = loadingAllGTLFModels(resourcePath);
+    this._textureLoading = loadingAllTextures(resourcePath);
+    this._cubeTextureLoading = loadingAllCubeTextures(resourcePath);
+    this._canvasImageLoading = loadingAllCanvasImages(resourcePath);
+    this._soundLoading = loadingAllSounds(resourcePath);
+
+    const allLoading = [].concat(
+      this._gltfLoading,
+      this._textureLoading,
+      this._cubeTextureLoading,
+      this._canvasImageLoading,
+      this._soundLoading,
+    );
+    this._allLoadingCounts = allLoading.length;
+    this._completedLoadingCounts = 0;
+    this._loading = new Subject();
+    allLoading.forEach(loading => {
+      loading.then(() => {
+        this._completedLoadingCounts ++;
+        this._loading.next({
+          type: 'LoadingProgress',
+          completedRate: this._completedLoadingCounts / this._allLoadingCounts
+        });
+      });
+    });
+  }
+
+  /**
+   * リソースを読み込む
+   *
+   * @return リソース管理オブジェクト
+   */
+  async load(): Promise<Resources> {
+    try {
+      const [gltfs, textures, cubeTextures, canvasImages, sounds] = await Promise.all([
+        Promise.all(this._gltfLoading),
+        Promise.all(this._textureLoading),
+        Promise.all(this._cubeTextureLoading),
+        Promise.all(this._canvasImageLoading),
+        Promise.all(this._soundLoading),
+      ]);
+
+      return {
+        path: this._resourcePath,
+        gltfs: gltfs,
+        textures: textures,
+        cubeTextures: cubeTextures,
+        canvasImages: canvasImages,
+        sounds: sounds,
+      };
+    } catch(e) {
+      throw e;
+    }
+  }
+
+  /**
+   * 読み込み進捗率ストリームを取得する
+   *
+   * @return 読み込み進捗率ストリーム
+   */
+  progress(): Observable<LoadingAction> {
+    return this._loading;
+  }
+}
