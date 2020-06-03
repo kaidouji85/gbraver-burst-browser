0b76c86aaab36802f78afbb8ef4e9795ed897652
竹内佑介 <kaidouji85@gmail.com>
2020-06-03 12:42:09 +0900

    ボタン表示アニメをシンプルにした

--- src/js/game-object/burst-button/animation/open.js
+++ src/js/game-object/burst-button/animation/open.js
@@ -5,16 +5,20 @@ import {Animate} from "../../../animation/animate";
 import {tween} from "../../../animation/tween";
 import {process} from '../../../animation/process';
 
-/** バーストボタンを表示する */
+/**
+ * バーストボタンを表示する
+ *
+ * @param model モデル
+ * @return アニメーション
+ */
 export function open(model: BurstButtonModel, canBurst: boolean): Animate {
   return process(() => {
     model.disabled = true;
     model.canBurst = canBurst;
     model.opacity = 0;
-    model.scale = 0.9;
-  }).chain(tween(model, t => t
-    .to({opacity: 1, scale: 1}, 200)
-  )).chain(process(() => {
-    model.disabled = false;
-  }));
+  })
+    .chain(tween(model, t => t.to({opacity: 1}, 200)))
+    .chain(process(() => {
+      model.disabled = false;
+    }));
 }
