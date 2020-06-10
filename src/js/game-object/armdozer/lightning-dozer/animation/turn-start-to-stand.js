e466ac44ef5db63ede7979f968defe8f022d38e4
竹内佑介 <kaidouji85@gmail.com>
2020-06-10 18:10:04 +0900

    ライトニングドーザ に駆動音を一式追加した

--- src/js/game-object/armdozer/lightning-dozer/animation/turn-start-to-stand.js
+++ src/js/game-object/armdozer/lightning-dozer/animation/turn-start-to-stand.js
@@ -4,19 +4,23 @@ import type {LightningDozerModel} from "../model/lightning-dozer-model";
 import {Animate} from "../../../../animation/animate";
 import {process} from "../../../../animation/process";
 import {tween} from "../../../../animation/tween";
+import {LightningDozerSounds} from "../sounds/lightning-dozer-sounds";
 
 /**
  * ターンスタート -> 立ち
  *
  * @param model モデル
+ * @param sounds 音
  * @return アニメーション
  */
-export function turnStartToStand(model: LightningDozerModel): Animate {
+export function turnStartToStand(model: LightningDozerModel, sounds: LightningDozerSounds): Animate {
   return process(() => {
     model.animation.type = 'GUTS_TO_STAND';
     model.animation.frame = 0;
-  }).chain(tween(model.animation, t => t.to({frame: 1}, 400))
-  ).chain(process(() => {
+    sounds.motor.play();
+  })
+    .chain(tween(model.animation, t => t.to({frame: 1}, 400)))
+    .chain(process(() => {
       model.animation.type = 'STAND';
       model.animation.frame = 0;
   }));
