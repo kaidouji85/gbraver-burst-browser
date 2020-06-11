b726ea1550c45c126b736a29766e375812a024c7
竹内佑介 <kaidouji85@gmail.com>
2020-06-11 20:31:14 +0900

    ライトニングドーザのバックステップモーションを調整した

--- src/js/game-object/armdozer/neo-landozer/animation/avoid-to-stand.js
+++ src/js/game-object/armdozer/neo-landozer/animation/avoid-to-stand.js
@@ -5,7 +5,6 @@ import type {NeoLandozerModel} from "../model/neo-landozer-model";
 import {process} from "../../../../animation/process";
 import {tween} from "../../../../animation/tween";
 import {NeoLandozerSounds} from "../sounds/neo-landozer-sounds";
-import {all} from "../../../../animation/all";
 
 /** 避け -> 立ち */
 export function avoidToStand(model: NeoLandozerModel, sounds: NeoLandozerSounds): Animate {
@@ -14,9 +13,7 @@ export function avoidToStand(model: NeoLandozerModel, sounds: NeoLandozerSounds)
     model.animation.frame = 1;
     sounds.motor.play();
   })
-    .chain(all(
-      tween(model.animation, t => t.to({frame: 0}, 300))
-    ))
+    .chain(tween(model.animation, t => t.to({frame: 0}, 300)))
     .chain(process(() => {
       model.animation.type = 'STAND';
       model.animation.frame = 0;
