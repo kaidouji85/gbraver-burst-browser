388b9edc030c9d12e9ece16a2bafa3bc1e9bf797
竹内佑介 <kaidouji85@gmail.com>
2020-06-11 15:30:58 +0900

    ネオランドーザバックステップを追加した

---
+++ src/js/game-object/armdozer/neo-landozer/mesh/back-step.js
@@ -0,0 +1,30 @@
+// @flow
+
+import type {ArmdozerAnimation} from "../../mesh/armdozer-animation";
+import type {Resources} from "../../../../resource";
+import {TEXTURE_IDS} from "../../../../resource/texture";
+import {HorizontalArmdozerAnimation} from "../../mesh/horizontal-animation";
+import {MESH_Y} from "./position";
+
+export const MAX_ANIMATION = 4;
+export const MESH_WIDTH = 600;
+export const MESH_HEIGHT = 600;
+
+/**
+ * ネオランドーザ バックステップ
+ * 
+ * @param resources リソース管理オブジェクト
+ * @return メッシュ
+ */
+export function neoLandozerBackStep(resources: Resources): ArmdozerAnimation {
+  const ret = new HorizontalArmdozerAnimation({
+    id: TEXTURE_IDS.NEO_LANDOZER_BACK_STEP,
+    maxAnimation: MAX_ANIMATION,
+    resources: resources,
+    width: MESH_WIDTH,
+    height: MESH_HEIGHT
+  });
+  const object = ret.getObject3D();
+  object.position.y = MESH_Y;
+  return ret;
+}
