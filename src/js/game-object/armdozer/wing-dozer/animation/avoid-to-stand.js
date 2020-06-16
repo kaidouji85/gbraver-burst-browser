e0d5ccf05e6e97d0853a0eca2e4c1f0f05a0f7c3
竹内佑介 <kaidouji85@gmail.com>
2020-06-16 15:38:57 +0900

    ウィングドーザのフロントステップを追加した

--- src/js/game-object/armdozer/wing-dozer/animation/avoid-to-stand.js
+++ src/js/game-object/armdozer/wing-dozer/animation/avoid-to-stand.js
@@ -6,6 +6,7 @@ import {tween} from "../../../../animation/tween";
 import type {WingDozerModel} from "../model/wing-dozer-model";
 import {WingDozerSounds} from "../sounds/wing-dozer-sounds";
 import {all} from "../../../../animation/all";
+import {delay} from "../../../../animation/delay";
 
 /**
  * 避け -> 立ち
@@ -16,14 +17,19 @@ import {all} from "../../../../animation/all";
  */
 export function avoidToStand(model: WingDozerModel, sounds: WingDozerSounds): Animate {
   return process(() => {
-    model.animation.type = 'BACK_STEP';
-    model.animation.frame = 1;
+    model.animation.type = 'FRONT_STEP';
+    model.animation.frame = 0;
     sounds.motor.play();
   })
     .chain(all(
-      tween(model.animation, t => t.to({frame: 0}, 400)),
-      tween(model.position, t => t.to({x: '-100'}, 400))
+      tween(model.animation, t => t.to({frame: 1}, 300)),
+      tween(model.position, t => t.to({x: '-100'}, 300))
     ))
+    .chain(delay(300))
+    .chain(process(() => {
+      sounds.motor.play();
+    }))
+    .chain(tween(model.animation, t => t.to({frame: 0}, 300)))
     .chain(process(() => {
       model.animation.type = 'STAND';
       model.animation.frame = 0;
--- src/js/game-object/armdozer/wing-dozer/animation/avoid.js
+++ src/js/game-object/armdozer/wing-dozer/animation/avoid.js
@@ -6,6 +6,7 @@ import {tween} from "../../../../animation/tween";
 import type {WingDozerModel} from "../model/wing-dozer-model";
 import {WingDozerSounds} from "../sounds/wing-dozer-sounds";
 import {all} from "../../../../animation/all";
+import {delay} from "../../../../animation/delay";
 
 /**
  * 避け
@@ -21,7 +22,16 @@ export function avoid(model: WingDozerModel, sounds: WingDozerSounds): Animate {
     sounds.motor.play();
   })
     .chain(all(
-      tween(model.animation, t => t.to({frame: 1}, 300)),
-      tween(model.position, t => t.to({x: '+100'}, 300))
-    ));
+      tween(model.animation, t => t.to({frame: 1}, 200)),
+      tween(model.position, t => t.to({x: '+100'}, 200))
+    ))
+    .chain(delay(300))
+    .chain(process(() => {
+      sounds.motor.play();
+    }))
+    .chain(tween(model.animation, t => t.to({frame: 0}, 300)))
+    .chain(process(() => {
+      model.animation.type = 'STAND';
+      model.animation.frame = 0;
+    }));
 }
