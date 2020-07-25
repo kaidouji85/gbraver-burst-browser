8fd1886e76c941bcd2bb5fe0d0ddfc7f0f546f33
竹内佑介 <kaidouji85@gmail.com>
2020-07-25 15:46:06 +0900

    パイロットボタンにスケール、ポジションを実装した

--- src/js/game-object/pilot-button/view/pilot-button-view.js
+++ src/js/game-object/pilot-button/view/pilot-button-view.js
@@ -6,7 +6,7 @@ import type {Resources} from "../../../resource";
 import {CANVAS_IMAGE_IDS} from "../../../resource/canvas-image";
 import type {PilotButtonModel} from "../model/pilot-button-model";
 import type {PreRender} from "../../../action/game-loop/pre-render";
-
+import {HUDUIScale} from "../../../hud-scale/hud-scale";
 
 /** キャンバスサイズ */
 const CANVAS_SIZE = 512;
@@ -24,6 +24,7 @@ const PADDING_BOTTOM = 80;
  * パイロットボタン ビュー
  */
 export class PilotButtonView {
+  _group: THREE.Group;
   _button: SimpleImageMesh;
 
   /**
@@ -32,6 +33,8 @@ export class PilotButtonView {
    * @param resources リソース管理オブジェクト
    */
   constructor(resources: Resources) {
+    this._group = new THREE.Group();
+
     const pilotButtonResource = resources.canvasImages.find(v => v.id === CANVAS_IMAGE_IDS.PILOT_BUTTON);
     const pilotButton: Image = pilotButtonResource
       ? pilotButtonResource.image
@@ -41,6 +44,7 @@ export class PilotButtonView {
       meshSize: CANVAS_SIZE,
       image: pilotButton,
     });
+    this._group.add(this._button.getObject3D());
   }
 
   /**
@@ -57,7 +61,21 @@ export class PilotButtonView {
    * @param preRender プリレンダー情報
    */
   engage(model: PilotButtonModel, preRender: PreRender): void {
-
+    const devicePerScale = HUDUIScale(preRender.rendererDOM, preRender.safeAreaInset);
+    this._group.scale.set(
+      GROUP_SCALE * devicePerScale,
+      GROUP_SCALE * devicePerScale,
+      GROUP_SCALE * devicePerScale
+    );
+    this._group.position.x =
+      -preRender.rendererDOM.clientWidth / 2
+      +preRender.safeAreaInset.left
+      +PADDING_LEFT * devicePerScale;
+    this._group.position.y =
+      -preRender.rendererDOM.clientHeight / 2
+      +preRender.safeAreaInset.bottom
+      +PADDING_BOTTOM * devicePerScale;
+    this._group.quaternion.copy(preRender.camera.quaternion);
   }
 
   /**
@@ -66,6 +84,6 @@ export class PilotButtonView {
    * @return シーンに追加するオブジェクト
    */
   getObject3D(): THREE.Object3D {
-    return this._button.getObject3D();
+    return this._group;
   }
 }
