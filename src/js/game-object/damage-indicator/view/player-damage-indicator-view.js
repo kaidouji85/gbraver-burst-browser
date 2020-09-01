fb1f11f2c06f20f2a3cd8784758a232a2ebccae5
竹内佑介 <kaidouji85@gmail.com>
2020-09-01 22:42:53 +0900

    ダメージ表示アニメーションを改良

--- src/js/game-object/damage-indicator/view/player-damage-indicator-view.js
+++ src/js/game-object/damage-indicator/view/player-damage-indicator-view.js
@@ -70,6 +70,8 @@ export class PlayerDamageIndicatorView implements DamageIndicatorView {
     this._group.position.x = ARMDOZER_EFFECT_STANDARD_X;
     this._group.position.y = ARMDOZER_EFFECT_STANDARD_Y;
     this._group.position.z = ARMDOZER_EFFECT_STANDARD_Z;
+
+    this._group.scale.set(model.scale, model.scale, model.scale);
   }
 
   /** カメラの方向を向く */
---
+++ stories/damage.stories.js
@@ -0,0 +1,44 @@
+// @flow
+
+import {enemyContinuousAttack, playerContinuousAttack} from "../src/js/game-object/pop-up/continuous-attack";
+import {delay} from "../src/js/animation/delay";
+import {TDGameObjectStub} from "./stub/td-game-object-stub";
+import {enemyDamageIndicator, playerDamageIndicator} from "../src/js/game-object/damage-indicator";
+
+export default {
+  title: 'damage-indicator',
+};
+
+export const player = () => {
+  const stub = new TDGameObjectStub((resources, listener) => {
+    const continuousAttack = playerDamageIndicator({
+      resources: resources,
+      listener: listener
+    });
+    delay(1000)
+      .chain(continuousAttack.popUp(1000))
+      .loop();
+    return [
+      continuousAttack.getObject3D()
+    ];
+  });
+  stub.start();
+  return stub.domElement();
+}
+
+export const enemy = () => {
+  const stub = new TDGameObjectStub((resources, listener) => {
+    const continuousAttack = enemyDamageIndicator({
+      resources: resources,
+      listener: listener
+    });
+    delay(1000)
+      .chain(continuousAttack.popUp(1000))
+      .loop();
+    return [
+      continuousAttack.getObject3D()
+    ];
+  });
+  stub.start();
+  return stub.domElement();
+}
