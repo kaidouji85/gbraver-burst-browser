684c3ff71876a0966c4baa449ba23d12bdd5f6de
竹内佑介 <kaidouji85@gmail.com>
2020-09-16 13:06:17 +0900

    ネオランドーザのターンスタートアニメーションを追加した

--- src/js/game/td-scenes/battle/animation/turn-start/index.js
+++ src/js/game/td-scenes/battle/animation/turn-start/index.js
@@ -5,6 +5,8 @@ import type {ArmDozerSprite} from "../../../../../game-object/armdozer/armdozer-
 import {TurnStart} from "../../../../../game-object/turn-start/turn-start";
 import {ShinBraver} from "../../../../../game-object/armdozer/shin-braver/shin-braver";
 import {shinBraverTurnStart} from "./shin-braver";
+import {neoLandozerTurnStart} from "./neo-landozer";
+import {NeoLandozer} from "../../../../../game-object/armdozer/neo-landozer/neo-landozer";
 
 /**
  * ターンスタートアニメーション
@@ -16,6 +18,10 @@ import {shinBraverTurnStart} from "./shin-braver";
 export function turnStartAnimation(sprite: ArmDozerSprite, turnStart: TurnStart): Animate {
   if (sprite instanceof ShinBraver) {
     return shinBraverTurnStart(sprite, turnStart);
+  }
+
+  if (sprite instanceof NeoLandozer) {
+    return neoLandozerTurnStart(sprite, turnStart);
   }
 
   return defaultTurnStartAnimation(sprite, turnStart);
