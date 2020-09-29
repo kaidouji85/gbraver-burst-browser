523ce4c48dde18a984a4d8458c64cc0d4122edc0
竹内佑介 <kaidouji85@gmail.com>
2020-09-29 20:52:56 +0900

    ガイカットインの大きさ調整

--- src/js/game-object/cut-in/gai/view/player-gai-view.js
+++ src/js/game-object/cut-in/gai/view/player-gai-view.js
@@ -13,7 +13,7 @@ import {HUD_CUT_IN_ZNIDEX} from "../../../../zindex/hud-zindex";
 /**
  * メッシュの大きさ
  */
-export const MESH_SIZE = 730;
+export const MESH_SIZE = 720;
 
 /**
  * アニメーション数
@@ -60,7 +60,7 @@ export class PlayerGaiView implements GaiView {
     this._mesh.getObject3D().scale.set(scale, scale, scale);
     this._mesh.getObject3D().position.set(
       model.tracking.x,
-      model.tracking.y - 40 * scale,
+      model.tracking.y,
       HUD_CUT_IN_ZNIDEX
     );
     this._mesh.setOpacity(model.opacity);
