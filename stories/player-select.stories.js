167130a57c444a8aa919abb3707ccf2ae9fba890
竹内佑介 <kaidouji85@gmail.com>
2020-10-09 08:19:23 +0900

    パイロットアイコンを追加した

--- stories/player-select.stories.js
+++ stories/player-select.stories.js
@@ -3,6 +3,8 @@
 import {PlayerSelect} from "../src/js/game/dom-scenes/player-select";
 import type {DOMStubStory} from "./stub/dom-stub";
 import {domStub} from "./stub/dom-stub";
+import {PilotIds} from "gbraver-burst-core/lib/master/pilots";
+import {PilotIcon} from "../src/js/game/dom-scenes/player-select/pilot-icon";
 
 export default {
   title: 'player-select',
@@ -11,4 +13,12 @@ export default {
 export const Scene: DOMStubStory = domStub(resources => {
   const scene = new PlayerSelect(resources);
   return scene.getRootHTMLElement();
+});
+
+export const Pilot: DOMStubStory = domStub(resources => {
+  const icon = new PilotIcon(resources, PilotIds.SHINYA);
+  icon.selectedNotifier().subscribe(() => {
+    icon.selected();
+  });
+  return icon.getRootHTMLElement();
 });
