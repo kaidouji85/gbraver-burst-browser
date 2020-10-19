cfe7f5154be4934008ba45155ec49ab395bee3c4
竹内佑介 <kaidouji85@gmail.com>
2020-10-19 20:06:00 +0900

    html要素を置き換えるヘルパー関数を追加した

---
+++ src/js/dom/replace-dom.js
@@ -0,0 +1,17 @@
+// @flow
+
+/**
+ * 指定したHTML要素を置換する
+ * 置換基に親要素がない場合は何もしない
+ * 
+ * @param oldElement 古いHTML要素
+ * @param newElement 新しいHTML要素
+ */
+export function replaceDOM(oldElement: HTMLElement, newElement: HTMLElement): void {
+  const parent = oldElement.parentElement;
+  if (!parent) {
+    return;
+  }
+
+  parent.replaceChild(oldElement, newElement);
+}
