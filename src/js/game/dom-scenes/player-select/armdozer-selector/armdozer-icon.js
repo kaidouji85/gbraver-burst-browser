1bd92ab4c8444954a9b6bdccd7c390fa00da9250
竹内佑介 <kaidouji85@gmail.com>
2020-11-08 14:47:39 +0900

    アームドーザアイコンにチェックマークを追加した

--- src/js/game/dom-scenes/player-select/armdozer-selector/armdozer-icon.js
+++ src/js/game/dom-scenes/player-select/armdozer-selector/armdozer-icon.js
@@ -6,17 +6,19 @@ import type {PushDOM} from "../../../../action/push/push-dom";
 import {pushDOMStream} from "../../../../action/push/push-dom";
 import {waitElementLoaded} from "../../../../wait/wait-element-loaded";
 import {pop} from "../../../../dom/animation/pop";
+import {PathIds} from "../../../../resource/path";
 
-/**
- * トール要素のCSSクラス名
- */
 const ROOT_CLASS_NAME = 'player-select__armdozer-icon';
+const IMAGE_CLASS_NAME = `${ROOT_CLASS_NAME}__image`;
+const CHECK_CLASS_NAME = `${ROOT_CLASS_NAME}__check`;
 
 /**
  * アームドーザアイコン ビュー
  */
 export class ArmdozerIcon {
+  _root: HTMLElement;
   _image: HTMLImageElement;
+  _check: HTMLImageElement;
   _isImageLoaded: Promise<void>;
   _select: Observable<PushDOM>;
 
@@ -28,13 +30,22 @@ export class ArmdozerIcon {
    * @param alt 代替テキスト
    */
   constructor(resources: Resources, imagePath: string, alt: string) {
+    this._root = document.createElement('div');
+    this._root.className = ROOT_CLASS_NAME;
+    this._select = pushDOMStream(this._root);
+
     this._image = document.createElement('img');
-    this._image.className = ROOT_CLASS_NAME;
-    
-    this._select = pushDOMStream(this._image)
+    this._image.className = IMAGE_CLASS_NAME;
     this._isImageLoaded = waitElementLoaded(this._image);
     this._image.src = imagePath;
     this._image.alt = alt;
+    this._root.appendChild(this._image);
+
+    this._check = document.createElement('img');
+    this._check.className = CHECK_CLASS_NAME;
+    this._check.src = resources.paths.find(v => v.id === PathIds.CHECK)
+      ?.path ?? '';
+    this._root.appendChild(this._check);
   }
 
   /**
@@ -52,7 +63,7 @@ export class ArmdozerIcon {
    * @return 取得結果
    */
   getRootHTMLElement(): HTMLElement {
-    return this._image;
+    return this._root;
   }
 
   /**
@@ -70,7 +81,7 @@ export class ArmdozerIcon {
    * @return アニメーション
    */
   async pop(): Promise<void> {
-    await pop(this._image);
+    await pop(this._root);
   }
 
   /**
@@ -80,7 +91,7 @@ export class ArmdozerIcon {
    */
   selected(isSelected: boolean): void {
     this._image.className = isSelected
-      ? `${ROOT_CLASS_NAME}--selected`
-      : ROOT_CLASS_NAME;
+      ? `${IMAGE_CLASS_NAME}--selected`
+      : IMAGE_CLASS_NAME;
   }
 }
