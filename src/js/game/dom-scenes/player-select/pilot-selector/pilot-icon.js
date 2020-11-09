18c5b71a3c25f038512d538ba127dc8aed6534c6
竹内佑介 <kaidouji85@gmail.com>
2020-11-09 09:10:16 +0900

    アームドーザアイコンにルート要素を追加した

--- src/js/game/dom-scenes/player-select/pilot-selector/pilot-icon.js
+++ src/js/game/dom-scenes/player-select/pilot-selector/pilot-icon.js
@@ -8,11 +8,13 @@ import {waitElementLoaded} from "../../../../wait/wait-element-loaded";
 import {pop} from "../../../../dom/animation/pop";
 
 const ROOT_CLASS_NAME = 'player-select__pilot-icon';
+const IMAGE_CLASS_NAME = `${ROOT_CLASS_NAME}__image`;
 
 /**
  * パイロットアイコン
  */
 export class PilotIcon {
+  _root: HTMLElement;
   _image: HTMLImageElement;
   _isImageLoaded: Promise<void>;
   _select: Observable<PushDOM>;
@@ -25,12 +27,17 @@ export class PilotIcon {
    * @param alt 代替テキスト
    */
   constructor(resources: Resources, path: string, alt: string) {
+    this._root = document.createElement('div');
+    this._root.className = ROOT_CLASS_NAME;
+
     this._image = document.createElement('img');
-    this._image.className = ROOT_CLASS_NAME;
-    this._select = pushDOMStream(this._image)
+    this._image.className = IMAGE_CLASS_NAME;
     this._isImageLoaded = waitElementLoaded(this._image);
     this._image.src = path;
     this._image.alt = alt;
+    this._root.appendChild(this._image);
+
+    this._select = pushDOMStream(this._image)
   }
 
   /**
@@ -48,7 +55,7 @@ export class PilotIcon {
    * @return 取得結果
    */
   getRootHTMLElement(): HTMLElement {
-    return this._image;
+    return this._root;
   }
 
   /**
