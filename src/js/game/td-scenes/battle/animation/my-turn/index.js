9e1bc70c045bfa185fbff0490dc92d2279f1f01d
竹内佑介 <kaidouji85@gmail.com>
2020-12-26 15:02:50 +0900

    シンブレイバーマイターンをアニメーションを追加した

--- src/js/game/td-scenes/battle/animation/my-turn/index.js
+++ src/js/game/td-scenes/battle/animation/my-turn/index.js
@@ -4,6 +4,7 @@ import {Animate} from "../../../../../animation/animate";
 import type {MyTurnAnimationParam} from "./animation-param";
 import {attentionArmDozer, toInitial} from "../td-camera";
 import {delay} from "../../../../../animation/delay";
+import {castShinBraverMyTUrn, shinBraverMyTurn} from "./shin-braver";
 
 /**
  * マイターン アニメーション
@@ -13,8 +14,14 @@ import {delay} from "../../../../../animation/delay";
  * @return アニメーション
  */
 export function myTurnAnimation(param: MyTurnAnimationParam, effects: Animate): Animate {
+  const shinBraver = castShinBraverMyTUrn(param);
+  if (shinBraver) {
+    return shinBraverMyTurn(shinBraver, effects);
+  }
+
   return attentionArmDozer(param.tdCamera, param.tdArmdozer.sprite(), 500)
     .chain(delay(500))
+    .chain(effects)
     .chain(toInitial(param.tdCamera, 500))
     .chain(delay(500));
 }
