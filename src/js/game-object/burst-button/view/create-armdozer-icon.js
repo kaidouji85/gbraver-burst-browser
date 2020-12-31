c543414297a80aec6cc3fd5c1428e884e288e913
竹内佑介 <kaidouji85@gmail.com>
2020-12-31 17:30:58 +0900

    ウィングドーザアイコンを追加した

--- src/js/game-object/burst-button/view/create-armdozer-icon.js
+++ src/js/game-object/burst-button/view/create-armdozer-icon.js
@@ -4,9 +4,10 @@ import type {ArmdozerIcon} from "./armdozer-icon";
 import type {ArmDozerId} from "gbraver-burst-core/lib/player/armdozer";
 import {ArmDozerIdList} from "gbraver-burst-core/lib/master/armdozers";
 import type {Resources} from "../../../resource";
-import {ShinBraverIcon} from "./shin-braver";
-import {NeoLandozerIcon} from "./neo-landozer";
-import {LightningDozerIcon} from "./lightning-dozer";
+import {ShinBraverIcon} from "./shin-braver-icon";
+import {NeoLandozerIcon} from "./neo-landozer-icon";
+import {LightningDozerIcon} from "./lightning-dozer-icon";
+import {WingDozerIcon} from "./wing-dozer-icon";
 
 /**
  * アームドーザIDに対応したアイコンを生成する
@@ -23,6 +24,8 @@ export function createArmdozerIcon(armDozerId: ArmDozerId, resources: Resources)
       return new NeoLandozerIcon(resources);
     case ArmDozerIdList.LIGHTNING_DOZER:
       return new LightningDozerIcon(resources);
+    case ArmDozerIdList.WING_DOZER:
+      return new WingDozerIcon(resources);
     default:
       return new ShinBraverIcon(resources);
   }
---
+++ src/js/game-object/burst-button/view/wing-dozer-icon.js
@@ -0,0 +1,62 @@
+// @flow
+
+import * as THREE from 'three';
+import type {ArmdozerIcon} from "./armdozer-icon";
+import type {Resources} from "../../../resource";
+import {TEXTURE_IDS} from "../../../resource/texture";
+import {HorizontalAnimationMesh} from "../../../mesh/horizontal-animation";
+
+/**
+ * ウィングドーザアイコン
+ */
+export class WingDozerIcon implements ArmdozerIcon {
+  _mesh: HorizontalAnimationMesh;
+  _group: typeof THREE.Group;
+
+  /**
+   * コンストラクタ
+   *
+   * @param resources リソース管理オブジェクト
+   */
+  constructor(resources: Resources) {
+    this._group = new THREE.Group();
+
+    const neoLandozer = resources.textures.find(v => v.id === TEXTURE_IDS.WING_DOZER_BURST_DOWN)
+      ?.texture ?? new THREE.Texture();
+    this._mesh = new HorizontalAnimationMesh({
+      texture: neoLandozer,
+      maxAnimation: 4,
+      width: 600,
+      height: 600,
+    });
+    this._mesh.animate(1);
+    this._mesh.getObject3D().position.x = 60;
+    this._mesh.getObject3D().position.y = 80;
+    this._group.add(this._mesh.getObject3D());
+  }
+
+  /**
+   * デストラクタ相当の処理
+   */
+  destructor(): void {
+    this._mesh.destructor();
+  }
+
+  /**
+   * シーンに追加するオブジェクトを取得する
+   *
+   * @return シーンに追加するオブジェクト
+   */
+  getObject3D(): typeof THREE.Object3D {
+    return this._group;
+  }
+
+  /**
+   * 透明度を設定する
+   *
+   * @param opacity 透明度
+   */
+  setOpacity(opacity: number): void {
+    this._mesh.setOpacity(opacity);
+  }
+}
