30d5ce954773429e8ecac57ac72a52b114fc7a83
竹内佑介 <kaidouji85@gmail.com>
2021-01-02 16:52:29 +0900

    ウィングドーザのターンスタート用ダッシュを削除した

--- src/js/game-object/armdozer/wing-dozer/animation/dash.js
+++ src/js/game-object/armdozer/wing-dozer/animation/dash.js
@@ -6,44 +6,6 @@ import {tween} from "../../../../animation/tween";
 import {process} from '../../../../animation/process';
 import {delay} from "../../../../animation/delay";
 import {WingDozerSounds} from "../sounds/wing-dozer-sounds";
-import {all} from "../../../../animation/all";
-
-/**
- * ダッシュ前半
- *
- * @param model モデル
- * @return アニメーション
- */
-function dashUp(model: WingDozerModel): Animate {
-  return process(() => {
-    model.animation.type = 'DASH_UP';
-    model.animation.frame = 0;
-  })
-    .chain(tween(model.animation, t => t.to({frame: 1}, 300)))
-}
-
-/**
- * 待ち
- *
- * @return アニメーション
- */
-function wait(): Animate {
-  return delay(500);
-}
-
-/**
- * ダッシュ後半
- *
- * @param model モデル
- * @return アニメーション
- */
-function dashDown(model: WingDozerModel): Animate {
-  return process(() => {
-    model.animation.type = 'DASH_DOWN';
-    model.animation.frame = 0;
-  })
-    .chain(tween(model.animation, t => t.to({frame: 1}, 300)));
-}
 
 /**
  * ダッシュ
@@ -53,35 +15,17 @@ function dashDown(model: WingDozerModel): Animate {
  * @return アニメーション
  */
 export function dash(model: WingDozerModel, sounds: WingDozerSounds): Animate {
-  return all(
-    dashUp(model),
-    process(() => {
+  return　process(() => {
+      model.animation.type = 'DASH_UP';
+      model.animation.frame = 0;
       sounds.motor.play();
     })
-  )
-    .chain(wait())
-    .chain(all(
-      dashDown(model),
-      process(() => {
-        sounds.motor.play();
-      })
-    ));
-}
-
-/**
- * ターンスタート用 ダッシュ
- *
- * @param model モデル
- * @param sounds 音
- * @return アニメーション
- */
-export function dashForTurnStart(model: WingDozerModel, sounds: WingDozerSounds): Animate {
-  return all(
-    dashUp(model),
-    process(() => {
+    .chain(tween(model.animation, t => t.to({frame: 1}, 300)))
+    .chain(delay(500))
+    .chain(process(() => {
+      model.animation.type = 'DASH_DOWN';
+      model.animation.frame = 0;
       sounds.motor.play();
-    })
-  )
-    .chain(wait())
-    .chain(dashDown(model));
+    }))
+    .chain(tween(model.animation, t => t.to({frame: 1}, 300)));
 }
