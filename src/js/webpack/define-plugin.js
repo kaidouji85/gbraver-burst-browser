09b511203139ca3cdfae0902cb78e3aa24286992
竹内佑介 <kaidouji85@gmail.com>
2021-02-13 17:49:06 +0900

    FPS統計、SW利用を.envに切り出した

--- src/js/webpack/define-plugin.js
+++ src/js/webpack/define-plugin.js
@@ -13,4 +13,14 @@ export const DefinePlugin = {
    * 遊び方動画のURL
    */
   howToPlay: GBRAVER_BURST_HOW_TO_PLAY,
+
+  /**
+   * FPS統計を表示するか否か、trueで表示する
+   */
+  isPerformanceStatsVisible: GBRAVER_BURST_IS_PERFORMANCE_STATS_VISIBLE === 'true',
+
+  /**
+   * サービスワーカーを利用するか否か、trueで利用する
+   */
+  isServiceWorkerUsed: GBRAVER_BURST_IS_SERVICE_WORKER_USED === 'true',
 }
