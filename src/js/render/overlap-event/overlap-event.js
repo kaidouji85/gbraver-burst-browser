e4a59d99ea3761616516eee04accb7ef82d553bd
竹内佑介 <kaidouji85@gmail.com>
2021-03-13 18:54:24 +0900

    不要な関数を削除した

--- src/js/render/overlap-event/overlap-event.js
+++ src/js/render/overlap-event/overlap-event.js
@@ -8,7 +8,6 @@ import type {TouchStartRaycaster} from "./touch-start-raycaster";
 import {toTouchStartRaycaster} from "./touch-start-raycaster";
 import type {TouchMoveRaycaster} from "./touch-move-raycaster";
 import {toTouchMoveRaycaster} from "./touch-move-raycaster";
-import {Observable} from "rxjs";
 import type {RendererDOMEvent} from "../dom-event/dom-event";
 import * as THREE from "three";
 import {filter, map, share} from "rxjs/operators";
@@ -23,37 +22,6 @@ export type OverlapEvent =
   MouseMoveRaycaster |
   TouchStartRaycaster |
   TouchMoveRaycaster;
-
-/**
- * @deprecated
- * DOMイベントストリームを当たり判定ストリームに変換する
- *
- * @param origin 変換元
- * @param rendererDOM レンダラがバインドされているHTML要素
- * @param camera カメラ
- * @return 当たり判定ストリーム
- */
-export function deprecated_toOverlapStream(origin: Observable<RendererDOMEvent>, rendererDOM: HTMLElement, camera: typeof THREE.Camera): Observable<OverlapEvent> {
-  return origin.pipe(
-    map(v => {
-      switch (v.type) {
-        case 'mouseDown':
-          return toMouseDownRaycaster(v, rendererDOM, camera);
-        case 'mouseMove':
-          return toMouseMoveRaycaster(v, rendererDOM, camera);
-        case 'touchStart':
-          return toTouchStartRaycaster(v, rendererDOM, camera);
-        case 'touchMove':
-          return toTouchMoveRaycaster(v, rendererDOM, camera);
-        default:
-          return null;
-      }
-    }),
-    filter(v => !!v),
-    map(v => (v: OverlapEvent)),
-    share()
-  );
-}
 
 /**
  * DOMイベントストリームを当たり判定ストリームに変換する
