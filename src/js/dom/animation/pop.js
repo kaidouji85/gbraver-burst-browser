d4a2a03e31e2c6b969f02edad9e8673f5602c215
竹内佑介 <kaidouji85@gmail.com>
2021-03-22 08:23:32 +0900

    ポップアニメーションに拡大率パラメータを追加した

--- src/js/dom/animation/pop.js
+++ src/js/dom/animation/pop.js
@@ -6,12 +6,13 @@ import {waitFinishAnimation} from "../../wait/wait-finish-animation";
  * ポップアニメーション
  *
  * @param element アニメーション対象のHTML要素
+ * @param scale ポップする大きさ
  * @return アニメーション
  */
-export async function pop(element: HTMLElement): Promise<void> {
+export async function pop(element: HTMLElement, scale: number = 1.1): Promise<void> {
   const animation = element.animate([
     {transform: 'scale(1)'},
-    {transform: 'scale(1.1)'},
+    {transform: `scale(${scale})`},
     {transform: 'scale(1)'},
   ], {
     duration: 200,
