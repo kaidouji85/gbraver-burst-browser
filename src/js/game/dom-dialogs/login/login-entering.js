56cbb394bb1eb7126ff822b8e7d305d718316c8c
竹内佑介 <kaidouji85@gmail.com>
2021-06-16 01:03:19 +0900

    ログイン情報入力にHTML要素ヘルパー関数を適用した

--- src/js/game/dom-dialogs/login/login-entering.js
+++ src/js/game/dom-dialogs/login/login-entering.js
@@ -12,6 +12,72 @@ const ROOT_CLASS_NAME = 'login-entering';
 /** ルート要素が非表示の時のcssクラス名 */
 const INVISIBLE_ROOT_CLASS_NAME = `${ROOT_CLASS_NAME}--invisible`;
 
+/** data-idを集めたもの */
+type DataIDs = {
+  userID: string,
+  password: string,
+  submit: string,
+  closeButton: string,
+};
+
+/**
+ * ルート要素のinnerHTML
+ * 
+ * @param ids data-idを集めたもの
+ * @param caption ダイアログに表示するメッセージ
+ * @return innerHTML
+ */
+function rootInnerHTML(ids: DataIDs, caption: string): string {
+  return `
+    <div class="${ROOT_CLASS_NAME}__caption">${caption}</div>
+    <form class="${ROOT_CLASS_NAME}__form">
+      <div class="${ROOT_CLASS_NAME}__form__user-name">
+        <label class="${ROOT_CLASS_NAME}__form__user-name__label">userid</label>
+        <input class="${ROOT_CLASS_NAME}__form__user-name__input" type="text" name="userid" autocomplete="username" data-id="${ids.userID}">
+      </div>
+      <div class="${ROOT_CLASS_NAME}__form__password">
+        <label class="${ROOT_CLASS_NAME}__form__password__label">password</label>
+        <input class="${ROOT_CLASS_NAME}__form__password__input" type="password" name="password" autocomplete="current-password" data-id="${ids.password}">
+      </div>
+      <div class="${ROOT_CLASS_NAME}__form__footer">
+        <button class="${ROOT_CLASS_NAME}__form__footer__close" data-id="${ids.closeButton}">閉じる</button>
+        <button class="${ROOT_CLASS_NAME}__form__footer__sumit" type="submit" data-id="${ids.submit}">ログイン</buton>
+      </div>
+    </form>
+  `;
+}
+
+/** ルート要素の子孫要素 */
+type Elements = {
+  userID: HTMLInputElement,
+  password: HTMLInputElement,
+  submit: HTMLButtonElement,
+  closeButton: HTMLButtonElement,
+};
+
+/**
+ * ルート要素から子孫要素を抽出する
+ *
+ * @param root ルート要素
+ * @param ids data-idを集めたもの
+ * @return 抽出結果
+ */
+function extractElements(root: HTMLElement, ids: DataIDs): Elements {
+  const userIDElement = root.querySelector(`[data-id="${ids.userID}"]`);
+  const userID = (userIDElement instanceof HTMLInputElement) ? userIDElement : document.createElement('input');
+
+  const passwordElement = root.querySelector(`[data-id="${ids.password}"]`);
+  const password = (passwordElement instanceof HTMLInputElement) ? passwordElement : document.createElement('input');
+
+  const submitElement = root.querySelector(`[data-id="${ids.submit}"]`);
+  const submit = (submitElement instanceof HTMLButtonElement) ? submitElement : document.createElement('button');
+
+  const closeButtonElement = root.querySelector(`[data-id="${ids.closeButton}"]`);
+  const closeButton = (closeButtonElement instanceof HTMLButtonElement) ? closeButtonElement : document.createElement('button');
+
+  return {userID, password, submit, closeButton};
+}
+
 /** 入力完了情報 */
 export type InputComplete = {
   userID: string,
@@ -36,41 +102,16 @@ export class LoginEntering {
    * @param caption 入力フォームに表示されるメッセージ
    */
   constructor(caption: string) {
-    const useridID = domUuid();
-    const passwordID = domUuid();
-    const submitID = domUuid();
-    const closeButtonID = domUuid();
+    const dataIDs = {userID: domUuid(), password: domUuid(), submit: domUuid(), closeButton: domUuid()};
     this._root = document.createElement('div');
     this._root.className = ROOT_CLASS_NAME;
-    this._root.innerHTML = `
-      <div class="${ROOT_CLASS_NAME}__caption">${caption}</div>
-      <form class="${ROOT_CLASS_NAME}__form">
-        <div class="${ROOT_CLASS_NAME}__form__user-name">
-          <label class="${ROOT_CLASS_NAME}__form__user-name__label">userid</label>
-          <input class="${ROOT_CLASS_NAME}__form__user-name__input" type="text" name="userid" autocomplete="username" data-id="${useridID}">
-        </div>
-        <div class="${ROOT_CLASS_NAME}__form__password">
-          <label class="${ROOT_CLASS_NAME}__form__password__label">password</label>
-          <input class="${ROOT_CLASS_NAME}__form__password__input" type="password" name="password" autocomplete="current-password" data-id="${passwordID}">
-        </div>
-        <div class="${ROOT_CLASS_NAME}__form__footer">
-          <button class="${ROOT_CLASS_NAME}__form__footer__close" data-id="${closeButtonID}">閉じる</button>
-          <button class="${ROOT_CLASS_NAME}__form__footer__sumit" type="submit" data-id="${submitID}">ログイン</buton>
-        </div>
-      </form>
-    `;
+    this._root.innerHTML = rootInnerHTML(dataIDs, caption);
+    const elements = extractElements(this._root, dataIDs);
 
-    const userID = this._root.querySelector(`[data-id="${useridID}"]`);
-    this._userID = (userID instanceof HTMLInputElement) ? userID : document.createElement('input');
-
-    const password = this._root.querySelector(`[data-id="${passwordID}"]`);
-    this._password = (password instanceof HTMLInputElement) ? password : document.createElement('input');
-
-    const submit = this._root.querySelector(`[data-id="${submitID}"]`);
-    this._submit = (submit instanceof HTMLButtonElement) ? submit : document.createElement('button');
-
-    const closeButton = this._root.querySelector(`[data-id="${closeButtonID}"]`);
-    this._closeButton = (closeButton instanceof HTMLButtonElement) ? closeButton : document.createElement('button');
+    this._userID = elements.userID;
+    this._password = elements.password;
+    this._submit = elements.submit;
+    this._closeButton = elements.closeButton;
 
     this._inputCoomplete = new RxjsStreamSource();
     this._close = new RxjsStreamSource();
