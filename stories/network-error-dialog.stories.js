822868b1b0fc92b948c74480231de405b68fbc4c
Yuusuke <kaidouji85@gmail.com>
2021-07-05 09:02:42 +0000

    通信エラーダイアログの雛形を追加した

---
+++ stories/network-error-dialog.stories.js
@@ -0,0 +1,13 @@
+// @flow
+
+import type {DOMStubStory} from "./stub/dom-stub";
+import {domStub} from "./stub/dom-stub";
+import {NetworkErrorDialog} from "../src/js/game/dom-dialogs/network-error/network-error-dialog"
+
+export default {
+  title: 'network-error-dialog',
+};
+export const dialog: DOMStubStory = domStub(() => {
+  const dialog = new NetworkErrorDialog('通信エラーが発生しました');
+  return dialog.getRootHTMLElement();
+});
