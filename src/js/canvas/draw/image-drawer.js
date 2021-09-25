1f0ea9af695a396a2321395557a50bce7943346e
竹内佑介 <kaidouji85@gmail.com>
2021-09-25 11:51:41 +0900

    画像描画関数に横幅指定パラメータを追加した

--- src/js/canvas/draw/image-drawer.js
+++ src/js/canvas/draw/image-drawer.js
@@ -8,12 +8,12 @@
  * @param image canvas用画像
  * @param dx 描画X
  * @param dy 描画Y
- * @param scale 拡大率
+ * @param width 画像の横幅、指定なしの場合は画像本来の横幅になる
  */
-export function drawImageInCenter(context: CanvasRenderingContext2D, image: Image, dx: number, dy: number, scale: number = 1) {
-  const width = image.width * scale;
-  const height = image.height * scale;
-  const x = dx - width / 2;
-  const y = dy - height / 2;
-  context.drawImage(image, x, y, width, height);
+export function drawImageInCenter(context: CanvasRenderingContext2D, image: Image, dx: number, dy: number, width?: number): void {
+  const correctWidth = width ?? image.width;
+  const correctHeight = image.height * correctWidth / image.width;
+  const x = dx - correctWidth / 2;
+  const y = dy - correctHeight / 2;
+  context.drawImage(image, x, y, correctWidth, correctHeight);
 }
