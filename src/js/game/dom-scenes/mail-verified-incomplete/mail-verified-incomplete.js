3779c7bd5fdbb5652ac87f829cf04fea5d994252
竹内佑介 <kaidouji85@gmail.com>
2021-11-15 13:46:39 +0900

    メール認証未完了画面に手順、ボタンを追加した

--- src/js/game/dom-scenes/mail-verified-incomplete/mail-verified-incomplete.js
+++ src/js/game/dom-scenes/mail-verified-incomplete/mail-verified-incomplete.js
@@ -15,9 +15,16 @@ const ROOT_CLASS = 'mail-verified-incomplete';
 function rootInnerHTML(mailAddress: string): string {
   const escapedMailAddress = escapeHTML(mailAddress);
   return `
-    <div class="${ROOT_CLASS}__content">
-      <div class="${ROOT_CLASS}__content__caption">${escapedMailAddress}に認証用メールを送信しました。</div>
-      <div class="${ROOT_CLASS}__content__caption">同メールに記載されている認証用リンクをクリックして、サインアップを完了させてください。</div>
+    <div class="${ROOT_CLASS}__title">メール認証を完了させてください</div>
+    <div class="${ROOT_CLASS}__caption">以下手順でメール認証を完了させてから、ゲームを開始してください</div>
+    <ol class="${ROOT_CLASS}__procedure">
+      <li class="${ROOT_CLASS}__procedure__item">${escapedMailAddress}に送信された認証メールを開く</li>
+      <li class="${ROOT_CLASS}__procedure__item">認証メールに記載された認証用リンクをブラウザで開く</li>
+      <li class="${ROOT_CLASS}__procedure__item">Gブレイバーバーストを再読み込みする</li>
+    </ol>
+    <div class="${ROOT_CLASS}__controllers">
+      <button class="${ROOT_CLASS}__controllers__login-with-another-account">別アカウントでログイン</button>
+      <button class="${ROOT_CLASS}__controllers__reload">再読み込み</button>
     </div>
   `;
 }
