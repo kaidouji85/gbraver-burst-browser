078ae73124656ca33bbb36b5b4aea239ce26661d
Y.Takeuchi <kaidouji85@gmail.com>
2021-12-19 19:45:01 +0900

    ハードコースのウィングドーザNPCを調整した

--- src/js/npc/hard-wing-dozer.js
+++ src/js/npc/hard-wing-dozer.js
@@ -4,6 +4,7 @@ import type {NPC} from "./npc";
 import {ArmDozerIdList, ArmDozers, PilotIds, Pilots} from "gbraver-burst-core";
 import type {SimpleRoutine} from "./simple-npc";
 import {SimpleNPC} from "./simple-npc";
+import {canBeatDown} from "./can-beat-down";
 
 /** 0バッテリー */
 const ZERO_BATTERY = {
@@ -16,12 +17,23 @@ const ZERO_BATTERY = {
  * 攻撃ルーチン
  */
 const attackRoutine: SimpleRoutine = data => {
+  const burst = data.commands.find(v => v.type === 'BURST_COMMAND');
   const pilot = data.commands.find(v => v.type === 'PILOT_SKILL_COMMAND');
   const battery5 = data.commands.find(v => v.type === 'BATTERY_COMMAND' && v.battery === 5);
-  const allBatteryMinusOne = data.commands.find(v => v.type === 'BATTERY_COMMAND' && v.battery === data.enemy.armdozer.battery - 1)
+  const allBattery = data.commands.find(v => v.type === 'BATTERY_COMMAND' && v.battery === data.enemy.armdozer.battery);
+  const allBatteryMinusOne = data.commands.find(v => v.type === 'BATTERY_COMMAND' && v.battery === data.enemy.armdozer.battery - 1);
+  const canBeatDownWithAllBattery = canBeatDown(data.enemy, data.enemy.armdozer.battery, data.player, data.player.armdozer.battery);
+
+  if (burst && battery5) {
+    return battery5;
+  }
 
   if (pilot && battery5) {
     return battery5;
+  }
+
+  if (canBeatDownWithAllBattery && !data.player.armdozer.enableBurst && !data.player.pilot.enableSkill && allBattery) {
+    return allBattery;
   }
 
   if (data.enemy.armdozer.battery === 0 && pilot) {
@@ -40,14 +52,39 @@ const attackRoutine: SimpleRoutine = data => {
  * 防御ルーチン
  */
 const defenseRoutine: SimpleRoutine = data => {
+  const pilot = data.commands.find(v => v.type === 'PILOT_SKILL_COMMAND');
   const burst = data.commands.find(v => v.type === 'BURST_COMMAND');
   const battery1 = data.commands.find(v => v.type === 'BATTERY_COMMAND' && v.battery === 1);
+  const allBattery = data.commands.find(v => v.type === 'BATTERY_COMMAND' && v.battery === data.enemy.armdozer.battery);
+  const isDefeatedWithBattery1 = canBeatDown(data.player, data.player.armdozer.battery, data.enemy, 1);
+  const isDefeatedWithAllBattery = canBeatDown(data.player, data.player.armdozer.battery, data.enemy, data.enemy.armdozer.battery);
+  const isNotMaxBattery = data.enemy.armdozer.battery < data.enemy.armdozer.maxBattery;
 
-  if (burst) {
+  if (data.enemy.armdozer.battery === 0 && burst) {
     return burst;
   }
 
-  return battery1 ?? ZERO_BATTERY;
+  if (data.enemy.armdozer.battery === 0 && pilot) {
+    return pilot;
+  }
+
+  if (isDefeatedWithAllBattery && isNotMaxBattery && burst) {
+    return burst;
+  }
+
+  if (isDefeatedWithAllBattery && isNotMaxBattery && pilot) {
+    return pilot;
+  }
+
+  if (isDefeatedWithBattery1 && allBattery) {
+    return allBattery;
+  }
+
+  if (battery1) {
+    return battery1;
+  }
+
+  return ZERO_BATTERY;
 };
 
 /**
