12fed3d1adf044e7b5104d316345e4697d505c81
vagrant <vagrant@vagrant.vm>
2022-01-14 02:24:26 +0000

    コードレビュー対応

--- upload-module.sh
+++ upload-module.sh
@@ -9,7 +9,7 @@ S3_BUCKET=$1
 OWN_PATH=`cd $(dirname ${0}) && pwd`
 BUILD_PATH="${OWN_PATH}/build/production"
 
-for fileName in `ls -F ${BUILD_PATH} | grep -v / `; do
+for fileName in `ls -F "${BUILD_PATH}" | grep -v / `; do
   aws s3 cp --cache-control "no-cache,no-store" "${BUILD_PATH}/${fileName}" "s3://${S3_BUCKET}/${fileName}"
 done
 aws s3 sync --delete --exact-timestamps "${BUILD_PATH}" "s3://${S3_BUCKET}"
