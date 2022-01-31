6b6e70401f7f16eaa6739ebf8da6fd2c07726284
vagrant <vagrant@vagrant.vm>
2022-01-31 08:58:29 +0000

    WebGLPixelRatioパース関数を追加した

--- src/js/game/config/browser-config.js
+++ src/js/game/config/browser-config.js
@@ -4,7 +4,23 @@
 export type WebGLPixelRatio = 1 | 2;
 
 /** 設定可能なWebGLピクセルレートを集めたもの */
-export const WebGLPixelRatios: WebGLPixelRatio[] = [1, 2];
+export const WebGLPixelRatios: [WebGLPixelRatio, WebGLPixelRatio] = [1, 2];
+
+/**
+ * 任意のオブジェクトをWebGLピクセルレートにパースする
+ * パースできない場合はnullを返す
+ *
+ * @return パース結果
+ */
+export function parseWebGLPixexRatio(origin: any): ?WebGLPixelRatio {
+  const parsedInt = parseInt(origin);
+  if (isNaN(parsedInt)) {
+    return null;
+  }
+
+  const finedWebGLPixelRatio = WebGLPixelRatios.find(v => v === parsedInt);
+  return finedWebGLPixelRatio ?? null;
+}
 
 /** Gブレイバーバースト ブラウザ側設定項目 */
 export type GbraverBurstBrowserConfig = {
