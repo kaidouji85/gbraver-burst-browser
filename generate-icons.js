207078251c884eafeed0da3426aea8f19cf84edb
vagrant <vagrant@vagrant.vm>
2022-02-04 10:16:30 +0000

    コードレビュー対応

--- generate-icons.js
+++ generate-icons.js
@@ -7,21 +7,25 @@ const buildRoot = path.resolve(__dirname, 'build/production');
 const originIconPath = path.resolve(buildRoot, `app-icon-512x512.png`);
 
 /**
- * アプリアイコンを新規ファイルでリサイズする
+ * オリジナルのアプリアイコンをコピーした上でリサイズする
  *
  * @param {string} fileName 保存するファイル名
- * @param {number} width リサイズ後の横幅
- * @param {number} height リサイズ後の縦幅
+ * @param {number} size ピクセル単位で指定するリサイズ後の大きさ
  * @return {Promise<void>} 大きさ変更が完了したら発火するPromise
  */
-async function resizeAppIcon(fileName, width, height) {
+async function resizeAppIcon(fileName, size) {
   const buffer = await sharp(originIconPath)
-    .resize(width, height)
+    .resize(size, size)
     .toBuffer();
   const filePath = path.resolve(buildRoot, fileName);  
   return sharp(buffer).toFile(filePath);
 }
 
+/**
+ * アプリアイコンからfavicon.icoを生成する
+ *
+ * @return {Promise<void>} 処理が完了したら発火するPromise
+ */
 async function toFavicon() {
   const pngBuffer = await sharp(originIconPath)
     .resize(48, 48)
@@ -31,14 +35,18 @@ async function toFavicon() {
   await fs.writeFile(icoPath, icoBuffer);
 }
 
-/** 512x512のアイコンから様々なサイズのアイコンを自動生成する */
+/** 
+ * 512x512のアイコンから様々なサイズのアイコンを自動生成する
+ */
 (async () => {
   console.log('start generate icons');
+  const appIcon = (size) => resizeAppIcon(`app-icon-${size}x${size}.png`, size);
+  const pngFavicon = size => resizeAppIcon(`favicon-${size}x${size}.png`, size);
   await Promise.all([
-    resizeAppIcon('app-icon-192x192.png', 192, 192),
-    resizeAppIcon('app-icon-180x180.png', 180, 180),
-    resizeAppIcon('favicon-32x32.png', 32, 32),
-    resizeAppIcon('favicon-16x16.png', 16, 16),
+    appIcon(192),
+    appIcon(180),
+    pngFavicon(32),
+    pngFavicon(16),
     toFavicon()
   ]);
   console.log('end generate icons');
