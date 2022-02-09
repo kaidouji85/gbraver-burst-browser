5fc1d4ec08912d4cc368f85fa9ac92da21ba8f42
vagrant <vagrant@vagrant.vm>
2022-02-09 04:13:58 +0000

    codebuildにassetlink.json生成処理を追加した

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-if [ $# != 2 ]; then
+if [ $# != 3 ]; then
   echo 'invalid param'
   exit 1
 fi
 
 S3_BUCKET=$1
 DISTRIBUTION_ID=$2
+ASSETLINK_JSON_URI=$3
 OWN_PATH=`cd $(dirname ${0}) && pwd`
 
 npm run build:production
+aws s3 cp "${ASSETLINK_JSON_URI}" "${OWN_PATH}/build/.well-known/assetlinks.json"
 "${OWN_PATH}/upload-module.sh" "${S3_BUCKET}"
 "${OWN_PATH}/clear-cdn.sh" "${DISTRIBUTION_ID}"
