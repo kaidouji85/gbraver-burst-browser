b4ab6fb183e155752b938dd2938fdab7e27f2886
vagrant <vagrant@vagrant.vm>
2022-02-09 04:32:26 +0000

    ビルド不具合を修正

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ ASSETLINK_JSON_URI=$3
 OWN_PATH=`cd $(dirname ${0}) && pwd`
 
 npm run build:production
-aws s3 cp "${ASSETLINK_JSON_URI}" "${OWN_PATH}/build/.well-known/assetlinks.json"
+aws s3 cp "${ASSETLINK_JSON_URI}" "${OWN_PATH}/build/production/.well-known/assetlinks.json"
 "${OWN_PATH}/upload-module.sh" "${S3_BUCKET}"
 "${OWN_PATH}/clear-cdn.sh" "${DISTRIBUTION_ID}"
