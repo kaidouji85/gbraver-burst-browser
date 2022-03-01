10a384cebb4ae612bae88d7f147de52284d7ff69
Y.Takeuchi <kaidouji85@gmail.com>
2022-03-01 21:34:33 +0900

    不要なファイルを削除した

--- src/js/game-object/turn-start/animation/show.js
+++ src/js/game-object/turn-start/animation/show.js
@@ -5,7 +5,6 @@ import type {TurnStartModel} from "../model/turn-start-model";
 import {process} from "../../../animation/process";
 import {all} from "../../../animation/all";
 import {tween} from "../../../animation/tween";
-import {TurnStartSounds} from "../sounds/turn-start-sounds";
 
 /**
  * 表示アニメーション
@@ -21,21 +20,5 @@ export function show(model: TurnStartModel): Animate {
     .chain(all(
       tween(model, t => t.to({opacity: 1}, 400)),
       tween(model.position, t => t.to({x: 0}, 400))
-    ))
-}
-
-/**
- * 音付きで表示する
- *
- * @param model モデル
- * @param sounds 効果音
- * @return アニメーション
- */
-export function showWithSound(model: TurnStartModel, sounds: TurnStartSounds): Animate {
-  return all(
-    show(model),
-    process(() => {
-      sounds.batteryRecover.play()
-    })
-  );
+    ));
 }
--- src/js/game-object/turn-start/turn-start.js
+++ src/js/game-object/turn-start/turn-start.js
@@ -6,9 +6,8 @@ import type {TurnStartModel} from "./model/turn-start-model";
 import {createInitialValue} from "./model/initial-value";
 import type {PreRender} from "../../game-loop/pre-render";
 import {Animate} from "../../animation/animate";
-import {show, showWithSound} from './animation/show';
+import {show} from './animation/show';
 import {hidden} from './animation/hidden';
-import {TurnStartSounds} from "./sounds/turn-start-sounds";
 import type {Resources} from "../../resource";
 import type {GameObjectAction} from "../action/game-object-action";
 import type {Stream, Unsubscriber} from "../../stream/core";
@@ -19,7 +18,6 @@ import type {Stream, Unsubscriber} from "../../stream/core";
 export class TurnStart {
   _model: TurnStartModel;
   _view: TurnStartView;
-  _sounds: TurnStartSounds;
   _unsubscriber: Unsubscriber;
 
   /**
@@ -32,7 +30,6 @@ export class TurnStart {
   constructor(view: TurnStartView, resources: Resources, gameObjectAction: Stream<GameObjectAction>) {
     this._model = createInitialValue();
     this._view = view;
-    this._sounds = new TurnStartSounds(resources);
     this._unsubscriber = gameObjectAction.subscribe(action => {
       if (action.type === 'PreRender') {
         this._onPreRender(action);
@@ -53,15 +50,6 @@ export class TurnStart {
    */
   show(): Animate {
     return show(this._model);
-  }
-
-  /**
-   * 効果音付きで表示する
-   *
-   * @return アニメーション
-   */
-  showWithSound(): Animate {
-    return showWithSound(this._model, this._sounds);
   }
 
   /**
