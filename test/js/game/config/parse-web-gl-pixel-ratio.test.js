16a3e909582d459d7dd31c4a017194491f6240e5
vagrant <vagrant@vagrant.vm>
2022-04-01 05:58:05 +0000

    ブラウザ設定系コードのtypoを修正した

--- test/js/game/config/parse-web-gl-pixel-ratio.test.js
+++ test/js/game/config/parse-web-gl-pixel-ratio.test.js
@@ -1,35 +1,35 @@
 // @flow
 
-import { parseWebGLPixexRatio } from "../../../../src/js/game/config/browser-config";
+import {parseWebGLPixelRatio} from "../../../../src/js/game/config/browser-config";
 
 test('WebGLPixelRatioで有効な整数ならパースできる', () => {
-  expect(parseWebGLPixexRatio(2)).toBe(2);
+  expect(parseWebGLPixelRatio(2)).toBe(2);
 });
 
 test('文字列でも有効なPixelRatioならパースできる', () => {
-  expect(parseWebGLPixexRatio('1')).toBe(1);
+  expect(parseWebGLPixelRatio('1')).toBe(1);
 });
 
 test('整数でも無効なpixelRatioならパースできない', () => {
-  expect(parseWebGLPixexRatio(4)).toBe(null);
+  expect(parseWebGLPixelRatio(4)).toBe(null);
 });
 
 test('文字列の内容が整数でも、無効なpixelRatioならパースできない', () => {
-  expect(parseWebGLPixexRatio('5')).toBe(null);
+  expect(parseWebGLPixelRatio('5')).toBe(null);
 });
 
 test('空文字の場合はパースできない', () => {
-  expect(parseWebGLPixexRatio('')).toBe(null);
+  expect(parseWebGLPixelRatio('')).toBe(null);
 });
 
 test('正しいpixelRatioが含まれている文字列でもパースできない', () => {
-  expect(parseWebGLPixexRatio('hello, 1')).toBe(null);
+  expect(parseWebGLPixelRatio('hello, 1')).toBe(null);
 });
 
 test('undefinedの場合はパースできない', () => {
-  expect(parseWebGLPixexRatio(undefined)).toBe(null);
+  expect(parseWebGLPixelRatio(undefined)).toBe(null);
 });
 
 test('nullの場合はパースできない', () => {
-  expect(parseWebGLPixexRatio(null)).toBe(null);
+  expect(parseWebGLPixelRatio(null)).toBe(null);
 });
