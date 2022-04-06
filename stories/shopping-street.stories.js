6d0322aaf92cf4291ce4bd371100e7b7e415065c
Y.Takeuchi <kaidouji85@gmail.com>
2022-04-06 22:08:09 +0900

    商店街ストーリーに高解像度静止画を追加した

--- stories/shopping-street.stories.js
+++ stories/shopping-street.stories.js
@@ -1,9 +1,15 @@
 // @flow
-
+import * as THREE from "three";
 import {TDGameObjectStub} from "./stub/td-game-object-stub";
 import ShoppingStreet from "../src/js/game-object/stage/shopping-street";
 import {Illumination} from "../src/js/game-object/illumination/illumination";
 import {skyBox} from "../src/js/game/td-scenes/battle/view/td/sky-box";
+import {RxjsStreamSource} from "../src/js/stream/rxjs";
+import type {Stream} from '../src/js/stream/core';
+import {StorybookResourceRoot} from "./resource-root/storybook-resource-root";
+import {fullResourceLoading} from "../src/js/resource";
+import type {GameObjectAction} from "../src/js/game-object/action/game-object-action";
+import {getViewPortHeight, getViewPortWidth} from "../src/js/view-port/view-port-size";
 
 export default {
   title: 'shopping-street',
@@ -33,3 +39,38 @@ export const longShot = (): HTMLElement => {
   stub.start();
   return stub.domElement();
 }
+
+export const highResolutionStillImage = (): HTMLElement => {
+  const width = getViewPortWidth();
+  const height = getViewPortHeight();
+  const pixelRatio = window.devicePixelRatio;
+  const position = {x: 0, y: 220, z: 300};
+  const target = {x: 0, y: 200, z: 0};
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(width, height);
+  renderer.setPixelRatio(pixelRatio);
+
+  (async () => {
+    const resourceRoot = new StorybookResourceRoot();
+    const resourceLoading = fullResourceLoading(resourceRoot);
+    const resources = await resourceLoading.resources;
+    const emptyGameObjectAction: Stream<GameObjectAction> = new RxjsStreamSource();
+    const scene = new THREE.Scene();
+    const aspect = width / height;
+    const camera = new THREE.PerspectiveCamera(75, aspect, 1, 10000);
+    camera.position.x = position.x;
+    camera.position.y = position.y;
+    camera.position.z = position.z;
+    camera.lookAt(target.x, target.y, target.z);
+    const illumination = new Illumination(emptyGameObjectAction);
+    const backGround = new ShoppingStreet(resources);
+    scene.background = skyBox(resources);
+    const object3Ds = [...illumination.getObject3Ds(), ...backGround.getThreeJsObjects()];
+    object3Ds.forEach(v => {
+      scene.add(v);
+    });
+    renderer.render(scene, camera);
+  })();
+
+  return renderer.domElement;
+};
