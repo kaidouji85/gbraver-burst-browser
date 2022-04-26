32b56bd033e895edb70a2407fa747a6ea1491122
kaidouji85 <kaidouji85@gmail.com>
2022-04-26 21:30:31 +0900

    scaleTweenDurationに複雑なチェインのケースを追加した

--- test/js/animation/scale-tween-duration.test.js
+++ test/js/animation/scale-tween-duration.test.js
@@ -26,3 +26,22 @@ test('ループしたTweenでもDurationがスケールされている', () => {
   expect(t1._duration).toBe(300);
   expect(t2._duration).toBe(200);
 });
+
+test('複雑にチェインしたTweenでもDurationがスケールされている', () => {
+  const t1 = new Tween({}).to({}, 100);
+  const t2 = new Tween({}).to({}, 200);
+  const t3 = new Tween({}).to({}, 300);
+  const t4 = new Tween({}).to({}, 400);
+  const t5 = new Tween({}).to({}, 500);
+  t1.chain(
+    t2.chain(t3.chain(t5)),
+    t4
+  );
+
+  scaleTweenDuration(t1, 0.5);
+  expect(t1._duration).toBe(50);
+  expect(t2._duration).toBe(100);
+  expect(t3._duration).toBe(150);
+  expect(t4._duration).toBe(200);
+  expect(t5._duration).toBe(250);
+});
