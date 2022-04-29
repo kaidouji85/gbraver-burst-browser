e73356506e0424be87b63dbcc176cf529d4ccd6a
kaidouji85 <kaidouji85@gmail.com>
2022-04-29 15:28:58 +0900

    バッテリー表示時間を短くした

--- src/js/game-object/battery-number/animation/hidden.js
+++ src/js/game-object/battery-number/animation/hidden.js
@@ -11,5 +11,5 @@ import type {BatteryNumberModel} from "../model/battery-number-model";
  * @return アニメーション
  */
 export function hidden(model: BatteryNumberModel): Animate {
-  return tween(model, t => t.to({opacity: 0, scale: 1.1}, 300))
+  return tween(model, t => t.to({opacity: 0, scale: 1.1}, 200))
 }
