c7f8b301ef94a8775dd7b182620fd9498d33c731
kaidouji85 <kaidouji85@gmail.com>
2022-06-09 02:27:29 +0000

    ブラウザ設定リポジトリを追加した

--- src/js/game/config/local-storage.js
+++ src/js/game/config/local-storage.js
@@ -1,6 +1,7 @@
 // @flow
-import type {BattleAnimationTimeScale, GbraverBurstBrowserConfig, SoundVolume, WebGLPixelRatio} from "./browser-config";
+import type {BattleAnimationTimeScale, GbraverBurstBrowserConfig, GbraverBurstBrowserConfigRepository, SoundVolume, WebGLPixelRatio} from "./browser-config";
 import {parseBattleAnimationTimeScale, parseSoundVolume, parseWebGLPixelRatio} from "./browser-config";
+import {DefaultConfig} from "./default-config";
 
 /** 設定項目名とLocalStorageキーのマッピング */
 const LocalStorageKeys = {
@@ -15,6 +16,7 @@ const LocalStorageKeys = {
 }
 
 /**
+ * @deprecated
  * LocalStorageからGブレイバーバースト ブラウザ側設定項目を抽出する
  * 抽出できなかった場合、nullを返す
  * 
@@ -33,10 +35,11 @@ export function configFromLocalStorage(): GbraverBurstBrowserConfig | null {
   const battleAnimationTimeScale: BattleAnimationTimeScale = parsedBattleAnimationTimeScale;
   const bgmVolume: SoundVolume = parsedBGMVolume;
   const seVolume: SoundVolume = parsedSEVolume;
-  return {webGLPixelRatio, battleAnimationTimeScale, bgmVolume, seVolume};
+  return { webGLPixelRatio, battleAnimationTimeScale, bgmVolume, seVolume };
 }
 
 /**
+ * @deprecated
  * Gブレイバーバースト ブラウザ側設定項目をLocalStorageに保存する
  *
  * @param config Gブレイバーバースト ブラウザ側設定項目
@@ -46,4 +49,26 @@ export function saveConfigToLocalStorage(config: GbraverBurstBrowserConfig): voi
   localStorage.setItem(LocalStorageKeys.BattleAnimationTimeScale, `${config.battleAnimationTimeScale}`);
   localStorage.setItem(LocalStorageKeys.BGMVolume, `${config.bgmVolume}`);
   localStorage.setItem(LocalStorageKeys.SEVolume, `${config.seVolume}`);
+}
+
+/** ブラウザ設定リポジトリのLocalStorage実装 */
+class LocalStorageConfigRepository implements GbraverBurstBrowserConfigRepository {
+  /** @override */
+  async save(config: GbraverBurstBrowserConfig): Promise<void> {
+    saveConfigToLocalStorage(config);
+  }
+
+  /** @override */
+  async load(): Promise<GbraverBurstBrowserConfig> {
+    return configFromLocalStorage() ?? DefaultConfig;
+  }
+}
+
+/**
+ * ブラウザ設定リポジトリLocalStorage実装を生成する
+ * 
+ * @return 生成結果
+ */
+export function createLocalStorageConfigRepository(): GbraverBurstBrowserConfigRepository {
+  return new LocalStorageConfigRepository();
 }
