c2b9d581d54c061bb30c98feb7774a2d55903398
kaidouji85 <kaidouji85@gmail.com>
2022-07-01 21:47:30 +0900

    シンヤカットインをプライベートフィールド対応させた

--- src/js/game-object/cut-in/shinya/shinya.js
+++ src/js/game-object/cut-in/shinya/shinya.js
@@ -17,10 +17,10 @@ import type {ShinyaView} from "./view/shinya-view";
  * シンヤ カットイン
  */
 export class ShinyaCutIn {
-  _model: ShinyaModel;
-  _view: ShinyaView;
-  _sounds: ShinyaSounds;
-  _unsubscriber: Unsubscriber;
+  #model: ShinyaModel;
+  #view: ShinyaView;
+  #sounds: ShinyaSounds;
+  #unsubscriber: Unsubscriber;
 
   /**
    * コンストラクタ
@@ -30,12 +30,12 @@ export class ShinyaCutIn {
    * @param gameObjectAction ゲームオブジェクトアクション
    */
   constructor(view: ShinyaView, resources: Resources, gameObjectAction: Stream<GameObjectAction>) {
-    this._model = createInitialValue();
-    this._view = view;
-    this._sounds = new ShinyaSounds(resources);
-    this._unsubscriber = gameObjectAction.subscribe(action => {
+    this.#model = createInitialValue();
+    this.#view = view;
+    this.#sounds = new ShinyaSounds(resources);
+    this.#unsubscriber = gameObjectAction.subscribe(action => {
       if (action.type === 'PreRender') {
-        this._onPreRender(action);
+        this.#onPreRender(action);
       }
     });
   }
@@ -44,8 +44,8 @@ export class ShinyaCutIn {
    * デストラクタ相当の処理
    */
   destructor(): void {
-    this._view.destructor();
-    this._unsubscriber.unsubscribe();
+    this.#view.destructor();
+    this.#unsubscriber.unsubscribe();
   }
 
   /**
@@ -54,7 +54,7 @@ export class ShinyaCutIn {
    * @return アニメーション
    */
   show(): Animate {
-    return show(this._model, this._sounds);
+    return show(this.#model, this.#sounds);
   }
 
   /**
@@ -63,7 +63,7 @@ export class ShinyaCutIn {
    * @return アニメーション
    */
   hidden(): Animate {
-    return hidden(this._model);
+    return hidden(this.#model);
   }
 
   /**
@@ -72,7 +72,7 @@ export class ShinyaCutIn {
    * @return シーンに追加するオブジェクト
    */
   getObject3D(): typeof THREE.Object3D {
-    return this._view.getObject3D();
+    return this.#view.getObject3D();
   }
 
   /**
@@ -80,7 +80,7 @@ export class ShinyaCutIn {
    *
    * @param action アクション
    */
-  _onPreRender(action: PreRender): void {
-    this._view.engage(this._model, action);
+  #onPreRender(action: PreRender): void {
+    this.#view.engage(this.#model, action);
   }
 }
