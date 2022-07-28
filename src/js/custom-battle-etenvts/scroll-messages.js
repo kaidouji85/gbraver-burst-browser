66e609d6d58b89e4994ec6062714965037fed7d5
kaidouji85 <kaidouji85@gmail.com>
2022-07-28 06:32:14 +0000

    JSDocを追加した

--- src/js/custom-battle-etenvts/scroll-messages.js
+++ src/js/custom-battle-etenvts/scroll-messages.js
@@ -1,22 +1,49 @@
 // @flow
 import type {MessageWindow} from "../game-dom/message-window/message-window";
 import type {BattleSceneProps} from "../game/td-scenes/battle/custom-battle-event";
-import {waitUntilWindowPush} from "./wait-until-window-push";
+import type {Stream} from "../stream/stream";
+import type {PushWindow} from "../window/push-window";
+import {waitUntilWindowPushWithStream} from "./wait-until-window-push";
 
+/** メッセージウインドウのパラグラフ */
 type Paragraph = string[];
 
-export async function scrollMessages(props: BattleSceneProps, messageWindow: MessageWindow, paragraphs: Paragraph[]): Promise<void> {
+/**
+ * 複数メッセージをスクロール表示する
+ * 画面押下したら、次のメッセージがスクロール表示される
+ * 全てのメッセージを表示した後に画面押下したら、本関数の処理が完了する
+ *
+ * @param messageWindow メッセージウインドウ
+ * @param pushWindow 画面押下ストリーム
+ * @param paragraphs 表示するメッセージ
+ * @return 処理が完了したら発火するPromise
+ */
+async function scrollMessages(messageWindow: MessageWindow, pushWindow: Stream<PushWindow>, paragraphs: Paragraph[]): Promise<void> {
   for(let i=0; i < paragraphs.length; i ++) {
     messageWindow.scrollUp();
     messageWindow.messages(paragraphs[i]);
-    await waitUntilWindowPush(props);
+    await waitUntilWindowPushWithStream(pushWindow);
   }
 }
 
+/**
+ * 左側メッセージウインドウで複数メッセージスクロール表示をする
+ *
+ * @param props カスタムイベントで利用可能な戦闘シーンプロパティ
+ * @param paragraphs 表示するメッセージ
+ * @return 処理が完了したら発火するPromise
+ */
 export async function scrollLeftMessages(props: BattleSceneProps, paragraphs: Paragraph[]): Promise<void> {
-  await scrollMessages(props, props.view.dom.leftMessageWindow, paragraphs);
+  await scrollMessages(props.view.dom.leftMessageWindow, props.pushWindow, paragraphs);
 }
 
+/**
+ * 右側メッセージウインドウで複数メッセージスクロール表示をする
+ *
+ * @param props カスタムイベントで利用可能な戦闘シーンプロパティ
+ * @param paragraphs 表示するメッセージ
+ * @return 処理が完了したら発火するPromise
+ */
 export async function scrollRightMessages(props: BattleSceneProps, paragraphs: Paragraph[]): Promise<void> {
-  await scrollMessages(props, props.view.dom.rightMessageWindow, paragraphs);
+  await scrollMessages(props.view.dom.rightMessageWindow, props.pushWindow, paragraphs);
 }
