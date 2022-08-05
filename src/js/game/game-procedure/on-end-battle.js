bf3d1f97872057e94b99367ff69eef6f1ecc981c
kaidouji85 <kaidouji85@gmail.com>
2022-08-05 05:44:44 +0000

    チュートリアル終了時の処理を追加した

--- src/js/game/game-procedure/on-end-battle.js
+++ src/js/game/game-procedure/on-end-battle.js
@@ -5,11 +5,15 @@ import {
   PostNetworkBattleButtons,
   PostNPCBattleComplete,
   PostNPCBattleLoseButtons,
-  PostNPCBattleWinButtons
+  PostNPCBattleWinButtons,
+  PostTutorialWinButtons,
+  PostTutorialLoseButtons,
 } from "../dom-floaters/post-battle/post-battle-buttons";
 import type {EndBattle} from "../game-actions";
 import type {GameProps} from "../game-props";
 import type {NPCBattle, PlayingNPCBattle} from "../in-progress/npc-battle";
+import {isTutorialWin} from "../in-progress/tutorial";
+import type {Tutorial} from "../in-progress/tutorial";
 import {isNPCBattleStageClear, updateNPCBattle} from "../npc-battle";
 
 /**
@@ -49,6 +53,17 @@ const endNPCBattleStage = async (props: $ReadOnly<GameProps>, postBattleButtons:
 };
 
 /**
+ * チュートリアルが終了した際の処理
+ *
+ * @param props ゲームプロパティ
+ * @param postBattleButtons 戦闘終了後アクションボタン設定
+ * @return 処理が完了したら発火するPromise
+ */
+const endTutorial = async (props: $ReadOnly<GameProps>, postBattleButtons: PostBattleButtonConfig[]) => {
+  await props.domFloaters.showPostBattle(props.resources, postBattleButtons);
+};
+
+/**
  * 戦闘終了時の処理
  * 本関数にはpropsを変更する副作用がある
  *
@@ -71,17 +86,28 @@ export async function onEndBattle(props: GameProps, action: EndBattle): Promise<
         return PostNPCBattleComplete;
       } else if (isStageClear) {
         return PostNPCBattleWinButtons;
-      } else {
-        return PostNPCBattleLoseButtons;
       }
+      return PostNPCBattleLoseButtons;
     })();
     return {updatedInProgress, postBattleButtons};
   })(props);
+  const tutorial = ((props: $ReadOnly<GameProps>) => {
+    if (props.inProgress.type !== 'Tutorial') {
+      return null;
+    }
+    const inProgress = (props.inProgress: Tutorial);
+    const isWin = isTutorialWin(inProgress, action.gameEnd.result);
+    const postBattleButtons = isWin ? PostTutorialWinButtons : PostTutorialLoseButtons;
+    return {postBattleButtons};
+  })(props);
+
   await saveAnimationTimeScale(props, action.animationTimeScale);
   if (npcBattle) {
     props.inProgress = npcBattle.updatedInProgress;
     await endNPCBattleStage(props, npcBattle.postBattleButtons);  
   } else if (props.inProgress.type === 'CasualMatch') {
     await endCasualMatch(props);
+  } else if (tutorial) {
+    await endTutorial(props, tutorial.postBattleButtons);
   }
 }
