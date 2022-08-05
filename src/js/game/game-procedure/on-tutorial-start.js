ee34014eba51a483a7b44c64183bb16195e1d514
kaidouji85 <kaidouji85@gmail.com>
2022-08-05 06:15:20 +0000

    チュートリアル開始処理をヘルパー関数にした

--- src/js/game/game-procedure/on-tutorial-start.js
+++ src/js/game/game-procedure/on-tutorial-start.js
@@ -1,11 +1,8 @@
 // @flow
-import {fadeOut, stop} from "../../bgm/bgm-operators";
-import {createTutorialEvent} from "../../custom-battle-events/tutorial";
-import {NPCBattleRoom} from "../../npc/npc-battle-room";
-import {SOUND_IDS} from "../../resource/sound";
 import type {GameProps} from "../game-props";
 import {createTutorial} from "../in-progress/tutorial";
 import {fullResourceLoading} from "./full-resource-loading";
+import {startTutorial} from "./start-tutorial";
 
 /**
  * チュートリアル開始時の処理
@@ -21,17 +18,5 @@ export async function onTutorialStart(props: GameProps): Promise<void> {
 
   const tutorial = createTutorial();
   props.inProgress = tutorial;
-  await props.fader.fadeOut();
-  const tutorialEvent = createTutorialEvent(tutorial.playerId);
-  const npcBattle = new NPCBattleRoom(tutorialEvent.player, tutorialEvent.npc);
-  const battleProgress = {progress: v => Promise.resolve(npcBattle.progress(v))};
-  const config = await props.config.load();
-  const battleScene = props.tdScenes.startBattle({resources: props.resources, bgm: props.bgm,
-    playingBGM: SOUND_IDS.TUTORIAL_BGM, pixelRatio: config.webGLPixelRatio, initialAnimationTimeScale: config.battleAnimationTimeScale,
-    battleProgress, player: npcBattle.player, enemy: npcBattle.enemy, initialState: npcBattle.stateHistory(), customBattleEvent: tutorialEvent});
-  props.domScenes.hidden();
-  await props.bgm.do(fadeOut);
-  await props.bgm.do(stop);
-  await props.fader.fadeIn();
-  await battleScene.start();
+  startTutorial(props, tutorial.playerId);
 }
