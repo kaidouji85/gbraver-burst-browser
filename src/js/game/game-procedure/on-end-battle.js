5af94a2edf658d5edfafef6ae91c888e3c0c6f80
kaidouji85 <kaidouji85@gmail.com>
2022-08-05 04:59:58 +0000

    JSDocを追加した

--- src/js/game/game-procedure/on-end-battle.js
+++ src/js/game/game-procedure/on-end-battle.js
@@ -1,5 +1,6 @@
 // @flow
 import {BattleAnimationTimeScales, parseBattleAnimationTimeScale} from "../config/browser-config";
+import type {PostBattleButtonConfig} from "../dom-floaters/post-battle/post-battle-button-config";
 import {
   PostNetworkBattleButtons,
   PostNPCBattleComplete,
@@ -8,17 +9,15 @@ import {
 } from "../dom-floaters/post-battle/post-battle-buttons";
 import type {EndBattle} from "../game-actions";
 import type {GameProps} from "../game-props";
-import type {
-  NPCBattle,
-  NPCBattleX,
-  PlayingNPCBattle,
-} from "../in-progress/npc-battle";
+import type {NPCBattle, PlayingNPCBattle} from "../in-progress/npc-battle";
 import {isNPCBattleStageClear, updateNPCBattle} from "../npc-battle";
-import {stand} from "../../../../stories/wing-dozer.stories";
-import {NPCBattleCourses} from "../npc-battle-courses";
-import type {InProgress} from "../in-progress/in-progress";
-import type {PostBattleButtonConfig} from "../dom-floaters/post-battle/post-battle-button-config";
 
+/**
+ * 戦闘画面のアニメーションタイムスケールを設定に反映する
+ *
+ * @param props ゲームプロパティ
+ * @param animationTimeScale 反映するタイムスケール
+ */
 const saveAnimationTimeScale = async (props: $ReadOnly<GameProps>, animationTimeScale: number) => {
   const battleAnimationTimeScale = parseBattleAnimationTimeScale(animationTimeScale) ?? BattleAnimationTimeScales[0];
   const origin = await props.config.load();
@@ -26,12 +25,25 @@ const saveAnimationTimeScale = async (props: $ReadOnly<GameProps>, animationTime
   await props.config.save(update);
 };
 
+/**
+ * カジュアルマッチが終了した際の処理
+ *
+ * @param props ゲームプロパティ
+ * @return 処理が完了したら発火するPromise
+ */
 const endCasualMatch = async (props: $ReadOnly<GameProps>) => {
   props.suddenlyBattleEnd.unbind();
   await props.api.disconnectWebsocket();
   await props.domFloaters.showPostBattle(props.resources, PostNetworkBattleButtons);
 };
 
+/**
+ * NPCバトルステージが終了した際の処理
+ * 
+ * @param props ゲームプロパティ
+ * @param postBattleButtons 戦闘終了後アクションボタン設定
+ * @return 処理が完了したら発火するPromise
+ */
 const endNPCBattleStage = async (props: $ReadOnly<GameProps>, postBattleButtons: PostBattleButtonConfig[]) => {
   await props.domFloaters.showPostBattle(props.resources, postBattleButtons);
 };
@@ -65,7 +77,6 @@ export async function onEndBattle(props: GameProps, action: EndBattle): Promise<
     })();
     return {updatedInProgress, postBattleButtons};
   })(props);
-
   await saveAnimationTimeScale(props, action.animationTimeScale);
   if (npcBattle) {
     props.inProgress = npcBattle.updatedInProgress;
