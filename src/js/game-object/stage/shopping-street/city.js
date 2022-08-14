e768a41715d90babb6d5a0d772ab74e333e22837
kaidouji85 <kaidouji85@gmail.com>
2022-08-14 14:57:20 +0900

    背景モデルをprivate field対応させた

--- src/js/game-object/stage/shopping-street/city.js
+++ src/js/game-object/stage/shopping-street/city.js
@@ -6,20 +6,20 @@ import {disposeGltfModel, GLTF_IDS} from "../../../resource/gltf";
 
 /** 商店街 */
 export class City {
-  _gltf: GlTFResource;
+  #gltf: GlTFResource;
 
   constructor(resources: Resources) {
     const resource = resources.gltfs.find(v => v.id === GLTF_IDS.SHOPPING_STREET);
-    this._gltf = resource ? resource : {
+    this.#gltf = resource ? resource : {
       id: "",
       object: new THREE.Scene()
     };
-    this._gltf.object.scale.set(100, 100, 100);
+    this.#gltf.object.scale.set(100, 100, 100);
   }
 
   /** デストラクタ */
   destructor(): void {
-    disposeGltfModel(this._gltf);
+    disposeGltfModel(this.#gltf);
   }
 
   /**
@@ -28,6 +28,6 @@ export class City {
    * @return シーンに追加するオブジェクト
    */
   getObject3D(): typeof THREE.Object3D {
-    return this._gltf.object;
+    return this.#gltf.object;
   }
 }
