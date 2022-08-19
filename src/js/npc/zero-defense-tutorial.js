35abfe84dd2782deea9b4633b43f0168d3fd253d
kaidouji85 <kaidouji85@gmail.com>
2022-08-19 03:03:27 +0000

    0防御チュートリアル用のNPCを追加した

---
+++ src/js/npc/zero-defense-tutorial.js
@@ -0,0 +1,40 @@
+// @flow
+import {ArmDozerIdList, ArmDozers, PilotIds, Pilots} from "gbraver-burst-core";
+import type {NPC} from "./npc";
+import type {SimpleRoutine} from "./simple-npc";
+import {SimpleNPC} from "./simple-npc";
+
+/** 0バッテリー */
+const ZERO_BATTERY = {
+  type: 'BATTERY_COMMAND',
+  battery: 0
+};
+
+/**
+ * @override
+ * 攻撃ルーチン
+ */
+const attackRoutine: SimpleRoutine = data => {
+  const maxBattery = data.commands.find(v => v.type === 'BATTERY_COMMAND' && v.battery === data.enemy.armdozer.battery);
+  return maxBattery ?? ZERO_BATTERY;
+};
+
+/**
+ * @override
+ * 防御ルーチン
+ */
+const defenseRoutine: SimpleRoutine = data => {
+  const battery3 = data.commands.find(v => v.type === 'BATTERY_COMMAND' && v.battery === 3);
+  return battery3 ?? ZERO_BATTERY;
+};
+
+/**
+ * ゼロ防御チュートリアル用ネオランドーザNPC
+ *
+ * @return NPC
+ */
+export function zeroDefenseTutorialNPC(): NPC {
+  const armdozer = ArmDozers.find(v => v.id === ArmDozerIdList.NEO_LANDOZER) ?? ArmDozers[0];
+  const pilot = Pilots.find(v => v.id === PilotIds.GAI) ?? Pilots[0];
+  return new SimpleNPC(armdozer, pilot, attackRoutine, defenseRoutine);
+}
