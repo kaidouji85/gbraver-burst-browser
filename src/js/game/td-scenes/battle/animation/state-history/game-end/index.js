c2ac1139d84dca83ecac4550a2fc76847c45bef7
kaidouji85 <kaidouji85@gmail.com>
2022-08-21 20:25:22 +0900

    ゲーム終了アニメーションの分岐をシンプルにした

--- src/js/game/td-scenes/battle/animation/state-history/game-end/index.js
+++ src/js/game/td-scenes/battle/animation/state-history/game-end/index.js
@@ -1,5 +1,5 @@
 // @flow
-import type {GameEnd, GameEndX, GameOver, GameStateX} from "gbraver-burst-core";
+import type {GameEnd, GameStateX} from "gbraver-burst-core";
 import {Animate} from "../../../../../../animation/animate";
 import {empty} from "../../../../../../animation/delay";
 import type {StateAnimationProps} from "../state-animation-props";
@@ -15,31 +15,15 @@ import {toGameOverParam} from "./game-over/game-over-param";
  * @return アニメーション
  */
 export function gameEndAnimation(props: StateAnimationProps, gameState: GameStateX<GameEnd>): Animate {
-  if (gameState.effect.result.type !== 'EvenMatch') {
+  if (gameState.effect.result.type === 'EvenMatch') {
     return evenMatchAnimation(props);
   }
 
-  const gameOver = castGameOver(gameState);
-  if (gameOver) {
-    const param = toGameOverParam(props, gameOver);
-    return param ? gameOverAnimation(param) : empty();
+  const gameOverParam = (gameState.effect.result.type === 'GameOver')
+    ? toGameOverParam(props, gameState.effect.result) : null;
+  if (gameOverParam) {
+    return gameOverAnimation(gameOverParam);
   }
-  
+
   return empty();
-}
-
-/**
- * GameOverにキャストするヘルパー関数
- * キャストできない場合はnullを返す
- *
- * @param origin キャスト元
- * @return キャスト結果
- */
-function castGameOver(origin: GameStateX<GameEnd>): ?GameStateX<GameEndX<GameOver>> {
-  if (origin.effect.result.type !== 'GameOver') {
-    return null;
-  }
-
-  const gameOver: GameOver = origin.effect.result;
-  return ((origin: any): GameStateX<GameEndX<typeof gameOver>>);
 }
