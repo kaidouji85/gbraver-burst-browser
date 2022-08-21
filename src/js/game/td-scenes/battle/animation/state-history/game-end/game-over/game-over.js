807a67d14f937ee561ff63eb50db56dc501afe4f
kaidouji85 <kaidouji85@gmail.com>
2022-08-21 20:28:59 +0900

    シンブレイバーのゲームオーバーアニメーションパラメータキャストをシンプルにした

--- src/js/game/td-scenes/battle/animation/state-history/game-end/game-over/game-over.js
+++ src/js/game/td-scenes/battle/animation/state-history/game-end/game-over/game-over.js
@@ -1,10 +1,11 @@
 // @flow
 import {Animate} from "../../../../../../../animation/animate";
 import {empty} from "../../../../../../../animation/delay";
+import {ShinBraverTD} from "../../../../view/td/armdozer-objects/shin-braver";
 import type {GameOverParam} from "./game-over-param";
 import {castLightningDozerGameOver, lightningDozerWin} from "./lightning-dozer";
 import {castNeoLandozerGameOver, neoLandozerWin} from "./neo-landozer";
-import {castShinBraverGameOver, shinBraverWin} from "./shin-braver";
+import {shinBraverWin} from "./shin-braver";
 import {castWingDozerGameOver, wingDozerWin} from "./wing-dozer";
 
 /**
@@ -14,9 +15,9 @@ import {castWingDozerGameOver, wingDozerWin} from "./wing-dozer";
  * @return アニメーション
  */
 export function gameOverAnimation(param: GameOverParam): Animate {
-  const shinBraver = castShinBraverGameOver(param);
-  if (shinBraver) {
-    return shinBraverWin(shinBraver);
+  if (param.winnerTdArmdozer instanceof ShinBraverTD) {
+    const winnerTdArmdozer: ShinBraverTD = param.winnerTdArmdozer;
+    return shinBraverWin({...param, winnerTdArmdozer});
   }
 
   const neoLandozer = castNeoLandozerGameOver(param);
--- src/js/game/td-scenes/battle/animation/state-history/game-end/game-over/shin-braver.js
+++ src/js/game/td-scenes/battle/animation/state-history/game-end/game-over/shin-braver.js
@@ -4,28 +4,12 @@ import {all} from "../../../../../../../animation/all";
 import {Animate} from "../../../../../../../animation/animate";
 import {ShinBraverTD} from "../../../../view/td/armdozer-objects/shin-braver";
 import {dolly, track} from "../../../td-camera";
-import type {GameOverParam, GameOverParamX} from "./game-over-param";
+import type {GameOverParamX} from "./game-over-param";
 
 /**
  * シンブレイバー ゲームオーバ
  */
 export type ShinBraverGameOver = GameOverParamX<ShinBraverTD>;
-
-/**
- * シンブレイバーゲームオーバにキャストする
- * キャストできない場合はnullを返す
- *
- * @param origin キャスト元
- * @return キャスト結果
- */
-export function castShinBraverGameOver(origin: GameOverParam): ?ShinBraverGameOver {
-  if (origin.winnerTdArmdozer instanceof ShinBraverTD) {
-    const td: ShinBraverTD = origin.winnerTdArmdozer;
-    return ((origin: any): GameOverParamX<typeof td>);
-  }
-
-  return null;
-}
 
 /**
  * シンブレイバー 勝利
