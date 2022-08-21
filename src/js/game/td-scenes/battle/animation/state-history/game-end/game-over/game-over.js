d052b147a93cec846814bd7a23077882c2ee2b4a
kaidouji85 <kaidouji85@gmail.com>
2022-08-21 20:37:06 +0900

    ライトニングドーザのゲームオーバーアニメーションパラメータキャストをシンプルにした

--- src/js/game/td-scenes/battle/animation/state-history/game-end/game-over/game-over.js
+++ src/js/game/td-scenes/battle/animation/state-history/game-end/game-over/game-over.js
@@ -1,10 +1,11 @@
 // @flow
 import {Animate} from "../../../../../../../animation/animate";
 import {empty} from "../../../../../../../animation/delay";
+import {LightningDozerTD} from "../../../../view/td/armdozer-objects/lightning-dozer";
 import {NeoLandozerTD} from "../../../../view/td/armdozer-objects/neo-landozer";
 import {ShinBraverTD} from "../../../../view/td/armdozer-objects/shin-braver";
 import type {GameOverParam} from "./game-over-param";
-import {castLightningDozerGameOver, lightningDozerWin} from "./lightning-dozer";
+import {lightningDozerWin} from "./lightning-dozer";
 import {neoLandozerWin} from "./neo-landozer";
 import {shinBraverWin} from "./shin-braver";
 import {castWingDozerGameOver, wingDozerWin} from "./wing-dozer";
@@ -26,9 +27,9 @@ export function gameOverAnimation(param: GameOverParam): Animate {
     return neoLandozerWin({...param, winnerTdArmdozer});
   }
 
-  const lightningDozer = castLightningDozerGameOver(param);
-  if (lightningDozer) {
-    return lightningDozerWin(lightningDozer);
+  if (param.winnerTdArmdozer instanceof LightningDozerTD) {
+    const winnerTdArmdozer: LightningDozerTD = param.winnerTdArmdozer;
+    return lightningDozerWin({...param, winnerTdArmdozer});
   }
 
   const wingDozer = castWingDozerGameOver(param);
