ca83090f72f6d47e74c1db06d8c7609951667aa8
kaidouji85 <kaidouji85@gmail.com>
2022-09-02 06:19:47 +0000

    冒頭の打ち込みをした

--- src/js/custom-battle-events/burst-tutorial.js
+++ src/js/custom-battle-events/burst-tutorial.js
@@ -5,10 +5,16 @@ import type {
   LastState,
 } from "../game/td-scenes/battle/custom-battle-event";
 import type {GameState} from "gbraver-burst-core/lib/state/game-state";
-import {activeLeftMessageWindowWithFace} from "./active-message-window";
+import {
+  activeLeftMessageWindowWithFace,
+  activeRightMessageWindowWithFace,
+} from "./active-message-window";
 import {EmptyCustomBattleEvent} from "./empty-custom-battle-event";
-import {invisibleAllMessageWindows} from "./invisible-all-message-windows";
-import {scrollLeftMessages} from "./scroll-messages";
+import {
+  invisibleAllMessageWindows,
+  refreshConversation,
+} from "./invisible-all-message-windows";
+import {scrollLeftMessages, scrollRightMessages} from "./scroll-messages";
 
 /**
  * ストーリー 冒頭
@@ -18,9 +24,50 @@ import {scrollLeftMessages} from "./scroll-messages";
 const introduction = async (props: CustomBattleEventProps) => {
   activeLeftMessageWindowWithFace(props, 'Raito');
   await scrollLeftMessages(props, [
-    ['ライト', '「冒頭」'],
+    ['ライト', '「さすがは大田高校はん 一瞬で勝負がついてしもたな'],
+    ['どや まだ道路の占有時間も残っとるし ワイともう一戦やりまへんか']
   ]);
   props.view.dom.leftMessageWindow.darken();
+
+  activeRightMessageWindowWithFace(props, 'Tsubasa');
+  await scrollRightMessages(props, [
+    ['ツバサ', '「相談するから 少し待ってくれ」'],
+  ]);
+  await refreshConversation(props);
+
+  activeRightMessageWindowWithFace(props, 'Tsubasa');
+  await scrollRightMessages(props, [
+    ['ツバサ', '「監督からもGoサインが出た'],
+    ['シンヤ 悪いがもう一戦だけ頑張ってくれ']
+  ]);
+  await refreshConversation(props, 100);
+
+  activeRightMessageWindowWithFace(props, 'Shinya');
+  await scrollRightMessages(props, [
+    ['シンヤ', '「了解ッス'],
+  ]);
+  props.view.dom.rightMessageWindow.darken();
+
+  activeLeftMessageWindowWithFace(props, 'Raito');
+  await scrollLeftMessages(props, [
+    ['ライト', '「ほないくで 大田高校のエース君」']
+  ]);
+  await refreshConversation(props, 100);
+
+  activeLeftMessageWindowWithFace(props, 'Gai');
+  await scrollLeftMessages(props, [
+    ['ガイ', '「双方 姿勢を正して 礼!!」'],
+  ]);
+  await refreshConversation(props, 100);
+
+  activeLeftMessageWindowWithFace(props, 'Raito');
+  props.view.dom.leftMessageWindow.messages(['ライト', '「よろしくお願いします」']);
+  props.view.dom.leftMessageWindow.scrollUp();
+  activeRightMessageWindowWithFace(props, 'Shinya');
+  await scrollRightMessages(props, [
+    ['シンヤ', '「よろしくお願いします」']
+  ]);
+
   invisibleAllMessageWindows(props);
 };
 
