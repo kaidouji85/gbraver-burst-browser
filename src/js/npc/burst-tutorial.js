1309d5a2ad3941ead581d373a8426351259194ac
kaidouji85 <kaidouji85@gmail.com>
2022-09-02 04:33:28 +0000

    バーストチュートリアルのNPCを実装した

--- src/js/npc/burst-tutorial.js
+++ src/js/npc/burst-tutorial.js
@@ -5,27 +5,19 @@ import type {SimpleRoutine} from "./simple-npc";
 import {SimpleNPC} from "./simple-npc";
 
 /** 0バッテリー */
-const ZERO_BATTERY = {
-  type: 'BATTERY_COMMAND',
-  battery: 0
-};
+const ZERO_BATTERY = {type: 'BATTERY_COMMAND', battery: 0};
 
-/**
- * @override
- * 攻撃ルーチン
- */
+/** @override 攻撃ルーチン */
 const attackRoutine: SimpleRoutine = data => {
-  const maxBattery = data.commands.find(v => v.type === 'BATTERY_COMMAND' && v.battery === data.enemy.armdozer.battery);
-  return maxBattery ?? ZERO_BATTERY;
+  const battery5 = data.commands.find(v => v.type === 'BATTERY_COMMAND' && v.battery === 5);
+  return battery5 ?? ZERO_BATTERY;
 };
 
-/**
- * @override
- * 防御ルーチン
- */
+/** @override 防御ルーチン*/
 const defenseRoutine: SimpleRoutine = data => {
-  const battery3 = data.commands.find(v => v.type === 'BATTERY_COMMAND' && v.battery === 3);
-  return battery3 ?? ZERO_BATTERY;
+  const burst = data.commands.find(v => v.type === 'BURST_COMMAND');
+  const battery1 = data.commands.find(v => v.type === 'BATTERY_COMMAND' && v.battery === 1);
+  return burst ?? battery1 ?? ZERO_BATTERY;
 };
 
 /**
