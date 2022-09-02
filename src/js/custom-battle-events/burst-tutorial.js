37ff6c12d8bd235e23fc967a4d45f59f47a8262d
kaidouji85 <kaidouji85@gmail.com>
2022-09-02 05:37:30 +0000

    冒頭イベントの雛形を実装した

--- src/js/custom-battle-events/burst-tutorial.js
+++ src/js/custom-battle-events/burst-tutorial.js
@@ -1,6 +1,53 @@
 // @flow
-import type {CustomBattleEvent} from "../game/td-scenes/battle/custom-battle-event";
+import type {
+  CustomBattleEvent,
+  CustomBattleEventProps,
+  LastState,
+} from "../game/td-scenes/battle/custom-battle-event";
+import type {GameState} from "gbraver-burst-core/lib/state/game-state";
+import {activeLeftMessageWindowWithFace} from "./active-message-window";
 import {EmptyCustomBattleEvent} from "./empty-custom-battle-event";
+import {invisibleAllMessageWindows} from "./invisible-all-message-windows";
+import {scrollLeftMessages} from "./scroll-messages";
+
+/**
+ * ストーリー 冒頭
+ * @param props イベントプロパティ
+ * @return ストーリーが完了したら発火するPromise
+ */
+const introduction = async (props: CustomBattleEventProps) => {
+  activeLeftMessageWindowWithFace(props, 'Raito');
+  await scrollLeftMessages(props, [
+    ['ライト', '「冒頭」'],
+  ]);
+  props.view.dom.leftMessageWindow.darken();
+  invisibleAllMessageWindows(props);
+};
+
+/** バーストチュートリアル用のカスタムバトルイベント */
+class BurstTutorial extends EmptyCustomBattleEvent {
+  /** ステートヒストリー、 beforeLastState開始時に更新される */
+  stateHistory: GameState[];
+  /** イントロダクションを再生したか、trueで再生した */
+  isIntroductionComplete: boolean;
+  /**
+   * コンストラクタ
+   */
+  constructor() {
+    super();
+    this.stateHistory = [];
+    this.isIntroductionComplete = false;
+  }
+
+  /** @override */
+  async beforeLastState(props: LastState): Promise<void> {
+    this.stateHistory = [...this.stateHistory, ...props.update];
+    if (!this.isIntroductionComplete) {
+      await introduction(props);
+      this.isIntroductionComplete = true;
+    }
+  }
+}
 
 /**
  * バーストチュートリアル用のカスタムバトルイベントを生成する
@@ -8,5 +55,5 @@ import {EmptyCustomBattleEvent} from "./empty-custom-battle-event";
  * @return 生成したカスタムバトルイベント
  */
 export function createBurstTutorialEvent(): CustomBattleEvent {
-  return new EmptyCustomBattleEvent();
+  return new BurstTutorial();
 }
