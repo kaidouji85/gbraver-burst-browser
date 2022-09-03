c3575338dac42fe119703a750ca8715a6bcc6bd5
kaidouji85 <kaidouji85@gmail.com>
2022-09-03 22:53:26 +0900

    負けストーリーの中身を書いた

--- src/js/custom-battle-events/burst-tutorial.js
+++ src/js/custom-battle-events/burst-tutorial.js
@@ -271,6 +271,35 @@ const playerLose = async (props: CustomBattleEventProps) => {
     ['ガイ', '「やめ!!'],
     ['この勝負 台東高校の勝ち!!']
   ]);
+
+  activeLeftMessageWindowWithFace(props, 'Raito');
+  await scrollLeftMessages(props, [
+    ['ライト', '「どうや大田高校 これが台東高校の実力や」'],
+  ]);
+  props.view.dom.leftMessageWindow.darken();
+
+  activeRightMessageWindowWithFace(props, 'Shinya');
+  await scrollRightMessages(props, [
+    ['シンヤ', '「……これが上級生の力」'],
+  ]);
+  await refreshConversation(props);
+
+  activeLeftMessageWindowWithFace(props, 'Gai');
+  await scrollLeftMessages(props, [
+    ['ガイ', '「双方 姿勢を正して 礼!!」'],
+  ]);
+  await refreshConversation(props, 100);
+
+  activeLeftMessageWindowWithFace(props, 'Raito');
+  props.view.dom.leftMessageWindow.messages(
+    ['ライト', '「ありがとうございました」']
+  );
+  props.view.dom.leftMessageWindow.scrollUp();
+  activeRightMessageWindowWithFace(props, 'Shinya');
+  await scrollRightMessages(props, [
+    ['シンヤ', '「ありがとうございました」']
+  ]);
+  invisibleAllMessageWindows(props);
 };
 
 /** バースト注釈 */
