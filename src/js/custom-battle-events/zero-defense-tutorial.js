28caa4e898a4406d092e0320153353795ebbbcbc
kaidouji85 <kaidouji85@gmail.com>
2022-09-09 10:09:39 +0000

    導入ストーリーを大幅修正

--- src/js/custom-battle-events/zero-defense-tutorial.js
+++ src/js/custom-battle-events/zero-defense-tutorial.js
@@ -22,9 +22,23 @@ import {scrollLeftMessages, scrollRightMessages} from "./scroll-messages";
  * @return ストーリーが完了したら発火するPromise
  */
 const introduction = async (props: CustomBattleEventProps) => {
+  activeRightMessageWindowWithFace(props, 'Shinya');
+  await scrollRightMessages(props, [
+    ['シンヤ', '「俺 都立大田高校のシンヤ ッス'],
+    ['同じ一年生同士 よろしく頼むッス」']
+  ]);
+  props.view.dom.rightMessageWindow.darken();
+
+  activeLeftMessageWindowWithFace(props, 'Gai');
+  await scrollLeftMessages(props, [
+    ['ガイ', '「都立台東高校のガイだ'],
+    ['強豪校だと期待していたが こんな締まりのない奴が出てくるとはな」'],
+  ]);
+  props.view.dom.leftMessageWindow.darken();
+
   activeRightMessageWindowWithFace(props, 'Tsubasa');
   await scrollRightMessages(props, [
-    ['ツバサ', '「これより台東高校 大田高校の合同練習試合を始める'],
+    ['ツバサ', '「間もなく 台東高校 大田高校の合同練習試合を開始する'],
     ['一同 姿勢を正して 礼!!」']
   ]);
   props.view.dom.leftMessageWindow.darken();
@@ -39,20 +53,6 @@ const introduction = async (props: CustomBattleEventProps) => {
   await scrollRightMessages(props, [
     ['シンヤ', '「よろしくお願いします」']
   ]);
-  await refreshConversation(props, 100);
-
-  activeRightMessageWindowWithFace(props, 'Tsubasa');
-  await scrollRightMessages(props, [
-    ['ツバサ', '「相手は台東高校 期待のルーキーだ'],
-    ['シンヤ 油断は禁物だぞ」']
-  ]);
-  await refreshConversation(props, 100);
-
-  activeRightMessageWindowWithFace(props, 'Shinya');
-  await scrollRightMessages(props, [
-    ['シンヤ', '「了解ッス'],
-    ['大田高校の底力を見せてやるッス」']
-  ]);
   invisibleAllMessageWindows(props);
 };
 
@@ -66,7 +66,7 @@ const playerAttackGuard = async (props: CustomBattleEventProps) => {
   await scrollRightMessages(props, [
     ['シンヤ', '「よし 当たったッス」']
   ]);
-  await refreshConversation(props, 100);
+  props.view.dom.rightMessageWindow.darken();
 
   activeLeftMessageWindowWithFace(props, 'Gai');
   await scrollLeftMessages(props, [
