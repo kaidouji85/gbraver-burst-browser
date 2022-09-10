219f4418272b9571f9021cdba1452ba895099346
kaidouji85 <kaidouji85@gmail.com>
2022-09-10 14:19:36 +0900

    ストーリー終了でツバサ先輩も礼するようにした

--- src/js/custom-battle-events/battery-system-tutorial.js
+++ src/js/custom-battle-events/battery-system-tutorial.js
@@ -46,12 +46,12 @@ const introduction = async (props: CustomBattleEventProps) => {
   await scrollRightMessages(props, [
     ['シンヤ', '「よろしくお願いします」']
   ]);
-  await refreshConversation(props);
+  await refreshConversation(props, 100);
 
   activeLeftMessageWindowWithFace(props, 'Tsubasa');
   await scrollLeftMessages(props, [
-    ['ツバサ', '「では早速はじめよう'],
-    ['攻撃側 防御側でのバッテリーの出し合いが試合の基本だ'],
+    ['ツバサ', '「いい返事だな では早速はじめよう'],
+    ['試合の基本は攻撃側 防御側でのバッテリーの出し合いだ'],
     ['大きいバッテリーを出した側の行動が成功するのだが これは実際にやってみた方が早いな'],
     ['シンヤ 私が防御に回るから 好きなように攻撃してくれ」']
   ]);
@@ -468,13 +468,18 @@ const tutorialEnd = async (props: CustomBattleEventProps) => {
     ['ツバサ', '「これにて操縦訓練を終了する'],
     ['姿勢を正して 礼!!」」']
   ]);
-  props.view.dom.leftMessageWindow.darken();
+  await refreshConversation(props);
 
+  activeLeftMessageWindowWithFace(props, 'Tsubasa');
+  props.view.dom.leftMessageWindow.messages(
+    ['ツバサ', '「ありがとうございました」']
+  );
+  props.view.dom.leftMessageWindow.scrollUp();
   activeRightMessageWindowWithFace(props, 'Shinya');
   await scrollRightMessages(props, [
     ['シンヤ', '「ありがとうございました」']
   ]);
-  props.view.dom.rightMessageWindow.darken();
+  invisibleAllMessageWindows(props);
 };
 
 /** 攻撃バッテリー注釈 */
