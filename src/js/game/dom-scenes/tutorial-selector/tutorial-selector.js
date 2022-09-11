5b1f86735a62e31b32749cbc191780e8732830dd
kaidouji85 <kaidouji85@gmail.com>
2022-09-11 13:00:46 +0900

    チュートリアルセレクタcssの雛形を実装した

--- src/js/game/dom-scenes/tutorial-selector/tutorial-selector.js
+++ src/js/game/dom-scenes/tutorial-selector/tutorial-selector.js
@@ -4,7 +4,23 @@ import type {DOMScene} from "../dom-scene";
 /** ROOT要素class属性*/
 const ROOT_CLASS = 'tutorial-selector';
 
-/** チュートリアルステージセレクト */
+/**
+ * ルート要素のinnerHTML
+ * @return innerHTML
+ */
+export function rootInnerHTML(): string {
+  return `
+    <div class="${ROOT_CLASS}__title">チュートリアル</div>
+    <div class="${ROOT_CLASS}__stages">
+      <button class="${ROOT_CLASS}__stage-button">1</button>
+      <button class="${ROOT_CLASS}__stage-button">2</button>
+      <button class="${ROOT_CLASS}__stage-button">3</button>
+    </div>
+    <button class="${ROOT_CLASS}__prev">戻る</button>
+  `;
+}
+
+/** チュートリアルステージセレクト画面 */
 export class TutorialSelector implements DOMScene {
   root: HTMLElement;
 
@@ -14,7 +30,7 @@ export class TutorialSelector implements DOMScene {
   constructor() {
     this.root = document.createElement('div');
     this.root.className = ROOT_CLASS;
-    this.root.innerText = 'tutorial selector';
+    this.root.innerHTML = rootInnerHTML();
   }
 
 
