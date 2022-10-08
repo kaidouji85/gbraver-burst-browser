db00166f38a2c1cd1df0bde77d856b570218c393
kaidouji85 <kaidouji85@gmail.com>
2022-10-08 18:05:03 +0900

    captionsの変数定義を改行で区切った

--- src/js/custom-battle-events/battery-system-tutorial/captions.js
+++ src/js/custom-battle-events/battery-system-tutorial/captions.js
@@ -5,16 +5,19 @@ export const attackBatteryCaption = [
   '好きなバッテリーで攻撃してみよう',
   'ツバサ先輩よりも大きい数字を出せば攻撃が当たるぞ'
 ];
+
 /** 防御バッテリー注釈 */
 export const defenseBatteryCaption = [
   '好きなバッテリーで防御してみよう',
   'ツバサ先輩よりも大きい数字を出せば攻撃を回避できるぞ'
 ];
+
 /** バースト注釈 */
 export const burstCaption = [
   'このまま0防御すると負け確定だ',
   'バーストでバッテリーを回復させよう'
 ];
+
 /** パイロットスキル注釈 */
 export const pilotSkillCaption = [
   'このまま0防御すると負け確定だ',
