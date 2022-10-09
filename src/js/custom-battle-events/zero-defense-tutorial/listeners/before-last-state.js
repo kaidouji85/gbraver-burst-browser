3388e90e44fb8b82bcc291c254832c67793d6e5f
kaidouji85 <kaidouji85@gmail.com>
2022-10-09 17:54:13 +0900

    beforeLastStateをリファクタリングした

--- src/js/custom-battle-events/zero-defense-tutorial/listeners/before-last-state.js
+++ src/js/custom-battle-events/zero-defense-tutorial/listeners/before-last-state.js
@@ -8,6 +8,51 @@ import {introduction} from "../stories/introduction";
 import {zeroBatteryChance} from "../stories/zero-battery-chance";
 
 /**
+ * ダメージレースストーリーに必要となる情報を抽出する
+ * 抽出できない場合、nullを返す
+ *
+ * @param props イベントプロパティ
+ * @return 抽出結果
+ */
+function extractDamageRace(props: $ReadOnly<LastState>) {
+  const extractedBattle = extractBattle(props.update);
+  if (!extractedBattle) {
+    return null;
+  }
+
+  const battle: GameStateX<Battle> = extractedBattle;
+  const player = battle.players.find(v => v.playerId === props.playerId);
+  const enemy = battle.players.find(v => v.playerId !== props.playerId);
+  if (!player || !enemy) {
+    return null;
+  }
+
+  return battle.effect.attacker !== props.playerId ? {player, enemy} : null;
+}
+
+/**
+ * 0バッテリーチャンスストーリーを再生するべきかを判定する
+ *
+ * @param props イベントプロパティ
+ * @return 判定結果、trueで再生するべき
+ */
+function isZeroBatteryChance(props: $ReadOnly<LastState>): boolean {
+  const foundLastState = props.update[props.update.length - 1];
+  if (!foundLastState) {
+    return false;
+  }
+
+  const lastState: GameState = foundLastState;
+  const enemy = lastState.players.find(v => v.playerId !== props.playerId);
+  if (!enemy) {
+    return false;
+  }
+
+  const isPlayerTurn = lastState.activePlayerId === props.playerId;
+  return isPlayerTurn && (enemy.armdozer.battery === 0) && (0 < enemy.armdozer.hp);
+}
+
+/**
  * 最終ステート直前イベント
  *
  * @param props イベントプロパティ
@@ -22,29 +67,19 @@ export async function beforeLastState(props: $ReadOnly<LastState>, state: ZeroDe
   }
 
   const extractedGameEnd = extractGameEnd(props.update);
-  const extractedBattle = extractBattle(props.update);
-  if (extractedBattle && !extractedGameEnd) {
-    const battle: GameStateX<Battle> = extractedBattle;
-    const player = battle.players.find(v => v.playerId === props.playerId);
-    const enemy = battle.players.find(v => v.playerId !== props.playerId);
-    if (player && enemy) {
-      const isEnemyAttack = battle.effect.attacker !== props.playerId;
-      if (isEnemyAttack) {
-        await damageRace(props, player.armdozer.hp, enemy.armdozer.hp);
-        return {...updatedStateHistory, isDamageRaceComplete: true};
-      }
-    }
+  if (extractedGameEnd) {
+    return updatedStateHistory;
   }
 
-  const foundLastState = props.update[props.update.length - 1];
-  if (foundLastState) {
-    const lastState: GameState = foundLastState;
-    const isPlayerTurn = lastState.activePlayerId === props.playerId;
-    const enemy = lastState.players.find(v => v.playerId !== props.playerId);
-    if (enemy && isPlayerTurn && (enemy.armdozer.battery === 0) && (0 < enemy.armdozer.hp)) {
-      await zeroBatteryChance(props);
-    }
+  const extractedDamageRace = extractDamageRace(props);
+  if (extractedDamageRace && !state.isDamageRaceComplete) {
+    const {player, enemy} = extractedDamageRace
+    await damageRace(props, player.armdozer.hp, enemy.armdozer.hp);
   }
 
-  return updatedStateHistory;
+  if (isZeroBatteryChance(props)) {
+    await zeroBatteryChance(props);
+  }
+
+  return {...updatedStateHistory, isDamageRaceComplete: !!extractedDamageRace}
 }
