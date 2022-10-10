016a6dbea402633386197f85f3568c940b663201
kaidouji85 <kaidouji85@gmail.com>
2022-10-10 16:34:48 +0900

    onBatteryCommandSelectedを別ファイルに切り出した

--- src/js/custom-battle-events/burst-tutorial/index.js
+++ src/js/custom-battle-events/burst-tutorial/index.js
@@ -4,24 +4,16 @@ import type {
   BurstCommandSelected,
   CommandCanceled,
   CustomBattleEvent,
-  CustomBattleEventProps,
   LastState,
   PilotSkillCommandSelected,
 } from "../../game/td-scenes/battle/custom-battle-event";
 import {EmptyCustomBattleEvent} from "../empty-custom-battle-event";
-import {focusInBurstButton, focusInPilotButton, focusOutBurstButton, focusOutPilotButton} from "../focus";
-import {shouldBurst, shouldPilotSkill} from "./captions";
+import {focusOutBurstButton, focusOutPilotButton} from "../focus";
 import {beforeLastState} from "./listeners/before-last-state";
+import {onBatteryCommandSelected} from "./listeners/on-battery-command-selected";
 import type {BurstTutorialState, SelectableCommands} from "./state";
-import {canNotChangeBattery} from "./stories/can-not-change-battery";
-import {doBurstToRecoverBattery} from "./stories/do-burst-to-recover-battery";
-import {doPilotSkillToRecoverBattery} from "./stories/do-pilot-skill-to-recover-battery";
-import {notDefense5Carelessly} from "./stories/not-defense5-carelessly";
 import {playerLose} from "./stories/player-lose";
 import {playerWin} from "./stories/player-win";
-import {redoBatterySelect} from "./stories/redo-battery-select";
-import {shouldDefense5} from "./stories/should-defense5";
-import {shouldDefense5Again} from "./stories/should-defense5-again";
 
 /** バーストチュートリアル用のカスタムバトルイベント */
 class BurstTutorial extends EmptyCustomBattleEvent {
@@ -48,61 +40,9 @@ class BurstTutorial extends EmptyCustomBattleEvent {
 
   /** @override */
   async onBatteryCommandSelected(props: BatteryCommandSelected): Promise<CommandCanceled> {
-    const enableBurstCommand: SelectableCommands[] = ['All'];
-    if (!enableBurstCommand.includes(this.state.selectableCommands)) {
-      return {isCommandCanceled: true};
-    }
-
-    const foundLastState = this.state.stateHistory[this.state.stateHistory.length - 1];
-    const latestPlayer = (foundLastState?.players ?? []).find(v => v.playerId === props.playerId);
-    const latestEnemy = (foundLastState?.players ?? []).find(v => v.playerId !== props.playerId);
-    const lastState = foundLastState && latestPlayer && latestEnemy
-      ? {isEnemyTurn: foundLastState.activePlayerId !== props.playerId,
-        isPlayerFullBattery: latestPlayer.armdozer.battery === 5,
-        isEnemyFullBattery: latestEnemy.armdozer.battery === 5,
-        isHpLessThanEnemyPower: latestPlayer.armdozer.hp <= latestEnemy.armdozer.power,
-        enableBurst: latestPlayer.armdozer.enableBurst,
-        enablePilotSkill: latestPlayer.pilot.enableSkill}
-      : null;
-    const notBattery5 = props.battery.battery !== 5;
-    const defense5 = async (props: CustomBattleEventProps) => {
-      this.state.isLoseIfNoDefense5Complete ? await shouldDefense5Again(props) : await shouldDefense5(props);
-    };
-    if (notBattery5 && lastState && lastState.isEnemyTurn && lastState.isHpLessThanEnemyPower && lastState.isEnemyFullBattery
-      && !lastState.isPlayerFullBattery && lastState.enableBurst)
-    {
-      await defense5(props);
-      this.state.isLoseIfNoDefense5Complete = true;
-      await doBurstToRecoverBattery(props);
-      await focusInBurstButton(props, shouldBurst);
-      this.state.selectableCommands = 'BurstOnly';
-      return {isCommandCanceled: true};
-    } else if (notBattery5 && lastState && lastState.isEnemyTurn && lastState.isHpLessThanEnemyPower && lastState.isEnemyFullBattery
-      && !lastState.isPlayerFullBattery && !lastState.enableBurst && lastState.enablePilotSkill)
-    {
-      await defense5(props);
-      this.state.isLoseIfNoDefense5Complete = true;
-      await doPilotSkillToRecoverBattery(props);
-      await focusInPilotButton(props, shouldPilotSkill);
-      this.state.selectableCommands = 'PilotSkillOnly';
-      return {isCommandCanceled: true};
-    } else if (notBattery5 && lastState && lastState.isEnemyTurn && lastState.isHpLessThanEnemyPower && lastState.isEnemyFullBattery
-      && !lastState.isPlayerFullBattery && !lastState.enableBurst && !lastState.enablePilotSkill)
-    {
-      await defense5(props);
-      this.state.isLoseIfNoDefense5Complete = true;
-      await canNotChangeBattery(props);
-      return {isCommandCanceled: false};
-    } else if (notBattery5 && lastState && lastState.isEnemyTurn && lastState.isHpLessThanEnemyPower && lastState.isEnemyFullBattery
-      && lastState.isPlayerFullBattery)
-    {
-      await defense5(props);
-      this.state.isLoseIfNoDefense5Complete ? await notDefense5Carelessly(props) : await redoBatterySelect(props);
-      this.state.isLoseIfNoDefense5Complete = true;
-      return {isCommandCanceled: true};
-    }
-
-    return {isCommandCanceled: false};
+    const {state, cancel} = await onBatteryCommandSelected(props, this.state);
+    this.state = state;
+    return cancel;
   }
 
   /** @override */
