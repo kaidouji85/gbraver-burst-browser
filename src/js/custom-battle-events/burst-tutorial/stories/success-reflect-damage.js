b7754faf2b912319db56a12838a6bb21f1fb3f1d
kaidouji85 <kaidouji85@gmail.com>
2022-10-10 14:35:32 +0900

    ストーリーを別ファイルに切り出した

---
+++ src/js/custom-battle-events/burst-tutorial/stories/success-reflect-damage.js
@@ -0,0 +1,19 @@
+// @flow
+import type {CustomBattleEventProps} from "../../../game/td-scenes/battle/custom-battle-event";
+import {activeLeftMessageWindowWithFace} from "../../active-message-window";
+import {invisibleAllMessageWindows} from "../../invisible-all-message-windows";
+import {scrollLeftMessages} from "../../scroll-messages";
+
+/**
+ * ストーリー ダメージ反射成功
+ * @param props イベントプロパティ
+ * @return ストーリーが完了したら発火するPromise
+ */
+export const successReflectDamage = async (props: CustomBattleEventProps) => {
+  activeLeftMessageWindowWithFace(props, 'Raito');
+  await scrollLeftMessages(props, [
+    ['ライト', '「かかったな大田高校'],
+    ['これぞ奥義 電撃バリアや」']
+  ]);
+  invisibleAllMessageWindows(props);
+};
