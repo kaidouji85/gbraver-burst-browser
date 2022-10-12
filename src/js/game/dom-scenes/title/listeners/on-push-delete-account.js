eca9b0a6a7236f27b819bdb99d1da97bb7fa3f45
kaidouji85 <kaidouji85@gmail.com>
2022-10-12 06:45:39 +0000

    jsdoc引数漏れ対応

--- src/js/game/dom-scenes/title/listeners/on-push-delete-account.js
+++ src/js/game/dom-scenes/title/listeners/on-push-delete-account.js
@@ -5,6 +5,7 @@ import type {TitleProps} from "../props";
 /**
  * アカウント削除を押した時の処理
  * 
+ * @param props 画面プロパティ
  * @param action アクション
  */
 export function onPushDeleteAccount(props: TitleProps, action: $ReadOnly<PushDOM>): void {
