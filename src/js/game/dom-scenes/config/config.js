1b9de07c5055d1efa456615bdaf44832cc0a0e44
kaidouji85 <kaidouji85@gmail.com>
2022-10-13 23:34:07 +0900

    イベントリスナバインド処理を別ファイルに切り出した

--- src/js/game/dom-scenes/config/config.js
+++ src/js/game/dom-scenes/config/config.js
@@ -1,16 +1,9 @@
 // @flow
-import {inputDOMStream, pushDOMStream} from "../../../dom/event-stream";
 import type {Resources} from "../../../resource";
 import type {Stream, Unsubscriber} from "../../../stream/stream";
 import type {GbraverBurstBrowserConfig} from "../../config/browser-config";
 import type {DOMScene} from "../dom-scene";
-import {onAcceptConfigChange} from "./listeners/on-accept-config-change";
-import {onBGMVolumeChange} from "./listeners/on-bgm-volume-change";
-import {onConfigChangeButtonPush} from "./listeners/on-config-change-button-push";
-import {onDialogClose} from "./listeners/on-dialog-close";
-import {onDiscardConfigChange} from "./listeners/on-discard-config-change";
-import {onPrevButtonPush} from "./listeners/on-prev-button-push";
-import {onSEVolumeChange} from "./listeners/on-se-volume-change";
+import {bindEventListeners} from "./listeners";
 import type {ConfigProps} from "./props";
 import {createConfigProps} from "./props";
 
@@ -27,29 +20,7 @@ export class Config implements DOMScene {
    */
   constructor(resources: Resources, config: GbraverBurstBrowserConfig) {
     this.#props = createConfigProps(resources, config);
-    this.#unsubscriber = [
-      inputDOMStream(this.#props.bgmVolumeSelector).subscribe(action => {
-        onBGMVolumeChange(this.#props, action);
-      }),
-      inputDOMStream(this.#props.seVolumeSelector).subscribe(action => {
-        onSEVolumeChange(this.#props, action);
-      }),
-      pushDOMStream(this.#props.prevButton).subscribe(action => {
-        onPrevButtonPush(this.#props, action);
-      }),
-      pushDOMStream(this.#props.configChangeButton).subscribe(action => {
-        onConfigChangeButtonPush(this.#props, action);
-      }),
-      this.#props.dialog.closeNotifier().subscribe(() => {
-        onDialogClose(this.#props);
-      }),
-      this.#props.dialog.discardNotifier().subscribe(() => {
-        onDiscardConfigChange(this.#props);
-      }),
-      this.#props.dialog.acceptNotifier().subscribe(() => {
-        onAcceptConfigChange(this.#props);
-      }),
-    ];
+    this.#unsubscriber = bindEventListeners(this.#props);
   }
 
   /** @override */
