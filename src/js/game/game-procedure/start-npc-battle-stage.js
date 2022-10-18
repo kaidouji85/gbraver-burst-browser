30f947e650df04ab232d538917eb8f86471c4b68
kaidouji85 <kaidouji85@gmail.com>
2022-10-18 22:56:54 +0900

    NPCバトルの戦闘シーン開始処理を書き直した

--- src/js/game/game-procedure/start-npc-battle-stage.js
+++ src/js/game/game-procedure/start-npc-battle-stage.js
@@ -6,6 +6,8 @@ import {waitAnimationFrame} from "../../wait/wait-animation-frame";
 import {waitTime} from "../../wait/wait-time";
 import type {GameProps} from "../game-props";
 import type {NPCBattleStage} from "../npc-battle";
+import {BattleScene} from "../td-scenes/battle";
+import {battleSceneConnector} from "../td-scenes/battle-scene-connector";
 
 /**
  * NPCバトルのステージを開始するヘルパー関数
@@ -26,9 +28,13 @@ export async function startNPCBattleStage(props: $ReadOnly<GameProps>, player: P
   const startNPCStageTitleTime = Date.now();
   const battleProgress = {progress: (v: Command) => Promise.resolve(npcBattle.progress(v))}
   const config = await props.config.load();
-  const battleScene = props.tdScenes.startBattle({resources: props.resources, bgm: props.bgm, playingBGM: stage.bgm,
-    pixelRatio: config.webGLPixelRatio, initialAnimationTimeScale: config.battleAnimationTimeScale ,battleProgress,
-    player: npcBattle.player, enemy: npcBattle.enemy, initialState: npcBattle.stateHistory()});
+  props.renderer.setPixelRatio(config.webGLPixelRatio);
+  const battleScene = new BattleScene({resources: props.resources, bgm: props.bgm, playingBGM: stage.bgm,
+    initialAnimationTimeScale: config.battleAnimationTimeScale ,battleProgress,
+    player: npcBattle.player, enemy: npcBattle.enemy, initialState: npcBattle.stateHistory(),
+    resize: props.resize, pushWindow: props.pushWindow, gameLoop: props.gameLoop, renderer: props.renderer
+  });
+  props.tdScenes.bind(battleScene, battleSceneConnector);
   await waitAnimationFrame();
   const latency = Date.now() - startNPCStageTitleTime;
   await waitTime(3000- latency);
