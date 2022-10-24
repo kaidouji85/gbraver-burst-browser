25e46534c826064297f2bc84ae17966d81414807
kaidouji85 <kaidouji85@gmail.com>
2022-10-24 19:02:39 +0900

    DOM系モジュールを別ファイルに切り出した

--- src/js/dom-dialogs/difficulty/difficulty-dialog.js
+++ src/js/dom-dialogs/difficulty/difficulty-dialog.js
@@ -7,144 +7,14 @@ import { pushDOMStream } from "../../dom/event-stream";
 import { Exclusive } from "../../exclusive/exclusive";
 import type { NPCBattleCourseDifficulty } from "../../game/npc-battle-courses";
 import type { Resources } from "../../resource";
-import { PathIds } from "../../resource/path";
 import { SOUND_IDS } from "../../resource/sound";
 import type { Stream, StreamSource, Unsubscriber } from "../../stream/stream";
 import { createStreamSource } from "../../stream/stream";
 import { domUuid } from "../../uuid/dom-uuid";
 import type { DOMDialog } from "../dialog";
-
-/** ルート要素 class属性 */
-const ROOT_CLASS = "difficulty";
-
-/** data-idを集めたもの */
-type DataIDs = {
-  backGround: string,
-  closer: string,
-  easy: string,
-  easyButton: string,
-  normal: string,
-  normalButton: string,
-  hard: string,
-  hardButton: string,
-  veryHard: string,
-  veryHardButton: string,
-};
-
-/**
- * ルート要素のinnerHTML
- *
- * @param resources リソース管理オブジェクト
- * @param ids data-idを集めたもの
- * @return innerHTML
- */
-function rootInnerHTML(resources: Resources, ids: DataIDs): string {
-  const closerPath =
-    resources.paths.find((v) => v.id === PathIds.CLOSER)?.path ?? "";
-  const easyIconPath =
-    resources.paths.find((v) => v.id === PathIds.NPC_COURSE_EASY_ICON)?.path ??
-    "";
-  const normalIconPath =
-    resources.paths.find((v) => v.id === PathIds.NPC_COURSE_NORMAL_ICON)
-      ?.path ?? "";
-  const hardIconPath =
-    resources.paths.find((v) => v.id === PathIds.NPC_COURSE_HARD_ICON)?.path ??
-    "";
-  const veryHardIconPath =
-    resources.paths.find((v) => v.id === PathIds.NPC_COURSE_VERY_HARD_ICON)
-      ?.path ?? "";
-  return `
-    <div class="${ROOT_CLASS}__background" data-id="${ids.backGround}"></div>
-    <div class="${ROOT_CLASS}__dialog">
-      <img class="${ROOT_CLASS}__closer" alt="閉じる" src="${closerPath}" data-id="${ids.closer}">
-      <div class="${ROOT_CLASS}__caption">難易度を選択してください</div>
-      <div class="${ROOT_CLASS}__controllers">
-        <div class="${ROOT_CLASS}__easy" data-id="${ids.easy}">
-          <img class="${ROOT_CLASS}__easy-icon" alt="易" src="${easyIconPath}">
-          <button class="${ROOT_CLASS}__easy-button" alt="easy-course" data-id="${ids.easyButton}">かんたん</button>
-        </div>
-        <div class="${ROOT_CLASS}__normal" data-id="${ids.normal}">
-          <img class="${ROOT_CLASS}__normal-icon" alt="普" src="${normalIconPath}">
-          <button class="${ROOT_CLASS}__normal-button" alt="normal-course" data-id="${ids.normalButton}">ふつう</button>
-        </div>
-        <div class="${ROOT_CLASS}__hard" data-id="${ids.hard}">
-          <img class="${ROOT_CLASS}__hard-icon" alt="難" src="${hardIconPath}">
-          <button class="${ROOT_CLASS}__hard-button" alt="hard-course" data-id="${ids.hardButton}">むずい</button>
-        </div>
-        <div class="${ROOT_CLASS}__very-hard" data-id="${ids.veryHard}">
-          <img class="${ROOT_CLASS}__very-hard-icon" alt="檄" src="${veryHardIconPath}">
-          <button class="${ROOT_CLASS}__very-hard-button" alt="very-hard-course" data-id="${ids.veryHardButton}">げきむず</button>
-        </div>
-      </div>
-    </div>  
-  `;
-}
-
-/** ルート要素の子孫要素 */
-type Elements = {
-  backGround: HTMLElement,
-  closer: HTMLElement,
-  easy: HTMLElement,
-  easyButton: HTMLElement,
-  normal: HTMLElement,
-  normalButton: HTMLElement,
-  hard: HTMLElement,
-  hardButton: HTMLElement,
-  veryHard: HTMLElement,
-  veryHardButton: HTMLElement,
-};
-
-/**
- * ルート要素から子孫要素を抽出する
- *
- * @param root ルート要素
- * @param ids data-idを集めたもの
- * @return 抽出結果
- */
-function extractElements(root: HTMLElement, ids: DataIDs): Elements {
-  const backGround =
-    root.querySelector(`[data-id="${ids.backGround}"]`) ??
-    document.createElement("div");
-  const closer =
-    root.querySelector(`[data-id="${ids.closer}"]`) ??
-    document.createElement("div");
-  const easy =
-    root.querySelector(`[data-id="${ids.easy}"]`) ??
-    document.createElement("div");
-  const easyButton =
-    root.querySelector(`[data-id="${ids.easyButton}"]`) ??
-    document.createElement("div");
-  const normal =
-    root.querySelector(`[data-id="${ids.normal}"]`) ??
-    document.createElement("div");
-  const normalButton =
-    root.querySelector(`[data-id="${ids.normalButton}"]`) ??
-    document.createElement("div");
-  const hard =
-    root.querySelector(`[data-id="${ids.hard}"]`) ??
-    document.createElement("div");
-  const hardButton =
-    root.querySelector(`[data-id="${ids.hardButton}"]`) ??
-    document.createElement("div");
-  const veryHard =
-    root.querySelector(`[data-id="${ids.veryHard}"]`) ??
-    document.createElement("div");
-  const veryHardButton =
-    root.querySelector(`[data-id="${ids.veryHardButton}"]`) ??
-    document.createElement("div");
-  return {
-    backGround,
-    closer,
-    easy,
-    easyButton,
-    normal,
-    normalButton,
-    hard,
-    hardButton,
-    veryHard,
-    veryHardButton,
-  };
-}
+import { ROOT_CLASS } from "./dom/class-name";
+import { extractElements } from "./dom/elements";
+import { rootInnerHTML } from "./dom/root-inner-html";
 
 /** 難易度選択ダイアログ */
 export class DifficultyDialog implements DOMDialog {
---
+++ src/js/dom-dialogs/difficulty/dom/elements.js
@@ -0,0 +1,69 @@
+// @flow
+
+import type { DataIDs } from "./data-ids";
+
+/** ルート要素の子孫要素 */
+type Elements = {
+  backGround: HTMLElement,
+  closer: HTMLElement,
+  easy: HTMLElement,
+  easyButton: HTMLElement,
+  normal: HTMLElement,
+  normalButton: HTMLElement,
+  hard: HTMLElement,
+  hardButton: HTMLElement,
+  veryHard: HTMLElement,
+  veryHardButton: HTMLElement,
+};
+
+/**
+ * ルート要素から子孫要素を抽出する
+ *
+ * @param root ルート要素
+ * @param ids data-idを集めたもの
+ * @return 抽出結果
+ */
+export function extractElements(root: HTMLElement, ids: DataIDs): Elements {
+  const backGround =
+    root.querySelector(`[data-id="${ids.backGround}"]`) ??
+    document.createElement("div");
+  const closer =
+    root.querySelector(`[data-id="${ids.closer}"]`) ??
+    document.createElement("div");
+  const easy =
+    root.querySelector(`[data-id="${ids.easy}"]`) ??
+    document.createElement("div");
+  const easyButton =
+    root.querySelector(`[data-id="${ids.easyButton}"]`) ??
+    document.createElement("div");
+  const normal =
+    root.querySelector(`[data-id="${ids.normal}"]`) ??
+    document.createElement("div");
+  const normalButton =
+    root.querySelector(`[data-id="${ids.normalButton}"]`) ??
+    document.createElement("div");
+  const hard =
+    root.querySelector(`[data-id="${ids.hard}"]`) ??
+    document.createElement("div");
+  const hardButton =
+    root.querySelector(`[data-id="${ids.hardButton}"]`) ??
+    document.createElement("div");
+  const veryHard =
+    root.querySelector(`[data-id="${ids.veryHard}"]`) ??
+    document.createElement("div");
+  const veryHardButton =
+    root.querySelector(`[data-id="${ids.veryHardButton}"]`) ??
+    document.createElement("div");
+  return {
+    backGround,
+    closer,
+    easy,
+    easyButton,
+    normal,
+    normalButton,
+    hard,
+    hardButton,
+    veryHard,
+    veryHardButton,
+  };
+}
