0b6f3dd1f1aeff6fb34b831164cfeb1e8066ec53
kaidouji85 <kaidouji85@gmail.com>
2022-10-30 00:18:51 +0900

    ストーリーを書き込んだ

--- src/js/custom-battle-events/burst-tutorial/stories/burst-is-trump-card.js
+++ src/js/custom-battle-events/burst-tutorial/stories/burst-is-trump-card.js
@@ -1,9 +1,12 @@
 // @flow
 
 import type { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
-import { activeLeftMessageWindowWithFace } from "../../active-message-window";
+import {
+  activeLeftMessageWindowWithFace,
+  activeRightMessageWindowWithFace,
+} from "../../active-message-window";
 import { refreshConversation } from "../../invisible-all-message-windows";
-import { scrollLeftMessages } from "../../scroll-messages";
+import { scrollLeftMessages, scrollRightMessages } from "../../scroll-messages";
 
 /**
  * ストーリー バーストは切り札
@@ -14,6 +17,24 @@ export async function burstIsTrumpCard(
   props: CustomBattleEventProps
 ): Promise<void> {
   activeLeftMessageWindowWithFace(props, "Tsubasa");
-  await scrollLeftMessages(props, [["バーストは切り札だ"]]);
+  await scrollLeftMessages(props, [
+    ["ツバサ", "「シンヤ 今はまだバーストするタイミングではない」"],
+  ]);
+  props.view.dom.leftMessageWindow.darken();
+
+  activeRightMessageWindowWithFace(props, "Shinya");
+  await scrollRightMessages(props, [["シンヤ", "「へ？ どうしてッスか」"]]);
+  props.view.dom.rightMessageWindow.darken();
+
+  activeLeftMessageWindowWithFace(props, "Tsubasa");
+  await scrollLeftMessages(props, [
+    ["ツバサ", "「バーストすれば バッテリーを5回復できる"],
+    ["しかし今はバッテリーが満タン"],
+    ["だからバーストしても意味がないんだ」"],
+  ]);
+  props.view.dom.leftMessageWindow.darken();
+
+  activeRightMessageWindowWithFace(props, "Shinya");
+  await scrollRightMessages(props, [["シンヤ", "「なるほど 了解ッス」"]]);
   await refreshConversation(props, 100);
 }
