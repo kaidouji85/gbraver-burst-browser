1b122b982f8f28f25d663215dc10977f8b8389d0
kaidouji85 <kaidouji85@gmail.com>
2022-11-06 14:15:43 +0900

    アクティブ演出を派手にした

--- src/js/game-object/armdozer/lightning-dozer/view/player-lighting-dozer-view.js
+++ src/js/game-object/armdozer/lightning-dozer/view/player-lighting-dozer-view.js
@@ -132,7 +132,7 @@ export class PlayerLightingDozerView implements LightningDozerView {
     const currentActiveMesh = this.#getActiveMesh(model.animation.type);
     if (currentActiveMesh) {
       const activeOpacity =
-        (0.2 + model.active.strength * 0.05) * model.active.opacity;
+        (0.5 + model.active.strength * 0.1) * model.active.opacity;
       currentActiveMesh.opacity(activeOpacity);
       currentActiveMesh.animate(model.animation.frame);
     }
