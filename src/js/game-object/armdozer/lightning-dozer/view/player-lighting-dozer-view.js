c671932eac363062ea19556092279359308d9b95
kaidouji85 <kaidouji85@gmail.com>
2022-11-07 07:29:02 +0000

    アクティブの強さを調整した

--- src/js/game-object/armdozer/lightning-dozer/view/player-lighting-dozer-view.js
+++ src/js/game-object/armdozer/lightning-dozer/view/player-lighting-dozer-view.js
@@ -57,7 +57,7 @@ export class PlayerLightingDozerView implements LightningDozerView {
     );
     if (currentActiveMesh) {
       const activeOpacity =
-        (0.4 + model.active.strength * 0.1) * model.active.opacity;
+        (0.25 + model.active.strength * 0.05) * model.active.opacity;
       currentActiveMesh.mesh.opacity(activeOpacity);
       currentActiveMesh.mesh.animate(model.animation.frame);
     }
--- src/js/game-object/armdozer/wing-dozer/view/player-wing-dozer-view.js
+++ src/js/game-object/armdozer/wing-dozer/view/player-wing-dozer-view.js
@@ -68,7 +68,7 @@ export class PlayerWingDozerView implements WingDozerView {
     );
     if (currentActiveMesh) {
       const activeOpacity =
-        (0.4 + model.active.strength * 0.1) * model.active.opacity;
+        (0.25 + model.active.strength * 0.05) * model.active.opacity;
       currentActiveMesh.mesh.opacity(activeOpacity);
       currentActiveMesh.mesh.animate(model.animation.frame);
     }
