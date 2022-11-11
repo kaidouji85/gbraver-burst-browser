bea9bdbfbcf45ab938412b601b97d873d3967556
kaidouji85 <kaidouji85@gmail.com>
2022-11-11 04:14:32 +0000

    shfmtをかけた

--- upload-module.bash
+++ upload-module.bash
@@ -10,8 +10,7 @@ fi
 S3_BUCKET=$1
 BUILD_PATH=./build/production
 
-find "${BUILD_PATH}" -maxdepth 1 -type f -print0 | while IFS= read -r -d '' fileName
-do
+find "${BUILD_PATH}" -maxdepth 1 -type f -print0 | while IFS= read -r -d '' fileName; do
   aws s3 cp --cache-control no-store "${BUILD_PATH}/${fileName}" "s3://${S3_BUCKET}/${fileName}"
 done
 aws s3 sync --delete --exact-timestamps "${BUILD_PATH}" "s3://${S3_BUCKET}"
