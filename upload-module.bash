42b013b1332d8c5ab46ad7aa8578e2f590485b21
kaidouji85 <kaidouji85@gmail.com>
2022-11-11 04:40:18 +0000

    モジュールアップロード不具合修正

--- upload-module.bash
+++ upload-module.bash
@@ -10,7 +10,8 @@ fi
 S3_BUCKET=$1
 BUILD_PATH=./build/production
 
-find "${BUILD_PATH}" -maxdepth 1 -type f -print0 | while IFS= read -r -d '' fileName; do
-  aws s3 cp --cache-control no-store "${BUILD_PATH}/${fileName}" "s3://${S3_BUCKET}/${fileName}"
+find "${BUILD_PATH}" -maxdepth 1 -type f -print0 | while IFS= read -r -d '' filePath; do
+  s3FileName=$(basename "${filePath}")
+  aws s3 cp --cache-control no-store "${filePath}" "s3://${S3_BUCKET}/${s3FileName}"
 done
 aws s3 sync --delete --exact-timestamps "${BUILD_PATH}" "s3://${S3_BUCKET}"
