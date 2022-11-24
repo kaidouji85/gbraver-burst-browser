9238d2dc0482bdc3e40db37e055f77784a87d7b9
Y.Takeuchi <kaidouji85@gmail.com>
2022-11-24 23:55:16 +0900

    敵ジェネシスブレイバー追加

--- src/js/game-object/armdozer/genesis-braver/index.js
+++ src/js/game-object/armdozer/genesis-braver/index.js
@@ -4,6 +4,7 @@ import type { Resources } from "../../../resource";
 import type { Stream } from "../../../stream/stream";
 import type { GameObjectAction } from "../../action/game-object-action";
 import { GenesisBraver } from "./genesis-braver";
+import { EnemyGenesisBraverView } from "./view/enemy-genesis-braver-view";
 import { PlayerGenesisBraverView } from "./view/player-genesis-braver-view";
 
 /**
@@ -17,5 +18,19 @@ export function playerGenesisBraver(
   gameObjectAction: Stream<GameObjectAction>
 ): GenesisBraver {
   const view = new PlayerGenesisBraverView(resources);
+  return new GenesisBraver(view, resources, gameObjectAction);
+}
+
+/**
+ * 敵ジェネシスブレイバーを生成
+ * @param resources リソース管理オブジェクト
+ * @param gameObjectAction ゲームオブジェクトアクション
+ * @return 生成結果
+ */
+export function enemyGenesisBraver(
+  resources: Resources,
+  gameObjectAction: Stream<GameObjectAction>
+): GenesisBraver {
+  const view = new EnemyGenesisBraverView(resources);
   return new GenesisBraver(view, resources, gameObjectAction);
 }
--- stories/genesis-braver.stories.js
+++ stories/genesis-braver.stories.js
@@ -1,6 +1,9 @@
 // @flow
 
-import { playerGenesisBraver } from "../src/js/game-object/armdozer/genesis-braver";
+import {
+  enemyGenesisBraver,
+  playerGenesisBraver,
+} from "../src/js/game-object/armdozer/genesis-braver";
 import { TDGameObjectStub } from "./stub/td-game-object-stub";
 
 export default {
@@ -10,6 +13,15 @@ export default {
 export const playerStand = (): HTMLElement => {
   const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
     const sprite = playerGenesisBraver(resources, gameObjectAction);
+    return { objects: [sprite.getObject3D()] };
+  });
+  stub.start();
+  return stub.domElement();
+};
+
+export const enemyStand = (): HTMLElement => {
+  const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
+    const sprite = enemyGenesisBraver(resources, gameObjectAction);
     return { objects: [sprite.getObject3D()] };
   });
   stub.start();
