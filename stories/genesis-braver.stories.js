57955256a39ab32227cef3256d163a9770870ce9
kaidouji85 <kaidouji85@gmail.com>
2022-12-30 00:22:38 +0900

    敵ストレートパンチストーリー追加

--- stories/genesis-braver.stories.js
+++ stories/genesis-braver.stories.js
@@ -5,11 +5,22 @@ import {
   EnemyGenesisBraver,
   PlayerGenesisBraver,
 } from "../src/js/game-object/armdozer/genesis-braver";
+import {GenesisBraver} from "../src/js/game-object/armdozer/genesis-braver/genesis-braver";
 import { TDGameObjectStub } from "./stub/td-game-object-stub";
 
 export default {
   title: "genesis-braver",
 };
+
+const straightPunch = (sprite: GenesisBraver) => {
+  return delay(1000)
+    .chain(sprite.charge())
+    .chain(delay(1000))
+    .chain(sprite.straightPunch())
+    .chain(delay(1000))
+    .chain(sprite.spToStand())
+    .chain(delay(1000));
+}
 
 export const playerStand = (): HTMLElement => {
   const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
@@ -23,14 +34,7 @@ export const playerStand = (): HTMLElement => {
 export const playerStraightPunch = (): HTMLElement => {
   const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
     const sprite = PlayerGenesisBraver(resources, gameObjectAction);
-    delay(1000)
-      .chain(sprite.charge())
-      .chain(delay(1000))
-      .chain(sprite.straightPunch())
-      .chain(delay(1000))
-      .chain(sprite.spToStand())
-      .chain(delay(1000))
-      .loop();
+    straightPunch(sprite).loop();
     return { objects: [sprite.getObject3D()] };
   });
   stub.start();
@@ -40,6 +44,16 @@ export const playerStraightPunch = (): HTMLElement => {
 export const enemyStand = (): HTMLElement => {
   const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
     const sprite = EnemyGenesisBraver(resources, gameObjectAction);
+    return { objects: [sprite.getObject3D()] };
+  });
+  stub.start();
+  return stub.domElement();
+};
+
+export const enemyStraightPunch = (): HTMLElement => {
+  const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
+    const sprite = EnemyGenesisBraver(resources, gameObjectAction);
+    straightPunch(sprite).loop();
     return { objects: [sprite.getObject3D()] };
   });
   stub.start();
