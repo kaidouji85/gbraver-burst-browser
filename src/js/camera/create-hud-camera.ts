2739a7893f00df74f81224982df6ea6976a8c3e9
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-07 17:32:16 +0900

    createHUDCameraのTS対応

--- src/js/camera/create-hud-camera.ts
+++ src/js/camera/create-hud-camera.ts
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { getViewPortHeight, getViewPortWidth } from "../view-port/view-port-size";
 
 /** HUDレイヤーのカメラを生成して返す */
-export function createHUDCamera(): typeof THREE.OrthographicCamera {
+export function createHUDCamera(): THREE.OrthographicCamera {
   const width = getViewPortWidth();
   const height = getViewPortHeight();
   return new THREE.OrthographicCamera(-width / 2, width / 2, height / 2, -height / 2, 0, 30);
