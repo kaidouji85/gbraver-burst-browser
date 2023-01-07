8f713d5369e8e4026f95a871f462c3b8c7e213f2
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-07 17:23:14 +0900

    グローバル変数定義をエントリポイントに追加した

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,6 +4,25 @@ import { isMobile } from "./device-ditect/is-mobile";
 import { createLocalStorageConfigRepository } from "./game/config/local-storage";
 import { Game } from "./game/index";
 
+/** webpack.config.js Webpack Define Pluginで定義したグローバル変数 */
+declare var GBRAVER_BURST_DESKTOP_RESOURCE_ROOT: string;
+declare var GBRAVER_BURST_MOBILE_RESOURCE_ROOT: string;
+declare var GBRAVER_BURST_OWN_ROOT_URL: string;
+declare var GBRAVER_BURST_HOW_TO_PLAY: string;
+declare var GBRAVER_BURST_TERMS_OF_SERVICE_URL: string;
+declare var GBRAVER_BURST_PRIVACY_POLICY_URL: string;
+declare var GBRAVER_BURST_CONTACT_URL: string;
+declare var GBRAVER_BURST_REST_API_URL: string;
+declare var GBRAVER_BURST_WEBSOCKET_API_URL: string;
+declare var GBRAVER_BURST_IS_PERFORMANCE_STATS_VISIBLE: string;
+declare var GBRAVER_BURST_IS_SERVICE_WORKER_USED: string;
+declare var GBRAVER_BURST_IS_API_SERVER_ENABLE: string
+declare var GBRAVER_BURST_AUTH0_DOMAIN: string;
+declare var GBRAVER_BURST_AUTH0_CLIENT_ID: string;
+declare var GBRAVER_BURST_AUTH0_AUDIENCE: string;
+declare var GBRAVER_BURST_CAN_PLAY_TUTORIAL_IN_DEVELOPMENT: string;
+declare var GBRAVER_BURST_SHOULD_LOAD_DEVELOPING_RESOURCE: string;
+
 /**
  * Gブレイバーバーストのエントリポイント
  */
