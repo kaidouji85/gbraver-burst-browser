34b1a106135fb2fc76c1e5fe19f9c2f545031416
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-13 08:26:19 +0900

    とりあえず動くところまで作成

--- src/js/game-object/gauge/animation/battery.ts
+++ src/js/game-object/gauge/animation/battery.ts
@@ -12,12 +12,10 @@ import type { GaugeModel } from "../model/gauge-model";
  */
 export function battery(model: GaugeModel, value: number): Animate {
   const animations = model.batteryList.map((v) => {
-    const opacity = v.value <= value ? 1 : 0;
+    const brightness = v.value <= value ? 1 : 0;
     return tween(v, (t) =>
       t.to(
-        {
-          opacity: opacity,
-        },
+        {brightness},
         300
       )
     );
