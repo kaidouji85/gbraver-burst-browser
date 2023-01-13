4c8ad43fd006b147e6fa9f572a6aed174d323441
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-13 22:33:42 +0900

    ストーリーアニメをヘルパー関数化した

--- stories/gauge.stories.ts
+++ stories/gauge.stories.ts
@@ -1,11 +1,37 @@
 import { all } from "../src/js/animation/all";
+import { Animate } from "../src/js/animation/animate";
 import { delay } from "../src/js/animation/delay";
 import { enemyGauge, playerGauge } from "../src/js/game-object/gauge";
+import { Gauge } from "../src/js/game-object/gauge/gauge";
 import { HUDGameObjectStub } from "./stub/hud-game-object-stub";
 
 export default {
   title: "gauge",
 };
+
+function gaugeChange(gauge: Gauge): Animate {
+  return delay(1000)
+    .chain(gauge.hp(1000))
+    .chain(gauge.battery(2))
+    .chain(delay(1000))
+    .chain(gauge.hp(300))
+    .chain(gauge.battery(0))
+    .chain(delay(1000))
+    .chain(gauge.hp(3100))
+    .chain(gauge.battery(5));
+}
+
+function maxBatteryChange(gauge: Gauge): Animate {
+  return delay(1000)
+    .chain(gauge.battery(1))
+    .chain(delay(1000))
+    .chain(all(gauge.battery(8), gauge.maxBattery(8)))
+    .chain(delay(1000))
+    .chain(gauge.battery(3))
+    .chain(delay(1000))
+    .chain(all(gauge.battery(4), gauge.maxBattery(4)))
+    .chain(delay(1000));
+}
 
 export const player = (): HTMLElement => {
   const stub = new HUDGameObjectStub(({ resources, gameObjectAction }) => {
@@ -15,16 +41,7 @@ export const player = (): HTMLElement => {
       hp: 3100,
       battery: 5,
     });
-    const animation = delay(1000)
-      .chain(gauge.hp(1000))
-      .chain(gauge.battery(2))
-      .chain(delay(1000))
-      .chain(gauge.hp(300))
-      .chain(gauge.battery(0))
-      .chain(delay(1000))
-      .chain(gauge.hp(3100))
-      .chain(gauge.battery(5));
-    animation.loop();
+    gaugeChange(gauge).loop();
     return [gauge.getObject3D()];
   });
   stub.start();
@@ -39,16 +56,7 @@ export const playerMaxBatteryChange = (): HTMLElement => {
       hp: 3100,
       battery: 4,
     });
-    delay(1000)
-      .chain(gauge.battery(1))
-      .chain(delay(1000))
-      .chain(all(gauge.battery(8), gauge.maxBattery(8)))
-      .chain(delay(1000))
-      .chain(gauge.battery(3))
-      .chain(delay(1000))
-      .chain(all(gauge.battery(4), gauge.maxBattery(4)))
-      .chain(delay(1000))
-      .loop();
+    maxBatteryChange(gauge).loop();
     return [gauge.getObject3D()];
   });
   stub.start();
@@ -63,23 +71,14 @@ export const enemy = (): HTMLElement => {
       hp: 3100,
       battery: 5,
     });
-    const animation = delay(1000)
-      .chain(gauge.hp(1000))
-      .chain(gauge.battery(2))
-      .chain(delay(1000))
-      .chain(gauge.hp(300))
-      .chain(gauge.battery(0))
-      .chain(delay(1000))
-      .chain(gauge.hp(3100))
-      .chain(gauge.battery(5));
-    animation.loop();
+    gaugeChange(gauge).loop();
     return [gauge.getObject3D()];
   });
   stub.start();
   return stub.domElement();
 };
 
-export const enemyBattery4 = (): HTMLElement => {
+export const enemyMaxBatteryChange = (): HTMLElement => {
   const stub = new HUDGameObjectStub(({ resources, gameObjectAction }) => {
     const gauge = enemyGauge({
       resources,
@@ -87,6 +86,7 @@ export const enemyBattery4 = (): HTMLElement => {
       hp: 3100,
       battery: 4,
     });
+    maxBatteryChange(gauge).loop();
     return [gauge.getObject3D()];
   });
   stub.start();
