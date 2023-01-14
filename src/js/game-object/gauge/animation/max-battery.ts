de9bf8c66bed9d75e5a7756f36f433f9271e1843
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-14 09:16:55 +0900

    バッテリー最大値に応じてフレームを変更

--- src/js/game-object/gauge/animation/max-battery.ts
+++ src/js/game-object/gauge/animation/max-battery.ts
@@ -3,6 +3,7 @@ import { Animate } from "../../../animation/animate";
 import { tween } from "../../../animation/tween";
 import type { GaugeModel } from "../model/gauge-model";
 import { getBatteryGaugeUnitOpacity } from "../model/get-battery-gauge-unit-opacity";
+import { process } from "../../../animation/process";
 
 /**
  * バッテリー最大値を変更するアニメーション
@@ -12,13 +13,16 @@ import { getBatteryGaugeUnitOpacity } from "../model/get-battery-gauge-unit-opac
  */
 export function maxBattery(model: GaugeModel, value: number): Animate {
   return all(
-    ...model.batteryList.map((gaugeUnit) => {
-      return tween(gaugeUnit, (t) =>
+    process(() => {
+      model.maxBattery = value;
+    }),
+    ...model.batteryList.map((gaugeUnit) =>
+      tween(gaugeUnit, (t) =>
         t.to(
           { opacity: getBatteryGaugeUnitOpacity(gaugeUnit.value, value) },
           300
         )
-      );
-    })
+      )
+    )
   );
 }
