cbdc38e673254c075c70e4fb4c250759da5ebe3f
Y.Takeuchi <kaidouji85@gmail.com>
2023-01-14 13:53:33 +0900

    可変バッテリーゲージのストーリー追加

--- stories/battery-selector.stories.ts
+++ stories/battery-selector.stories.ts
@@ -27,3 +27,53 @@ export const batterySelector = (): HTMLElement => {
   stub.start();
   return stub.domElement();
 };
+
+export const batterySelector4  = (): HTMLElement => {
+  const stub = new HUDGameObjectStub(({ resources, gameObjectAction }) => {
+    const selector = new BatterySelector({
+      resources: resources,
+      gameObjectAction: gameObjectAction,
+      maxBattery: 5,
+    });
+    selector.open(1, 4, 4, "Attack").play();
+    selector.decidePushNotifier().subscribe((event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      selector.decide().play();
+    });
+    selector.batteryPlusPushNotifier().subscribe(() => {
+      selector.batteryPlus().play();
+    });
+    selector.batteryMinusPushNotifier().subscribe(() => {
+      selector.batteryMinus().play();
+    });
+    return [selector.getObject3D()];
+  });
+  stub.start();
+  return stub.domElement();
+};
+
+export const batterySelector8  = (): HTMLElement => {
+  const stub = new HUDGameObjectStub(({ resources, gameObjectAction }) => {
+    const selector = new BatterySelector({
+      resources: resources,
+      gameObjectAction: gameObjectAction,
+      maxBattery: 5,
+    });
+    selector.open(1, 8, 8, "Attack").play();
+    selector.decidePushNotifier().subscribe((event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      selector.decide().play();
+    });
+    selector.batteryPlusPushNotifier().subscribe(() => {
+      selector.batteryPlus().play();
+    });
+    selector.batteryMinusPushNotifier().subscribe(() => {
+      selector.batteryMinus().play();
+    });
+    return [selector.getObject3D()];
+  });
+  stub.start();
+  return stub.domElement();
+};
