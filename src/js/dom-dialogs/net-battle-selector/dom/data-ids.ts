090ab649257bc2b8eef85e53de5ab27a4581155a
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-02 21:58:15 +0900

    DataIDs追加

---
+++ src/js/dom-dialogs/net-battle-selector/dom/data-ids.ts
@@ -0,0 +1,11 @@
+/** data-idをあつめたもの */
+export type DataIDs = {
+  /** 背景 */
+  backGround: string;
+  /** クロージャ */
+  closer: string;
+  /** カジュアルマッチボタン */
+  casualMatchButton: string;
+  /** プライベートマッチボタン */
+  privateMatchButton: string;
+};
