d55e19db067e819b6b3a51fa8136debd79a993da
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-02 09:24:52 +0000

    要素雛形を追加

--- src/js/dom-dialogs/net-battle-selector/dom/root-inner-html.ts
+++ src/js/dom-dialogs/net-battle-selector/dom/root-inner-html.ts
@@ -10,11 +10,22 @@ import { ROOT_CLASS } from "./class-name";
 export function rootInnerHTML(resources: Resources): string {
   const closerPath =
     resources.paths.find((v) => v.id === PathIds.CLOSER)?.path ?? "";
+  const casualMatchDescription = "すべてのプレイヤーでランダムマッチをおこないます";
+  const privateMatchDescription = "合言葉を共有して、知り合いと対戦します";
   return `
     <div class="${ROOT_CLASS}__background"></div>
     <div class="${ROOT_CLASS}__dialog">
       <img class="${ROOT_CLASS}__closer" alt="閉じる" src="${closerPath}">
-      hello
+      <div class="${ROOT_CLASS}__casual-match">
+        <div class="${ROOT_CLASS}__casual-match-title">カジュアルマッチ</div>
+        <div class="${ROOT_CLASS}__casual-match-description">${casualMatchDescription}</div>
+        <button class="${ROOT_CLASS}__casual-match-selector">カジュアルマッチする</button>
+      </div>
+      <div class="${ROOT_CLASS}__private-match">
+        <div class="${ROOT_CLASS}__private-match-title">プライベートマッチ</div>
+        <div class="${ROOT_CLASS}__private-match-description">${privateMatchDescription}</div>
+        <button class="${ROOT_CLASS}__private-match-selector">カジュアルマッチする</button>
+      </div>
     </div>
   `;
 }
