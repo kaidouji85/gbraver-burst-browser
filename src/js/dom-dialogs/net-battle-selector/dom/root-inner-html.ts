fd4a6b6734793b3ffeaf0cd1310ea624392b6099
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-02 09:02:40 +0000

    css雛形を追加

--- src/js/dom-dialogs/net-battle-selector/dom/root-inner-html.ts
+++ src/js/dom-dialogs/net-battle-selector/dom/root-inner-html.ts
@@ -1,7 +1,14 @@
+import { ROOT_CLASS } from "./class-name";
+
 /**
  * ルートHTML要素のinnerHTMLを生成する
  * @return 生成結果
  */
 export function rootInnerHTML(): string {
-  return `dialog`;
+  return `
+    <div class="${ROOT_CLASS}__background"></div>
+    <div class="${ROOT_CLASS}__dialog">
+      hello
+    </div>
+  `;
 }
