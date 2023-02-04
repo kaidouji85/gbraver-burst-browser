7606fdd387e3fa1143286633171413fef033e56c
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-04 17:46:47 +0900

    PrivateMatchGuestDialog雛形

---
+++ src/js/dom-dialogs/private-match-guest/index.ts
@@ -0,0 +1,24 @@
+import {DOMDialog} from "../dialog";
+
+/** プライベートマッチゲストダイアログ */
+export class PrivateMatchGuestDialog implements DOMDialog {
+  /** ルートHTML要素 */
+  #root: HTMLElement;
+
+  /**
+   * コンストラクタ
+   */
+  constructor() {
+    this.#root = document.createElement("div");
+  }
+
+  /** @override */
+  destructor(): void {
+    // NOP
+  }
+
+  /** @override */
+  getRootHTMLElement(): HTMLElement {
+    return this.#root;
+  }
+}
