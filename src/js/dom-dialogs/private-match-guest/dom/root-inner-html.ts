1800e5a19726c8bab08c5f7fbe77ae4b14ae4fc8
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-05 16:57:06 +0900

    スタイル調整

--- src/js/dom-dialogs/private-match-guest/dom/root-inner-html.ts
+++ src/js/dom-dialogs/private-match-guest/dom/root-inner-html.ts
@@ -10,12 +10,13 @@ import { ROOT_CLASS } from "./class-name";
 export function rootInnerHtml(resources: Resources): string {
   const closerPath =
     resources.paths.find((v) => v.id === PathIds.CLOSER)?.path ?? "";
-  const entryDescription = "ルームIDを入力してください。";
+  const entryDescription = "ホストから共有されたルームIDを、入力してください。";
   return `
     <div class="${ROOT_CLASS}__background"></div>
     <div class="${ROOT_CLASS}__dialog">
       <img class="${ROOT_CLASS}__closer" src="${closerPath}">
       <div class="${ROOT_CLASS}__entry-form">
+        <div class="${ROOT_CLASS}__entry-title">プライベートマッチ（ゲスト）</div>
         <div class="${ROOT_CLASS}__entry-description">${entryDescription}</div>
         <input class="${ROOT_CLASS}__room-id" type="text">
         <button class="${ROOT_CLASS}__enter">プライベートマッチ開始</button>
