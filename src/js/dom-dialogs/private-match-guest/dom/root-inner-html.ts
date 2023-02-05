0942a35821a0ffa4129bae2db79668d88b940d28
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-05 10:53:22 +0900

    各種要素追加

--- src/js/dom-dialogs/private-match-guest/dom/root-inner-html.ts
+++ src/js/dom-dialogs/private-match-guest/dom/root-inner-html.ts
@@ -5,10 +5,15 @@ import { ROOT_CLASS } from "./class-name";
  * @return 生成結果
  */
 export function rootInnerHtml(): string {
+  const entryDescription = "ルームIDを入力してください。";
   return `
     <div class="${ROOT_CLASS}__background"></div>
     <div class="${ROOT_CLASS}__dialog">
-      dialog
+      <div class="${ROOT_CLASS}__entry-form">
+        <div class="${ROOT_CLASS}__entry-description">${entryDescription}</div>
+        <input class="${ROOT_CLASS}__room-id" type="text">
+        <button class="${ROOT_CLASS}__enter">プライベートマッチ開始</button>
+      </div>
     </div>
   `;
 }
