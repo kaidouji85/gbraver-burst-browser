b57bd9a11f6db015dc7175b42f4c52c65ed54d30
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-06 03:00:04 +0000

    Elements追加

---
+++ src/js/dom-dialogs/private-match-guest/dom/elements.ts
@@ -0,0 +1,31 @@
+import { DataIDs } from "./data-ids";
+
+/** ルート要素の子孫要素 */
+export type Elements = {
+    /** クロージャ */
+    closer: HTMLElement;
+    /** 背景 */
+    background: HTMLElement;
+    /** プライベートマット開始ボタン */
+    enterButton: HTMLElement;
+};
+
+/**
+ * ルート要素から子孫要素を抽出する
+ *
+ * @param root ルート要素
+ * @param ids data-idを集めたもの
+ * @return 抽出結果
+ */
+export function extractElements(root: HTMLElement, ids: DataIDs): Elements {
+  const closer: HTMLElement =
+    root.querySelector(`[data-id="${ids.closer}"]`) ??
+    document.createElement("div");
+  const background: HTMLElement =
+    root.querySelector(`[data-id="${ids.background}"]`) ??
+    document.createElement("div");
+  const enterButton: HTMLElement =
+    root.querySelector(`[data-id="${ids.enterButton}"]`) ??
+    document.createElement("div");  
+  return { closer, background, enterButton };
+}
