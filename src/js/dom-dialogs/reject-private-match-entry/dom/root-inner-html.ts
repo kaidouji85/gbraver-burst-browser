37c8690a526e7bdfc95829d2e51f43c8ff7f6737
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-06 10:50:16 +0000

    css 雛形

--- src/js/dom-dialogs/reject-private-match-entry/dom/root-inner-html.ts
+++ src/js/dom-dialogs/reject-private-match-entry/dom/root-inner-html.ts
@@ -1,9 +1,14 @@
+import { ROOT_CLASS } from "./class-name";
+
 /**
  * ルートHTML要素のinnerHTMLを生成する
  * @return 生成結果
  */
 export function rootInnerHTML() {
   return `
-    hello
+    <div class="${ROOT_CLASS}__background"></div>
+    <div class="${ROOT_CLASS}__dialog">
+      reject private match entry
+    </div>
   `;
 }
