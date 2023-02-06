97ba722c824cc08b8fdbd85a3d2ec4ea4ed3225e
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-06 02:57:01 +0000

    DataIDs追加

---
+++ src/js/dom-dialogs/private-match-guest/dom/data-ids.ts
@@ -0,0 +1,9 @@
+/** data-idをあつめたもの */
+export type DataIDs = {
+  /** クロージャ */
+  closer: string;
+  /** 背景 */
+  background: string;
+  /** プライベートマット開始ボタン */
+  enterButton: string;
+};
--- src/js/dom-dialogs/private-match-guest/props.ts
+++ src/js/dom-dialogs/private-match-guest/props.ts
@@ -1,5 +1,7 @@
 import { Resources } from "../../resource";
+import { domUuid } from "../../uuid/dom-uuid";
 import { ROOT_CLASS } from "./dom/class-name";
+import { DataIDs } from "./dom/data-ids";
 import { rootInnerHtml } from "./dom/root-inner-html";
 
 /** プライベートマッチゲストダイアログのプロパティ */
@@ -18,6 +20,11 @@ export function createPrivateMatchGuestDialogProps(
 ): PrivateMatchGuestDialogProps {
   const root = document.createElement("div");
   root.className = ROOT_CLASS;
-  root.innerHTML = rootInnerHtml(resources);
+  const dataIDs: DataIDs = {
+    closer: domUuid(),
+    background: domUuid(),
+    enterButton: domUuid(),
+  };
+  root.innerHTML = rootInnerHtml(resources, dataIDs);
   return { root };
 }
