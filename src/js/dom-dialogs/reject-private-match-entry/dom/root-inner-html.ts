4d608db94e59b056d137345ff9daad2de10cfeff
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-07 07:32:39 +0000

    必要な要素を追加した

--- src/js/dom-dialogs/reject-private-match-entry/dom/root-inner-html.ts
+++ src/js/dom-dialogs/reject-private-match-entry/dom/root-inner-html.ts
@@ -10,11 +10,14 @@ import { ROOT_CLASS } from "./class-name";
 export function rootInnerHTML(resources: Resources) {
   const closerPath =
     resources.paths.find((v) => v.id === PathIds.CLOSER)?.path ?? "";
+  const description = "ホストに連絡して、ルームを既に破棄していないか、先にマッチメイクしたプレイヤーがいないかを、確認してください。";
   return `
     <div class="${ROOT_CLASS}__background"></div>
     <div class="${ROOT_CLASS}__dialog">
       <img class="${ROOT_CLASS}__closer" src="${closerPath}" alt="閉じる">
-      reject private match entry
+      <div class="${ROOT_CLASS}__title">マッチメイク失敗</div>
+      <div class="${ROOT_CLASS}__description">${description}</div>
+      <button class="${ROOT_CLASS}__close-dialog">閉じる</button>
     </div>
   `;
 }
