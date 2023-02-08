46ce8751418e4d10e7a8d348ced8e2b2e96938ce
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-08 06:06:51 +0000

    InProgressにPrivateMatchHost追加

--- src/js/game/in-progress/in-progress.ts
+++ src/js/game/in-progress/in-progress.ts
@@ -1,9 +1,10 @@
 import type { CasualMatch } from "./casual-match";
 import type { None } from "./none";
 import type { NPCBattle } from "./npc-battle";
+import { PrivateMatchHost } from "./private-match-host";
 import type { Tutorial } from "./tutorial";
 
 /**
  * 現在進行中のフローの状態を保持する
  */
-export type InProgress = None | NPCBattle | CasualMatch | Tutorial;
+export type InProgress = None | NPCBattle | CasualMatch | Tutorial | PrivateMatchHost;
