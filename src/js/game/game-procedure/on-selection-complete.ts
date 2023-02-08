649e370d953b70f4859622262ab1a1cca8eff61f
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-08 10:08:12 +0000

    ヘルパー関数を別ファイルに切り出した

--- src/js/game/game-procedure/on-selection-complete.ts
+++ src/js/game/game-procedure/on-selection-complete.ts
@@ -1,116 +1,8 @@
-import type { Battle as BattleSDK } from "@gbraver-burst-network/browser-core";
-
-import { fadeOut, stop } from "../../bgm/bgm-operators";
 import { DifficultyDialog } from "../../dom-dialogs/difficulty";
-import { MatchingDialog } from "../../dom-dialogs/matching/matching-dialog";
-import { NetworkErrorDialog } from "../../dom-dialogs/network-error/network-error-dialog";
-import { WaitingDialog } from "../../dom-dialogs/waiting/waiting-dialog";
-import { MatchCard } from "../../dom-scenes/match-card";
-import { SOUND_IDS } from "../../resource/sound";
-import { BattleScene } from "../../td-scenes/battle";
-import type { BattleProgress } from "../../td-scenes/battle/battle-progress";
-import { waitAnimationFrame } from "../../wait/wait-animation-frame";
-import { waitTime } from "../../wait/wait-time";
-import { battleSceneConnector } from "../action-connector/battle-scene-connector";
 import { difficultyDialogConnector } from "../action-connector/difficulty-dialog-connector";
-import { matchCardConnector } from "../action-connector/match-card-connector";
-import { matchingDialogConnector } from "../action-connector/matching-dialog-connector";
-import { networkErrorDialogConnector } from "../action-connector/network-error-dialog-connector";
-import { waitingDialogConnector } from "../action-connector/waiting-dialog-connector";
-import { MAX_LOADING_TIME } from "../dom-scene-binder/max-loading-time";
-import type { SelectionComplete } from "../game-actions";
+import { SelectionComplete } from "../game-actions";
 import type { GameProps } from "../game-props";
-
-async function waitUntilMatching(
-  props: Readonly<GameProps>,
-  action: SelectionComplete
-): Promise<BattleSDK> {
-  try {
-    await props.api.disconnectWebsocket();
-    return await props.api.startCasualMatch(
-      action.armdozerId,
-      action.pilotId
-    );
-  } catch (e) {
-    const dialog = new NetworkErrorDialog(props.resources, {
-      type: "GotoTitle",
-    });
-    props.domDialogBinder.bind(dialog, networkErrorDialogConnector);
-    throw e;
-  }
-}
-
-function createBattleProgress(props: Readonly<GameProps>, battle: BattleSDK): BattleProgress {
-  return {
-    progress: async (v) => {
-      try {
-        const dialog = new WaitingDialog("通信中......");
-        props.domDialogBinder.bind(dialog, waitingDialogConnector);
-        const update = await battle.progress(v);
-        props.domDialogBinder.hidden();
-        return update;
-      } catch (e) {
-        const dialog = new NetworkErrorDialog(props.resources, {
-          type: "GotoTitle",
-        });
-        props.domDialogBinder.bind(dialog, networkErrorDialogConnector);
-        throw e;
-      }
-    },
-  };
-} 
-
-async function startCasualMatch(
-  props: Readonly<GameProps>,
-  action: SelectionComplete,
-): Promise<void> {
-  const dialog = new MatchingDialog(props.resources);
-  props.domDialogBinder.bind(dialog, matchingDialogConnector);
-  const battle = await waitUntilMatching(props, action);
-  props.suddenlyBattleEnd.bind(battle);
-  await props.fader.fadeOut();
-  props.domDialogBinder.hidden();
-  const scene = new MatchCard({
-    resources: props.resources,
-    player: battle.player.armdozer.id,
-    enemy: battle.enemy.armdozer.id,
-    caption: "CASUAL MATCH",
-  });
-  props.domSceneBinder.bind(scene, matchCardConnector);
-  await Promise.race([scene.waitUntilLoaded(), waitTime(MAX_LOADING_TIME)]);
-  await props.fader.fadeIn();
-  const battleProgress = createBattleProgress(props, battle);
-  const config = await props.config.load();
-  props.renderer.setPixelRatio(config.webGLPixelRatio);
-  const battleScene = new BattleScene({
-    resources: props.resources,
-    bgm: props.bgm,
-    playingBGM: SOUND_IDS.BATTLE_BGM_01,
-    initialAnimationTimeScale: config.battleAnimationTimeScale,
-    battleProgress,
-    player: battle.player,
-    enemy: battle.enemy,
-    initialState: battle.initialState,
-    resize: props.resize,
-    pushWindow: props.pushWindow,
-    gameLoop: props.gameLoop,
-    renderer: props.renderer,
-  });
-  props.tdBinder.bind(battleScene, battleSceneConnector);
-  await waitAnimationFrame();
-  await Promise.all([
-    (async () => {
-      await props.fader.fadeOut();
-      props.domSceneBinder.hidden();
-    })(),
-    (async () => {
-      await props.bgm.do(fadeOut);
-      await props.bgm.do(stop);
-    })(),
-  ]);
-  await props.fader.fadeIn();
-  await battleScene.start();
-}
+import { startCasualMatch } from "./start-casual-match";
 
 /**
  * プレイヤーキャラクター 選択完了時の処理
@@ -133,14 +25,23 @@ export async function onSelectionComplete(
         pilotId: action.pilotId,
       },
     };
-    props.domDialogBinder.bind(new DifficultyDialog(props.resources), difficultyDialogConnector);
+    props.domDialogBinder.bind(
+      new DifficultyDialog(props.resources),
+      difficultyDialogConnector
+    );
   } else if (props.inProgress.type === "CasualMatch") {
+    props.inProgress = {
+      ...props.inProgress,
+      subFlow: {
+        type: "Waiting",
+      },
+    };
+    await startCasualMatch(props, action);
     props.inProgress = {
       ...props.inProgress,
       subFlow: {
         type: "Battle",
       },
     };
-    await startCasualMatch(props, action);
   }
 }
