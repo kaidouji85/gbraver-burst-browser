7ae5364a5f3741d0e09a58051428fe8fd3959d01
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-09 10:25:09 +0000

    プライベートマッチホストダイアログにイベント関連付け

--- src/js/game/action-connector/private-match-host-dialog-connector.ts
+++ src/js/game/action-connector/private-match-host-dialog-connector.ts
@@ -5,4 +5,8 @@ import type { DomDialogActionConnector } from "../dom-dialog-binder/dom-dialog-a
 type Connector = DomDialogActionConnector<PrivateMatchHostDialog>;
 
 /** プライベートマッチ（ホスト）ダイアログとゲームアクションを関連付ける */
-export const privateMatchHostDialogConnector: Connector = () => [];
+export const privateMatchHostDialogConnector: Connector = (dialog, gameAction) => [
+  dialog.notifyDialogClosed().subscribe(() => {
+    gameAction.next({ type: "MatchingCanceled" });
+  })
+];
