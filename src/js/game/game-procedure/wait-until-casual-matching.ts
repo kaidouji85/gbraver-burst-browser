1aca7ac9fc12febda22c400124cd85a79f10eaf5
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-09 05:13:21 +0000

    waitUntilCasualMatching追加

---
+++ src/js/game/game-procedure/wait-until-casual-matching.ts
@@ -0,0 +1,34 @@
+import type { Battle as BattleSDK } from "@gbraver-burst-network/browser-core";
+
+import { MatchingDialog } from "../../dom-dialogs/matching/matching-dialog";
+import { NetworkErrorDialog } from "../../dom-dialogs/network-error/network-error-dialog";
+import { matchingDialogConnector } from "../action-connector/matching-dialog-connector";
+import { networkErrorDialogConnector } from "../action-connector/network-error-dialog-connector";
+import { SelectionComplete } from "../game-actions";
+import { GameProps } from "../game-props";
+
+/**
+ * カジュアルマッチングするまで待機するヘルパー関数
+ * @param props ゲームプロパティ
+ * @param action アクション
+ * @return バトルSDK
+ */
+export async function waitUntilCasualMatching(
+  props: Readonly<GameProps>,
+  action: SelectionComplete
+): Promise<BattleSDK> {
+  try {
+    props.domDialogBinder.bind(
+      new MatchingDialog(props.resources),
+      matchingDialogConnector
+    );
+    await props.api.disconnectWebsocket();
+    return await props.api.startCasualMatch(action.armdozerId, action.pilotId);
+  } catch (e) {
+    const dialog = new NetworkErrorDialog(props.resources, {
+      type: "GotoTitle",
+    });
+    props.domDialogBinder.bind(dialog, networkErrorDialogConnector);
+    throw e;
+  }
+}
