d607f1897147558e0f2e54b6f13a57fbf9e61775
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-10 02:24:30 +0000

    endTutorialをifの中に戻した

--- src/js/game/game-procedure/on-end-battle.ts
+++ src/js/game/game-procedure/on-end-battle.ts
@@ -1,10 +1,7 @@
-import type { GameEndResult, GameOver } from "gbraver-burst-core";
-
 import {
   BattleAnimationTimeScales,
   parseBattleAnimationTimeScale,
 } from "../config/browser-config";
-import type { PostBattleButtonConfig } from "../dom-floaters/post-battle/post-battle-button-config";
 import {
   PostNetworkBattleButtons,
   PostNPCBattleComplete,
@@ -15,9 +12,6 @@ import {
 } from "../dom-floaters/post-battle/post-battle-buttons";
 import type { EndBattle } from "../game-actions";
 import type { GameProps } from "../game-props";
-import type { InProgress } from "../in-progress/in-progress";
-import type { NPCBattle, PlayingNPCBattle } from "../in-progress/npc-battle";
-import type { PlayingTutorialStage } from "../in-progress/tutorial";
 import type { NPCBattleResult } from "../npc-battle";
 import { updateNPCBattleState } from "../npc-battle";
 
@@ -58,51 +52,6 @@ const postNPCBattleButtons = (result: NPCBattleResult) => {
 };
 
 /**
- * チュートリアル進行中に利用するデータを生成する
- *
- * @param inProgress 進行中のフロー
- * @param gameEndResult 戦闘結果
- * @return 生成結果、チュートリアル中でない場合はnullを返す
- */
-const createTutorial = (
-  inProgress: InProgress,
-  gameEndResult: GameEndResult
-) => {
-  if (
-    inProgress.type === "Tutorial" &&
-    inProgress.subFlow.type === "PlayingTutorialStage" &&
-    gameEndResult.type === "GameOver"
-  ) {
-    const gameOver = gameEndResult as GameOver;
-    const playingTutorialStage = inProgress.subFlow as PlayingTutorialStage;
-    const isPlayerWin =
-      gameOver.winner === playingTutorialStage.stage.player.playerId;
-    const postBattleButtons = isPlayerWin
-      ? PostTutorialWinButtons
-      : PostTutorialLoseButtons;
-    return {
-      postBattleButtons,
-    };
-  }
-
-  return null;
-};
-
-/**
- * チュートリアルが終了した際の処理
- *
- * @param props ゲームプロパティ
- * @param postBattleButtons 戦闘終了後アクションボタン設定
- * @return 処理が完了したら発火するPromise
- */
-const endTutorial = async (
-  props: Readonly<GameProps>,
-  postBattleButtons: PostBattleButtonConfig[]
-) => {
-  await props.domFloaters.showPostBattle(props.resources, postBattleButtons);
-};
-
-/**
  * 戦闘終了時の処理
  * 本関数にはpropsを変更する副作用がある
  *
@@ -114,7 +63,6 @@ export async function onEndBattle(
   props: GameProps,
   action: EndBattle
 ): Promise<void> {
-  const tutorial = createTutorial(props.inProgress, action.gameEnd.result);
   await saveAnimationTimeScale(props, action.animationTimeScale);
 
   if (props.inProgress.type === "NPCBattle" && props.inProgress.subFlow.type === "PlayingNPCBattle") {
@@ -136,7 +84,16 @@ export async function onEndBattle(
       props.resources,
       PostNetworkBattleButtons
     );
-  } else if (tutorial) {
-    //await endTutorial(props, tutorial.postBattleButtons);
+  } else if (
+    props.inProgress.type === "Tutorial" 
+    && props.inProgress.subFlow.type === "PlayingTutorialStage"
+    && action.gameEnd.result.type === "GameOver"
+  ) {
+    const isPlayerWin =
+      action.gameEnd.result.winner === props.inProgress.subFlow.stage.player.playerId;
+    const postBattleButtons = isPlayerWin
+      ? PostTutorialWinButtons
+      : PostTutorialLoseButtons;
+    await props.domFloaters.showPostBattle(props.resources, postBattleButtons);
   }
 }
