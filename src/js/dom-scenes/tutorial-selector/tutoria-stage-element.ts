cf4349bcc1257c94bc0ed08f47c08cab7c7234f4
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-14 20:44:34 +0900

    スタイルをさらに調整

--- src/js/dom-scenes/tutorial-selector/tutoria-stage-element.ts
+++ src/js/dom-scenes/tutorial-selector/tutoria-stage-element.ts
@@ -18,7 +18,7 @@ const ROOT_CLASS = "tutorial-stage";
  */
 function rootInnerHTML(level: number, title: string): string {
   return `
-    <div class="${ROOT_CLASS}__prefix">Tutorial ${level}</div>
+    <div class="${ROOT_CLASS}__prefix">${level}.　</div>
     <div class="${ROOT_CLASS}__title">${title}</div>
   `;
 }
