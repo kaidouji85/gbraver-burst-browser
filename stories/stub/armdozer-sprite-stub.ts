bdcf41d40e2b95191a10febbef372818ca4164df
kaidouji85 <kaidouji85@gmail.com>
2023-02-20 23:03:54 +0900

    アームドーザスプライトスタブ

---
+++ stories/stub/armdozer-sprite-stub.ts
@@ -0,0 +1,34 @@
+import {Resources} from "../../src/js/resource";
+import {Stream} from "../../src/js/stream/stream";
+import {GameObjectAction} from "../../src/js/game-object/action/game-object-action";
+import {ArmDozerSprite} from "../../src/js/game-object/armdozer/armdozer-sprite";
+import {TDGameObjectStub} from "./td-game-object-stub";
+
+/**
+ * アームドーザスプライト ジェネレータ
+ * @param resources リソース管理オブジェクト
+ * @param gameObjectAction ゲームオブジェクトアクション
+ * @return アームドーザスプライト
+ */
+type SpriteGenerator<X extends ArmDozerSprite> = (resources: Resources, gameObjectAction: Stream<GameObjectAction>) => X;
+
+/**
+ * アームドーザスプライト スタブ
+ * @param generator スプライトジェネレータ
+ * @param fn スプライト操作関数
+ * @return ルートHTML要素
+ */
+const armdozerSpriteStub = <X extends ArmDozerSprite>(
+  generator: SpriteGenerator<X>,
+  fn: (sprite: X) => void
+): HTMLElement => {
+  const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
+    const sprite = generator(resources, gameObjectAction);
+    fn(sprite);
+    return {
+      objects: [sprite.getObject3D()],
+    };
+  });
+  stub.start();
+  return stub.domElement();
+};
