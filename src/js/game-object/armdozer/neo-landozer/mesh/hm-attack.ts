67ec0e80ce08d98ee8c512af4047d4940ab7c26a
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-21 06:50:04 +0000

    アームハンマーメッシュを改善

--- src/js/game-object/armdozer/neo-landozer/mesh/hm-attack.ts
+++ src/js/game-object/armdozer/neo-landozer/mesh/hm-attack.ts
@@ -1,18 +1,27 @@
+import * as THREE from "three";
+
 import type { Resources } from "../../../../resource";
 import { TEXTURE_IDS } from "../../../../resource/texture/ids";
 import type { ArmdozerAnimation } from "../../mesh/armdozer-animation";
-import { createHorizontalAnimationFromResources } from "../../mesh/horizontal-animation";
+import { createHorizontalAnimation } from "../../mesh/horizontal-animation";
 import { MESH_Y } from "./position";
+
 export const MAX_ANIMATION = 4;
 export const MESH_WIDTH = 600;
 export const MESH_HEIGHT = 600;
 
-/** ネオラインドーザ アームハンマー攻撃 */
+/** 
+ * ネオラインドーザ アームハンマー攻撃
+ * @param resources リソース管理オブジェクト
+ * @return 生成したメッシュ
+ */
 export function neoLandozerHMAttack(resources: Resources): ArmdozerAnimation {
-  const ret = createHorizontalAnimationFromResources({
-    id: TEXTURE_IDS.NEO_LANDOZER_HM_ATTACK,
+  const texture =
+  resources.textures.find((v) => v.id === TEXTURE_IDS.NEO_LANDOZER_HM_ATTACK)
+    ?.texture ?? new THREE.Texture();
+  const ret = createHorizontalAnimation({
+    texture,
     maxAnimation: MAX_ANIMATION,
-    resources: resources,
     width: MESH_WIDTH,
     height: MESH_HEIGHT,
   });
