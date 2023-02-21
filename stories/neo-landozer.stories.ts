c332570ccc2c138277e26780a384f5d8299e0fe2
Y.Takeuchi <kaidouji85@gmail.com>
2023-02-21 04:56:49 +0000

    立ちストーリー改善

--- stories/neo-landozer.stories.ts
+++ stories/neo-landozer.stories.ts
@@ -1,9 +1,32 @@
 import { delay } from "../src/js/animation/delay";
-import { PlayerNeoLandozer } from "../src/js/game-object/armdozer/neo-landozer";
+import { EnemyNeoLandozer, PlayerNeoLandozer } from "../src/js/game-object/armdozer/neo-landozer";
+import { armdozerSpriteStub } from "./stub/armdozer-sprite-stub";
 import { TDGameObjectStub } from "./stub/td-game-object-stub";
+
 export default {
   title: "neo-landozer",
 };
+
+/** プレイヤー 立ち */
+export const playerStand = () => armdozerSpriteStub(PlayerNeoLandozer, () => {
+  // NOP
+});
+
+/** プレイヤー アクティブ 立ち */
+export const playerActiveStand = () => armdozerSpriteStub(PlayerNeoLandozer, (sprite) => {
+  sprite.startActive().play();
+});
+
+/** 敵 立ち */
+export const enemyStand = () => armdozerSpriteStub(EnemyNeoLandozer, () => {
+  // NOP
+});
+
+/** 敵 アクティブ 立ち */
+export const enemyActiveStand = () => armdozerSpriteStub(EnemyNeoLandozer, (sprite) => {
+  sprite.startActive().play();
+});
+
 export const activeStand = (): HTMLElement => {
   const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
     const sprite = PlayerNeoLandozer(resources, gameObjectAction);
