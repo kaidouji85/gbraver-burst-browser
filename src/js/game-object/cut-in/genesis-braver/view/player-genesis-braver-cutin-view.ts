3601bf8f5c0b3a5621fbace2e18debdcfb83ca25
Y.Takeuchi <kaidouji85@gmail.com>
2023-03-02 04:14:16 +0000

    スケールとか色々実装

--- src/js/game-object/cut-in/genesis-braver/view/player-genesis-braver-cutin-view.ts
+++ src/js/game-object/cut-in/genesis-braver/view/player-genesis-braver-cutin-view.ts
@@ -1,13 +1,18 @@
 import * as THREE from "three";
 import { PreRender } from "../../../../game-loop/pre-render";
 import { Resources } from "../../../../resource";
+import { HUD_CUT_IN_ZNIDEX } from "../../../hud-zindex";
+import { HUDCutInScale } from "../../../scale";
 import { GenesisBraverCutInModel } from "../model/genesis-braver-cutin-model";
 import { AnimationMeshMapping } from "./animation-mesh-mapping";
 import { GenesisBraverCutInView } from "./genesis-braver-cutin-view";
 import { createMeshes } from "./meshes";
 
+/** ベースとなるpadding top */
+export const BASE_PADDING_TOP = 100;
+
 /** プレイヤー ジェネシスブレイバー カットイン ビュー */
-export class PlayerGenesisBraverCutInView implements GenesisBraverCutInView  {
+export class PlayerGenesisBraverCutInView implements GenesisBraverCutInView {
   /** グループ */
   #group: THREE.Group;
   /** メッシュ */
@@ -20,22 +25,20 @@ export class PlayerGenesisBraverCutInView implements GenesisBraverCutInView  {
   constructor(resources: Resources) {
     this.#group = new THREE.Group();
     this.#meshes = createMeshes(resources);
-    this.#meshes.forEach(({mesh}) => {
+    this.#meshes.forEach(({ mesh }) => {
       this.#group.add(mesh.getObject3D());
     });
   }
 
   /** @override */
   destructor(): void {
-    this.#meshes.forEach(({mesh}) => {
+    this.#meshes.forEach(({ mesh }) => {
       mesh.destructor();
     });
   }
 
   /** @override */
-  engage(model: GenesisBraverCutInModel, action: PreRender): void {
-    this.#group.position.x = model.tracking.x;
-    this.#group.position.y = model.tracking.y;
+  engage(model: GenesisBraverCutInModel, preRender: PreRender): void {
     const currentMesh = this.#meshes.find(v => v.type === model.animation.type);
     if (currentMesh) {
       currentMesh.mesh.animate(model.animation.frame);
@@ -43,13 +46,23 @@ export class PlayerGenesisBraverCutInView implements GenesisBraverCutInView  {
     }
 
     this.#meshes.filter(v => v !== currentMesh)
-      .forEach(({mesh}) => {
+      .forEach(({ mesh }) => {
         mesh.setOpacity(0);
       });
+
+    const scale =
+      HUDCutInScale(preRender.rendererDOM, preRender.safeAreaInset) *
+      model.scale;
+    this.#group.position.x = model.tracking.x;
+    this.#group.position.y = model.tracking.y - BASE_PADDING_TOP * scale;
+    this.#group.position.z = HUD_CUT_IN_ZNIDEX;
+    this.#group.scale.x = scale;
+    this.#group.scale.y = scale;
+    this.#group.scale.z = scale;
   }
 
   /** @override */
   getObject3D(): THREE.Object3D {
-    return this.#group;   
+    return this.#group;
   }
 }
