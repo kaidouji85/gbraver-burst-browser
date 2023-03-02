802d838860355c15e82c4246e39b4aba93784b2f
Y.Takeuchi <kaidouji85@gmail.com>
2023-03-02 04:41:22 +0000

    ストーリーにアニメーションをつけた

--- stories/genesis-braver-cutin.stories.ts
+++ stories/genesis-braver-cutin.stories.ts
@@ -1,15 +1,30 @@
+import { delay } from "../src/js/animation/delay";
 import { enemyGenesisBraverCutIn, playerGenesisBraverCutIn } from "../src/js/game-object/cut-in/genesis-braver";
+import { GenesisBraverCutIn } from "../src/js/game-object/cut-in/genesis-braver/genesis-braver-cutin";
 import { HUDGameObjectStub } from "./stub/hud-game-object-stub"
 
 export default {
   title: "genesis-braver-cut-in",
 };
 
+/**
+ * カットインのストーリー
+ * @param cutIn カットイン
+ */
+const story = (cutIn: GenesisBraverCutIn) => {
+  delay(1000)
+    .chain(cutIn.show())
+    .chain(delay(1000))
+    .chain(cutIn.hidden())
+    .loop();
+};
+
 /** プレイヤーカットイン */
 export const playerCutIn = () => {
   const stub = new HUDGameObjectStub(({resources, gameObjectAction}) => {
-    const cutin = playerGenesisBraverCutIn(resources, gameObjectAction);
-    return [cutin.getObject3D()];
+    const cutIn = playerGenesisBraverCutIn(resources, gameObjectAction);
+    story(cutIn);
+    return [cutIn.getObject3D()];
   });
   stub.start();
   return stub.domElement();
@@ -18,8 +33,9 @@ export const playerCutIn = () => {
 /** 敵カットイン */
 export const enemyCutIn = () => {
   const stub = new HUDGameObjectStub(({resources, gameObjectAction}) => {
-    const cutin = enemyGenesisBraverCutIn(resources, gameObjectAction);
-    return [cutin.getObject3D()];
+    const cutIn = enemyGenesisBraverCutIn(resources, gameObjectAction);
+    story(cutIn);
+    return [cutIn.getObject3D()];
   });
   stub.start();
   return stub.domElement();
