e77418a4c703d4cb85a40ecabe3adaeeec0dfad1
Y.Takeuchi <kaidouji85@gmail.com>
2023-03-03 05:35:03 +0000

    getTracksFromHUDArmdozerをシンプルにした

--- src/js/td-scenes/battle/view/tracking/armdozer-sprite.ts
+++ src/js/td-scenes/battle/view/tracking/armdozer-sprite.ts
@@ -15,7 +15,6 @@ import { ThreeDimensionLayer } from "../td";
 
 /**
  * アームドーザスプライトをトラッキングする
- *
  * @param td 3Dレイヤー
  * @param hud HUDレイヤー
  * @param rendererDOM レンダラDOM
@@ -48,7 +47,6 @@ export function trackingArmdozerSprites(
 
 /**
  * HUDアームドーザからHUDTrackingを取得する
- *
  * @param hudArmdozer 取得元
  * @return 取得結果
  */
@@ -56,73 +54,26 @@ function getTracksFromHUDArmdozer(
   hudArmdozer: HUDArmdozerObjects
 ): HUDTracking[] {
   if (hudArmdozer instanceof ShinBraverHUD) {
-    return getTracksFromShinBraverHUD(hudArmdozer);
+    return [hudArmdozer.cutIn];
   }
 
   if (hudArmdozer instanceof NeoLandozerHUD) {
-    return getTracksFromNeoLandozerHUD(hudArmdozer);
+    return [hudArmdozer.cutIn];
   }
 
   if (hudArmdozer instanceof LightningDozerHUD) {
-    return getTracksFromLightningDozerHUD(hudArmdozer);
+    return [hudArmdozer.cutIn];
   }
 
   if (hudArmdozer instanceof WingDozerHUD) {
-    return getTracksFromWingDozerHUD(hudArmdozer);
+    return [hudArmdozer.cutIn];
   }
 
   return [];
 }
 
 /**
- * シンブレイバーHUDからHUDTrackingを取得する
- *
- * @param shinBraverHUD 取得元
- * @return 取得結果
- */
-function getTracksFromShinBraverHUD(
-  shinBraverHUD: ShinBraverHUD
-): HUDTracking[] {
-  return [shinBraverHUD.cutIn];
-}
-
-/**
- * ネオランドーザHUDからHUDTrackingを取得する
- *
- * @param neoLandozerHUD 取得元
- * @return 取得結果
- */
-function getTracksFromNeoLandozerHUD(
-  neoLandozerHUD: NeoLandozerHUD
-): HUDTracking[] {
-  return [neoLandozerHUD.cutIn];
-}
-
-/**
- * ライトニングドーザHUDからHUDTrackingを取得する
- *
- * @param lightningDozerHUD 取得元
- * @return 取得結果
- */
-function getTracksFromLightningDozerHUD(
-  lightningDozerHUD: LightningDozerHUD
-): HUDTracking[] {
-  return [lightningDozerHUD.cutIn];
-}
-
-/**
- * ウィングドーザHUDからHUDTrackingを取得する
- *
- * @param wingDozerHUD 取得元
- * @return 取得結果
- */
-function getTracksFromWingDozerHUD(wingDozerHUD: WingDozerHUD): HUDTracking[] {
-  return [wingDozerHUD.cutIn];
-}
-
-/**
  * 3D座標系をHUD座標系に変換する
- *
  * @param tdCamera 3Dレイヤーのカメラ
  * @param rendererDOM レンダラDOM
  * @param sprite トラッキングするスプライト
