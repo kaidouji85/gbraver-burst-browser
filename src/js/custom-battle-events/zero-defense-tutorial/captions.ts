be0ad5b193a3d8dee2bb99a1cfb7ffa564dd4f42
Y.Takeuchi <kaidouji85@gmail.com>
2023-03-13 04:08:19 +0000

    WIP 0防御警告編集中

--- src/js/custom-battle-events/zero-defense-tutorial/captions.ts
+++ src/js/custom-battle-events/zero-defense-tutorial/captions.ts
@@ -1,11 +1,5 @@
 /** バースト注釈 */
-export const shouldBurst = [
-  "このまま0防御すると負け確定だ",
-  "バーストでバッテリーを回復しよう",
-];
+export const shouldBurst = ["”バースト” ボタンを押そう"];
 
 /** パイロットスキル注釈 */
-export const shouldPilotSkill = [
-  "このまま0防御すると負け確定だ",
-  "パイロットスキルを発動してバッテリーを回復しよう",
-];
+export const shouldPilotSkill = ["”パイロット” ボタンを押そう"];
