3c16262bcf76a9e7f8c6151a70ea1e0b237b187c
Y.Takeuchi <kaidouji85@gmail.com>
2023-04-20 11:15:20 +0000

    バッテリーボタンを動的生成するようにした

--- src/js/game-dom/mini-controller/dom/root-inner-html.ts
+++ src/js/game-dom/mini-controller/dom/root-inner-html.ts
@@ -1,5 +1,18 @@
+import * as R from "ramda";
 import { ROOT } from "./class-name";
 import { DataIDs } from "./data-ids";
+
+/** バッテリーボタン最大個数 */
+const MAX_BATTERY_BUTTON = 6;
+
+/**
+ * バッテリーボタンHTMLを生成する
+ * @param battery バッテリー値
+ * @return 生成結果
+ */
+const batteryButton = (battery: number) => `
+  <button class="${ROOT}__battery" accesskey="${battery}" data-battery="${battery}">${battery}</button>
+`;
 
 /**
  * ルートHTML要素のinnerHTMLを生成する
@@ -7,14 +20,12 @@ import { DataIDs } from "./data-ids";
  * @return 生成結果
  */
 export function rootInnerHTML(ids: DataIDs): string {
+  const batteryButtons: string = R.times(R.identity, MAX_BATTERY_BUTTON)
+    .map(batteryButton)
+    .join("");
   return `
     <div class="${ROOT}__batteries" data-id="${ids.batteries}">
-      <div class="${ROOT}__battery">0</div>
-      <div class="${ROOT}__battery">1</div>
-      <div class="${ROOT}__battery">2</div>
-      <div class="${ROOT}__battery">3</div>
-      <div class="${ROOT}__battery">4</div>
-      <div class="${ROOT}__battery">5</div>
+      ${batteryButtons}
     </div>
     <button type="button" class="${ROOT}__burst" data-id="${ids.burst}" accesskey="b">バースト(b)</button>
     <button type="button" class="${ROOT}__pilot" data-id="${ids.pilot}" accesskey="p">パイロット(p)</button>
