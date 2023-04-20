526766a51514dffc3b4f2c5997bffe1ee7a55221
Y.Takeuchi <kaidouji85@gmail.com>
2023-04-20 08:28:56 +0000

    レイアウトをシンプルにした

--- src/js/game-dom/mini-controller/dom/root-inner-html.ts
+++ src/js/game-dom/mini-controller/dom/root-inner-html.ts
@@ -6,18 +6,15 @@ import { ROOT } from "./class-name";
  */
 export function rootInnerHTML(): string {
   return `
-    <div class="${ROOT}__attack-label">コウゲキ</div>
-    <div class="${ROOT}__battery-buttons">
-      <button class="${ROOT}__battery-button">0</button>
-      <button class="${ROOT}__battery-button">1</button>
-      <button class="${ROOT}__battery-button">2</button>
-      <button class="${ROOT}__battery-button">3</button>
-      <button class="${ROOT}__battery-button">4</button>
-      <button class="${ROOT}__battery-button">5</button>
+    <div class="${ROOT}__batteries">
+      <button class="${ROOT}__battery">0</button>
+      <button class="${ROOT}__battery">1</button>
+      <button class="${ROOT}__battery">2</button>
+      <button class="${ROOT}__battery">3</button>
+      <button class="${ROOT}__battery">4</button>
+      <button class="${ROOT}__battery">5</button>
     </div>
-    <div class="${ROOT}__burst-label">バースト</div>
-    <button class="${ROOT}__burst-button">発動</button>
-    <div class="${ROOT}__pilot-label">パイロット</div>
-    <button class="${ROOT}__pilot-button">発動</button>
+    <button class="${ROOT}__burst">バースト(b)</button>
+    <button class="${ROOT}__pilot">パイロット(p)</button>
   `;
 }
