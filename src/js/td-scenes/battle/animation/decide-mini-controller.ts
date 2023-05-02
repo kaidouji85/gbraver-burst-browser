1333e7361db33efc484da0dba64864769985df9b
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-02 22:06:19 +0900

    戦闘アニメ速度ボタンが隠れない不具合を修正

--- src/js/td-scenes/battle/animation/decide-mini-controller.ts
+++ src/js/td-scenes/battle/animation/decide-mini-controller.ts
@@ -1,15 +1,19 @@
 import { Animate } from "../../../animation/animate";
 import { delay } from "../../../animation/delay";
 import { BattleSceneView } from "../view";
+import {all} from "../../../animation/all";
 
 /**
  * ミニコントローラー決定アニメーション
- * @param props 戦闘シーンプロパティ
+ * @param view 戦闘シーンビュー
  * @return アニメーションが完了したら発火するPromise
  */
 export function decideMiniController(view: Readonly<BattleSceneView>): Animate {
-  return view.dom.miniController
-    .decided()
-    .chain(delay(200))
-    .chain(view.dom.miniController.hidden());
+  return all(
+    view.dom.miniController
+      .decided()
+      .chain(delay(200))
+      .chain(view.dom.miniController.hidden()),
+    view.hud.gameObjects.timeScaleButton.close(),
+  );
 }
