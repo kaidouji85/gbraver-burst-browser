42ba41f7e5f78bd3e999f17937f5b904cf3a459e
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-02 09:41:03 +0000

    ユニットテスト不具合を修正

--- src/js/game/config/schema/sound-volume.ts
+++ src/js/game/config/schema/sound-volume.ts
@@ -8,19 +8,27 @@ export const SoundVolumes: SoundVolume[] = [
   0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1,
 ];
 
+/** 可能であればNumber型に変換するzod preprocess */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const parseNumber = (origin: any) => 
+  /* eslint-enable */
+  (origin === null || origin === undefined || origin === "")
+    ? null
+    : Number(origin);
+
 /** 音量 zod scehma */
 export const SoundVolumeSchema = z.union([
-  z.preprocess(Number, z.literal(0)),
-  z.preprocess(Number, z.literal(0.1)),
-  z.preprocess(Number, z.literal(0.2)),
-  z.preprocess(Number, z.literal(0.3)),
-  z.preprocess(Number, z.literal(0.4)),
-  z.preprocess(Number, z.literal(0.5)),
-  z.preprocess(Number, z.literal(0.6)),
-  z.preprocess(Number, z.literal(0.7)),
-  z.preprocess(Number, z.literal(0.8)),
-  z.preprocess(Number, z.literal(0.9)),
-  z.preprocess(Number, z.literal(1)),
+  z.preprocess(parseNumber, z.literal(0)),
+  z.preprocess(parseNumber, z.literal(0.1)),
+  z.preprocess(parseNumber, z.literal(0.2)),
+  z.preprocess(parseNumber, z.literal(0.3)),
+  z.preprocess(parseNumber, z.literal(0.4)),
+  z.preprocess(parseNumber, z.literal(0.5)),
+  z.preprocess(parseNumber, z.literal(0.6)),
+  z.preprocess(parseNumber, z.literal(0.7)),
+  z.preprocess(parseNumber, z.literal(0.8)),
+  z.preprocess(parseNumber, z.literal(0.9)),
+  z.preprocess(parseNumber, z.literal(1)),
 ]);
 
 /**
