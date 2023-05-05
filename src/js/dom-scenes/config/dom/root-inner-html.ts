da723f6ddc2ad324b8706de313ffc4f54e973916
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-05 18:18:02 +0900

    インデント調整

--- src/js/dom-scenes/config/dom/root-inner-html.ts
+++ src/js/dom-scenes/config/dom/root-inner-html.ts
@@ -91,63 +91,63 @@ export function rootInnerHTML(
   config: GbraverBurstBrowserConfig
 ): string {
   return `
-      <div class="${ROOT_CLASS}__title">設定</div>
-      <div class="${ROOT_CLASS}__configs">
-        <div class="${ROOT_CLASS}__battle-animation-time-scale">
-          <div class="${ROOT_CLASS}__battle-animation-time-scale-caption">戦闘アニメ再生速度</div>
-          <select class="${ROOT_CLASS}__battle-animation-time-scale-selector"
-            data-id="${ids.battleAnimationTimeScaleSelector}"
-           >
-            ${battleAnimationTimeScaleOptions(config.battleAnimationTimeScale)}
-          </select>
-        </div>
-        <div class="${ROOT_CLASS}__webgl-pixel-ratio">
-          <div class="${ROOT_CLASS}__webgl-pixel-ratio-caption">戦闘画面のピクセルレート</div>
-          <select class="${ROOT_CLASS}__webgl-pixel-ratio-selector"
-            data-id="${ids.webGLPixelRatioSelector}"
-          >
-            ${webGLPixelRatioOptions(config.webGLPixelRatio)}
-          </select>
-        </div>
-        <div class="${ROOT_CLASS}__battle-controller-type">
-          <div class="${ROOT_CLASS}__battle-controller-type-caption">戦闘画面コントローラー</div>
-          <select class="${ROOT_CLASS}__battle-controller-type-selector"
-            data-id="${ids.battleControllerTypeSelector}"
-          >
-            ${battleControllerTypeOptions(config.battleControllerType)}
-          </select>
-        </div>
-        <div class="${ROOT_CLASS}__bgm-volume">
-          <div class="${ROOT_CLASS}__bgm-volume-caption">BGM音量</div>
-          <input class="${ROOT_CLASS}__bgm-volume-selector" 
-            type="range" min="0" max="1" step="0.1" 
-            value="${config.bgmVolume}" 
-            data-id="${ids.bgmVolumeSelector}"
-          >
-          <div class="${ROOT_CLASS}__bgm-volume-value"
-            data-id="${ids.bgmVolumeValue}"
-          >
-          ${soundVolumeLabel(config.bgmVolume)}</div>
-        </div>
-        <div class="${ROOT_CLASS}__se-volume">
-          <div class="${ROOT_CLASS}__se-volume-caption">SE音量</div>
-          <input class="${ROOT_CLASS}__se-volume-selector" type="range" min="0" max="1" step="0.1"
-            value="${config.seVolume}" 
-            data-id="${ids.seVolumeSelector}"
-          >
-          <div class="${ROOT_CLASS}__se-volume-value"
-            data-id="${ids.seVolumeValue}"
-          >
-          ${soundVolumeLabel(config.seVolume)}</div>
-        </div>
+    <div class="${ROOT_CLASS}__title">設定</div>
+    <div class="${ROOT_CLASS}__configs">
+      <div class="${ROOT_CLASS}__battle-animation-time-scale">
+        <div class="${ROOT_CLASS}__battle-animation-time-scale-caption">戦闘アニメ再生速度</div>
+        <select class="${ROOT_CLASS}__battle-animation-time-scale-selector"
+          data-id="${ids.battleAnimationTimeScaleSelector}"
+         >
+          ${battleAnimationTimeScaleOptions(config.battleAnimationTimeScale)}
+        </select>
       </div>
-      <div class="${ROOT_CLASS}__footer">
-        <button class="${ROOT_CLASS}__prev" data-id="${ids.prev}">戻る</button>
-        <button class="${ROOT_CLASS}__config-change"
-          data-id="${ids.configChange}"
+      <div class="${ROOT_CLASS}__webgl-pixel-ratio">
+        <div class="${ROOT_CLASS}__webgl-pixel-ratio-caption">戦闘画面のピクセルレート</div>
+        <select class="${ROOT_CLASS}__webgl-pixel-ratio-selector"
+          data-id="${ids.webGLPixelRatioSelector}"
         >
-          この設定にする
-        </button>
+          ${webGLPixelRatioOptions(config.webGLPixelRatio)}
+        </select>
       </div>
+      <div class="${ROOT_CLASS}__battle-controller-type">
+        <div class="${ROOT_CLASS}__battle-controller-type-caption">戦闘画面コントローラー</div>
+        <select class="${ROOT_CLASS}__battle-controller-type-selector"
+          data-id="${ids.battleControllerTypeSelector}"
+        >
+          ${battleControllerTypeOptions(config.battleControllerType)}
+        </select>
+      </div>
+      <div class="${ROOT_CLASS}__bgm-volume">
+        <div class="${ROOT_CLASS}__bgm-volume-caption">BGM音量</div>
+        <input class="${ROOT_CLASS}__bgm-volume-selector" 
+          type="range" min="0" max="1" step="0.1" 
+          value="${config.bgmVolume}" 
+          data-id="${ids.bgmVolumeSelector}"
+        >
+        <div class="${ROOT_CLASS}__bgm-volume-value"
+          data-id="${ids.bgmVolumeValue}"
+        >
+        ${soundVolumeLabel(config.bgmVolume)}</div>
+      </div>
+      <div class="${ROOT_CLASS}__se-volume">
+        <div class="${ROOT_CLASS}__se-volume-caption">SE音量</div>
+        <input class="${ROOT_CLASS}__se-volume-selector" type="range" min="0" max="1" step="0.1"
+          value="${config.seVolume}" 
+          data-id="${ids.seVolumeSelector}"
+        >
+        <div class="${ROOT_CLASS}__se-volume-value"
+          data-id="${ids.seVolumeValue}"
+        >
+        ${soundVolumeLabel(config.seVolume)}</div>
+      </div>
+    </div>
+    <div class="${ROOT_CLASS}__footer">
+      <button class="${ROOT_CLASS}__prev" data-id="${ids.prev}">戻る</button>
+      <button class="${ROOT_CLASS}__config-change"
+        data-id="${ids.configChange}"
+      >
+        この設定にする
+      </button>
+    </div>
   `;
 }
