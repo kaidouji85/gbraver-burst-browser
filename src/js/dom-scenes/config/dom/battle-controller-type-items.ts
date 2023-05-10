cf4b8744a749d48c5208d5dadda272412e109c1e
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-10 04:58:55 +0000

    キーボードショートカットリストのスタイルを調整

--- src/js/dom-scenes/config/dom/battle-controller-type-items.ts
+++ src/js/dom-scenes/config/dom/battle-controller-type-items.ts
@@ -19,21 +19,60 @@ const battleControllerTypeOptionLabel = (value: BattleControllerType) => {
   }
 };
 
+/** おおきいボタン詳細 */
 const bigButtonDetail = `
-  おおきいボタン詳細
+  <div class="${ROOT_CLASS}__big-button-detail">
+    見栄え重視の、おおきいボタンのコントローラーです。
+  </div>
 `;
 
+/** ミニコントローラー詳細 */
 const miniControllerDetail = `
-  ミニコントローラー詳細
+  <div class="${ROOT_CLASS}__mini-controller-detail">
+    <div class="${ROOT_CLASS}__mini-controller-overview">
+      利便性重視の、ミニコントローラーです。
+      キーボードショートカットに対応しているので、画面共有をしてもこちらのコマンドが漏れません。
+    </div>
+    <div class="${ROOT_CLASS}__mini-controller-keyboard-shortcuts">
+      <div class="${ROOT_CLASS}__mini-controller-keyboard-shortcuts-title">
+        キーボードショートカット（Windows版Chromeの場合）
+      </div>
+      <div class="${ROOT_CLASS}__mini-controller-keyboard-shortcuts-detail">
+        <div class="${ROOT_CLASS}__mini-controller-command">バッテリー</div>
+        <div class="${ROOT_CLASS}__mini-controller-keyboard-shortcut">
+          <kbd class="${ROOT_CLASS}__keyboard-icon">alt</kbd>
+          +
+          <kbd class="${ROOT_CLASS}__keyboard-icon">数字キー（0～8）</kbd>
+        </div>
+        <div class="${ROOT_CLASS}__mini-controller-command">バースト</div>
+        <div class="${ROOT_CLASS}__mini-controller-keyboard-shortcut">
+          <kbd class="${ROOT_CLASS}__keyboard-icon">alt</kbd> 
+          +
+          <kbd class="${ROOT_CLASS}__keyboard-icon">b</kbd>
+        </div>
+        <div class="${ROOT_CLASS}__mini-controller-command">パイロット</div>
+        <div class="${ROOT_CLASS}__mini-controller-keyboard-shortcut">
+          <kbd class="${ROOT_CLASS}__keyboard-icon">alt</kbd>
+          +
+          <kbd class="${ROOT_CLASS}__keyboard-icon">p</kbd>
+        </div>
+      </div>
+    </div>
+  </div>
 `;
 
+/**
+ * 値に応じたコントローラー詳細を取得する
+ * @param value 戦闘画面コントローラー
+ * @return コントローラー詳細
+ */
 const getDetail = (value: BattleControllerType) => {
   switch (value) {
     case "MiniController":
-      return bigButtonDetail;
+      return miniControllerDetail;
     case "BigButton":
     default:
-      return miniControllerDetail;
+      return bigButtonDetail;
   }
 };
 
