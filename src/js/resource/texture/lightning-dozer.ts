cf620adbf220a23cf46bf356ba071b68748e76f2
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-16 07:59:47 +0000

    ライトニングドーザテクスチャ設定を別ファイルに切り出した

---
+++ src/js/resource/texture/lightning-dozer.ts
@@ -0,0 +1,66 @@
+import { TEXTURE_IDS } from "./ids";
+import { TextureConfig } from "./resource";
+
+/** ライトニングドーザのテクスチャ設定をまとめたもの */
+export const LightningDozerTextureConfigs: TextureConfig[] = [
+  {
+    id: TEXTURE_IDS.LIGHTNING_DOZER_STAND,
+    path: "armdozer/lightning-dozer/stand.webp",
+  },
+  {
+    id: TEXTURE_IDS.LIGHTNING_DOZER_HM_CHARGE,
+    path: "armdozer/lightning-dozer/hm-charge.webp",
+  },
+  {
+    id: TEXTURE_IDS.LIGHTNING_DOZER_HM_ATTACK,
+    path: "armdozer/lightning-dozer/hm-attack.webp",
+  },
+  {
+    id: TEXTURE_IDS.LIGHTNING_DOZER_HM_TO_STAND,
+    path: "armdozer/lightning-dozer/hm-to-stand.webp",
+  },
+  {
+    id: TEXTURE_IDS.LIGHTNING_DOZER_KNOCK_BACK,
+    path: "armdozer/lightning-dozer/knock-back.webp",
+  },
+  {
+    id: TEXTURE_IDS.LIGHTNING_DOZER_DOWN,
+    path: "armdozer/lightning-dozer/down.webp",
+  },
+  {
+    id: TEXTURE_IDS.LIGHTNING_DOZER_GUTS_UP,
+    path: "armdozer/lightning-dozer/guts-up.webp",
+  },
+  {
+    id: TEXTURE_IDS.LIGHTNING_DOZER_GUTS_DOWN,
+    path: "armdozer/lightning-dozer/guts-down.webp",
+  },
+  {
+    id: TEXTURE_IDS.LIGHTNING_DOZER_GUTS_TO_STAND,
+    path: "armdozer/lightning-dozer/guts-to-stand.webp",
+  },
+  {
+    id: TEXTURE_IDS.LIGHTNING_DOZER_GUARD,
+    path: "armdozer/lightning-dozer/guard.webp",
+  },
+  {
+    id: TEXTURE_IDS.LIGHTNING_DOZER_CUTIN_UP,
+    path: "armdozer/lightning-dozer/cutin-up.webp",
+  },
+  {
+    id: TEXTURE_IDS.LIGHTNING_DOZER_CUTIN_DOWN,
+    path: "armdozer/lightning-dozer/cutin-down.webp",
+  },
+  {
+    id: TEXTURE_IDS.LIGHTNING_DOZER_BACK_STEP,
+    path: "armdozer/lightning-dozer/back-step.webp",
+  },
+  {
+    id: TEXTURE_IDS.LIGHTNING_DOZER_FRONT_STEP,
+    path: "armdozer/lightning-dozer/front-step.webp",
+  },
+  {
+    id: TEXTURE_IDS.LIGHTNING_DOZER_BURST_BUTTON_ICON,
+    path: "armdozer/lightning-dozer/burst-button-icon.webp",
+  }, 
+];
