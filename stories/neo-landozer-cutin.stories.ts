29fd57dad7ecfbaba6dec3e4b707cd6d47345ff2
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-16 07:05:41 +0000

    ネオランドーザのカットインストーリーを追加

---
+++ stories/neo-landozer-cutin.stories.ts
@@ -0,0 +1,37 @@
+import { Animate } from "../src/js/animation/animate";
+import { delay } from "../src/js/animation/delay";
+import { enemyNeoLandozerCutIn, playerNeoLandozerCutIn } from "../src/js/game-object/cut-in/neo-landozer";
+import { NeoLandozerCutIn } from "../src/js/game-object/cut-in/neo-landozer/neo-landozer-cutin";
+import { HUDGameObjectStub } from "./stub/hud-game-object-stub";
+
+export default {
+  title: "neo-landozer-cutin",
+};
+
+function cutInAnimation(cutIn: NeoLandozerCutIn): Animate {
+  return cutIn
+    .show()
+    .chain(delay(2000))
+    .chain(cutIn.hidden())
+    .chain(delay(2000));
+}
+
+export const Player = (): HTMLElement => {
+  const stub = new HUDGameObjectStub(({ resources, gameObjectAction }) => {
+    const cutIn = playerNeoLandozerCutIn(resources, gameObjectAction);
+    cutInAnimation(cutIn).loop();
+    return [cutIn.getObject3D()];
+  });
+  stub.start();
+  return stub.domElement();
+};
+
+export const Enemy = (): HTMLElement => {
+  const stub = new HUDGameObjectStub(({ resources, gameObjectAction }) => {
+    const cutIn = enemyNeoLandozerCutIn(resources, gameObjectAction);
+    cutInAnimation(cutIn).loop();
+    return [cutIn.getObject3D()];
+  });
+  stub.start();
+  return stub.domElement();
+};
