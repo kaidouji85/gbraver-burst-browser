f8322495d343c0aa70f29064207fc896d29f552c
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-18 04:33:04 +0000

    衝撃波ストーリーを簡略化した

--- stories/shock-wave.stories.ts
+++ stories/shock-wave.stories.ts
@@ -1,39 +1,49 @@
+import { Observable } from "rxjs";
 import { delay } from "../src/js/animation/delay";
 import { enemyShockWave, playerShockWave } from "../src/js/game-object/hitmark/shock-wave";
 import { ShockWave } from "../src/js/game-object/hitmark/shock-wave/shock-wave";
+import { Resources } from "../src/js/resource";
 import { TDGameObjectStub } from "./stub/td-game-object-stub";
+import { GameObjectAction } from "../src/js/game-object/action/game-object-action";
 
 export default {
   title: "shock-wave"
-};
+}
 
-const shockWaveAnimation = (shockWave: ShockWave) => {
+/**
+ * 衝撃波ストーリー
+ * @param generator 衝撃波生成オブジェクト
+ * @param fn 衝撃波を操作する関数
+ * @return story
+ */
+const shockWaveStory = (
+  generator: (resources: Resources, gameObjectAction: Observable<GameObjectAction>) => ShockWave,
+  fn: (shockWave: ShockWave) => void
+) => () => {
+  const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
+    const shockWave = generator(resources, gameObjectAction);
+    fn(shockWave);
+    return {
+      objects: [shockWave.getObject3D()],
+    };
+  });
+  stub.start();
+  return stub.domElement();
+}
+
+/**
+ * ポップアップ
+ * @param shockWave 衝撃波 
+ */
+const popUp = (shockWave: ShockWave) => {
   delay(1000)
     .chain(shockWave.popUp())
     .chain(delay(1000))
     .loop();
 };
 
-export const player = () => {
-  const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
-    const shockWave = playerShockWave(resources, gameObjectAction);
-    shockWaveAnimation(shockWave);
-    return {
-      objects: [shockWave.getObject3D()],
-    };
-  });
-  stub.start();
-  return stub.domElement();
-};
+/** プレイヤー衝撃波 ポップアップ */
+export const player = shockWaveStory(playerShockWave, popUp);
 
-export const enemy = () => {
-  const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
-    const shockWave = enemyShockWave(resources, gameObjectAction);
-    shockWaveAnimation(shockWave);
-    return {
-      objects: [shockWave.getObject3D()],
-    };
-  });
-  stub.start();
-  return stub.domElement();
-};
+/** 敵衝撃波 ポップアップ */
+export const enemy = shockWaveStory(enemyShockWave, popUp);
