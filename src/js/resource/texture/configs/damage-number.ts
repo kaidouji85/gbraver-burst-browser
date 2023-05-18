dd1e500d472be080787e87a921ae58fcf8a10ae5
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-18 09:32:54 +0000

    ダメージ数字のテクスチャ設定を別ファイルに切り出した

---
+++ src/js/resource/texture/configs/damage-number.ts
@@ -0,0 +1,10 @@
+import { TEXTURE_IDS } from "../ids";
+import { TextureConfig } from "../resource";
+
+/** ダメージ数字のテクスチャ設定をあつめたもの */
+export const DamageNumberTextureConfigs: TextureConfig[] = [
+  {
+    id: TEXTURE_IDS.DAMAGE_NUMBER,
+    path: "damage-indicator/damage-number.webp",
+  },
+];
