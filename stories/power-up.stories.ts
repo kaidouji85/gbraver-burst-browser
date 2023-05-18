cd56a7ca53e6db407c0f2e238031fdc33ecdc406
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-18 12:04:10 +0000

    攻撃アップストーリーを改良

--- stories/power-up.stories.ts
+++ stories/power-up.stories.ts
@@ -1,28 +1,46 @@
+import { Observable } from "rxjs";
 import { delay } from "../src/js/animation/delay";
 import { enemyPowerUp, playerPowerUp } from "../src/js/game-object/power-up";
+import { Resources } from "../src/js/resource";
 import { TDGameObjectStub } from "./stub/td-game-object-stub";
+import { GameObjectAction } from "../src/js/game-object/action/game-object-action";
+import { PowerUp } from "../src/js/game-object/power-up/power-up";
+
 export default {
   title: "power-up",
 };
-export const player = (): HTMLElement => {
+
+/**
+ * 攻撃アップのストーリー
+ * @param generator 攻撃アップ生成関数
+ * @param fn 攻撃アップ操作関数
+ * @return story
+ */
+const powerUpStory = (
+  generator: (resources: Resources, gameObjectAction: Observable<GameObjectAction>) => PowerUp,
+  fn: (powerUp: PowerUp) => void
+) => () => {
   const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
-    const continuousAttack = playerPowerUp(resources, gameObjectAction);
-    delay(1000).chain(continuousAttack.popUp()).loop();
+    const powerUp = generator(resources, gameObjectAction);
+    fn(powerUp);
     return {
-      objects: [continuousAttack.getObject3D()],
+      objects: [powerUp.getObject3D()],
     };
   });
   stub.start();
   return stub.domElement();
 };
-export const enemy = (): HTMLElement => {
-  const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
-    const continuousAttack = enemyPowerUp(resources, gameObjectAction);
-    delay(1000).chain(continuousAttack.popUp()).loop();
-    return {
-      objects: [continuousAttack.getObject3D()],
-    };
-  });
-  stub.start();
-  return stub.domElement();
+
+/**
+ * ポップアップ
+ * @param powerUp 攻撃アップ
+ */
+const popUp = (powerUp: PowerUp) => {
+  delay(1000).chain(powerUp.popUp()).loop();
 };
+
+/** プレイヤー 攻撃アップ ポップアップ */
+export const playerPopUp = powerUpStory(playerPowerUp, popUp);
+
+/** 敵 攻撃アップ ポップアップ */
+export const enemyPopUp = powerUpStory(enemyPowerUp, popUp);
