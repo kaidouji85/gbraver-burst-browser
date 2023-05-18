99c2437d17aa7f6767e096efc69127a5941d790c
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-18 09:44:57 +0000

    lint、コードフォーマット

--- stories/damage.stories.ts
+++ stories/damage.stories.ts
@@ -1,13 +1,14 @@
 import { Observable } from "rxjs";
+
 import { delay } from "../src/js/animation/delay";
+import { GameObjectAction } from "../src/js/game-object/action/game-object-action";
 import {
   enemyDamageIndicator,
   playerDamageIndicator,
 } from "../src/js/game-object/damage-indicator";
+import { DamageIndicator } from "../src/js/game-object/damage-indicator/damage-indicator";
 import { Resources } from "../src/js/resource";
 import { TDGameObjectStub } from "./stub/td-game-object-stub";
-import { GameObjectAction } from "../src/js/game-object/action/game-object-action";
-import { DamageIndicator } from "../src/js/game-object/damage-indicator/damage-indicator";
 
 export default {
   title: "damage-indicator",
@@ -19,24 +20,29 @@ export default {
  * @param fn ダメージインジケータ操作関数
  * @return story
  */
-const damageIndicatorStory = (
-  generator: (resources: Resources, gameObjectAction: Observable<GameObjectAction>) => DamageIndicator,
-  fn: (damageIndicator: DamageIndicator) => void,
-) => () => {
-  const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
-    const damageIndicator = generator(resources, gameObjectAction);
-    fn(damageIndicator);
-    return {
-      objects: [damageIndicator.getObject3D()],
-    };
-  });
-  stub.start();
-  return stub.domElement();
-};
+const damageIndicatorStory =
+  (
+    generator: (
+      resources: Resources,
+      gameObjectAction: Observable<GameObjectAction>
+    ) => DamageIndicator,
+    fn: (damageIndicator: DamageIndicator) => void
+  ) =>
+  () => {
+    const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
+      const damageIndicator = generator(resources, gameObjectAction);
+      fn(damageIndicator);
+      return {
+        objects: [damageIndicator.getObject3D()],
+      };
+    });
+    stub.start();
+    return stub.domElement();
+  };
 
 /**
  * ポップアップ
- * @param damageIndicator ダメージインジケータ 
+ * @param damageIndicator ダメージインジケータ
  */
 const popUp = (damageIndicator: DamageIndicator) => {
   delay(1000).chain(damageIndicator.popUp(1000)).loop();
