310957733c8757be7d58d032c7857360342ac6eb
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-18 04:42:39 +0000

    電撃アタックのstoryを追加した

---
+++ stories/lightning.stories.ts
@@ -0,0 +1,49 @@
+import { Observable } from "rxjs";
+import { Resources } from "../src/js/resource";
+import { GameObjectAction } from "../src/js/game-object/action/game-object-action";
+import { Lightning } from "../src/js/game-object/hitmark/lightning/lightning";
+import { TDGameObjectStub } from "./stub/td-game-object-stub";
+import { delay } from "../src/js/animation/delay";
+import { enemyLightning, playerLightning } from "../src/js/game-object/hitmark/lightning";
+
+export default {
+  title: "lightning"
+};
+
+/**
+ * 電撃アタックストーリー
+ * @param generator 電撃アタック生成関数
+ * @param fn 電撃アタックを操作する関数
+ * @return story
+ */
+const lightingStory = (
+  generator: (resources: Resources, gameObjectAction: Observable<GameObjectAction>) => Lightning,
+  fn: (lightning: Lightning) => void,
+) => () => {
+  const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
+    const shockWave = generator(resources, gameObjectAction);
+    fn(shockWave);
+    return {
+      objects: [shockWave.getObject3D()],
+    };
+  });
+  stub.start();
+  return stub.domElement();
+};
+
+/**
+ * ポップアップ
+ * @param lightning 電撃アタック
+ */
+const popUp = (lightning: Lightning) => {
+  delay(1000)
+    .chain(lightning.popUp())
+    .chain(delay(1000))
+    .loop();
+};
+
+/** プレイヤー 電撃アタック ポップアップ */
+export const playerPopUp = lightingStory(playerLightning, popUp);
+
+/** 敵 電撃アタック ポップアップ */
+export const enemyPopUp = lightingStory(enemyLightning, popUp);
