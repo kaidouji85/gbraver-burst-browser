6fa86bf2a6e057ad0dcf5c98fcadf701e40dda20
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-18 04:44:29 +0000

    lint、コードフォーマット

--- stories/lightning.stories.ts
+++ stories/lightning.stories.ts
@@ -1,13 +1,17 @@
 import { Observable } from "rxjs";
-import { Resources } from "../src/js/resource";
-import { GameObjectAction } from "../src/js/game-object/action/game-object-action";
-import { Lightning } from "../src/js/game-object/hitmark/lightning/lightning";
-import { TDGameObjectStub } from "./stub/td-game-object-stub";
+
 import { delay } from "../src/js/animation/delay";
-import { enemyLightning, playerLightning } from "../src/js/game-object/hitmark/lightning";
+import { GameObjectAction } from "../src/js/game-object/action/game-object-action";
+import {
+  enemyLightning,
+  playerLightning,
+} from "../src/js/game-object/hitmark/lightning";
+import { Lightning } from "../src/js/game-object/hitmark/lightning/lightning";
+import { Resources } from "../src/js/resource";
+import { TDGameObjectStub } from "./stub/td-game-object-stub";
 
 export default {
-  title: "lightning"
+  title: "lightning",
 };
 
 /**
@@ -16,30 +20,32 @@ export default {
  * @param fn 電撃アタックを操作する関数
  * @return story
  */
-const lightingStory = (
-  generator: (resources: Resources, gameObjectAction: Observable<GameObjectAction>) => Lightning,
-  fn: (lightning: Lightning) => void,
-) => () => {
-  const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
-    const shockWave = generator(resources, gameObjectAction);
-    fn(shockWave);
-    return {
-      objects: [shockWave.getObject3D()],
-    };
-  });
-  stub.start();
-  return stub.domElement();
-};
+const lightingStory =
+  (
+    generator: (
+      resources: Resources,
+      gameObjectAction: Observable<GameObjectAction>
+    ) => Lightning,
+    fn: (lightning: Lightning) => void
+  ) =>
+  () => {
+    const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
+      const shockWave = generator(resources, gameObjectAction);
+      fn(shockWave);
+      return {
+        objects: [shockWave.getObject3D()],
+      };
+    });
+    stub.start();
+    return stub.domElement();
+  };
 
 /**
  * ポップアップ
  * @param lightning 電撃アタック
  */
 const popUp = (lightning: Lightning) => {
-  delay(1000)
-    .chain(lightning.popUp())
-    .chain(delay(1000))
-    .loop();
+  delay(1000).chain(lightning.popUp()).chain(delay(1000)).loop();
 };
 
 /** プレイヤー 電撃アタック ポップアップ */
--- stories/shock-wave.stories.ts
+++ stories/shock-wave.stories.ts
@@ -1,14 +1,18 @@
 import { Observable } from "rxjs";
+
 import { delay } from "../src/js/animation/delay";
-import { enemyShockWave, playerShockWave } from "../src/js/game-object/hitmark/shock-wave";
+import { GameObjectAction } from "../src/js/game-object/action/game-object-action";
+import {
+  enemyShockWave,
+  playerShockWave,
+} from "../src/js/game-object/hitmark/shock-wave";
 import { ShockWave } from "../src/js/game-object/hitmark/shock-wave/shock-wave";
 import { Resources } from "../src/js/resource";
 import { TDGameObjectStub } from "./stub/td-game-object-stub";
-import { GameObjectAction } from "../src/js/game-object/action/game-object-action";
 
 export default {
-  title: "shock-wave"
-}
+  title: "shock-wave",
+};
 
 /**
  * 衝撃波ストーリー
@@ -16,30 +20,32 @@ export default {
  * @param fn 衝撃波を操作する関数
  * @return story
  */
-const shockWaveStory = (
-  generator: (resources: Resources, gameObjectAction: Observable<GameObjectAction>) => ShockWave,
-  fn: (shockWave: ShockWave) => void
-) => () => {
-  const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
-    const shockWave = generator(resources, gameObjectAction);
-    fn(shockWave);
-    return {
-      objects: [shockWave.getObject3D()],
-    };
-  });
-  stub.start();
-  return stub.domElement();
-}
+const shockWaveStory =
+  (
+    generator: (
+      resources: Resources,
+      gameObjectAction: Observable<GameObjectAction>
+    ) => ShockWave,
+    fn: (shockWave: ShockWave) => void
+  ) =>
+  () => {
+    const stub = new TDGameObjectStub(({ resources, gameObjectAction }) => {
+      const shockWave = generator(resources, gameObjectAction);
+      fn(shockWave);
+      return {
+        objects: [shockWave.getObject3D()],
+      };
+    });
+    stub.start();
+    return stub.domElement();
+  };
 
 /**
  * ポップアップ
- * @param shockWave 衝撃波 
+ * @param shockWave 衝撃波
  */
 const popUp = (shockWave: ShockWave) => {
-  delay(1000)
-    .chain(shockWave.popUp())
-    .chain(delay(1000))
-    .loop();
+  delay(1000).chain(shockWave.popUp()).chain(delay(1000)).loop();
 };
 
 /** プレイヤー衝撃波 ポップアップ */
