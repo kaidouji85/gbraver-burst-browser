a3189ba854ff6c6c9287274b7f09fe3a4654b895
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-18 10:50:44 +0000

    バッテリーセレクタストーリーを改良

--- stories/battery-selector.stories.ts
+++ stories/battery-selector.stories.ts
@@ -1,20 +1,37 @@
 import { BatterySelector } from "../src/js/game-object/battery-selector";
+import { ButtonLabel } from "../src/js/game-object/battery-selector/model/button-label";
 import { HUDGameObjectStub } from "./stub/hud-game-object-stub";
+
 export default {
   title: "battery-selector",
 };
-export const batterySelector = (): HTMLElement => {
+
+/**
+ * バッテリーセレクタのストーリー
+ * @param initialValue 初期選択バッテリー
+ * @param maxBattery 最大バッテリー
+ * @param enableValue 選択可能な最大値
+ * @param buttonLabel ボタンラベル
+ * @return story
+ */
+const batterySelectorStory = (
+  initialValue: number,
+  maxBattery: number,
+  enableValue: number,
+  buttonLabel: ButtonLabel
+) => () => {
   const stub = new HUDGameObjectStub(({ resources, gameObjectAction }) => {
     const selector: BatterySelector = new BatterySelector({
       resources: resources,
       gameObjectAction: gameObjectAction,
-      maxBattery: 5,
+      maxBattery: maxBattery,
     });
-    selector.open(1, 5, 5, "Attack").play();
+    selector.open(initialValue, maxBattery, enableValue, buttonLabel).play();
     selector.notifyDecision().subscribe((event) => {
       event.preventDefault();
       event.stopPropagation();
       selector.decide().play();
+      console.log(selector.getBattery());
     });
     selector.notifyBatteryPlus().subscribe(() => {
       selector.batteryPlus().play();
@@ -28,52 +45,20 @@ export const batterySelector = (): HTMLElement => {
   return stub.domElement();
 };
 
-export const batterySelector4 = (): HTMLElement => {
-  const stub = new HUDGameObjectStub(({ resources, gameObjectAction }) => {
-    const selector: BatterySelector = new BatterySelector({
-      resources: resources,
-      gameObjectAction: gameObjectAction,
-      maxBattery: 5,
-    });
-    selector.open(1, 4, 4, "Attack").play();
-    selector.notifyDecision().subscribe((event) => {
-      event.preventDefault();
-      event.stopPropagation();
-      selector.decide().play();
-    });
-    selector.notifyBatteryPlus().subscribe(() => {
-      selector.batteryPlus().play();
-    });
-    selector.notifyBatteryMinus().subscribe(() => {
-      selector.batteryMinus().play();
-    });
-    return [selector.getObject3D()];
-  });
-  stub.start();
-  return stub.domElement();
-};
+/** 攻撃 最大値5 */
+export const attack5 = batterySelectorStory(1, 5, 5, "Attack");
 
-export const batterySelector8 = (): HTMLElement => {
-  const stub = new HUDGameObjectStub(({ resources, gameObjectAction }) => {
-    const selector: BatterySelector = new BatterySelector({
-      resources: resources,
-      gameObjectAction: gameObjectAction,
-      maxBattery: 5,
-    });
-    selector.open(1, 8, 8, "Attack").play();
-    selector.notifyDecision().subscribe((event) => {
-      event.preventDefault();
-      event.stopPropagation();
-      selector.decide().play();
-    });
-    selector.notifyBatteryPlus().subscribe(() => {
-      selector.batteryPlus().play();
-    });
-    selector.notifyBatteryMinus().subscribe(() => {
-      selector.batteryMinus().play();
-    });
-    return [selector.getObject3D()];
-  });
-  stub.start();
-  return stub.domElement();
-};
+/** 防御 最大値5 */
+export const defense5 = batterySelectorStory(1, 5, 5, "Defense");
+
+/** 攻撃 最大値4 */
+export const attack4 = batterySelectorStory(1, 4, 4, "Attack");
+
+/** 防御 最大値4 */
+export const defense4 = batterySelectorStory(1, 4, 4, "Defense");
+
+/** 攻撃 最大値8 */
+export const attack8 = batterySelectorStory(1, 8, 8, "Attack");
+
+/** 防御 最大値8 */
+export const defense8 = batterySelectorStory(1, 8, 8, "Defense");
