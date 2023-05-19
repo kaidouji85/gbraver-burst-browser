b94aa4a39db8f40346b95929fce783f334afaba0
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-19 09:41:38 +0000

    リザルトテクスチャ設定を別ファイルに切り出した

---
+++ src/js/resource/texture/configs/result.ts
@@ -0,0 +1,18 @@
+import { TEXTURE_IDS } from "../ids";
+import { TextureConfig } from "../resource";
+
+/** リザルトのテクスチャ設定をあつめたもの */
+export const ResultTextureConfigs: TextureConfig[] = [
+  {
+    id: TEXTURE_IDS.WIN,
+    path: "result/win.png",
+  },
+  {
+    id: TEXTURE_IDS.LOSE,
+    path: "result/lose.png",
+  },
+  {
+    id: TEXTURE_IDS.DRAW,
+    path: "result/draw.png",
+  },
+];
