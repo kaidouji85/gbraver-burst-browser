14aebf7b075c08aaba09186ce6b3a46cd658b87a
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-21 15:38:10 +0900

    その他プライベートメソッドも外出し

---
+++ src/js/game-object/battery-selector/procedure/battery-plus-pop.ts
@@ -0,0 +1,14 @@
+import {Animate} from "../../../animation/animate";
+import {batteryPlusPop as batteryPlusPopAnimate} from "../animation/battery-plus-pop";
+import {BatterySelectorProps} from "../props";
+
+/**
+ * バッテリープラスボタン ポップ
+ * @param props ゲームオブジェクトプロパティ
+ * @return アニメーション
+ */
+export function batteryPlusPop(props: BatterySelectorProps): Animate {
+  props.batteryPlusTween.update();
+  props.batteryPlusTween.removeAll();
+  return batteryPlusPopAnimate(props.model, props.sounds, props.batteryPlusTween);
+}
