89ea440f52f2354a053dd8c151c7c150d6eac7a9
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-23 09:12:50 +0000

    @deprecated追加

--- src/js/custom-battle-events/zero-defense-tutorial/state.ts
+++ src/js/custom-battle-events/zero-defense-tutorial/state.ts
@@ -1,22 +1,18 @@
 import type { GameState } from "gbraver-burst-core";
 
-/** 選択可能なコマンド */
+/** @deprecated 選択可能なコマンド */
 export type SelectableCommands = "BurstOnly" | "PilotSkillOnly" | "All";
 
 /** 0防御チュートリアルステート */
 export type ZeroDefenseTutorialState = {
   /** ステートヒストリー、 beforeLastState開始時に更新される */
   stateHistory: GameState[];
-
-  /** 選択可能なコマンド */
+  /** @deprecated 選択可能なコマンド */
   selectableCommands: SelectableCommands;
-
   /** イントロダクションを再生したか、trueで再生した */
   isIntroductionComplete: boolean;
-
   /** ダメージレースストーリーを再生したか、trueで再生した */
   isDamageRaceComplete: boolean;
-
   /** 0バッテリーチャンスを再生したか、trueで再生した */
   isZeroBatteryChangeComplete: boolean;
 };
