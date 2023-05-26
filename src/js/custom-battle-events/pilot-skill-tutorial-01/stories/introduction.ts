41c3f70c773b2cd8b8b467049353ec0639121b60
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-26 10:44:16 +0000

    序盤を作りこんだ

--- src/js/custom-battle-events/pilot-skill-tutorial-01/stories/introduction.ts
+++ src/js/custom-battle-events/pilot-skill-tutorial-01/stories/introduction.ts
@@ -1,6 +1,7 @@
 import { CustomBattleEvent, CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
-import { activeLeftMessageWindowWithFace } from "../../active-message-window";
-import { scrollLeftMessages } from "../../scroll-messages";
+import { activeLeftMessageWindowWithFace, activeRightMessageWindowWithFace } from "../../active-message-window";
+import { refreshConversation } from "../../invisible-all-message-windows";
+import { scrollLeftMessages, scrollRightMessages } from "../../scroll-messages";
 
 /**
  * 序盤
@@ -15,4 +16,28 @@ export async function introduction(
     ["ツバサ", "「これより摸擬戦を行う"],
     ["姿勢を正して 礼!!」"],
   ]);
+  await refreshConversation(props);
+  activeLeftMessageWindowWithFace(props, "Tsubasa");
+  props.view.dom.leftMessageWindow.messages([
+    "ツバサ",
+    "「よろしくお願いします」",
+  ]);
+  props.view.dom.leftMessageWindow.scrollUp();
+  activeRightMessageWindowWithFace(props, "Shinya");
+  await scrollRightMessages(props, [["シンヤ", "「よろしくお願いします」"]]);
+  await refreshConversation(props, 100);
+  activeLeftMessageWindowWithFace(props, "Tsubasa");
+  await scrollLeftMessages(props, [
+    ["ツバサ", "「今日も元気だな シンヤ"],
+    ["それでは早速だが パイロットスキルについてレクチャーする"],
+    ["ロボとパイロットは自由に組み合わせることができるが"],
+    ["この相性で勝負が決まると言っても過言ではない"],
+    ["……と言われても実感が湧かないと思うので"],
+    ["まずは試合を進めてみよう」"],
+  ]);
+  props.view.dom.leftMessageWindow.darken();
+  activeRightMessageWindowWithFace(props, "Shinya");
+  await scrollRightMessages(props, [["シンヤ", "「了解ッス"]]);
+  await scrollRightMessages(props, [["今日も俺が勝ちますよ ツバサ先輩」"]]);
+  props.view.dom.rightMessageWindow.darken();
 }
