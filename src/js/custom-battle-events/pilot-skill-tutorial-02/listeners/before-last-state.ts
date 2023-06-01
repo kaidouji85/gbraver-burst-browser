cc00b0fb9e42cfae5c0941c920f7d7b2cc16324d
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-01 03:01:06 +0000

    序盤ストーリーをイベントに組み込んだ

--- src/js/custom-battle-events/pilot-skill-tutorial-02/listeners/before-last-state.ts
+++ src/js/custom-battle-events/pilot-skill-tutorial-02/listeners/before-last-state.ts
@@ -1,5 +1,8 @@
 import { LastState } from "../../../td-scenes/battle/custom-battle-event";
+import { invisibleAllMessageWindows } from "../../invisible-all-message-windows";
+import { turnCount } from "../../turn-count";
 import { PilotSkillTutorial02State } from "../state";
+import { introduction } from "../stories/introduction";
 
 /**
  * 最終ステート直前イベント
@@ -11,5 +14,11 @@ export async function beforeLastState(
   props: Readonly<LastState>,
   state: Readonly<PilotSkillTutorial02State>
 ): Promise<PilotSkillTutorial02State> {
+  const turn = turnCount(props.stateHistory);
+  if (turn === 1 && !state.isIntroductionComplete) {
+    await introduction(props);
+    invisibleAllMessageWindows(props);
+    return { ...state, isIntroductionComplete: true };
+  }
   return state;
 }
