4e14446c2fad592a700f7bc181e92d82ea7cab93
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-05 22:39:03 +0900

    不要な改行削除

--- src/js/game-dom/message-window/dom/to-root-class.ts
+++ src/js/game-dom/message-window/dom/to-root-class.ts
@@ -11,7 +11,6 @@ import {
 
 /**
  * メッセージウインドウ位置に対応したroot要素class属性を取得する
- *
  * @param position メッセージウインドウ位置
  * @return root要素のclass属性
  */
@@ -19,22 +18,16 @@ export function toRootClass(position: Position): string {
   switch (position) {
     case "Center":
       return ROOT_CLASS;
-
     case "Left":
       return ROOT_CLASS_LEFT;
-
     case "Right":
       return ROOT_CLASS_RIGHT;
-
     case "NearBatterySelector":
       return ROOT_CLASS_NEAR_BATTERY_SELECTOR;
-
     case "NearBurstButton":
       return ROOT_CLASS_NEAR_BURST_BUTTON;
-
     case "NearPilotButton":
       return ROOT_CLASS_NEAR_PILOT_BUTTON;
-
     default:
       return ROOT_CLASS_INVISIBLE;
   }
