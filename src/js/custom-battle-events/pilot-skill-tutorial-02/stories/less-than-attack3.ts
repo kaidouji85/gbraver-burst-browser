833da5ccd0ee95694d3da79a6fd9450e2deb06f0
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-05 12:10:52 +0900

    3未満攻撃の警告ストーリーを追加

---
+++ src/js/custom-battle-events/pilot-skill-tutorial-02/stories/less-than-attack3.ts
@@ -0,0 +1,19 @@
+import {CustomBattleEventProps} from "../../../td-scenes/battle/custom-battle-event";
+import {activeRightMessageWindowWithFace} from "../../active-message-window";
+import {scrollRightMessages} from "../../scroll-messages";
+import {invisibleAllMessageWindows} from "../../invisible-all-message-windows";
+
+/**
+ * 3未満攻撃だと警告
+ * @param props イベントプロパティ
+ * @return ストーリーが完了したら発火するPromise
+ */
+export async function lessThanAttack3(
+  props: Readonly<CustomBattleEventProps>
+): Promise<void> {
+  activeRightMessageWindowWithFace(props, "Gai");
+  await scrollRightMessages(props, [
+    ["ガイ", "「おっと このターンは3攻撃以上するんだったな」"],
+  ]);
+  invisibleAllMessageWindows(props);
+}
