38579791ffcb6bf68d3aac53d0038ffba7dd6c94
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-05 22:30:39 +0900

    class属性を別ファイルに移動

---
+++ src/js/game-dom/message-window/dom/class-name.ts
@@ -0,0 +1,26 @@
+/** ルートHTML要素のclass属性 */
+export const ROOT_CLASS = "message-window";
+
+/** ルートHTML要素が非表示の際のclass属性 */
+export const ROOT_CLASS_INVISIBLE = `${ROOT_CLASS}--invisible`;
+
+/** ルート要素が左側表示されている時のclass属性 */
+export const ROOT_CLASS_LEFT = `${ROOT_CLASS}--left`;
+
+/** ルート要素が右側表示されている時のclass属性 */
+export const ROOT_CLASS_RIGHT = `${ROOT_CLASS}--right`;
+
+/** ルート要素がバッテリーセレクタの隣に表示されている時のclass属性 */
+export const ROOT_CLASS_NEAR_BATTERY_SELECTOR = `${ROOT_CLASS}--near-battery-selector`;
+
+/** ルート要素がバーストボタンの隣に表示されている時のclass属性 */
+export const ROOT_CLASS_NEAR_BURST_BUTTON = `${ROOT_CLASS}--near-burst-button`;
+
+/** ルート要素がパイロットボタンの隣に表示されている時のclass属性 */
+export const ROOT_CLASS_NEAR_PILOT_BUTTON = `${ROOT_CLASS}--near-pilot-button`;
+
+/** 次メッセージアイコンのclass属性 */
+export const NEXT_MESSAGE_ICON_CLASS = `${ROOT_CLASS}__next-message-icon`;
+
+/** 次メッセージアイコン非表示時のclass属性 */
+export const NEXT_MESSAGE_ICON_CLASS_INVISIBLE = `${NEXT_MESSAGE_ICON_CLASS}--invisible`;
--- src/js/game-dom/message-window/message-window.ts
+++ src/js/game-dom/message-window/message-window.ts
@@ -1,36 +1,20 @@
-import { waitFinishAnimation } from "../../dom/animation";
-import { replaceDOM } from "../../dom/replace-dom";
-import type { Resources } from "../../resource";
-import { domUuid } from "../../uuid/dom-uuid";
-import type { FaceOrientation, FaceType } from "./face-graphic";
-import { FaceGraphic } from "./face-graphic";
-
-/** ルートHTML要素のclass属性 */
-const ROOT_CLASS = "message-window";
-
-/** ルートHTML要素が非表示の際のclass属性 */
-const ROOT_CLASS_INVISIBLE = `${ROOT_CLASS}--invisible`;
-
-/** ルート要素が左側表示されている時のclass属性 */
-const ROOT_CLASS_LEFT = `${ROOT_CLASS}--left`;
-
-/** ルート要素が右側表示されている時のclass属性 */
-const ROOT_CLASS_RIGHT = `${ROOT_CLASS}--right`;
-
-/** ルート要素がバッテリーセレクタの隣に表示されている時のclass属性 */
-const ROOT_CLASS_NEAR_BATTERY_SELECTOR = `${ROOT_CLASS}--near-battery-selector`;
-
-/** ルート要素がバーストボタンの隣に表示されている時のclass属性 */
-const ROOT_CLASS_NEAR_BURST_BUTTON = `${ROOT_CLASS}--near-burst-button`;
-
-/** ルート要素がパイロットボタンの隣に表示されている時のclass属性 */
-const ROOT_CLASS_NEAR_PILOT_BUTTON = `${ROOT_CLASS}--near-pilot-button`;
-
-/** 次メッセージアイコンのclass属性 */
-const NEXT_MESSAGE_ICON_CLASS = `${ROOT_CLASS}__next-message-icon`;
-
-/** 次メッセージアイコン非表示時のclass属性 */
-const NEXT_MESSAGE_ICON_CLASS_INVISIBLE = `${NEXT_MESSAGE_ICON_CLASS}--invisible`;
+import {waitFinishAnimation} from "../../dom/animation";
+import {replaceDOM} from "../../dom/replace-dom";
+import type {Resources} from "../../resource";
+import {domUuid} from "../../uuid/dom-uuid";
+import type {FaceOrientation, FaceType} from "./face-graphic";
+import {FaceGraphic} from "./face-graphic";
+import {
+  NEXT_MESSAGE_ICON_CLASS,
+  NEXT_MESSAGE_ICON_CLASS_INVISIBLE,
+  ROOT_CLASS,
+  ROOT_CLASS_INVISIBLE,
+  ROOT_CLASS_LEFT,
+  ROOT_CLASS_NEAR_BATTERY_SELECTOR,
+  ROOT_CLASS_NEAR_BURST_BUTTON,
+  ROOT_CLASS_NEAR_PILOT_BUTTON,
+  ROOT_CLASS_RIGHT
+} from "./dom/class-name";
 
 /** メッセージウインドウ位置 */
 type Position =
