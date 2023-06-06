1862a60866e77247fcee9ed3f87fc935c2d3c4cf
kaidouji85 <kaidouji85@gmail.com>
2023-06-06 12:16:38 +0900

    scrollUpを別ファイルに切り出した

---
+++ src/js/game-dom/message-window/procedure/scroll-up.ts
@@ -0,0 +1,27 @@
+import {waitFinishAnimation} from "../../../dom/animation";
+import {MessageWindowProps} from "../props";
+
+/**
+ * メッセージを上スクロールする
+ * @param props コンポネントプロパティ
+ * @return アニメーションが完了したら発火するPromise
+ */
+export async function scrollUp(
+  props: Readonly<MessageWindowProps>
+): Promise<void> {
+  await waitFinishAnimation(
+    props.messages.animate(
+      [
+        {
+          transform: "translateY(2vh)",
+        },
+        {
+          transform: "translateY(0%)",
+        },
+      ],
+      {
+        duration: 100,
+      }
+    )
+  );
+}
