ec570751bc7b48bad393c0aa96035e80d634cef7
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-06 07:54:33 +0000

    ミニコントローラーのショートカットが使えない不具合を修正した

--- src/js/game-dom/mini-controller/battery-button/procedure/bind-event-lisnters.ts
+++ src/js/game-dom/mini-controller/battery-button/procedure/bind-event-lisnters.ts
@@ -1,6 +1,6 @@
 import { Unsubscribable } from "rxjs";
 
-import { domImmediatePushStream } from "../../../../dom/push-dom";
+import { domPushStream } from "../../../../dom/push-dom";
 import { BatteryButtonProps } from "../props";
 import { onBatteryPush } from "./on-battery-push";
 
@@ -13,7 +13,7 @@ export function bindEventListeners(
   props: BatteryButtonProps
 ): Unsubscribable[] {
   return [
-    domImmediatePushStream(props.root).subscribe((action) => {
+    domPushStream(props.root).subscribe((action) => {
       onBatteryPush(props, action);
     }),
   ];
