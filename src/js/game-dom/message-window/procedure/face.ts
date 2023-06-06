69d85eec03e88fd23436edaf9ea69fdc24ec712f
kaidouji85 <kaidouji85@gmail.com>
2023-06-06 12:20:31 +0900

    faceを別ファイルに切り出した

---
+++ src/js/game-dom/message-window/procedure/face.ts
@@ -0,0 +1,16 @@
+import {FaceType} from "../face-graphic";
+import {MessageWindowProps} from "../props";
+import {getTargetFaceGraphic} from "./get-target-face-graphic";
+
+/**
+ * 顔画像を変更する
+ * @param props メッセージウインドウのプロパティ
+ * @param faceType 変更する顔画像
+ */
+export function face(
+  props: Readonly<MessageWindowProps>,
+  faceType: FaceType
+): void {
+  const target = getTargetFaceGraphic(props);
+  target.face(faceType, props.faceOrientation);
+}
