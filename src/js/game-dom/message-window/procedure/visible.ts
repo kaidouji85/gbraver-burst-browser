fd8414af60e5bab97dc59a769359e772d57f380e
kaidouji85 <kaidouji85@gmail.com>
2023-06-06 12:09:31 +0900

    visibleを別ファイルに切り出した

---
+++ src/js/game-dom/message-window/procedure/visible.ts
@@ -0,0 +1,17 @@
+import {MessageWindowProps} from "../props";
+import {toRootClass} from "../dom/to-root-class";
+import {ROOT_CLASS_INVISIBLE} from "../dom/class-name";
+
+/**
+ * メッセージウインドウの表示状態を設定する
+ * @param props コンポネントプロパティ
+ * @param isVisible trueで表示する
+ */
+export function visible(
+  props: Readonly<MessageWindowProps>,
+  isVisible: boolean
+): void {
+  props.root.className = isVisible
+    ? toRootClass(props.position)
+    : ROOT_CLASS_INVISIBLE;
+}
