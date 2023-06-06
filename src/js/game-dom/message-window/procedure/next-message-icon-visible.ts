6958f76c85d0bde750a64cb57232c50b6b08766f
kaidouji85 <kaidouji85@gmail.com>
2023-06-06 12:23:44 +0900

    nextMessageIconVisibleを別ファイルに切り出した

---
+++ src/js/game-dom/message-window/procedure/next-message-icon-visible.ts
@@ -0,0 +1,16 @@
+import {NEXT_MESSAGE_ICON_CLASS, NEXT_MESSAGE_ICON_CLASS_INVISIBLE} from "../dom/class-name";
+import {MessageWindowProps} from "../props";
+
+/**
+ * 次メッセージアイコンの表示、非表示設定
+ * @param props メッセージウインドウのプロパティ
+ * @param isNextMessageIconVisible 次メッセージアイコンを表示するか、trueで表示する
+ */
+export function nextMessageIconVisible(
+  props: Readonly<MessageWindowProps>,
+  isNextMessageIconVisible: boolean
+): void {
+  props.nextMessageIcon.className = isNextMessageIconVisible
+    ? NEXT_MESSAGE_ICON_CLASS
+    : NEXT_MESSAGE_ICON_CLASS_INVISIBLE;
+}
