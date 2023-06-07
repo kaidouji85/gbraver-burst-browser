a1d2c6f9febb16f7762c3baf9e7a4eb240fb2689
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-07 05:11:51 +0000

    ツバサ先輩スキル発動判定関数を追加

---
+++ src/js/custom-battle-events/pilot-skill-tutorial-01/listeners/is-pilot-skill-activated-by-tsubasa.ts
@@ -0,0 +1,13 @@
+import { CustomStateAnimation } from "../../../td-scenes/battle/custom-battle-event";
+
+/**
+ * ツバサ先輩のパイロットスキル発動か否かを判定する
+ * @param props イベントプロパティ
+ * @return trueでツバサ先輩がスキル発動
+ */
+export function isPilotSkillActivatedByTsubasa(
+  props: Readonly<CustomStateAnimation>
+): boolean {
+  return props.currentState.effect.name === "PilotSkillEffect" &&
+    props.currentState.effect.invokerId !== props.playerId;
+}
