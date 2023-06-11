cc29d00ded5b5e70e866ed63050e1098ca59c5d9
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-11 22:55:23 +0900

    切り出したファイルで置き換え

--- src/js/dom-scenes/tutorial-selector/tutorial-selector.ts
+++ src/js/dom-scenes/tutorial-selector/tutorial-selector.ts
@@ -1,31 +1,16 @@
-import {Observable, Subject, Unsubscribable} from "rxjs";
+import {Observable, Unsubscribable} from "rxjs";
 
-import {pop} from "../../dom/animation";
-import {domPushStream, PushDOM} from "../../dom/push-dom";
-import {Exclusive} from "../../exclusive/exclusive";
 import type {Resources} from "../../resource";
-import type {SoundResource} from "../../resource/sound";
-import {createEmptySoundResource, SOUND_IDS} from "../../resource/sound";
-import {domUuid} from "../../uuid/dom-uuid";
-import {waitElementLoaded} from "../../wait/wait-element-loaded";
 import type {DOMScene} from "../dom-scene";
 import type {TutorialStage, TutorialStageSelect,} from "./tutoria-stage-element";
-import {TutorialStageElement} from "./tutoria-stage-element";
-import {ROOT_CLASS} from "./dom/class-name";
-import {rootInnerHTML} from "./dom/root-inner-html";
-import {extractElements} from "./dom/elements";
-import {stageSeparator} from "./dom/stage-separator";
+import {createTutorialSelectorProps, TutorialSelectorProps} from "./props";
+import {bindEventListeners} from "./listeners/bind-event-listeners";
 
 /** チュートリアルステージセレクト画面 */
 export class TutorialSelector implements DOMScene {
-  #root: HTMLElement;
-  #stages: HTMLElement;
-  #prevButton: HTMLElement;
-  #exclusive: Exclusive;
-  #prev: Subject<void>;
-  #stageSelect: Subject<TutorialStageSelect>;
-  #changeValue: SoundResource;
-  #isImageCutsLoaded: Promise<unknown>;
+  /** プロパティ */
+  #props: TutorialSelectorProps;
+  /** アンサブスクライバ */
   #unsubscribers: Unsubscribable[];
 
   /**
@@ -34,50 +19,8 @@ export class TutorialSelector implements DOMScene {
    * @param stages チュートリアルステージ情報
    */
   constructor(resources: Resources, stages: TutorialStage[]) {
-    const ids = {
-      stages: domUuid(),
-      imageCuts: domUuid(),
-      prevButton: domUuid(),
-    };
-    this.#root = document.createElement("div");
-    this.#root.className = ROOT_CLASS;
-    this.#root.innerHTML = rootInnerHTML(ids, resources);
-    const elements = extractElements(this.#root, ids);
-    this.#stages = elements.stages;
-    this.#prevButton = elements.prevButton;
-    this.#exclusive = new Exclusive();
-    this.#prev = new Subject();
-    this.#stageSelect = new Subject();
-    this.#changeValue =
-      resources.sounds.find((v) => v.id === SOUND_IDS.CHANGE_VALUE) ??
-      createEmptySoundResource();
-    this.#isImageCutsLoaded = Promise.all(
-      Array.from(elements.imageCuts.children).map((img) =>
-        waitElementLoaded(img as HTMLElement)
-      )
-    );
-    const stageElements = stages.map(
-      (stage, index) => new TutorialStageElement(resources, stage, index + 1)
-    );
-    const stageElementsWithLastRemoved = stageElements.slice(0, -1);
-    stageElementsWithLastRemoved.forEach((stage) => {
-      this.#stages.appendChild(stage.getRootHTMLElement());
-      this.#stages.appendChild(stageSeparator());
-    });
-    const lastStageElement = stageElements[stageElements.length - 1];
-    if (lastStageElement) {
-      this.#stages.appendChild(lastStageElement.getRootHTMLElement());
-    }
-    this.#unsubscribers = [
-      domPushStream(this.#prevButton).subscribe((action) => {
-        this.#onPrevPush(action);
-      }),
-      ...stageElements.map((stage) =>
-        stage.notifyStageSelection().subscribe(() => {
-          this.#onTutorialStageSelect(stage);
-        })
-      ),
-    ];
+    this.#props = createTutorialSelectorProps(resources, stages);
+    this.#unsubscribers = bindEventListeners(this.#props);
   }
 
   /** @override */
@@ -89,7 +32,7 @@ export class TutorialSelector implements DOMScene {
 
   /** @override */
   getRootHTMLElement(): HTMLElement {
-    return this.#root;
+    return this.#props.root;
   }
 
   /**
@@ -97,7 +40,7 @@ export class TutorialSelector implements DOMScene {
    * @return 待機結果
    */
   async waitUntilLoaded(): Promise<void> {
-    await this.#isImageCutsLoaded;
+    await this.#props.isImageCutsLoaded;
   }
 
   /**
@@ -105,7 +48,7 @@ export class TutorialSelector implements DOMScene {
    * @return 通知ストリーム
    */
   notifyPrev(): Observable<void> {
-    return this.#prev;
+    return this.#props.prev;
   }
 
   /**
@@ -113,34 +56,6 @@ export class TutorialSelector implements DOMScene {
    * @return 通知ストリーム
    */
   notifyStageSelection(): Observable<TutorialStageSelect> {
-    return this.#stageSelect;
-  }
-
-  /**
-   * 戻るボタンを押した時の処理
-   * @param action アクション
-   */
-  #onPrevPush(action: PushDOM): void {
-    action.event.stopPropagation();
-    action.event.preventDefault();
-    this.#exclusive.execute(async () => {
-      this.#changeValue.sound.play();
-      await pop(this.#prevButton);
-      this.#prev.next();
-    });
-  }
-
-  /**
-   * チュートリアルステージ選択
-   * @param stage チュートリアルステージ HTML要素
-   */
-  #onTutorialStageSelect(stage: TutorialStageElement): void {
-    this.#exclusive.execute(async () => {
-      await stage.selected();
-      this.#stageSelect.next({
-        id: stage.id,
-        level: stage.level,
-      });
-    });
+    return this.#props.stageSelect;
   }
 }
