a63d8eaf4edcbdf499b23c858dc6c6fe22db265c
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-11 22:14:09 +0900

    Elementsを別ファイルに切り出した

--- src/js/dom-scenes/tutorial-selector/tutorial-selector.ts
+++ src/js/dom-scenes/tutorial-selector/tutorial-selector.ts
@@ -12,38 +12,8 @@ import type {DOMScene} from "../dom-scene";
 import type {TutorialStage, TutorialStageSelect,} from "./tutoria-stage-element";
 import {TutorialStageElement} from "./tutoria-stage-element";
 import {ROOT_CLASS} from "./dom/class-name";
-import {DataIDs} from "./dom/data-ids";
 import {rootInnerHTML} from "./dom/root-inner-html";
-
-/** ルート要素の子孫要素 */
-type Elements = {
-  stages: HTMLElement;
-  imageCuts: HTMLElement;
-  prevButton: HTMLElement;
-};
-
-/**
- * ルート要素から子孫要素を抽出する
- * @param root ルート要素
- * @param ids data-idを集めたもの
- * @return 抽出結果
- */
-function extractElements(root: HTMLElement, ids: DataIDs): Elements {
-  const stages: HTMLElement =
-    root.querySelector(`[data-id="${ids.stages}"]`) ??
-    document.createElement("div");
-  const imageCuts: HTMLElement =
-    root.querySelector(`[data-id="${ids.imageCuts}"]`) ??
-    document.createElement("div");
-  const prevButton: HTMLElement =
-    root.querySelector(`[data-id="${ids.prevButton}"]`) ??
-    document.createElement("div");
-  return {
-    stages,
-    imageCuts,
-    prevButton,
-  };
-}
+import {extractElements} from "./dom/elements";
 
 /**
  * ステージセパレータを生成する
