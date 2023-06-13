a60e04690745548037037254aa6623314becf05d
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-13 18:41:17 +0900

    「0防御は即死」ストーリーの再生条件を変更

--- src/js/custom-battle-events/pilot-skill-tutorial-01/listeners/on-battery-command-selected.ts
+++ src/js/custom-battle-events/pilot-skill-tutorial-01/listeners/on-battery-command-selected.ts
@@ -3,9 +3,40 @@ import {
   CommandCanceled,
 } from "../../../td-scenes/battle/custom-battle-event";
 import { invisibleAllMessageWindows } from "../../invisible-all-message-windows";
-import { turnCount } from "../../turn-count";
 import { PilotSkillTutorial01State } from "../state";
 import { noZeroDefense } from "../stories/no-zero-defense";
+
+/**
+ * 条件を満たした場合「0防御は即死」を再生する
+ * @param props イベントプロパティ
+ * @return 再生した否か、trueで再生した
+ */
+async function executeNoZeroDefenseIfNeeded(
+  props: Readonly<BatteryCommandSelected>
+): Promise<boolean> {
+  const lastState = props.stateHistory[props.stateHistory.length - 1];
+  if (!lastState) {
+    return false;
+  }
+
+  const player = lastState.players.find((v) => v.playerId === props.playerId);
+  if (!player) {
+    return false;
+  }
+
+  const isEnemyTurn = lastState.activePlayerId !== props.playerId;
+  if (
+    isEnemyTurn &&
+    0 < player.armdozer.battery &&
+    props.battery.battery <= 0
+  ) {
+    await noZeroDefense(props);
+    invisibleAllMessageWindows(props);
+    return true;
+  }
+
+  return false;
+}
 
 /** イベント終了情報 */
 type Ret = {
@@ -25,10 +56,8 @@ export async function onBatteryCommandSelected(
   props: Readonly<BatteryCommandSelected>,
   state: Readonly<PilotSkillTutorial01State>
 ): Promise<Ret> {
-  const turn = turnCount(props.stateHistory);
-  if (props.battery.battery === 0 && turn === 1) {
-    await noZeroDefense(props);
-    invisibleAllMessageWindows(props);
+  const isNoZeroDefenseExecuted = await executeNoZeroDefenseIfNeeded(props);
+  if (isNoZeroDefenseExecuted) {
     return {
       cancel: {
         isCommandCanceled: true,
