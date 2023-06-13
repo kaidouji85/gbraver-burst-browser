4b01cce3268d990cb4b77bcf503583864bb44321
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-13 18:22:14 +0900

    0防御は即死ストーリーを追加

---
+++ src/js/custom-battle-events/pilot-skill-tutorial-01/stories/no-zero-defense.ts
@@ -0,0 +1,15 @@
+import {CustomBattleEventProps} from "../../../td-scenes/battle/custom-battle-event";
+import {activeRightMessageWindowWithFace} from "../../active-message-window";
+import {scrollRightMessages} from "../../scroll-messages";
+
+/**
+ * 0防御は即死
+ * @param props イベントプロパティ
+ * @return 処理が完了したら発火するPromise
+ */
+export async function noZeroDefense(
+  props: Readonly<CustomBattleEventProps>
+): Promise<void> {
+  activeRightMessageWindowWithFace(props, "Shinya");
+  await scrollRightMessages(props, [["シンヤ", "「0防御は即死 瞬殺されるところだったッス」"]]);
+}
