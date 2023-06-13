9e97d0a4dd5f263ced3821a6d755426e04d605f3
kaidouji85 <kaidouji85@gmail.com>
2023-06-13 21:57:53 +0900

    プロパティを追加

---
+++ src/js/dom-scenes/tutorial-selector/tutorial-stage-element/props.ts
@@ -0,0 +1,56 @@
+import {TutorialStageID} from "../../../game/tutorial-stages/tutorial-stage";
+import {createEmptySoundResource, SOUND_IDS, SoundResource} from "../../../resource/sound";
+import {map, Observable} from "rxjs";
+import {Resources} from "../../../resource";
+import {TutorialStage} from "./tutorial-stage";
+import {ROOT_CLASS} from "./dom/class-name";
+import {rootInnerHTML} from "./dom/root-inner-html";
+import {domClickStream} from "../../../dom/push-dom";
+
+/** チュートリアルステージ HTML要素 プロパティ */
+export type TutorialStageElementProps = {
+  /** ステージID */
+  readonly id: TutorialStageID;
+  /** ステージレベル */
+  readonly level: number;
+  /** ルートHTML要素 */
+  root: HTMLElement;
+  /** プッシュボタン効果音 */
+  pushButton: SoundResource;
+  /** 選択通知ストリーム */
+  select: Observable<void>;
+};
+
+/**
+ * TutorialStageElementPropsを生成する
+ * @param resources リソース管理オブジェクト
+ * @param stage ステージ情報
+ * @param level レベル
+ * @return 生成結果
+ */
+export function createTutorialStageElementProps(
+  resources: Readonly<Resources>,
+  stage: Readonly<TutorialStage>,
+  level: Readonly<number>
+) {
+  const pushButton =
+    resources.sounds.find((v) => v.id === SOUND_IDS.PUSH_BUTTON) ??
+    createEmptySoundResource();
+  const root = document.createElement("div");
+  root.className = ROOT_CLASS;
+  root.innerHTML = rootInnerHTML(level, stage.title);
+  const select = domClickStream(root).pipe(
+    map((action) => {
+      action.event.preventDefault();
+      action.event.stopPropagation();
+    })
+  );
+
+  return {
+    id: stage.id,
+    level,
+    root,
+    pushButton,
+    select,
+  };
+}
