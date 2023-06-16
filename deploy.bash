07620ea6741cb13052bc58bf1dde0c86e2b3e17f
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-16 22:04:00 +0900

    ビルド系を並列実行するようにした

--- deploy.bash
+++ deploy.bash
@@ -11,11 +11,9 @@ S3_BUCKET=$1
 DISTRIBUTION_ID=$2
 ASSETLINKS_JSON_URI=$3
 
-npm run build:production
+npm run build-for-deployment
 npm run generate-icons
 npm run scale-down-mobile-images
-npm run build:sw
-npm run build:clear-sw
 aws s3 cp "${ASSETLINKS_JSON_URI}" ./build/production/.well-known/assetlinks.json
 ./upload-module.bash "${S3_BUCKET}"
 ./clear-cdn.bash "${DISTRIBUTION_ID}"
