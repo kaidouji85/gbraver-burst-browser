681af33f576e28d1d3544f462f8ef2b7099d70af
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-23 11:16:12 +0000

    チュートリアル後のボタンをシンプルにした

--- src/js/game/game-procedure/on-end-battle/execute-post-tutorial-battle-if-needed.ts
+++ src/js/game/game-procedure/on-end-battle/execute-post-tutorial-battle-if-needed.ts
@@ -1,27 +1,8 @@
-import { GameOver } from "gbraver-burst-core";
-
-import { PostBattleButtonConfig } from "../../dom-floaters/post-battle/post-battle-button-config";
 import {
-  PostTutorialLoseButtons,
-  PostTutorialWinButtons,
+  PostTutorialButtons,
 } from "../../dom-floaters/post-battle/post-battle-buttons";
 import { EndBattle } from "../../game-actions/end-battle";
 import { GameProps } from "../../game-props";
-import { PlayingTutorialStage } from "../../in-progress/tutorial";
-
-/**
- * チュートリアル終了後に表示するアクションボタンを求める
- * @param gameOver ゲームオーバー情報
- * @param tutorial チュートリアルステージプレイ情報
- * @return 表示するアクションボタン
- */
-const postTutorialBattleButtons = (
-  gameOver: Readonly<GameOver>,
-  tutorial: Readonly<PlayingTutorialStage>
-): PostBattleButtonConfig[] => {
-  const isPlayerWin = gameOver.winner == tutorial.stage.player.playerId;
-  return isPlayerWin ? PostTutorialWinButtons : PostTutorialLoseButtons;
-};
 
 /**
  * 条件を満たした場合、チュートリアル終了後処理を実行する
@@ -43,7 +24,7 @@ export async function executePostTutorialBattleIfNeeded(
 
   await props.domFloaters.showPostBattle(
     props.resources,
-    postTutorialBattleButtons(action.gameEnd.result, props.inProgress.tutorial)
+    PostTutorialButtons
   );
   return true;
 }
