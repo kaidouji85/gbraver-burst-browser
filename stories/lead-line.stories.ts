20fbbe92adc47439b1b068349a826fda062c2650
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-26 02:45:01 +0000

    斜め線ストーリーを追加した

--- stories/lead-line.stories.ts
+++ stories/lead-line.stories.ts
@@ -10,9 +10,12 @@ export default {
  * @param fn 引き出し線操作関数
  */
 const leadLineStory =
-  (fn: (leadLine: Leadline) => void) => () => {
-    const stub = new HUDGameObjectStub(({ resources, gameObjectAction }) => {
-      const leadLine = new Leadline();
+  (
+    generator: () => Leadline,
+    fn: (leadLine: Leadline) => void
+  ) => () => {
+    const stub = new HUDGameObjectStub(() => {
+      const leadLine = generator();
       fn(leadLine);
       return [leadLine.getObject3D()];
     });
@@ -20,6 +23,10 @@ const leadLineStory =
     return stub.domElement();
   };
 
-export const tekitou = leadLineStory((leadLine: Leadline) => {
-  // NOP
+/** 青線 */
+const blueLine = () => new Leadline();
+
+/** 斜め線 */
+export const diagonalLine = leadLineStory(blueLine, (leadLine: Leadline) => {
+  leadLine.set({x: 0, y: 0}, {x: 100, y: 100});
 });
