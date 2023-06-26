f5421ea09ba8be1f1213cf03f5776b94e190bdf4
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-26 02:33:47 +0000

    線設定メソッドを追加した

--- src/js/game-object/lead-line/lead-line.ts
+++ src/js/game-object/lead-line/lead-line.ts
@@ -1,10 +1,15 @@
 import * as THREE from "three";
 
 /** 座標 */
-// type Position = {
-//   x: number;
-//   y: number;
-// };
+type Position = {
+  /** x座標 */
+  x: number;
+  /** y座標 */
+  y: number;
+};
+
+/** ベースとなる線の長さ */
+const BaseLength = 100;
 
 /** 引き出し線 */
 export class Leadline {
@@ -19,14 +24,24 @@ export class Leadline {
     const material = new THREE.LineBasicMaterial({
       color,
     });
-    const points = [new THREE.Vector3(0, 0, 0), new THREE.Vector3(100, 0, 0)];
+    const points = [new THREE.Vector3(0, 0, 0), new THREE.Vector3(BaseLength, 0, 0)];
     const geometry = new THREE.BufferGeometry().setFromPoints(points);
     this.#line = new THREE.Line(geometry, material);
   }
 
-  // set(a: Position, b: Position): void {
-  //
-  // }
+  /**
+   * 引数の点A、点Bを結ぶ線を引き出し線とする
+   * @param a 点A
+   * @param b 点B
+   */
+  set(a: Position, b: Position): void {
+    this.#line.position.x = a.x;
+    this.#line.position.y = a.y;
+    const length = Math.sqrt((b.x - a.x) ** 2 + (b.y - a.y) ** 2);
+    this.#line.scale.x = length / BaseLength;
+    const radian = Math.atan2(b.y - a.y, b.x - a.x);
+    this.#line.rotation.z = radian;
+  }
 
   /**
    * シーンに追加するオブジェクトを取得する
