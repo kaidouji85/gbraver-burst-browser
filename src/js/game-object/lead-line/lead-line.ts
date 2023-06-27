77c8cd35e2078f55b50cdec681c30a69570fc9fb
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-27 22:48:14 +0900

    モデル、ビューを組み込んだ

--- src/js/game-object/lead-line/lead-line.ts
+++ src/js/game-object/lead-line/lead-line.ts
@@ -1,20 +1,14 @@
 import * as THREE from "three";
-
-/** 座標 */
-type Position = {
-  /** x座標 */
-  x: number;
-  /** y座標 */
-  y: number;
-};
-
-/** ベースとなる線の長さ */
-const BaseLength = 100;
+import {LeadLineModel, Position} from "./model/lead-line-model";
+import {LeadLineView} from "./view/lead-line-view";
+import {initialValue} from "./model/initial-value";
 
 /** 引き出し線 */
 export class LeadLine {
-  /** メッシュ */
-  #mesh: THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial>;
+  /** モデル */
+  #model: LeadLineModel;
+  /** ビュー */
+  #view: LeadLineView;
 
   /**
    * コンストラクタ
@@ -22,34 +16,25 @@ export class LeadLine {
    * @param width 線の太さ
    */
   constructor(color: THREE.ColorRepresentation, width: number) {
-    const geometry = new THREE.PlaneGeometry(BaseLength, width);
-    const material = new THREE.MeshBasicMaterial({
-      color,
-      side: THREE.DoubleSide,
-    });
-    this.#mesh = new THREE.Mesh(geometry, material);
+    this.#model = initialValue();
+    this.#view = new LeadLineView(color, width);
   }
 
   /**
    * デストラクタ相当の処理
    */
   destructor(): void {
-    this.#mesh.material.dispose();
-    this.#mesh.geometry.dispose();
+    this.#view.destructor();
   }
 
   /**
-   * 引数の点A、点Bを結ぶ線を引き出し線とする
-   * @param a 点A
-   * @param b 点B
+   * 引き出し線を設定する
+   * @param start 起点
+   * @param end 終点
    */
-  set(a: Position, b: Position): void {
-    const length = Math.sqrt((b.x - a.x) ** 2 + (b.y - a.y) ** 2);
-    this.#mesh.scale.x = length / BaseLength;
-    this.#mesh.position.x = a.x + (b.x - a.x) / 2;
-    this.#mesh.position.y = a.y + (b.y - a.y) / 2;
-    const radian = Math.atan2(b.y - a.y, b.x - a.x);
-    this.#mesh.rotation.z = radian;
+  set(start: Position, end: Position): void {
+    this.#model.start = start;
+    this.#model.end = end;
   }
 
   /**
@@ -57,6 +42,6 @@ export class LeadLine {
    * @return シーンに追加するオブジェクト
    */
   getObject3D(): THREE.Object3D {
-    return this.#mesh;
+    return this.#view.getObject3D();
   }
 }
