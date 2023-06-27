0b1c3db9a3904bae8055427bc7f9a96ca3e50348
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-27 22:45:02 +0900

    ビューを追加

---
+++ src/js/game-object/lead-line/view/lead-line-view.ts
@@ -0,0 +1,53 @@
+import * as THREE from "three";
+import {LeadLineModel} from "../model/lead-line-model";
+
+/** ベースとなる線の長さ */
+const BaseLength = 100;
+
+/** 引き出し線ビュー */
+export class LeadLineView {
+  /** メッシュ */
+  #mesh: THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial>;
+
+  /**
+   * コンストラクタ
+   * @param color 線の色
+   * @param width 線の太さ
+   */
+  constructor(color: THREE.ColorRepresentation, width: number) {
+    const geometry = new THREE.PlaneGeometry(BaseLength, width);
+    const material = new THREE.MeshBasicMaterial({
+      color,
+      side: THREE.DoubleSide,
+    });
+    this.#mesh = new THREE.Mesh(geometry, material);
+  }
+
+  /**
+   * デストラクタ相当の処理
+   */
+  destructor(): void {
+    this.#mesh.material.dispose();
+    this.#mesh.geometry.dispose();
+  }
+
+  /**
+   * モデルをビューに反映させる
+   * @param model モデル
+   */
+  engage(model: Readonly<LeadLineModel>): void {
+    const length = Math.sqrt((model.end.x - model.start.x) ** 2 + (model.end.y - model.start.y) ** 2);
+    this.#mesh.scale.x = length / BaseLength;
+    this.#mesh.position.x = model.start.x + (model.end.x - model.start.x) / 2;
+    this.#mesh.position.y = model.start.y + (model.end.y - model.start.y) / 2;
+    this.#mesh.rotation.z = Math.atan2(model.end.y - model.start.y, model.end.x - model.start.x);
+  }
+
+  /**
+   * シーンに追加するオブジェクトを取得する
+   * @return シーンに追加するオブジェクト
+   */
+  getObject3D(): THREE.Object3D {
+    return this.#mesh;
+  }
+}
