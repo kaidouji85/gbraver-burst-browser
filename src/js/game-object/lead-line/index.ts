50bd66e6d3ca8d6859d3539e3e871600eadb25b4
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-28 10:00:34 +0000

    各種ボタン専用の引き出し線を追加した

--- src/js/game-object/lead-line/index.ts
+++ src/js/game-object/lead-line/index.ts
@@ -5,6 +5,7 @@ import { LeadLine } from "./lead-line";
 import { LeadLineView } from "./view/lead-line-view";
 
 /**
+ * @deprecated
  * 戦闘画面ボタンの引き出し線を生成する
  * @param gameObjectAction ゲームオブジェクトアクション
  * @return 引き出し線
@@ -13,5 +14,41 @@ export function battleButtonLeadLine(
   gameObjectAction: Observable<GameObjectAction>
 ): LeadLine {
   const view = new LeadLineView(0xdcdcdc, 90, 0.8);
+  return new LeadLine(view, gameObjectAction);
+}
+
+/**
+ * バッテリーセレクタの引き出し線を生成する
+ * @param gameObjectAction ゲームオブジェクトアクション
+ * @return 引き出し線
+ */
+export function batterySelectorLeadLine(
+  gameObjectAction: Observable<GameObjectAction>
+): LeadLine {
+  const view = new LeadLineView(0xdcdcdc, 90, 0.8);
+  return new LeadLine(view, gameObjectAction);
+}
+
+/**
+ * バーストボタンの引き出し線を生成する
+ * @param gameObjectAction ゲームオブジェクトアクション
+ * @return 引き出し線
+ */
+export function burstButtonLeadLine(
+  gameObjectAction: Observable<GameObjectAction>
+): LeadLine {
+  const view = new LeadLineView(0xdcdcdc, 45, 0.8);
+  return new LeadLine(view, gameObjectAction);
+}
+
+/**
+ * パイロットボタンの引き出し線を生成する
+ * @param gameObjectAction ゲームオブジェクトアクション
+ * @return 引き出し線
+ */
+export function pilotButtonLeadLine(
+  gameObjectAction: Observable<GameObjectAction>
+): LeadLine {
+  const view = new LeadLineView(0xdcdcdc, 45, 0.8);
   return new LeadLine(view, gameObjectAction);
 }
