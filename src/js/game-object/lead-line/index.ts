a1c39acb03b7d99d51eeaf6384bad67724d6fd45
Y.Takeuchi <kaidouji85@gmail.com>
2023-06-28 10:11:07 +0000

    不要なモジュールを削除

--- src/js/game-object/lead-line/index.ts
+++ src/js/game-object/lead-line/index.ts
@@ -5,19 +5,6 @@ import { LeadLine } from "./lead-line";
 import { LeadLineView } from "./view/lead-line-view";
 
 /**
- * @deprecated
- * 戦闘画面ボタンの引き出し線を生成する
- * @param gameObjectAction ゲームオブジェクトアクション
- * @return 引き出し線
- */
-export function battleButtonLeadLine(
-  gameObjectAction: Observable<GameObjectAction>
-): LeadLine {
-  const view = new LeadLineView(0xdcdcdc, 90, 0.8);
-  return new LeadLine(view, gameObjectAction);
-}
-
-/**
  * バッテリーセレクタの引き出し線を生成する
  * @param gameObjectAction ゲームオブジェクトアクション
  * @return 引き出し線
