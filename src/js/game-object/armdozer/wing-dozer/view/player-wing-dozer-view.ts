c9e1f1e008a41e6d4ff46e8238970fd97d1dff87
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-02 22:48:21 +0900

    ウィングドーザのアクティブフラッシュを調整

--- src/js/game-object/armdozer/wing-dozer/view/player-wing-dozer-view.ts
+++ src/js/game-object/armdozer/wing-dozer/view/player-wing-dozer-view.ts
@@ -64,7 +64,7 @@ export class PlayerWingDozerView implements WingDozerView {
     );
     if (currentActiveMesh) {
       const activeOpacity =
-        (0.4 + model.active.strength * 0.07) * model.active.opacity;
+        (0.4 + model.active.strength * 0.1) * model.active.opacity;
       currentActiveMesh.mesh.opacity(activeOpacity);
       currentActiveMesh.mesh.animate(model.animation.frame);
     }
