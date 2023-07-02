2ff171476b4a6c5dceddd3727b0d4bf1e8a52526
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-02 11:19:05 +0900

    線メッシュ生成ヘルパー関数を追加

---
+++ src/js/game-object/lead-line/view/base-line-length.ts
@@ -0,0 +1,2 @@
+/** ベースとなる線の長さ */
+export const BaseLineLength = 100;
---
+++ src/js/game-object/lead-line/view/line.ts
@@ -0,0 +1,25 @@
+import * as THREE from "three";
+import {SPRITE_RENDER_ORDER} from "../../../render/render-order/td-render-order";
+import {BaseLineLength} from "./base-line-length";
+
+/**
+ * 線メッシュを生成する
+ * @param color 線の色
+ * @param width 線の幅
+ * @return 生成結果
+ */
+export function line(color: THREE.ColorRepresentation, width: number): THREE.Mesh<THREE.BufferGeometry, THREE.MeshBasicMaterial> {
+  const geometry = new THREE.BufferGeometry().setFromPoints([
+    new THREE.Vector3(BaseLineLength, 0, 0),
+    new THREE.Vector3(0, width / 2, 0),
+    new THREE.Vector3(0, -width / 2, 0),
+  ]);
+  const material = new THREE.MeshBasicMaterial({
+    color,
+    side: THREE.DoubleSide,
+    transparent: true,
+  });
+  const line = new THREE.Mesh(geometry, material);
+  line.renderOrder = SPRITE_RENDER_ORDER;
+  return line;
+}
