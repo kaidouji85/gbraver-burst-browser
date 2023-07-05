d1085c709b6ec6b1b21dc70a61e8edadc3baff6c
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-05 10:35:44 +0000

    @deprecated追加

--- src/js/game-object/armdozer/neo-landozer/mesh/silhouette-texture.ts
+++ src/js/game-object/armdozer/neo-landozer/mesh/silhouette-texture.ts
@@ -11,6 +11,7 @@ export const ACTIVE_COLOR_G = 100;
 export const ACTIVE_COLOR_B = 100;
 
 /**
+ * @deprecated
  * アクティブ用にシルエット化したテクスチャを生成する
  * @param texture 加工前のテクスチャ
  * @return シルエット化したテクスチャ
@@ -36,6 +37,7 @@ export const OUTLINE_COLOR_G = 255;
 export const OUTLINE_COLOR_B = 255;
 
 /**
+ * @deprecated
  * アウトライン用にシルエット化したテクスチャを生成する
  * @param texture 加工前のテクスチャ
  * @return シルエット化したテクスチャ
