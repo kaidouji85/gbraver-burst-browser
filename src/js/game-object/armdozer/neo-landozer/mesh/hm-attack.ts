537d6424b08bed6152739df22161f0ad4b88d16b
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-05 22:01:43 +0900

    アームハンマー攻撃改良

--- src/js/game-object/armdozer/neo-landozer/mesh/hm-attack.ts
+++ src/js/game-object/armdozer/neo-landozer/mesh/hm-attack.ts
@@ -1,14 +1,14 @@
-import * as THREE from "three";
-
 import type { Resources } from "../../../../resource";
 import { TEXTURE_IDS } from "../../../../resource/texture/ids";
 import type { ArmdozerAnimation } from "../../mesh/armdozer-animation";
-import { createHorizontalAnimation } from "../../mesh/horizontal-animation";
 import { MESH_Y } from "./position";
+import {createNeoLandozerMesh} from "./create-mesh";
+import {MESH_HEIGHT, MESH_WIDTH} from "./mesh-size";
 
+/** アニメーション枚数 */
 export const MAX_ANIMATION = 4;
-export const MESH_WIDTH = 600;
-export const MESH_HEIGHT = 600;
+/** テクスチャID */
+export const TEXTURE_ID = TEXTURE_IDS.NEO_LANDOZER_HM_ATTACK;
 
 /**
  * ネオラインドーザ アームハンマー攻撃
@@ -16,17 +16,12 @@ export const MESH_HEIGHT = 600;
  * @return 生成したメッシュ
  */
 export function neoLandozerHMAttack(resources: Resources): ArmdozerAnimation {
-  const texture =
-    resources.textures.find((v) => v.id === TEXTURE_IDS.NEO_LANDOZER_HM_ATTACK)
-      ?.texture ?? new THREE.Texture();
-  const ret = createHorizontalAnimation({
-    texture,
+  return createNeoLandozerMesh({
+    resources,
+    textureId: TEXTURE_ID,
     maxAnimation: MAX_ANIMATION,
     width: MESH_WIDTH,
     height: MESH_HEIGHT,
+    positionY: MESH_Y,
   });
-  const object = ret.getObject3D();
-  object.position.y = MESH_Y;
-  object.position.z = 1;
-  return ret;
 }
