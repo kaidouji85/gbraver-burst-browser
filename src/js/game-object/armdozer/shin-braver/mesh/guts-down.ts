f8e25923a4024aa4d882741ae45119ab41bd8c4e
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-05 10:02:30 +0000

    ガッツダウン改良

--- src/js/game-object/armdozer/shin-braver/mesh/guts-down.ts
+++ src/js/game-object/armdozer/shin-braver/mesh/guts-down.ts
@@ -1,30 +1,30 @@
-import * as THREE from "three";
-
 import type { Resources } from "../../../../resource";
 import { TEXTURE_IDS } from "../../../../resource/texture/ids";
 import type { ArmdozerAnimation } from "../../mesh/armdozer-animation";
-import { createHorizontalAnimation } from "../../mesh/horizontal-animation";
 import { MESH_Y } from "./position";
+import { createShinBraverMesh } from "./create-mesh";
+
+/** メッシュ幅 */
 export const MESH_WIDTH = 600;
+/** メッシュ高 */
 export const MESH_HEIGHT = 600;
+/** テクスチャID */
+export const TEXTURE_ID = TEXTURE_IDS.SHIN_BRAVER_GUTS_DOWN;
+/** アニメーション枚数 */
 export const MAX_ANIMATION = 4;
 
 /**
- * 腕下げガッツ
+ * ガッツダウン
  * @param resources リソース管理オブジェクト
  * @return 生成したメッシュ
  */
 export function shinBraverGutsDown(resources: Resources): ArmdozerAnimation {
-  const texture =
-    resources.textures.find((v) => v.id === TEXTURE_IDS.SHIN_BRAVER_GUTS_DOWN)
-      ?.texture ?? new THREE.Texture();
-  const ret = createHorizontalAnimation({
-    texture,
+  return createShinBraverMesh({
+    resources,
+    textureId: TEXTURE_ID,
     maxAnimation: MAX_ANIMATION,
     width: MESH_WIDTH,
     height: MESH_HEIGHT,
+    positionY: MESH_Y,
   });
-  const object = ret.getObject3D();
-  object.position.y = MESH_Y;
-  return ret;
 }
