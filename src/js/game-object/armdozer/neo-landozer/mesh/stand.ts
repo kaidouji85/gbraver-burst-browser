4810ddb330266a929afc09a20f1adcfe695d16ea
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-05 06:20:23 +0000

    アウトライン立ちメッシュ追加

--- src/js/game-object/armdozer/neo-landozer/mesh/stand.ts
+++ src/js/game-object/armdozer/neo-landozer/mesh/stand.ts
@@ -4,15 +4,14 @@ import type { Resources } from "../../../../resource";
 import { TEXTURE_IDS } from "../../../../resource/texture/ids";
 import type { ArmdozerAnimation } from "../../mesh/armdozer-animation";
 import { createHorizontalAnimation } from "../../mesh/horizontal-animation";
-import { createActiveSilhouetteTexture } from "./silhouette-texture";
+import { createActiveSilhouetteTexture, createOutlineSilhouetteTexture } from "./silhouette-texture";
 import { MESH_Y } from "./position";
+import { OutlineWidth } from "./outline-width";
 
 /** アニメーション枚数 */
 export const MAX_ANIMATION = 1;
-
 /** メッシュ幅 */
 export const MESH_WIDTH = 600;
-
 /** メッシュ高 */
 export const MESH_HEIGHT = 600;
 
@@ -57,5 +56,30 @@ export function neoLandozerActiveStand(
   const object = ret.getObject3D();
   object.position.y = MESH_Y;
   object.position.z = 0.01;
+  return ret;
+}
+
+/**
+ * ネオラインドーザ アウトライン立ち メッシュを生成
+ * @param resources リソース管理オブジェクト
+ * @return メッシュ
+ */
+export function neoLandozerOutlineStand(
+  resources: Resources
+): ArmdozerAnimation {
+  const texture =
+    resources.textures.find((v) => v.id === TEXTURE_IDS.NEO_LANDOZER_STAND)
+      ?.texture ?? new THREE.Texture();
+  const silhouetteTexture = createOutlineSilhouetteTexture(texture);
+  const ret = createHorizontalAnimation({
+    texture: silhouetteTexture,
+    maxAnimation: MAX_ANIMATION,
+    width: MESH_WIDTH + OutlineWidth,
+    height: MESH_HEIGHT + OutlineWidth,
+    blending: THREE.AdditiveBlending,
+  });
+  const object = ret.getObject3D();
+  object.position.y = MESH_Y;
+  object.position.z = -0.01;
   return ret;
 }
