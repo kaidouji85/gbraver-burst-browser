81cf5f48d3d11756668e1366d99dbdf5e5ef9290
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-05 09:23:04 +0000

    バックステップ改良

--- src/js/game-object/armdozer/shin-braver/mesh/back-step.ts
+++ src/js/game-object/armdozer/shin-braver/mesh/back-step.ts
@@ -1,36 +1,30 @@
-import * as THREE from "three";
-
 import type { Resources } from "../../../../resource";
 import { TEXTURE_IDS } from "../../../../resource/texture/ids";
 import type { ArmdozerAnimation } from "../../mesh/armdozer-animation";
-import { createHorizontalAnimation } from "../../mesh/horizontal-animation";
 import { MESH_Y } from "./position";
+import { createShinBraverMesh } from "./create-mesh";
 
 /** メッシュ幅 */
 export const MESH_WIDTH = 600;
-
 /** メッシュ高 */
 export const MESH_HEIGHT = 600;
-
+/** テクスチャID */
+export const TEXTURE_ID = TEXTURE_IDS.SHIN_BRAVER_BACK_STEP
 /** アニメーション枚数 */
 export const MAX_ANIMATION = 4;
 
 /**
- * バックステップメッシュ生成
+ * シンブレイバー バックステップメッシュ生成
  * @param resources リソース管理オブジェクト
  * @return メッシュ
  */
 export function shinBraverBackStep(resources: Resources): ArmdozerAnimation {
-  const texture =
-    resources.textures.find((v) => v.id === TEXTURE_IDS.SHIN_BRAVER_BACK_STEP)
-      ?.texture ?? new THREE.Texture();
-  const ret = createHorizontalAnimation({
-    texture,
+  return createShinBraverMesh({
+    resources,
+    textureId: TEXTURE_ID,
     maxAnimation: MAX_ANIMATION,
     width: MESH_WIDTH,
     height: MESH_HEIGHT,
+    positionY: MESH_Y,
   });
-  const object = ret.getObject3D();
-  object.position.y = MESH_Y;
-  return ret;
 }
