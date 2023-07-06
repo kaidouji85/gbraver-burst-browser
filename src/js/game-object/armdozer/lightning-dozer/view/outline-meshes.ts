5855766b0244b5c3cbaeda5d1f0a07ffdbcd44e7
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-06 02:51:51 +0000

    createOutlineMeshesのひな型追加

---
+++ src/js/game-object/armdozer/lightning-dozer/view/outline-meshes.ts
@@ -0,0 +1,13 @@
+import { Resources } from "../../../../resource";
+import { AnimationMeshMapping } from "./animation-mesh-mapping";
+
+/**
+ * アウトラインアニメーションメッシュマッピングを生成
+ * @param resources リソース管理オブジェクト
+ * @return 生成結果
+ */
+export function createOutlineMeshes(
+  resources: Resources
+): AnimationMeshMapping[] {
+  return [];
+}
