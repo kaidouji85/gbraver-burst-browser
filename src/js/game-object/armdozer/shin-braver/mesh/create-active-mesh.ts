796956b1eda681885cfb67fc23e4f9cfdc95e47a
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-06 02:41:06 +0000

    ファイル名変更

--- src/js/game-object/armdozer/shin-braver/mesh/create-active-mesh.ts
+++ src/js/game-object/armdozer/shin-braver/mesh/create-active-mesh.ts
@@ -6,7 +6,7 @@ import { TextureId } from "../../../../resource/texture/resource";
 import { CanvasDisposeTexture } from "../../../../texture/canvas-dispose-texture";
 import { ArmdozerAnimation } from "../../mesh/armdozer-animation";
 import { createHorizontalAnimation } from "../../mesh/horizontal-animation";
-import { MESH_HEIGHT, MESH_WIDTH } from "./mes-size";
+import { MESH_HEIGHT, MESH_WIDTH } from "./mesh-size";
 import { MESH_Y } from "./position";
 
 /** アクティブレイヤー Red */
