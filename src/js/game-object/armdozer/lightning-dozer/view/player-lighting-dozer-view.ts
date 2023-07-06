55a7597f42d59ae842df9174362dd0496e8d42d8
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-06 04:01:47 +0000

    アウトラインメッシュをビューに統合

--- src/js/game-object/armdozer/lightning-dozer/view/player-lighting-dozer-view.ts
+++ src/js/game-object/armdozer/lightning-dozer/view/player-lighting-dozer-view.ts
@@ -6,6 +6,7 @@ import { createActiveMeshes } from "./active-meshes";
 import type { AnimationMeshMapping } from "./animation-mesh-mapping";
 import type { LightningDozerView } from "./lightning-dozer-view";
 import { createMeshes } from "./meshes";
+import { createOutlineMeshes } from "./outline-meshes";
 
 /** プレイヤー側のライトニングドーザビュー */
 export class PlayerLightingDozerView implements LightningDozerView {
@@ -15,6 +16,8 @@ export class PlayerLightingDozerView implements LightningDozerView {
   #meshes: AnimationMeshMapping[];
   /** アクティブメッシュ */
   #activeMeshes: AnimationMeshMapping[];
+  /** アウトラインメッシュ */
+  #outlineMeshes: AnimationMeshMapping[];
 
   /**
    * コンストラクタ
@@ -24,14 +27,15 @@ export class PlayerLightingDozerView implements LightningDozerView {
     this.#group = new THREE.Group();
     this.#meshes = createMeshes(resources);
     this.#activeMeshes = createActiveMeshes(resources);
-    [...this.#meshes, ...this.#activeMeshes].forEach(({ mesh }) => {
+    this.#outlineMeshes = createOutlineMeshes(resources);
+    [...this.#meshes, ...this.#activeMeshes, ...this.#outlineMeshes].forEach(({ mesh }) => {
       this.#group.add(mesh.getObject3D());
     });
   }
 
   /** @override */
   destructor(): void {
-    [...this.#meshes, ...this.#activeMeshes].forEach(({ mesh }) => {
+    [...this.#meshes, ...this.#activeMeshes, ...this.#outlineMeshes].forEach(({ mesh }) => {
       mesh.destructor();
     });
   }
@@ -41,6 +45,7 @@ export class PlayerLightingDozerView implements LightningDozerView {
     this.#group.position.x = model.position.x;
     this.#group.position.y = model.position.y;
     this.#group.position.z = model.position.z;
+
     const currentMesh = this.#meshes.find(
       (v) => v.type === model.animation.type
     );
@@ -59,9 +64,20 @@ export class PlayerLightingDozerView implements LightningDozerView {
       currentActiveMesh.mesh.animate(model.animation.frame);
     }
 
-    [...this.#meshes, ...this.#activeMeshes]
+    const currentOutlineMesh = this.#outlineMeshes.find(
+      (v) => v.type === model.animation.type
+    );
+    if (currentOutlineMesh) {
+      const outlineOpacity =
+        (0.9 + model.active.strength * 0.1) * model.active.opacity;
+      currentOutlineMesh.mesh.opacity(outlineOpacity);
+      currentOutlineMesh.mesh.animate(model.animation.frame);
+    }
+
+    [...this.#meshes, ...this.#activeMeshes, ...this.#outlineMeshes]
       .filter((v) => v !== currentMesh)
       .filter((v) => v !== currentActiveMesh)
+      .filter((v) => v !== currentOutlineMesh) 
       .forEach(({ mesh }) => {
         mesh.opacity(0);
       });
