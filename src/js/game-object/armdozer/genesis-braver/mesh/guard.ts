dc0a082fe41b7f1566ae4a51a36ef9a74fb0a61e
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-06 10:45:34 +0000

    ガードを改良

--- src/js/game-object/armdozer/genesis-braver/mesh/guard.ts
+++ src/js/game-object/armdozer/genesis-braver/mesh/guard.ts
@@ -1,23 +1,13 @@
-import * as THREE from "three";
-
 import type { Resources } from "../../../../resource";
 import { TEXTURE_IDS } from "../../../../resource/texture/ids";
 import type { ArmdozerAnimation } from "../../mesh/armdozer-animation";
-import { createHorizontalAnimation } from "../../mesh/horizontal-animation";
-import { createSilhouetteTexture } from "./create-silhouette-texture";
-import { MESH_Y } from "./position";
-
-/** スプライト幅 */
-export const WIDTH = 600;
-
-/** スプライト高 */
-export const HEIGHT = 600;
-
-/** アニメーション枚数 */
-export const MAX_ANIMATION = 4;
+import { createGenesisBraverMesh } from "./create-mesh";
+import { createGenesisBraverActiveMesh } from "./create-active-mesh";
 
 /** テクスチャID */
-const MESH_TEXTURE = TEXTURE_IDS.GENESIS_BRAVER_GUARD;
+export const TEXTURE_ID = TEXTURE_IDS.GENESIS_BRAVER_GUARD;
+/** アニメーション枚数 */
+export const MAX_ANIMATION = 4;
 
 /**
  * ジェネシスブレイバー ノックバック ガードを生成
@@ -25,18 +15,11 @@ const MESH_TEXTURE = TEXTURE_IDS.GENESIS_BRAVER_GUARD;
  * @return 生成結果
  */
 export function genesisBraverGuard(resources: Resources): ArmdozerAnimation {
-  const texture =
-    resources.textures.find((v) => v.id === MESH_TEXTURE)?.texture ??
-    new THREE.Texture();
-  const mesh = createHorizontalAnimation({
-    texture,
+  return createGenesisBraverMesh({
+    resources,
+    textureId: TEXTURE_ID,
     maxAnimation: MAX_ANIMATION,
-    width: WIDTH,
-    height: HEIGHT,
   });
-  mesh.getObject3D().position.y = MESH_Y;
-  mesh.getObject3D().position.z = 1;
-  return mesh;
 }
 
 /**
@@ -47,17 +30,9 @@ export function genesisBraverGuard(resources: Resources): ArmdozerAnimation {
 export function genesisBraverActiveGuard(
   resources: Resources
 ): ArmdozerAnimation {
-  const texture =
-    resources.textures.find((v) => v.id === MESH_TEXTURE)?.texture ??
-    new THREE.Texture();
-  const silhouetteTexture = createSilhouetteTexture(texture);
-  const mesh = createHorizontalAnimation({
-    texture: silhouetteTexture,
+  return createGenesisBraverActiveMesh({
+    resources,
+    textureId: TEXTURE_ID,
     maxAnimation: MAX_ANIMATION,
-    width: WIDTH,
-    height: HEIGHT,
   });
-  mesh.getObject3D().position.y = MESH_Y;
-  mesh.getObject3D().position.z = 1;
-  return mesh;
 }
