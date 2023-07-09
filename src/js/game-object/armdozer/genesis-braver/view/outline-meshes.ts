3b2c4e69ae4f5d0137479781d04f25a10124b497
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-09 11:24:32 +0900

    アウトラインメッシュ生成関数追加

---
+++ src/js/game-object/armdozer/genesis-braver/view/outline-meshes.ts
@@ -0,0 +1,13 @@
+import { Resources } from "../../../../resource";
+import { AnimationMeshMapping } from "./animation-mesh-mapping";
+
+/**
+ * アニメーションアクティブメッシュマッピング生成
+ * @param resources リソース管理オブジェクト
+ * @return 生成結果
+ */
+export function createOutlineMeshes(
+  resources: Resources,
+): AnimationMeshMapping[] {
+  return [];
+}
