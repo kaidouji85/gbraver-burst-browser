cfa1bc262fdcc2e92aac68957b373d49e32cead1
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-09 11:43:58 +0900

    立ちメッシュにアウトライン追加

--- src/js/game-object/armdozer/genesis-braver/view/outline-meshes.ts
+++ src/js/game-object/armdozer/genesis-braver/view/outline-meshes.ts
@@ -1,6 +1,7 @@
 import { Resources } from "../../../../resource";
 import { AnimationMeshMapping } from "./animation-mesh-mapping";
 import {genesisBraverOutlineKnockBack} from "../mesh/knock-back";
+import {genesisBraverOutlineStand} from "../mesh/stand";
 
 /**
  * アニメーションアクティブメッシュマッピング生成
@@ -11,6 +12,10 @@ export function createOutlineMeshes(
   resources: Resources,
 ): AnimationMeshMapping[] {
   return [
+    {
+      type: "STAND",
+      mesh: genesisBraverOutlineStand(resources),
+    },
     {
       type: "KNOCK_BACK",
       mesh: genesisBraverOutlineKnockBack(resources)
