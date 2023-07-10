06d5f115f06400f565187dd02dd0955ba3247a3c
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-10 12:45:43 +0900

    ライトニングドーザを調整

--- src/js/game-object/armdozer/lightning-dozer/animation/end-active.ts
+++ src/js/game-object/armdozer/lightning-dozer/animation/end-active.ts
@@ -1,6 +1,8 @@
 import { Animate } from "../../../../animation/animate";
 import { tween } from "../../../../animation/tween";
 import type { LightningDozerModel } from "../model/lightning-dozer-model";
+import {process} from "../../../../animation/process";
+import {ARMDOZER_SPRITE_STANDARD_Z} from "../../position";
 
 /**
  * アクティブ状態を終了する
@@ -8,12 +10,16 @@ import type { LightningDozerModel } from "../model/lightning-dozer-model";
  * @return アニメーション
  */
 export function endActive(model: LightningDozerModel): Animate {
-  return tween(model.active, (t) =>
-    t.to(
-      {
-        opacity: 0,
-      },
-      500,
-    ),
+  return process(() => {
+    model.position.z = ARMDOZER_SPRITE_STANDARD_Z;
+  }).chain(
+    tween(model.active, (t) =>
+      t.to(
+        {
+          opacity: 0,
+        },
+        500,
+      ),
+    )
   );
 }
