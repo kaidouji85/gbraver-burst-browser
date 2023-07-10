b2594af15deba44958e2fc1b91b0002c2ea2ebd1
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-10 12:42:57 +0900

    ネオランドーザ調整

--- src/js/game-object/armdozer/neo-landozer/animation/end-active.ts
+++ src/js/game-object/armdozer/neo-landozer/animation/end-active.ts
@@ -1,6 +1,8 @@
 import { Animate } from "../../../../animation/animate";
 import { tween } from "../../../../animation/tween";
 import type { NeoLandozerModel } from "../model/neo-landozer-model";
+import {process} from "../../../../animation/process";
+import {ARMDOZER_SPRITE_STANDARD_Z} from "../../position";
 
 /**
  * アクティブ状態を終了する
@@ -8,12 +10,16 @@ import type { NeoLandozerModel } from "../model/neo-landozer-model";
  * @return アニメーション
  */
 export function endActive(model: NeoLandozerModel): Animate {
-  return tween(model.active, (t) =>
-    t.to(
-      {
-        opacity: 0,
-      },
-      500,
-    ),
+  return process(() => {
+    model.position.z = ARMDOZER_SPRITE_STANDARD_Z;
+  }).chain(
+    tween(model.active, (t) =>
+      t.to(
+        {
+          opacity: 0,
+        },
+        500,
+      ),
+    )
   );
 }
--- src/js/game-object/armdozer/neo-landozer/animation/start-active.ts
+++ src/js/game-object/armdozer/neo-landozer/animation/start-active.ts
@@ -1,6 +1,8 @@
 import { Animate } from "../../../../animation/animate";
 import { tween } from "../../../../animation/tween";
 import type { NeoLandozerModel } from "../model/neo-landozer-model";
+import {process} from "../../../../animation/process";
+import {ARMDOZER_SPRITE_ATTACKER_Z} from "../../position";
 
 /**
  * アクティブ状態を開始する
@@ -8,12 +10,14 @@ import type { NeoLandozerModel } from "../model/neo-landozer-model";
  * @return アニメーション
  */
 export function startActive(model: NeoLandozerModel): Animate {
-  return tween(model.active, (t) =>
+  return process(() => {
+    model.position.z = ARMDOZER_SPRITE_ATTACKER_Z;
+  }).chain(tween(model.active, (t) =>
     t.to(
       {
         opacity: 1,
       },
       500,
     ),
-  );
+  ));
 }
