18fc5740a8ed14163b0bcfaf4e6e963b8b103e46
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-11 22:27:57 +0900

    アクティブアニメでのz軸変更を削除

--- src/js/game-object/armdozer/neo-landozer/animation/start-active.ts
+++ src/js/game-object/armdozer/neo-landozer/animation/start-active.ts
@@ -1,7 +1,5 @@
 import { Animate } from "../../../../animation/animate";
-import { process } from "../../../../animation/process";
 import { tween } from "../../../../animation/tween";
-import { ARMDOZER_SPRITE_ATTACKER_Z } from "../../position";
 import type { NeoLandozerModel } from "../model/neo-landozer-model";
 
 /**
@@ -10,16 +8,12 @@ import type { NeoLandozerModel } from "../model/neo-landozer-model";
  * @return アニメーション
  */
 export function startActive(model: NeoLandozerModel): Animate {
-  return process(() => {
-    model.position.z = ARMDOZER_SPRITE_ATTACKER_Z;
-  }).chain(
-    tween(model.active, (t) =>
+  return tween(model.active, (t) =>
       t.to(
         {
           opacity: 1,
         },
         200,
       ),
-    ),
-  );
+    );
 }
