080b638355bb5fb61f42cf57d0bdccaa39db38a0
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-12 01:47:19 +0000

    lint、コードフォーマット

--- src/js/animation/align-end.ts
+++ src/js/animation/align-end.ts
@@ -4,14 +4,17 @@ import { delay } from "./delay";
 
 /**
  * 終了時間ぞろえする
- * @param animations 終了時間ぞろえするアニメーション 
+ * @param animations 終了時間ぞろえするアニメーション
  * @return アニメーション
  */
 export function alignEnd(...animations: Animate[]): Animate {
-  const maxDuration = Math.max(0, ...animations.map(animate => animate._time));
+  const maxDuration = Math.max(
+    0,
+    ...animations.map((animate) => animate._time),
+  );
   return all(
-    ...animations.map(animate => 
-      delay(maxDuration - animate._time).chain(animate)
-    )
-  )
+    ...animations.map((animate) =>
+      delay(maxDuration - animate._time).chain(animate),
+    ),
+  );
 }
