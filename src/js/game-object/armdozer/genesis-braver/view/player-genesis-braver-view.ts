727ed5fc08c0e56ec34615e926fa2ee5bc8593a8
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-13 02:24:13 +0000

    旧メッシュ生成系メソッドを削除

--- src/js/game-object/armdozer/genesis-braver/view/player-genesis-braver-view.ts
+++ src/js/game-object/armdozer/genesis-braver/view/player-genesis-braver-view.ts
@@ -2,17 +2,10 @@ import * as THREE from "three";
 
 import type { Resources } from "../../../../resource";
 import type { GenesisBraverModel } from "../model/genesis-braver-model";
-import type { AnimationMeshMapping } from "./animation-mesh-mapping";
 import type { GenesisBraverView } from "./genesis-braver-view";
-import { createMeshes } from "./meshes";
-import { createOutlineMeshes } from "./outline-meshes";
 
 /** プレイヤー ジェネシスブレイバービュー */
 export class PlayerGenesisBraverView implements GenesisBraverView {
-  /** メッシュ */
-  #meshes: AnimationMeshMapping[];
-  /** アウトラインメッシュ */
-  #outlineMeshes: AnimationMeshMapping[];
   /** グループ */
   #group: THREE.Group;
 
@@ -22,18 +15,11 @@ export class PlayerGenesisBraverView implements GenesisBraverView {
    */
   constructor(resources: Resources) {
     this.#group = new THREE.Group();
-    this.#meshes = createMeshes(resources);
-    this.#outlineMeshes = createOutlineMeshes(resources);
-    [...this.#meshes, ...this.#outlineMeshes].forEach(({ mesh }) => {
-      this.#group.add(mesh.getObject3D());
-    });
   }
 
   /** @override */
   destructor() {
-    [...this.#meshes, ...this.#outlineMeshes].forEach(({ mesh }) => {
-      mesh.destructor();
-    });
+
   }
 
   /** @override */
@@ -46,33 +32,6 @@ export class PlayerGenesisBraverView implements GenesisBraverView {
     this.#group.position.x = model.position.x;
     this.#group.position.y = model.position.y;
     this.#group.position.z = model.position.z;
-    const currentMesh = this.#meshes.find(
-      (v) => v.type === model.animation.type,
-    );
-    if (currentMesh) {
-      currentMesh.mesh.opacity(1);
-      currentMesh.mesh.animate(model.animation.frame);
-      const colorStrength =
-        1 - (0.2 + model.active.strength * 0.1) * model.active.opacity;
-      currentMesh.mesh.color(colorStrength, colorStrength, colorStrength);
-    }
-
-    const currentOutlineMesh = this.#outlineMeshes.find(
-      (v) => v.type === model.animation.type,
-    );
-    if (currentOutlineMesh) {
-      const outlineOpacity =
-        (0.9 + model.active.strength * 0.1) * model.active.opacity;
-      currentOutlineMesh.mesh.opacity(outlineOpacity);
-      currentOutlineMesh.mesh.animate(model.animation.frame);
-    }
-
-    [...this.#meshes, ...this.#outlineMeshes]
-      .filter((v) => v !== currentMesh)
-      .filter((v) => v !== currentOutlineMesh)
-      .forEach(({ mesh }) => {
-        mesh.opacity(0);
-      });
   }
 
   /** @override */
