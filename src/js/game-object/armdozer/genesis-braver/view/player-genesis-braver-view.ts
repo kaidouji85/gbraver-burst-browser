72eb7d13df4e334dfc8a6a491ab28020010faf3a
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-13 03:54:41 +0000

    新しいメッシュで置き換え

--- src/js/game-object/armdozer/genesis-braver/view/player-genesis-braver-view.ts
+++ src/js/game-object/armdozer/genesis-braver/view/player-genesis-braver-view.ts
@@ -3,11 +3,15 @@ import * as THREE from "three";
 import type { Resources } from "../../../../resource";
 import type { GenesisBraverModel } from "../model/genesis-braver-model";
 import type { GenesisBraverView } from "./genesis-braver-view";
+import { AnimationMeshMapping } from "../mesh/animation-mesh-mapping";
+import { createMeshes } from "../mesh";
 
 /** プレイヤー ジェネシスブレイバービュー */
 export class PlayerGenesisBraverView implements GenesisBraverView {
   /** グループ */
   #group: THREE.Group;
+  /** メッシュ */
+  #meshes: AnimationMeshMapping[];
 
   /**
    * コンストラクタ
@@ -15,11 +19,17 @@ export class PlayerGenesisBraverView implements GenesisBraverView {
    */
   constructor(resources: Resources) {
     this.#group = new THREE.Group();
+    this.#meshes = createMeshes(resources);
+    this.#meshes.forEach(({mesh}) => {
+      this.#group.add(mesh.getObject3D());
+    });
   }
 
   /** @override */
   destructor() {
-
+    this.#meshes.forEach(({mesh}) => {
+      mesh.destructor();
+    });
   }
 
   /** @override */
@@ -32,6 +42,30 @@ export class PlayerGenesisBraverView implements GenesisBraverView {
     this.#group.position.x = model.position.x;
     this.#group.position.y = model.position.y;
     this.#group.position.z = model.position.z;
+
+    const currentStandardMesh = this.#meshes.find(v => v.meshType === "STANDARD" && v.animationType === model.animation.type);
+    if (currentStandardMesh) {
+      currentStandardMesh.mesh.opacity(1);
+      currentStandardMesh.mesh.animate(model.animation.frame);
+      const colorStrength =
+        1 - (0.2 + model.active.strength * 0.1) * model.active.opacity;
+        currentStandardMesh.mesh.color(colorStrength, colorStrength, colorStrength);
+    }
+
+    const currentOutlineMesh = this.#meshes.find(v => v.meshType === "OUTLINE" && v.animationType === model.animation.type);
+    if (currentOutlineMesh) {
+      const outlineOpacity =
+        (0.9 + model.active.strength * 0.1) * model.active.opacity;
+      currentOutlineMesh.mesh.opacity(outlineOpacity);
+      currentOutlineMesh.mesh.animate(model.animation.frame);
+    }
+
+    this.#meshes
+    .filter((v) => v !== currentStandardMesh)
+    .filter((v) => v !== currentOutlineMesh)
+    .forEach(({ mesh }) => {
+      mesh.opacity(0);
+    });
   }
 
   /** @override */
