41a7c4ec00b4da215b5f15bb8b6e16c3ec38bd02
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-13 02:31:56 +0000

    アニメーションメッシュマッピングを修正

--- src/js/game-object/armdozer/genesis-braver/view/animation-mesh-mapping.ts
+++ src/js/game-object/armdozer/genesis-braver/view/animation-mesh-mapping.ts
@@ -1,11 +1,15 @@
 import type { ArmdozerAnimation } from "../../mesh/armdozer-animation";
 import type { AnimationType } from "../model/animation-type";
 
+/** メッシュタイプ */
+export type MeshType = 'STANDARD' | 'OUTLINE';
+
 /** アニメーション、メッシュマッピング */
 export type AnimationMeshMapping = {
   /** アニメーションタイプ */
-  type: AnimationType;
-
+  animationType: AnimationType;
+  /** メッシュタイプ */
+  meshType: MeshType;
   /** 対応するメッシュ */
   mesh: ArmdozerAnimation;
 };
