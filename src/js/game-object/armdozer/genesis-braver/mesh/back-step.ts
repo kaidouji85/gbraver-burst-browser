6ca1aa132febd745f76be281a9275777193daa03
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-13 02:46:32 +0000

    バックステップを修正

--- src/js/game-object/armdozer/genesis-braver/mesh/back-step.ts
+++ src/js/game-object/armdozer/genesis-braver/mesh/back-step.ts
@@ -1,9 +1,7 @@
 import type { Resources } from "../../../../resource";
 import { TEXTURE_IDS } from "../../../../resource/texture/ids";
-import type { ArmdozerAnimation } from "../../mesh/armdozer-animation";
-import { createGenesisBraverActiveMesh } from "./create-active-mesh";
-import { createStandardMesh } from "./create-standard-mesh";
-import { createOutlineMesh } from "./create-outline-mesh";
+import { AnimationMeshMapping } from "./animation-mesh-mapping";
+import { createMeshesForAnimation } from "./create-meshes-for-animation";
 
 /** テクスチャID */
 export const TEXTURE_ID = TEXTURE_IDS.GENESIS_BRAVER_BACK_STEP;
@@ -11,43 +9,14 @@ export const TEXTURE_ID = TEXTURE_IDS.GENESIS_BRAVER_BACK_STEP;
 export const MAX_ANIMATION = 4;
 
 /**
- * ジェネシスブレイバー バックステップ スプライトを生成
+ * バックステップ メッシュ群を生成
  * @param resources リソース管理オブジェクト
  * @return 生成結果
  */
-export function genesisBraverBackStep(resources: Resources): ArmdozerAnimation {
-  return createStandardMesh({
+export function backStep(resources: Resources): AnimationMeshMapping[] {
+  return createMeshesForAnimation({
     resources,
-    textureId: TEXTURE_ID,
-    maxAnimation: MAX_ANIMATION,
-  });
-}
-
-/**
- * ジェネシスブレイバー アクティブ バックステップ スプライトを生成
- * @param resources リソース管理オブジェクト
- * @return 生成結果
- */
-export function genesisBraverActiveBackStep(
-  resources: Resources,
-): ArmdozerAnimation {
-  return createGenesisBraverActiveMesh({
-    resources,
-    textureId: TEXTURE_ID,
-    maxAnimation: MAX_ANIMATION,
-  });
-}
-
-/**
- * ジェネシスブレイバー アウトライン バックステップ スプライトを生成
- * @param resources リソース管理オブジェクト
- * @return 生成結果
- */
-export function genesisBraverOutlineBackStep(
-  resources: Resources,
-): ArmdozerAnimation {
-  return createOutlineMesh({
-    resources,
+    animationType: "BACK_STEP",
     textureId: TEXTURE_ID,
     maxAnimation: MAX_ANIMATION,
   });
