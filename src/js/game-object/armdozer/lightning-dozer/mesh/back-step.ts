34c6ac6c87457367bd919d0c73d6765c1423eb60
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-14 02:45:00 +0000

    バックステップのメッシュ生成処理を改善

--- src/js/game-object/armdozer/lightning-dozer/mesh/back-step.ts
+++ src/js/game-object/armdozer/lightning-dozer/mesh/back-step.ts
@@ -1,9 +1,7 @@
 import type { Resources } from "../../../../resource";
 import { TEXTURE_IDS } from "../../../../resource/texture/ids";
-import type { ArmdozerAnimation } from "../../mesh/armdozer-animation";
-import { createLightningDozerActiveMesh } from "./create-active-mesh";
-import { createStandardMesh } from "./create-standard-mesh";
-import { createOutlineMesh } from "./create-outline-mesh";
+import { AnimationMeshMapping } from "./animation-mesh-mapping";
+import { createMeshesForAnimation } from "./create-meshes-for-animation";
 
 /** テクスチャID */
 export const TEXTURE_ID = TEXTURE_IDS.LIGHTNING_DOZER_BACK_STEP;
@@ -11,45 +9,14 @@ export const TEXTURE_ID = TEXTURE_IDS.LIGHTNING_DOZER_BACK_STEP;
 export const MAX_ANIMATION = 4;
 
 /**
- * ライトニングドーザ バックステップ
+ * バックステップ メッシュ群を生成
  * @param resources リソース管理オブジェクト
- * @return メッシュ
+ * @return 生成結果
  */
-export function lightningDozerBackStep(
-  resources: Resources,
-): ArmdozerAnimation {
-  return createStandardMesh({
+export function backStep(resources: Resources): AnimationMeshMapping[] {
+  return createMeshesForAnimation({
     resources,
-    textureId: TEXTURE_ID,
-    maxAnimation: MAX_ANIMATION,
-  });
-}
-
-/**
- * ライトニングドーザ アクティブ バックステップ
- * @param resources リソース管理オブジェクト
- * @return メッシュ
- */
-export function lightningDozerActiveBackStep(
-  resources: Resources,
-): ArmdozerAnimation {
-  return createLightningDozerActiveMesh({
-    resources,
-    textureId: TEXTURE_ID,
-    maxAnimation: MAX_ANIMATION,
-  });
-}
-
-/**
- * ライトニングドーザ アウトライン バックステップ
- * @param resources リソース管理オブジェクト
- * @return メッシュ
- */
-export function lightningDozerOutlineBackStep(
-  resources: Resources,
-): ArmdozerAnimation {
-  return createOutlineMesh({
-    resources,
+    animationType: "BACK_STEP",
     textureId: TEXTURE_ID,
     maxAnimation: MAX_ANIMATION,
   });
