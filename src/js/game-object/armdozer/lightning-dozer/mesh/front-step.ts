0e20c59857bc35123cb9af31a6296e0b09399745
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-14 04:45:23 +0000

    ライトニングドーザも同様の対応

--- src/js/game-object/armdozer/lightning-dozer/mesh/front-step.ts
+++ src/js/game-object/armdozer/lightning-dozer/mesh/front-step.ts
@@ -1,7 +1,7 @@
 import type { Resources } from "../../../../resource";
 import { TEXTURE_IDS } from "../../../../resource/texture/ids";
-import { AnimationMeshMapping } from "./animation-mesh-mapping";
-import { createMeshesForAnimation } from "./create-meshes-for-animation";
+import { AnimationMesh } from "./animation-mesh";
+import { createAnimationMeshes } from "./create-animation-meshes";
 
 /** テクスチャID */
 export const TEXTURE_ID = TEXTURE_IDS.LIGHTNING_DOZER_FRONT_STEP;
@@ -13,8 +13,8 @@ export const MAX_ANIMATION = 4;
  * @param resource リソース管理オブジェクト
  * @return 生成結果
  */
-export function frontStep(resource: Resources): AnimationMeshMapping[] {
-  return createMeshesForAnimation({
+export function frontStep(resource: Resources): AnimationMesh[] {
+  return createAnimationMeshes({
     resources: resource,
     animationType: "FRONT_STEP",
     textureId: TEXTURE_ID,
--- src/js/game-object/armdozer/lightning-dozer/mesh/guts-down.ts
+++ src/js/game-object/armdozer/lightning-dozer/mesh/guts-down.ts
@@ -1,7 +1,7 @@
 import type { Resources } from "../../../../resource";
 import { TEXTURE_IDS } from "../../../../resource/texture/ids";
-import { AnimationMeshMapping } from "./animation-mesh-mapping";
-import { createMeshesForAnimation } from "./create-meshes-for-animation";
+import { AnimationMesh } from "./animation-mesh";
+import { createAnimationMeshes } from "./create-animation-meshes";
 
 /** テクスチャID */
 export const TEXTURE_ID = TEXTURE_IDS.LIGHTNING_DOZER_GUTS_DOWN;
@@ -13,8 +13,8 @@ export const MAX_ANIMATION = 4;
  * @param resources リソース管理オブジェクト
  * @return 生成結果
  */
-export function gutsDown(resources: Resources): AnimationMeshMapping[] {
-  return createMeshesForAnimation({
+export function gutsDown(resources: Resources): AnimationMesh[] {
+  return createAnimationMeshes({
     resources,
     animationType: "GUTS_DOWN",
     textureId: TEXTURE_ID,
