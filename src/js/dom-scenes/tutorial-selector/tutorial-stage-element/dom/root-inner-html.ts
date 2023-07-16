6367ce10883f1cb68adc40bfb99d919d2b602787
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-16 15:00:33 +0900

    チュートリアルタイプアイコンを追加

--- src/js/dom-scenes/tutorial-selector/tutorial-stage-element/dom/root-inner-html.ts
+++ src/js/dom-scenes/tutorial-selector/tutorial-stage-element/dom/root-inner-html.ts
@@ -1,5 +1,36 @@
 import { ROOT_CLASS } from "./class-name";
 import { DataIDs } from "./data-ids";
+import {TutorialType} from "../../../../game/tutorial-stages/tutorial-stage";
+
+/** 初級 */
+const beginner = () =>
+  `<div class="${ROOT_CLASS}__beginner">初級</div>`;
+
+/** 中級 */
+const intermediate = () =>
+  `<div class="${ROOT_CLASS}__intermediate">中級</div>`;
+
+/** 上級 */
+const advanced = () =>
+  `<div class="${ROOT_CLASS}__advanced">上級</div>`;
+
+/**
+ * チュートリアル対応に応じたアイコンを生成する
+ * @param tutorialType チュートリアルタイプ
+ * @return アイコン
+ */
+const tutorialType = (tutorialType: TutorialType): string => {
+  switch(tutorialType) {
+    case "Beginner":
+      return beginner();
+    case "Intermediate":
+      return intermediate();
+    case "Advanced":
+      return advanced();
+    default:
+      return beginner();
+  }
+};
 
 /**
  * ルート要素のinnerHTML
