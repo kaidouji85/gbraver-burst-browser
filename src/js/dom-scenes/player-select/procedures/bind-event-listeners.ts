047eb03c272fd06fee27f4ab46723c7757811d18
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-20 07:45:03 +0000

    bindEventListeners実装

---
+++ src/js/dom-scenes/player-select/procedures/bind-event-listeners.ts
@@ -0,0 +1,39 @@
+import { Unsubscribable } from "rxjs";
+
+import { PlayerSelectProps } from "../props";
+import { onArmdozerChange } from "./on-armdozer-change";
+import { onArmdozerDecided } from "./on-armdozer-decided";
+import { onArmdozerSelectorPrev } from "./on-armdozer-selector-prev";
+import { onPilotChange } from "./on-pilot-change";
+import { onPilotDecided } from "./on-pilot-decided";
+import { onPilotSelectorPrev } from "./on-pilot-selector-prev";
+
+/**
+ * シーンにイベントリスナをバインドする
+ * @param props シーンプロパティ
+ * @return アンサブスクライバ
+ */
+export function bindEventListeners(
+  props: Readonly<PlayerSelectProps>
+): Unsubscribable[] {
+  return [
+    props.armdozerSelector.notifyChanges().subscribe((v) => {
+      onArmdozerChange(props, v);
+    }),
+    props.armdozerSelector.notifyDecision().subscribe((v) => {
+      onArmdozerDecided(props, v);
+    }),
+    props.armdozerSelector.notifyPrev().subscribe(() => {
+      onArmdozerSelectorPrev(props);
+    }),
+    props.pilotSelector.notifyChanges().subscribe((v) => {
+      onPilotChange(props, v);
+    }),
+    props.pilotSelector.notifyDecision().subscribe((v) => {
+      onPilotDecided(props, v);
+    }),
+    props.pilotSelector.notifyPrev().subscribe(() => {
+      onPilotSelectorPrev(props);
+    }),
+  ];
+}
