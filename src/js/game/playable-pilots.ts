7c402f2e40ba1b1e30995167d2365ced5f07df36
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-20 09:19:18 +0000

    getPlayablePilots実装

---
+++ src/js/game/playable-pilots.ts
@@ -0,0 +1,30 @@
+import { PilotId, PilotIds } from "gbraver-burst-core";
+
+import { GameProps } from "./game-props";
+
+/** プレイアブルなパイロット */
+export const PlayablePilots: PilotId[] = [
+  PilotIds.SHINYA,
+  PilotIds.TSUBASA,
+  PilotIds.GAI,
+  PilotIds.RAITO,
+];
+
+/** 開発中も含めたプレイアブルなパイロット */
+export const DevelopingPlayablePilots: PilotId[] = [
+  ...PlayablePilots,
+  PilotIds.YUUYA,
+];
+
+/**
+ * プレイアブルパイロットを取得するヘルパー関数
+ * @param props ゲームプロパティ
+ * @return 取得結果
+ */
+export function getPlayablePilots(
+  props: Readonly<GameProps>
+): PilotId[] {
+  return props.canPlayDevelopingPilot
+    ? DevelopingPlayablePilots
+    : PlayablePilots;
+}
