a698ac1d5bbdaf0bd1b5bfc572f1e4ca0034faa9
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-20 07:36:28 +0000

    onPilotChangeを別ファイルに切り出した

---
+++ src/js/dom-scenes/player-select/listeners/on-pilot-change.ts
@@ -0,0 +1,16 @@
+import { PilotId } from "gbraver-burst-core";
+
+import { PlayerSelectProps } from "../props";
+
+/**
+ * パイロットが変更された時の処理
+ * @param props シーンプロパティ
+ * @param pilotId 変更したパイロットID
+ */
+export function onPilotChange(
+  props: PlayerSelectProps,
+  pilotId: PilotId
+): void {
+  props.pilotId = pilotId;
+  props.pilotBustShot.switch(pilotId);
+}
