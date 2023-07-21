5ad282a89e39fc4b16c8b1228af72e222b389361
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-21 10:51:45 +0000

    シンヤのアニメーションをコピーして、ユウヤの雛形を作った

---
+++ src/js/td-scenes/battle/animation/game-state/pilot-skill/yuuya.ts
@@ -0,0 +1,71 @@
+import type { BatteryBoostSkill, PilotSkill } from "gbraver-burst-core";
+
+import { all } from "../../../../../animation/all";
+import { Animate } from "../../../../../animation/animate";
+import { delay, empty } from "../../../../../animation/delay";
+import { ShinyaHUD } from "../../../view/hud/pilot-objects/shinya";
+import { dolly, toInitial, track } from "../../td-camera";
+import type { PilotSkillAnimationParamX } from "./animation-param";
+
+/**
+ * パイロットスキル ユウヤ アニメーションパラメータ
+ * @template SKILL パイロットスキル
+ */
+export type YuuyaAnimationParamX<SKILL extends PilotSkill> =
+  PilotSkillAnimationParamX<SKILL, ShinyaHUD>;
+
+/**
+ * パイロットスキル ユウヤ アニメーションパラメータ
+ */
+export type YuuyaAnimationParam = YuuyaAnimationParamX<PilotSkill>;
+
+/**
+ * ユウヤ パイロットスキルアニメーション
+ * @param param パラメータ
+ * @return アニメーション
+ */
+export function yuuyaAnimation(param: YuuyaAnimationParam): Animate {
+  if (param.skill.type === "BatteryBoostSkill") {
+    const skill: BatteryBoostSkill = param.skill;
+    return yuuyaBatteryBoost({ ...param, skill });
+  }
+
+  return empty();
+}
+
+/**
+ * ユウヤ バッテリーブースト アニメーション
+ * @param param パラメータ
+ * @return アニメーション
+ */
+function yuuyaBatteryBoost(
+  param: YuuyaAnimationParamX<BatteryBoostSkill>,
+): Animate {
+  return all(
+    param.pilot.cutIn.show(),
+    track(param.tdCamera, param.invokerSprite.getObject3D().position.x, 500),
+    dolly(param.tdCamera, "-40", 500),
+    param.tdObjects.skyBrightness.brightness(0.2, 500),
+    param.tdObjects.illumination.intensity(0.2, 500),
+    param.isActivePlayer
+      ? param.invokerSprite.endActive()
+      : param.anotherSprite.endActive(),
+  )
+    .chain(delay(800))
+    .chain(param.pilot.cutIn.hidden())
+    .chain(delay(200))
+    .chain(
+      all(
+        param.invokerHUD.gauge.battery(param.invokerState.armdozer.battery),
+        param.invokerTD.recoverBattery.popUp(param.skill.recoverBattery),
+      ),
+    )
+    .chain(
+      all(
+        toInitial(param.tdCamera, 500),
+        param.tdObjects.skyBrightness.brightness(1, 500),
+        param.tdObjects.illumination.intensity(1, 500),
+      ),
+    )
+    .chain(delay(200));
+}
