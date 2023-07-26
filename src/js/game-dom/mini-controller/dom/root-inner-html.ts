3d960bcc185e039768cc4583e81bf70dae235a33
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-26 06:17:26 +0000

    hbsファイルをimportできるようにした

--- src/js/game-dom/mini-controller/dom/root-inner-html.ts
+++ src/js/game-dom/mini-controller/dom/root-inner-html.ts
@@ -1,5 +1,6 @@
 import { ROOT } from "./class-name";
 import { DataIDs } from "./data-ids";
+import template from "./root-inner-html.hbs";
 
 /**
  * ルートHTML要素のinnerHTMLを生成する
@@ -7,6 +8,7 @@ import { DataIDs } from "./data-ids";
  * @return 生成結果
  */
 export function rootInnerHTML(ids: DataIDs): string {
+  console.log(template);  // TODO 開発が終わったら削除する
   return `
     <div class="${ROOT}__batteries" data-id="${ids.batteries}"></div>
   `;
