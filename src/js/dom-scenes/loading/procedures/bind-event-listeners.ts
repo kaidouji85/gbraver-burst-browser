3f4660856f0c4fe04009b2a833b8b6b78e63674b
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-27 05:17:01 +0000

    bindEventListeners実装

---
+++ src/js/dom-scenes/loading/procedures/bind-event-listeners.ts
@@ -0,0 +1,22 @@
+import { Observable, Unsubscribable } from "rxjs";
+
+import { LoadingActions } from "../../../resource/loading/loading-actions";
+import { LoadingProps } from "../props";
+import { onLoadingProgress } from "./on-loading-progress";
+
+/**
+ * 画面にイベントリストを関連付ける
+ * @param props 画面プロパティ
+ * @param loading ローディングストリーム
+ * @return アンサブスクライバ
+ */
+export function bindEventListeners(
+  props: Readonly<LoadingProps>,
+  loading: Observable<LoadingActions>,
+): Unsubscribable {
+  return loading.subscribe((action) => {
+    if (action.type === "LoadingProgress") {
+      onLoadingProgress(props, action);
+    }
+  });
+}
