13db69d43f4587d9a1db79cc75133b4f184f358d
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-27 07:38:36 +0000

    extractElements追加

---
+++ src/js/dom-scenes/match-card/dom/elements.ts
@@ -0,0 +1,28 @@
+import { DataIDs } from "./data-ids";
+
+/** root要素の子孫要素 */
+export type Elements = {
+  /** プレイヤーのアームドーザ画像 */
+  player: HTMLImageElement;
+  /** 敵のアームドーザ画像 */
+  enemy: HTMLImageElement;
+};
+
+/**
+ * root要素から子孫要素を抽出する
+ * @param root root要素
+ * @param ids data-idを集めたもの
+ * @return 抽出結果
+ */
+export function extractElements(
+  root: HTMLElement,
+  ids: DataIDs,
+): Elements {
+  const foundedPlayer = root.querySelector(`[data-id="${ids.player}"]`);
+  const player: HTMLImageElement =
+    foundedPlayer instanceof HTMLImageElement ? foundedPlayer : new Image();
+  const foundedEnemy = root.querySelector(`[data-id="${ids.enemy}"]`);
+  const enemy: HTMLImageElement =
+    foundedEnemy instanceof HTMLImageElement ? foundedEnemy : new Image();
+  return { player, enemy };
+}
