6acd680d440e701a934fa7a72ff0407b01a88606
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-27 06:04:01 +0000

    onGotoTitleButtonPushを別ファイルに切り出した

---
+++ src/js/dom-scenes/mail-verified-incomplete/procedures/on-goto-title-button-push.ts
@@ -0,0 +1,19 @@
+import { pop } from "../../../dom/pop";
+import { PushDOM } from "../../../dom/push-dom";
+import { MailVerifiedIncompleteProps } from "../props";
+
+/**
+ * タイトルへボタンが押された時の処理
+ * @param action アクション
+ */
+export function onGotoTitleButtonPush(
+  props: Readonly<MailVerifiedIncompleteProps>,
+  action: PushDOM,
+): void {
+  props.exclusive.execute(async () => {
+    action.event.preventDefault();
+    action.event.stopPropagation();
+    await pop(props.gotoTitleButton);
+    props.gotoTitle.next();
+  });
+}
