9f14f7bd9854c1b73a3e88b53879ed4a6c9174e9
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-27 05:39:17 +0000

    storybookに改行追加

--- stories/mail-verified-incomplete.stories.ts
+++ stories/mail-verified-incomplete.stories.ts
@@ -1,9 +1,11 @@
 import { MailVerifiedIncomplete } from "../src/js/dom-scenes/mail-verified-incomplete/mail-verified-incomplete";
 import type { DOMStubStory } from "./stub/dom-stub";
 import { domStub } from "./stub/dom-stub";
+
 export default {
   title: "mail-verified-incomplete",
 };
+
 export const scene: DOMStubStory = domStub(() => {
   const scene = new MailVerifiedIncomplete("test@mail.address.com");
   scene.notifyTitleTransition().subscribe(() => {
