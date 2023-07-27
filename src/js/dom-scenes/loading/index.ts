1db7a6e320f2fc283775bfa50b271147c76f025a
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-27 04:32:54 +0000

    LoadingPresentationを削除した

--- src/js/dom-scenes/loading/index.ts
+++ src/js/dom-scenes/loading/index.ts
@@ -4,15 +4,19 @@ import type {
   LoadingActions,
   LoadingProgress,
 } from "../../resource/loading/loading-actions";
+import { domUuid } from "../../uuid/dom-uuid";
 import type { DOMScene } from "../dom-scene";
-import { LoadingPresentation } from "./presentation";
 
 /** ローディング画面 */
 export class Loading implements DOMScene {
   /** ローディング完了率 */
   #completedRate: number;
-  /** @deprecated プレゼンテーション */
-  #presentation: LoadingPresentation;
+  /** ルートHTML要素 */
+  #root: HTMLElement;
+  /** ローディング完了率のテキスト */
+  #text: HTMLElement;
+  /** ローディングバー */
+  #bar: HTMLElement;
   /** アンサブスクライバ */
   #unsubscriber: Unsubscribable;
 
@@ -22,7 +26,27 @@ export class Loading implements DOMScene {
    */
   constructor(loading: Observable<LoadingActions>) {
     this.#completedRate = 0;
-    this.#presentation = new LoadingPresentation();
+    const textId = domUuid();
+    const barId = domUuid();
+    this.#root = document.createElement("div");
+    this.#root.innerHTML = `
+      <div class="loading__progress">
+        <div class="loading__progress-rate" data-id="${textId}"></div>
+        <div class="loading__outer-progress-bar">
+          <div class="loading__progress-bar" data-id="${barId}"></div>
+        </div>
+      </div>
+    `;
+    this.#root.className = "loading";
+    this.#root.style.display = "flex";
+    this.#text =
+      this.#root.querySelector(`[data-id="${textId}"]`) ||
+      document.createElement("div");
+    this.#bar =
+      this.#root.querySelector(`[data-id="${barId}"]`) ||
+      document.createElement("div");
+    this.#setCompletedRate(0);
+
     this.#unsubscriber = loading.subscribe((action) => {
       if (action.type === "LoadingProgress") {
         this.#onLoadingProgress(action);
@@ -37,7 +61,7 @@ export class Loading implements DOMScene {
 
   /** @override */
   getRootHTMLElement(): HTMLElement {
-    return this.#presentation.getRootHTMLElement();
+    return this.#root;
   }
 
   /**
@@ -47,6 +71,15 @@ export class Loading implements DOMScene {
    */
   #onLoadingProgress(action: LoadingProgress): void {
     this.#completedRate = Math.max(action.completedRate, this.#completedRate);
-    this.#presentation.setCompletedRate(this.#completedRate);
+    this.#setCompletedRate(this.#completedRate);
+  }
+
+  /**
+   * ローディング進捗を変更する
+   * @param completedRate 0〜1で指定する進捗率、1で完了
+   */
+  #setCompletedRate(completedRate: number): void {
+    this.#text.innerText = `LOADING... ${Math.floor(completedRate * 100)}%`;
+    this.#bar.style.width = `${completedRate * 100}%`;
   }
 }
