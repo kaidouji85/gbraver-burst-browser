ab3bef4c1d1b2486afedc77f2a5929f06cc6f5ee
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-27 04:47:57 +0000

    createLoadingProps追加

---
+++ src/js/dom-scenes/loading/procedures/create-loading-props.ts
@@ -0,0 +1,23 @@
+import { domUuid } from "../../../uuid/dom-uuid";
+import { extractElements } from "../dom/elements";
+import { rootInnerHTML } from "../dom/root-inner-html";
+import { LoadingProps } from "../props";
+
+/**
+ * ローディング画面プロパティを生成する
+ * @return 生成結果
+ */
+export function createLoadingProps(): LoadingProps {
+  const completedRate = 0;
+  const ids = { text: domUuid(), bar: domUuid() };
+  const root = document.createElement("div");
+  root.innerHTML = rootInnerHTML(ids);
+  root.className = "loading";
+  root.style.display = "flex";
+  const elements = extractElements(root, ids);
+  return {
+    ...elements,
+    root,
+    completedRate,
+  };
+}
