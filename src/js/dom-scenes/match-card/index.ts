00b506175613a1896f91f5b0df49ad998587896a
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-27 07:57:17 +0000

    マッチガード画面を新モジュール群で置き換えた

--- src/js/dom-scenes/match-card/index.ts
+++ src/js/dom-scenes/match-card/index.ts
@@ -1,71 +1,21 @@
-import type { ArmDozerId } from "gbraver-burst-core";
-
-import { getArmdozerIconPathId } from "../../path/armdozer-icon-path";
-import type { Resources } from "../../resource";
-import { domUuid } from "../../uuid/dom-uuid";
-import { waitElementLoaded } from "../../wait/wait-element-loaded";
 import type { DOMScene } from "../dom-scene";
+import { createMatchCardProps,GenerateMatchCardPropsParams } from "./procedures/create-match-card-props";
+import { MatchCardProps } from "./props";
 
 /** コンストラクタのパラメータ */
-type Param = {
-  /** リソース管理オブジェクト */
-  resources: Resources;
-  /** プレイヤー情報 */
-  player: ArmDozerId;
-  /** 敵情報 */
-  enemy: ArmDozerId;
-  /** キャプション */
-  caption: string;
-};
+type Param = GenerateMatchCardPropsParams;
 
 /** 対戦カード画面 */
 export class MatchCard implements DOMScene {
-  /** ルートHTML要素 */
-  #root: HTMLElement;
-  /** プレイヤー画像の読み込みが完了したら発火するPromise */
-  #isPlayerLoaded: Promise<void>;
-  /** 敵画像の読み込みが完了したら発火するPromise */
-  #isEnemyLoaded: Promise<void>;
+  /** 画面プロパティ */
+  #props: MatchCardProps;
 
   /**
    * コンストラクタ
    * @param param パラメータ
    */
   constructor(param: Param) {
-    const playerId = domUuid();
-    const enemyId = domUuid();
-    this.#root = document.createElement("div");
-    this.#root.className = "match-card";
-    this.#root.innerHTML = `
-      <div class="match-card__contents">
-        <div class="match-card__caption">
-          ${param.caption}
-        </div>
-        <div class="match-card__cards">
-          <img class="match-card__enemy-card" data-id="${enemyId}">
-          <div class="match-card__vs">vs</div>
-          <img class="match-card__player-card"  data-id="${playerId}"">
-        </div>
-      </div>
-    `;
-    const playerElement = this.#root.querySelector(`[data-id="${playerId}"]`);
-    const playerImage: HTMLImageElement =
-      playerElement instanceof HTMLImageElement ? playerElement : new Image();
-    this.#isPlayerLoaded = waitElementLoaded(playerImage);
-    const playerIconPath = getArmdozerIconPathId(param.player);
-    const playerIconResource = param.resources.paths.find(
-      (v) => v.id === playerIconPath,
-    );
-    playerImage.src = playerIconResource ? playerIconResource.path : "";
-    const enemyElement = this.#root.querySelector(`[data-id="${enemyId}"]`);
-    const enemyImage =
-      enemyElement instanceof HTMLImageElement ? enemyElement : new Image();
-    this.#isEnemyLoaded = waitElementLoaded(enemyImage);
-    const enemyIconPath = getArmdozerIconPathId(param.enemy);
-    const enemyIconResource = param.resources.paths.find(
-      (v) => v.id === enemyIconPath,
-    );
-    enemyImage.src = enemyIconResource ? enemyIconResource.path : "";
+    this.#props = createMatchCardProps(param);
   }
 
   /** @override */
@@ -78,11 +28,11 @@ export class MatchCard implements DOMScene {
    * @return 待機結果
    */
   async waitUntilLoaded(): Promise<void> {
-    await Promise.all([this.#isPlayerLoaded, this.#isEnemyLoaded]);
+    await Promise.all([this.#props.isPlayerLoaded, this.#props.isEnemyLoaded]);
   }
 
   /** @override */
   getRootHTMLElement(): HTMLElement {
-    return this.#root;
+    return this.#props.root;
   }
 }
