ffea6f2dac2839cac489d601985ba73d60d6d869
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-27 02:37:59 +0000

    戦闘アニメーション倍率をhandlebars化

--- src/js/dom-scenes/config/dom/battle-animation-time-scale-options.ts
+++ src/js/dom-scenes/config/dom/battle-animation-time-scale-options.ts
@@ -2,6 +2,7 @@ import {
   BattleAnimationTimeScale,
   BattleAnimationTimeScales,
 } from "../../../game/config/browser-config";
+import battleAnimationTimeScaleTemplate from "./battle-animation-time-scale.hbs";
 import { ROOT_CLASS } from "./class-name";
 
 /**
@@ -16,15 +17,10 @@ export const battleAnimationTimeScaleOptions = (
     (value) => {
       const checked = value === selected ? "checked" : "";
       const magnification = Math.floor(1 / value);
-      return `
-      <label class="${ROOT_CLASS}__battle-animation-time-scale-label">
-        <input class="${ROOT_CLASS}__battle-animation-time-scale-radio"
-          name="battle-animation-time-scale"
-          type="radio"
-          value="${value}"
-          ${checked}
-        >
-        ${magnification}倍
-      </label>
-    `;
+      return battleAnimationTimeScaleTemplate({
+        ROOT_CLASS,
+        value,
+        checked,
+        magnification
+      });
   }).reduce((a, b) => a + b);
