2c371256a85874c767b095bea486b0c09723b638
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-27 05:13:44 +0000

    onLoadingProgressを追加

---
+++ src/js/dom-scenes/loading/procedures/engage-completed-rate.ts
@@ -0,0 +1,12 @@
+import { LoadingProps } from "../props";
+
+/**
+ * ローディング進捗を画面に反映する
+ * @param props 画面プロパティ
+ */
+export function engageCompletedRate(
+  props: LoadingProps,
+): void {
+  props.text.innerText = `LOADING... ${Math.floor(props.completedRate * 100)}%`;
+  props.bar.style.width = `${props.completedRate * 100}%`;
+}
--- src/js/dom-scenes/loading/props.ts
+++ src/js/dom-scenes/loading/props.ts
@@ -1,6 +1,10 @@
 /** ローディング画面のプロパティ */
 export type LoadingProps = {
-  /** @deprecated ローディング完了率 */
+  /** 
+   * 0～1で指定するローディング完了率
+   * イベント発火の順番で、進捗率が下がって通知されることがあるので、
+   * 最後に通知された進捗率の最大値を保持する
+   */
   completedRate: number;
   /** ルートHTML要素 */
   root: HTMLElement;
