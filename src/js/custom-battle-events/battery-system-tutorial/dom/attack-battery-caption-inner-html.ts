afff3654a1ba76a58c8ab8981521e4df21bf62e3
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-29 19:16:23 +0900

    画像をメッセージウインドウに追加した

--- src/js/custom-battle-events/battery-system-tutorial/dom/attack-battery-caption-inner-html.ts
+++ src/js/custom-battle-events/battery-system-tutorial/dom/attack-battery-caption-inner-html.ts
@@ -1,6 +1,7 @@
 import { ROOT_CLASS as MESSAGE_WINDOW_ROOT } from "../../../game-dom/message-window/dom/class-name";
 import { Resources } from "../../../resource";
 import template from "./attack-battery-caption-inner-html.hbs";
+import {PathIds} from "../../../resource/path";
 
 /**
  * 攻撃バッテリー注釈の innerHTML を生成する
@@ -10,7 +11,13 @@ import template from "./attack-battery-caption-inner-html.hbs";
 export function attackBatteryCaptionInnerHtml(
   resources: Resources
 ): string {
+  const minusBatteryPath = resources.paths
+    .find(v => v.id === PathIds.MESSAGE_WINDOW_MINUS_BATTERY)?.path ?? "";
+  const plusBatteryPath = resources.paths
+    .find(v => v.id === PathIds.MESSAGE_WINDOW_PLUS_BATTERY)?.path ?? "";
   return template({
-    MESSAGE_WINDOW_ROOT
+    MESSAGE_WINDOW_ROOT,
+    minusBatteryPath,
+    plusBatteryPath,
   });
 }
