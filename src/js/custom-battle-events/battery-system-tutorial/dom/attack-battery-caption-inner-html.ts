839402ebe09e8977856e947cd63c2c62ce189c45
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-29 19:29:25 +0900

    lint、コードフォーマット

--- src/js/custom-battle-events/battery-system-tutorial/dom/attack-battery-caption-inner-html.ts
+++ src/js/custom-battle-events/battery-system-tutorial/dom/attack-battery-caption-inner-html.ts
@@ -1,22 +1,23 @@
 import { ROOT_CLASS as MESSAGE_WINDOW_ROOT } from "../../../game-dom/message-window/dom/class-name";
 import { Resources } from "../../../resource";
+import { PathIds } from "../../../resource/path";
 import template from "./attack-battery-caption-inner-html.hbs";
-import {PathIds} from "../../../resource/path";
 
 /**
  * 攻撃バッテリー注釈の innerHTML を生成する
  * @param resources リソース管理オブジェクト
  * @returns 生成した innerHTML
  */
-export function attackBatteryCaptionInnerHtml(
-  resources: Resources
-): string {
-  const minusBatteryPath = resources.paths
-    .find(v => v.id === PathIds.MESSAGE_WINDOW_MINUS_BATTERY)?.path ?? "";
-  const plusBatteryPath = resources.paths
-    .find(v => v.id === PathIds.MESSAGE_WINDOW_PLUS_BATTERY)?.path ?? "";
-  const attackBatteryPath = resources.paths
-    .find(v => v.id === PathIds.MESSAGE_WINDOW_ATTACK_BATTERY)?.path ?? "";
+export function attackBatteryCaptionInnerHtml(resources: Resources): string {
+  const minusBatteryPath =
+    resources.paths.find((v) => v.id === PathIds.MESSAGE_WINDOW_MINUS_BATTERY)
+      ?.path ?? "";
+  const plusBatteryPath =
+    resources.paths.find((v) => v.id === PathIds.MESSAGE_WINDOW_PLUS_BATTERY)
+      ?.path ?? "";
+  const attackBatteryPath =
+    resources.paths.find((v) => v.id === PathIds.MESSAGE_WINDOW_ATTACK_BATTERY)
+      ?.path ?? "";
   return template({
     MESSAGE_WINDOW_ROOT,
     minusBatteryPath,
--- stories/message-window.stories.ts
+++ stories/message-window.stories.ts
@@ -1,10 +1,8 @@
+import { attackBatteryCaptionInnerHtml } from "../src/js/custom-battle-events/battery-system-tutorial/dom/attack-battery-caption-inner-html";
 import { MessageWindow } from "../src/js/game-dom/message-window";
 import { ROOT_CLASS } from "../src/js/game-dom/message-window/dom/class-name";
 import type { DOMStubStory } from "./stub/dom-stub";
 import { domStub } from "./stub/dom-stub";
-import {
-  attackBatteryCaptionInnerHtml
-} from "../src/js/custom-battle-events/battery-system-tutorial/dom/attack-battery-caption-inner-html";
 
 export default {
   title: "message-window",
