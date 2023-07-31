4ebd627c4cce016ddbfbc6ec1e5823f33d73b767
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-31 18:28:10 +0900

    createStageTitleProps追加

---
+++ src/js/dom-scenes/stage-title/procedures/create-stage-title-props.ts
@@ -0,0 +1,54 @@
+import {Resources} from "../../../resource";
+import {ArmDozerId} from "gbraver-burst-core";
+import {domUuid} from "../../../uuid/dom-uuid";
+import {waitElementLoaded} from "../../../wait/wait-element-loaded";
+import {getArmdozerIconPathId} from "../../../path/armdozer-icon-path";
+import {ROOT_CLASS} from "../dom/class-name";
+import {rootInnerHTML} from "../dom/root-inner-html";
+import {extractElements} from "../dom/elements";
+
+/** StageTitleProps生成パラメータ */
+export type StageTitleParams = {
+  /** リソース管理オブジェクト */
+  resources: Resources;
+  /** ステージレベル */
+  level: number;
+  /** ステージ名 */
+  caption: string[];
+  /** 対戦するアームドーザのID */
+  armDozerId: ArmDozerId;
+};
+
+/**
+ * StageTitlePropsを生成する
+ * @param params 生成パラメータ
+ */
+export function createStageTitleProps(
+  params: StageTitleParams
+) {
+  const ids = {
+    caption: domUuid(),
+    armDozerIcon: domUuid(),
+  };
+  const root = document.createElement("div");
+  root.className = ROOT_CLASS;
+  root.innerHTML = rootInnerHTML(ids, params.level);
+  const elements = extractElements(root, ids);
+  const armDozerIconPathID = getArmdozerIconPathId(params.armDozerId);
+  elements.armDozerIcon.src =
+    params.resources.paths.find((v) => v.id === armDozerIconPathID)?.path ??
+    "";
+  elements.caption.innerHTML = params.caption
+    .map(
+      (v) => `
+        <span class="${ROOT_CLASS}__caption-clause">
+          ${v}
+        </span>
+      `,
+    )
+    .reduce((a, b) => a + b);
+  return {
+    root,
+    isArmDozerIconLoaded: waitElementLoaded(elements.armDozerIcon)
+  };
+}
