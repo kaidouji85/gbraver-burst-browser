fea4a4b0977a8c7f02b31cc08687c084cfba7960
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-02 02:38:36 +0000

    データ定義、パスIDを別ファイルに切り出した

---
+++ src/js/resource/path/resource.ts
@@ -0,0 +1,24 @@
+import { ResourceRoot } from "../resource-root";
+
+/** パス ID */
+export type PathId = string;
+
+/** パス設定 */
+export type PathConfig = {
+  /** パスID */
+  id: PathId;
+  /**
+   * パス生成関数
+   * @param basePath リソースフォルダのルート 
+   * @return 生成したパス
+   */
+  path: (basePath: ResourceRoot) => string;
+};
+
+/** パス */
+export type Path = {
+  /** パスID */
+  id: PathId;
+  /** リソースのパス */
+  path: string;
+};
