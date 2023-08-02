79bb79e328c061c5cdb668abae5535b868b410c7
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-02 05:30:11 +0000

    旧定義を削除

--- src/js/resource/loading/full-resource-loading.ts
+++ src/js/resource/loading/full-resource-loading.ts
@@ -1,36 +1,11 @@
 import type { Resources } from "..";
-import { CANVAS_IMAGE_CONFIGS } from "../canvas-image";
-import { CUBE_TEXTURE_CONFIGS } from "../cube-texture";
-import { GLTF_CONFIGS } from "../gltf";
 import { PreLoadPathConfigs } from "../path/configs";
 import type { ResourceRoot } from "../resource-root";
-import { SOUND_CONFIGS } from "../sound";
-import {
-  DEVELOPING_TEXTURE_CONFIGS,
-  TEXTURE_CONFIGS,
-} from "../texture/configs";
 import { extractUnloadedResourceConfigs } from "./extract-unloaded-resource-configs";
+import { DEVELOPING_FULL_RESOURCE_CONFIGS, FULL_RESOURCE_CONFIGS } from "./full-resource-configs";
 import { mergeResources } from "./merge-resources";
 import type { LoadingTargetConfigs, ResourceLoading } from "./resource-loading";
 import { resourceLoading } from "./resource-loading";
-
-/** フルリソースの設定 */
-const FULL_RESOURCE_CONFIGS: LoadingTargetConfigs = {
-  gltfConfigs: GLTF_CONFIGS,
-  textureConfigs: TEXTURE_CONFIGS,
-  cubeTextureConfigs: CUBE_TEXTURE_CONFIGS,
-  canvasImageConfigs: CANVAS_IMAGE_CONFIGS,
-  soundConfigs: SOUND_CONFIGS,
-};
-
-/** 開発中素材も含めたフルリソース設定 */
-const DEVELOPING_FULL_RESOURCE_CONFIGS: LoadingTargetConfigs = {
-  gltfConfigs: GLTF_CONFIGS,
-  textureConfigs: [...TEXTURE_CONFIGS, ...DEVELOPING_TEXTURE_CONFIGS],
-  cubeTextureConfigs: CUBE_TEXTURE_CONFIGS,
-  canvasImageConfigs: CANVAS_IMAGE_CONFIGS,
-  soundConfigs: SOUND_CONFIGS,
-};
 
 /**
  * フルリソースを読み込む
