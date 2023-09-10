f5ce71a8cf1c49c14bbe24fa7a7cee84c6debc0c
Y.Takeuchi <kaidouji85@gmail.com>
2023-09-10 16:44:46 +0900

    それっぽく動くようになった

--- src/js/custom-battle-events/battery-system-tutorial/stories/introduction.ts
+++ src/js/custom-battle-events/battery-system-tutorial/stories/introduction.ts
@@ -1,10 +1,66 @@
-import type { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
+import { delay } from "../../../animation/delay";
+import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
+import { ShinBraverTD } from "../../../td-scenes/battle/view/td/armdozer-objects/shin-braver";
+import { WingDozerTD } from "../../../td-scenes/battle/view/td/armdozer-objects/wing-dozer";
 import {
   activeLeftMessageWindowWithFace,
   activeRightMessageWindowWithFace,
 } from "../../active-message-window";
 import { refreshConversation } from "../../invisible-all-message-windows";
 import { scrollLeftMessages, scrollRightMessages } from "../../scroll-messages";
+import {all} from "../../../animation/all";
+import {waitUntilWindowPush} from "../../wait-until-window-push";
+
+/**
+ * 条件を満たした場合、気をつけ、礼をする
+ * @param props イベントプロパティ
+ * @return ストーリーが完了したら発火するPromise
+ */
+async function uprightBowIfNeed(props: CustomBattleEventProps): Promise<void> {
+  const foundShinBraverTD = props.view.td.armdozerObjects.find(
+    (v) => v.playerId === props.playerId,
+  );
+  const foundWingDozerTD = props.view.td.armdozerObjects.find(
+    (v) => v.playerId !== props.playerId,
+  );
+  if (
+    !(foundShinBraverTD instanceof ShinBraverTD) ||
+    !(foundWingDozerTD instanceof WingDozerTD)
+  ) {
+    return;
+  }
+
+  const shinBraverTD: ShinBraverTD = foundShinBraverTD;
+  const wingDozerTD: WingDozerTD = foundWingDozerTD;
+  activeLeftMessageWindowWithFace(props, "Tsubasa");
+  props.view.dom.leftMessageWindow.messages(["姿勢を正して"]);
+  await all(
+    shinBraverTD.shinBraver.upright(),
+    wingDozerTD.wingDozer.upright(),
+  ).play();
+  await waitUntilWindowPush(props);
+  await scrollLeftMessages(props, [["礼!!」"]]);
+  await refreshConversation(props);
+  activeLeftMessageWindowWithFace(props, "Tsubasa");
+  props.view.dom.leftMessageWindow.messages([
+    "ツバサ",
+    "「よろしくお願いします」",
+  ]);
+  props.view.dom.leftMessageWindow.scrollUp();
+  activeRightMessageWindowWithFace(props, "Shinya");
+  await scrollRightMessages(props, [["シンヤ", "「よろしくお願いします」"]]);
+  await all(
+    shinBraverTD.shinBraver
+      .bowDown()
+      .chain(delay(200))
+      .chain(shinBraverTD.shinBraver.bowUp()),
+    wingDozerTD.wingDozer
+      .bowDown()
+      .chain(delay(200))
+      .chain(wingDozerTD.wingDozer.bowUp()),
+  ).play();
+  await waitUntilWindowPush(props);
+}
 
 /**
  * ストーリー 冒頭
@@ -15,17 +71,8 @@ export async function introduction(props: CustomBattleEventProps) {
   activeLeftMessageWindowWithFace(props, "Tsubasa");
   await scrollLeftMessages(props, [
     ["ツバサ", "「これより 操縦訓練を開始する"],
-    ["姿勢を正して 礼!!」"],
   ]);
-  await refreshConversation(props);
-  activeLeftMessageWindowWithFace(props, "Tsubasa");
-  props.view.dom.leftMessageWindow.messages([
-    "ツバサ",
-    "「よろしくお願いします」",
-  ]);
-  props.view.dom.leftMessageWindow.scrollUp();
-  activeRightMessageWindowWithFace(props, "Shinya");
-  await scrollRightMessages(props, [["シンヤ", "「よろしくお願いします」"]]);
+  await uprightBowIfNeed(props);
   await refreshConversation(props, 100);
   activeLeftMessageWindowWithFace(props, "Tsubasa");
   await scrollLeftMessages(props, [
