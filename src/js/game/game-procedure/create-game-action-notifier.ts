7142cc75571d0dcbd513552d053270fee1950c3d
Y.Takeuchi <kaidouji85@gmail.com>
2023-09-28 05:11:16 +0000

    lint、コードフォーマット

--- src/js/game/game-procedure/create-game-action-notifier.ts
+++ src/js/game/game-procedure/create-game-action-notifier.ts
@@ -1,4 +1,4 @@
-import { map, merge,Observable } from "rxjs";
+import { map, merge, Observable } from "rxjs";
 
 import { GameAction } from "../game-actions";
 import { GameProps } from "../game-props";
@@ -8,21 +8,24 @@ import { GameProps } from "../game-props";
  * @param props ゲームプロパティ
  * @return ゲームアクション通知ストリーム
  */
-export function createGameActionNotifier(props: GameProps): Observable<GameAction> {
-  const suddenlyBattleEnd: Observable<GameAction> =
-  props.suddenlyBattleEnd.stream().pipe(
-    map(() => ({
-      type: "SuddenlyBattleEnd",
-    })),
-  );
-const webSocketAPIError: Observable<GameAction> = props.api
-  .websocketErrorNotifier()
-  .pipe(
-    map((error) => ({
-      type: "WebSocketAPIError",
-      error,
-    })),
-  );
+export function createGameActionNotifier(
+  props: Readonly<GameProps>,
+): Observable<GameAction> {
+  const suddenlyBattleEnd: Observable<GameAction> = props.suddenlyBattleEnd
+    .stream()
+    .pipe(
+      map(() => ({
+        type: "SuddenlyBattleEnd",
+      })),
+    );
+  const webSocketAPIError: Observable<GameAction> = props.api
+    .websocketErrorNotifier()
+    .pipe(
+      map((error) => ({
+        type: "WebSocketAPIError",
+        error,
+      })),
+    );
   return merge(
     props.tdBinder.gameActionNotifier(),
     props.domSceneBinder.gameActionNotifier(),
