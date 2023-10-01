2138eb9611c947bde71cb5412631868adbcb9928
Y.Takeuchi <kaidouji85@gmail.com>
2023-10-01 16:12:01 +0900

    startEpisodeにリネーム

--- src/js/game/game-procedure/on-select-episode.ts
+++ src/js/game/game-procedure/on-select-episode.ts
@@ -2,7 +2,7 @@ import { Episodes, EpisodesInDevelopment } from "../episodes";
 import { SelectEpisode } from "../game-actions/select-episode";
 import type { GameProps } from "../game-props";
 import type { Story } from "../in-progress/story";
-import { startTutorial } from "./start-tutorial";
+import { startEpisode } from "./start-episode";
 
 /**
  * エピソード選択時の処理
@@ -23,15 +23,15 @@ export async function onSelectEpisode(
   const tutorialStages = props.canPlayEpisodeInDevelopment
     ? EpisodesInDevelopment
     : Episodes;
-  const stage =
+  const episode =
     tutorialStages.find((v) => v.id === action.id) ?? tutorialStages[0];
   props.inProgress = {
     ...inProgress,
     story: {
       type: "PlayingEpisode",
-      episode: stage,
+      episode,
       level: action.level,
     },
   };
-  await startTutorial(props, action.level, stage);
+  await startEpisode(props, episode);
 }
--- src/js/game/game-procedure/start-episode.ts
+++ src/js/game/game-procedure/start-episode.ts
@@ -11,16 +11,13 @@ import type { Episode } from "../episodes/episode";
 import type { GameProps } from "../game-props";
 
 /**
- * チュートリアルを開始するヘルパー関数
- *
+ * エピソードを開始するヘルパー関数
  * @param props ゲームプロパティ
- * @param level チュートリアルステージレベル
  * @param episode エピソード
  * @return 処理が完了したら発火するPromise
  */
-export async function startTutorial(
+export async function startEpisode(
   props: Readonly<GameProps>,
-  level: number,
   episode: Episode,
 ): Promise<void> {
   const npcBattle = new NPCBattleRoom(episode.player, episode.npc);
@@ -33,7 +30,7 @@ export async function startTutorial(
   ]);
   const scene = new EpisodeTitle({
     resources: props.resources,
-    level,
+    level: episode.number,
     title: [episode.title],
   });
   props.domSceneBinder.bind(scene, tutorialTitleConnector);
