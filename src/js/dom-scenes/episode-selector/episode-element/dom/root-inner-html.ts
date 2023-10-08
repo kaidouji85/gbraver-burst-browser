f527030f1997e39e594b3f23d5bdb3e22b68abb5
Y.Takeuchi <kaidouji85@gmail.com>
2023-10-08 11:53:25 +0900

    不要なものを削除

--- src/js/dom-scenes/episode-selector/episode-element/dom/root-inner-html.ts
+++ src/js/dom-scenes/episode-selector/episode-element/dom/root-inner-html.ts
@@ -1,32 +1,9 @@
-import { EpisodeType } from "../../../../game/episodes/episode";
 import { Resources } from "../../../../resource";
 import { Episode } from "../episode";
 import { ROOT_CLASS } from "./class-name";
 import { DataIDs } from "./data-ids";
 import rootInnerHTMLTemplate from "./root-inner-html.hbs";
 import {PathIds} from "../../../../resource/path/ids";
-
-/** エピソード */
-const main = () => `<div class="${ROOT_CLASS}__main">メイン</div>`;
-
-/** サイドエピソード */
-const side = () => `<div class="${ROOT_CLASS}__side">サイド</div>`;
-
-/**
- * エピソードタイプに応じたアイコンを生成する
- * @param type エピソードタイプ
- * @return アイコン
- */
-const episodeType = (type: EpisodeType): string => {
-  switch (type) {
-    case "Episode":
-      return main();
-    case "Side Episode":
-      return side();
-    default:
-      return main();
-  }
-};
 
 /**
  * ルート要素のinnerHTML
@@ -41,7 +18,6 @@ export function rootInnerHTML(ids: DataIDs, resources: Resources, episode: Episo
     ...episode,
     ids,
     ROOT_CLASS,
-    type: episodeType(episode.type),
     check,
   });
 }
