31cdad684647e841b9ba4cb76d713e33f786bb9c
Y.Takeuchi <kaidouji85@gmail.com>
2023-10-13 09:08:43 +0000

    ファイル名を変更

--- src/js/game/game-procedure/on-post-battle/index.ts
+++ src/js/game/game-procedure/on-post-battle/index.ts
@@ -1,10 +1,10 @@
 import { PostBattleAction } from "../../game-actions/post-battle-action";
 import { GameProps } from "../../game-props";
 import { gotoEndingIfNeeded } from "./goto-ending-if-needed";
+import { gotoEpisodeIfNeeded } from "./goto-episode-if-needed";
+import { gotoEpisodeSelectorIfNeeded } from "./goto-episode-selector-if-needed";
 import { gotoNPCBattleStageIfNeeded } from "./goto-npc-battle-stage";
 import { gotoTitleIfNeeded } from "./goto-title-if-needed";
-import { gotoEpisodeIfNeeded } from "./goto-tutorial-if-needed";
-import { gotoEpisodeSelectorIfNeeded } from "./goto-tutorial-selector-if-needed";
 
 /**
  * 戦闘終了後アクション決定時の処理
