713135ea22c1a254d67afbceaa24587e3f0661e4
Y.Takeuchi <kaidouji85@gmail.com>
2023-10-13 02:40:56 +0000

    チェックマークの表示、非表示を追加した

--- src/js/dom-scenes/episode-selector/episode-element/dom/class-name.ts
+++ src/js/dom-scenes/episode-selector/episode-element/dom/class-name.ts
@@ -3,3 +3,9 @@ export const ROOT_CLASS = "episode";
 
 /** ルートHTML要素 非表示 class属性 */
 export const ROOT_CLASS_INVISIBLE = `${ROOT_CLASS}--invisible`;
+
+/** チェックマーク class属性 */
+export const CHECKER_CLASS = `${ROOT_CLASS}__checker`;
+
+/** チェックマーク 非表示 class属性 */
+export const CHECKER_CLASS_INVISIBLE = `${CHECKER_CLASS}--invisible`;
--- src/js/dom-scenes/episode-selector/episode-element/dom/root-inner-html.ts
+++ src/js/dom-scenes/episode-selector/episode-element/dom/root-inner-html.ts
@@ -1,7 +1,7 @@
 import { Resources } from "../../../../resource";
 import { PathIds } from "../../../../resource/path/ids";
 import { Episode } from "../../episode";
-import { ROOT_CLASS } from "./class-name";
+import { CHECKER_CLASS_INVISIBLE, ROOT_CLASS } from "./class-name";
 import { DataIDs } from "./data-ids";
 import rootInnerHTMLTemplate from "./root-inner-html.hbs";
 
@@ -18,6 +18,7 @@ export function rootInnerHTML(resources: Resources, ids: DataIDs, episode: Episo
     ...episode,
     ids,
     ROOT_CLASS,
+    CHECKER_CLASS_INVISIBLE,
     checkPath,
   });
 }
