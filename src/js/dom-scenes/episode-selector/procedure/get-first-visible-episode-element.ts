3a2b3540e222264e8167a9121cbae3f86b0380b7
Y.Takeuchi <kaidouji85@gmail.com>
2023-10-13 08:52:38 +0000

    エピソード取得のヘルパー関数をシンプルにした

---
+++ src/js/dom-scenes/episode-selector/procedure/get-first-visible-episode-element.ts
@@ -0,0 +1,13 @@
+import { EpisodeElement } from "../episode-element";
+import { EpisodeSelectorProps } from "../props";
+
+/**
+ * 現在表示されているエピソード要素で一番上のものを取得する
+ * @param props 画面プロパティ
+ * @return 取得結果、見つからない場合はnull
+ */
+export function getFirstVisibleEpisodeElement(
+  props: Readonly<EpisodeSelectorProps>,
+): EpisodeElement | null {
+  return props.episodeElements.find((episode) => episode.isVisible()) ?? null;
+}
