7a4ca8757a85ea487c7675904b7adcd02e4ddaee
Y.Takeuchi <kaidouji85@gmail.com>
2023-10-19 23:35:42 +0900

    プロパティ生成関数を外だしした

---
+++ src/js/dom-scenes/episode-selector/episode-element/procedure/create-episode-element-props.ts
@@ -0,0 +1,27 @@
+import {Subject} from "rxjs";
+
+import {Resources} from "../../../../resource";
+import {Episode} from "../../episode";
+import {BLOCK} from "../dom/class-name";
+import {extractChecker} from "../dom/extract-element";
+import {rootInnerHTML} from "../dom/root-inner-html";
+
+/**
+ * EpisodeElementPropsを生成する
+ * @param resources リソース管理オブジェクト
+ * @param episode エピソード情報
+ * @return 生成結果
+ */
+export function createEpisodeElementProps(
+  resources: Resources,
+  episode: Readonly<Episode>,
+) {
+  const root: HTMLElement = document.createElement("div");
+  root.className = BLOCK;
+  root.innerHTML = rootInnerHTML(resources, episode);
+  return {
+    root,
+    checker: extractChecker(root),
+    select: new Subject<void>(),
+  };
+}
--- src/js/dom-scenes/episode-selector/episode-element/props.ts
+++ src/js/dom-scenes/episode-selector/episode-element/props.ts
@@ -1,11 +1,5 @@
 import { Subject } from "rxjs";
 
-import { Resources } from "../../../resource";
-import { Episode } from "../episode";
-import { BLOCK } from "./dom/class-name";
-import { extractChecker } from "./dom/extract-element";
-import { rootInnerHTML } from "./dom/root-inner-html";
-
 /** エピソードHTML要素プロパティ */
 export type EpisodeElementProps = {
   /** ルートHTML要素 */
@@ -15,23 +9,3 @@ export type EpisodeElementProps = {
   /** 選択通知ストリーム */
   select: Subject<void>;
 };
-
-/**
- * EpisodeElementPropsを生成する
- * @param resources リソース管理オブジェクト
- * @param episode エピソード情報
- * @return 生成結果
- */
-export function createEpisodeElementProps(
-  resources: Resources,
-  episode: Readonly<Episode>,
-) {
-  const root: HTMLElement = document.createElement("div");
-  root.className = BLOCK;
-  root.innerHTML = rootInnerHTML(resources, episode);
-  return {
-    root,
-    checker: extractChecker(root),
-    select: new Subject<void>(),
-  };
-}
