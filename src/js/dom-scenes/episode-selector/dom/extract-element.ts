a66f7bad2fc546cc8f6bd29bcf2e95929f99a987
Y.Takeuchi <kaidouji85@gmail.com>
2023-10-19 23:15:49 +0900

    HTML要素抽出関数をリファクタリングした

--- src/js/dom-scenes/episode-selector/dom/extract-element.ts
+++ src/js/dom-scenes/episode-selector/dom/extract-element.ts
@@ -3,94 +3,68 @@
  * @param root ルート要素
  * @return 取得結果
  */
-export function extractEpisodes(root: HTMLElement): HTMLElement {
-  return (
-    root.querySelector(`[data-id="episodes"]`) ?? document.createElement("div")
-  );
-}
+export const extractEpisodes = (root: HTMLElement): HTMLElement =>
+  root.querySelector(`[data-id="episodes"]`) ?? document.createElement("div");
 
 /**
  * イメージカット集合のルート要素を抽出する
  * @param root ルート要素
  * @return 取得結果
  */
-export function extractEpisodeImageCutContainer(
+export const extractEpisodeImageCutContainer = (
   root: HTMLElement,
-): HTMLElement {
-  return (
-    root.querySelector(`[data-id="episodeImageCutContainer"]`) ??
-    document.createElement("div")
-  );
-}
+): HTMLElement =>
+  root.querySelector(`[data-id="episodeImageCutContainer"]`) ??
+  document.createElement("div");
 
 /**
  * メインエピソードタブを抽出する
  * @param root ルート要素
  * @return 取得結果
  */
-export function extractMainEpisodeTab(root: HTMLElement): HTMLElement {
-  return (
-    root.querySelector(`[data-id="mainEpisodeTab"]`) ??
-    document.createElement("div")
-  );
-}
+export const extractMainEpisodeTab = (root: HTMLElement): HTMLElement =>
+  root.querySelector(`[data-id="mainEpisodeTab"]`) ??
+  document.createElement("div");
 
 /**
  * サイドエピソードタブを抽出する
  * @param root ルート要素
  * @return 取得結果
  */
-export function extractSideEpisodeTab(root: HTMLElement): HTMLElement {
-  return (
-    root.querySelector(`[data-id="sideEpisodeTab"]`) ??
-    document.createElement("div")
-  );
-}
+export const extractSideEpisodeTab = (root: HTMLElement): HTMLElement =>
+  root.querySelector(`[data-id="sideEpisodeTab"]`) ??
+  document.createElement("div");
 
 /**
  * エピソードタイトルを抽出する
  * @param root ルート要素
  * @return 取得結果
  */
-export function extractEpisodeTitle(root: HTMLElement): HTMLElement {
-  return (
-    root.querySelector(`[data-id="episodeTitle"]`) ??
-    document.createElement("div")
-  );
-}
+export const extractEpisodeTitle = (root: HTMLElement): HTMLElement =>
+  root.querySelector(`[data-id="episodeTitle"]`) ??
+  document.createElement("div");
 
 /**
  * エピソード導入を抽出する
  * @param root ルート要素
  * @return 取得結果
  */
-export function extractEpisodeIntroduction(root: HTMLElement): HTMLElement {
-  return (
-    root.querySelector(`[data-id="episodeIntroduction"]`) ??
-    document.createElement("div")
-  );
-}
+export const extractEpisodeIntroduction = (root: HTMLElement): HTMLElement =>
+  root.querySelector(`[data-id="episodeIntroduction"]`) ??
+  document.createElement("div");
 
 /**
  * 戻るボタンを抽出する
  * @param root ルート要素
  * @return 取得結果
  */
-export function extractPlayButton(root: HTMLElement): HTMLElement {
-  return (
-    root.querySelector(`[data-id="playButton"]`) ??
-    document.createElement("div")
-  );
-}
+export const extractPlayButton = (root: HTMLElement): HTMLElement =>
+  root.querySelector(`[data-id="playButton"]`) ?? document.createElement("div");
 
 /**
  * 戻るボタンを抽出する
  * @param root ルート要素
  * @return 取得結果
  */
-export function extractPrevButton(root: HTMLElement): HTMLElement {
-  return (
-    root.querySelector(`[data-id="prevButton"]`) ??
-    document.createElement("div")
-  );
-}
+export const extractPrevButton = (root: HTMLElement): HTMLElement =>
+  root.querySelector(`[data-id="prevButton"]`) ?? document.createElement("div");
