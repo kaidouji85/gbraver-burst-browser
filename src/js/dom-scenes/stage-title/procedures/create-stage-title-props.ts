19df7623c05e92f8ebf3e2ec1dbc15cb168c5cd7
Y.Takeuchi <kaidouji85@gmail.com>
2023-10-19 10:44:02 +0000

    ファイル名変更

--- src/js/dom-scenes/stage-title/procedures/create-stage-title-props.ts
+++ src/js/dom-scenes/stage-title/procedures/create-stage-title-props.ts
@@ -5,7 +5,7 @@ import { Resources } from "../../../resource";
 import { waitElementLoaded } from "../../../wait/wait-element-loaded";
 import { ROOT_CLASS } from "../dom/class-name";
 import { createDataIDs } from "../dom/data-ids";
-import { extractArmdozerIcon, extractCaption } from "../dom/elements";
+import { extractArmdozerIcon, extractCaption } from "../dom/extract-element";
 import { rootInnerHTML } from "../dom/root-inner-html";
 import { StageTitleProps } from "../props";
 
