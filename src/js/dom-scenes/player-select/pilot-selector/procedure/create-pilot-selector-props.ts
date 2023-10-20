0e4c0d77dc35a6a9ef16a37eef9cedb83801961f
Y.Takeuchi <kaidouji85@gmail.com>
2023-10-20 02:56:00 +0000

    プロパティとプロパティ生成関数を追加した

---
+++ src/js/dom-scenes/player-select/pilot-selector/procedure/create-pilot-selector-props.ts
@@ -0,0 +1,61 @@
+import { PilotId } from "gbraver-burst-core";
+import { Howl } from "howler";
+import { Subject } from "rxjs";
+
+import { replaceDOM } from "../../../../dom/replace-dom";
+import { Exclusive } from "../../../../exclusive/exclusive";
+import { Resources } from "../../../../resource";
+import { SOUND_IDS } from "../../../../resource/sound";
+import { createPilotIcon } from "../create-pilot-icon";
+import { BLOCK } from "../dom/class-name";
+import { extractDummyStatus, extractIcons, extractOkButton, extractPrevButton } from "../dom/extract-element";
+import { rootInnerHTML } from "../dom/root-inner-html";
+import { PilotStatus } from "../pilot-status";
+import { PilotSelectorProps } from "../props";
+
+/**
+ * PilotIconPropsを生成する
+ * @param resources リソース管理オブジェクト
+ * @param pilotIds 選択可能なパイロットIDリスト
+ * @param initialPilotId パイロットIDの初期値
+ * @return 生成結果
+ */
+export function createPilotIconProps(
+  resources: Resources,
+  pilotIds: PilotId[],
+  initialPilotId: PilotId,
+): PilotSelectorProps {
+    const root = document.createElement("div");
+    root.className = BLOCK;
+    root.innerHTML = rootInnerHTML();
+    
+    const pilotStatus = new PilotStatus();
+    pilotStatus.switch(initialPilotId);
+    const dummyStatus = extractDummyStatus(root);
+    replaceDOM(dummyStatus, pilotStatus.getRootHTMLElement());
+    
+    const pilotIcons = pilotIds.map((v) => ({
+      pilotId: v,
+      icon: createPilotIcon(resources, v),
+    }));
+    const icons = extractIcons(root);
+    pilotIcons.forEach((v) => {
+      v.icon.selected(v.pilotId === initialPilotId);
+      icons.appendChild(v.icon.getRootHTMLElement());
+    });
+ 
+    return {
+      pilotId: initialPilotId,
+      exclusive: new Exclusive(),
+      root,
+      pilotStatus,
+      pilotIcons,
+      okButton: extractOkButton(root),
+      prevButton: extractPrevButton(root),
+      changeValueSound: resources.sounds.find((v) => v.id === SOUND_IDS.CHANGE_VALUE)?.sound ?? new Howl({ src: "" }),
+      decideSound: resources.sounds.find((v) => v.id === SOUND_IDS.PUSH_BUTTON)?.sound ?? new Howl({ src: "" }),
+      change: new Subject(),
+      decide: new Subject(),
+      prev: new Subject(),
+    };
+}
