e8ee4adc90f97ade9eb8bd50f561bb9b60e947cb
Y.Takeuchi <kaidouji85@gmail.com>
2023-10-22 22:30:17 +0900

    ガイのアニメを調整

--- src/js/td-scenes/battle/animation/game-state/pilot-skill/gai.ts
+++ src/js/td-scenes/battle/animation/game-state/pilot-skill/gai.ts
@@ -33,19 +33,23 @@ export function gaiAnimation(param: GaiAnimationParam): Animate {
   return empty();
 }
 
+/** イン アニメーション時間 */
+const inDuration = 400;
+/** アウト アニメーション時間 */
+const outDuration = 400;
+
 /**
  * ガイ 攻撃バフ アニメーション
- *
  * @param param パラメータ
  * @return アニメーション
  */
 function gaiBuffPower(param: GaiAnimationParamX<BuffPowerSkill>): Animate {
   return all(
     param.pilot.cutIn.show(),
-    track(param.tdCamera, param.invokerSprite.getObject3D().position.x, 500),
-    dolly(param.tdCamera, "-40", 500),
-    param.tdObjects.skyBrightness.brightness(0.2, 500),
-    param.tdObjects.illumination.intensity(0.2, 500),
+    track(param.tdCamera, param.invokerSprite.getObject3D().position.x, inDuration),
+    dolly(param.tdCamera, "-40", inDuration),
+    param.tdObjects.skyBrightness.brightness(0.2, inDuration),
+    param.tdObjects.illumination.intensity(0.2, inDuration),
     param.isActivePlayer
       ? param.invokerSprite.endActive()
       : param.anotherSprite.endActive(),
@@ -56,9 +60,9 @@ function gaiBuffPower(param: GaiAnimationParamX<BuffPowerSkill>): Animate {
     .chain(param.invokerTD.armdozerEffects.powerUp.popUp())
     .chain(
       all(
-        toInitial(param.tdCamera, 500),
-        param.tdObjects.skyBrightness.brightness(1, 500),
-        param.tdObjects.illumination.intensity(1, 500),
+        toInitial(param.tdCamera, outDuration),
+        param.tdObjects.skyBrightness.brightness(1, outDuration),
+        param.tdObjects.illumination.intensity(1, outDuration),
       ),
     )
     .chain(delay(200));
