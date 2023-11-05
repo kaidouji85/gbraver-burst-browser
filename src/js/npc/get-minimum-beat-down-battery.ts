c881b6d24700650293eeeffc228f867f2414b3c9
Y.Takeuchi <kaidouji85@gmail.com>
2023-11-05 13:00:42 +0900

    getMinimumBeatDownBatteryを追加した

---
+++ src/js/npc/get-minimum-beat-down-battery.ts
@@ -0,0 +1,25 @@
+import { PlayerState } from "gbraver-burst-core";
+import * as R from "ramda";
+
+import {canBeatDown} from "./can-beat-down";
+
+/**
+ * 相手を倒せる最低限のバッテリーを取得する
+ * @param attacker 攻撃側ステータス
+ * @param defender 防御側ステータス
+ * @param defenderBattery 防御側バッテリー
+ * @return 計算結果、今のバッテリーで倒せない場合はnullを返す
+ */
+export function getMinimumBeatDownBattery(
+  attacker: PlayerState,
+  defender: PlayerState,
+  defenderBattery: number,
+): number | null {
+  const defenderBatteries = R.range(1, defender.armdozer.battery + 1);
+  const beatDownBatteries = defenderBatteries.filter(
+    (battery) => !canBeatDown(attacker, battery, defender, defenderBattery),
+  );
+  return 0 < beatDownBatteries.length
+    ? Math.min(...beatDownBatteries)
+    : null;
+}
