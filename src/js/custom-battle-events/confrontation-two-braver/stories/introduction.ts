4844c8304d18ade24d21f22d28a8aee668fb2815
Y.Takeuchi <kaidouji85@gmail.com>
2023-11-20 09:20:18 +0900

    導入を追加した

---
+++ src/js/custom-battle-events/confrontation-two-braver/stories/introduction.ts
@@ -0,0 +1,18 @@
+import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
+import {activeRightMessageWindowWithFace} from "../../active-message-window";
+import {scrollRightMessages} from "../../scroll-messages";
+
+/**
+ * 導入
+ * @param props イベントプロパティ
+ * @return ストーリーが完了したら発火するPromise
+ */
+export async function introduction(
+  props: CustomBattleEventProps,
+): Promise<void> {
+  activeRightMessageWindowWithFace(props, "Shinya");
+  await scrollRightMessages(props, [
+    ["シンヤ", "「全国大会の覇者 ユウヤさん"],
+    ["俺はこの人に憧れて 機動倶楽部に入ったんだ」"],
+  ]);
+}
