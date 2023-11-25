59b0f44a0ed869d328336b1befb5fd81e36aee17
Y.Takeuchi <kaidouji85@gmail.com>
2023-11-25 15:35:44 +0900

    ユウヤバースト叫びにヘルパー関数を使うようにした

--- src/js/custom-battle-events/confrontation-two-braver/animation/yuuya-burst-when-shinya-advantage.ts
+++ src/js/custom-battle-events/confrontation-two-braver/animation/yuuya-burst-when-shinya-advantage.ts
@@ -1,22 +1,11 @@
-import {CustomBattleEventProps} from "../../../td-scenes/battle/custom-battle-event";
-import {Animate} from "../../../animation/animate";
-import {process} from "../../../animation/process";
+import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
+import { enemyPilotCry } from "../../pilot-cry";
 
 /**
  * ユウヤのバースト シンヤ有利時
  * @param props イベントプロパティ
  * @return アニメーション
  */
-export function yuuyaBurstWhenShinyaAdvantage(
-  props: Readonly<CustomBattleEventProps>
-): Animate {
-  const messageWindow = props.view.dom.enemyCryMessageWindow;
-  return process(() => {
-    messageWindow.visible(true);
-    messageWindow.lighten();
-    messageWindow.face("Yuuya");
-    messageWindow.faceVisible(true);
-    messageWindow.messages(["甘いぜ シンヤ！！"]);
-    messageWindow.nextMessageIconVisible(false);
-  });
-}
+export const yuuyaBurstWhenShinyaAdvantage = (
+  props: Readonly<CustomBattleEventProps>,
+) => enemyPilotCry(props, "Yuuya", "甘いぜ シンヤ！！");
