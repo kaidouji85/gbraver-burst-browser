9eb0630f85ebf288d6f97e79b4335442440b88f6
Y.Takeuchi <kaidouji85@gmail.com>
2023-11-25 17:12:23 +0900

    Chapterを追加

---
+++ src/js/custom-battle-events/confrontation-two-braver/chapter.ts
@@ -0,0 +1,17 @@
+/** チャプターなし */
+export type NoneChapter = {
+  type: "None";
+};
+
+/** シンヤが有利 */
+export type ShinyaHasAdvantage = {
+  /** シンヤの独白を再生したか、trueで再生した */
+  isShinyaMonologuePlayed: boolean;
+  /** ユウヤの叫び1を再生したか、trueで再生した */
+  isYuuyaCry1Played: boolean;
+  /** ユウヤの叫び2を再生したか、trueで再生した */
+  isYuuyaCry2Played: boolean;
+};
+
+/** 複数フェイズにまたがるチャプチャーの情報 */
+export type Chapter = NoneChapter | ShinyaHasAdvantage;
