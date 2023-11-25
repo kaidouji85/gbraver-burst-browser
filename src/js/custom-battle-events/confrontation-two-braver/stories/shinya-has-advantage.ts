00c5b27ab19e91916f3761638c4e58c90a08f8e3
Y.Takeuchi <kaidouji85@gmail.com>
2023-11-25 10:16:08 +0900

    シンヤ有利を追加

---
+++ src/js/custom-battle-events/confrontation-two-braver/stories/shinya-has-advantage.ts
@@ -0,0 +1,20 @@
+import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
+import { activeRightMessageWindowWithFace } from "../../active-message-window";
+import { invisibleAllMessageWindows} from "../../invisible-all-message-windows";
+import { scrollRightMessages } from "../../scroll-messages";
+
+/**
+ * シンヤが有利
+ * @param props イベントプロパティ
+ * @return ストーリーが完了したら発火するPromise
+ */
+export async function shinyaHasAdvantage(
+  props: Readonly<CustomBattleEventProps>,
+): Promise<void> {
+  activeRightMessageWindowWithFace(props, "Shinya");
+  await scrollRightMessages(props, [
+    ["シンヤ", "「チャンピオン相手に リードをとった"],
+    ["この勝負 いただきッス」"],
+  ]);
+  invisibleAllMessageWindows(props);
+}
