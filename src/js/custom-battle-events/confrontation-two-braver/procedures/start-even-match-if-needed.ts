aeacd854d7e662e77c42ca84a30e66fd922cc981
Y.Takeuchi <kaidouji85@gmail.com>
2023-12-01 04:41:37 +0000

    プロシージャにヘルパー関数を組み込んだ

--- src/js/custom-battle-events/confrontation-two-braver/procedures/start-even-match-if-needed.ts
+++ src/js/custom-battle-events/confrontation-two-braver/procedures/start-even-match-if-needed.ts
@@ -1,9 +1,9 @@
 import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
-import { isAllPlayerNoDamage } from "../../is-all-player-no-damage";
 import { separatePlayers } from "../../separate-players";
 import { turnCount } from "../../turn-count";
 import { ConfrontationTwoBraverProps } from "../props";
 import { shinyaMonologueWhenEvenMatch } from "../stories/shinya-monologue-when-even-match";
+import { isEvenMatch } from "./is-even-match";
 
 /**
  * 条件を満たした場合、チャプター「イーブンマッチ」を開始する
@@ -13,33 +13,21 @@ import { shinyaMonologueWhenEvenMatch } from "../stories/shinya-monologue-when-e
 export async function startEvenMatchIfNeeded(
   props: Readonly<CustomBattleEventProps & ConfrontationTwoBraverProps>,
 ): Promise<boolean> {
-  if (props.state.chapter.type === "EvenMatch") {
-    return false;
-  }
-
-  const turn = turnCount(props.stateHistory);
-  if (turn !== 3) {
-    return false;
-  }
-
-  const lastState = props.stateHistory.at(-1);
-  if (!lastState) {
-    return false;
-  }
-
   const separatedPlayers = separatePlayers(props);
   if (!separatedPlayers) {
     return false;
   }
 
   const { player: shinya, enemy: yuuya } = separatedPlayers;
+  const turn = turnCount(props.stateHistory);
   if (
-    !isAllPlayerNoDamage([shinya, yuuya]) &&
-    shinya.armdozer.hp !== yuuya.armdozer.hp
+    props.state.chapter.type !== "EvenMatch" &&
+    turn === 3 &&
+    isEvenMatch({ shinya, yuuya })
   ) {
-    return false;
+    await shinyaMonologueWhenEvenMatch(props);
+    return true;
   }
 
-  await shinyaMonologueWhenEvenMatch(props);
-  return true;
+  return false;
 }
