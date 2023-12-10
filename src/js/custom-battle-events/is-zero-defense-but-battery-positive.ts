43bc0adf69ba0339d78cce583e61c6ed4b700ac7
Y.Takeuchi <kaidouji85@gmail.com>
2023-12-10 15:01:54 +0900

    ユニットテストを追加

--- src/js/custom-battle-events/is-zero-defense-but-battery-positive.ts
+++ src/js/custom-battle-events/is-zero-defense-but-battery-positive.ts
@@ -1,32 +1,13 @@
-import { GameState } from "gbraver-burst-core";
+import { BatteryDeclaration, GameStateX } from "gbraver-burst-core";
 
 /**
  * バッテリーが残っているのに0防御したかを判定する
- * @param stateHistory ステートヒストリー
+ * @param batteryDeclaration バッテリー宣言
  * @return 判定結果、trueでバッテリーが残っているのに0防御した
  */
 export function isZeroDefenseButBatteryPositive(
-  stateHistory: Readonly<GameState[]>,
+  batteryDeclaration: Readonly<GameStateX<BatteryDeclaration>>,
 ): boolean {
-  const gameOver = stateHistory.find(
-    (state) =>
-      state.effect.name === "GameEnd" &&
-      state.effect.result.type === "GameOver",
-  );
-  if (!gameOver || gameOver.effect.name !== "GameEnd" || gameOver.effect.result.type !== "GameOver") {
-    return false;
-  }
-
-  const batteryDeclaration = stateHistory.find(
-    (state) => state.effect.name === "BatteryDeclaration",
-  );
-  if (
-    !batteryDeclaration ||
-    batteryDeclaration.effect.name !== "BatteryDeclaration"
-  ) {
-    return false;
-  }
-
   const defender = batteryDeclaration.players.find(
     (player) => player.playerId !== batteryDeclaration.activePlayerId,
   );
@@ -34,8 +15,7 @@ export function isZeroDefenseButBatteryPositive(
     return false;
   }
 
-  const isDefenderBeatedDown = gameOver.effect.result.winner !== defender.playerId;
   const isZeroDefense = batteryDeclaration.effect.defenderBattery === 0;
   const isDefenderPositiveBattery = 0 < defender.armdozer.battery;
-  return isDefenderBeatedDown && isZeroDefense && isDefenderPositiveBattery;
+  return isZeroDefense && isDefenderPositiveBattery;
 }
