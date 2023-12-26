f1f338a3279034784c74bfcae50ed1bb1277b1e9
Y.Takeuchi <kaidouji85@gmail.com>
2023-12-26 10:32:41 +0000

    序盤のターン数を定数化した

--- src/js/custom-battle-events/confrontation-two-braver/procedures/on-state-animation/shinya-burst.ts
+++ src/js/custom-battle-events/confrontation-two-braver/procedures/on-state-animation/shinya-burst.ts
@@ -6,17 +6,22 @@ import { earlyShinyaBurstCry } from "../../animation/early-shinya-burst-cry";
 import { shinyaBurstCry } from "../../animation/shinya-burst-cry";
 import { ConfrontationTwoBraverProps } from "../../props";
 
+/** 序盤とされるターン数の上限 */
+const EARLY_TURN_LIMIT = 2;
+
 /** シンヤ バースト 叫び */
 export const shinyaBurst: ConditionalAnimation<
   CustomStateAnimation & ConfrontationTwoBraverProps
 >[] = [
   (props) => {
-    return turnCount(props.stateHistory) <= 2 && isPlayerBurstActivated(props)
+    return turnCount(props.stateHistory) <= EARLY_TURN_LIMIT &&
+      isPlayerBurstActivated(props)
       ? earlyShinyaBurstCry(props)
       : null;
   },
   (props) => {
-    return 2 < turnCount(props.stateHistory) && isPlayerBurstActivated(props)
+    return EARLY_TURN_LIMIT < turnCount(props.stateHistory) &&
+      isPlayerBurstActivated(props)
       ? shinyaBurstCry(props)
       : null;
   },
