8e4b64bf2ecf14ddccf0058d1a18b89c20676b95
Y.Takeuchi <kaidouji85@gmail.com>
2023-12-26 05:37:43 +0000

    LoginDialogPropsを追加した

--- src/js/dom-dialogs/login/dom/root-inner-html.ts
+++ src/js/dom-dialogs/login/dom/root-inner-html.ts
@@ -3,6 +3,12 @@ import { PathIds } from "../../../resource/path/ids";
 import { ROOT_CLASS_NAME } from "./class-name";
 import template from "./root-inner-html.hbs";
 
+/**
+ * ルート要素のinnerHTML
+ * @param resources リソース管理オブジェクト
+ * @param caption キャプション
+ * @return 生成結果 
+ */
 export function rootInnerHTML(resources: Resources, caption: string): string {
   const closerPath =
     resources.paths.find((v) => v.id === PathIds.CLOSER)?.path ?? "";
