f1d5407a5a8859e8be74de8ccf39cda0730b0fcb
Y.Takeuchi <kaidouji85@gmail.com>
2023-12-27 02:27:43 +0000

    bindEventListeners追加

---
+++ src/js/dom-dialogs/delete-account-consent/procedure/bind-event-listeners.ts
@@ -0,0 +1,32 @@
+import { Unsubscribable } from "rxjs";
+
+import { domPushStream } from "../../../dom/push-dom";
+import { DeleteAccountConsentDialogProps } from "../props";
+import { onCloseButtonPush } from "./on-close-button-push";
+import { onCloserPush } from "./on-closer-push";
+import { onDeleteAccountButtonPush } from "./on-delete-account-button-push";
+import { onPushOutsideOfDialog } from "./on-push-outside-of-dialog";
+
+/**
+ * ダイアログにイベントリスナを関連づける
+ * @param props ダイアログのプロパティ
+ * @return アンサブスクライバ
+ */
+export function bindEventListeners(
+  props: DeleteAccountConsentDialogProps,
+): Unsubscribable[] {
+  return [
+    domPushStream(props.backGround).subscribe((action) => {
+      onPushOutsideOfDialog(props, action);
+    }),
+    domPushStream(props.closer).subscribe((action) => {
+      onCloserPush(props, action);
+    }),
+    domPushStream(props.deleteAccountButton).subscribe((action) => {
+      onDeleteAccountButtonPush(props, action);
+    }),
+    domPushStream(props.closeButton).subscribe((action) => {
+      onCloseButtonPush(props, action);
+    }),
+  ];
+}
