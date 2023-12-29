85e50617b4d876a065d9045c4bc35e04757c1193
Y.Takeuchi <kaidouji85@gmail.com>
2023-12-29 14:10:57 +0900

    playerBattleCountを追加

--- src/js/custom-battle-events/battle-count.ts
+++ src/js/custom-battle-events/battle-count.ts
@@ -1,4 +1,19 @@
-import { GameState } from "gbraver-burst-core";
+import { Battle, GameState, GameStateX, PlayerId } from "gbraver-burst-core";
+
+/**
+ * ステートヒストリーから戦闘を抽出する
+ * @param stateHistory ステートヒストリー
+ * @return 抽出結果
+ */
+function extractBattle(stateHistory: GameState[]): GameStateX<Battle>[] {
+  return stateHistory.reduce(
+    (battles: GameStateX<Battle>[], state: GameState) =>
+      state.effect.name === "Battle"
+        ? [...battles, { ...state, effect: state.effect }]
+        : battles,
+    [],
+  );
+}
 
 /**
  * 戦闘回数を計算するヘルパー関数
@@ -6,5 +21,20 @@ import { GameState } from "gbraver-burst-core";
  * @return 戦闘回数
  */
 export function battleCount(stateHistory: GameState[]): number {
-  return stateHistory.filter((state) => state.effect.name === "Battle").length;
+  return extractBattle(stateHistory).length;
+}
+
+/**
+ * 指定したプレイヤーの戦闘回数を計算するヘルパー関数
+ * @param stateHistory ステートヒストリー
+ * @param playerId プレイヤーID
+ * @return 戦闘回数
+ */
+export function playerBattleCount(
+  stateHistory: GameState[],
+  playerId: PlayerId,
+): number {
+  return extractBattle(stateHistory).filter(
+    (battle) => battle.effect.attacker === playerId,
+  ).length;
 }
