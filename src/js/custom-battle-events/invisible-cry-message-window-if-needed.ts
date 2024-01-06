3bda9242855365f62c7bd47d2862403d716c9e57
Y.Takeuchi <kaidouji85@gmail.com>
2024-01-06 16:22:16 +0900

    叫びウインドウ削除判定ロジックを変更

--- src/js/custom-battle-events/invisible-cry-message-window-if-needed.ts
+++ src/js/custom-battle-events/invisible-cry-message-window-if-needed.ts
@@ -1,4 +1,26 @@
+import { GameState } from "gbraver-burst-core";
+
 import { LastState } from "../td-scenes/battle/custom-battle-event";
+
+/**
+ * プレイヤーがコマンド選択不可であるかを判定する
+ * @param lastState 最終ゲームステート
+ * @return 判定結果、trueであればプレイヤーがコマンド選択不可である
+ */
+function isPlayerNotSelectable(lastState: GameState): boolean {
+  if (lastState.effect.name !== "InputCommand") {
+    return false;
+  }
+
+  const playerCommand = lastState.effect.players.find(
+    (v) => v.playerId === "player",
+  );
+  if (!playerCommand) {
+    return false;
+  }
+
+  return !playerCommand.selectable;
+}
 
 /**
  * 条件を満たした場合、叫びメッセージウインドウを非表示にする
@@ -9,23 +31,10 @@ export function invisibleCryMessageWindowIfNeeded(
   props: Readonly<LastState>,
 ): void {
   const lastState = props.update.at(-1);
-  if (!lastState) {
+  if (lastState && isPlayerNotSelectable(lastState)) {
     return;
   }
 
-  if (lastState.effect.name !== "InputCommand") {
-    return;
-  }
-
-  const playerCommand = lastState.effect.players.find(
-    (v) => v.playerId === props.playerId,
-  );
-  if (!playerCommand) {
-    return;
-  }
-
-  if (playerCommand.selectable) {
-    props.view.dom.playerCryMessageWindow.visible(false);
-    props.view.dom.enemyCryMessageWindow.visible(false);
-  }
+  props.view.dom.playerCryMessageWindow.visible(false);
+  props.view.dom.enemyCryMessageWindow.visible(false);
 }
