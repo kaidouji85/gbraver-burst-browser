f75fc91a20971122a47e508ef5e02da71a081a43
Y.Takeuchi <kaidouji85@gmail.com>
2024-01-14 20:38:07 +0900

    誤字を修正

--- src/js/custom-battle-events/burst-tutorial/animation/shinya-cry-when-self-initiated-pilot-skill.ts
+++ src/js/custom-battle-events/burst-tutorial/animation/shinya-cry-when-self-initiated-pilot-skill.ts
@@ -4,4 +4,4 @@ import { playerPilotCry } from "../../pilot-cry";
 /** シンヤ叫び 自発的にパイロットスキル発動 */
 export const shinyaCryWhenSelfInitiatedPilotSkill = (
   props: Readonly<CustomBattleEventProps>,
-) => playerPilotCry(props, "Shinya", "何故か無償にやる気が出てきたッス");
+) => playerPilotCry(props, "Shinya", "何故か無性にやる気が出てきたッス");
