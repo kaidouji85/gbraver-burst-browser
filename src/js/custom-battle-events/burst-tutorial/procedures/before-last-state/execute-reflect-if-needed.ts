954d0c455245d1bdd802e72e41a4f20d0678cfa1
Y.Takeuchi <kaidouji85@gmail.com>
2024-01-14 14:25:56 +0900

    ヘルパー関数を外だしした

---
+++ src/js/custom-battle-events/burst-tutorial/procedures/before-last-state/execute-reflect-if-needed.ts
@@ -0,0 +1,44 @@
+import { Battle, GameState } from "gbraver-burst-core";
+
+import { LastState } from "../../../../td-scenes/battle/custom-battle-event";
+import { failReflectDamage } from "../../stories/fail-reflect-damage";
+import { successReflectDamage } from "../../stories/success-reflect-damage";
+
+/**
+ * 条件を満たした場合、ダメージ反射ストーリーを再生する
+ * @param props イベントプロパティ
+ * @return 処理が完了したら発火するPromise
+ */
+export async function executeReflectIfNeeded(
+  props: Readonly<LastState>,
+): Promise<void> {
+  const foundLastBattle = props.update.find((v) => v.effect.name === "Battle");
+  if (!foundLastBattle) {
+    return;
+  }
+
+  const lastBattle: GameState = foundLastBattle;
+  const player = lastBattle.players.find((v) => v.playerId === props.playerId);
+  const enemy = lastBattle.players.find((v) => v.playerId !== props.playerId);
+  if (!player || !enemy || lastBattle.effect.name !== "Battle") {
+    return;
+  }
+
+  const battleEffect: Battle = lastBattle.effect;
+  const isEnemyAttack = battleEffect.attacker !== props.playerId;
+  const hasNotEnemyTryReflect =
+    enemy.armdozer.effects.filter((v) => v.type === "TryReflect").length <= 0;
+  if (isEnemyAttack || hasNotEnemyTryReflect) {
+    return;
+  }
+
+  const reflectSuccessful =
+    props.update.filter(
+      (v) =>
+        v.effect.name === "Reflect" &&
+        v.effect.damagedPlayer === props.playerId,
+    ).length > 0;
+  reflectSuccessful
+    ? await successReflectDamage(props)
+    : await failReflectDamage(props);
+}
--- src/js/custom-battle-events/burst-tutorial/procedures/before-last-state/index.ts
+++ src/js/custom-battle-events/burst-tutorial/procedures/before-last-state/index.ts
@@ -1,50 +1,8 @@
-import { Battle, GameState } from "gbraver-burst-core";
-
-import { LastState } from "../../../../td-scenes/battle/custom-battle-event";
-import { BurstTutorialProps } from "../../props";
-import { BurstTutorialState } from "../../state";
-import { failReflectDamage } from "../../stories/fail-reflect-damage";
-import { introduction } from "../../stories/introduction";
-import { successReflectDamage } from "../../stories/success-reflect-damage";
-
-/**
- * 条件を満たした場合、ダメージ反射ストーリーを再生する
- * @param props イベントプロパティ
- * @return 処理が完了したら発火するPromise
- */
-async function executeReflectIfNeeded(
-  props: Readonly<LastState>,
-): Promise<void> {
-  const foundLastBattle = props.update.find((v) => v.effect.name === "Battle");
-  if (!foundLastBattle) {
-    return;
-  }
-
-  const lastBattle: GameState = foundLastBattle;
-  const player = lastBattle.players.find((v) => v.playerId === props.playerId);
-  const enemy = lastBattle.players.find((v) => v.playerId !== props.playerId);
-  if (!player || !enemy || lastBattle.effect.name !== "Battle") {
-    return;
-  }
-
-  const battleEffect: Battle = lastBattle.effect;
-  const isEnemyAttack = battleEffect.attacker !== props.playerId;
-  const hasNotEnemyTryReflect =
-    enemy.armdozer.effects.filter((v) => v.type === "TryReflect").length <= 0;
-  if (isEnemyAttack || hasNotEnemyTryReflect) {
-    return;
-  }
-
-  const reflectSuccessful =
-    props.update.filter(
-      (v) =>
-        v.effect.name === "Reflect" &&
-        v.effect.damagedPlayer === props.playerId,
-    ).length > 0;
-  reflectSuccessful
-    ? await successReflectDamage(props)
-    : await failReflectDamage(props);
-}
+import {LastState} from "../../../../td-scenes/battle/custom-battle-event";
+import {BurstTutorialProps} from "../../props";
+import {BurstTutorialState} from "../../state";
+import {introduction} from "../../stories/introduction";
+import {executeReflectIfNeeded} from "./execute-reflect-if-needed";
 
 /**
  * 最終ステート直前イベント
