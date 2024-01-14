f4514faf701d6d3b68f42cb5ec6fe6514fb2670a
Y.Takeuchi <kaidouji85@gmail.com>
2024-01-14 14:22:53 +0900

    自発的なバースト、パイロットスキルの解説を追加

---
+++ src/js/custom-battle-events/burst-tutorial/stories/self-initiated-pilot-skill.ts
@@ -0,0 +1,21 @@
+import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
+import { activeLeftMessageWindowWithFace } from "../../active-message-window";
+import { invisibleAllMessageWindows } from "../../invisible-all-message-windows";
+import { scrollLeftMessages } from "../../scroll-messages";
+
+/**
+ * プレイヤーが自主的にパイロットスキルを発動した
+ * @param props イベントプロパティ
+ * @return ストーリーが完了したら発火するPromise
+ */
+export async function selfInitiatedPilotSkill(
+  props: Readonly<CustomBattleEventProps>,
+): Promise<void> {
+  activeLeftMessageWindowWithFace(props, "Tsubasa");
+  await scrollLeftMessages(props, [
+    ["ツバサ", "「今 君が発動したのはパイロットスキルといい"],
+    ["一試合に一回だけ パイロット固有の効果を得ることができるんだ"],
+    ["先ほどのバッテリー2回復は 君のパイロットスキルによるものだ」"],
+  ]);
+  invisibleAllMessageWindows(props);
+}
