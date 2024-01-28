970eb45b741ad028f00714874071b36aa11a546e
Y.Takeuchi <kaidouji85@gmail.com>
2024-01-28 14:45:40 +0900

    コード整形

--- stories/active-armdozer-pointer.stories.ts
+++ stories/active-armdozer-pointer.stories.ts
@@ -1,7 +1,7 @@
-import { TDGameObjectStub } from "./stub/td-game-object-stub";
 import { playerActiveArmdozerPointer } from "../src/js/game-object/active-armdozer-pointer";
-import {Resources} from "../src/js/resource";
-import {ActiveArmdozerPointer} from "../src/js/game-object/active-armdozer-pointer/active-armdozer-pointer";
+import { ActiveArmdozerPointer } from "../src/js/game-object/active-armdozer-pointer/active-armdozer-pointer";
+import { Resources } from "../src/js/resource";
+import { TDGameObjectStub } from "./stub/td-game-object-stub";
 
 export default {
   title: "active-armdozer-pointer",
@@ -12,18 +12,19 @@ export default {
  * @param generator アクティブアームドーザポインタ生成関数
  * @return ストーリー
  */
-const activeArmdozerPointerStory = (
-  generator: (resources: Resources) => ActiveArmdozerPointer,
-) => () => {
-  const stub = new TDGameObjectStub(({ resources }) => {
-    const activeArmdozerPointer = generator(resources);
-    return {
-      objects: [activeArmdozerPointer.getObject3D()],
-    };
-  });
-  stub.start();
-  return stub.domElement();
-}
+const activeArmdozerPointerStory =
+  (generator: (resources: Resources) => ActiveArmdozerPointer) => () => {
+    const stub = new TDGameObjectStub(({ resources }) => {
+      const activeArmdozerPointer = generator(resources);
+      return {
+        objects: [activeArmdozerPointer.getObject3D()],
+      };
+    });
+    stub.start();
+    return stub.domElement();
+  };
 
 /** プレイヤー側 アクティブアームドーザポインタ */
-export const playerPointer = activeArmdozerPointerStory(playerActiveArmdozerPointer);
+export const playerPointer = activeArmdozerPointerStory(
+  playerActiveArmdozerPointer,
+);
