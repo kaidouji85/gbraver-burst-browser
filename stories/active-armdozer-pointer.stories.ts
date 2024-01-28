0922f9d4821f93f1a0b7704117a2bea590758038
Y.Takeuchi <kaidouji85@gmail.com>
2024-01-28 14:45:22 +0900

    ストーリーヘルパー関数を追加した

--- stories/active-armdozer-pointer.stories.ts
+++ stories/active-armdozer-pointer.stories.ts
@@ -1,18 +1,29 @@
 import { TDGameObjectStub } from "./stub/td-game-object-stub";
 import { playerActiveArmdozerPointer } from "../src/js/game-object/active-armdozer-pointer";
+import {Resources} from "../src/js/resource";
+import {ActiveArmdozerPointer} from "../src/js/game-object/active-armdozer-pointer/active-armdozer-pointer";
 
 export default {
   title: "active-armdozer-pointer",
 };
 
-/** プレイヤー側 アクティブアームドーザポインタ */
-export const playerPointer = () => {
+/**
+ * アクティブアームドーザポインタストーリー
+ * @param generator アクティブアームドーザポインタ生成関数
+ * @return ストーリー
+ */
+const activeArmdozerPointerStory = (
+  generator: (resources: Resources) => ActiveArmdozerPointer,
+) => () => {
   const stub = new TDGameObjectStub(({ resources }) => {
-    const activeArmdozerPointer = playerActiveArmdozerPointer(resources);
+    const activeArmdozerPointer = generator(resources);
     return {
       objects: [activeArmdozerPointer.getObject3D()],
     };
   });
   stub.start();
   return stub.domElement();
-};
+}
+
+/** プレイヤー側 アクティブアームドーザポインタ */
+export const playerPointer = activeArmdozerPointerStory(playerActiveArmdozerPointer);
