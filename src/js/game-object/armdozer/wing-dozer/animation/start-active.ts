131bfefa7a9de18035449aefc4983fc1ca457d33
Y.Takeuchi <kaidouji85@gmail.com>
2024-01-31 10:05:13 +0000

    ウィングドーザのアクティブをシンプルにした

--- src/js/game-object/armdozer/wing-dozer/animation/start-active.ts
+++ src/js/game-object/armdozer/wing-dozer/animation/start-active.ts
@@ -1,6 +1,10 @@
+import { all } from "../../../../animation/all";
 import { Animate } from "../../../../animation/animate";
 import { tween } from "../../../../animation/tween";
 import type { WingDozerModel } from "../model/wing-dozer-model";
+
+/** アニメーション時間 */
+const duration = 200;
 
 /**
  * アクティブ状態を開始する
@@ -8,12 +12,8 @@ import type { WingDozerModel } from "../model/wing-dozer-model";
  * @return アニメーション
  */
 export function startActive(model: WingDozerModel): Animate {
-  return tween(model.active, (t) =>
-    t.to(
-      {
-        opacity: 1,
-      },
-      200,
-    ),
+  return all(
+    tween(model.standard, (t) => t.to({ colorStrength: 0.8 }, duration)),
+    tween(model.outline, (t) => t.to({ opacity: 1 }, duration)),
   );
 }
