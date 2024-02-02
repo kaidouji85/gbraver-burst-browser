50c5b87652d05f11af282bc3474da4c97fb6f48e
Y.Takeuchi <kaidouji85@gmail.com>
2024-02-02 02:40:19 +0000

    アクティブ状態の色味を明るくした

--- src/js/game-object/armdozer/lightning-dozer/animation/start-active.ts
+++ src/js/game-object/armdozer/lightning-dozer/animation/start-active.ts
@@ -13,7 +13,7 @@ const duration = 200;
  */
 export function startActive(model: LightningDozerModel): Animate {
   return all(
-    tween(model.standard, (t) => t.to({ colorStrength: 0.8 }, duration)),
+    tween(model.standard, (t) => t.to({ colorStrength: 0.9 }, duration)),
     tween(model.outline, (t) => t.to({ opacity: 1 }, duration)),
   );
 }
