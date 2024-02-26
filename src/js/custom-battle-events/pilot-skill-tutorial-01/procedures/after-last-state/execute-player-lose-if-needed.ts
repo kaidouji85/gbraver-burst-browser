49975b1e73b8076c462f37c41b25e9bb9106fd5e
Y.Takeuchi <kaidouji85@gmail.com>
2024-02-26 05:27:29 +0000

    afterLastStateのフォルダ構成変更

---
+++ src/js/custom-battle-events/pilot-skill-tutorial-01/procedures/after-last-state/execute-player-lose-if-needed.ts
@@ -0,0 +1,21 @@
+import { LastState } from "../../../../td-scenes/battle/custom-battle-event";
+import { invisibleAllMessageWindows } from "../../../invisible-all-message-windows";
+import { playerLose } from "../../stories/player-lose";
+
+/**
+ * 条件を満たした場合「プレイヤー敗北ストーリー」を再生する
+ * @param props イベントプロパティ
+ * @return trueでストーリーを再生した
+ */
+export async function executePlayerLoseIfNeeded(
+  props: Readonly<LastState>,
+): Promise<boolean> {
+  const foundGameEnd = props.update.find((v) => v.effect.name === "GameEnd");
+  if (!foundGameEnd || foundGameEnd.effect.name !== "GameEnd") {
+    return false;
+  }
+
+  await playerLose(props);
+  invisibleAllMessageWindows(props);
+  return true;
+}
