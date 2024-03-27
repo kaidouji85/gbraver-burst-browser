4db46e854f79585836c4cd33be9e916c8d906e7d
Y.Takeuchi <kaidouji85@gmail.com>
2024-03-27 01:25:26 +0000

    アイコンパス取得関数を改良した

--- src/js/path/pilot-icon-path.ts
+++ src/js/path/pilot-icon-path.ts
@@ -3,24 +3,19 @@ import { PilotId, PilotIds } from "gbraver-burst-core";
 import { PathIds } from "../resource/path/ids";
 import { PathId } from "../resource/path/resource";
 
+/** パイロットIDと対応するアイコンのパスIDのマッピング */
+const PilotIconPathIds: Record<PilotId, PathId> = {
+  [PilotIds.SHINYA]: PathIds.SHINYA_ICON,
+  [PilotIds.TSUBASA]: PathIds.TSUBASA_ICON,
+  [PilotIds.GAI]: PathIds.GAI_ICON,
+  [PilotIds.RAITO]: PathIds.RAITO_ICON,
+  [PilotIds.YUUYA]: PathIds.YUUYA_ICON,
+};
+
 /**
  * パイロットIDに対応するアイコンのパスIDを返す
  * @param pilotId パイロットID
  * @return パスID
  */
-export function getPilotIconPathId(pilotId: PilotId): PathId {
-  switch (pilotId) {
-    case PilotIds.SHINYA:
-      return PathIds.SHINYA_ICON;
-    case PilotIds.TSUBASA:
-      return PathIds.TSUBASA_ICON;
-    case PilotIds.GAI:
-      return PathIds.GAI_ICON;
-    case PilotIds.RAITO:
-      return PathIds.RAITO_ICON;
-    case PilotIds.YUUYA:
-      return PathIds.YUUYA_ICON;
-    default:
-      return PathIds.SHINYA_ICON;
-  }
-}
+export const getPilotIconPathId = (pilotId: PilotId): PathId =>
+  PilotIconPathIds[pilotId] ?? PilotIds.SHINYA;
