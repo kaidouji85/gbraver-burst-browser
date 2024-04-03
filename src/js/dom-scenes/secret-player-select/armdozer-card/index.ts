829280829ddc2916e6e7a0552ab1b8ccf2a6c268
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-03 23:38:26 +0900

    アームドーザカードのスタイルを調整

--- src/js/dom-scenes/secret-player-select/armdozer-card/index.ts
+++ src/js/dom-scenes/secret-player-select/armdozer-card/index.ts
@@ -6,6 +6,7 @@ import { Resources } from "../../../resource";
 import { PathIds } from "../../../resource/path/ids";
 import { ROOT } from "./class-name";
 import template from "./inner-html.hbs";
+import {getArmdozerBustShotPathId} from "../../../path/armdozer-bust-shot-path";
 
 /** アームドーザカード */
 export class ArmdozerCard {
@@ -27,7 +28,7 @@ export class ArmdozerCard {
     const batteryIconPath =
       resources.paths.find((p) => p.id === PathIds.BATTERY_ICON)?.path ?? "";
     const armdozerIconPath =
-      resources.paths.find((p) => p.id === getArmdozerIconPathId(armdozerId))
+      resources.paths.find((p) => p.id === getArmdozerBustShotPathId(armdozerId))
         ?.path ?? "";
     const armdozer = Armdozers.find((a) => a.id === armdozerId) ?? Armdozers[0];
     const burst = burstOverview(armdozer.burst);
