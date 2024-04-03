7111835e10cd1559b2b71eba032f75be6a303273
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-03 07:48:31 +0000

    シンヤ以外のパイロットカードのStoryBookを追加した

--- stories/pilot-card.stories.ts
+++ stories/pilot-card.stories.ts
@@ -12,3 +12,27 @@ export const shinya = domStub((resources) => {
   const card = new PilotCard(resources, PilotIds.SHINYA);
   return card.getRootHTMLElement();
 });
+
+/** ツバサ */
+export const tsubasa = domStub((resources) => {
+  const card = new PilotCard(resources, PilotIds.TSUBASA);
+  return card.getRootHTMLElement();
+});
+
+/** ガイ */
+export const gai = domStub((resources) => {
+  const card = new PilotCard(resources, PilotIds.GAI);
+  return card.getRootHTMLElement();
+});
+
+/** ライト */
+export const raito = domStub((resources) => {
+  const card = new PilotCard(resources, PilotIds.RAITO);
+  return card.getRootHTMLElement();
+});
+
+/** ユウヤ */
+export const yuuya = domStub((resources) => {
+  const card = new PilotCard(resources, PilotIds.YUUYA);
+  return card.getRootHTMLElement();
+});
