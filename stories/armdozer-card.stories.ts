2bae880e4a4648fbc1081a7525c4949ba2228029
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-03 02:44:22 +0000

    他ロボのStoryBookを追加した

--- stories/armdozer-card.stories.ts
+++ stories/armdozer-card.stories.ts
@@ -12,3 +12,27 @@ export const shinBraver = domStub((resources) => {
   const card = new ArmdozerCard(resources, ArmdozerIds.SHIN_BRAVER);
   return card.getRootHTMLElement();
 });
+
+/** ウィングドーザ */
+export const wingDozer = domStub((resources) => {
+  const card = new ArmdozerCard(resources, ArmdozerIds.WING_DOZER);
+  return card.getRootHTMLElement();
+});
+
+/** ネオランドーザ */
+export const neoLandozer = domStub((resources) => {
+  const card = new ArmdozerCard(resources, ArmdozerIds.NEO_LANDOZER);
+  return card.getRootHTMLElement();
+});
+
+/** ライトニングドーザ */
+export const lightningDozer = domStub((resources) => {
+  const card = new ArmdozerCard(resources, ArmdozerIds.LIGHTNING_DOZER);
+  return card.getRootHTMLElement();
+});
+
+/** ジェネシスブレイバー */
+export const genesisBraver = domStub((resources) => {
+  const card = new ArmdozerCard(resources, ArmdozerIds.GENESIS_BRAVER);
+  return card.getRootHTMLElement();
+});
