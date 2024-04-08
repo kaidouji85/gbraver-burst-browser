8f4d46f1ec363d2e9ad453a63892aef208135030
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-08 04:39:35 +0000

    不要なファイルを削除した

--- src/js/dom-scenes/player-select/dom/extract-elements.ts
+++
@@ -1,8 +0,0 @@
-/**
- * アームドーザアイコン集合を抽出する
- * @param root ルート要素
- * @return 抽出結果
- */
-export const extractArmdozerIcons = (root: HTMLElement): HTMLElement =>
-  root.querySelector('[data-id="armdozer-icons"]') ??
-  document.createElement("div");
