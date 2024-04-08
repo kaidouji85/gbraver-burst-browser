e14ee5917e85b7e2c3decf05270f3b60068d0aef
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-08 23:11:04 +0900

    hbsのlintを追加した

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,10 @@ const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const jest = require("eslint-plugin-jest");
 const simpleImportSort = require("eslint-plugin-simple-import-sort");
-// const html = require("@html-eslint/eslint-plugin");
-// const htmlParser = require("@html-eslint/parser");
 
 module.exports = [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  //html,
   // TODO srcとそれ以外でglobalsを出し分けする
   {
     languageOptions: {
@@ -35,30 +32,5 @@ module.exports = [
     rules: {
       ...jest.configs["flat/recommended"].rules,
     },
-  },
-  // {
-  //   files: ["**/*.hbs"],
-  //   plugins: {
-  //     "@html-eslint": html,
-  //   },
-  //   languageOptions: {
-  //     parser: htmlParser,
-  //   },
-  //   rules: {
-  //     "@html-eslint/require-doctype": "off",
-  //     "@html-eslint/indent": ["error", 2],
-  //     "@html-eslint/require-closing-tags": [
-  //       "error",
-  //       {
-  //         selfClosing: "always",
-  //       },
-  //     ],
-  //     "@html-eslint/no-extra-spacing-attrs": [
-  //       "error",
-  //       {
-  //         enforceBeforeSelfClose: true,
-  //       },
-  //     ],
-  //   },
-  // },
+  }
 ];
---
+++ eslint.hbs.config.js
@@ -0,0 +1,25 @@
+const html = require("@html-eslint/eslint-plugin");
+const parser = require("@html-eslint/parser");
+
+module.exports = [
+  html.configs["flat/recommended"],
+  {
+    files: ["**/*.hbs"],
+    plugins: {
+      "@html-eslint": html,
+    },
+    languageOptions: {
+      parser,
+    },
+    rules: {
+      "@html-eslint/require-doctype": "off",
+      "@html-eslint/indent": ["error", 2],
+      "@html-eslint/require-closing-tags": ["error", {
+        "selfClosing": "always"
+      }],
+      "@html-eslint/no-extra-spacing-attrs": ["error", {
+        "enforceBeforeSelfClose": true
+      }]
+    }
+  },
+];
