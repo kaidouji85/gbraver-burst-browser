96fb6f76efdddef544ab7ce5c71317c215d64655
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-10 22:17:29 +0900

    SEPlayerを追加した

---
+++ src/js/se/se-player.ts
@@ -0,0 +1,38 @@
+import { SoundResource } from "../resource/sound/resource";
+import { Resources } from "../resource";
+
+/** 効果音再生オブジェクト */
+export interface SEPlayer {
+  /** 効果音のマスターボリューム、設定画面で入力したものをセットする */
+  volume: number;
+
+  /**
+   * 効果音を再生する
+   * @param sound 再生する効果音
+   */
+  play(sound: SoundResource): void;
+}
+
+/** シンプルな効果音再生オブジェクト */
+class SimpleSEPlayer implements SEPlayer {
+  /** @override */
+  volume: number;
+
+  /**
+   * コンストラクタ
+   */
+  constructor() {
+    this.volume = 1;
+  }
+
+  /** @override */
+  play(sound: SoundResource): void {
+    sound.sound.volume(this.volume * sound.volumeScale);
+  }
+}
+
+/**
+ * SEPlayerを生成する
+ * @return 生成したSEPlayer
+ */
+export const createSEPlayer = (): SEPlayer => new SimpleSEPlayer();
