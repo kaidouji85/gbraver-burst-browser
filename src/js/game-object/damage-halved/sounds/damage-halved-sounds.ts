3626cd0889d18ee9fa7613f16003e8ea1b0bd252
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-10 10:12:30 +0000

    ダメージ半減をSoundResourceにした

--- src/js/game-object/damage-halved/sounds/damage-halved-sounds.ts
+++ src/js/game-object/damage-halved/sounds/damage-halved-sounds.ts
@@ -1,20 +1,20 @@
-import { Howl } from "howler";
-
 import type { Resources } from "../../../resource";
+import { createEmptySoundResource } from "../../../resource/sound/empty-sound-resource";
 import { SOUND_IDS } from "../../../resource/sound/ids";
+import { SoundResource } from "../../../resource/sound/resource";
 
 /** ダメージ半減 効果音 */
 export class DamageHalvedSounds {
-  benefitEffect: Howl;
+  /** メリット効果音 */
+  benefitEffect: SoundResource;
 
   /**
    * コンストラクタ
-   *
    * @param resources リソース管理オブジェクト
    */
   constructor(resources: Resources) {
     this.benefitEffect =
-      resources.sounds.find((v) => v.id === SOUND_IDS.BENEFIT_EFFECT)?.sound ??
-      new Howl({ src: "" });
+      resources.sounds.find((v) => v.id === SOUND_IDS.BENEFIT_EFFECT) ??
+      createEmptySoundResource()
   }
 }
