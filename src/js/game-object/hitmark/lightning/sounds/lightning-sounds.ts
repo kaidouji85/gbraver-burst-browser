ec0bf3cbc76edf2b424f1dbb5dc04ee4b13fc4fe
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-10 10:14:01 +0000

    電撃をSoundResourceにした

--- src/js/game-object/hitmark/lightning/sounds/lightning-sounds.ts
+++ src/js/game-object/hitmark/lightning/sounds/lightning-sounds.ts
@@ -1,25 +1,19 @@
-import { Howl } from "howler";
-
 import type { Resources } from "../../../../resource";
+import { createEmptySoundResource } from "../../../../resource/sound/empty-sound-resource";
 import { SOUND_IDS } from "../../../../resource/sound/ids";
+import { SoundResource } from "../../../../resource/sound/resource";
 
-/**
- * 電撃 音
- */
+/** 電撃 音 */
 export class LightningSounds {
-  lightning: Howl;
+  /** 電撃 */
+  lightning: SoundResource;
 
   /**
    * コンストラクタ
-   *
    * @param resources リソース管理オブジェクト
    */
   constructor(resources: Resources) {
-    const lightningResource = resources.sounds.find(
-      (v) => v.id === SOUND_IDS.LIGHTNING_ATTACK,
-    );
-    this.lightning = lightningResource
-      ? lightningResource.sound
-      : new Howl({ src: "" });
+    this.lightning = resources.sounds.find((v) => v.id === SOUND_IDS.LIGHTNING_ATTACK)
+      ?? createEmptySoundResource();
   }
 }
