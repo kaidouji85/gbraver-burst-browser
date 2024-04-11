3e9b78c15954e716f02ecf440f6837291634f357
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-11 06:48:03 +0000

    シンブレイバーアニメーションプロパティを別ファイルに切り出した

--- src/js/game-object/armdozer/shin-braver/props/shin-braver-props.ts
+++ src/js/game-object/armdozer/shin-braver/props/shin-braver-props.ts
@@ -1,13 +1,8 @@
-import { ShinBraverModel } from "../model/shin-braver-model";
-import { ShinBraverSounds } from "../sounds/shin-braver-sounds";
+import { ShinBraverAnimationProps } from "../animation/animation-props";
 import { ShinBraverView } from "../view/shin-braver-view";
 
 /** シンブレイバー プロパティ */
-export type ShinBraverProps = {
-  /** モデル */
-  model: ShinBraverModel;
+export type ShinBraverProps = ShinBraverAnimationProps & {
   /** ビュー */
   view: ShinBraverView;
-  /** サウンド */
-  sounds: ShinBraverSounds;
 };
