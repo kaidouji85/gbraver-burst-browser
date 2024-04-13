f1ebd05bd6c3f4e39f5cf970c46ddbeabcdc83d8
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-13 17:24:16 +0900

    プロパティを追加した

---
+++ src/js/game-object/cut-in/neo-landozer/props/create-neo-landozer-cutin-props.ts
@@ -0,0 +1,24 @@
+import { createInitialValue } from "../model/initial-value";
+import { NeoLandozerCutInView } from "../view/neo-landozer-cutin-view";
+import { NeoLandozerCutInProps } from "./neo-landozer-cutin-props";
+
+/** NeoLandozerCutInProps生成パラメータ */
+export type GenerateNeoLandozerCutInPropsParams = {
+  /** ビュー */
+  view: NeoLandozerCutInView;
+};
+
+/**
+ * NeoLandozerCutInPropsを生成する
+ * @param params 生成パラメータ
+ * @returns 生成結果
+ */
+export function createNeoLandozerCutInProps(
+  params: GenerateNeoLandozerCutInPropsParams,
+): NeoLandozerCutInProps {
+  const { view } = params;
+  return {
+    model: createInitialValue(),
+    view,
+  };
+}
---
+++ src/js/game-object/cut-in/neo-landozer/props/neo-landozer-cutin-props.ts
@@ -0,0 +1,10 @@
+import {NeoLandozerCutInModel} from "../model/neo-landozer-cutin-model";
+import {NeoLandozerCutInView} from "../view/neo-landozer-cutin-view";
+
+/** ネオランドーザ カットイン プロパティ */
+export type NeoLandozerCutInProps = {
+  /** モデル */
+  model: NeoLandozerCutInModel;
+  /** ビュー */
+  view: NeoLandozerCutInView;
+}
