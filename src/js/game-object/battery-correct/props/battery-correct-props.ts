42575aae88304ece1eaa897e920bca8ba34be079
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-13 12:45:13 +0900

    プロパティを追加

---
+++ src/js/game-object/battery-correct/props/battery-correct-props.ts
@@ -0,0 +1,8 @@
+import { BatteryCorrectModel } from "../model/battery-correct-model";
+import { BatteryCorrectView } from "../view/battery-correct-view";
+
+/** バッテリー補正 プロパティ */
+export type BatteryCorrectProps = {
+  model: BatteryCorrectModel;
+  view: BatteryCorrectView;
+};
