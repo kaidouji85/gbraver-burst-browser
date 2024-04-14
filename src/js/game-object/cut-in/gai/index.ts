561bb3cc4a86efae157d8a8e0655edb2c74bc9cb
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-15 00:31:26 +0900

    ガイカットインも対応

--- src/js/game-object/cut-in/gai/index.ts
+++ src/js/game-object/cut-in/gai/index.ts
@@ -1,6 +1,7 @@
 import { Observable } from "rxjs";
 
 import type { Resources } from "../../../resource";
+import { SEPlayer } from "../../../se/se-player";
 import type { GameObjectAction } from "../../action/game-object-action";
 import { GaiCutIn } from "./gai";
 import { EnemyGaiView } from "./view/enemy-gai-view";
@@ -10,6 +11,8 @@ import { PlayerGaiView } from "./view/player-gai-view";
 export type GenerateGaiCutInParams = {
   /** リソース管理オブジェクト */
   resources: Resources;
+  /** 効果音再生オブジェクト */
+  se: SEPlayer;
   /** ゲームオブジェクトアクション */
   gameObjectAction: Observable<GameObjectAction>;
 };
