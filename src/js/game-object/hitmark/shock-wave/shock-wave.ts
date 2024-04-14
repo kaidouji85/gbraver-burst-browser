5ae68dc872d0e40c4100bc31e4d7f1e4cc74c3de
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-14 15:19:06 +0900

    プロパティを本体に統合した

--- src/js/game-object/hitmark/shock-wave/shock-wave.ts
+++ src/js/game-object/hitmark/shock-wave/shock-wave.ts
@@ -5,22 +5,17 @@ import { Animate } from "../../../animation/animate";
 import { onStart } from "../../../animation/on-start";
 import type { PreRender } from "../../../game-loop/pre-render";
 import type { Resources } from "../../../resource";
-import { createEmptySoundResource } from "../../../resource/sound/empty-sound-resource";
-import { SOUND_IDS } from "../../../resource/sound/ids";
-import { SoundResource } from "../../../resource/sound/resource";
 import type { GameObjectAction } from "../../action/game-object-action";
 import { popUp } from "./animation/pop-up";
 import type { ShockWaveModel } from "./model/shock-wave-model";
+import { createShockWaveProps } from "./props/create-shock-wave-props";
+import { ShockWaveProps } from "./props/shock-wave-props";
 import type { ShockWaveView } from "./view/shock-wave-view";
 
 /** 衝撃波 */
 export class ShockWave {
-  /** モデル */
-  #model: ShockWaveModel;
-  /** ビュー */
-  #view: ShockWaveView;
-  /** ヒット音 */
-  #hitSound: SoundResource;
+  /** プロパティ */
+  #props: ShockWaveProps;
   /** アンサブスクライバ */
   #unsubscriber: Unsubscribable;
 
@@ -37,11 +32,7 @@ export class ShockWave {
     resources: Resources,
     gameObjectAction: Observable<GameObjectAction>,
   ) {
-    this.#model = initialModel;
-    this.#view = view;
-    this.#hitSound =
-      resources.sounds.find((v) => v.id === SOUND_IDS.MECHA_IMPACT) ??
-      createEmptySoundResource();
+    this.#props = createShockWaveProps({ view, initialModel, resources });
     this.#unsubscriber = gameObjectAction.subscribe((action) => {
       if (action.type === "Update") {
         this.#onUpdate();
@@ -55,43 +46,40 @@ export class ShockWave {
    * デストラクタ相当の処理
    */
   destructor(): void {
-    this.#view.destructor();
+    this.#props.view.destructor();
     this.#unsubscriber.unsubscribe();
   }
 
   /**
    * 衝撃波を表示する
-   *
    * @return アニメーション
    */
   popUp(): Animate {
     return onStart(() => {
-      this.#hitSound.sound.play();
-    }).chain(popUp(this.#model));
+      this.#props.hitSound.sound.play();
+    }).chain(popUp(this.#props.model));
   }
 
   /**
    * シーンに追加するオブジェクトを返す
-   *
    * @return シーンに追加するオブジェクト
    */
   getObject3D(): THREE.Object3D {
-    return this.#view.getObject3D();
+    return this.#props.view.getObject3D();
   }
 
   /**
    * アップデート時の処理
    */
   #onUpdate(): void {
-    this.#view.engage(this.#model);
+    this.#props.view.engage(this.#props.model);
   }
 
   /**
    * プリレンダー時の処理
-   *
    * @param action アクション
    */
   #onPreRender(action: PreRender): void {
-    this.#view.lookAt(action.camera);
+    this.#props.view.lookAt(action.camera);
   }
 }
