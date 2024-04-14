d4452deb28ad9ed1531aa3138356b5f57f8e6719
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-14 17:25:09 +0900

    シンブレイバー対応

--- src/js/game-object/armdozer/shin-braver/animation/burst-to-stand.ts
+++ src/js/game-object/armdozer/shin-braver/animation/burst-to-stand.ts
@@ -10,11 +10,11 @@ import { ShinBraverAnimationProps } from "./animation-props";
  * @return アニメーション
  */
 export function burstToStand(props: ShinBraverAnimationProps): Animate {
-  const { model, sounds } = props;
+  const { model, sounds, se } = props;
   return onStart(() => {
     model.animation.type = "BURST_DOWN";
     model.animation.frame = 1;
-    sounds.motor.sound.play();
+    se.play(sounds.motor);
   })
     .chain(
       tween(model.animation, (t) =>
@@ -35,7 +35,7 @@ export function burstToStand(props: ShinBraverAnimationProps): Animate {
     .chain(delay(500))
     .chain(
       onStart(() => {
-        sounds.motor.sound.play();
+        se.play(sounds.motor);
       }),
     )
     .chain(
--- src/js/game-object/armdozer/shin-braver/props/create-shin-braver-props.ts
+++ src/js/game-object/armdozer/shin-braver/props/create-shin-braver-props.ts
@@ -3,6 +3,7 @@ import { createInitialValue } from "../model/initial-value";
 import { ShinBraverSounds } from "../sounds/shin-braver-sounds";
 import { ShinBraverView } from "../view/shin-braver-view";
 import { ShinBraverProps } from "./shin-braver-props";
+import { SEPlayer } from "../../../../se/se-player";
 
 /** ShinBraverProps生成パラメータ */
 export type GenerateShinBraverPropsParams = {
@@ -10,6 +11,8 @@ export type GenerateShinBraverPropsParams = {
   view: ShinBraverView;
   /** リソース管理オブジェクト */
   resources: Resources;
+  /** SE再生 */
+  se: SEPlayer;
 };
 
 /**
@@ -20,9 +23,9 @@ export type GenerateShinBraverPropsParams = {
 export function createShinBraverProps(
   params: GenerateShinBraverPropsParams,
 ): ShinBraverProps {
-  const { view, resources } = params;
+  const { resources } = params;
   return {
-    view,
+    ...params,
     model: createInitialValue(),
     sounds: new ShinBraverSounds(resources),
   };
