d0c33ce7386bdbdba2cce45048d1b57644118e35
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-14 13:07:34 +0900

    プロパティを追加

---
+++ src/js/game-object/cut-in/wing-dozer/props/wing-dozer-cutin-props.ts
@@ -0,0 +1,10 @@
+import { WingDozerCutInModel } from "../model/wing-dozer-cutin-model";
+import { WingDozerCutInView } from "../view/wing-dozer-cutin-view";
+
+/** ウィングドーザ カットイン プロパティ */
+export type WingDozerCutInProps = {
+  /** モデル */
+  model: WingDozerCutInModel;
+  /** ビュー */
+  view: WingDozerCutInView;
+};
