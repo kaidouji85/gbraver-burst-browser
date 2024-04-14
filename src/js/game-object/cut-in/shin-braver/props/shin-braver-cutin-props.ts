b188480bf6e9c9b3aaf66e4cc4a61bd28a40b094
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-14 11:03:56 +0900

    プロパティ実装

---
+++ src/js/game-object/cut-in/shin-braver/props/shin-braver-cutin-props.ts
@@ -0,0 +1,10 @@
+import { ShinBraverCutInModel } from "../model/shin-braver-cutin-model";
+import { ShinBraverCutInView } from "../view/shin-braver-cutin-view";
+
+/** シンブレイバーカットイン プロパティ */
+export type ShinBraverCutInProps = {
+  /** モデル */
+  model: ShinBraverCutInModel;
+  /** ビュー */
+  view: ShinBraverCutInView;
+};
