7f30808a4b119ff626e36a5feac2804867820d49
kaidouji85 <kaidouji85@gmail.com>
2024-04-17 02:34:02 +0000

    各モードの詳細を追加した

--- src/js/dom-scenes/config/dom/player-selector-types.ts
+++ src/js/dom-scenes/config/dom/player-selector-types.ts
@@ -3,21 +3,27 @@ import {
   PlayerSelectorTypes,
 } from "../../../game/config/browser-config";
 import { ROOT_CLASS } from "./class-name";
+import OpenPlayerSelectorDetailTemplate from "./open-player-selector-detail.hbs";
 import PlayerSelectorTypeTemplate from "./player-selector-type.hbs";
+import SecretPlayerSelectorDetailTemplate from "./secret-player-selector-detail.hbs";
 
 /** ボタン設定 */
 type ButtonConfig = {
   /** ラベル */
   label: string;
+  /** 詳細説明 */
+  detail: string;
 };
 
 /** ボタン設定をあつめたもの */
 export const configs: { [key in PlayerSelectorType]: ButtonConfig } = {
   open: {
     label: "オープン",
+    detail: OpenPlayerSelectorDetailTemplate({ROOT_CLASS}),
   },
   secret: {
     label: "シークレット",
+    detail: SecretPlayerSelectorDetailTemplate({ROOT_CLASS}),
   },
 };
 
@@ -29,11 +35,12 @@ export const configs: { [key in PlayerSelectorType]: ButtonConfig } = {
 export const playerSelectorTypeItems = (selected: PlayerSelectorType) =>
   PlayerSelectorTypes.map((value) => {
     const config = configs[value] ?? configs.open;
-    const { label } = config;
+    const { label, detail } = config;
     const checked = selected === value ? "checked" : "";
     return PlayerSelectorTypeTemplate({
       ROOT_CLASS,
       label,
+      detail,
       value,
       checked,
     });
