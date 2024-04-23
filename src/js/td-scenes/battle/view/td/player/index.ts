65a43dbe5a8df478d355e82b00e4d1af5ef48417
kaidouji85 <kaidouji85@gmail.com>
2024-04-23 06:00:21 +0000

    TDPlayerをモジュール分割した

--- src/js/td-scenes/battle/view/td/player/index.ts
+++ src/js/td-scenes/battle/view/td/player/index.ts
@@ -1,84 +1,14 @@
-import type { PlayerId } from "gbraver-burst-core";
 import * as THREE from "three";
 
-import {
-  enemyBatteryCorrect,
-  playerBatteryCorrect,
-} from "../../../../../game-object/battery-correct";
-import { BatteryCorrect } from "../../../../../game-object/battery-correct/battery-correct";
-import {
-  enemyBatteryEnchantment,
-  playerBatteryEnchantment,
-} from "../../../../../game-object/battery-enchantment";
-import { BatteryEnchantment } from "../../../../../game-object/battery-enchantment/battery-enchantment";
-import {
-  enemyBatteryNumber,
-  playerBatteryNumber,
-} from "../../../../../game-object/battery-number";
-import { BatteryNumber } from "../../../../../game-object/battery-number/battery-number";
-import {
-  enemyContinuousAttack,
-  playerContinuousAttack,
-} from "../../../../../game-object/continuous-attack";
-import { ContinuousAttackIndicator } from "../../../../../game-object/continuous-attack/continuous-attack-indicator";
-import {
-  enemyDamageHalved,
-  playerDamageHalved,
-} from "../../../../../game-object/damage-halved";
-import { DamageHalved } from "../../../../../game-object/damage-halved/damage-halved";
-import {
-  enemyDamageIndicator,
-  playerDamageIndicator,
-} from "../../../../../game-object/damage-indicator";
-import { DamageIndicator } from "../../../../../game-object/damage-indicator/damage-indicator";
-import {
-  enemyLightning,
-  playerLightning,
-} from "../../../../../game-object/hitmark/lightning";
-import { Lightning } from "../../../../../game-object/hitmark/lightning/lightning";
-import {
-  enemyShockWave,
-  playerShockWave,
-} from "../../../../../game-object/hitmark/shock-wave";
-import { ShockWave } from "../../../../../game-object/hitmark/shock-wave/shock-wave";
-import { enemyPowerUp, playerPowerUp } from "../../../../../game-object/power-up";
-import { PowerUp } from "../../../../../game-object/power-up/power-up";
-import {
-  enemyRecoverBattery,
-  playerRecoverBattery,
-} from "../../../../../game-object/recover-battery";
-import { RecoverBattery } from "../../../../../game-object/recover-battery/recover-battery";
-import {
-  enemyReflectIndicator,
-  playerReflectIndicator,
-} from "../../../../../game-object/reflect-indicator";
-import { ReflectIndicator } from "../../../../../game-object/reflect-indicator/reflect-indicator";
 import { TDLayerObjectCreatorParams } from "../creator-params";
+import { createEnemyProps } from "./procedure/create-enemy-props";
+import { createPlayerProps } from "./procedure/create-player-props";
+import { destructor } from "./procedure/destructor";
+import { getObject3Ds } from "./procedure/get-object-3d";
+import { TDPlayerProps } from "./props";
 
-/**
- * 3Dレイヤー プレイヤー関係オブジェクト フィールド
- */
-export interface TDPlayerField {
-  playerId: PlayerId;
-  hitMark: {
-    shockWave: ShockWave;
-    lightning: Lightning;
-  };
-  armdozerEffects: {
-    powerUp: PowerUp;
-    reflect: ReflectIndicator;
-    continuousAttack: ContinuousAttackIndicator;
-    damageHalved: DamageHalved;
-    batteryEnchantment: BatteryEnchantment;
-  };
-  batteryNumber: BatteryNumber;
-  batteryCorrect: BatteryCorrect;
-  recoverBattery: RecoverBattery;
-  damageIndicator: DamageIndicator;
-}
-
-/** 3Dレイヤー プレイヤー関係オブジェクト */
-export interface TDPlayer extends TDPlayerField {
+/** 3Dレイヤー プレイヤーオブジェクト */
+export type TDPlayer = TDPlayerProps & {
   /**
    * デストラクタ相当の処理
    */
@@ -86,80 +16,22 @@ export interface TDPlayer extends TDPlayerField {
 
   /**
    * シーンに追加するオブジェクトを取得する
-   *
    * @return シーンに追加するオブジェクト
    */
   getObject3Ds(): THREE.Object3D[];
-}
+};
 
 /**
- * 3Dレイヤー プレイヤー関係オブジェクト 実装
+ * TDPlayer生成関数
+ * @param props プロパティ
+ * @return 生成結果
  */
-class TDPlayerImpl implements TDPlayer {
-  playerId: PlayerId;
-  hitMark: {
-    shockWave: ShockWave;
-    lightning: Lightning;
+function createTDPlayer(props: TDPlayerProps): TDPlayer {
+  return {
+    ...props,
+    destructor: () => destructor(props),
+    getObject3Ds: () => getObject3Ds(props),
   };
-  armdozerEffects: {
-    powerUp: PowerUp;
-    reflect: ReflectIndicator;
-    continuousAttack: ContinuousAttackIndicator;
-    damageHalved: DamageHalved;
-    batteryEnchantment: BatteryEnchantment;
-  };
-  batteryNumber: BatteryNumber;
-  batteryCorrect: BatteryCorrect;
-  recoverBattery: RecoverBattery;
-  damageIndicator: DamageIndicator;
-
-  constructor(param: TDPlayerField) {
-    this.playerId = param.playerId;
-    this.hitMark = param.hitMark;
-    this.armdozerEffects = param.armdozerEffects;
-    this.batteryNumber = param.batteryNumber;
-    this.batteryCorrect = param.batteryCorrect;
-    this.recoverBattery = param.recoverBattery;
-    this.damageIndicator = param.damageIndicator;
-  }
-
-  /**
-   * デストラクタ相当の処理
-   */
-  destructor(): void {
-    this.batteryNumber.destructor();
-    this.batteryCorrect.destructor();
-    this.damageIndicator.destructor();
-    this.hitMark.shockWave.destructor();
-    this.hitMark.lightning.destructor();
-    this.armdozerEffects.powerUp.destructor();
-    this.armdozerEffects.reflect.destructor();
-    this.armdozerEffects.continuousAttack.destructor();
-    this.armdozerEffects.damageHalved.destructor();
-    this.armdozerEffects.batteryEnchantment.destructor();
-    this.recoverBattery.destructor();
-  }
-
-  /**
-   * シーンに追加するオブジェクトを取得する
-   *
-   * @return シーンに追加するオブジェクト
-   */
-  getObject3Ds(): THREE.Object3D[] {
-    return [
-      this.hitMark.shockWave.getObject3D(),
-      this.hitMark.lightning.getObject3D(),
-      this.armdozerEffects.powerUp.getObject3D(),
-      this.armdozerEffects.reflect.getObject3D(),
-      this.armdozerEffects.continuousAttack.getObject3D(),
-      this.armdozerEffects.damageHalved.getObject3D(),
-      this.armdozerEffects.batteryEnchantment.getObject3D(),
-      this.batteryNumber.getObject3D(),
-      this.batteryCorrect.getObject3D(),
-      this.recoverBattery.getObject3D(),
-      this.damageIndicator.getObject3D(),
-    ];
-  }
 }
 
 /**
@@ -168,25 +40,8 @@ class TDPlayerImpl implements TDPlayer {
  * @return 3Dプレイヤーオブジェクト
  */
 export function playerTDObjects(params: TDLayerObjectCreatorParams): TDPlayer {
-  const { resources, player, gameObjectAction } = params;
-  return new TDPlayerImpl({
-    playerId: player.playerId,
-    hitMark: {
-      shockWave: playerShockWave(params),
-      lightning: playerLightning(params),
-    },
-    armdozerEffects: {
-      powerUp: playerPowerUp(params),
-      reflect: playerReflectIndicator(resources, gameObjectAction),
-      continuousAttack: playerContinuousAttack(params),
-      damageHalved: playerDamageHalved(params),
-      batteryEnchantment: playerBatteryEnchantment(params),
-    },
-    batteryNumber: playerBatteryNumber(params),
-    batteryCorrect: playerBatteryCorrect(params),
-    recoverBattery: playerRecoverBattery(params),
-    damageIndicator: playerDamageIndicator(params),
-  });
+  const props = createPlayerProps(params);
+  return createTDPlayer(props);
 }
 
 /**
@@ -195,23 +50,6 @@ export function playerTDObjects(params: TDLayerObjectCreatorParams): TDPlayer {
  * @return 3Dプレイヤーオブジェクト
  */
 export function enemyTDObject(params: TDLayerObjectCreatorParams): TDPlayer {
-  const { resources, enemy, gameObjectAction } = params;
-  return new TDPlayerImpl({
-    playerId: enemy.playerId,
-    hitMark: {
-      shockWave: enemyShockWave(params),
-      lightning: enemyLightning(params),
-    },
-    armdozerEffects: {
-      powerUp: enemyPowerUp(params),
-      reflect: enemyReflectIndicator(resources, gameObjectAction),
-      continuousAttack: enemyContinuousAttack(params),
-      damageHalved: enemyDamageHalved(params),
-      batteryEnchantment: enemyBatteryEnchantment(params),
-    },
-    batteryNumber: enemyBatteryNumber(params),
-    batteryCorrect: enemyBatteryCorrect(params),
-    recoverBattery: enemyRecoverBattery(params),
-    damageIndicator: enemyDamageIndicator(params),
-  });
+  const props = createEnemyProps(params);
+  return createTDPlayer(props);
 }
---
+++ src/js/td-scenes/battle/view/td/player/procedure/create-enemy-props.ts
@@ -0,0 +1,42 @@
+import { enemyBatteryCorrect } from "../../../../../../game-object/battery-correct";
+import { enemyBatteryEnchantment } from "../../../../../../game-object/battery-enchantment";
+import { enemyBatteryNumber } from "../../../../../../game-object/battery-number";
+import { enemyContinuousAttack } from "../../../../../../game-object/continuous-attack";
+import { enemyDamageHalved } from "../../../../../../game-object/damage-halved";
+import { enemyDamageIndicator } from "../../../../../../game-object/damage-indicator";
+import { enemyLightning } from "../../../../../../game-object/hitmark/lightning";
+import { enemyShockWave } from "../../../../../../game-object/hitmark/shock-wave";
+import { enemyPowerUp } from "../../../../../../game-object/power-up";
+import { enemyRecoverBattery } from "../../../../../../game-object/recover-battery";
+import { enemyReflectIndicator } from "../../../../../../game-object/reflect-indicator";
+import { TDLayerObjectCreatorParams } from "../../creator-params";
+import { TDPlayerProps } from "../props";
+
+/**
+ * 敵側のTDPlayerPropsを生成する
+ * @param params 生成パラメータ
+ * @return 生成結果
+ */
+export function createEnemyProps(
+  params: TDLayerObjectCreatorParams,
+): TDPlayerProps {
+  const { resources, enemy, gameObjectAction } = params;
+  return {
+    playerId: enemy.playerId,
+    hitMark: {
+      shockWave: enemyShockWave(params),
+      lightning: enemyLightning(params),
+    },
+    armdozerEffects: {
+      powerUp: enemyPowerUp(params),
+      reflect: enemyReflectIndicator(resources, gameObjectAction),
+      continuousAttack: enemyContinuousAttack(params),
+      damageHalved: enemyDamageHalved(params),
+      batteryEnchantment: enemyBatteryEnchantment(params),
+    },
+    batteryNumber: enemyBatteryNumber(params),
+    batteryCorrect: enemyBatteryCorrect(params),
+    recoverBattery: enemyRecoverBattery(params),
+    damageIndicator: enemyDamageIndicator(params),
+  };
+}
