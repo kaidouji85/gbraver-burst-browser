e8aeb5916c28090da0fbc6ff777beb45112f025e
kaidouji85 <kaidouji85@gmail.com>
2024-04-23 08:49:21 +0000

    HUDPlayerをシンプルにした

--- src/js/td-scenes/battle/view/hud/player/index.ts
+++ src/js/td-scenes/battle/view/hud/player/index.ts
@@ -1,109 +1,60 @@
-import type { PlayerId } from "gbraver-burst-core";
 import * as THREE from "three";
 
-import { enemyGauge, playerGauge } from "../../../../../game-object/gauge";
-import { Gauge } from "../../../../../game-object/gauge/gauge";
-import {
-  loseIndicator,
-  winIndicator,
-} from "../../../../../game-object/result-indicator";
-import { ResultIndicator } from "../../../../../game-object/result-indicator/result-indicator";
-import {
-  enemyTurnStart,
-  playerTurnStart,
-} from "../../../../../game-object/turn-start";
-import { TurnStart } from "../../../../../game-object/turn-start/turn-start";
 import { HUDLayerObjectCreatorParams } from "../creator-params";
+import { createEnemyProps } from "./procedure/create-enemy-props";
+import { createPlayerProps } from "./procedure/create-player-props";
+import { destructor } from "./procedure/destructor";
+import { getObject3Ds } from "./procedure/get-object-3ds";
+import { HUDPlayerProps } from "./props";
 
-/**
- * HUDレイヤー プレイヤー固有オブジェクト フィールド
- */
-export interface HUDPlayerField {
-  playerId: PlayerId;
-  gauge: Gauge;
-  turnStart: TurnStart;
-  resultIndicator: ResultIndicator;
-}
-
-/**
- * HUDレイヤー プレイヤー固有オブジェクト
- */
-export class HUDPlayer implements HUDPlayerField {
-  playerId: PlayerId;
-  gauge: Gauge;
-  turnStart: TurnStart;
-  resultIndicator: ResultIndicator;
-
-  constructor(param: HUDPlayerField) {
-    this.playerId = param.playerId;
-    this.gauge = param.gauge;
-    this.turnStart = param.turnStart;
-    this.resultIndicator = param.resultIndicator;
-  }
-
+/** HUDプレイヤーブジェクト */
+export type HUDPlayer = HUDPlayerProps & {
   /**
    * デストラクタ相当の処理
    */
-  destructor(): void {
-    this.gauge.destructor();
-    this.turnStart.destructor();
-    this.resultIndicator.destructor();
-  }
+  destructor(): void;
 
   /**
    * シーンに追加するオブジェクトを取得する
    *
    * @return シーンに追加するオブジェクト
    */
-  getObject3Ds(): THREE.Object3D[] {
-    return [
-      this.gauge.getObject3D(),
-      this.turnStart.getObject3D(),
-      this.resultIndicator.getObject3D(),
-    ];
-  }
+  getObject3Ds(): THREE.Object3D[];
+};
+
+/**
+ * HUDPlayerを生成する
+ * @param props プロパティ
+ * @return 生成結果
+ */
+function createHUDPlayer(props: HUDPlayerProps): HUDPlayer {
+  return {
+    ...props,
+    destructor: () => destructor(props),
+    getObject3Ds: () => getObject3Ds(props),
+  };
 }
 
 /**
- * プレイヤー側 HUDレイヤープレイヤー固有オブジェクト
+ * プレイヤー側 HUDプレイヤーオブジェクト
  * @param params 生成パラメータ
  * @return 生成結果
  */
 export function playerHUDObjects(
   params: HUDLayerObjectCreatorParams,
 ): HUDPlayer {
-  const { resources, player, gameObjectAction } = params;
-  return new HUDPlayer({
-    playerId: player.playerId,
-    gauge: playerGauge({
-      resources: resources,
-      gameObjectAction: gameObjectAction,
-      hp: player.armdozer.maxHp,
-      battery: player.armdozer.maxBattery,
-    }),
-    turnStart: playerTurnStart(resources, gameObjectAction),
-    resultIndicator: winIndicator(resources, gameObjectAction),
-  });
+  const props = createPlayerProps(params);
+  return createHUDPlayer(props);
 }
 
 /**
- * 敵側 HUDレイヤープレイヤー固有オブジェクト
+ * 敵側 HUDプレイヤーオブジェクト
  * @param params 生成パラメータ
  * @return 生成結果
  */
 export function enemyHUDObjects(
   params: HUDLayerObjectCreatorParams,
 ): HUDPlayer {
-  const { resources, enemy, gameObjectAction } = params;
-  return new HUDPlayer({
-    playerId: enemy.playerId,
-    gauge: enemyGauge({
-      resources: resources,
-      gameObjectAction: gameObjectAction,
-      hp: enemy.armdozer.maxHp,
-      battery: enemy.armdozer.maxBattery,
-    }),
-    turnStart: enemyTurnStart(resources, gameObjectAction),
-    resultIndicator: loseIndicator(resources, gameObjectAction),
-  });
+  const props = createEnemyProps(params);
+  return createHUDPlayer(props);
 }
---
+++ src/js/td-scenes/battle/view/hud/player/procedure/create-player-props.ts
@@ -0,0 +1,27 @@
+import { playerGauge } from "../../../../../../game-object/gauge";
+import { winIndicator } from "../../../../../../game-object/result-indicator";
+import { playerTurnStart } from "../../../../../../game-object/turn-start";
+import { HUDLayerObjectCreatorParams } from "../../creator-params";
+import { HUDPlayerProps } from "../props";
+
+/**
+ * プレイヤー側のHUDPlayerPropsを生成する
+ * @param params 生成パラメータ
+ * @return 生成結果
+ */
+export function createPlayerProps(
+  params: HUDLayerObjectCreatorParams,
+): HUDPlayerProps {
+  const { resources, player, gameObjectAction } = params;
+  return {
+    playerId: player.playerId,
+    gauge: playerGauge({
+      resources: resources,
+      gameObjectAction: gameObjectAction,
+      hp: player.armdozer.maxHp,
+      battery: player.armdozer.maxBattery,
+    }),
+    turnStart: playerTurnStart(resources, gameObjectAction),
+    resultIndicator: winIndicator(resources, gameObjectAction),
+  };
+}
