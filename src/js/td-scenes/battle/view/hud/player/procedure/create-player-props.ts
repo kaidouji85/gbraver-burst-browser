9f59b3ee4c63d2b311d700d8b22f63c5e2ff6dbd
kaidouji85 <kaidouji85@gmail.com>
2024-04-26 09:59:02 +0000

    ダメージ予測をゲージ配下に追加した

--- src/js/td-scenes/battle/view/hud/player/procedure/create-player-props.ts
+++ src/js/td-scenes/battle/view/hud/player/procedure/create-player-props.ts
@@ -1,4 +1,5 @@
 import { playerGauge } from "../../../../../../game-object/gauge";
+import { PredicatedDamage } from "../../../../../../game-object/predicated-damage";
 import { winIndicator } from "../../../../../../game-object/result-indicator";
 import { playerTurnStart } from "../../../../../../game-object/turn-start";
 import { HUDLayerObjectCreatorParams } from "../../creator-params";
@@ -13,14 +14,20 @@ export function createPlayerProps(
   params: HUDLayerObjectCreatorParams,
 ): HUDPlayerProps {
   const { resources, player, gameObjectAction } = params;
+
+  const gauge = playerGauge({
+    ...params,
+    hp: player.armdozer.maxHp,
+    battery: player.armdozer.maxBattery,
+  });
+
+  const predicatedDamage = new PredicatedDamage(params);
+  gauge.addObject3D(predicatedDamage.getObject3D());
+
   return {
     playerId: player.playerId,
-    gauge: playerGauge({
-      resources: resources,
-      gameObjectAction: gameObjectAction,
-      hp: player.armdozer.maxHp,
-      battery: player.armdozer.maxBattery,
-    }),
+    gauge,
+    predicatedDamage,
     turnStart: playerTurnStart(resources, gameObjectAction),
     resultIndicator: winIndicator(resources, gameObjectAction),
   };
