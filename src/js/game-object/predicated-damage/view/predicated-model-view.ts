daa8b573c49213183241717fc6fe622217e4804d
kaidouji85 <kaidouji85@gmail.com>
2024-04-26 09:35:17 +0000

    ダメージ数字が表示されるようになった

--- src/js/game-object/predicated-damage/view/predicated-model-view.ts
+++ src/js/game-object/predicated-damage/view/predicated-model-view.ts
@@ -1,3 +1,4 @@
+import * as R from "ramda";
 import * as THREE from "three";
 
 import { HorizontalAnimationMesh } from "../../../mesh/horizontal-animation";
@@ -11,13 +12,24 @@ const MAX_ANIMATION = 16;
 /** メッシュサイズ */
 const MESH_SIZE = 64;
 
+/** 数字の最大桁数 */
+const NUMBER_OF_DIGITS = 5;
+
+/** 最大ダメージ */
+const MAX_DAMAGE = 9999;
+
+/** 最小ダメージ */
+const MIN_DAMAGE = 0;
+
 /** コンストラクタのパラメータ */
 export type PredicatedDamageViewConstructParams = ResourcesContainer;
 
 /** ダメージ予想 ビュー */
 export class PredicatedDamageView {
-  /** 数字 */
-  #number: HorizontalAnimationMesh;
+  /** グループ */
+  #group: THREE.Group;
+  /** 数字メッシュをあつめたもの */
+  #numbers: HorizontalAnimationMesh[];
 
   /**
    * コンストラクタ
@@ -25,14 +37,25 @@ export class PredicatedDamageView {
    */
   constructor(params: PredicatedDamageViewConstructParams) {
     const { resources } = params;
+
+    this.#group = new THREE.Group();
+
     const texture =
       resources.textures.find((t) => t.id === TEXTURE_IDS.HP_NUMBER)?.texture ??
       new THREE.Texture();
-    this.#number = new HorizontalAnimationMesh({
-      texture,
-      maxAnimation: MAX_ANIMATION,
-      width: MESH_SIZE,
-      height: MESH_SIZE,
+
+    this.#numbers = R.times((v) => {
+      const mesh = new HorizontalAnimationMesh({
+        texture,
+        maxAnimation: MAX_ANIMATION,
+        width: MESH_SIZE,
+        height: MESH_SIZE,
+      });
+      mesh.getObject3D().position.x = -v * 32;
+      return mesh;
+    }, NUMBER_OF_DIGITS);
+    this.#numbers.forEach((n) => {
+      this.#group.add(n.getObject3D());
     });
   }
 
@@ -40,7 +63,9 @@ export class PredicatedDamageView {
    * デストラクタ相当の処理
    */
   destructor(): void {
-    this.#number.destructor();
+    this.#numbers.forEach((n) => {
+      n.destructor();
+    });
   }
 
   /**
@@ -49,7 +74,24 @@ export class PredicatedDamageView {
    */
   engage(model: PredicatedDamageModel): void {
     const { damage } = model;
-    this.#number.animate((damage % 10) / MAX_ANIMATION);
+
+    this.#numbers.forEach((n) => {
+      n.opacity(0);
+    });
+    const correctDamage = Math.max(MIN_DAMAGE, Math.min(damage, MAX_DAMAGE));
+    const values = String(correctDamage)
+      .split("")
+      .reverse()
+      .map((v) => Number(v));
+    R.zip(this.#numbers, values)
+      .map((n) => ({
+        mesh: n[0],
+        value: n[1],
+      }))
+      .forEach(({ mesh, value }) => {
+        mesh.animate(value / MAX_ANIMATION);
+        mesh.opacity(1);
+      });
   }
 
   /**
@@ -57,6 +99,6 @@ export class PredicatedDamageView {
    * @return 取得結果
    */
   getObject3D(): THREE.Object3D {
-    return this.#number.getObject3D();
+    return this.#group;
   }
 }
