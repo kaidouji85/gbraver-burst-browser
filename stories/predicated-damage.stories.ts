95a93073f248409b8fdbb24c7a77ac99bbaa91b8
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-29 14:10:17 +0900

    点滅を追加

--- stories/predicated-damage.stories.ts
+++ stories/predicated-damage.stories.ts
@@ -1,3 +1,4 @@
+import { delay } from "../src/js/animation/delay";
 import { PredicatedDamage } from "../src/js/game-object/predicated-damage";
 import { HUDGameObjectStub } from "./stub/hud-game-object-stub";
 
@@ -5,10 +6,24 @@ export default {
   title: "predicated-damage",
 };
 
+/**
+ * 点滅
+ * @param predicatedDamage
+ * @param damage
+ */
+const flash = (predicatedDamage: PredicatedDamage, damage: number) => {
+  predicatedDamage.show(damage)
+    .chain(delay(2000))
+    .chain(predicatedDamage.hidden())
+    .chain(delay(2000))
+    .loop();
+}
+
 /** ダメージ予想の表示 */
 export const predicatedDamage = (): HTMLElement => {
   const stub = new HUDGameObjectStub((params) => {
     const predicatedDamage = new PredicatedDamage(params);
+    flash(predicatedDamage, 2000);
     return [predicatedDamage.getObject3D()];
   });
   stub.start();
