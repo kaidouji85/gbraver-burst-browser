206df92f47049733ac8ac582666c67a7c4e65706
kaidouji85 <kaidouji85@gmail.com>
2024-05-14 05:30:04 +0000

    アームドーザ画像をパラメータで決めるようにした

--- src/js/dom-dialogs/battle-simulator/dom/root-inner-html.ts
+++ src/js/dom-dialogs/battle-simulator/dom/root-inner-html.ts
@@ -1,26 +1,43 @@
+import { ArmdozerIds, PlayerId, PlayerState } from "gbraver-burst-core";
+
+import { getArmdozerStandPathId } from "../../../path/armdozer-stand-path";
 import { ResourcesContainer } from "../../../resource";
 import { PathIds } from "../../../resource/path/ids";
 import { ROOT } from "./class-name";
 import template from "./root-inner-html.hbs";
 
 /** 生成パラメータ */
-type RootInnerHtmlParams = ResourcesContainer;
-
+type RootInnerHtmlParams = ResourcesContainer & {
+  /** ゲームに参加しているプレイヤー */
+  players: [PlayerState, PlayerState];
+  /** 現在画面を開いているプレイヤーID */
+  playerId: PlayerId;
+};
 
 /**
  * ルート要素のinnerHTMLを生成する
  * @returns 生成結果
  */
 export function rootInnerHTML(params: RootInnerHtmlParams) {
-  const { resources } = params;
+  const { resources, players, playerId } = params;
 
+  const player = players.find((p) => p.playerId === playerId);
+  const playerArmdozerId = player?.armdozer.id ?? ArmdozerIds.SHIN_BRAVER;
   const playerArmdozerPath =
-    resources.paths.find((p) => p.id === PathIds.SHIN_BRAVER_STAND)?.path ?? "";
+    resources.paths.find(
+      (p) => p.id === getArmdozerStandPathId(playerArmdozerId),
+    )?.path ?? "";
+
+  const enemy = players.find((p) => p.playerId !== playerId);
+  const enemyArmdozerId = enemy?.armdozer.id ?? ArmdozerIds.SHIN_BRAVER;
   const enemyArmdozerPath =
-    resources.paths.find((p) => p.id === PathIds.NEO_LANDOZER_STAND)?.path ??
-    "";
+    resources.paths.find(
+      (p) => p.id === getArmdozerStandPathId(enemyArmdozerId),
+    )?.path ?? "";
+
   const turnIndicatorPath =
     resources.paths.find((p) => p.id === PathIds.TURN_INDICATOR)?.path ?? "";
+
   const closerPath =
     resources.paths.find((p) => p.id === PathIds.CLOSER)?.path ?? "";
   return template({
