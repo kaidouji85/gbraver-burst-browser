0860a367bce4d4f8ee7a7fc3b8f605ee3385ade2
kaidouji85 <kaidouji85@gmail.com>
2024-05-17 08:43:40 +0000

    updateDamageを簡略化した

--- src/js/dom-dialogs/battle-simulator/dom/update-damage.ts
+++ src/js/dom-dialogs/battle-simulator/dom/update-damage.ts
@@ -3,23 +3,6 @@ import { BattleResult } from "gbraver-burst-core";
 import { PlayerElements } from "./player-elements";
 
 /**
- * ダメージあり
- * @param damage ダメージHTML要素 
- * @param value ダメージ値
- */
-const hasDamage = (damage: HTMLElement, value: number) => {
-  damage.innerText = `-${value}`;
-};
-
-/**
- * ダメージなし
- * @param damage ダメージHTML要素 
- */
-const noDamage = (damage: HTMLElement) => {
-  damage.innerText = "0";
-};
-
-/**
  * ダメージを更新する
  * @param elements プレイヤー関連のHTML要素
  * @param result 戦闘結果
@@ -29,13 +12,10 @@ export const updateDamage = (
   result: BattleResult,
 ) => {
   const { damage } = elements;
-  if (
+  damage.innerText =
     result.name === "NormalHit" ||
     result.name === "Guard" ||
     result.name === "CriticalHit"
-  ) {
-    hasDamage(damage, result.damage);
-  } else {
-    noDamage(damage);
-  }
+      ? `-${result.damage}`
+      : "0";
 };
