e48e72acc9dbf6edf0d69b4e68a238c5217388ba
kaidouji85 <kaidouji85@gmail.com>
2024-05-31 02:50:53 +0000

    HUDGameObjectsのアクション通知をシンプルにした

--- src/js/td-scenes/battle/view/hud/game-objects/index.ts
+++ src/js/td-scenes/battle/view/hud/game-objects/index.ts
@@ -1,12 +1,12 @@
-import { Observable, Subject } from "rxjs";
+import { Observable } from "rxjs";
 import * as THREE from "three";
 
 import { BattleSceneAction } from "../../../actions";
 import { HUDLayerObjectCreatorParams } from "../creator-params";
-import { bindEventListener } from "./procedure/bind-event-listener";
 import { createHUDGameObjectsProps } from "./procedure/create-hud-game-objects-props";
 import { destructor } from "./procedure/destructor";
 import { getObject3Ds } from "./procedure/get-object-3ds";
+import { notifyBattleAction } from "./procedure/notify-battle-action";
 import { HUDGameObjectsProps } from "./props";
 
 /** HUDレイヤーのゲームオブジェクト */
@@ -38,17 +38,10 @@ export function createHUDGameObjects(
   params: HUDLayerObjectCreatorParams,
 ): HUDGameObjects {
   const props = createHUDGameObjectsProps(params);
-  const battleAction = new Subject<BattleSceneAction>();
-  const unsubscrribers = bindEventListener(props, battleAction);
   return {
     ...props,
-    destructor: () => {
-      destructor(props);
-      unsubscrribers.forEach((u) => {
-        u.unsubscribe();
-      });
-    },
+    destructor: () => destructor(props),
     getObject3Ds: () => getObject3Ds(props),
-    notifyBattleAction: () => battleAction,
+    notifyBattleAction: () => notifyBattleAction(props),
   };
 }
