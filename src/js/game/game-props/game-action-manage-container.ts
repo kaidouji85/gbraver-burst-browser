eb2dc1f69fad1db6b9e0a82a9b547dbc8b14acb4
Y.Takeuchi <kaidouji85@gmail.com>
2024-06-04 09:19:26 +0900

    戦闘シーンのアクションコネクタを改善

--- src/js/game/game-props/game-action-manage-container.ts
+++ src/js/game/game-props/game-action-manage-container.ts
@@ -7,5 +7,5 @@ export type GameActionManageContainer = {
    * ゲームアクション管理オブジェクト
    * 動的生成されるシーン、ダイアログの通知を購読するために利用する
    */
-  gameAction: ActionManager<GameAction>;
+  readonly gameAction: ActionManager<GameAction>;
 };
