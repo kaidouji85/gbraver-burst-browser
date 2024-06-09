2a181475133e56b42402e9f796a985c95de2024a
Y.Takeuchi <kaidouji85@gmail.com>
2024-06-09 16:52:03 +0900

    フルバッテリーアタックのセリフを追加

---
+++ src/js/custom-battle-events/queen-of-tragedy/animation/yuuya-full-battery-attack-on-turn2.ts
@@ -0,0 +1,20 @@
+import { onStart } from "../../../animation/on-start";
+import { wbr } from "../../../dom/wbr";
+import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
+import { playerPilotOnlyShout } from "../../pilot-shout";
+
+/**
+ * ユウヤ フルバッテリー攻撃 2ターン目
+ * @param props イベントプロパティ
+ * @returns アニメーション
+ */
+export const yuuyaFullBatteryAttackOnTurn2 = (
+  props: Readonly<CustomBattleEventProps>,
+) =>
+  onStart(() => {
+    playerPilotOnlyShout(
+      props,
+      "Yuuya",
+      `ツバサ これで${wbr}終わりだ`,
+    );
+  });
