bd47368ac3475998fb133e99a6c6cc0a2952a08a
Y.Takeuchi <kaidouji85@gmail.com>
2024-06-09 18:02:51 +0900

    Revert "キリがいいので一旦保存"
    This reverts commit 90762de0bfc2d0ecbceadb904126bd528bfb1177.

--- src/js/custom-battle-events/queen-of-tragedy/procedure/on-state-animation/tsubasa-burst.ts
+++ src/js/custom-battle-events/queen-of-tragedy/procedure/on-state-animation/tsubasa-burst.ts
@@ -3,24 +3,10 @@ import { ConditionalAnimation } from "../../../get-animation-if-conditional-met"
 import { isEnemyBurstActivated } from "../../../is-burst-activated";
 import { tsubasaBurstShout } from "../../animation/tsubasa-burst-shout";
 import { QueenOfTragedyProps } from "../../props";
-import { Animate } from "../../../../animation/animate";
-import { tsubasaBurstShoutOnTraumaOfLastYear } from "../../animation/tsubasa-burst-shout-on-trauma-of-last-year";
 
 /** ツバサ バースト */
 export const tsubasaBurst: ConditionalAnimation<
   CustomStateAnimation & QueenOfTragedyProps
 >[] = [
-  (props) => {
-    let result: Animate | null = null;
-
-    const { chapter } = props.state;
-    const isTsubasaBurstActivated = isEnemyBurstActivated(props);
-    if (chapter.type === "TraumaOfLastYear" && isTsubasaBurstActivated) {
-      result = tsubasaBurstShoutOnTraumaOfLastYear(props);
-    } else if (isEnemyBurstActivated(props)) {
-      result = tsubasaBurstShout(props);
-    }
-
-    return result;
-  },
+  (props) => (isEnemyBurstActivated(props) ? tsubasaBurstShout(props) : null),
 ];
