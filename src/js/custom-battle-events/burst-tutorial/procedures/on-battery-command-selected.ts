d39b6290ad3e7cbd82e10629998467f5ea53ca12
Y.Takeuchi <kaidouji85@gmail.com>
2024-06-12 00:22:41 +0900

    BurstTutorialも対応

--- src/js/custom-battle-events/burst-tutorial/procedures/on-battery-command-selected.ts
+++ src/js/custom-battle-events/burst-tutorial/procedures/on-battery-command-selected.ts
@@ -27,8 +27,8 @@ async function defense5(
 
 /** イベント終了情報 */
 type BatteryEventResult = {
-  /** ステート更新結果 */
-  state: BurstTutorialState;
+  /** イベントステート更新結果 */
+  eventState: BurstTutorialState;
   /** コマンドキャンセル情報 */
   cancel: CommandCanceled;
 };
@@ -42,7 +42,7 @@ export async function onBatteryCommandSelected(
   props: Readonly<BatteryCommandSelected & BurstTutorialProps>,
 ): Promise<BatteryEventResult> {
   let result: BatteryEventResult = {
-    state: props.eventState,
+    eventState: props.eventState,
     cancel: { isCommandCanceled: false },
   };
 
@@ -66,17 +66,17 @@ export async function onBatteryCommandSelected(
     await doBurstToRecoverBattery(props);
     await focusInBurstButton(props, shouldBurst);
     result = {
-      state: { ...result.state, isLoseIfNoDefense5Complete: true },
+      eventState: { ...result.eventState, isLoseIfNoDefense5Complete: true },
       cancel: { isCommandCanceled: true },
     };
   } else if (willPlayerDeath && isPlayerFullBattery) {
     props.view.hud.gameObjects.batterySelector.toBatterySilently(5);
     await defense5(props);
-    result.state.isLoseIfNoDefense5Complete
+    result.eventState.isLoseIfNoDefense5Complete
       ? await notDefense5Carelessly(props)
       : await redoBatterySelect(props);
     result = {
-      state: { ...result.state, isLoseIfNoDefense5Complete: true },
+      eventState: { ...result.eventState, isLoseIfNoDefense5Complete: true },
       cancel: {
         isCommandCanceled: true,
       },
