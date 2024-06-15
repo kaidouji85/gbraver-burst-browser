4625b1d9736b2cdd3799811bc3182d55c9eacd8f
Y.Takeuchi <kaidouji85@gmail.com>
2024-06-15 14:49:02 +0900

    条件判断に必要な追加変数を別データに切り出した

---
+++ src/js/custom-battle-events/queen-of-tragedy/procedure/before-last-state/conditions.ts
@@ -0,0 +1,9 @@
+import {PlayerState} from "gbraver-burst-core";
+
+/** 条件オブジェクト */
+export type Conditions = {
+  /** ターン数 */
+  turn: number;
+  /** 敵のステート */
+  enemy: PlayerState;
+};
--- src/js/custom-battle-events/queen-of-tragedy/procedure/before-last-state/index.ts
+++ src/js/custom-battle-events/queen-of-tragedy/procedure/before-last-state/index.ts
@@ -1,12 +1,11 @@
 import { LastState } from "../../../../td-scenes/battle/custom-battle-event";
 import { invisibleShoutMessageWindowWhenInputCommand } from "../../../invisible-shout-message-window";
-import { separatePlayersFromLastState } from "../../../separate-players";
-import { turnCount } from "../../../turn-count";
 import { QueenOfTragedyProps } from "../../props";
 import { QueenOfTragedyState } from "../../state";
 import { introduction } from "../../stories/introduction";
 import { notRepeatMistake } from "../../stories/not-repeat-mistake";
 import { startOfTurn3 } from "../../stories/start-of-turn3";
+import { createConditions } from "./create-conditions";
 
 /**
  * 最終ステート直前イベント
@@ -17,20 +16,20 @@ export async function beforeLastState(
   props: LastState & QueenOfTragedyProps,
 ): Promise<QueenOfTragedyState> {
   invisibleShoutMessageWindowWhenInputCommand(props);
+
   let result: QueenOfTragedyState = props.eventState;
+  const conditions = createConditions(props);
+  if (!conditions) {
+    return result;
+  }
 
-  const { stateHistory } = props;
-  const separatedPlayers = separatePlayersFromLastState(props);
-  const enemy = separatedPlayers?.enemy;
-  const turn = turnCount(stateHistory);
-
+  const { turn, enemy } = conditions;
   if (turn === 1 && !result.isIntroductionComplete) {
     await introduction(props);
     result = { ...result, isIntroductionComplete: true };
   } else if (
     turn === 3 &&
     !result.isStoryOfTurn3Complete &&
-    enemy &&
     enemy.armdozer.hp <= 100
   ) {
     await notRepeatMistake(props);
