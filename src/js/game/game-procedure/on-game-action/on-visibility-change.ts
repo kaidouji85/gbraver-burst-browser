d4e88dbd026b8648d802db7735e166aa54934904
Y.Takeuchi <kaidouji85@gmail.com>
2024-06-20 23:24:16 +0900

    iPad、iPhoneの場合、ミュート系イベントをスキップ

--- src/js/game/game-procedure/on-game-action/on-visibility-change.ts
+++ src/js/game/game-procedure/on-game-action/on-visibility-change.ts
@@ -1,5 +1,7 @@
 import { Howler } from "howler";
 
+import { isIPad } from "../../../device-ditect/is-ipad";
+import { isIPhone } from "../../../device-ditect/is-iphone";
 import { GameAction } from "../../game-actions";
 import { GameProps } from "../../game-props";
 
@@ -7,6 +9,11 @@ import { GameProps } from "../../game-props";
  * VisibilityChange時の処理
  */
 function onVisibilityChange(): void {
+  // iPhone、iPadの場合はホーム画面から戻った場合に本イベントを検知しないため、何もしない
+  if (isIPhone() || isIPad()) {
+    return;
+  }
+
   const shouldMute = document.visibilityState === "hidden";
   Howler.mute(shouldMute);
 }
