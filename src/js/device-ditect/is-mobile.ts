3e35c1f6ced3f6d8614449fb1beabc75421da68e
Y.Takeuchi <kaidouji85@gmail.com>
2024-06-20 23:16:21 +0900

    ユーザーエージェントiPadは廃止されたので削除

--- src/js/device-ditect/is-mobile.ts
+++ src/js/device-ditect/is-mobile.ts
@@ -1,10 +1,9 @@
 /**
  * ユーザエージェントからデバイスがモバイルか否かを判定する
- *
  * @returns 判定結果、trueでモバイルである
  */
 export function isMobile(): boolean {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+  return /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     navigator.userAgent,
   );
 }
