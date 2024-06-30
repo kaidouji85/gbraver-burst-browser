ea3a759206ba37e6c180f1907af25def1c9832ed
Y.Takeuchi <kaidouji85@gmail.com>
2024-07-01 07:21:10 +0900

    雑に新SDKに対応した

--- src/js/game/game-procedure/initialize.ts
+++ src/js/game/game-procedure/initialize.ts
@@ -1,4 +1,3 @@
-import { MailVerifiedIncomplete } from "../../dom-scenes/mail-verified-incomplete";
 import { invisibleFirstView } from "../../first-view/first-view-visible";
 import { titleResourceLoading } from "../../resource/loading/title-resource-loading";
 import { loadServiceWorker } from "../../service-worker/load-service-worker";
@@ -8,7 +7,6 @@ import { playTitleBGM } from "./play-title-bgm";
 import { reflectPerformanceStatsVisibility } from "./reflect-performance-stats-visibility";
 import { reflectSoundVolume } from "./reflect-sound-volume";
 import { startTitle } from "./start-title";
-import { switchMailVerifiedIncomplete } from "./switch-scene/switch-mail-verified-incomplete";
 
 /**
  * ゲームの初期化
@@ -21,19 +19,6 @@ export async function initialize(props: GameProps): Promise<void> {
   const startTime = Date.now();
   if (props.isServiceWorkerUsed) {
     props.serviceWorker = await loadServiceWorker();
-  }
-
-  const [isLogin, isMailVerified] = await Promise.all([
-    props.api.isLogin(),
-    props.api.isMailVerified(),
-  ]);
-  if (isLogin && !isMailVerified) {
-    const mailAddress = await props.api.getMail();
-    const scene = new MailVerifiedIncomplete(mailAddress);
-    switchMailVerifiedIncomplete(props, scene);
-    invisibleFirstView();
-    await props.fader.fadeIn();
-    return;
   }
 
   const resourceLoading = titleResourceLoading(props.resourceRoot);
