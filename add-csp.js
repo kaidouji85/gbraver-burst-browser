e5c2fc9dc3d347cb664ed8ceaaa11f252bbe4aab
Y.Takeuchi <kaidouji85@gmail.com>
2024-07-07 18:03:54 +0900

    コードフォーマット

--- add-csp.js
+++ add-csp.js
@@ -1,4 +1,4 @@
-const cheerio = require('cheerio');
+const cheerio = require("cheerio");
 const path = require("path");
 const fs = require("fs");
 
@@ -8,9 +8,9 @@ const INDEX_HTML_PATH = path.resolve(__dirname, "build/production/index.html");
 console.log("start adding csp");
 
 const originIndexHTML = fs.readFileSync(INDEX_HTML_PATH, "utf-8");
-const $ = cheerio.load = cheerio.load(originIndexHTML);
+const $ = (cheerio.load = cheerio.load(originIndexHTML));
 $("head").prepend(
-  `<meta http-equiv="Content-Security-Policy" content="script-src 'self'">`
+  `<meta http-equiv="Content-Security-Policy" content="script-src 'self'">`,
 );
 const modifiedIndexHTML = $.html();
 fs.writeFileSync(INDEX_HTML_PATH, modifiedIndexHTML, "utf-8");
