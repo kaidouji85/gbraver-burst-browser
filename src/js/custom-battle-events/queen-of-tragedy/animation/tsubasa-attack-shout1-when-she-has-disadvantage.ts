0ecfd0a137fe1c9902718877184603c64c6ef7fc
Y.Takeuchi <kaidouji85@gmail.com>
2024-07-15 13:40:00 +0900

    セリフを清書

--- src/js/custom-battle-events/queen-of-tragedy/animation/tsubasa-attack-shout1-when-she-has-disadvantage.ts
+++ src/js/custom-battle-events/queen-of-tragedy/animation/tsubasa-attack-shout1-when-she-has-disadvantage.ts
@@ -1,4 +1,5 @@
 import { onStart } from "../../../animation/on-start";
+import { wbr } from "../../../dom/wbr";
 import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
 import { enemyPilotOnlyShout } from "../../pilot-shout";
 
@@ -11,5 +12,9 @@ export const tsubasaAttackShout1WhenSheHasDisadvantage = (
   props: Readonly<CustomBattleEventProps>,
 ) =>
   onStart(() => {
-    enemyPilotOnlyShout(props, "Tsubasa", `まだリードを保つとは `);
+    enemyPilotOnlyShout(
+      props,
+      "Tsubasa",
+      `まだ${wbr}リードを${wbr}保つ${wbr}とは`,
+    );
   });
