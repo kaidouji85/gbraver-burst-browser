4bc85fce02215cb7581f7d67a23d3a1375adf1d4
Y.Takeuchi <kaidouji85@gmail.com>
2024-07-16 08:43:43 +0900

    既存のセリフバグを修正

--- src/js/custom-battle-events/queen-of-tragedy/procedure/on-state-animation/tsubasa-attack-when-continuous-attack.ts
+++ src/js/custom-battle-events/queen-of-tragedy/procedure/on-state-animation/tsubasa-attack-when-continuous-attack.ts
@@ -1,4 +1,4 @@
-import { Animate } from "../../../../animation/animate";
+import { empty } from "../../../../animation/delay";
 import { CustomStateAnimation } from "../../../../td-scenes/battle/custom-battle-event";
 import { playerBattleCount } from "../../../battle-count";
 import { ConditionalAnimation } from "../../../get-animation-if-conditional-met";
@@ -9,8 +9,6 @@ import { QueenOfTragedyProps } from "../../props";
 export const tsubasaAttackWhenContinuousAttack: ConditionalAnimation<
   CustomStateAnimation & QueenOfTragedyProps
 > = (props) => {
-  let result: Animate | null = null;
-
   const { enemyId, stateHistory } = props;
   const { effect } = props.currentState;
   const battleCount = playerBattleCount(stateHistory, enemyId);
@@ -20,8 +18,14 @@ export const tsubasaAttackWhenContinuousAttack: ConditionalAnimation<
     effect.name === "BatteryDeclaration" &&
     effect.attacker === enemyId
   ) {
-    result = tsubasaAttackShoutWhenContinuousAttack(props);
+    return tsubasaAttackShoutWhenContinuousAttack(props);
+  } else if (
+    battleCount === 3 &&
+    effect.name === "Battle" &&
+    effect.attacker === enemyId
+  ) {
+    return empty();
   }
 
-  return result;
+  return null;
 };
