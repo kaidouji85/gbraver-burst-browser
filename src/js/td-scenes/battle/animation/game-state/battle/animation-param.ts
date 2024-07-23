6cadc4cb9a06f36e41abe625f2a94125c85ef586
Y.Takeuchi <kaidouji85@gmail.com>
2024-07-23 14:47:12 +0900

    利用しないプロパティを削除

--- src/js/td-scenes/battle/animation/game-state/battle/animation-param.ts
+++ src/js/td-scenes/battle/animation/game-state/battle/animation-param.ts
@@ -5,7 +5,6 @@ import {
   PlayerState,
 } from "gbraver-burst-core";
 
-import { BGMManager } from "../../../../../bgm/bgm-manager";
 import { ArmdozerSprite } from "../../../../../game-object/armdozer/armdozer-sprite";
 import { PlainHUDCamera } from "../../../../../game-object/camera/plain-hud/plain-hud-camera";
 import { TDCamera } from "../../../../../game-object/camera/td";
@@ -51,9 +50,6 @@ export type BattleAnimationParamX<
   hudObjects: HUDGameObjects;
   /** HUDカメラ */
   hudCamera: PlainHUDCamera;
-
-  /** BGM管理オブジェクト */
-  bgm: BGMManager;
 
   /** 死亡フラグ */
   isDeath: boolean;
@@ -134,7 +130,6 @@ function extractOthers(
     hudCamera: hud.camera,
     isDeath: battle.isDeath,
     result: battle.result,
-    bgm: props.bgm,
   };
 }
 
