727edafe1b3721dd344aa4877c905b73b199a91f
Y.Takeuchi <kaidouji85@gmail.com>
2024-07-26 08:39:37 +0900

    autoprefixer復活

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 const postcssImport = require("postcss-import");
 const postcssPresetEnv = require("postcss-preset-env");
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
-  plugins: [postcssImport(), postcssPresetEnv()],
+  plugins: [postcssImport(), postcssPresetEnv(), autoprefixer],
 };
