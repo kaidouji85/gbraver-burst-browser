fff56a59edd06b57f5800734622b02ffcf9486e6
Y.Takeuchi <kaidouji85@gmail.com>
2024-07-27 21:28:16 +0900

    tween.jsのインポートを修正

--- test/js/animation/scale-tween-duration.test.ts
+++ test/js/animation/scale-tween-duration.test.ts
@@ -1,9 +1,9 @@
-import * as TWEEN from "@tweenjs/tween.js";
+import { Tween } from "@tweenjs/tween.js";
 
 import { scaleTweenDuration } from "../../../src/js/animation/duration";
 
 test("Tween Durationがスケールされている", () => {
-  const t1 = new TWEEN.Tween({}).to({}, 400);
+  const t1 = new Tween({}).to({}, 400);
   scaleTweenDuration(t1, 0.5);
   /* eslint-disable @typescript-eslint/no-explicit-any */
   expect((t1 as any)._duration).toBe(200);
@@ -11,8 +11,8 @@ test("Tween Durationがスケールされている", () => {
 });
 
 test("チェインしたTween Durationがスケールされている", () => {
-  const t1 = new TWEEN.Tween({}).to({}, 500);
-  const t2 = new TWEEN.Tween({}).to({}, 300);
+  const t1 = new Tween({}).to({}, 500);
+  const t2 = new Tween({}).to({}, 300);
   t1.chain(t2);
   scaleTweenDuration(t1, 0.5);
   /* eslint-disable @typescript-eslint/no-explicit-any */
@@ -22,8 +22,8 @@ test("チェインしたTween Durationがスケールされている", () => {
 });
 
 test("ループしたTweenでもDurationがスケールされている", () => {
-  const t1 = new TWEEN.Tween({}).to({}, 600);
-  const t2 = new TWEEN.Tween({}).to({}, 400);
+  const t1 = new Tween({}).to({}, 600);
+  const t2 = new Tween({}).to({}, 400);
   t1.chain(t2);
   t2.chain(t1);
   scaleTweenDuration(t1, 0.5);
@@ -34,11 +34,11 @@ test("ループしたTweenでもDurationがスケールされている", () => {
 });
 
 test("複雑にチェインしたTweenでもDurationがスケールされている", () => {
-  const t1 = new TWEEN.Tween({}).to({}, 100);
-  const t2 = new TWEEN.Tween({}).to({}, 200);
-  const t3 = new TWEEN.Tween({}).to({}, 300);
-  const t4 = new TWEEN.Tween({}).to({}, 400);
-  const t5 = new TWEEN.Tween({}).to({}, 500);
+  const t1 = new Tween({}).to({}, 100);
+  const t2 = new Tween({}).to({}, 200);
+  const t3 = new Tween({}).to({}, 300);
+  const t4 = new Tween({}).to({}, 400);
+  const t5 = new Tween({}).to({}, 500);
   t1.chain(t2.chain(t3.chain(t5)), t4);
   scaleTweenDuration(t1, 0.5);
   /* eslint-disable @typescript-eslint/no-explicit-any */
