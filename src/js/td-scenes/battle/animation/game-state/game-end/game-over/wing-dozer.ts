93665eedb96559d9b03cb86ed5ee1d07e18667be
Y.Takeuchi <kaidouji85@gmail.com>
2024-07-28 15:01:28 +0900

    ウィングドーザー勝利のカメラを改良

--- src/js/td-scenes/battle/animation/game-state/game-end/game-over/wing-dozer.ts
+++ src/js/td-scenes/battle/animation/game-state/game-end/game-over/wing-dozer.ts
@@ -1,23 +1,32 @@
 import { all } from "../../../../../../animation/all";
 import { Animate } from "../../../../../../animation/animate";
 import { WingDozerTD } from "../../../../view/td/armdozer-objects/wing-dozer";
-import { dolly, track } from "../../../td-camera";
 import type { GameOverParamX } from "./game-over-param";
 
 /**
+ * ウィングドーザにフォーカスを合わせる
+ * @param param パラメータ
+ * @returns アニメーション
+ */
+function focusToWingDozer(param: GameOverParamX<WingDozerTD>): Animate {
+  const duration = 800;
+  const x = param.winnerTdArmdozer.wingDozer.getObject3D().position.x;
+  const z = "-60";
+  return all(
+    param.tdCamera.move({ x, z }, duration),
+    param.tdCamera.lookAt({ x, z }, duration),
+  );
+
+}
+
+/**
  * ウィングドーザ 勝利
- *
  * @param param パラメータ
  * @returns アニメーション
  */
 export function wingDozerWin(param: GameOverParamX<WingDozerTD>): Animate {
   return all(
     param.winnerTdArmdozer.wingDozer.dash(),
-    track(
-      param.tdCamera,
-      param.winnerTdArmdozer.wingDozer.getObject3D().position.x,
-      800,
-    ),
-    dolly(param.tdCamera, "-60", 800),
+    focusToWingDozer(param),
   );
 }
