95e563379a62b3d19f7b8bbbc3cb96ac1dae438f
Y.Takeuchi <kaidouji85@gmail.com>
2024-07-30 07:33:37 +0900

    ガードも対応

--- src/js/game-object/armdozer/lightning-dozer/animation/guard.ts
+++ src/js/game-object/armdozer/lightning-dozer/animation/guard.ts
@@ -1,6 +1,5 @@
 import { all } from "../../../../animation/all";
 import { Animate } from "../../../../animation/animate";
-import { onStart } from "../../../../animation/on-start";
 import { tween } from "../../../../animation/tween";
 import { LightningDozerAnimationProps } from "./animation-props";
 
@@ -12,10 +11,11 @@ import { LightningDozerAnimationProps } from "./animation-props";
 export function guard(props: LightningDozerAnimationProps): Animate {
   const { model } = props;
   return all(
-    onStart(() => {
-      model.animation.frame = 1;
-      model.animation.type = "GUARD";
-    }),
+    tween(model.animation, (t) =>
+      t.to({ frame: 1 }, 0).onStart(() => {
+        model.animation.type = "GUARD";
+      }),
+    ),
     tween(model.position, (t) =>
       t.to(
         {
