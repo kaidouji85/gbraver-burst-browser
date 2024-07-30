0a80e3108828b76c1eeec175647e35d2027e8e48
Y.Takeuchi <kaidouji85@gmail.com>
2024-07-31 06:45:08 +0900

    knockBackも対応

--- src/js/game-object/armdozer/shin-braver/animation/knock-back.ts
+++ src/js/game-object/armdozer/shin-braver/animation/knock-back.ts
@@ -1,7 +1,6 @@
 import { all } from "../../../../animation/all";
 import { Animate } from "../../../../animation/animate";
 import { empty } from "../../../../animation/delay";
-import { onStart } from "../../../../animation/on-start";
 import { tween } from "../../../../animation/tween";
 import { ShinBraverAnimationProps } from "./animation-props";
 
@@ -12,10 +11,11 @@ import { ShinBraverAnimationProps } from "./animation-props";
  */
 export function knockBack(props: ShinBraverAnimationProps): Animate {
   const { model } = props;
-  const motion = onStart(() => {
-    model.animation.frame = 1;
-    model.animation.type = "KNOCK_BACK";
-  });
+  const motion = tween(model.animation, (t) =>
+    t.to({ frame: 1 }, 0).onStart(() => {
+      model.animation.type = "KNOCK_BACK";
+    }),
+  );
   const position = tween(model.position, (t) =>
     t.to(
       {
