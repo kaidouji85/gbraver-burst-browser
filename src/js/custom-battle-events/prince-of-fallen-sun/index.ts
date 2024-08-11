550311b6828886cfa2f4c49a10f2de26b98d3756
Y.Takeuchi <kaidouji85@gmail.com>
2024-08-11 13:35:04 +0900

    beforeLastStateの雛形を追加

--- src/js/custom-battle-events/prince-of-fallen-sun/index.ts
+++ src/js/custom-battle-events/prince-of-fallen-sun/index.ts
@@ -1,19 +1,31 @@
-import { CustomBattleEvent } from "../../td-scenes/battle/custom-battle-event";
+import {
+  CustomBattleEvent,
+  LastState,
+} from "../../td-scenes/battle/custom-battle-event";
 import { EmptyCustomBattleEvent } from "../empty-custom-battle-event";
+import { beforeLastState } from "./procedure/before-last-state/before-last-state";
 import { createPrinceOfFallenSunProps } from "./procedure/create-prince-of-fallen-sun-props";
 import { PrinceOfFallenSunProps } from "./props";
 
 /** 落日の王子 イベント */
 class PrinceOfFallenSun extends EmptyCustomBattleEvent {
   /** イベントプロパティ */
-  #props: PrinceOfFallenSunProps;
+  #eventProps: PrinceOfFallenSunProps;
 
   /**
    * コンストラクタ
    */
   constructor() {
     super();
-    this.#props = createPrinceOfFallenSunProps();
+    this.#eventProps = createPrinceOfFallenSunProps();
+  }
+
+  /** @override */
+  async beforeLastState(props: LastState): Promise<void> {
+    this.#eventProps.eventState = await beforeLastState({
+      ...props,
+      ...this.#eventProps,
+    });
   }
 }
 
