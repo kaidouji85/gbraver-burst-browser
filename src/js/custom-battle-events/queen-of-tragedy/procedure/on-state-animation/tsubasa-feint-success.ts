bea2b1a48572179250bb83a542722c1462712d41
Y.Takeuchi <kaidouji85@gmail.com>
2024-08-14 09:36:59 +0900

    フェイント成功回数に応じてセリフを変えるようにした

--- src/js/custom-battle-events/queen-of-tragedy/procedure/on-state-animation/tsubasa-feint-success.ts
+++ src/js/custom-battle-events/queen-of-tragedy/procedure/on-state-animation/tsubasa-feint-success.ts
@@ -1,31 +1,50 @@
+import { Animate } from "../../../../animation/animate";
 import { empty } from "../../../../animation/delay";
 import { CustomStateAnimation } from "../../../../td-scenes/battle/custom-battle-event";
 import { ConditionalAnimation } from "../../../get-animation-if-conditional-met";
 import { tsubasaFeintSuccessShout } from "../../animation/tsubasa-feint-success-shout";
+import { tsubasaFeintSuccessShoutAfterThird } from "../../animation/tsubasa-feint-success-shout-after-third";
+import { tsubasaFeintSuccessShoutOnSecond } from "../../animation/tsubasa-feint-success-shout-on-second";
 import { QueenOfTragedyProps } from "../../props";
 
 /** ツバサ フェイント 成功 */
 export const tsubasaFeintSuccess: ConditionalAnimation<
   CustomStateAnimation & QueenOfTragedyProps
 > = (props) => {
-  const { enemyId } = props;
+  let result: Animate | null = null;
+  const { enemyId, stateHistory } = props;
   const { effect } = props.currentState;
-
-  if (
+  const feintSuccessCount = stateHistory.filter(
+    (s) =>
+      s.effect.name === "Battle" &&
+      s.effect.attacker === enemyId &&
+      s.effect.result.name === "Feint" &&
+      s.effect.result.isDefenderMoved,
+  ).length;
+  const isBatteryDeclarationOnFeintSuccess =
     effect.name === "BatteryDeclaration" &&
     effect.attacker === enemyId &&
     effect.attackerBattery === 0 &&
-    0 < effect.defenderBattery
-  ) {
-    return tsubasaFeintSuccessShout(props);
-  } else if (
+    0 < effect.defenderBattery;
+  const isBattleOnFeintSuccess =
     effect.name === "Battle" &&
     effect.attacker === enemyId &&
     effect.result.name === "Feint" &&
-    effect.result.isDefenderMoved
-  ) {
-    return empty();
+    effect.result.isDefenderMoved;
+
+  if (isBatteryDeclarationOnFeintSuccess && feintSuccessCount === 1) {
+    result = tsubasaFeintSuccessShout(props);
+  } else if (isBattleOnFeintSuccess && feintSuccessCount === 1) {
+    result = empty();
+  } else if (isBatteryDeclarationOnFeintSuccess && feintSuccessCount === 2) {
+    result = tsubasaFeintSuccessShoutOnSecond(props);
+  } else if (isBattleOnFeintSuccess && feintSuccessCount === 2) {
+    result = empty();
+  } else if (isBatteryDeclarationOnFeintSuccess && 3 <= feintSuccessCount) {
+    result = tsubasaFeintSuccessShoutAfterThird(props);
+  } else if (isBattleOnFeintSuccess && 3 <= feintSuccessCount) {
+    result = empty();
   }
 
-  return null;
+  return result;
 };
