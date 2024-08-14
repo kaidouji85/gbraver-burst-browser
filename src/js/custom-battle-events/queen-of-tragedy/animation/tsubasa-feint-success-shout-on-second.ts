d426d40091a3e895949cafcf497e82c60fd4b50d
Y.Takeuchi <kaidouji85@gmail.com>
2024-08-14 09:42:18 +0900

    セリフ微調整

--- src/js/custom-battle-events/queen-of-tragedy/animation/tsubasa-feint-success-shout-on-second.ts
+++ src/js/custom-battle-events/queen-of-tragedy/animation/tsubasa-feint-success-shout-on-second.ts
@@ -1,4 +1,5 @@
 import { onStart } from "../../../animation/on-start";
+import { wbr } from "../../../dom/wbr";
 import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
 import { enemyPilotOnlyShout } from "../../pilot-shout";
 
@@ -11,5 +12,5 @@ export const tsubasaFeintSuccessShoutOnSecond = (
   props: Readonly<CustomBattleEventProps>,
 ) =>
   onStart(() => {
-    enemyPilotOnlyShout(props, "Tsubasa", `また かかったな`);
+    enemyPilotOnlyShout(props, "Tsubasa", `また かかったな${wbr}ユウヤ`);
   });
