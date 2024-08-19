b89acdf0dd67820408dcaa3ff681b1961edfc9e8
Y.Takeuchi <kaidouji85@gmail.com>
2024-08-19 10:00:29 +0900

    ガイ勝利を組み込んだ

--- src/js/custom-battle-events/prince-of-fallen-sun/procedure/after-last-state/index.ts
+++ src/js/custom-battle-events/prince-of-fallen-sun/procedure/after-last-state/index.ts
@@ -1,6 +1,9 @@
 import { LastState } from "../../../../td-scenes/battle/custom-battle-event";
 import { invisibleShoutMessageWindowWhenGameEnd } from "../../../invisible-shout-message-window";
 import { PrinceOfFallenSunProps } from "../../props";
+import { gaiVictory } from "../../stories/gai-victory";
+import { createConditions } from "./create-conditions";
+import { shouldGaiVictory } from "./should-gai-victory";
 
 /**
  * 最終ステート完了後イベント
@@ -10,4 +13,9 @@ export async function afterLastState(
   props: LastState & PrinceOfFallenSunProps,
 ) {
   invisibleShoutMessageWindowWhenGameEnd(props);
+
+  const conditions = createConditions(props);
+  if (shouldGaiVictory(conditions)) {
+    await gaiVictory(props);
+  }
 }
