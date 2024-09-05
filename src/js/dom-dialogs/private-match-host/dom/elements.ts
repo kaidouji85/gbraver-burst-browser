fef0a1b05f3b28fb6114571c08064610dc31078f
Y.Takeuchi <kaidouji85@gmail.com>
2024-09-05 09:26:23 +0900

    下準備

--- src/js/dom-dialogs/private-match-host/dom/elements.ts
+++ src/js/dom-dialogs/private-match-host/dom/elements.ts
@@ -5,3 +5,17 @@
  */
 export const extractCloser = (root: HTMLElement): HTMLElement =>
   root.querySelector(`[data-id="closer"]`) ?? document.createElement("div");
+
+/**
+ * QRコード表示キャンバスを抽出する
+ * @param root ルート要素
+ * @returns 抽出結果
+ */
+export const extractQrCode = (root: HTMLElement): HTMLCanvasElement => {
+  const extractedQrCode = root.querySelector(
+    `[data-id="qr-code"]`,
+  ) as HTMLCanvasElement;
+  return extractedQrCode instanceof HTMLCanvasElement
+    ? extractedQrCode
+    : document.createElement("canvas");
+};
