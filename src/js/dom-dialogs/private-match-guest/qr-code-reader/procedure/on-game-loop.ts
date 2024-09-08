99832a37201fc00eedf72caff1d17c64b960676b
Y.Takeuchi <kaidouji85@gmail.com>
2024-09-08 13:59:16 +0900

    読み込んだQRコードに縁取りするようにした

--- src/js/dom-dialogs/private-match-guest/qr-code-reader/procedure/on-game-loop.ts
+++ src/js/dom-dialogs/private-match-guest/qr-code-reader/procedure/on-game-loop.ts
@@ -3,6 +3,29 @@ import jsQR from "jsqr";
 import { extractRoomIDFromPrivateMatchQRCodeText } from "../../../../qr-code/private-match-qr-code";
 import { PrivateMatchQRCodeReaderProps } from "../props";
 
+/** jsQRが使用するポイント */
+type jsQRPoint = {
+  /** X座標 */
+  x: number,
+  /** Y座標 */
+  y: number,
+};
+
+/**
+ * キャンバスに線を描画する
+ * @param canvas キャンバス
+ * @param begin 始点
+ * @param end 終点
+ */
+function drawLine(canvas: CanvasRenderingContext2D, begin: jsQRPoint, end: jsQRPoint) {
+  canvas.beginPath();
+  canvas.moveTo(begin.x, begin.y);
+  canvas.lineTo(end.x, end.y);
+  canvas.lineWidth = 4;
+  canvas.strokeStyle = "#FF3B58";
+  canvas.stroke();
+}
+
 /**
  * ゲームループ内での処理を行う
  * @param props プロパティ
@@ -24,6 +47,10 @@ export function onGameLoop(props: PrivateMatchQRCodeReaderProps) {
     return;
   }
 
+  drawLine(canvas, code.location.topLeftCorner, code.location.topRightCorner);
+  drawLine(canvas, code.location.topRightCorner, code.location.bottomRightCorner);
+  drawLine(canvas, code.location.bottomRightCorner, code.location.bottomLeftCorner);
+  drawLine(canvas, code.location.bottomLeftCorner, code.location.topLeftCorner);
   const roomID = extractRoomIDFromPrivateMatchQRCodeText(code.data);
   if (roomID === null) {
     return;
