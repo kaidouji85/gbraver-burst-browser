35cbbf8345111a0058e554342e3c5b30a4059507
Y.Takeuchi <kaidouji85@gmail.com>
2024-09-08 12:24:39 +0900

    とりあえず動くようになった

--- stories/private-match-qr-code-reader.stories.ts
+++ stories/private-match-qr-code-reader.stories.ts
@@ -3,6 +3,10 @@ import { StoryFn } from "@storybook/html";
 import { PrivateMatchQRCodeReader } from "../src/js/dom-dialogs/private-match-guest/qr-code-reader";
 import { domStub } from "./stub/dom-stub";
 
+export default {
+  title: "private-match-qr-code-reader",
+};
+
 /** QRコードリーダー */
 export const qrCodeReadr: StoryFn = domStub(() => {
   const reader = new PrivateMatchQRCodeReader();
