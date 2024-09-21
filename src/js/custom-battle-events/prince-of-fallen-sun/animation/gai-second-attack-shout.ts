25cd1de05ee7d84443c21e082432fa0d9113065d
Y.Takeuchi <kaidouji85@gmail.com>
2024-09-21 16:49:16 +0900

    ガイのセリフを追加

---
+++ src/js/custom-battle-events/prince-of-fallen-sun/animation/gai-second-attack-shout.ts
@@ -0,0 +1,16 @@
+import { onStart } from "../../../animation/on-start";
+import { wbr } from "../../../dom/wbr";
+import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
+import { enemyPilotOnlyShout } from "../../pilot-shout";
+
+/**
+ * ガイ セカンドアタック 叫び
+ * @param props イベントプロパティ
+ * @returns アニメーション
+ */
+export const gaiSecondAttackShout = (
+  props: Readonly<CustomBattleEventProps>,
+) =>
+  onStart(() => {
+    enemyPilotOnlyShout(props, "Gai", `なんて奴だ 一分の${wbr}隙もない`);
+  });
--- src/js/custom-battle-events/prince-of-fallen-sun/procedure/on-state-animation/index.ts
+++ src/js/custom-battle-events/prince-of-fallen-sun/procedure/on-state-animation/index.ts
@@ -9,6 +9,7 @@ import { gaiFeintSuccess } from "./gai-feint-success";
 import { gaiFinish } from "./gai-finish";
 import { gaiFirstAttack } from "./gai-first-attack";
 import { gaiPilotSkill } from "./gai-pilot-skill";
+import { gaiSecondAttack } from "./gai-second-attack";
 import { yuuyaBurst } from "./yuuya-burst";
 import { yuuyaBurstWhenGaiActivatedSkill } from "./yuuya-burst-when-gai-activated-skill";
 import { yuuyaBurstWhenOneTurn } from "./yuuya-burst-when-one-turn";
@@ -38,6 +39,7 @@ export function onStateAnimation(
     gaiFinish,
     gaiFeintSuccess,
     gaiFirstAttack,
+    gaiSecondAttack,
     invisibleShoutMessageWindowWhenTurnChange,
   ];
   return getAnimationIfConditionMet(props, conditionalAnimations) ?? empty();
