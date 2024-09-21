5737dfdfbab0cae5690536f118193deb47912b12
Y.Takeuchi <kaidouji85@gmail.com>
2024-09-21 16:01:15 +0900

    セリフを調整

--- src/js/custom-battle-events/prince-of-fallen-sun/stories/yuuya-victory.ts
+++ src/js/custom-battle-events/prince-of-fallen-sun/stories/yuuya-victory.ts
@@ -1,8 +1,11 @@
 import { wbr } from "../../../dom/wbr";
 import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
-import { activeLeftMessageWindowWithFace } from "../../active-message-window";
+import {
+  activeLeftMessageWindowWithFace,
+  activeRightMessageWindowWithFace,
+} from "../../active-message-window";
 import { invisibleAllMessageWindows } from "../../invisible-all-message-windows";
-import { scrollLeftMessages } from "../../scroll-messages";
+import { scrollLeftMessages, scrollRightMessages } from "../../scroll-messages";
 
 /**
  * ユウヤの勝利
@@ -12,11 +15,18 @@ export async function yuuyaVictory(props: CustomBattleEventProps) {
   activeLeftMessageWindowWithFace(props, "Gai");
   await scrollLeftMessages(props, [
     ["ガイ", `「何故だ`],
-    [
-      `ネオランドーザだって Gブレイバーには${wbr}引けを${wbr}取らない${wbr}はずなのに」`,
-    ],
+    [`うちじゃ${wbr}Gブレイバーに${wbr}勝てない${wbr}のか」`],
   ]);
   props.view.dom.leftMessageWindow.darken();
+
+  activeRightMessageWindowWithFace(props, "Yuuya");
+  await scrollRightMessages(props, [
+    ["ユウヤ", `「一つだけ${wbr}教えてやろう`],
+    [`ロボの${wbr}スペックは${wbr}もちろん${wbr}大切だが`],
+    [
+      `最終的には${wbr}パイロットの${wbr}読み合いが${wbr}勝敗を${wbr}分けるん${wbr}だぜ」`,
+    ],
+  ]);
 
   invisibleAllMessageWindows(props);
 }
