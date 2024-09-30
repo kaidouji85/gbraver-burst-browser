b51168a85d21e48e8c90061d431de03dd0f0517e
Y.Takeuchi <kaidouji85@gmail.com>
2024-09-30 14:12:44 +0900

    導入を清書した

--- src/js/game/episodes/prince-of-fallen-sun.ts
+++ src/js/game/episodes/prince-of-fallen-sun.ts
@@ -9,7 +9,7 @@ import { Episode } from "./episode";
 import { EpisodeIDs } from "./episode-ids";
 
 /** 導入 */
-const introduction = `あとで考える`;
+const introduction = `世界初の競技用巨大ロボを製造した浅草重工。巨大ロボの創業家ともいえる同社だが、近年はGブレイバーを開発した洛南建機にシェアを奪われ、業界2位に甘んじていた。宿敵ともいえる両社の戦いが、今、静かに幕を開けようとしている。`;
 
 /** 落日の王子 */
 export const PrinceOfFallenSun: Episode = {
