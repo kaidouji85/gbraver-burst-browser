2a2ac918c369ce923c78bd760f9ba089acbdd9a1
Y.Takeuchi <kaidouji85@gmail.com>
2024-10-07 10:23:30 +0900

    不要な処理を削除した

--- src/js/game/game-procedure/load-full-resource.ts
+++ src/js/game/game-procedure/load-full-resource.ts
@@ -4,12 +4,10 @@ import {
   fullResourceDifferentialLoad,
 } from "../../resource/loading/full-resource-differential-load";
 import type { GameProps } from "../game-props";
-import { reflectSoundVolume } from "./reflect-sound-volume";
 import { switchLoading } from "./switch-scene/switch-loading";
 
 /**
  * フルリソース読み込みを行うヘルパー関数
- * リソース読み込み中は専用画面に遷移し、読み込んだ音リソースには音量設定が反映される
  * 本関数にはpropsを変更する副作用がある
  * @param props ゲームプロパティ
  * @returns 処理完了したら発火するPromise
@@ -23,7 +21,5 @@ export async function loadFullResource(props: GameProps): Promise<void> {
   switchLoading(props, scene);
   await props.fader.fadeIn();
   props.resources = await resourceLoading.resources;
-  const config = await props.config.load();
-  await reflectSoundVolume(props, config);
   props.isFullResourceLoaded = true;
 }
