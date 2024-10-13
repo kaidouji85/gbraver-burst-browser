65d6379a1d256c8cb103eb7bff7a8d879ba9985f
Y.Takeuchi <kaidouji85@gmail.com>
2024-10-13 11:55:01 +0900

    セリフを調整

--- src/js/custom-battle-events/prince-of-fallen-sun/animation/gai-battle-shout-when-miss.ts
+++ src/js/custom-battle-events/prince-of-fallen-sun/animation/gai-battle-shout-when-miss.ts
@@ -1,4 +1,5 @@
 import { onStart } from "../../../animation/on-start";
+import { wbr } from "../../../dom/wbr";
 import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
 import { enemyPilotOnlyShout } from "../../pilot-shout";
 
@@ -11,5 +12,5 @@ export const gaiBattleShoutWhenMiss = (
   props: Readonly<CustomBattleEventProps>,
 ) =>
   onStart(() => {
-    enemyPilotOnlyShout(props, "Gai", `外したか まあいい`);
+    enemyPilotOnlyShout(props, "Gai", `クッ ここは${wbr}牽制攻撃だ`);
   });
