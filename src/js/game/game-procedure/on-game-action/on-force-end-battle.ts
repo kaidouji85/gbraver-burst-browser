2cc218afdc85391150653bb44bab87e7ecf378e2
Y.Takeuchi <kaidouji85@gmail.com>
2024-11-09 18:59:17 +0900

    ネット対戦の強制終了も実装した

--- src/js/game/game-procedure/on-game-action/on-force-end-battle.ts
+++ src/js/game/game-procedure/on-game-action/on-force-end-battle.ts
@@ -5,6 +5,8 @@ import { PlayingEpisode } from "../../in-progress/story";
 import { playTitleBGM } from "../play-title-bgm";
 import { startEpisodeSelector } from "../start-episode-selector";
 import { startTitle } from "../start-title";
+import { WaitingDialog } from "../../../dom-dialogs/waiting/waiting-dialog";
+import { switchWaitingDialog } from "../switch-dialog/switch-waiting-dialog";
 
 /**
  * NPCバトルの強制終了
@@ -30,8 +32,11 @@ async function forceEndNPCBattle(props: GameProps) {
 /**
  * 条件を満たした場合、ストーリーモードバトルを強制終了する
  * @param props ゲームプロパティ
+ * @returns バトルを強制終了した場合はtrue, それ以外はfalse
  */
-async function forceEndEpisodeIfNeeded(props: Readonly<GameProps>) {
+async function forceEndEpisodeIfNeeded(
+  props: Readonly<GameProps>,
+): Promise<boolean> {
   if (
     props.inProgress.type === "Story" &&
     props.inProgress.story.type === "PlayingEpisode"
@@ -56,12 +61,50 @@ async function forceEndEpisodeIfNeeded(props: Readonly<GameProps>) {
 }
 
 /**
+ * 条件を満たした場合、ネットバトルを強制終了する
+ * 本関数にはinProgressを更新する副作用がある
+ * @param props ゲームプロパティ
+ * @returns バトルを強制終了した場合はtrue, それ以外はfalse
+ */
+async function forceEndNetBattleIfNeeded(props: GameProps): Promise<boolean> {
+  if (
+    props.inProgress.type === "CasualMatch" ||
+    props.inProgress.type === "PrivateMatchHost" ||
+    props.inProgress.type === "PrivateMatchGuest"
+  ) {
+    props.inProgress = { type: "None" };
+    const dialog = new WaitingDialog("通信中......");
+    switchWaitingDialog(props, dialog);
+    await props.api.disconnectWebsocket();
+    props.domDialogBinder.hidden();
+
+    await Promise.all([
+      (async () => {
+        await props.fader.fadeOut();
+        await startTitle(props);
+      })(),
+      (async () => {
+        await props.bgm.do(fadeOut);
+        await props.bgm.do(stop);
+      })(),
+    ]);
+    await props.fader.fadeIn();
+    playTitleBGM(props);
+    return true;
+  }
+
+  return false;
+}
+
+/**
  * プレイヤーによるバトル強制終了
  * 本関数にはinProgressを更新する副作用がある
  * @param props ゲームプロパティ
  */
 async function onForceEndBattle(props: GameProps) {
   if (await forceEndEpisodeIfNeeded(props)) {
+    return;
+  } else if (await forceEndNetBattleIfNeeded(props)) {
     return;
   } else {
     await forceEndNPCBattle(props);
