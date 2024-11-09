ca007ccaf7f47d60c138f9febbcf18f518c98ca7
Y.Takeuchi <kaidouji85@gmail.com>
2024-11-09 16:00:43 +0900

    data-id、抽出メソッドを追加

--- src/js/game-dom/battle-hamburger-menu/dom/extract-element.ts
+++ src/js/game-dom/battle-hamburger-menu/dom/extract-element.ts
@@ -30,3 +30,49 @@ export const extractRetry = (root: HTMLElement): HTMLElement =>
  */
 export const extractEndBattle = (root: HTMLElement): HTMLElement =>
   root.querySelector(`[data-id="end-battle"]`) ?? document.createElement("div");
+
+/**
+ * ルート要素からバックグラウンドを抽出する
+ * @param root ルート要素
+ * @returns 抽出結果
+ */
+export const extractBackGround = (root: HTMLElement): HTMLElement =>
+  root.querySelector(`[data-id="background"]`) ?? document.createElement("div");
+
+/**
+ * ルート要素からリトライ確認ダイアログを抽出する
+ * @param root ルート要素
+ * @returns 抽出結果
+ */
+export const extractRetryConfirmDialog = (root: HTMLElement): HTMLElement =>
+  root.querySelector(`[data-id="retry-confirm-dialog"]`) ??
+  document.createElement("div");
+
+/**
+ * ルート要素からリトライ確認ダイアログのクローザーを抽出する
+ * @param root ルート要素
+ * @returns 抽出結果
+ */
+export const extractRetryConfirmDialogCloser = (
+  root: HTMLElement,
+): HTMLElement =>
+  root.querySelector(`[data-id="retry-confirm-dialog-closer"]`) ??
+  document.createElement("div");
+
+/**
+ * ルート要素からリトライボタンを抽出する
+ * @param root ルート要素
+ * @returns 抽出結果
+ */
+export const extractRetryButton = (root: HTMLElement): HTMLElement =>
+  root.querySelector(`[data-id="retry-button"]`) ??
+  document.createElement("div");
+
+/**
+ * ルート要素からリトライキャンセルボタンを抽出する
+ * @param root ルート要素
+ * @returns 抽出結果
+ */
+export const extractRetryCancelButton = (root: HTMLElement): HTMLElement =>
+  root.querySelector(`[data-id="retry-cancel-button"]`) ??
+  document.createElement("div");
