412e42d33deecd30171057baee911cd188ee1331
kaidouji85 <kaidouji85@gmail.com>
2024-12-06 07:41:53 +0000

    ファイルパスの修正

--- upload-module.bash
+++ upload-module.bash
@@ -9,6 +9,6 @@ readonly BUILD_PATH=./build/production
 cd "$(dirname "${0}")"
 find "${BUILD_PATH}" -maxdepth 1 -type f -print0 | while IFS= read -r -d '' filePath; do
   s3FileName=$(basename "${filePath}")
-  aws s3 cp --cache-control no-store "${filePath}" "s3://${S3_BUCKET}/${STAGE}${s3FileName}"
+  aws s3 cp --cache-control no-store "${filePath}" "s3://${S3_BUCKET}/${STAGE}/${s3FileName}"
 done
 aws s3 sync --delete --exact-timestamps "${BUILD_PATH}" "s3://${S3_BUCKET}/${STAGE}"
