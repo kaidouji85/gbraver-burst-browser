6efaeb9967f9e56a06b5e2bc2a340bada0c238a5
kaidouji85 <kaidouji85@gmail.com>
2024-12-06 10:40:21 +0000

    WIP OriginPathの書き直し

--- switch-stage.bash
+++ switch-stage.bash
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 set -Ceu
-readonly USAGE="Usage: $0 <DISTRIBUTION_ID> <STAGE>"
+readonly USAGE="Usage: $0 <DISTRIBUTION_ID> <ORIGIN_ID> <STAGE>"
 readonly DISTRIBUTION_ID="${1?"${USAGE}"}"
-readonly STAGE="${2?"${USAGE}"}"
-readonly ORIGIN_DESTINATION_CONFIG="origin-destination-config.json"
-readonly UPDATE_DISTRIBUTION_CONFIG="update-distribution-config.json"
+readonly ORIGIN_ID="${2?"${USAGE}"}"
+readonly STAGE="${3?"${USAGE}"}"
+readonly ORIGIN="origin.json"
+readonly DISTRIBUTION_CONFIG="distribution-config.json"
+readonly UPDATED_ORIGIN_PATH="updated-origin-path.json"
 
-aws cloudfront get-distribution-config --id "${DISTRIBUTION_ID}" >"${ORIGIN_DESTINATION_CONFIG}"
-jq .DistributionConfig "${ORIGIN_DESTINATION_CONFIG}" >"${UPDATE_DISTRIBUTION_CONFIG}"
+aws cloudfront get-distribution-config --id "${DISTRIBUTION_ID}" >"${ORIGIN}"
+jq .DistributionConfig "${ORIGIN}" >"${DISTRIBUTION_CONFIG}"
+jq "(.Origins.Items[] | select(.Id == \"${ORIGIN_ID}\") | .OriginPath) |= \"/${STAGE}\"" "${UPDATE_DISTRIBUTION_CONFIG}" >"${UPDATED_ORIGIN_PATH}"
