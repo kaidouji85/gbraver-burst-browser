e2e03ac607f6657c45e41fd83dbd9e0e6a0c2e94
Y.Takeuchi <kaidouji85@gmail.com>
2024-12-21 11:51:42 +0900

    敵グランドーザのビュー指定ミスを修正

--- src/js/game-object/armdozer/gran-dozer/index.ts
+++ src/js/game-object/armdozer/gran-dozer/index.ts
@@ -3,6 +3,7 @@ import { Observable } from "rxjs";
 import { ResourcesContainer } from "../../../resource";
 import { GameObjectAction } from "../../action/game-object-action";
 import { GranDozer } from "./gran-dozer";
+import { EnemyGranDozerView } from "./view/enemy-gran-dozer-view";
 import { PlayerGranDozerView } from "./view/player-gran-dozer-view";
 
 /** オプション */
@@ -27,6 +28,6 @@ export function PlayerGranDozer(options: Options): GranDozer {
  * @returns 生成結果
  */
 export function EnemyGranDozer(options: Options): GranDozer {
-  const view = new PlayerGranDozerView(options.resources);
+  const view = new EnemyGranDozerView(options.resources);
   return new GranDozer({ ...options, view });
 }
