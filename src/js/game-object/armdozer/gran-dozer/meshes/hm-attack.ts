e2324cd9ab021066655005a8274684e13cd77849
Y.Takeuchi <kaidouji85@gmail.com>
2024-12-22 13:50:52 +0900

    メッシュ生成処理を追加した

--- src/js/game-object/armdozer/gran-dozer/meshes/hm-attack.ts
+++ src/js/game-object/armdozer/gran-dozer/meshes/hm-attack.ts
@@ -1,18 +1,18 @@
-import type { Resources } from "../../../../resource";
+import { Resources } from "../../../../resource";
 import { TEXTURE_IDS } from "../../../../resource/texture/ids";
 import { AnimationMesh } from "./animation-mesh";
 import { createAnimationMeshes } from "./create-animation-meshes";
 
 /**
- * 立ち メッシュを生成
+ * アームハンマーアタック メッシュを生成
  * @param resources リソース管理オブジェクト
  * @returns 生成結果
  */
-export function stand(resources: Resources): AnimationMesh[] {
+export function hmAttack(resources: Resources): AnimationMesh[] {
   return createAnimationMeshes({
     resources,
-    animationType: "STAND",
-    textureId: TEXTURE_IDS.GRAN_DOZER_STAND,
-    maxAnimation: 1,
+    animationType: "HM_ATTACK",
+    textureId: TEXTURE_IDS.GRAN_DOZER_HM_ATTACK,
+    maxAnimation: 4,
   });
 }
--- src/js/game-object/armdozer/gran-dozer/meshes/index.ts
+++ src/js/game-object/armdozer/gran-dozer/meshes/index.ts
@@ -1,5 +1,8 @@
 import { Resources } from "../../../../resource";
 import { AnimationMesh } from "./animation-mesh";
+import { hmAttack } from "./hm-attack";
+import { hmCharge } from "./hm-charge";
+import { hmToStand } from "./hm-to-stand";
 import { stand } from "./stand";
 
 /**
@@ -8,5 +11,10 @@ import { stand } from "./stand";
  * @returns 生成結果
  */
 export function createAllMeshes(resources: Resources): AnimationMesh[] {
-  return [...stand(resources)];
+  return [
+    ...stand(resources),
+    ...hmCharge(resources),
+    ...hmAttack(resources),
+    ...hmToStand(resources),
+  ];
 }
