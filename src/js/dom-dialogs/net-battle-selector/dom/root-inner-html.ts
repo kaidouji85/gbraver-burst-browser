f7ba74474f0ee13475123e9c368890b7fd2192f3
Y.Takeuchi <kaidouji85@gmail.com>
2025-01-01 20:36:17 +0900

    ダイアログのHTMLをhbs化した

--- src/js/dom-dialogs/net-battle-selector/dom/root-inner-html.ts
+++ src/js/dom-dialogs/net-battle-selector/dom/root-inner-html.ts
@@ -2,6 +2,7 @@ import { Resources } from "../../../resource";
 import { PathIds } from "../../../resource/path/ids";
 import { ROOT_CLASS } from "./class-name";
 import { DataIDs } from "./data-ids";
+import template from "./root-inner-html.hbs";
 
 /**
  * ルートHTML要素のinnerHTMLを生成する
@@ -18,22 +19,12 @@ export function rootInnerHTML(resources: Resources, ids: DataIDs): string {
     "プライベートマッチを開催します、ルームIDをゲストに共有してください";
   const privateMatchGuestDescription =
     "ホストから共有されたルームIDを入力して、対戦を開始します";
-  return `
-    <div class="${ROOT_CLASS}__background" data-id="${ids.backGround}"></div>
-    <div class="${ROOT_CLASS}__dialog">
-      <img class="${ROOT_CLASS}__closer" alt="閉じる" src="${closerPath}" data-id="${ids.closer}">
-      <button class="${ROOT_CLASS}__casual-match" alt="カジュアルマッチをする" data-id="${ids.casualMatchButton}">
-        <div class="${ROOT_CLASS}__casual-match-title">カジュアルマッチ</div>
-        <div class="${ROOT_CLASS}__casual-match-description">${casualMatchDescription}</div>
-      </button>
-      <button class="${ROOT_CLASS}__private-match-host" alt="プライベートマッチ（ホスト）をする" data-id="${ids.privateMatchHostButton}">
-        <div class="${ROOT_CLASS}__private-match-host-title">プライベートマッチ（ホスト）</div>
-        <div class="${ROOT_CLASS}__private-match-host-description">${privateMatchHostDescription}</div>
-      </button>
-      <button class="${ROOT_CLASS}__private-match-guest" alt="プライベートマッチ（ゲスト）をする" data-id="${ids.privateMatchGuestButton}">
-        <div class="${ROOT_CLASS}__private-match-guest-title">プライベートマッチ（ゲスト）</div>
-        <div class="${ROOT_CLASS}__private-match-guest-description">${privateMatchGuestDescription}</div>
-      </button>
-    </div>
-  `;
+  return template({
+    ROOT_CLASS,
+    ids,
+    closerPath,
+    casualMatchDescription,
+    privateMatchHostDescription,
+    privateMatchGuestDescription,
+  });
 }
