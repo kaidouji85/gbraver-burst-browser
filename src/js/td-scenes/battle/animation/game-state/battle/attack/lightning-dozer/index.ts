060753e320c606c893d2ffe8994aef1e015e29c0
Y.Takeuchi <kaidouji85@gmail.com>
2025-01-12 13:30:37 +0900

    ファイル移動

--- src/js/td-scenes/battle/animation/game-state/battle/attack/lightning-dozer/index.ts
+++ src/js/td-scenes/battle/animation/game-state/battle/attack/lightning-dozer/index.ts
@@ -7,13 +7,13 @@ import type {
   NormalHit,
 } from "gbraver-burst-core";
 
-import { all } from "../../../../../../animation/all";
-import { Animate } from "../../../../../../animation/animate";
-import { delay, empty } from "../../../../../../animation/delay";
-import { LightningDozer } from "../../../../../../game-object/armdozer/lightning-dozer/lightning-dozer";
-import { TDCamera } from "../../../../../../game-object/camera/td";
-import { toInitial } from "../../../td-camera";
-import type { BattleAnimationParamX } from "../animation-param";
+import { all } from "../../../../../../../animation/all";
+import { Animate } from "../../../../../../../animation/animate";
+import { delay, empty } from "../../../../../../../animation/delay";
+import { LightningDozer } from "../../../../../../../game-object/armdozer/lightning-dozer/lightning-dozer";
+import { TDCamera } from "../../../../../../../game-object/camera/td";
+import { toInitial } from "../../../../td-camera";
+import type { BattleAnimationParamX } from "../../animation-param";
 
 /**
  * ライトニングドーザ 戦闘アニメーション パラメータ
