5494e9838f919514a281e131f19040c40054a501
Y.Takeuchi <kaidouji85@gmail.com>
2025-01-12 13:16:03 +0900

    ファイル移動

--- src/js/td-scenes/battle/animation/game-state/battle/attack/wing-dozer/index.ts
+++ src/js/td-scenes/battle/animation/game-state/battle/attack/wing-dozer/index.ts
@@ -7,13 +7,13 @@ import type {
   NormalHit,
 } from "gbraver-burst-core";
 
-import { all } from "../../../../../../animation/all";
-import { Animate } from "../../../../../../animation/animate";
-import { delay, empty } from "../../../../../../animation/delay";
-import { WingDozer } from "../../../../../../game-object/armdozer/wing-dozer/wing-dozer";
-import { TDCamera } from "../../../../../../game-object/camera/td";
-import { toInitial } from "../../../td-camera";
-import type { BattleAnimationParamX } from "../animation-param";
+import { all } from "../../../../../../../animation/all";
+import { Animate } from "../../../../../../../animation/animate";
+import { delay, empty } from "../../../../../../../animation/delay";
+import { WingDozer } from "../../../../../../../game-object/armdozer/wing-dozer/wing-dozer";
+import { TDCamera } from "../../../../../../../game-object/camera/td";
+import { toInitial } from "../../../../td-camera";
+import type { BattleAnimationParamX } from "../../animation-param";
 
 /**
  * ウィングドーザ 戦闘アニメーション パラメータ
