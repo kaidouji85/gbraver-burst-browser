c1e8ccc5880bdfe6da954b731f65b6ada131356f
Y.Takeuchi <kaidouji85@gmail.com>
2025-01-12 11:58:14 +0900

    行数を減らした

--- src/js/td-scenes/battle/animation/game-state/battle/attack/shin-braver/index.ts
+++ src/js/td-scenes/battle/animation/game-state/battle/attack/shin-braver/index.ts
@@ -1,11 +1,4 @@
-import {
-  BattleResult,
-  CriticalHit,
-  Feint,
-  Guard,
-  Miss,
-  NormalHit,
-} from "gbraver-burst-core";
+import { BattleResult } from "gbraver-burst-core";
 
 import { Animate } from "../../../../../../../animation/animate";
 import { empty } from "../../../../../../../animation/delay";
@@ -24,45 +17,26 @@ import { ShinBraverBattle } from "./shin-braver-battle";
 export function shinBraverAttack(
   param: ShinBraverBattle<BattleResult>,
 ): Animate {
-  if (param.isDeath && param.result.name === "NormalHit") {
-    const result: NormalHit = param.result;
+  let ret = empty();
+
+  const { result } = param;
+  if (param.isDeath && result.name === "NormalHit") {
+    ret = down({ ...param, result });
+  } else if (result.name === "NormalHit") {
+    ret = attack({ ...param, result });
+  } else if (param.isDeath && result.name === "CriticalHit") {
+    ret = down({ ...param, result });
+  } else if (result.name === "CriticalHit") {
+    ret = attack({ ...param, result });
+  } else if (param.isDeath && result.name === "Guard") {
     return down({ ...param, result });
-  }
-
-  if (param.result.name === "NormalHit") {
-    const result: NormalHit = param.result;
-    return attack({ ...param, result });
-  }
-
-  if (param.isDeath && param.result.name === "CriticalHit") {
-    const result: CriticalHit = param.result;
-    return down({ ...param, result });
-  }
-
-  if (param.result.name === "CriticalHit") {
-    const result: CriticalHit = param.result;
-    return attack({ ...param, result });
-  }
-
-  if (param.isDeath && param.result.name === "Guard") {
-    const result: Guard = param.result;
-    return down({ ...param, result });
-  }
-
-  if (param.result.name === "Guard") {
-    const result: Guard = param.result;
+  } else if (result.name === "Guard") {
     return guard({ ...param, result });
+  } else if (result.name === "Miss") {
+    ret = miss({ ...param, result });
+  } else if (result.name === "Feint") {
+    ret = feint({ ...param, result });
   }
 
-  if (param.result.name === "Miss") {
-    const result: Miss = param.result;
-    return miss({ ...param, result });
-  }
-
-  if (param.result.name === "Feint") {
-    const result: Feint = param.result;
-    return feint({ ...param, result });
-  }
-
-  return empty();
+  return ret;
 }
