3cd3acaa22d8d236bdb8b2220a4ea13639a82e89
Y.Takeuchi <kaidouji85@gmail.com>
2025-01-12 14:12:07 +0900

    攻撃ヒットアニメを実装した

--- src/js/td-scenes/battle/animation/game-state/battle/attack/gran-dozer/index.ts
+++ src/js/td-scenes/battle/animation/game-state/battle/attack/gran-dozer/index.ts
@@ -2,6 +2,7 @@ import { BattleResult } from "gbraver-burst-core";
 
 import { Animate } from "../../../../../../../animation/animate";
 import { empty } from "../../../../../../../animation/delay";
+import { attack } from "./attack";
 import { GranDozerBattle } from "./gran-dozer-battle";
 
 /**
@@ -9,11 +10,12 @@ import { GranDozerBattle } from "./gran-dozer-battle";
  * @param param パラメータ
  * @returns アニメーション
  */
-export function granDozerAttack(
-  param: GranDozerBattle<BattleResult>,
-): Animate {
+export function granDozerAttack(param: GranDozerBattle<BattleResult>): Animate {
   const { result } = param;
   switch (result.name) {
+    case "NormalHit":
+    case "CriticalHit":
+      return attack({ ...param, result });
     default:
       return empty();
   }
