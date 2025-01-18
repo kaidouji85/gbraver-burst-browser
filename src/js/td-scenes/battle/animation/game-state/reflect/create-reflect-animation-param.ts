fdfc67cbba47eee9379fa7c88cbd90ee772df6ab
Y.Takeuchi <kaidouji85@gmail.com>
2025-01-18 15:01:31 +0900

    アニメーションパラメータ生成を別ファイルに切り出した

---
+++ src/js/td-scenes/battle/animation/game-state/reflect/create-reflect-animation-param.ts
@@ -0,0 +1,53 @@
+import { GameStateX, Reflect } from "gbraver-burst-core";
+
+import { StateAnimationProps } from "../state-animation-props";
+import { ReflectAnimationParam } from "./animation-param";
+
+/**
+ * ダメージ反射のアニメーションパラメータを生成する
+ * @param props ステートアニメーションプロパティ
+ * @param gameState ゲームステート
+ * @returns 生成結果、生成できない場合はnullを返す
+ */
+export function createReflectAnimationParam(
+  props: StateAnimationProps,
+  gameState: GameStateX<Reflect>,
+): ReflectAnimationParam | null {
+  const { view } = props;
+  const { effect, players } = gameState;
+  const stateOfDamaged = players.find(
+    (v) => v.playerId === effect.damagedPlayer,
+  );
+  const tdArmdozerOfDamaged = view.td.armdozers.find(
+    (v) => v.playerId === effect.damagedPlayer,
+  );
+  const tdPlayerOfDamaged = view.td.players.find(
+    (v) => v.playerId === effect.damagedPlayer,
+  );
+  const hudPlayerOfDamaged = view.hud.players.find(
+    (v) => v.playerId === effect.damagedPlayer,
+  );
+  const hudPlayerOfReflecting = view.hud.players.find(
+    (v) => v.playerId !== effect.damagedPlayer,
+  );
+  if (
+    !stateOfDamaged ||
+    !tdArmdozerOfDamaged ||
+    !tdPlayerOfDamaged ||
+    !hudPlayerOfDamaged ||
+    !hudPlayerOfReflecting
+  ) {
+    return null;
+  }
+
+  const damaged = {
+    state: stateOfDamaged,
+    sprite: tdArmdozerOfDamaged.sprite(),
+    td: tdPlayerOfDamaged,
+    hud: hudPlayerOfDamaged,
+  };
+  const reflecting = {
+    hud: hudPlayerOfReflecting,
+  };
+  return { effect, damaged, reflecting };
+}
