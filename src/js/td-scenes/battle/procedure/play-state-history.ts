c3445b044429c3cf04c5c608844f649d11e02fee
Y.Takeuchi <kaidouji85@gmail.com>
2025-01-25 02:25:35 +0900

    lint、コードフォーマット

--- src/js/td-scenes/battle/procedure/play-state-history.ts
+++ src/js/td-scenes/battle/procedure/play-state-history.ts
@@ -3,8 +3,8 @@ import { GameState } from "gbraver-burst-core";
 import { all } from "../../../animation/all";
 import { empty } from "../../../animation/delay";
 import { stateAnimation } from "../animation/game-state";
-import { BattleSceneProps } from "../props";
 import { createAnimationPlay } from "../play-animation";
+import { BattleSceneProps } from "../props";
 
 /**
  * 同時再生する効果
