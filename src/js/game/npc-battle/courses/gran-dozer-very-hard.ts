e49dcbb8f01f42e3546b633de5f4ed574cd8ffed
Y.Takeuchi <kaidouji85@gmail.com>
2025-01-25 18:26:13 +0900

    グランドーザNPCコースを変更

--- src/js/game/npc-battle/courses/gran-dozer-very-hard.ts
+++ src/js/game/npc-battle/courses/gran-dozer-very-hard.ts
@@ -3,7 +3,7 @@ import { ArmdozerIds } from "gbraver-burst-core";
 import { SOUND_IDS } from "../../../resource/sound/ids";
 import { VeryHardGenesisBraver } from "../stages/very-hard-genesis-braver";
 import { VeryHardLightningDozer } from "../stages/very-hard-lightning-dozer";
-import { VeryHardNeoLandozer } from "../stages/very-hard-neo-landozer";
+import { VeryHardShinBraver } from "../stages/very-hard-shin-braver";
 import { VeryHardWingDozer } from "../stages/very-hard-wing-dozer";
 import { NPCBattleCourse } from "./npc-battle-course";
 
@@ -13,7 +13,7 @@ export const GranDozerVeryHard: NPCBattleCourse = {
   difficulty: "VeryHard",
   stages: [
     { ...VeryHardLightningDozer, bgm: SOUND_IDS.BATTLE_BGM_01 },
-    { ...VeryHardNeoLandozer, bgm: SOUND_IDS.BATTLE_BGM_02 },
+    { ...VeryHardShinBraver, bgm: SOUND_IDS.BATTLE_BGM_02 },
     { ...VeryHardWingDozer, bgm: SOUND_IDS.BATTLE_BGM_03 },
     { ...VeryHardGenesisBraver, bgm: SOUND_IDS.YUUYA_BATTLE },
   ],
