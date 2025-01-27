76926c3214deb451d5aac978f54685945f2db023
kaidouji85 <kaidouji85@gmail.com>
2025-01-27 03:00:06 +0000

    ストリーム生成処理を分割した

--- src/js/game/game-procedure/create-game-action-notifier.ts
+++ src/js/game/game-procedure/create-game-action-notifier.ts
@@ -4,37 +4,50 @@ import { GameAction } from "../game-actions";
 import { GameProps } from "../game-props";
 
 /**
+ * バトル強制終了の通知ストリームを生成する
+ * @param props ゲームプロパティ
+ * @returns バトル強制終了の通知ストリーム
+ */
+const createSuddenlyBattleEnd = (
+  props: Readonly<GameProps>,
+): Observable<GameAction> =>
+  props.suddenlyBattleEnd
+    .stream()
+    .pipe(map(() => ({ type: "SuddenlyBattleEnd" })));
+
+/**
+ * WebSocketAPIエラーの通知ストリームを生成する
+ * @param props ゲームプロパティ
+ * @returns WebSocketAPIエラーの通知ストリーム
+ */
+const createWebSocketAPIError = (
+  props: Readonly<GameProps>,
+): Observable<GameAction> =>
+  props.api
+    .websocketErrorNotifier()
+    .pipe(map((error) => ({ type: "WebSocketAPIError", error })));
+
+/**
+ * visibilitychangeイベントの通知ストリームを生成する
+ * @returns visibilitychangeイベントの通知ストリーム
+ */
+const createVisibilityChange = (): Observable<GameAction> =>
+  fromEvent(document, "visibilitychange").pipe(
+    map(() => ({ type: "VisibilityChange" })),
+  );
+
+/**
  * ゲームアクション通知ストリームを生成する
  * @param props ゲームプロパティ
  * @returns ゲームアクション通知ストリーム
  */
-export function createGameActionNotifier(
+export const createGameActionNotifier = (
   props: Readonly<GameProps>,
-): Observable<GameAction> {
-  const suddenlyBattleEnd: Observable<GameAction> = props.suddenlyBattleEnd
-    .stream()
-    .pipe(
-      map(() => ({
-        type: "SuddenlyBattleEnd",
-      })),
-    );
-  const webSocketAPIError: Observable<GameAction> = props.api
-    .websocketErrorNotifier()
-    .pipe(
-      map((error) => ({
-        type: "WebSocketAPIError",
-        error,
-      })),
-    );
-  const visibilityChange: Observable<GameAction> = fromEvent(
-    document,
-    "visibilitychange",
-  ).pipe(map(() => ({ type: "VisibilityChange" })));
-  return merge(
+): Observable<GameAction> =>
+  merge(
     props.gameAction.notify(),
     props.domFloaters.gameActionNotifier(),
-    suddenlyBattleEnd,
-    webSocketAPIError,
-    visibilityChange,
+    createSuddenlyBattleEnd(props),
+    createWebSocketAPIError(props),
+    createVisibilityChange(),
   );
-}
