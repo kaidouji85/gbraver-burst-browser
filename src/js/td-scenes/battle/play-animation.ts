da281e35235116f6875055eb54d309d01f5fc474
Y.Takeuchi <kaidouji85@gmail.com>
2025-01-27 23:00:36 +0900

    戦闘シーン用アニメーション再生関数にAbortSignalを追加した

--- src/js/td-scenes/battle/play-animation.ts
+++ src/js/td-scenes/battle/play-animation.ts
@@ -1,11 +1,16 @@
+import { AbortControllerContainer } from "../../abort-cntroller/abort-controller-container";
 import { Animate } from "../../animation/animate";
 import { AnimationTimeScaleContainer } from "./animation-time-scale-container";
+
+/** 戦闘シーンプロパティから本関数で利用するものをピックアップしたもの */
+type Props = AnimationTimeScaleContainer & AbortControllerContainer;
 
 /**
  * 戦闘シーン専用のアニメーション再生関数を生成する
  * @param props 戦闘シーンプロパティ
  * @returns アニメーション再生関数
  */
-export const createAnimationPlay =
-  (props: AnimationTimeScaleContainer) => (animate: Animate) =>
-    animate.timeScale(props.animationTimeScale).play();
+export const createAnimationPlay = (props: Props) => (animate: Animate) => {
+  const { signal } = props.abortController;
+  return animate.timeScale(props.animationTimeScale).play({ signal });
+};
