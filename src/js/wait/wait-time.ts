a5e564d24ba3c6b8ee5b898931edb5c9317661d1
kaidouji85 <kaidouji85@gmail.com>
2025-01-28 07:11:52 +0000

    waitTimeにキャンセルを追加

--- src/js/wait/wait-time.ts
+++ src/js/wait/wait-time.ts
@@ -1,13 +1,34 @@
+import { SignalContainer } from "../abort-cntroller/signal-container";
+
 /**
  * 指定した時間だけ待つ
- *
  * @param ms 待ち時間(ミリ秒)
+ * @param options オプション
  * @returns 待機Promise
  */
-export function waitTime(ms: number): Promise<void> {
-  return new Promise((resolve) => {
+export function waitTime(
+  ms: number,
+  options?: Partial<SignalContainer>,
+): Promise<void> {
+  const signal = options?.signal;
+  let onAbort: (() => void) | null = null;
+  let onCleanupOfAbort: (() => void) | null = null;
+  return new Promise<void>((resolve, reject) => {
+    if (signal?.aborted) {
+      reject(signal.reason);
+      return;
+    }
+
+    onAbort = () => reject(signal?.reason);
+    signal?.addEventListener("abort", onAbort);
+
+    onCleanupOfAbort = () =>
+      signal && onAbort && signal.removeEventListener("abort", onAbort);
+
     setTimeout(() => {
       resolve();
     }, ms);
+  }).finally(() => {
+    onCleanupOfAbort?.();
   });
 }
