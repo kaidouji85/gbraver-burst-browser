c2019478bb9525d523a4008acc73ae035a7f5494
kaidouji85 <kaidouji85@gmail.com>
2025-01-29 10:31:05 +0000

    不要なJSDocを削除

--- src/js/game-dom/battle-hamburger-menu/procedure/enable-battle-simulator.ts
+++ src/js/game-dom/battle-hamburger-menu/procedure/enable-battle-simulator.ts
@@ -4,7 +4,6 @@ import { BattleHamburgerMenuProps } from "../props";
 /**
  * バトルシミュレーターを選択可能にする
  * @param props プロパティ
- * @param isBattleSimulatorEnable バトルシミュレーターが選択可能な場合true
  */
 export function enableBattleSimulator(props: BattleHamburgerMenuProps) {
   props.canBattleSimulatorStart = true;
