54df663315a5ae3518be22a07c68df2e2833dc19
kaidouji85 <kaidouji85@gmail.com>
2025-01-31 07:32:51 +0000

    WIP 関数を作り直した

--- src/js/game/npc-battle/npc-battle-result.ts
+++ src/js/game/npc-battle/npc-battle-result.ts
@@ -1,21 +1,31 @@
+import { GameEndResult } from "gbraver-burst-core";
+
+import { NPCBattleState } from "./npc-battle-state";
+
 /** NPCバトル結果 */
 export type NPCBattleResult = "StageClear" | "StageMiss" | "NPCBattleComplete";
 
 /**
  * NPCバトル結果を求める
- * @param isStageClear ステージクリアしたか否かのフラグ、trueでステージクリア
- * @param isLastStage ラストステージか否かのフラグ、trueでラストステージ
+ * @param origin 更新前のステート
+ * @param gameEndResult 戦闘結果
  * @returns 判定結果
  */
 export function getNPCBattleResult(
-  isStageClear: boolean,
-  isLastStage: boolean,
+  origin: Readonly<NPCBattleState>,
+  gameEndResult: Readonly<GameEndResult>,
 ): NPCBattleResult {
+  const isStageClear =
+    gameEndResult.type === "GameOver" &&
+    gameEndResult.winner === origin.player.playerId;
+  const isLastStage = origin.stageIndex === origin.stages.length - 1;
+
+  let result: NPCBattleResult = "StageMiss";
   if (isStageClear && isLastStage) {
-    return "NPCBattleComplete";
+    result = "NPCBattleComplete";
   } else if (isStageClear && !isLastStage) {
-    return "StageClear";
+    result = "StageClear";
   }
 
-  return "StageMiss";
+  return result;
 }
--- src/js/game/npc-battle/updated-npc-battle-state.ts
+++ src/js/game/npc-battle/updated-npc-battle-state.ts
@@ -1,42 +1,26 @@
-import { GameEndResult } from "gbraver-burst-core";
-
-import { getCurrentNPCStage } from "./get-current-npc-stage";
-import { getNPCBattleResult, NPCBattleResult } from "./npc-battle-result";
+import { NPCBattleResult } from "./npc-battle-result";
 import { NPCBattleState } from "./npc-battle-state";
 
 /** NPCバトル更新結果 */
 export type UpdatedNPCBattleState = {
   /** 更新されたステート */
-  state: NPCBattleState;
+  readonly state: NPCBattleState;
   /** NPCバトル結果 */
-  result: NPCBattleResult;
+  readonly result: NPCBattleResult;
 };
 
 /**
  * NPCバトルステートを更新する
  * @param origin 更新前のステート
- * @param gameEndResult 戦闘結果
+ * @param npcBattleResult NPCバトル結果
  * @returns NPCバトル更新結果
  */
 export function updateNPCBattleState(
   origin: Readonly<NPCBattleState>,
-  gameEndResult: Readonly<GameEndResult>,
+  result: NPCBattleResult,
 ): UpdatedNPCBattleState | null {
-  const stage = getCurrentNPCStage(origin);
-  if (!stage) {
-    return null;
-  }
-
-  const isStageClear =
-    gameEndResult.type === "GameOver" &&
-    gameEndResult.winner === origin.player.playerId;
-  const isLastStage = origin.stageIndex === origin.stages.length - 1;
-  const result = getNPCBattleResult(isStageClear, isLastStage);
   const nextStageIndex =
     result === "StageClear" ? origin.stageIndex + 1 : origin.stageIndex;
-  const updatedState = { ...origin, stageIndex: nextStageIndex };
-  return {
-    state: updatedState,
-    result,
-  };
+  const state = { ...origin, stageIndex: nextStageIndex };
+  return { state, result };
 }
