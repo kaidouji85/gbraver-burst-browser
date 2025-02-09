d58171398b3074ddc0a9b938b56644c0e4f94fe2
Y.Takeuchi <kaidouji85@gmail.com>
2025-02-09 17:14:01 +0900

    ガードを修正

--- src/js/td-scenes/battle/animation/game-state/battle/attack/gran-dozer/guard.ts
+++ src/js/td-scenes/battle/animation/game-state/battle/attack/gran-dozer/guard.ts
@@ -3,9 +3,9 @@ import { Guard } from "gbraver-burst-core";
 import { all } from "../../../../../../../animation/all";
 import { Animate } from "../../../../../../../animation/animate";
 import { delay } from "../../../../../../../animation/delay";
-import { GranDozerBattle } from "./gran-dozer-battle";
-import { focusToAttacker } from "./focus-to-attacker";
 import { toInitial } from "../../../../td-camera";
+import { focusToAttacker } from "./focus-to-attacker";
+import { GranDozerBattle } from "./gran-dozer-battle";
 
 /**
  * ガード
@@ -16,22 +16,20 @@ export function guard(param: GranDozerBattle<Guard>): Animate {
   return all(
     param.attackerSprite.charge().chain(delay(500)),
     focusToAttacker(param.tdCamera, param.attackerSprite),
-  ).chain(
-    all(
-      param.attackerSprite
-        .tackle()
-        .chain(delay(1000))
-        .chain(param.attackerSprite.tackleToStand())
-        .chain(delay(500)),
-    ),
-    delay(100).chain(
+  )
+    .chain(param.attackerSprite.tackle())
+    .chain(
       all(
+        param.attackerSprite
+          .tackleRecoil()
+          .chain(delay(1000))
+          .chain(param.attackerSprite.tackleToStand())
+          .chain(delay(500)),
         toInitial(param.tdCamera, 100),
         param.defenderTD.damageIndicator.popUp(param.result.damage),
         param.defenderSprite.guard(),
         param.defenderTD.hitMark.shockWave.popUp(),
         param.defenderHUD.gauge.hp(param.defenderState.armdozer.hp),
       ),
-    ),
-  );
+    );
 }
