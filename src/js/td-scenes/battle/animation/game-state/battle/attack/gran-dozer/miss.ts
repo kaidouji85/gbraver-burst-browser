d616fb7f42a7fa752c9c942de222c1745c487c9d
Y.Takeuchi <kaidouji85@gmail.com>
2025-02-09 17:12:33 +0900

    ミスを修正

--- src/js/td-scenes/battle/animation/game-state/battle/attack/gran-dozer/miss.ts
+++ src/js/td-scenes/battle/animation/game-state/battle/attack/gran-dozer/miss.ts
@@ -1,6 +1,5 @@
 import { Miss } from "gbraver-burst-core";
 
-import { all } from "../../../../../../../animation/all";
 import { Animate } from "../../../../../../../animation/animate";
 import { delay } from "../../../../../../../animation/delay";
 import { GranDozerBattle } from "./gran-dozer-battle";
@@ -14,12 +13,9 @@ export function miss(param: GranDozerBattle<Miss>): Animate {
   return param.attackerSprite
     .charge()
     .chain(delay(500))
-    .chain(
-      all(
-        param.attackerSprite.tackle(),
-        delay(100).chain(param.defenderSprite.avoid().chain(delay(500))),
-      ),
-    )
+    .chain(param.attackerSprite.tackle())
+    .chain(param.defenderSprite.avoid())
+    .chain(delay(500))
     .chain(param.attackerSprite.tackleToStand())
     .chain(delay(500));
 }
