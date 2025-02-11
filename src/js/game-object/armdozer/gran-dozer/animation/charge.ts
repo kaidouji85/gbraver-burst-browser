a665b8d521be13f32ad8e5d2db87c1a8b4454ef8
Y.Takeuchi <kaidouji85@gmail.com>
2025-02-11 23:35:40 +0900

    仮調整

--- src/js/game-object/armdozer/gran-dozer/animation/charge.ts
+++ src/js/game-object/armdozer/gran-dozer/animation/charge.ts
@@ -1,4 +1,3 @@
-import { all } from "../../../../animation/all";
 import { Animate } from "../../../../animation/animate";
 import { tween } from "../../../../animation/tween";
 import { GranDozerAnimationProps } from "./animation-props";
@@ -10,13 +9,10 @@ import { GranDozerAnimationProps } from "./animation-props";
  */
 export function charge(props: GranDozerAnimationProps): Animate {
   const { model, sounds, se } = props;
-  return all(
-    tween(model.animation, (t) =>
-      t.to({ frame: 0 }, 0).onStart(() => {
-        model.animation.type = "TACKLE_CHARGE";
-        se.play(sounds.motor);
-      }),
-    ).chain(tween(model.animation, (t) => t.to({ frame: 1 }, 300))),
-    tween(model.position, (t) => t.to({ x: "+80" }, 300)),
-  );
+  return tween(model.animation, (t) =>
+    t.to({ frame: 0 }, 0).onStart(() => {
+      model.animation.type = "TACKLE_CHARGE";
+      se.play(sounds.motor);
+    }),
+  ).chain(tween(model.animation, (t) => t.to({ frame: 1 }, 300)));
 }
