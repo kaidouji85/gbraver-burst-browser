ca69fefc8832013a6282f0e49830c99209511b6e
kaidouji85 <kaidouji85@gmail.com>
2025-02-20 11:28:19 +0000

    waitFinishAnimationに強制停止を追加した

--- src/js/dom/wait-finish-animation.ts
+++ src/js/dom/wait-finish-animation.ts
@@ -1,12 +1,35 @@
+import { SignalContainer } from "../abort-controller/signal-container";
+
 /**
  * アニメーションが完了するまで待機する
  * @param animation アニメーション
+ * @param options オプション
  * @returns アニメーションPromise
  */
-export function waitFinishAnimation(animation: Animation): Promise<void> {
-  return new Promise((resolve) => {
+export function waitFinishAnimation(
+  animation: Animation,
+  options?: Partial<SignalContainer>,
+): Promise<void> {
+  let onAbort: (() => void) | null = null;
+  const signal = options?.signal;
+  return new Promise<void>((resolve, reject) => {
+    if (signal?.aborted) {
+      reject(signal.reason);
+      return;
+    }
+
+    onAbort = () => {
+      animation.pause();
+      reject(signal?.reason);
+    };
+    signal?.addEventListener("abort", onAbort);
+
     animation.onfinish = () => {
       resolve();
     };
+  }).finally(() => {
+    if (signal && onAbort) {
+      signal.removeEventListener("abort", onAbort);
+    }
   });
 }
