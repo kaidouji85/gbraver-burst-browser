da54585a46966956c033d224023113b12f3931b2
Y.Takeuchi <kaidouji85@gmail.com>
2025-02-24 18:04:41 +0900

    ノックバックアニメーションを仮作成

---
+++ src/js/game-object/armdozer/gran-dozer/animation/knock-back-to-stand.ts
@@ -0,0 +1,26 @@
+import { Animate } from "../../../../animation/animate";
+import { tween } from "../../../../animation/tween";
+import { GranDozerAnimationProps } from "./animation-props";
+
+/**
+ * ノックバック -> 立ち
+ * @param props アニメーションプロパティ
+ * @returns アニメーション
+ */
+export function knockBackToStand(props: GranDozerAnimationProps): Animate {
+  const { model, sounds, se } = props;
+  return tween(model.animation, (t) =>
+    t.to({ frame: 1 }, 0).onStart(() => {
+      model.animation.type = "KNOCK_BACK";
+      se.play(sounds.motor);
+    }),
+  )
+    .chain(tween(model.animation, (t) => t.to({ frame: 0 }, 300)))
+    .chain(
+      tween(model.animation, (t) =>
+        t.to({ frame: 0 }, 0).onStart(() => {
+          model.animation.type = "STAND";
+        }),
+      ),
+    );
+}
