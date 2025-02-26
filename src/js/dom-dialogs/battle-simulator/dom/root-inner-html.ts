36c5d31e74f77a100f1efbfc57d02caf2191d802
kaidouji85 <kaidouji85@gmail.com>
2025-02-26 02:45:09 +0000

    プレイヤー、敵のステート生成を別関数に切り出した

--- src/js/dom-dialogs/battle-simulator/dom/root-inner-html.ts
+++ src/js/dom-dialogs/battle-simulator/dom/root-inner-html.ts
@@ -3,6 +3,7 @@ import { ArmdozerIds, correctPower, PlayerState } from "gbraver-burst-core";
 import { getArmdozerStandPathId } from "../../../path/armdozer-stand-path";
 import { ResourcesContainer } from "../../../resource";
 import { PathIds } from "../../../resource/path/ids";
+import { PathId } from "../../../resource/path/resource";
 import {
   BATTLE_RESULT_NAME,
   DAMAGE,
@@ -12,8 +13,66 @@ import {
 } from "./class-name";
 import template from "./root-inner-html.hbs";
 
-/** 生成パラメータ */
-type RootInnerHtmlParams = ResourcesContainer & {
+/**
+ * アームドーザの攻撃を取得する
+ * @param player 対象プレイヤーのステート
+ * @returns アームドーザの攻撃
+ */
+const getPower = (player: PlayerState) =>
+  player.armdozer.power + correctPower(player.armdozer.effects);
+
+/**
+ * 各種パスを取得する、取得できない場合は空文字を返す
+ * @param options オプション
+ * @param options.resources リソース管理オブジェクト
+ * @param options.pathId パスID
+ * @returns
+ */
+const getPath = (options: ResourcesContainer & { pathId: PathId }) => {
+  const { resources, pathId } = options;
+  return resources.paths.find((p) => p.id === pathId)?.path ?? "";
+};
+
+/**
+ * プレイヤーのステートを取得する
+ * @param options オプション
+ * @param options.player プレイヤーのステート
+ * @returns 
+ */
+const getPlayerState = (
+  options: ResourcesContainer & { player: PlayerState },
+) => {
+  const { player } = options;
+  const playerPower = getPower(player);
+  const playerArmdozerId = player.armdozer.id ?? ArmdozerIds.SHIN_BRAVER;
+  const playerArmdozerPath = getPath({
+    ...options,
+    pathId: getArmdozerStandPathId(playerArmdozerId),
+  });
+  return { player, playerPower, playerArmdozerPath };
+};
+
+/**
+ * 敵のステートを取得する
+ * @param options オプション
+ * @param options.enemy 敵のステート
+ * @returns 
+ */
+const getEnemyState = (
+  options: ResourcesContainer & { enemy: PlayerState },
+) => {
+  const { enemy } = options;
+  const enemyPower = getPower(enemy);
+  const enemyArmdozerId = enemy.armdozer.id ?? ArmdozerIds.SHIN_BRAVER;
+  const enemyArmdozerPath = getPath({
+    ...options,
+    pathId: getArmdozerStandPathId(enemyArmdozerId),
+  });
+  return { enemy, enemyPower, enemyArmdozerPath };
+};
+
+/** 生成オプション */
+type RootInnerHtmlOptions = ResourcesContainer & {
   /** プレイヤーのステート */
   player: PlayerState;
   /** 敵のステート */
@@ -24,28 +83,11 @@ type RootInnerHtmlParams = ResourcesContainer & {
 
 /**
  * ルート要素のinnerHTMLを生成する
+ * @param options 生成オプション
  * @returns 生成結果
  */
-export function rootInnerHTML(params: RootInnerHtmlParams) {
-  const { resources, player, enemy, isPlayerAttacker } = params;
-
-  const playerPower =
-    player.armdozer.power + correctPower(player.armdozer.effects);
-  const enemyPower =
-    enemy.armdozer.power + correctPower(enemy.armdozer.effects);
-
-  const playerArmdozerId = player?.armdozer.id ?? ArmdozerIds.SHIN_BRAVER;
-  const playerArmdozerPath =
-    resources.paths.find(
-      (p) => p.id === getArmdozerStandPathId(playerArmdozerId),
-    )?.path ?? "";
-
-  const enemyArmdozerId = enemy?.armdozer.id ?? ArmdozerIds.SHIN_BRAVER;
-  const enemyArmdozerPath =
-    resources.paths.find(
-      (p) => p.id === getArmdozerStandPathId(enemyArmdozerId),
-    )?.path ?? "";
-
+export function rootInnerHTML(options: RootInnerHtmlOptions) {
+  const { resources, isPlayerAttacker } = options;
   const turnIndicatorClass = isPlayerAttacker
     ? TURN_INDICATOR
     : TURN_INDICATOR_ENEMY;
@@ -59,17 +101,13 @@ export function rootInnerHTML(params: RootInnerHtmlParams) {
   const batteryIconPath =
     resources.paths.find((p) => p.id === PathIds.BATTERY_ICON)?.path ?? "";
   return template({
-    player,
-    playerPower,
-    enemy,
-    enemyPower,
+    ...getPlayerState(options),
+    ...getEnemyState(options),
     ROOT,
     DAMAGE,
     BATTLE_RESULT_NAME,
     turnIndicatorClass,
     closerPath,
-    playerArmdozerPath,
-    enemyArmdozerPath,
     turnIndicatorPath,
     batteryIconPath,
   });
