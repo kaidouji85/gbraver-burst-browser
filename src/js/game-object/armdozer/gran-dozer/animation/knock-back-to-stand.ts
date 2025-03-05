70d9d6ee09d9b3bfa1001c0fedcb808af29e6ac7
kaidouji85 <kaidouji85@gmail.com>
2025-03-05 23:49:03 +0900

    モーション調整

--- src/js/game-object/armdozer/gran-dozer/animation/knock-back-to-stand.ts
+++ src/js/game-object/armdozer/gran-dozer/animation/knock-back-to-stand.ts
@@ -2,6 +2,7 @@ import { Animate } from "../../../../animation/animate";
 import { tween } from "../../../../animation/tween";
 import { GranDozerAnimationProps } from "./animation-props";
 import { onStart } from "../../../../animation/on-start";
+import { delay } from "../../../../animation/delay";
 
 /**
  * ノックバック -> 立ち
@@ -33,12 +34,14 @@ export function knockBackToStand(props: GranDozerAnimationProps): Animate {
           .to({ frame: 0 }, 0),
       ),
     )
-    .chain(tween(model.animation, (t) => t.to({ frame: 1 }, 200)))
+    .chain(tween(model.animation, (t) => t.to({ frame: 1 }, 300)))
     .chain(
       tween(model.animation, (t) =>
-        t.to({ frame: 0 }, 300).onStart(() => {
-          se.play(sounds.motor);
-        }),
+        t
+          .onStart(() => {
+            se.play(sounds.motor);
+          })
+          .to({ frame: 0 }, 300),
       ),
     )
     .chain(
