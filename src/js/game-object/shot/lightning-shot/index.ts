2a34646557c331269bfc3ac60514862c7f0df627
Y.Takeuchi <kaidouji85@gmail.com>
2025-03-15 17:01:02 +0900

    敵電撃ショットを追加した

--- src/js/game-object/shot/lightning-shot/index.ts
+++ src/js/game-object/shot/lightning-shot/index.ts
@@ -14,3 +14,15 @@ export function playerLightningShot(
   const view = new PlayerLightningShotView(options);
   return new LightningShot({ ...options, view });
 }
+
+/**
+ * 敵の電撃ショットを生成する
+ * @param options オプション
+ * @returns 敵の電撃ショット
+ */
+export function enemyLightningShot(
+  options: ResourcesContainer & GameObjectActionContainer,
+): LightningShot {
+  const view = new PlayerLightningShotView(options);
+  return new LightningShot({ ...options, view });
+}
--- stories/lightning-shot.stories.ts
+++ stories/lightning-shot.stories.ts
@@ -1,12 +1,21 @@
-import { playerLightningShot } from "../src/js/game-object/shot/lightning-shot";
+import {
+  enemyLightningShot,
+  playerLightningShot,
+} from "../src/js/game-object/shot/lightning-shot";
 import { tdGameObjectStory } from "./stub/td-game-object-stub";
 
 export default {
   title: "lightning-shot",
 };
 
-/** 電撃ショット */
+/** プレイヤー電撃ショット */
 export const player = tdGameObjectStory((params) => {
   const shot = playerLightningShot(params);
+  return { objects: [shot.getObject3D()] };
+});
+
+/** 敵電撃ショット */
+export const enemy = tdGameObjectStory((params) => {
+  const shot = enemyLightningShot(params);
   return { objects: [shot.getObject3D()] };
 });
