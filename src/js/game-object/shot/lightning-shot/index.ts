ef46beb822900440f4c9382341d0cac9e296a60b
Y.Takeuchi <kaidouji85@gmail.com>
2025-03-15 15:14:55 +0900

    電撃の雛形を追加した

---
+++ src/js/game-object/shot/lightning-shot/index.ts
@@ -0,0 +1,42 @@
+import * as THREE from "three";
+
+import { HorizontalAnimationMesh } from "../../../mesh/horizontal-animation";
+import { Resources } from "../../../resource";
+import { TEXTURE_IDS } from "../../../resource/texture/ids";
+
+/** 電撃ショット */
+export class LightningShot {
+  /** メッシュ */
+  #mesh: HorizontalAnimationMesh;
+
+  /**
+   * コンストラクタ
+   * @param resources リソース管理オブジェクト
+   */
+  constructor(resources: Resources) {
+    const texture =
+      resources.textures.find((t) => t.id === TEXTURE_IDS.LIGHTNING_SHOT)
+        ?.texture ?? new THREE.Texture();
+    this.#mesh = new HorizontalAnimationMesh({
+      texture,
+      maxAnimation: 8,
+      width: 200,
+      height: 200,
+    });
+  }
+
+  /**
+   * デストラクタ相当の処理
+   */
+  destructor(): void {
+    this.#mesh.destructor();
+  }
+
+  /**
+   * シーンに追加するオブジェクトを取得する
+   * @returns シーンに追加するオブジェクト
+   */
+  getObject3D(): THREE.Object3D {
+    return this.#mesh.getObject3D();
+  }
+}
