dd7e233760db50faee11a2ed9eb79a65c75a5a09
Y.Takeuchi <kaidouji85@gmail.com>
2025-03-16 13:12:18 +0900

    雛形を追加

---
+++ src/js/game-object/armdozer/gran-dozer/animation/burst-to-stand.ts
@@ -0,0 +1,47 @@
+import { delay } from "../../../../animation/delay";
+import { tween } from "../../../../animation/tween";
+import { ARMDOZER_SPRITE_ATTACKER_Z } from "../../position";
+import { GranDozerAnimationProps } from "./animation-props";
+
+/**
+ * バースト -> 立ち
+ * @param props アニメーションプロパティ
+ * @returns アニメーション
+ */
+export function burstToStand(props: GranDozerAnimationProps) {
+  const { model, sounds, se } = props;
+  return tween(model.animation, (t) =>
+    t.to({ frame: 1 }, 0).onStart(() => {
+      model.animation.type = "TACKLE_ATTACK";
+      se.play(sounds.motor);
+    }),
+  )
+    .chain(tween(model.animation, (t) => t.to({ frame: 0 }, 300)))
+    .chain(
+      tween(model, (t) =>
+        t
+          .onStart(() => {
+            model.animation.type = "TACKLE_CHARGE";
+            se.play(sounds.motor);
+          })
+          .to(
+            {
+              animation: { frame: 1 },
+              position: { z: ARMDOZER_SPRITE_ATTACKER_Z },
+            },
+            0,
+          ),
+      ),
+    )
+    .chain(delay(300))
+    .chain(tween(model, (t) => t.to({ animation: { frame: 0 } }, 300)))
+    .chain(
+      tween(model, (t) =>
+        t
+          .onStart(() => {
+            model.animation.type = "STAND";
+          })
+          .to({ animation: { frame: 0 } }, 0),
+      ),
+    );
+}
