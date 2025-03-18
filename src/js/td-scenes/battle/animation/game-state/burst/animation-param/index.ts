cf8e8fb17708f9df7c1de6e0b24142d500ac5df5
Y.Takeuchi <kaidouji85@gmail.com>
2025-03-19 00:05:36 +0900

    OtherParam追加

--- src/js/td-scenes/battle/animation/game-state/burst/animation-param/index.ts
+++ src/js/td-scenes/battle/animation/game-state/burst/animation-param/index.ts
@@ -1,14 +1,11 @@
 import { Burst, BurstEffect, GameStateX } from "gbraver-burst-core";
 
-import { PlainHUDCamera } from "../../../../../../game-object/camera/plain-hud/plain-hud-camera";
-import { TDCamera } from "../../../../../../game-object/camera/td";
 import { HUDArmdozerObjects } from "../../../../view/hud/armdozer-objects/hud-armdozer-objects";
-import { HUDGameObjects } from "../../../../view/hud/game-objects";
 import { TDArmdozerObjects } from "../../../../view/td/armdozer-objects/armdozer-objects";
-import { TDGameObjects } from "../../../../view/td/game-objects";
 import { StateAnimationProps } from "../../state-animation-props";
-import { toAttackerParam } from "./attacker-param";
+import { AttackerParam, toAttackerParam } from "./attacker-param";
 import { BurstPlayerParam, toBurstPlayerParam } from "./burst-player-param";
+import { OtherParam, toOtherParam } from "./other-param";
 
 /**
  * バーストアニメーションのパラメータ
@@ -21,19 +18,9 @@ export type BurstAnimationParamX<
   TD_ARMDOZER extends TDArmdozerObjects,
   HUD_ARMDOZER extends HUDArmdozerObjects,
   BURST extends Burst,
-> = BurstPlayerParam<TD_ARMDOZER, HUD_ARMDOZER, BURST> & {
-  /** 攻撃側3Dアームドーザ */
-  attackerArmdozerTD: TDArmdozerObjects;
-
-  /** 3Dレイヤーオブジェクト */
-  tdObjects: TDGameObjects;
-  /** 3Dカメラ */
-  tdCamera: TDCamera;
-  /** HUDレイヤーオブジェクト */
-  hudObjects: HUDGameObjects;
-  /** HUDカメラ */
-  hudCamera: PlainHUDCamera;
-};
+> = BurstPlayerParam<TD_ARMDOZER, HUD_ARMDOZER, BURST> &
+  AttackerParam &
+  OtherParam;
 
 /** バーストアニメーションのパラメータ */
 export type BurstAnimationParam = BurstAnimationParamX<
@@ -62,9 +49,6 @@ export function toBurstAnimationParam(
   return {
     ...burstPlayerParam,
     ...attackerParam,
-    tdObjects: props.view.td.gameObjects,
-    tdCamera: props.view.td.camera,
-    hudObjects: props.view.hud.gameObjects,
-    hudCamera: props.view.hud.camera,
+    ...toOtherParam(props),
   };
 }
