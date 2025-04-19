02084923acc6fb05c390854eb187a75ab07393aa
Y.Takeuchi <kaidouji85@gmail.com>
2025-04-19 22:33:09 +0900

    DynamicArmdozerResourceを追加した

--- src/js/resource/resource-type.ts
+++ src/js/resource/resource-type.ts
@@ -1,5 +1,10 @@
+import { ArmdozerId } from "gbraver-burst-core";
+
 /** リソース種別 */
-export type ResourceType = BootResource | SharedResource;
+export type ResourceType =
+  | BootResource
+  | SharedResource
+  | DynamicArmdozerResource;
 
 /** タイトル画面のための必要最低限のリソース */
 export type BootResource = {
@@ -9,4 +14,11 @@ export type BootResource = {
 /** ゲーム開始したら読み込まれっぱなしとなるリソース */
 export type SharedResource = {
   type: "Shared";
+};
+
+/** ダイナミックにロードするアームドーザ関連リソース */
+export type DynamicArmdozerResource = {
+  type: "DynamicArmdozer";
+  /** アームドーザーのID */
+  id: ArmdozerId;
 };
