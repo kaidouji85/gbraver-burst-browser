6bf8fa455116d167f7219173597e6567fa309664
Y.Takeuchi <kaidouji85@gmail.com>
2025-04-19 16:27:30 +0900

    SoundにResourceTypeを追加した

--- src/js/resource/sound/empty-sound-resource.ts
+++ src/js/resource/sound/empty-sound-resource.ts
@@ -8,11 +8,9 @@ import { SoundResource } from "./resource";
  */
 export function createEmptySoundResource(): SoundResource {
   return {
+    type: "Shared",
     id: "EmptyResource",
-    sound: new Howl({
-      src: "",
-      mute: true,
-    }),
+    sound: new Howl({ src: "", mute: true }),
     volumeScale: 1,
   };
 }
