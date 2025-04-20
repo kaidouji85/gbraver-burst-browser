036ff6f2b330ddc031ece2d02a18223eb3701b57
Y.Takeuchi <kaidouji85@gmail.com>
2025-04-20 16:51:23 +0900

    Refactors resource deletion for efficiency
    Uses ramda groupBy to improve resource deletion,
    making the code more concise and efficient.
    
    Avoids unnecessary filtering and mutation,
    instead groups resources based on whether they should be deleted,
    and then disposes of the resources in each group.

--- src/js/resource/update-battle-scene-resources/delete-unused-resources.ts
+++ src/js/resource/update-battle-scene-resources/delete-unused-resources.ts
@@ -1,4 +1,5 @@
 import { ArmdozerId } from "gbraver-burst-core";
+import * as R from "ramda";
 
 import { disposeGltfModel } from "../gltf/dispose-gltf-model";
 import { Resources } from "../index";
@@ -14,62 +15,51 @@ import { ResourceType } from "../resource-type";
 export const deleteUnusedResources = (options: {
   resources: Readonly<Resources>;
   deletionArmdozerIds: Readonly<ArmdozerId[]>;
-}) => {
+}): Resources => {
   const { resources, deletionArmdozerIds } = options;
   const shouldDelete = (t: ResourceType) =>
     t.type === "DynamicArmdozer" && deletionArmdozerIds.includes(t.armdozerId);
 
-  const gltfs = resources.gltfs.filter((g) => {
-    if (shouldDelete(g)) {
-      disposeGltfModel(g);
-      return false;
-    } else {
-      return true;
-    }
+  const { deleteGltfs, keepGltfs } = R.groupBy(
+    (g) => (shouldDelete(g) ? "deleteGltfs" : "keepGltfs"),
+    resources.gltfs,
+  );
+  deleteGltfs?.forEach((g) => disposeGltfModel(g));
+
+  const { deleteTextures, keepTextures } = R.groupBy(
+    (t) => (shouldDelete(t) ? "deleteTextures" : "keepTextures"),
+    resources.textures,
+  );
+  deleteTextures?.forEach((textures) => {
+    textures.texture.dispose();
   });
 
-  const textures = resources.textures.filter((t) => {
-    if (shouldDelete(t)) {
-      t.texture.dispose();
-      return false;
-    } else {
-      return true;
-    }
+  const { deleteCubeTextures, keepCubeTextures } = R.groupBy(
+    (c) => (shouldDelete(c) ? "deleteCubeTextures" : "keepCubeTextures"),
+    resources.cubeTextures,
+  );
+  deleteCubeTextures?.forEach((textures) => {
+    textures.texture.dispose();
   });
 
-  const cubeTextures = resources.cubeTextures.filter((t) => {
-    if (shouldDelete(t)) {
-      t.texture.dispose();
-      return false;
-    } else {
-      return true;
-    }
-  });
+  const { deleteCanvasImages, keepCanvasImages } = R.groupBy(
+    (c) => (shouldDelete(c) ? "deleteCanvasImages" : "keepCanvasImages"),
+    resources.canvasImages,
+  );
+  deleteCanvasImages?.forEach((c) => (c.image.src = ""));
 
-  const canvasImages = resources.canvasImages.filter((t) => {
-    if (shouldDelete(t)) {
-      t.image.src = "";
-      return false;
-    } else {
-      return true;
-    }
-  });
-
-  const sounds = resources.sounds.filter((t) => {
-    if (shouldDelete(t)) {
-      t.sound.unload();
-      return false;
-    } else {
-      return true;
-    }
-  });
+  const { deleteSounds, keepSounds } = R.groupBy(
+    (s) => (shouldDelete(s) ? "deleteSounds" : "keepSounds"),
+    resources.sounds,
+  );
+  deleteSounds?.forEach((s) => s.sound.unload());
 
   return {
     ...resources,
-    gltfs,
-    textures,
-    cubeTextures,
-    canvasImages,
-    sounds,
+    gltfs: keepGltfs ?? [],
+    textures: keepTextures ?? [],
+    cubeTextures: keepCubeTextures ?? [],
+    canvasImages: keepCanvasImages ?? [],
+    sounds: keepSounds ?? [],
   };
 };
