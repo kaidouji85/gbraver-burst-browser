3418329d91173826b8d47a4235e2ceefa6348942
Y.Takeuchi <kaidouji85@gmail.com>
2025-04-20 14:31:07 +0900

    Clarifies JSDoc for armdozer ID extraction
    Adds details to JSDoc comments in functions that extract Armdozer IDs, improving clarity and maintainability.

--- src/js/resource/update-battle-scene-resources/get-player-armdozer-ids.ts
+++ src/js/resource/update-battle-scene-resources/get-player-armdozer-ids.ts
@@ -3,7 +3,7 @@ import { ArmdozerId, Player } from "gbraver-burst-core";
 /**
  * プレイヤーステートからアームドーザIDを抽出する
  * @param players バトルに参加している全プレイヤー
- * @returns アームドーザID
+ * @returns 抽出したアームドーザID
  */
 export const getPlayerArmdozerIds = (
   players: [Player, Player],
--- src/js/resource/update-battle-scene-resources/get-resource-armdozer-ids.ts
+++ src/js/resource/update-battle-scene-resources/get-resource-armdozer-ids.ts
@@ -4,7 +4,8 @@ import { ResourceType } from "../resource-type";
 
 /**
  * リソース管理オブジェクトからアームドーザIDを抽出する
- * @param resourceTypes
+ * @param resourceTypes リソース管理オブジェクトから抽出したリソース種別一覧
+ * @returns 抽出したアームドーザID
  */
 export const getResourceArmdozerIds = (
   resourceTypes: ResourceType[],
