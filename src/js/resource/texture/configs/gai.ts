c8c4b02a7dd17687e54182601ec022e51031c983
Y.Takeuchi <kaidouji85@gmail.com>
2025-04-23 08:36:24 +0900

    DynamicPilotResourceを追加した

--- src/js/resource/texture/configs/gai.ts
+++ src/js/resource/texture/configs/gai.ts
@@ -1,3 +1,5 @@
+import { PilotIds } from "gbraver-burst-core";
+
 import { TEXTURE_IDS } from "../ids";
 import { TextureConfig } from "../resource";
 
@@ -7,4 +9,4 @@ export const GaiTextureConfigs: TextureConfig[] = [
     id: TEXTURE_IDS.GAI_CUTIN,
     path: "pilot/gai/skill-cutin.webp",
   },
-].map((t) => ({ ...t, type: "Shared" }));
+].map((t) => ({ ...t, type: "DynamicPilot", pilotId: PilotIds.GAI }));
