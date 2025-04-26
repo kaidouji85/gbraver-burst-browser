269e60b7db59f37749b20266db8ec94299b5d139
Y.Takeuchi <kaidouji85@gmail.com>
2025-04-26 17:34:45 +0900

    シナリオ雛形を追加した

--- src/js/custom-battle-events/survive-super-power-with-guard/index.ts
+++ src/js/custom-battle-events/survive-super-power-with-guard/index.ts
@@ -1,7 +1,11 @@
-import { CustomBattleEvent } from "../../td-scenes/battle/custom-battle-event";
+import {
+  CustomBattleEvent,
+  LastState,
+} from "../../td-scenes/battle/custom-battle-event";
 import { EmptyCustomBattleEvent } from "../empty-custom-battle-event";
-import { SurviveSuperPowerWithGuardProps } from "./props";
+import { beforeLastState } from "./procedures/before-last-state";
 import { createSurviveSuperPowerWithGuardProps } from "./procedures/create-survive-super-power-with-guard-props";
+import { SurviveSuperPowerWithGuardProps } from "./props";
 
 /** 「超火力はガードで凌げ」用のカスタムバトルイベント*/
 class SurviveSuperPowerWithGuard extends EmptyCustomBattleEvent {
@@ -14,6 +18,11 @@ class SurviveSuperPowerWithGuard extends EmptyCustomBattleEvent {
   constructor() {
     super();
     this.#props = createSurviveSuperPowerWithGuardProps();
+  }
+
+  /** @override */
+  async beforeLastState(props: LastState): Promise<void> {
+    this.#props.state = await beforeLastState({ ...props, ...this.#props });
   }
 }
 
