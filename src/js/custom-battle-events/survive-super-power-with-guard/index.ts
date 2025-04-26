f451305ea7616e62807a701844d380681dc4c4f2
Y.Takeuchi <kaidouji85@gmail.com>
2025-04-26 17:15:57 +0900

    SurviveSuperPowerWithGuard追加

--- src/js/custom-battle-events/survive-super-power-with-guard/index.ts
+++ src/js/custom-battle-events/survive-super-power-with-guard/index.ts
@@ -1,10 +1,20 @@
 import { CustomBattleEvent } from "../../td-scenes/battle/custom-battle-event";
 import { EmptyCustomBattleEvent } from "../empty-custom-battle-event";
 
+/** 「超火力はガードで凌げ」用のカスタムバトルイベント*/
+class SurviveSuperPowerWithGuard extends EmptyCustomBattleEvent {
+  /**
+   * コンストラクタ
+   */
+  constructor() {
+    super();
+  }
+}
+
 /**
  * 「超火力はガードで凌げ」用のカスタムバトルイベントを生成する
  * @returns カスタムバトルイベント
  */
 export function createSurviveSuperPowerWithGuardEvent(): CustomBattleEvent {
-  return new EmptyCustomBattleEvent();
+  return new SurviveSuperPowerWithGuard();
 }
