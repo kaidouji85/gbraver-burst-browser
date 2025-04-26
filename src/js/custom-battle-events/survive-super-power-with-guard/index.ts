080f77598cdd88bc8a37d0c34c07bf90015bb5f7
Y.Takeuchi <kaidouji85@gmail.com>
2025-04-26 17:23:10 +0900

    Adds SurviveSuperPowerWithGuard event
    Initializes the "Survive Super Power With Guard" custom battle event.
    
    This commit introduces the structure and initial state for the
    event, including properties and a factory function for creating the
    initial state.

--- src/js/custom-battle-events/survive-super-power-with-guard/index.ts
+++ src/js/custom-battle-events/survive-super-power-with-guard/index.ts
@@ -1,13 +1,19 @@
 import { CustomBattleEvent } from "../../td-scenes/battle/custom-battle-event";
 import { EmptyCustomBattleEvent } from "../empty-custom-battle-event";
+import { SurviveSuperPowerWithGuardProps } from "./props";
+import { createSurviveSuperPowerWithGuardProps } from "./procedures/create-survive-super-power-with-guard-props";
 
 /** 「超火力はガードで凌げ」用のカスタムバトルイベント*/
 class SurviveSuperPowerWithGuard extends EmptyCustomBattleEvent {
+  /** イベントプロパティ */
+  #props: SurviveSuperPowerWithGuardProps;
+
   /**
    * コンストラクタ
    */
   constructor() {
     super();
+    this.#props = createSurviveSuperPowerWithGuardProps();
   }
 }
 
