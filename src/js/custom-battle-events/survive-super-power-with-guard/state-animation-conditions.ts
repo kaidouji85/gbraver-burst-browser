5a85e1097522b57ccdb4ca7ffcd487b45e076b65
Y.Takeuchi <kaidouji85@gmail.com>
2025-05-17 13:19:25 +0900

    StateAnimationConditionsを追加した

---
+++ src/js/custom-battle-events/survive-super-power-with-guard/state-animation-conditions.ts
@@ -0,0 +1,17 @@
+import { PlayerState } from "gbraver-burst-core";
+
+/**
+ * カスタムステートアニメーション系イベントの条件判断オブジェクト
+ * 本オブジェクトはイベント呼び出しのたびに更新される
+ */
+export type StateAnimationConditions = {
+  /** プレイヤー */
+  readonly player: PlayerState;
+  /** プレイヤーのバトル回数 */
+  readonly playerBattleCount: number;
+
+  /** 敵 */
+  readonly enemy: PlayerState;
+  /** 敵のバトル回数 */
+  readonly enemyBattleCount: number;
+};
