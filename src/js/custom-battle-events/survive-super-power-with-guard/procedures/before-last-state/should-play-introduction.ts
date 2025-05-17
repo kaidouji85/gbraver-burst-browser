8a67903c95f866c2d480dd4a49b4ca9790233d5b
Y.Takeuchi <kaidouji85@gmail.com>
2025-05-17 16:38:25 +0900

    Refactors introduction check to dedicated module
    Moves the logic for determining whether to play the introduction into a separate module for better organization and readability.

---
+++ src/js/custom-battle-events/survive-super-power-with-guard/procedures/before-last-state/should-play-introduction.ts
@@ -0,0 +1,18 @@
+import { LastState } from "../../../../td-scenes/battle/custom-battle-event";
+import { LastStateConditionContainer } from "../../last-state-condition";
+import { SurviveSuperPowerWithGuardProps } from "../../props";
+
+/**
+ * Introductionを再生するかどうかの判定
+ * @param props イベントプロパティ
+ * @returns 再生する場合はtrue
+ */
+export const shouldPlayIntroduction = (
+  props: Readonly<
+    LastState & SurviveSuperPowerWithGuardProps & LastStateConditionContainer
+  >,
+) => {
+  const { isIntroductionComplete } = props.state;
+  const { turn } = props.lastStateCondition;
+  return !isIntroductionComplete && turn === 1;
+};
