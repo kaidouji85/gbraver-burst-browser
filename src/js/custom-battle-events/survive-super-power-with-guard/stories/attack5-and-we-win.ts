0194c543d7c8ecce08f8f1bef284cceb403ca1ad
Y.Takeuchi <kaidouji85@gmail.com>
2025-05-18 17:15:30 +0900

    Adds attack5 and we win story
    Adds a new story where the attacker wins after 5 attacks.
    The story includes dialogue from the attacker expressing confidence in their victory.

---
+++ src/js/custom-battle-events/survive-super-power-with-guard/stories/attack5-and-we-win.ts
@@ -0,0 +1,22 @@
+import { wbr } from "../../../dom/wbr";
+import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
+import { activeLeftMessageWindowWithFace } from "../../active-message-window";
+import { refreshConversation } from "../../invisible-all-message-windows";
+import { scrollLeftMessages } from "../../scroll-messages";
+
+/**
+ * 5攻撃すれば勝利
+ * @param props イベントプロパティ
+ */
+export async function attack5AndWeWin(props: CustomBattleEventProps) {
+  activeLeftMessageWindowWithFace(props, "Gai");
+  await scrollLeftMessages(props, [
+    ["ガイ", `「フハハハハ 見ろ${wbr}大田高校`],
+    [`この新型の${wbr}グランドーザの${wbr}パワーなら`],
+    [`ウィングドーザなど${wbr}一撃`],
+    [`5攻撃すれば${wbr}俺たちの勝ちだ」`],
+  ]);
+  props.view.dom.leftMessageWindow.darken();
+
+  await refreshConversation(props);
+}
