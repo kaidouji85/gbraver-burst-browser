5a861a85b1babb1a6779607e87ebde90b8b40069
Y.Takeuchi <kaidouji85@gmail.com>
2025-05-18 17:19:00 +0900

    Adds "attack 5 and win" flag to state
    Adds a flag to track the completion of the "attack 5
    times and win" condition in the survive super power
    with guard custom battle event. This allows the game
    to correctly determine when this win condition is
    met and proceed accordingly.

--- src/js/custom-battle-events/survive-super-power-with-guard/state.ts
+++ src/js/custom-battle-events/survive-super-power-with-guard/state.ts
@@ -2,4 +2,6 @@
 export type SurviveSuperPowerWithGuardState = {
   /** イントロダクションが完了したか否か、trueで完了 */
   readonly isIntroductionComplete: boolean;
+  /** 「5攻撃すれば勝利」が完了したか否か、trueで完了 */
+  readonly isAttack5AndWeWinComplete: boolean;
 };
