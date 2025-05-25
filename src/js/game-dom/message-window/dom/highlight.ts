5b464a267f4c75e3d763db4481c48b65025bc458
Y.Takeuchi <kaidouji85@gmail.com>
2025-05-25 14:49:18 +0900

    Adds text highlighting to message windows
    Adds functionality to highlight specific text within message windows.
    
    This improves readability and allows for emphasis on key information presented to the user.

---
+++ src/js/game-dom/message-window/dom/highlight.ts
@@ -0,0 +1,9 @@
+import { ROOT_CLASS } from "./class-name";
+
+/**
+ * 特定の文字列をハイライトするためのHTMLを生成
+ * @param text ハイライトするテキスト
+ * @return ハイライトされたテキストを含むHTML文字列
+ */
+export const highlight = (text: string) =>
+  `<span class="${ROOT_CLASS}">${text}</span>`;
