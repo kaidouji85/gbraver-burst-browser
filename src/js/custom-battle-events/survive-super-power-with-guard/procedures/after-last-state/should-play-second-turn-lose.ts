14fe0e44417dcf9424924b4a6a2f18aee08bfed0
kaidouji85 <kaidouji85@gmail.com>
2025-06-02 04:10:11 +0000

    LastStateConditionContainerの依存を削除し、mainTurnCountを使用してSecondTurnLoseの判定を修正

--- src/js/custom-battle-events/survive-super-power-with-guard/procedures/after-last-state/should-play-second-turn-lose.ts
+++ src/js/custom-battle-events/survive-super-power-with-guard/procedures/after-last-state/should-play-second-turn-lose.ts
@@ -1,5 +1,4 @@
 import { LastStateEventProps } from "../../../../td-scenes/battle/custom-battle-event";
-import { LastStateConditionContainer } from "../../last-state-condition";
 import { SurviveSuperPowerWithGuardProps } from "../../props";
 
 /**
@@ -8,18 +7,14 @@ import { SurviveSuperPowerWithGuardProps } from "../../props";
  * @returns 再生する場合はtrue
  */
 export const shouldPlaySecondTurnLose = (
-  props: Readonly<
-    LastStateEventProps &
-      SurviveSuperPowerWithGuardProps &
-      LastStateConditionContainer
-  >,
+  props: Readonly<LastStateEventProps & SurviveSuperPowerWithGuardProps>,
 ) => {
-  const { turn, enemy } = props.lastStateCondition;
+  const { mainTurnCount, enemy } = props;
   const isEnemyWin = props.stateHistory.some(
     (s) =>
       s.effect.name === "GameEnd" &&
       s.effect.result.type === "GameOver" &&
       s.effect.result.winner === enemy.playerId,
   );
-  return turn === 2 && isEnemyWin;
+  return mainTurnCount === 2 && isEnemyWin;
 };
