0a4235c93f6c4b71afdd52ad04d03507394f1d65
kaidouji85 <kaidouji85@gmail.com>
2025-06-02 04:21:51 +0000

    StateAnimationConditionContainerの依存を削除し、メインターン数を使用して各攻撃判定を修正

--- src/js/custom-battle-events/survive-super-power-with-guard/procedures/on-state-animation/is-raito-first-attack.ts
+++ src/js/custom-battle-events/survive-super-power-with-guard/procedures/on-state-animation/is-raito-first-attack.ts
@@ -1,5 +1,4 @@
 import { CustomStateAnimationProps } from "../../../../td-scenes/battle/custom-battle-event";
-import { StateAnimationConditionContainer } from "../../state-animation-condition";
 
 /**
  * 「RaitoFirstAttack」か否かを判定する
@@ -7,13 +6,12 @@ import { StateAnimationConditionContainer } from "../../state-animation-conditio
  * @returns 判定結果、trueの場合は「RaitoFirstAttack」
  */
 export function isRaitoFirstAttack(
-  props: Readonly<CustomStateAnimationProps & StateAnimationConditionContainer>,
+  props: Readonly<CustomStateAnimationProps>,
 ): boolean {
-  const { currentState } = props;
-  const { enemy, enemyBattleCount } = props.stateAnimationCondition;
+  const { currentState, enemy, enemyMainTurnCount } = props;
   return (
     currentState.effect.name === "BatteryDeclaration" &&
     currentState.effect.attacker === enemy.playerId &&
-    enemyBattleCount === 0
+    enemyMainTurnCount === 0
   );
 }
