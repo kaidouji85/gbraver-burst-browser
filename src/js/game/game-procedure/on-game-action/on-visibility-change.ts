00c4dea74d6e76566ed4a5a6a40fec44eeeee627
Y.Takeuchi <kaidouji85@gmail.com>
2025-06-07 00:07:36 +0900

    onHiddenおよびonVisible関数のログ出力をHowler.ctxを含むように更新

--- src/js/game/game-procedure/on-game-action/on-visibility-change.ts
+++ src/js/game/game-procedure/on-game-action/on-visibility-change.ts
@@ -7,7 +7,7 @@ import { GameProps } from "../../game-props";
  * visibilityState = "hidden" の場合の処理
  */
 const onHidden = () => {
-  console.log("visibilityState = hidden"); // TODO: ログ出力を削除する
+  console.log("visibilityState = hidden", Howler.ctx); // TODO: ログ出力を削除する
   Howler.mute(true);
 };
 
@@ -15,23 +15,8 @@ const onHidden = () => {
  * visibilityState = "visible" の場合の処理
  */
 const onVisible = () => {
-  console.log("visibilityState = visible"); // TODO: ログ出力を削除する
+  console.log("visibilityState = visible", Howler.ctx); // TODO: ログ出力を削除する
   Howler.mute(false);
-  if (Howler.ctx && typeof Howler.ctx.resume === "function") {
-    Howler.ctx.resume().catch(() => {
-      // resume失敗時はユーザー操作で再試行
-      const tryResume = () => {
-        Howler.ctx.resume().then(() => {
-          // ここで再生中の音があれば再度play()を呼ぶ必要がある場合はここで呼ぶ
-        }).finally(() => {
-          document.body.removeEventListener("touchend", tryResume);
-          document.body.removeEventListener("mousedown", tryResume);
-        });
-      };
-      document.body.addEventListener("touchend", tryResume, { once: true });
-      document.body.addEventListener("mousedown", tryResume, { once: true });
-    });
-  }
 };
 
 /** オプション */
