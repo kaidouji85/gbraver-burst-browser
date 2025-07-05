9ece9d88e604e2851f706277eb3369d0d47e8f00
Y.Takeuchi <kaidouji85@gmail.com>
2025-07-05 19:57:50 +0900

    更新: ゼロ防御時のバッテリー残量に関する新しいイベントを追加

--- src/js/custom-battle-events/survive-super-power-with-guard/stories/zero-defense-but-positive-battery.ts
+++ src/js/custom-battle-events/survive-super-power-with-guard/stories/zero-defense-but-positive-battery.ts
@@ -1,17 +1,22 @@
 import { wbr } from "../../../dom/wbr";
 import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
 import { activeLeftMessageWindowWithFace } from "../../active-message-window";
+import { invisibleAllMessageWindows } from "../../invisible-all-message-windows";
 import { scrollLeftMessages } from "../../scroll-messages";
 
 /**
- * 地に落ちたもんやな
+ * バッテリーが残っているのに0防御した
  * @param props イベントプロパティ
  */
-export async function chiniOchita(props: CustomBattleEventProps) {
+export async function zeroDefenseButPositiveBattery(
+  props: CustomBattleEventProps,
+) {
   activeLeftMessageWindowWithFace(props, "Raito");
   await scrollLeftMessages(props, [
-    ["ライト", `「なんや もう${wbr}終わり${wbr}かいな`],
-    [`全国２位も${wbr}地に落ちた${wbr}もんやな」`],
+    ["ライト", `「どないした まだバッテリーが${wbr}残っとるや${wbr}ないか`],
+    [`わいの${wbr}強さに${wbr}恐れを${wbr}なしたか ツバサ」`],
   ]);
   props.view.dom.leftMessageWindow.darken();
+
+  invisibleAllMessageWindows(props);
 }
