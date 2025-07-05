cafb09697278bbf1d36629693fed59b223176810
Y.Takeuchi <kaidouji85@gmail.com>
2025-07-05 20:32:59 +0900

    更新: チニオチタイベントを削除し、ファーストターン敗北時のメッセージ処理を統合

--- src/js/custom-battle-events/survive-super-power-with-guard/stories/player-lose.ts
+++ src/js/custom-battle-events/survive-super-power-with-guard/stories/player-lose.ts
@@ -1,12 +1,20 @@
+import { wbr } from "../../../dom/wbr";
 import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
+import { activeLeftMessageWindowWithFace } from "../../active-message-window";
 import { invisibleAllMessageWindows } from "../../invisible-all-message-windows";
-import { chiniOchita } from "./chini-ochita";
+import { scrollLeftMessages } from "../../scroll-messages";
 
 /**
  * プレイヤーが負けた
  * @param props イベントプロパティ
  */
 export async function playerLose(props: CustomBattleEventProps) {
-  await chiniOchita(props);
+  activeLeftMessageWindowWithFace(props, "Raito");
+  await scrollLeftMessages(props, [
+    ["ライト", `「東日本ブロック${wbr}での借りは${wbr}返したで ツバサ`],
+    [`これが わいの${wbr}実力なんや」`],
+  ]);
+  props.view.dom.leftMessageWindow.darken();
+
   invisibleAllMessageWindows(props);
 }
