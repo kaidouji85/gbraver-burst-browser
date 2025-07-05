40fb1f72b380f573077b1225e86132f641d93c9d
Y.Takeuchi <kaidouji85@gmail.com>
2025-07-05 17:04:19 +0900

    更新: ツバサの勝利イベントでメッセージのスクロール方向を右から左に変更

--- src/js/custom-battle-events/survive-super-power-with-guard/stories/tsubasa-victory.ts
+++ src/js/custom-battle-events/survive-super-power-with-guard/stories/tsubasa-victory.ts
@@ -2,7 +2,7 @@ import { wbr } from "../../../dom/wbr";
 import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
 import { activeLeftMessageWindowWithFace } from "../../active-message-window";
 import { invisibleAllMessageWindows } from "../../invisible-all-message-windows";
-import { scrollRightMessages } from "../../scroll-messages";
+import { scrollLeftMessages } from "../../scroll-messages";
 
 /**
  * ツバサの勝利
@@ -10,12 +10,10 @@ import { scrollRightMessages } from "../../scroll-messages";
  */
 export async function tsubasaVictory(props: CustomBattleEventProps) {
   activeLeftMessageWindowWithFace(props, "Gai");
-  await scrollRightMessages(props, [
-    [
-      "ガイ",
-      `「ライト先輩と${wbr}新型でも 大田高校に${wbr}勝てない${wbr}なんて`,
-    ],
-    [`かくなる上は${wbr}アレを${wbr}出すしか${wbr}ない`],
+  await scrollLeftMessages(props, [
+    ["ガイ", `「そんな……`],
+    [`「ライト先輩でも 大田高校に${wbr}勝てない${wbr}なんて`],
+    [`かくなる上は アレを${wbr}出すしか${wbr}ない`],
   ]);
   props.view.dom.leftMessageWindow.darken();
 
