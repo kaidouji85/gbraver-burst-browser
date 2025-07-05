d2dd34072b0f026739997b3d2bd8cb8b1afdae19
Y.Takeuchi <kaidouji85@gmail.com>
2025-07-05 20:06:34 +0900

    新規: ゼロ防御時にバーストを発動できるイベントを追加

---
+++ src/js/custom-battle-events/survive-super-power-with-guard/stories/zero-defense-but-enable-burst.ts
@@ -0,0 +1,20 @@
+import { wbr } from "../../../dom/wbr";
+import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
+import { activeLeftMessageWindowWithFace } from "../../active-message-window";
+import { invisibleAllMessageWindows } from "../../invisible-all-message-windows";
+import { scrollLeftMessages } from "../../scroll-messages";
+
+/**
+ * バースト出来るのに0防御した
+ * @param props イベントプロパティ
+ */
+export async function zeroDefenseButEnableBurst(props: CustomBattleEventProps) {
+  activeLeftMessageWindowWithFace(props, "Raito");
+  await scrollLeftMessages(props, [
+    ["ライト", `「なんで${wbr}バーストを${wbr}発動せぇへん${wbr}のや`],
+    [`勝負を${wbr}捨てたか ツバサ」`],
+  ]);
+  props.view.dom.leftMessageWindow.darken();
+
+  invisibleAllMessageWindows(props);
+}
