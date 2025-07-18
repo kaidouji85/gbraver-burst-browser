bf8a81de6c4fa49fb557259a195e040217b70b61
Y.Takeuchi <kaidouji85@gmail.com>
2025-07-18 10:07:34 +0900

    チュートリアル開始前の同意アクションを追加し、GameActionタイプに統合

--- src/js/game/game-actions/index.ts
+++ src/js/game/game-actions/index.ts
@@ -29,6 +29,7 @@ import { SelectionCancel } from "./selection-cancel";
 import { SelectionComplete } from "./selection-complete";
 import { StoryStart } from "./story-start";
 import { SuddenlyBattleEnd } from "./suddenly-battle-end";
+import { TutorialConsent } from "./tutorial-consent";
 import { TutorialStart } from "./tutorial-start";
 import { UnhandledRejection } from "./unhandled-rejection";
 import { UniversalLogin } from "./universal-login";
@@ -67,6 +68,7 @@ export type GameAction =
   | ConfigChangeStart
   | ConfigChangeComplete
   | ConfigChangeCancel
+  | TutorialConsent
   | TutorialStart
   | StoryStart
   | CancelTutorialSelect
