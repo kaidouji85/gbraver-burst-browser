f9b512d7b65e5b4b24b6775b56911705c0d00742
Y.Takeuchi <kaidouji85@gmail.com>
2025-08-30 19:48:24 +0900

    ステータスダイアログの状態管理を改善し、必要な依存関係をインポートしました

--- stories/status-dialog.stories.ts
+++ stories/status-dialog.stories.ts
@@ -1,4 +1,12 @@
-import { ArmdozerIds, Armdozers, PilotIds, Pilots } from "gbraver-burst-core";
+import {
+  ArmdozerIds,
+  Armdozers,
+  createPlayerState,
+  EMPTY_ARMDOZER,
+  EMPTY_PILOT,
+  PilotIds,
+  Pilots,
+} from "gbraver-burst-core";
 
 import { StatusDialog } from "../src/js/dom-dialogs/status";
 import { domStub } from "./stub/dom-stub";
@@ -9,12 +17,10 @@ export default {
 
 /** ステータスダイアログ */
 export const statusDialog = domStub(() => {
-  const armdozer = Armdozers.find((a) => a.id === ArmdozerIds.SHIN_BRAVER);
-  const pilot = Pilots.find((p) => p.id === PilotIds.SHINYA);
-  
-  const dialog = new StatusDialog({
-    armdozer,
-    pilot,
-  });
+  const armdozer =
+    Armdozers.find((a) => a.id === ArmdozerIds.SHIN_BRAVER) ?? EMPTY_ARMDOZER;
+  const pilot = Pilots.find((p) => p.id === PilotIds.SHINYA) ?? EMPTY_PILOT;
+  const state = createPlayerState({ playerId: "test-player", armdozer, pilot });
+  const dialog = new StatusDialog({ state });
   return dialog.getRootHTMLElement();
 });
