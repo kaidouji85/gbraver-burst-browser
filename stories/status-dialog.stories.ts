7b60c2726ae6c6225a4372eaa068036fb3e01c8e
Y.Takeuchi <kaidouji85@gmail.com>
2025-08-31 17:54:55 +0900

    ステータスダイアログのストーリー生成関数を追加し、オプションを受け取るように修正しました。また、シンブレイバーのストーリーを更新しました

--- stories/status-dialog.stories.ts
+++ stories/status-dialog.stories.ts
@@ -1,11 +1,14 @@
 import {
+  ArmdozerId,
   ArmdozerIds,
   Armdozers,
   createPlayerState,
   EMPTY_ARMDOZER,
   EMPTY_PILOT,
+  PilotId,
   PilotIds,
   Pilots,
+  PlayerState,
 } from "gbraver-burst-core";
 
 import { StatusDialog } from "../src/js/dom-dialogs/status";
@@ -15,21 +18,45 @@ export default {
   title: "status-dialog",
 };
 
-/** ステータスダイアログ */
-export const statusDialog = domStub((options) => {
-  const armdozer =
-    Armdozers.find((a) => a.id === ArmdozerIds.SHIN_BRAVER) ?? EMPTY_ARMDOZER;
-  const pilot = Pilots.find((p) => p.id === PilotIds.SHINYA) ?? EMPTY_PILOT;
-  const origin = createPlayerState({
-    playerId: "test-player",
-    armdozer,
-    pilot,
+/**
+ * ステータスダイアログのストーリーを生成する
+ * @param options オプション
+ * @returns ストーリー
+ */
+const story = (options: {
+  /** アームドーザID */
+  armdozerId: ArmdozerId;
+  /** パイロットID */
+  pilotId: PilotId;
+  /**
+   * ステート変更関数
+   * @param origin オリジナル
+   * @returns 変更したステート
+   */
+  fn?: (origin: PlayerState) => PlayerState;
+}) =>
+  domStub((stubOptions) => {
+    const { armdozerId, pilotId, fn } = options;
+    const armdozer =
+      Armdozers.find((a) => a.id === armdozerId) ?? EMPTY_ARMDOZER;
+    const pilot = Pilots.find((p) => p.id === pilotId) ?? EMPTY_PILOT;
+    const origin = createPlayerState({
+      playerId: "test-player",
+      armdozer,
+      pilot,
+    });
+    const state = fn?.(origin) ?? origin;
+    const dialog = new StatusDialog({ ...stubOptions, state });
+    return dialog.getRootHTMLElement();
   });
-  const state = {
+
+/** シンブレイバー + シンヤ */
+export const shinBraverShinya = story({
+  armdozerId: ArmdozerIds.SHIN_BRAVER,
+  pilotId: PilotIds.SHINYA,
+  fn: (origin) => ({
     ...origin,
     armdozer: { ...origin.armdozer, enableBurst: true },
     pilot: { ...origin.pilot, enableSkill: false },
-  };
-  const dialog = new StatusDialog({ ...options, state });
-  return dialog.getRootHTMLElement();
+  }),
 });
