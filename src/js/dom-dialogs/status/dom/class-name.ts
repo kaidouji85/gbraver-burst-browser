edeac1b43e8ccdce3ed6d258d99515f63937ed56
Y.Takeuchi <kaidouji85@gmail.com>
2025-09-07 21:48:26 +0900

    アームドーザーパワーの強化状態を反映するクラスを追加し、関連するスタイルを更新

--- src/js/dom-dialogs/status/dom/class-name.ts
+++ src/js/dom-dialogs/status/dom/class-name.ts
@@ -1,6 +1,12 @@
 /** ルート要素のclass属性 */
 export const ROOT = "status";
 
+/** アームドーザーパワーの値のclass属性 */
+export const ARMDOZER_POWER_VALUE = `${ROOT}__armdozer-power-value`;
+
+/** アームドーザーパワーが強化されている場合の値のclass属性 */
+export const ARMDOZER_POWER_VALUE_BUFFED = `${ARMDOZER_POWER_VALUE}--buffed`;
+
 /** 発動可能ボックスのclass属性 */
 export const AVAILABLE_BOX = `${ROOT}__available-box`;
 
