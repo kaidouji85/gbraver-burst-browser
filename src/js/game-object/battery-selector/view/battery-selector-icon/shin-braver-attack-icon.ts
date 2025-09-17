ff98cbcf491c5c8d410efca9c1b3ad8d9f9c3c68
Y.Takeuchi <kaidouji85@gmail.com>
2025-09-18 08:27:21 +0900

    シンブレイバー攻撃アイコンの生成処理を修正し、アウトラインメッシュを追加

--- src/js/game-object/battery-selector/view/battery-selector-icon/shin-braver-attack-icon.ts
+++ src/js/game-object/battery-selector/view/battery-selector-icon/shin-braver-attack-icon.ts
@@ -1,8 +1,15 @@
+import * as THREE from "three";
+
 import { HorizontalAnimationMesh } from "../../../../mesh/horizontal-animation";
 import { Resources } from "../../../../resource";
 import { findTextureOrThrow } from "../../../../resource/find-texture-or-throw";
 import { TEXTURE_IDS } from "../../../../resource/texture/ids";
+import { createOutlineSilhouetteTexture } from "../../../../texture/create-outline-silhouette-texture";
 import { BatterySelectorIcon } from "./battery-selector-icon";
+
+const MESH_SIZE = 600;
+
+const OUTLINE_SIZE = MESH_SIZE + 70;
 
 /**
  * シンブレイバー攻撃アイコンを生成する
@@ -12,6 +19,8 @@ import { BatterySelectorIcon } from "./battery-selector-icon";
 export const shinBraverAttackIcon = (
   resources: Resources,
 ): BatterySelectorIcon => {
+  const group = new THREE.Group();
+
   const texture = findTextureOrThrow(
     resources,
     TEXTURE_IDS.SHIN_BRAVER_SP_ATTACK,
@@ -19,20 +28,36 @@ export const shinBraverAttackIcon = (
   const mesh = new HorizontalAnimationMesh({
     texture,
     maxAnimation: 4,
-    width: 600,
-    height: 600,
+    width: MESH_SIZE,
+    height: MESH_SIZE,
   });
-  const position = { x: 0, y: 100 };
   mesh.animate(1);
+  group.add(mesh.getObject3D());
+
+  const outlineTexture = createOutlineSilhouetteTexture({
+    texture,
+    color: { r: 255, g: 0, b: 255 },
+  });
+  const outlineMesh = new HorizontalAnimationMesh({
+    texture: outlineTexture,
+    maxAnimation: 4,
+    width: OUTLINE_SIZE,
+    height: OUTLINE_SIZE,
+  });
+  outlineMesh.animate(1);
+  outlineMesh.getObject3D().position.set(0, 0, -0.01);
+  group.add(outlineMesh.getObject3D());
 
   return {
     destructor: () => {
       mesh.destructor();
+      outlineMesh.destructor();
     },
-    getObject3D: () => mesh.getObject3D(),
+    getObject3D: () => group,
     opacity: (value: number) => {
       mesh.opacity(value);
+      outlineMesh.opacity(value);
     },
-    position,
+    position: { x: 0, y: 100 },
   };
 };
