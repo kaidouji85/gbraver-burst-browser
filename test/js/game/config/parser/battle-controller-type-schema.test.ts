bca8f586599bac9055e1566c8429dc448cb6c9fd
Y.Takeuchi <kaidouji85@gmail.com>
2025-10-20 14:45:45 +0900

    refactor: 戦闘シーンコントローラータイプのパース機能を削除し、テストを追加

--- test/js/game/config/parser/battle-controller-type-schema.test.ts
+++ test/js/game/config/parser/battle-controller-type-schema.test.ts
@@ -1,28 +1,28 @@
-import { parseBattleControllerType } from "../../../../../src/js/game/config/parser/battle-controller-type";
+import { BattleControllerTypeSchema } from "../../../../../src/js/game/config/parser/battle-controller-type";
 import { BattleControllerTypes } from "../../../../../src/js/td-scenes/battle/controller-type";
 
 test("BattleControllerTypesは正しくパースできる", () => {
   BattleControllerTypes.forEach((v) => {
-    expect(parseBattleControllerType(v)).toBe(v);
+    expect(BattleControllerTypeSchema.parse(v)).toBe(v);
   });
 });
 
 test("不正な文字列はパースできない", () => {
   const data = "NoDefinedControllerType";
-  expect(parseBattleControllerType(data)).toBe(null);
+  expect(() => BattleControllerTypeSchema.parse(data)).toThrow();
 });
 
 test("空文字はパースできない", () => {
   const data = "";
-  expect(parseBattleControllerType(data)).toBe(null);
+  expect(() => BattleControllerTypeSchema.parse(data)).toThrow();
 });
 
 test("nullはパースできない", () => {
   const data = null;
-  expect(parseBattleControllerType(data)).toBe(null);
+  expect(() => BattleControllerTypeSchema.parse(data)).toThrow();
 });
 
 test("undefinedはパースできない", () => {
   const data = undefined;
-  expect(parseBattleControllerType(data)).toBe(null);
+  expect(() => BattleControllerTypeSchema.parse(data)).toThrow();
 });
