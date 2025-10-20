c5eac3bd7278ad8fe3c1b337bd06b233abc8bbc2
Y.Takeuchi <kaidouji85@gmail.com>
2025-10-20 14:26:51 +0900

    fix: プレイヤー側パイロットの表示設定オプション情報を整理し、説明を追加

--- src/js/dom-scenes/config/dom/player-pilot-visibility-options.ts
+++ src/js/dom-scenes/config/dom/player-pilot-visibility-options.ts
@@ -5,16 +5,23 @@ import {
 import { ROOT_CLASS } from "./class-name";
 import template from "./player-pilot-visibility-option.hbs";
 
+/** 「visible」のオプション情報 */
 const visible = {
   label: "表示する",
   description: `常にプレイヤー側のパイロット情報を表示します。`,
 };
 
+/** 「hidden」のオプション情報 */
 const hidden = {
   label: "表示しない",
   description: `スキル未発動時にはプレイヤー側のパイロット情報を表示しません。ゲーム画面を動画配信する際に、視聴者に配信者が選択したパイロットを悟られません。 `,
 };
 
+/**
+ * PlayerPilotVisibilityに応じたオプション情報を取得する
+ * @param visibility プレイヤーパイロット表示設定
+ * @returns 取得したオプション情報
+ */
 const getOptions = (visibility: PlayerPilotVisibility) => {
   switch (visibility) {
     case "hidden":
