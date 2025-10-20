e1c43cd30daf9e85b87461764d657e44b56e4244
Y.Takeuchi <kaidouji85@gmail.com>
2025-10-20 13:09:11 +0900

    feat: プレイヤーパイロット表示設定の詳細情報を追加し、オプションの取得処理を更新

--- src/js/dom-scenes/config/dom/player-pilot-visibility-options.ts
+++ src/js/dom-scenes/config/dom/player-pilot-visibility-options.ts
@@ -5,6 +5,26 @@ import {
 import { ROOT_CLASS } from "./class-name";
 import template from "./player-pilot-visibility-option.hbs";
 
+const visible = {
+  label: "表示する",
+  description: `常にプレイヤー側のパイロット情報を表示します。`,
+};
+
+const hidden = {
+  label: "表示しない",
+  description: `スキル未発動時にはプレイヤー側のパイロット情報を表示しません。ゲーム画面を動画配信する際に、視聴者に配信者が選択したパイロットを悟られません。 `,
+};
+
+const getOptions = (visibility: PlayerPilotVisibility) => {
+  switch (visibility) {
+    case "hidden":
+      return hidden;
+    case "visible":
+    default:
+      return visible;
+  }
+};
+
 /**
  * プレイヤーパイロット表示設定のラジオボタン要素HTMLを生成する
  * @param selected 初期に選択されているプレイヤーパイロット表示設定
@@ -13,12 +33,13 @@ import template from "./player-pilot-visibility-option.hbs";
 export const playerPilotVisibilityOptions = (selected: PlayerPilotVisibility) =>
   PlayerPilotVisibilities.map((v) => {
     const checked = v === selected ? "checked" : "";
-    const label = v === "visible" ? "表示する" : "表示しない";
-    const value = v;
+    const options = getOptions(v);
+    const { label, description } = options;
     return template({
       ROOT_CLASS,
       checked,
       label,
-      value,
+      value: v,
+      description,
     });
   }).reduce((a, b) => a + b);
