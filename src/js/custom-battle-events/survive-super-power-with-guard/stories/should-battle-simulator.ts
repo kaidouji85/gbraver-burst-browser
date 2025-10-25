e9b854e4d8e8e560c958e827f0a3f8c6e23fbd0a
Y.Takeuchi <kaidouji85@gmail.com>
2025-10-25 10:38:49 +0900

    feat: バトルシミュレーターのダメージ計算機能を追加

---
+++ src/js/custom-battle-events/survive-super-power-with-guard/stories/should-battle-simulator.ts
@@ -0,0 +1,19 @@
+import { wbr } from "../../../dom/wbr";
+import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
+import { activeRightMessageWindowWithFace } from "../../active-message-window";
+import { invisibleAllMessageWindows } from "../../invisible-all-message-windows";
+import { scrollRightMessages } from "../../scroll-messages";
+
+/**
+ * バトルシミュレーターでダメージ計算
+ * @param props イベントプロパティ
+ */
+export async function shouldBattleSimulator(props: CustomBattleEventProps) {
+  activeRightMessageWindowWithFace(props, "Tsubasa");
+  await scrollRightMessages(props, [
+    ["ツバサ", `「嫌な予感がする`],
+    [`念のために${wbr}ダメージ計算を${wbr}しておこう」`],
+  ]);
+  props.view.dom.rightMessageWindow.darken();
+  invisibleAllMessageWindows(props);
+}
