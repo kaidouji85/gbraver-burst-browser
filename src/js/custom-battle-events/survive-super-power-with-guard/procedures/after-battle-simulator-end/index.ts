518282d6d4905de54b8649f7577222044627b770
Y.Takeuchi <kaidouji85@gmail.com>
2025-10-25 18:03:45 +0900

    feat: バトルシミュレーター終了後のイベント処理を整理し、プレイヤー死亡判定を新しいメソッドに分離

---
+++ src/js/custom-battle-events/survive-super-power-with-guard/procedures/after-battle-simulator-end/index.ts
@@ -0,0 +1,30 @@
+import { BattleSimulatorEventProps } from "../../../../td-scenes/battle/custom-battle-event";
+import { SurviveSuperPowerWithGuardProps } from "../../props";
+import { SurviveSuperPowerWithGuardState } from "../../state";
+import { willNotSurviveCurrentBattery } from "../../stories/will-not-survive-current-battery";
+import { willSurviveCurrentBattery } from "../../stories/will-survive-current-battery";
+import { willPlayerDieWithCurrentBattery } from "./will-player-die-with-current-battery";
+
+/**
+ * バトルシミュレーターが閉じられた後のイベント処理
+ * @param props イベントプロパティ
+ * @returns 処理が完了したら発火するPromise
+ */
+export async function afterBattleSimulatorEnd(
+  props: Readonly<BattleSimulatorEventProps & SurviveSuperPowerWithGuardProps>,
+): Promise<SurviveSuperPowerWithGuardState> {
+  let { state } = props;
+
+  const shouldPlaySurviveEvent =
+    state.isUseBattleSimulatorComplete && !state.isSurviveCheckComplete;
+  const willPlayerDie = willPlayerDieWithCurrentBattery(props);
+  if (shouldPlaySurviveEvent && !willPlayerDie) {
+    await willSurviveCurrentBattery(props);
+    state = { ...state, isSurviveCheckComplete: true };
+  } else if (shouldPlaySurviveEvent && willPlayerDie) {
+    await willNotSurviveCurrentBattery(props);
+    state = { ...state, isSurviveCheckComplete: true };
+  }
+
+  return state;
+}
