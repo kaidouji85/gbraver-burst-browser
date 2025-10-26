b679c83c885c88219bd2c81581bab5600df9a167
Y.Takeuchi <kaidouji85@gmail.com>
2025-10-26 14:44:24 +0900

    fix: バッテリーセレクタ数字のスケール計算関数の定義を簡素化

--- src/js/game-object/battery-selector/view/battery-number.ts
+++ src/js/game-object/battery-selector/view/battery-number.ts
@@ -53,8 +53,6 @@ export function batteryNumberPosition(
  * @param maxBattery
  * @returns 計算結果
  */
-export function batteryNumberScale(
-  maxBattery: number,
-): number {
+export function batteryNumberScale(maxBattery: number): number {
   return maxBattery <= 5 ? 1 : 0.8;
 }
