9d5678d213f170abd9bd49fadd753c84289b60c0
Y.Takeuchi <kaidouji85@gmail.com>
2025-11-01 16:17:45 +0900

    feat: ０攻撃の推奨機能を追加

---
+++ src/js/custom-battle-events/survive-super-power-with-guard/stories/recommend-zero-attack.ts
@@ -0,0 +1,28 @@
+import { wbr } from "../../../dom/wbr";
+import { highlight } from "../../../game-dom/message-window/dom/highlight";
+import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
+import { waitTime } from "../../../wait/wait-time";
+import { activeRightMessageWindowWithFace } from "../../active-message-window";
+import { invisibleAllMessageWindows } from "../../invisible-all-message-windows";
+import { scrollRightMessages } from "../../scroll-messages";
+import { SurviveSuperPowerWithGuardProps } from "../props";
+
+/**
+ * ０攻撃の推奨
+ * @param props イベントプロパティ
+ * @return 処理が完了したら発火するPromise
+ */
+export async function recommendZeroAttack(
+  props: Readonly<CustomBattleEventProps & SurviveSuperPowerWithGuardProps>,
+) {
+  await waitTime(200);
+  activeRightMessageWindowWithFace(props, "Tsubasa");
+  await scrollRightMessages(props, [
+    ["ツバサ", `「あの図体${wbr}での攻撃 まともに${wbr}くらえば${wbr}即死だな`],
+    [
+      `ならば${wbr}${highlight("0攻撃")}${wbr}して 次のターンの${wbr}${highlight(`防御バッテリーを${wbr}確保しよう`)}」`,
+    ],
+  ]);
+  props.view.dom.rightMessageWindow.darken();
+  invisibleAllMessageWindows(props);
+}
