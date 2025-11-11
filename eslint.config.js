a036316afb01df769ae02142144a17def6ae5e06
Y.Takeuchi <kaidouji85@gmail.com>
2025-11-11 22:45:50 +0900

    fix: eslint-plugin-storybookを削除し、依存関係を整理

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 const globals = require("globals");
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
