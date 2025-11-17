da4dd5ab7ece9af6c0c153b8b85267c2f6dcd623
Y.Takeuchi <kaidouji85@gmail.com>
2025-11-17 10:30:17 +0900

    fix: ガイの叫びアニメーションのバーストおよびパイロットスキル判定を直接条件に変更

--- src/js/custom-battle-events/pilot-skill-tutorial-02/procedures/on-state-animation/gai-shout.ts
+++ src/js/custom-battle-events/pilot-skill-tutorial-02/procedures/on-state-animation/gai-shout.ts
@@ -1,7 +1,5 @@
 import { CustomStateAnimationProps } from "../../../../td-scenes/battle/custom-battle-event";
 import { ConditionalAnimation } from "../../../get-animation-if-conditional-met";
-import { isPlayerBurstActivatedFromCurrentState } from "../../../is-burst-activated";
-import { isPlayerPilotSkillActivatedFromCurrentState } from "../../../is-pilot-skill-activated";
 import { gaiBurst } from "../../animation/ga-burst";
 import { gaiFinishBlow } from "../../animation/gai-finish-blow";
 import { gaiPilotSkill } from "../../animation/gai-pilot-skill";
@@ -12,9 +10,13 @@ export const gaiShout: ConditionalAnimation<
   CustomStateAnimationProps & PilotSkillTutorial02Props
 >[] = [
   (props) =>
-    isPlayerBurstActivatedFromCurrentState(props) ? gaiBurst(props) : null,
+    props.currentState.effect.name === "BurstEffect" &&
+    props.currentState.effect.burstPlayer === props.playerId
+      ? gaiBurst(props)
+      : null,
   (props) =>
-    isPlayerPilotSkillActivatedFromCurrentState(props)
+    props.currentState.effect.name === "PilotSkillEffect" &&
+    props.currentState.effect.invokerId === props.playerId
       ? gaiPilotSkill(props)
       : null,
   (props) => {
