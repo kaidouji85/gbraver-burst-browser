0a9c45b138568797d5f5738261d3a633b3adc3b8
Y.Takeuchi <kaidouji85@gmail.com>
2025-11-17 10:18:45 +0900

    feat: ガイ バースト叫びのアニメーションを追加

---
+++ src/js/custom-battle-events/pilot-skill-tutorial-02/animation/ga-burst.ts
@@ -0,0 +1,18 @@
+import { onStart } from "../../../animation/on-start";
+import { wbr } from "../../../dom/wbr";
+import { CustomBattleEventProps } from "../../../td-scenes/battle/custom-battle-event";
+import { playerPilotOnlyShout } from "../../pilot-shout";
+
+/**
+ * ガイ バースト 叫び
+ * @param props イベントプロパティ
+ * @returns アニメーション
+ */
+export const gaiBurst = (props: Readonly<CustomBattleEventProps>) =>
+  onStart(() => {
+    playerPilotOnlyShout(
+      props,
+      "Gai",
+      `これが${wbr}シンブレイバーの${wbr}バースト${wbr}か`,
+    );
+  });
