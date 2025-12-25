bd218ba0716da4de22737f37d59fdc53076da6b4
Y.Takeuchi <kaidouji85@gmail.com>
2025-12-25 09:28:33 +0900

    BGMの停止処理を追加し、アニメーションの流れを改善。勝利時のBGM再生を整理。

--- src/js/td-scenes/battle/animation/game-state/game-end/game-over/neo-landozer.ts
+++ src/js/td-scenes/battle/animation/game-state/game-end/game-over/neo-landozer.ts
@@ -32,10 +32,12 @@ export function neoLandozerWin(param: GameOverParamX<NeoLandozerTD>): Animate {
     focusToNeoLandozer(param),
     param.winnerHUD.resultIndicator
       .slideIn()
+      .chain(delay(500))
       .chain(
-        delay(500),
-        onStart(() => param.bgm.do(play(param.battleEndBGM))),
-      )
-      .chain(param.winnerHUD.resultIndicator.moveToEdge()),
+        all(
+          param.winnerHUD.resultIndicator.moveToEdge(),
+          onStart(() => param.bgm.do(play(param.battleEndBGM))),
+        ),
+      ),
   );
 }
