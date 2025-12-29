ef5322eda42616b19218a287bb3da08c657ca100
Y.Takeuchi <kaidouji85@gmail.com>
2025-12-29 14:39:57 +0900

    非表示アニメーションと表示アニメーションのコードを簡素化し、不要なインポートを削除しました。

--- src/js/game-object/turn-start/animation/show.ts
+++ src/js/game-object/turn-start/animation/show.ts
@@ -1,37 +1,20 @@
-import { all } from "../../../animation/all";
+import { Easing } from "@tweenjs/tween.js";
+
 import { Animate } from "../../../animation/animate";
-import { onStart } from "../../../animation/on-start";
 import { tween } from "../../../animation/tween";
-import type { TurnStartModel } from "../model/turn-start-model";
+import { TurnStartModel } from "../model/turn-start-model";
 
 /**
  * 表示アニメーション
- *
  * @param model モデル
  * @returns アニメーション
  */
 export function show(model: TurnStartModel): Animate {
-  return onStart(() => {
-    model.opacity = 0;
-    model.position.x = 200;
-  }).chain(
-    all(
-      tween(model, (t) =>
-        t.to(
-          {
-            opacity: 1,
-          },
-          400,
-        ),
-      ),
-      tween(model.position, (t) =>
-        t.to(
-          {
-            x: 0,
-          },
-          400,
-        ),
-      ),
+  return tween(model, (t) =>
+    t.to({ opacity: 1, position: { x: 100 } }, 0),
+  ).chain(
+    tween(model.position, (t) =>
+      t.to({ x: 0 }, 400).easing(Easing.Quadratic.Out),
     ),
   );
 }
