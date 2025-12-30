6aab663ef3324675976c9924443e7cea5b3f39ab
Y.Takeuchi <kaidouji85@gmail.com>
2025-12-30 09:40:46 +0900

    スライドインアニメーションのローカル座標を修正し、アニメーションの持続時間を統一しました

--- src/js/game-object/result-indicator/animation/slide-in-to-edge.ts
+++ src/js/game-object/result-indicator/animation/slide-in-to-edge.ts
@@ -10,17 +10,18 @@ import { ResultIndicatorModel } from "../model/result-indicator-model";
  * @returns アニメーション
  */
 export function slideInToEdge(model: ResultIndicatorModel): Animate {
+  const duration = 400;
   return onStart(() => {
     model.opacity = 0;
     model.worldCoordinate.x = -1;
     model.worldCoordinate.y = 1;
-    model.localCoordinate.x = -30;
+    model.localCoordinate.x = 30;
     model.localCoordinate.y = 0;
     model.scale = 1;
   }).chain(
     all(
-      tween(model.localCoordinate, (t) => t.to({ x: 0 }, 200)),
-      tween(model, (t) => t.to({ opacity: 1 }, 200)),
+      tween(model.localCoordinate, (t) => t.to({ x: 0 }, duration)),
+      tween(model, (t) => t.to({ opacity: 1 }, duration)),
     ),
   );
 }
