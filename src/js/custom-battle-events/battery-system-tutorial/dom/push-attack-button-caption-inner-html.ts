51b75c1f3dc0f1ba21ea0e979c9854f34781f1c7
kaidouji85 <kaidouji85@gmail.com>
2026-01-30 13:42:37 +0900

    コウゲキボタンを押すキャプションのテンプレートインポートパスを修正

--- src/js/custom-battle-events/battery-system-tutorial/dom/push-attack-button-caption-inner-html.ts
+++ src/js/custom-battle-events/battery-system-tutorial/dom/push-attack-button-caption-inner-html.ts
@@ -1,5 +1,5 @@
 import { ROOT_CLASS as MESSAGE_WINDOW_ROOT } from "../../../game-dom/message-window/dom/class-name";
-import template from "./push-attack-button-caption-inner.hbs";
+import template from "./push-attack-button-caption-inner-html.hbs";
 
 /**
  * コウゲキボタンを押すキャプションのinnerHTMLを生成する
