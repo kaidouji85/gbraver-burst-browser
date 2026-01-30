c7fc27785a2343407e4c1fa4d12edfa2aac51291
kaidouji85 <kaidouji85@gmail.com>
2026-01-30 15:42:49 +0900

    リソースを色々と復帰させた

--- src/js/custom-battle-events/battery-system-tutorial/props.ts
+++ src/js/custom-battle-events/battery-system-tutorial/props.ts
@@ -1,4 +1,5 @@
 import { Resources } from "../../resource";
+import { attackBatteryCaptionInnerHtml } from "./dom/attack-battery-caption-inner-html";
 import { changeBatteryCaptionInnerHtml } from "./dom/change-battery-caption-inner-html";
 import { defenseBatteryCaptionInnerHtml } from "./dom/defense-battery-caption-inner-html";
 import { pushAttackButtonCaptionInnerHtml } from "./dom/push-attack-button-caption-inner-html";
@@ -8,10 +9,12 @@ import { BatterySystemTutorialState } from "./state";
 export type BatterySystemTutorialProps = {
   /** チュートリアルのステート */
   eventState: BatterySystemTutorialState;
-  /** バッテリー値変更キャプション innerHTML */
+  /** @deprecated バッテリー値変更キャプション innerHTML */
   readonly changeBatteryCaption: string;
-  /** コウゲキボタンを押すキャプション innerHTML */
+  /** @deprecated コウゲキボタンを押すキャプション innerHTML */
   readonly pushAttackButtonCaption: string;
+  /** 攻撃バッテリー注釈 innerHTML */
+  readonly attackBatteryCaption: string;
   /** 防御バッテリー注釈 innerHTML */
   readonly defenseBatteryCaption: string;
 };
@@ -33,6 +36,7 @@ export function createBatterySystemTutorialProps(
     eventState,
     changeBatteryCaption: changeBatteryCaptionInnerHtml(),
     pushAttackButtonCaption: pushAttackButtonCaptionInnerHtml(),
+    attackBatteryCaption: attackBatteryCaptionInnerHtml(resources),
     defenseBatteryCaption: defenseBatteryCaptionInnerHtml(resources),
   };
 }
