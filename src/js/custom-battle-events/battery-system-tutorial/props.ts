58df9ebccdd36eef5041cccf23885bb87b304688
kaidouji85 <kaidouji85@gmail.com>
2026-01-30 16:49:08 +0900

    バッテリーシステムチュートリアルの不要なキャプションを削除し、ポインタの位置を調整

--- src/js/custom-battle-events/battery-system-tutorial/props.ts
+++ src/js/custom-battle-events/battery-system-tutorial/props.ts
@@ -1,18 +1,12 @@
 import { Resources } from "../../resource";
 import { attackBatteryCaptionInnerHtml } from "./dom/attack-battery-caption-inner-html";
-import { changeBatteryCaptionInnerHtml } from "./dom/change-battery-caption-inner-html";
 import { defenseBatteryCaptionInnerHtml } from "./dom/defense-battery-caption-inner-html";
-import { pushAttackButtonCaptionInnerHtml } from "./dom/push-attack-button-caption-inner-html";
 import { BatterySystemTutorialState } from "./state";
 
 /** バッテリーシステムチュートリアル固有のプロパティ */
 export type BatterySystemTutorialProps = {
   /** チュートリアルのステート */
   eventState: BatterySystemTutorialState;
-  /** @deprecated バッテリー値変更キャプション innerHTML */
-  readonly changeBatteryCaption: string;
-  /** @deprecated コウゲキボタンを押すキャプション innerHTML */
-  readonly pushAttackButtonCaption: string;
   /** 攻撃バッテリー注釈 innerHTML */
   readonly attackBatteryCaption: string;
   /** 防御バッテリー注釈 innerHTML */
@@ -34,8 +28,6 @@ export function createBatterySystemTutorialProps(
   };
   return {
     eventState,
-    changeBatteryCaption: changeBatteryCaptionInnerHtml(),
-    pushAttackButtonCaption: pushAttackButtonCaptionInnerHtml(),
     attackBatteryCaption: attackBatteryCaptionInnerHtml(resources),
     defenseBatteryCaption: defenseBatteryCaptionInnerHtml(resources),
   };
