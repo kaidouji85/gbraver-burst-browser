9d573ff8c97278f6de552fc16f1437badb0e2aa7
kaidouji85 <kaidouji85@gmail.com>
2026-01-30 15:46:49 +0900

    攻撃説明ストーリーのキャプションを修正し、不要なコードを削除

--- src/js/custom-battle-events/battery-system-tutorial/stories/attack-description.ts
+++ src/js/custom-battle-events/battery-system-tutorial/stories/attack-description.ts
@@ -1,9 +1,6 @@
 import { LastStateEventProps } from "../../../td-scenes/battle/custom-battle-event";
-import { waitTime } from "../../../wait/wait-time";
-import { waitUntilWindowPushWithStream } from "../../../wait/wait-until-window-push-with-stream";
 import { activeNearBatterySelectorMessageWindow } from "../../active-message-window";
 import { focusInBatterySelector } from "../../focus";
-import { invisibleAllMessageWindows } from "../../invisible-all-message-windows";
 import { BatterySystemTutorialProps } from "../props";
 
 /**
@@ -15,27 +12,8 @@ export async function attackDescription(
   props: Readonly<LastStateEventProps & BatterySystemTutorialProps>,
 ): Promise<void> {
   await focusInBatterySelector(props);
-  const signal = props.abort.getAbortController().signal;
-
   activeNearBatterySelectorMessageWindow(props);
   props.view.dom.nearBatterySelectorMessageWindow.messagesInInnerHTML(
-    props.changeBatteryCaption,
-  );
-  props.view.dom.nearBatterySelectorMessageWindow.scrollUp();
-  props.se.play(props.sounds.sendMessage);
-  await waitUntilWindowPushWithStream(props.pushWindow, { signal });
-
-  invisibleAllMessageWindows(props);
-  await props.view.hud.gameObjects.batterySelector.batteryPlus({ signal });
-  await waitTime(200, { signal });
-  await props.view.hud.gameObjects.batterySelector.batteryPlus({ signal });
-  await waitTime(200, { signal });
-  await props.view.hud.gameObjects.batterySelector.batteryPlus({ signal });
-  await waitTime(200, { signal });
-  await props.view.hud.gameObjects.batterySelector.batteryMinus({ signal });
-
-  activeNearBatterySelectorMessageWindow(props);
-  props.view.dom.nearBatterySelectorMessageWindow.messagesInInnerHTML(
-    props.pushAttackButtonCaption,
+    props.attackBatteryCaption,
   );
 }
