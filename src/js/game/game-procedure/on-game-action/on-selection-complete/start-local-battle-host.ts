7eeb5c5698db3ac07ebaadc34ae77f7c749fd7bc
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-23 23:36:46 +0900

    feat: ローカル対戦ホスト開始処理にstartLocalBattle関数を追加

--- src/js/game/game-procedure/on-game-action/on-selection-complete/start-local-battle-host.ts
+++ src/js/game/game-procedure/on-game-action/on-selection-complete/start-local-battle-host.ts
@@ -3,6 +3,7 @@ import { GameProps } from "../../../game-props";
 import { InProgress } from "../../../in-progress";
 import { LocalBattleHost } from "../../../in-progress/local-battle-host";
 import { Online } from "../../../network-context/online";
+import { startLocalBattle } from "../../start-local-battle";
 import { waitUntilLocalBattleMatchingAsHost } from "../../wait-until-local-battle-matching-as-host";
 
 /**
@@ -19,7 +20,7 @@ export const startLocalBattleHost = async (
 ): Promise<InProgress> => {
   props.networkContext.localHostSDK.disconnectWebRTC();
   const battle = await waitUntilLocalBattleMatchingAsHost(props, action);
-  // TODO ロジックを作る
+  await startLocalBattle(props, battle);
   return {
     ...props.inProgress,
     localBattleHost: { type: "Battle" },
