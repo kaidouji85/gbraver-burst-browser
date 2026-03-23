db07d2ce17fd75b8d790796187a535db67cbe2da
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-23 10:15:17 +0000

    feat: ローカル対戦（ゲスト）および（ホスト）のサブフローと型を追加

---
+++ src/js/game/in-progress/local-battle-host.ts
@@ -0,0 +1,19 @@
+/** ローカル対戦（ホスト）サブフロー キャラ選択 */
+export type PlayerSelect = {
+  type: "PlayerSelect";
+};
+
+/** ローカル対戦（ホスト）サブフロー 戦闘中 */
+export type Battle = {
+  type: "Battle";
+};
+
+/** ローカル対戦（ホスト）のサブフロー */
+export type LocalBattleHostSubFlow = PlayerSelect | Battle;
+
+/** ローカル対戦（ホスト） */
+export type LocalBattleHost = {
+  type: "LocalBattleHost";
+  /** サブフロー */
+  localBattleHost: LocalBattleHostSubFlow;
+};
