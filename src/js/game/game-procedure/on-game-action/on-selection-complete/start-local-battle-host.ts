30424aa50e60de51b1636c0f4729dde476a5b8dc
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-23 11:23:47 +0000

    feat: ローカル対戦（ホスト）マッチング待機処理とダイアログ切り替え処理を追加

--- src/js/game/game-procedure/on-game-action/on-selection-complete/start-local-battle-host.ts
+++ src/js/game/game-procedure/on-game-action/on-selection-complete/start-local-battle-host.ts
@@ -3,6 +3,7 @@ import { GameProps } from "../../../game-props";
 import { InProgress } from "../../../in-progress";
 import { LocalBattleHost } from "../../../in-progress/local-battle-host";
 import { Online } from "../../../network-context/online";
+import { waitUntilLocalBattleMatchingAsHost } from "../../wait-until-local-battle-matching-as-host";
 
 /**
  * ローカル対戦（ホスト）を開始する
@@ -11,10 +12,13 @@ import { Online } from "../../../network-context/online";
  * @returns inProgress更新結果
  */
 export const startLocalBattleHost = async (
-  props: GameProps & { networkContext: Online; inProgress: LocalBattleHost },
-  action: SelectionComplete,
+  props: Readonly<
+    GameProps & { networkContext: Online; inProgress: LocalBattleHost }
+  >,
+  action: Readonly<SelectionComplete>,
 ): Promise<InProgress> => {
   props.networkContext.localHostSDK.disconnectWebRTC();
+  const battle = await waitUntilLocalBattleMatchingAsHost(props, action);
   // TODO ロジックを作る
   return {
     ...props.inProgress,
