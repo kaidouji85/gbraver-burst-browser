0d3ed867c05f65f487ece9765edb21e24b907cab
Y.Takeuchi <kaidouji85@gmail.com>
2026-03-23 23:25:12 +0900

    feat: disconnectConnection関数を使用してWebSocket接続を切断する処理を統一

--- src/js/game/game-procedure/on-game-action/on-private-match-entry.ts
+++ src/js/game/game-procedure/on-game-action/on-private-match-entry.ts
@@ -2,6 +2,7 @@ import { MatchingDialog } from "../../../dom-dialogs/matching/matching-dialog";
 import { RejectPrivateMatchEntryDialog } from "../../../dom-dialogs/reject-private-match-entry";
 import { PrivateMatchEntry } from "../../game-actions/private-match-entry";
 import { GameProps } from "../../game-props";
+import { disconnectConnection } from "../disconnect-connection";
 import { startOnlineBattle } from "../start-online-battle";
 import { switchMatchingDialog } from "../switch-dialog/switch-matching-dialog";
 import { switchRejectPrivateMatchEntryDialog } from "../switch-dialog/switch-reject-private-match-entry-dialog";
@@ -29,7 +30,7 @@ export async function onPrivateMatchEntry(options: Options): Promise<void> {
   }
 
   switchMatchingDialog(props, new MatchingDialog(props));
-  await props.networkContext.sdk.disconnectWebsocket();
+  await disconnectConnection(props);
   const { armdozerId, pilotId } = props.inProgress.privateMatchGuest;
   const battle = await props.networkContext.sdk.enterPrivateMatchRoom(
     action.roomID,
