9d3c3f7f34e8a8733a45f5ece064be267546d4fc
Y.Takeuchi <kaidouji85@gmail.com>
2026-04-19 21:13:13 +0900

    feat: マッチング失敗メッセージのスタイルを追加し、アニメーションを改善

--- src/js/dom-dialogs/private-match-guest/procedure/flash-failed-message.ts
+++ src/js/dom-dialogs/private-match-guest/procedure/flash-failed-message.ts
@@ -10,10 +10,10 @@ export async function flashFailedMessage(props: PrivateMatchGuestDialogProps) {
   const { matchingFailed } = props;
   const animation = matchingFailed.animate(
     [
-      { transform: "translate(0, 40%)", opacity: 0 },
-      { transform: "translate(0, 0)", opacity: 1, offset: 0.2 },
+      { transform: "translate(0, 0%)", opacity: 0 },
+      { transform: "translate(0, -40%)", opacity: 1, offset: 0.2 },
       { opacity: 1, offset: 0.8 },
-      { transform: "translate(0, 0)", opacity: 0 },
+      { transform: "translate(0, -40%)", opacity: 0 },
     ],
     {
       duration: 2000,
