4d4f27b723d20a92c64e82f32837a679941708d2
Y.Takeuchi <kaidouji85@gmail.com>
2026-05-05 16:02:09 +0900

    feat: バトルモードのプレイヤー選択に絵文字を追加

--- src/js/game/game-procedure/on-game-action/on-arcade-start.ts
+++ src/js/game/game-procedure/on-game-action/on-arcade-start.ts
@@ -32,7 +32,11 @@ export async function onArcadeStart(options: Options): Promise<void> {
   await props.fader.fadeOut();
   const config = await props.config.load();
   await Promise.race([
-    bindPlayerSelectAccordingToConfig(props, config.playerSelectorType),
+    bindPlayerSelectAccordingToConfig(
+      props,
+      config.playerSelectorType,
+      "🕹️アーケード",
+    ),
     waitTime(MAX_LOADING_TIME),
   ]);
   await props.fader.fadeIn();
