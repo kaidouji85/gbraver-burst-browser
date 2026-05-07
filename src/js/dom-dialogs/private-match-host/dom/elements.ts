939c7bbf89e22bca8db86e54893bf4d6f593c180
Y.Takeuchi <kaidouji85@gmail.com>
2026-05-07 23:44:10 +0900

    fix: QRコード表示をキャンバスから画像に変更し、スタイルを追加

--- src/js/dom-dialogs/private-match-host/dom/elements.ts
+++ src/js/dom-dialogs/private-match-host/dom/elements.ts
@@ -7,17 +7,17 @@ export const extractCloser = (root: HTMLElement): HTMLElement =>
   root.querySelector(`[data-id="closer"]`) ?? document.createElement("div");
 
 /**
- * QRコード表示キャンバスを抽出する
+ * QRコード表示領域を抽出する
  * @param root ルート要素
  * @returns 抽出結果
  */
-export const extractQRCode = (root: HTMLElement): HTMLCanvasElement => {
+export const extractQRCode = (root: HTMLElement): HTMLImageElement => {
   const extractedQRCode = root.querySelector(
     `[data-id="qr-code"]`,
   ) as HTMLCanvasElement;
-  return extractedQRCode instanceof HTMLCanvasElement
+  return extractedQRCode instanceof HTMLImageElement
     ? extractedQRCode
-    : document.createElement("canvas");
+    : document.createElement("img");
 };
 
 /**
