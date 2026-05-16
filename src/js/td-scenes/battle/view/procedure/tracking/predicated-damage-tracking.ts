62976aba0b7c5456ec3d85b493ac0b744fcb9170
Y.Takeuchi <kaidouji85@gmail.com>
2026-05-16 18:48:43 +0900

    fix: プレイヤーと敵の予想ダメージトラッキングの位置を調整

--- src/js/td-scenes/battle/view/procedure/tracking/predicated-damage-tracking.ts
+++ src/js/td-scenes/battle/view/procedure/tracking/predicated-damage-tracking.ts
@@ -28,7 +28,7 @@ function playerPredicatedDamageTracking(
 ) {
   const { predicatedDamage, tdCamera, rendererDOM } = params;
   const origin = {
-    x: ARMDOZER_EFFECT_STANDARD_X - 150,
+    x: ARMDOZER_EFFECT_STANDARD_X - 100,
     y: ARMDOZER_EFFECT_STANDARD_Y,
     z: ARMDOZER_EFFECT_STANDARD_Z,
   };
@@ -44,7 +44,7 @@ function playerPredicatedDamageTracking(
 function enemyPredicatedDamageTracking(params: PredicatedDamageTrackingParams) {
   const { predicatedDamage, tdCamera, rendererDOM } = params;
   const origin = {
-    x: -ARMDOZER_EFFECT_STANDARD_X + 150,
+    x: -ARMDOZER_EFFECT_STANDARD_X + 100,
     y: ARMDOZER_EFFECT_STANDARD_Y,
     z: ARMDOZER_EFFECT_STANDARD_Z,
   };
