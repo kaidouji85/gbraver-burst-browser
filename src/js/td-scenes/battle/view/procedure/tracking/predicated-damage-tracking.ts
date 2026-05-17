7da60204be65793c3520243386a199832cf5d4b0
Y.Takeuchi <kaidouji85@gmail.com>
2026-05-17 13:46:21 +0900

    refactor: プレイヤーと敵のダメージ予想トラッキングを統合し、冗長なコードを削除

--- src/js/td-scenes/battle/view/procedure/tracking/predicated-damage-tracking.ts
+++ src/js/td-scenes/battle/view/procedure/tracking/predicated-damage-tracking.ts
@@ -1,57 +1,5 @@
-import * as THREE from "three";
-
-import { PredicatedDamage } from "../../../../../game-object/predicated-damage";
-import {
-  ARMDOZER_EFFECT_STANDARD_X,
-  ARMDOZER_EFFECT_STANDARD_Y,
-  ARMDOZER_EFFECT_STANDARD_Z,
-} from "../../../../../game-object/td-position";
 import { toHUDCoordinate } from "../../../../../tracking/to-hud-coordinate";
 import { TrackingParams } from "./tracking-params";
-
-/** ダメージ予想 トラッキング パラメータ */
-type PredicatedDamageTrackingParams = {
-  /** ダメージ予想 */
-  predicatedDamage: PredicatedDamage;
-  /** 3Dレイヤーカメラ */
-  tdCamera: THREE.PerspectiveCamera;
-  /** レンダラのHTML要素 */
-  rendererDOM: HTMLElement;
-};
-
-/**
- * ダメージ予想のトラッキング（プレイヤー側）
- * @param params パラメータ
- */
-function playerPredicatedDamageTracking(
-  params: PredicatedDamageTrackingParams,
-) {
-  const { predicatedDamage, tdCamera, rendererDOM } = params;
-  const origin = {
-    x: ARMDOZER_EFFECT_STANDARD_X - 30,
-    y: ARMDOZER_EFFECT_STANDARD_Y,
-    z: ARMDOZER_EFFECT_STANDARD_Z,
-  };
-  const hudCoordinate = toHUDCoordinate(origin, tdCamera, rendererDOM);
-  predicatedDamage.getObject3D().position.x = hudCoordinate.x;
-  predicatedDamage.getObject3D().position.y = hudCoordinate.y;
-}
-
-/**
- * ダメージ予想のトラッキング（敵側）
- * @param params パラメータ
- */
-function enemyPredicatedDamageTracking(params: PredicatedDamageTrackingParams) {
-  const { predicatedDamage, tdCamera, rendererDOM } = params;
-  const origin = {
-    x: -ARMDOZER_EFFECT_STANDARD_X + 30,
-    y: ARMDOZER_EFFECT_STANDARD_Y,
-    z: ARMDOZER_EFFECT_STANDARD_Z,
-  };
-  const hudCoordinate = toHUDCoordinate(origin, tdCamera, rendererDOM);
-  predicatedDamage.getObject3D().position.x = hudCoordinate.x;
-  predicatedDamage.getObject3D().position.y = hudCoordinate.y;
-}
 
 /**
  * 予想ダメージのトラッキング
@@ -59,17 +7,19 @@ function enemyPredicatedDamageTracking(params: PredicatedDamageTrackingParams) {
  */
 export function predicatedDamageTracking(params: TrackingParams) {
   const { td, hud, rendererDOM, playerId } = params;
-  hud.players.forEach(({ playerId: currentPlayerId, predicatedDamage }) => {
-    const isPlayer = currentPlayerId === playerId;
-    const trackingParams = {
-      tdCamera: td.camera.getCamera(),
-      rendererDOM,
-      predicatedDamage,
-    };
-    if (isPlayer) {
-      playerPredicatedDamageTracking(trackingParams);
-    } else {
-      enemyPredicatedDamageTracking(trackingParams);
+  hud.players.forEach(({ predicatedDamage }) => {
+    const armdozer = td.armdozers.find((a) => a.playerId === playerId);
+    if (!armdozer) {
+      return;
     }
+
+    const origin = armdozer.sprite().predicatedDamagePosition;
+    const hudCoordinate = toHUDCoordinate(
+      origin,
+      td.camera.getCamera(),
+      rendererDOM,
+    );
+    predicatedDamage.getObject3D().position.x = hudCoordinate.x;
+    predicatedDamage.getObject3D().position.y = hudCoordinate.y;
   });
 }
