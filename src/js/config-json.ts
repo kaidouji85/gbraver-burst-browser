a50db03dd17ca14ce3d065626d8b37687422db05
Y.Takeuchi <kaidouji85@gmail.com>
2026-06-14 15:15:27 +0900

    config.jsonとconfig-json.tsを追加し、webpack設定にconfig.jsonのコピーを追加

---
+++ src/js/config-json.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+/**
+ * config.jsonのデータ型
+ * リリース後のアプリ挙動を手軽に変更するために利用する
+ */
+export type ConfigJSON = {
+  "can-network-features": boolean;
+};
+
+/** ConfigJSON zod スキーマ */
+export const ConfigJSONSchema = z.object({
+  "can-network-features": z.boolean(),
+});
+
+/**
+ * config.jsonを取得する
+ * @returns 取得結果
+ */
+export const fetchConfigJSON = async (): Promise<ConfigJSON> => {
+  const response = await fetch("config.json");
+  const json = await response.json();
+  return ConfigJSONSchema.parse(json);
+};
