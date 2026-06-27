c5067a554d452b7457ec7ef96cd7bd1d8326823f
Y.Takeuchi <kaidouji85@gmail.com>
2026-06-27 11:59:17 +0900

    preLoadImage関数をプライベートに変更し、preloadImages関数を追加

--- src/js/resource/path/pre-load-image.ts
+++ src/js/resource/path/pre-load-image.ts
@@ -1,9 +1,12 @@
+import { Resources } from "..";
+import { PathId } from "./resource";
+
 /**
  * 画像をプリロードする
  * @param src 画像のパス
  * @returns プリロードが完了したら発火するPromise
  */
-export const preLoadImage = (src: string) => {
+const preLoadImage = (src: string) => {
   const img = new Image();
   let onLoad: null | (() => void) = null;
   let onError: null | ((e: Event) => void) = null;
@@ -22,4 +25,20 @@ export const preLoadImage = (src: string) => {
       img.removeEventListener("error", onError);
     }
   });
+};
+
+/**
+ * 指定されたパスIDの画像をプリロードする
+ * @param resources リソース管理オブジェクト
+ * @param pathIds プリロードするパスIDの配列
+ * @returns プリロードが完了したら発火するPromise
+ */
+export const preloadImages = (resources: Resources, pathIds: PathId[]) => {
+  const uniquePathIds = Array.from(new Set(pathIds));
+  return Promise.all(
+    uniquePathIds
+      .map((pathId) => resources.paths.find((p) => p.id === pathId)?.path)
+      .filter((p) => p !== undefined)
+      .map((path) => preLoadImage(path)),
+  );
 };
