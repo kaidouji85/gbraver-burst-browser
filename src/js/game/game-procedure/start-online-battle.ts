cecaa81154351f9661022f7c5be1bf4eb22912be
Y.Takeuchi <kaidouji85@gmail.com>
2026-06-28 16:06:06 +0900

    プリロード画像のインポートを修正し、カスタムバトルイベントのプリロード画像を追加

--- src/js/game/game-procedure/start-online-battle.ts
+++ src/js/game/game-procedure/start-online-battle.ts
@@ -7,7 +7,10 @@ import { NetworkErrorDialog } from "../../dom-dialogs/network-error/network-erro
 import { WaitingDialog } from "../../dom-dialogs/waiting/waiting-dialog";
 import { MAX_LOADING_TIME } from "../../dom-scenes/dom-scene-binder/max-loading-time";
 import { MatchCard } from "../../dom-scenes/match-card";
-import { preloadBattleSceneImages } from "../../resource/preload-images";
+import {
+  preloadBattleSceneImages,
+  preloadImages,
+} from "../../resource/preload-images";
 import { SOUND_IDS } from "../../resource/sound/ids";
 import { updateBattleSceneResources } from "../../resource/update-battle-scene-resources";
 import { BattleScene } from "../../td-scenes/battle";
@@ -85,12 +88,14 @@ export async function startOnlineBattle(
   const config = await props.config.load();
   props.renderer.setPixelRatio(config.webGLPixelRatio);
   const players: [Player, Player] = [battle.player, battle.enemy];
+  const customBattleEvent = createSeriousMatchEvent();
   const [updatedResources] = await Promise.all([
     updateBattleSceneResources({
       resources: props.resources,
       players,
     }),
     preloadBattleSceneImages(props.resources, players),
+    preloadImages(props.resources, customBattleEvent.preloadImagePathIds),
   ]);
   props.resources = updatedResources;
   const battleScene = new BattleScene({
@@ -104,7 +109,7 @@ export async function startOnlineBattle(
     controllerType: config.battleControllerType,
     playerPilotVisibility: config.playerPilotVisibility,
     emergencyStop: battle.suddenlyBattleEndNotifier(),
-    customBattleEvent: createSeriousMatchEvent(),
+    customBattleEvent,
     canRetry: false,
   });
   bindBattleScene(props, battleScene);
