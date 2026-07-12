64e19245937f297070282f4aeb99aa3c67a7a861
Y.Takeuchi <kaidouji85@gmail.com>
2026-07-12 17:08:01 +0900

    照明モデルに色味プロパティを追加し、初期値生成関数を更新。照明強さのコメントを明確化。

--- src/js/game-object/illumination/model/illumination-model.ts
+++ src/js/game-object/illumination/model/illumination-model.ts
@@ -2,7 +2,17 @@
 export type IlluminationModel = {
   /**
    * 照明の強さ
-   * 大きいほど明るくなる
+   * 0から1で指定して大きいほど明るくなる
    */
   intensity: number;
+
+  /** 照明の色味、0から1で指定する */
+  color: {
+    /** 赤成分 */
+    r: number;
+    /** 緑成分 */
+    g: number;
+    /** 青成分 */
+    b: number;
+  };
 };
