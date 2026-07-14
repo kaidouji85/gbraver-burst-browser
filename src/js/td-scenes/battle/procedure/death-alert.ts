33b056e3e2ca58a48c2e930dff9f24a3993eb667
Y.Takeuchi <kaidouji85@gmail.com>
2026-07-14 22:22:39 +0900

    デスアラートの再生状態判定を関数化し、重複コードを削除。関数呼び出しを統一して可読性を向上。

--- src/js/td-scenes/battle/procedure/death-alert.ts
+++ src/js/td-scenes/battle/procedure/death-alert.ts
@@ -11,17 +11,26 @@ type DeathAlertProps = Readonly<SEPlayerContainer> & {
 };
 
 /**
+ * デスアラートがすでに再生中かどうかを判定する
+ * @param props ゲームプロパティ
+ * @returns 判定結果、trueなら再生中
+ */
+const isDeathAlertAlreadyPlaying = (props: Readonly<DeathAlertProps>) =>
+  // デスアラートの効果音が再生されていれば、デスアラートは再生中と判定する
+  // デスアラート音はループ再生されるので、再生中かどうかの判定はこの条件で十分である
+  props.sounds.deathAlert.sound.playing();
+
+/**
  * プレイヤーのデスアラートを開始する
  * @param props ゲームプロパティ
  */
 export const startPlayerDeathAlert = (props: Readonly<DeathAlertProps>) => {
-  const isDeathAlertAlreadyPlaying = props.sounds.deathAlert.sound.playing();
-  if (isDeathAlertAlreadyPlaying) {
+  if (isDeathAlertAlreadyPlaying(props)) {
     return;
   }
 
   props.se.loop(props.sounds.deathAlert);
-  //props.view.hud.gameObjects.deathAlertVignette.fadeOut().play();
+  props.view.hud.gameObjects.deathAlertVignette.fadeOut().play();
 };
 
 /**
@@ -29,6 +38,10 @@ export const startPlayerDeathAlert = (props: Readonly<DeathAlertProps>) => {
  * @param props ゲームプロパティ
  */
 export const stopDeathAlert = (props: Readonly<DeathAlertProps>) => {
+  if (!isDeathAlertAlreadyPlaying(props)) {
+    return;
+  }
+
   props.sounds.deathAlert.sound.stop();
-  //props.view.hud.gameObjects.deathAlertVignette.fadeIn().play();
+  props.view.hud.gameObjects.deathAlertVignette.fadeIn().play();
 };
