08608f3a0682112f2e4fbba9edff1f1504208071
Y.Takeuchi <kaidouji85@gmail.com>
2026-07-14 22:10:42 +0900

    フェードイン・フェードアウト関数にdurationパラメータを追加し、アニメーション時間を柔軟に設定できるように変更

--- src/js/game-object/fader/animation/fade-in.ts
+++ src/js/game-object/fader/animation/fade-in.ts
@@ -5,8 +5,9 @@ import type { FaderModel } from "../model/fader-model";
 /**
  * フェードイン
  * @param model モデル
+ * @param duration アニメーションの時間（ミリ秒）
  * @returns アニメーション
  */
-export function fadeIn(model: FaderModel): Animate {
-  return tween(model, (t) => t.to({ opacity: 0 }, 500));
+export function fadeIn(model: FaderModel, duration: number): Animate {
+  return tween(model, (t) => t.to({ opacity: 0 }, duration));
 }
--- src/js/game-object/fader/animation/fade-out.ts
+++ src/js/game-object/fader/animation/fade-out.ts
@@ -5,8 +5,9 @@ import type { FaderModel } from "../model/fader-model";
 /**
  * フェードアウト
  * @param model モデル
+ * @param duration アニメーションの時間（ミリ秒）
  * @returns アニメーション
  */
-export function fadeOut(model: FaderModel): Animate {
-  return tween(model, (t) => t.to({ opacity: 1 }, 500));
+export function fadeOut(model: FaderModel, duration: number): Animate {
+  return tween(model, (t) => t.to({ opacity: 1 }, duration));
 }
