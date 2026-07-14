a29bf974f6fbed9cdf63c75497ee2782d239140c
Y.Takeuchi <kaidouji85@gmail.com>
2026-07-14 09:11:24 +0900

    デスアラートビネットのストーリーを新規作成し、フェードイン・フェードアウトのアニメーションを追加

---
+++ stories/fader.stories.ts
@@ -0,0 +1,19 @@
+import { delay } from "../src/js/animation/delay";
+import { deathAlertVignette } from "../src/js/game-object/fader";
+import { hudGameObjectStory } from "./stub/hud-game-object-stub";
+
+export default {
+  title: "fader",
+};
+
+/** デスアラートビネット */
+export const deathAlert = hudGameObjectStory((params) => {
+  const vignette = deathAlertVignette({ ...params, isVisible: false });
+  delay(1000)
+    .chain(vignette.fadeIn())
+    .chain(delay(1000))
+    .chain(vignette.fadeOut())
+    .chain(delay(1000))
+    .loop();
+  return [vignette.getObject3D()];
+});
