a239a6d5df15640f406962d7af65b7fd5828f0bf
Y.Takeuchi <kaidouji85@gmail.com>
2026-07-20 09:13:18 +0900

    downメソッドの遅延処理の時間を調整し、アニメーションの流れを改善

--- src/js/td-scenes/battle/animation/game-state/battle/attack/neo-landozer/down.ts
+++ src/js/td-scenes/battle/animation/game-state/battle/attack/neo-landozer/down.ts
@@ -27,7 +27,7 @@ export function down(param: NeoLandozerBattle<DownResult>): Animate {
       all(
         param.tdObjects.skyBrightness.brightness(0.3, 100),
         param.tdObjects.illumination.intensity(0.3, 100),
-        delay(1900).chain(param.attackerSprite.hmToStand()).chain(delay(500)),
+        delay(1800).chain(param.attackerSprite.hmToStand()).chain(delay(600)),
         toInitial(param.tdCamera, 100),
         param.defenderTD.damageIndicator.popUp(param.result.damage),
         onStart(() => param.bgm.do(stop)),
