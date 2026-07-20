6926bff4b24552aaade9d0163c38b05520d0265f
Y.Takeuchi <kaidouji85@gmail.com>
2026-07-20 14:35:35 +0900

    敵デスアラートの処理を追加し、関連するインポートを更新

--- stories/death-alert.stories.ts
+++ stories/death-alert.stories.ts
@@ -1,6 +1,7 @@
 import { delay } from "../src/js/animation/delay";
 import { onStart } from "../src/js/animation/on-start";
 import { DeathAlert } from "../src/js/game-object/death-alert";
+import { ENEMY_DEATH_ALERT_COLOR } from "../src/js/game-object/death-alert/color";
 import { hudGameObjectStory } from "./stub/hud-game-object-stub";
 
 export default {
@@ -12,6 +13,18 @@ export const playerDeathAlert = hudGameObjectStory((params) => {
   const alert = new DeathAlert(params);
   delay(1000)
     .chain(onStart(() => alert.play(200)))
+    .chain(delay(1000))
+    .chain(onStart(() => alert.stop(200)))
+    .chain(delay(1000))
+    .loop();
+  return [alert.getObject3D()];
+});
+
+/** 敵デスアラート */
+export const enemyDeathAlert = hudGameObjectStory((params) => {
+  const alert = new DeathAlert(params);
+  delay(1000)
+    .chain(onStart(() => alert.play(200, ENEMY_DEATH_ALERT_COLOR)))
     .chain(delay(1000))
     .chain(onStart(() => alert.stop(200)))
     .chain(delay(1000))
