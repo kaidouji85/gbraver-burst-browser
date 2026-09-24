50de2f3168dd6ec0ca20a7cece72579cb27976a1
Y.Takeuchi <kaidouji85@gmail.com>
2026-09-25 00:14:20 +0900

    パイロットピッカーダイアログにタイトルへボタンと再戦ボタンの抽出処理を追加

--- src/js/dom-dialogs/player-picker/dom/extract-element.ts
+++ src/js/dom-dialogs/player-picker/dom/extract-element.ts
@@ -21,3 +21,26 @@ export const extractPilotPicker = (root: HTMLElement): HTMLElement => {
     document.createElement("div")
   );
 };
+
+/**
+ * ルートHTML要素からタイトルへボタンを抽出する
+ * @param root ルートHTML要素
+ * @returns タイトルへボタンのHTML要素（存在しない場合は空のdiv要素）
+ */
+export const extractGotoTitleButton = (root: HTMLElement): HTMLElement => {
+  return (
+    root.querySelector(`[data-id="goto-title"]`) ??
+    document.createElement("div")
+  );
+};
+
+/**
+ * ルートHTML要素から再戦ボタンを抽出する
+ * @param root ルートHTML要素
+ * @returns 再戦ボタンのHTML要素（存在しない場合は空のdiv要素）
+ */
+export const extractRetryButton = (root: HTMLElement): HTMLElement => {
+  return (
+    root.querySelector(`[data-id="retry"]`) ?? document.createElement("div")
+  );
+};
